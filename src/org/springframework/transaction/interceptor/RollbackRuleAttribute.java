c4482616ec8271a69a74f2a36b95361361b60c85
Juergen Hoeller <jhoeller@interface21.com>
2003-08-18 16:21:04 +0000

    added getExceptionName
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@18 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/interceptor/RollbackRuleAttribute.java
+++ src/org/springframework/transaction/interceptor/RollbackRuleAttribute.java
@@ -5,32 +5,24 @@
  
 package org.springframework.transaction.interceptor;
 
-//import attrib4j.Attribute;
-
 /**
- * Rule determining whether or not a given exception 
- * (and any subclasses) should
- * cause a rollback. Multiple such rules can be applied to
- * determine whether a transaction should commit or rollback
- * after an exception has been thrown.
- *
+ * Rule determining whether or not a given exception (and any subclasses)
+ * should cause a rollback. Multiple such rules can be applied to
+ * determine whether a transaction should commit or rollback after an
+ * exception has been thrown.
  * @since 09-Apr-2003
- * @version $Id: RollbackRuleAttribute.java,v 1.1.1.1 2003-08-14 16:20:40 trisberg Exp $
+ * @version $Id: RollbackRuleAttribute.java,v 1.2 2003-08-18 16:21:04 jhoeller Exp $
  * @author Rod Johnson
  */
-public class RollbackRuleAttribute
-// TODO currently need to implement Attrib4j tag interface for metadata operation.
-// This restriction should soon disappear.
-//implements Attribute 
-{
+public class RollbackRuleAttribute {
 	
 	public static final RollbackRuleAttribute ROLLBACK_ON_RUNTIME_EXCEPTIONS = new RollbackRuleAttribute("java.lang.RuntimeException");
 	
-	// Could hold exception, resolving classname
-	// but would always require FQN.
-	// This way does multiple string comparisons,
-	// but how often do we decide whether to roll back
-	// a transaction following an exception?
+	/**
+	 * Could hold exception, resolving classname but would always require FQN.
+	 * This way does multiple string comparisons, but how often do we decide
+	 * whether to roll back a transaction following an exception?
+	 */
 	private final String exceptionName;
 
 	/**
@@ -43,22 +35,23 @@ public class RollbackRuleAttribute
 	public RollbackRuleAttribute(String exceptionName) {
 		this.exceptionName = exceptionName;
 	}
-	
-	
+
+	/**
+	 * Return the pattern for the exception name.
+	 */
+	public String getExceptionName() {
+		return exceptionName;
+	}
+
 	/**
 	 * Return the depth to the superclass matching.
-	 * 0 means t matches.
-	 * Return -1 if there's no match.
-	 * Otherwise, return depth.
-	 * Lowest depth wins.
-	 * @param t
-	 * @return int
+	 * 0 means t matches. Return -1 if there's no match.
+	 * Otherwise, return depth. Lowest depth wins.
 	 */
 	public int getDepth(Throwable t) {
 		return getDepth(t.getClass(), 0);
 	}
-	
-	
+
 	private int getDepth(Class exceptionClass, int depth) {
 		if (exceptionClass.getName().indexOf(this.exceptionName) != -1)
 			// Found it!
@@ -74,4 +67,5 @@ public class RollbackRuleAttribute
 	public String toString() {
 		return "RollbackRule: pattern='" + this.exceptionName + "'";
 	}
+
 }
