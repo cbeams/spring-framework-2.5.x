46f7b146980c7b43cfa5dc3067ef44d8824cb24d
Juergen Hoeller <jhoeller@interface21.com>
2003-08-21 15:44:56 +0000

    extends IllegalArgumentException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@48 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/AopConfigException.java
+++ src/org/springframework/aop/framework/AopConfigException.java
@@ -2,25 +2,19 @@
  * The Spring Framework is published under the terms
  * of the Apache Software License.
  */
- 
+
 package org.springframework.aop.framework;
 
-import org.springframework.core.NestedRuntimeException;
-
 /**
- * 
+ * Exception that gets thrown on illegal AOP configuration arguments.
  * @author Rod Johnson
  * @since 13-Mar-2003
- * @version $Revision: 1.1.1.1 $
+ * @version $Revision: 1.2 $
  */
-public class AopConfigException extends NestedRuntimeException {
+public class AopConfigException extends IllegalArgumentException {
 
-	/**
-	 * @param mesg
-	 */
-	public AopConfigException(String mesg) {
-		super(mesg);
+	public AopConfigException(String msg) {
+		super(msg);
 	}
-
 
 }
