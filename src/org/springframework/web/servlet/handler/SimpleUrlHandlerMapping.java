5420a900c9dca136c5f4609b94ee9e0f7f9b0e7c
Juergen Hoeller <jhoeller@interface21.com>
2003-08-23 22:13:50 +0000

    added urlMap property, reworked initHandler
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@64 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/handler/SimpleUrlHandlerMapping.java
+++ src/org/springframework/web/servlet/handler/SimpleUrlHandlerMapping.java
@@ -12,20 +12,24 @@
 package org.springframework.web.servlet.handler;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
 /**
  * Implementation of the HandlerMapping interface to map from URLs
  * to request handler beans.
  *
- * <p>Mappings are set via the "mappings" property, in a form accepted
- * by the java.util.Properties class, like as follows:<br>
+ * <p>The "urlMap" property is suitable for populating the map values
+ * as bean references, e.g. via the map element in XML bean definitions.
+ *
+ * <p>Mappings can also be set via the "mappings" property, in a form
+ * accepted by the java.util.Properties class, like as follows:<br>
  * <code>
  * /welcome.html=ticketController
  * /show.html=ticketController
  * </code><br>
  * The syntax is PATH=HANDLER_BEAN_NAME.
- * If the path doesn't begin with a /, one is prepended.
+ * If the path doesn't begin with a slash, one is prepended.
  *
  * <p>Supports direct matches (given "/test" -> registered "/test")
  * and "*" matches (given "/test" -> registered "/t*").
@@ -33,43 +37,65 @@ import java.util.Properties;
  * @see org.springframework.web.servlet.DispatcherServlet
  * @author Rod Johnson
  * @author Juergen Hoeller
+ * @see java.util.Properties
  */
 public class SimpleUrlHandlerMapping extends AbstractUrlHandlerMapping {
 	
-	private Properties mappings;
-	
+	private Map urlMap;
+
 	/**
-	 * Set mappings from a properties object. This property must
-	 * be set to configure this object.
-	 * @param mappings properties
-	 * @see java.util.Properties
+	 * Set a Map with URLs as keys and handler beans as values.
+	 * Convenient for population with bean references.
+	 * @param urlMap map with URLs as keys and beans as values
+	 */
+	public void setUrlMap(Map urlMap) {
+		this.urlMap = urlMap;
+	}
+
+	/**
+	 * Set URL to handler bean name mappings from a Properties object.
+	 * @param mappings properties with URL as key and bean name as value
 	 */
 	public void setMappings(Properties mappings) {
-		this.mappings = mappings;
-		logger.debug("Set properties to [" + mappings + "]");
+		this.urlMap = mappings;
 	}
 
 	public void initApplicationContext() {
-		if (!this.mappings.isEmpty()) {
-			Iterator itr = mappings.keySet().iterator();
+		if (this.urlMap == null) {
+			throw new IllegalArgumentException("Either 'urlMap' or 'mappings' is required");
+		}
+		
+		if (!this.urlMap.isEmpty()) {
+			Iterator itr = this.urlMap.keySet().iterator();
 			while (itr.hasNext()) {
 				String url = (String) itr.next();
-				String beanName = this.mappings.getProperty(url);
-				logger.info("Controller mapping from URL '" + url + "' to '" + beanName + "'");
+				Object handler = this.urlMap.get(url);
+
+				if (handler instanceof String) {
+					// convert bean name (assumably from "mappings") to bean instance
+					handler = getApplicationContext().getBean((String) handler);
+				}
+				initHandler(handler, url);
+
 				if ("*".equals(url)) {
-					setDefaultHandler(initHandler(beanName, url));
+					// register default handler
+					if (getDefaultHandler() != null) {
+						throw new IllegalArgumentException("Cannot apply * mapping: default handler is already set");
+					}
+					setDefaultHandler(handler);
 				}
 				else {
-					// Prepend with / if it's not present
-					if (!url.startsWith("/"))
-						url = "/" + url;					
-					Object handler = initHandler(beanName, url);
+					// register specific handler
+					// prepend with slash if it's not present
+					if (!url.startsWith("/")) {
+						url = "/" + url;
+					}
 					registerHandler(url, handler);
 				}
 			}
 		}
 		else {
-			logger.warn("No mappings in " + getClass().getName());
+			logger.warn("No mappings defined in " + getClass().getName());
 		}
 	}
 
