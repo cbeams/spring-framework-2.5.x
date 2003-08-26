bb2727fbf93f4a5c093b0b94d726733b9aaa469f
Juergen Hoeller <jhoeller@interface21.com>
2003-08-26 10:17:27 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@72 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/PropertyValues.java
+++ src/org/springframework/beans/PropertyValues.java
@@ -1,36 +1,32 @@
-
 package org.springframework.beans;
 
 /**
- * Object containing 0 or more PropertyValues comprising
- * one update.
- * @author  Rod Johnson
+ * Object containing 0 or more PropertyValues comprising one update.
+ * @author Rod Johnson
  * @since 13 May 2001
- * @version $Id: PropertyValues.java,v 1.1.1.1 2003-08-14 16:20:17 trisberg Exp $
+ * @version $Id: PropertyValues.java,v 1.2 2003-08-26 10:17:27 jhoeller Exp $
  */
 public interface PropertyValues {
    
 	/** 
-	 * Return an array of the PropertyValue objects
-	 * held in this object.
-	 * @return an array of the PropertyValue objects
-	 * held in this object.
+	 * Return an array of the PropertyValue objects held in this object.
+	 * @return an array of the PropertyValue objects held in this object.
 	 */
 	PropertyValue[] getPropertyValues();	
 	
-	/** 
-	 * Is there a propertyValue object for this property?
-	 * @param propertyName name of the property we're interested in
-	 *@return whether there is a propertyValue object for this property?
-	 */
-	boolean contains(String propertyName);
-	
-	/** 
-	 * Return the property value with the given name
+	/**
+	 * Return the property value with the given name.
 	 * @param propertyName name to search for
 	 * @return pv or null
 	 */
 	PropertyValue getPropertyValue(String propertyName);
+
+	/**
+	 * Is there a property value for this property?
+	 * @param propertyName name of the property we're interested in
+	 * @return whether there is a property value for this property
+	 */
+	boolean contains(String propertyName);
 	
 	/**
 	 * Return the changes since the previous PropertyValues.
