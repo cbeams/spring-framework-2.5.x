49b319f4b02a9a6b9c6d77736cf3cdbf9a76a4fa
Juergen Hoeller <jhoeller@interface21.com>
2003-10-04 09:49:05 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@201 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/propertyeditors/PropertiesEditor.java
+++ src/org/springframework/beans/propertyeditors/PropertiesEditor.java
@@ -7,19 +7,20 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
-import java.util.StringTokenizer;
 
 /**
- * Editor for java.util.Properties objects. Handles
- * conversion from String to Properties object. Not
- * a GUI editor.
- * <br>NB: this editor must be registered with the JavaBeans API before it
- * will be available. Editors in this package are
- * registered by BeanWrapperImpl.
- * <br>The required format is defined in java.util.Properties documentation.
+ * Editor for java.util.Properties objects. Handles conversion from String
+ * to Properties object. Not a GUI editor.
+ *
+ * <p>This editor must be registered with the JavaBeans API before it will be
+ * available. Editors in this package are registered by BeanWrapperImpl.
+ *
+ * <p>The required format is defined in java.util.Properties documentation.
  * Each property must be on a new line.
+ *
  * @author Rod Johnson
- * @version $Id: PropertiesEditor.java,v 1.1.1.1 2003-08-14 16:20:21 trisberg Exp $
+ * @version $Id: PropertiesEditor.java,v 1.2 2003-10-04 09:49:05 jhoeller Exp $
+ * @see org.springframework.beans.BeanWrapperImpl
  */
 public class PropertiesEditor extends PropertyEditorSupport {
 	
@@ -34,52 +35,12 @@ public class PropertiesEditor extends PropertyEditorSupport {
 	 * @see java.beans.PropertyEditor#setAsText(String)
 	 */
 	public void setAsText(String s) throws IllegalArgumentException {
-		
 		if (s == null)
 			throw new IllegalArgumentException("Cannot set properties to null");
-	
-		Properties props =
-			load(s); 
-			//parse(s);
+		Properties props = load(s);
 		setValue(props);
 	}
 	
-	
-	/**
-	 * Parse the string ourselves.
-	 * Workaround for Orion 1.6 issue
-	 * @param s
-	 * @return Properties
-	 */
-	private Properties parse(String s) {
-		Properties props = new Properties();
-		
-		// Zap whitespace
-		StringTokenizer st = new StringTokenizer(s);
-		while (st.hasMoreTokens()) {
-			String tok = st.nextToken();
-
-			// Tokens look like "/welcome.html=mainController"
-			int eqpos = tok.indexOf("=");
-			if (eqpos == -1) {
-				// We only have the property name, but
-				// the value is the the empty string
-				props.put(tok, "");
-			}
-			else {
-				String key = tok.substring(0, eqpos);
-				String value = tok.substring(eqpos + 1);
-				props.put(key, value);
-			}
-		}
-		
-		return props;
-	}	// parse
-
-
-	/** NB: the following code, using properties default works in JBoss 3.0.0,
-	 * but not Orion 1.6
-	*/
 	private Properties load(String s) {
 		Properties props = new Properties();
 		try {
@@ -90,10 +51,8 @@ public class PropertiesEditor extends PropertyEditorSupport {
 			// Shouldn't happen
 			throw new IllegalArgumentException("Failed to read String");
 		}
-
 		return props;
 	}
-
 
 	/**
 	 * Remove comment lines. We shouldn't need to do this,
@@ -120,4 +79,3 @@ public class PropertiesEditor extends PropertyEditorSupport {
 	}
 
 }
-
