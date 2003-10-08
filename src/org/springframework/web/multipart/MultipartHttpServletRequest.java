e4c6452464381b7018b5dcff72a1b76a83975f11
Juergen Hoeller <jhoeller@interface21.com>
2003-10-08 15:31:06 +0000

    polished javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@244 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/multipart/MultipartHttpServletRequest.java
+++ src/org/springframework/web/multipart/MultipartHttpServletRequest.java
@@ -7,29 +7,32 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * Interface which provides additional methods for dealing with multipart content
- * within a servlet request.
+ * within a servlet request, allowing to access uploaded files.
  *
- * <p>A concrete implementation is DefaultMultipartHttpServletRequest.
+ * <p>A concrete implementation is DefaultMultipartHttpServletRequest. As an
+ * intermediate step, AbstractMultipartHttpServletRequest can be subclassed.
  *
  * @author Juergen Hoeller
  * @author Trevor D. Cook
  * @since 29-Sep-2003
- * @see org.springframework.web.multipart.MultipartResolver
+ * @see MultipartResolver
+ * @see MultipartFile
  * @see org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest
+ * @see org.springframework.web.multipart.support.AbstractMultipartHttpServletRequest
  */
 public interface MultipartHttpServletRequest extends HttpServletRequest {
 
 	/**
 	 * Return an Iterator of String objects containing the parameter names of the
-	 * multipart files contained in this request.  These are the names reference
-	 * in the request, not the actual filenames.
+	 * multipart files contained in this request. These are the field names of
+	 * the form (like with normal parameters), not the original file names.
 	 * @return the names of the files
 	 */
 	Iterator getFileNames();
 
 	/**
-	 * Return the contents/description of an uploaded file in this request, or
-	 * null if it does not exist.
+	 * Return the contents/description of an uploaded file in this request,
+	 * or null if it does not exist.
 	 * @param name a String specifying the parameter name of the multipart file
 	 * @return the uploaded content 
 	 */
@@ -37,8 +40,9 @@ public interface MultipartHttpServletRequest extends HttpServletRequest {
 
 	/**
 	 * Return a Map of the multipart files contained in this request.
-	 * @return a map containing the parameter names as the key, and the multipart
-	 * file as the value.
+	 * @return a map containing the parameter names as keys, and the
+	 * MultipartFile instances file as values
+	 * @see MultipartFile
 	 */
 	Map getFileMap();
 
