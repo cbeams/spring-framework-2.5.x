f1fd075f961cc80425261216de91834ea6a9945d
Juergen Hoeller <jhoeller@interface21.com>
2003-10-10 14:08:37 +0000

    added BeanPostProcessor test that injects implicit CGLIB proxies
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@269 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/context/support/StaticApplicationContextTestSuite.java
+++ test/org/springframework/context/support/StaticApplicationContextTestSuite.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
+import org.springframework.aop.framework.InvokerInterceptor;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.TestBean;
 import org.springframework.beans.factory.LBIInit;
+import org.springframework.beans.factory.support.BeanPostProcessor;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ACATest;
 import org.springframework.context.AbstractApplicationContextTests;
 import org.springframework.context.ApplicationContext;
@@ -18,14 +20,11 @@ import org.springframework.context.BeanThatListens;
  * Classname doesn't match XXXXTestSuite pattern, so as to avoid
  * being invoked by Ant JUnit run, as it's abstract
  * @author Rod Johnson
- * @version $Revision: 1.1.1.1 $
+ * @version $Revision: 1.2 $
  */
 public class StaticApplicationContextTestSuite extends AbstractApplicationContextTests {
 
 	protected StaticApplicationContext sac;
-
-	public StaticApplicationContextTestSuite() throws Exception {
-	}
 
 	/** Run for each test */
 	protected ApplicationContext createContext() throws Exception {
@@ -33,9 +32,9 @@ public class StaticApplicationContextTestSuite extends AbstractApplicationContex
 		parent.addListener(parentListener) ;
 		Map m = new HashMap();
 		m.put("name", "Roderick");
-		parent.registerPrototype("rod", org.springframework.beans.TestBean.class, new MutablePropertyValues(m));
+		parent.registerPrototype("rod", TestBean.class, new MutablePropertyValues(m));
 		m.put("name", "Albert");
-		parent.registerPrototype("father", org.springframework.beans.TestBean.class, new MutablePropertyValues(m));
+		parent.registerPrototype("father", TestBean.class, new MutablePropertyValues(m));
 		parent.rebuild();
 
 		StaticMessageSource parentMessageSource = (StaticMessageSource) parent.getBean("messageSource");
@@ -43,6 +42,7 @@ public class StaticApplicationContextTestSuite extends AbstractApplicationContex
 
 		this.sac = new StaticApplicationContext(parent);
 		sac.addListener(listener);
+		sac.registerSingleton("testBeanProcessor", TestBeanPostProcessor.class, new MutablePropertyValues());
 		sac.registerSingleton("beanThatListens", BeanThatListens.class, new MutablePropertyValues());
 		sac.registerSingleton("aca", ACATest.class, new MutablePropertyValues());
 		sac.registerPrototype("aca-prototype", ACATest.class, new MutablePropertyValues());
@@ -57,20 +57,34 @@ public class StaticApplicationContextTestSuite extends AbstractApplicationContex
 
 	/** Overridden */
 	public void testCount() throws Exception {
-		assertCount(16);
+		assertCount(17);
 	}
 
-	protected void tearDown() {
+	public void testCustomBeanPostProcessor() {
+		String[] beanNames = getListableBeanFactory().getBeanDefinitionNames();
+		for (int i = 0; i < beanNames.length; i++) {
+			if (beanNames[i].toLowerCase().indexOf("factory") == -1 && !beanNames[i].equals("messageSource") &&
+			    !beanNames[i].equals("typeMismatch") && !beanNames[i].equals("testBeanProcessor")) {
+				Object bean = getListableBeanFactory().getBean(beanNames[i]);
+				assertTrue("Enhanced bean class for bean '" + beanNames[i] + "': " + bean.getClass().getName(),
+				           bean.getClass().getName().indexOf("EnhancedByCGLIB") != -1);
+			}
+		}
 	}
 
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(suite());
-		//	junit.swingui.TestRunner.main(new String[] {PrototypeFactoryTests.class.getName() } );
-	}
 
-	public static Test suite() {
-		return new TestSuite(StaticApplicationContextTestSuite.class);
-	}
+	public static class TestBeanPostProcessor implements BeanPostProcessor {
 
+		public Object postProcessBean(Object bean, String name, RootBeanDefinition definition) {
+			if (!(bean instanceof StaticMessageSource)) {
+				ProxyFactory proxyFactory = new ProxyFactory();
+				proxyFactory.addInterceptor(new InvokerInterceptor(bean));
+				return proxyFactory.getProxy();
+			}
+			else {
+				return bean;
+			}
+		}
+	}
 
 }
