c25750174a6fec550cce4ba16f9e967f1902df3f
Juergen Hoeller <jhoeller@interface21.com>
2003-10-14 21:43:54 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@293 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/dao/OptimisticLockingFailureException.java
+++ src/org/springframework/dao/OptimisticLockingFailureException.java
@@ -1,8 +1,8 @@
 /**
- * Generic framework code included with 
+ * Generic framework code included with
  * <a href="http://www.amazon.com/exec/obidos/tg/detail/-/1861007841/">Expert One-On-One J2EE Design and Development</a>
- * by Rod Johnson (Wrox, 2002). 
- * This code is free to use and modify. 
+ * by Rod Johnson (Wrox, 2002).
+ * This code is free to use and modify.
  * Please contact <a href="mailto:rod.johnson@interface21.com">rod.johnson@interface21.com</a>
  * for commercial support.
  */
@@ -10,26 +10,17 @@
 package org.springframework.dao;
 
 /**
- * Exception thrown on an optimistic locking violation. This exception
- * will be thrown either by O/R mapping tools or by DAO implementations.
+ * Exception thrown on an optimistic locking violation. This exception will
+ * be thrown either by O/R mapping tools or by custom DAO implementations.
  * @author Rod Johnson
- * @version $Id: OptimisticLockingFailureException.java,v 1.2 2003-10-14 16:30:13 jhoeller Exp $
- */ 
+ * @version $Id: OptimisticLockingFailureException.java,v 1.3 2003-10-14 21:43:54 jhoeller Exp $
+ */
 public class OptimisticLockingFailureException extends DataAccessException {
 
-	/**
-	 * Constructor for OptimisticLockingFailureDataAccessException.
-	 * @param msg mesg
-	 */
 	public OptimisticLockingFailureException(String msg) {
 		super(msg);
 	}
 
-	/**
-	 * Constructor for OptimisticLockingFailureDataAccessException.
-	 * @param msg mesg
-	 * @param ex root cause
-	 */
 	public OptimisticLockingFailureException(String msg, Throwable ex) {
 		super(msg, ex);
 	}
