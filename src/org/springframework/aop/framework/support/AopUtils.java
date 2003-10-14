60b9a47f3826048e178c402786c91e6beba58483
Rod Johnson <rjohnson@interface21.com>
2003-10-14 18:56:46 +0000

    Added convenience method to check whether a pointcut can ever apply. Can allow more efficient implementation of framework.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@292 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/support/AopUtils.java
+++ src/org/springframework/aop/framework/support/AopUtils.java
@@ -5,11 +5,14 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.aopalliance.intercept.AttributeRegistry;
+import org.springframework.aop.framework.StaticMethodPointcut;
+
 /**
  * Utility methods used by the AOP framework.
  * @author Rod Johnson
  * @author Juergen Hoeller
- * @version $Id: AopUtils.java,v 1.2 2003-10-13 16:51:35 johnsonr Exp $
+ * @version $Id: AopUtils.java,v 1.3 2003-10-14 18:56:29 johnsonr Exp $
  */
 public class AopUtils {
 
@@ -65,6 +68,33 @@ public class AopUtils {
 				// Didn't find it...keep going
 			}
 		}	// for
+		return false;
+	}
+
+	/**
+	 * Can the given pointcut apply at all on the given class?
+	 * This is an important test as it can be used to optimize
+	 * out a pointcut for a class
+	 * @param pc pc static or dynamic pointcut
+	 * @param ar AttributeRegistry
+	 * @param clazz class we're testing
+	 * @param proxyInterfaces proxy interfaces. If null, all methods
+	 * on class may be proxied
+	 * @return whether the pointcut can apply on any method
+	 */
+	public static boolean canApply(StaticMethodPointcut pc, AttributeRegistry ar, Class clazz, Class[] proxyInterfaces) {
+		// Check whether it can apply on any method
+		Method[] methods = clazz.getDeclaredMethods();
+		for (int i = 0; i < methods.length; i++) {
+			Method m = methods[i];
+			// If we're looking only at interfaces and this method
+			// isn't on any of them, skip it
+			if (proxyInterfaces != null && !methodIsOnOneOfTheseInterfaces(m, proxyInterfaces)) {
+				continue;
+			}
+			if (pc.applies(m, ar))
+				return true;
+		}
 		return false;
 	}
 
--- test/org/springframework/aop/framework/support/AopUtilsTests.java
+++ test/org/springframework/aop/framework/support/AopUtilsTests.java
@@ -7,6 +7,11 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.aopalliance.intercept.AttributeRegistry;
+import org.springframework.aop.framework.AbstractMethodPointcut;
+import org.springframework.aop.framework.AlwaysInvoked;
+import org.springframework.aop.framework.StaticMethodPointcut;
+import org.springframework.aop.interceptor.DebugInterceptor;
 import org.springframework.beans.DerivedTestBean;
 import org.springframework.beans.IOther;
 import org.springframework.beans.ITestBean;
@@ -57,6 +62,55 @@ public class AopUtilsTests extends TestCase {
 		catch (IllegalArgumentException ex) {
 			// Ok
 		}
+	}
+	
+	
+	public void testPointcutCanNeverApply() {
+		class TestPointcut extends AbstractMethodPointcut implements StaticMethodPointcut {
+			public boolean applies(Method method, AttributeRegistry attributeRegistry) {
+				return false;
+			}
+		}
+	
+		StaticMethodPointcut no = new TestPointcut();
+		assertFalse(AopUtils.canApply(no, null, Object.class, null));
+	}
+
+	public void testPointcutAlwaysApplies() {
+		assertTrue(AopUtils.canApply(new AlwaysInvoked(new DebugInterceptor()), null, Object.class, null));
+		assertTrue(AopUtils.canApply(new AlwaysInvoked(new DebugInterceptor()), null, TestBean.class, new Class[] { ITestBean.class }));
+	}
+
+	public void testPointcutAppliesToOneMethodOnObject() {
+		class TestPointcut extends AbstractMethodPointcut implements StaticMethodPointcut {
+			public boolean applies(Method method, AttributeRegistry attributeRegistry) {
+				return method.getName().equals("hashCode");
+			}
+		}
+
+		StaticMethodPointcut pc = new TestPointcut();
+	
+		// Will return true if we're not proxying interfaces
+		assertTrue(AopUtils.canApply(pc, null, Object.class, null));
+	
+		// Will return false if we're proxying interfaces
+		assertFalse(AopUtils.canApply(pc, null, Object.class, new Class[] { ITestBean.class }));
+	}
+
+	public void testPointcutAppliesToOneInterfaceOfSeveral() {
+		class TestPointcut extends AbstractMethodPointcut implements StaticMethodPointcut {
+			public boolean applies(Method method, AttributeRegistry attributeRegistry) {
+				return method.getName().equals("getName");
+			}
+		}
+
+		StaticMethodPointcut pc = new TestPointcut();
+
+		// Will return true if we're proxying interfaces including ITestBean 
+		assertTrue(AopUtils.canApply(pc, null, TestBean.class, new Class[] { ITestBean.class, Comparable.class }));
+	
+		// Will return true if we're proxying interfaces including ITestBean 
+		assertFalse(AopUtils.canApply(pc, null, TestBean.class, new Class[] { Comparable.class }));
 	}
 
 }
