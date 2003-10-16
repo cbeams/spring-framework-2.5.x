73b6c42e12beb17fd1ba51739dbb733c8e484e6f
Juergen Hoeller <jhoeller@interface21.com>
2003-10-16 18:56:48 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@304 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/NotWritablePropertyException.java
+++ src/org/springframework/beans/NotWritablePropertyException.java
@@ -1,20 +1,17 @@
-
 package org.springframework.beans;
 
 /**
- * Exception thrown on an attempt to set the value of
- * a property that isn't writable, because there's no
- * setter method.
+ * Exception thrown on an attempt to set the value of a property
+ * that isn't writable, because there's no setter method.
  * @author Rod Johnson
  */
 public class NotWritablePropertyException extends BeansException {
 
-    /**
-	 * Creates new <code>NotWritablePropertyException</code> without detail message.
-     */
-    public NotWritablePropertyException(String propertyName, Class beanClass) {
+	/**
+	 * Creates new NotWritablePropertyException.
+	 */
+	public NotWritablePropertyException(String propertyName, Class beanClass) {
 		super("Property [" + propertyName + "] is not writable in bean class [" + beanClass.getName() + "]");
-    }
+	}
+
 }
-
-
