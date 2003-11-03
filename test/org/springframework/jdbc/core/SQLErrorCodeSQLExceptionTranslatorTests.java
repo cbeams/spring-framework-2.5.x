7ce051948eb242224183af5fb26d980334f4a92c
Rod Johnson <rjohnson@interface21.com>
2003-11-03 15:14:53 +0000

    New tests for default SQLErrorCodeTranslator loading
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@414 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ test/org/springframework/jdbc/core/SQLErrorCodeSQLExceptionTranslatorTests.java
@@ -0,0 +1,91 @@
+
+package org.springframework.jdbc.core;
+
+import java.sql.SQLException;
+
+import junit.framework.TestCase;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+
+/**
+ * @author Rod Johnson
+ */
+public class SQLErrorCodeSQLExceptionTranslatorTests extends TestCase {
+	
+	private static SQLErrorCodes ERROR_CODES = new SQLErrorCodes();
+	static {
+		ERROR_CODES.setBadSqlGrammarCodes(new String[] { "1", "2" });
+		ERROR_CODES.setDataIntegrityViolationCodes(new String[] { "3", "4" });
+	}
+	
+	static SQLException BAD_SQL_EX = new SQLException() {
+		public int getErrorCode() {
+			return 1;
+		}
+	};
+	
+	static SQLException INTEG_VIOLATION_EX = new SQLException() {
+		public int getErrorCode() {
+			return 3;
+		}
+	};
+
+	/**
+	 * Constructor for SQLErrorCodeSQLExceptionTranslatorTests.
+	 * @param arg0
+	 */
+	public SQLErrorCodeSQLExceptionTranslatorTests(String arg0) {
+		super(arg0);
+	}
+	
+	/**
+	 * Run tests on this translator
+	 * @param sext
+	 */
+	private void runTests(SQLExceptionTranslator sext) {
+		BadSqlGrammarException ex = (BadSqlGrammarException) sext.translate("task", "SQL", BAD_SQL_EX);
+		assertEquals("SQL", ex.getSql());
+		assertEquals(BAD_SQL_EX, ex.getSQLException());
+		DataIntegrityViolationException diex = (DataIntegrityViolationException) sext.translate("task", "SQL", INTEG_VIOLATION_EX);
+		assertEquals(INTEG_VIOLATION_EX, diex.getRootCause());
+		
+		// Test fallback. We assume that no database will ever return this error code,
+		// but 07xxx will be bad grammar picked up by the fallback SQLState translator
+		SQLException sex = new SQLException("", "07xxx") {
+			public int getErrorCode() {
+				return 666666666;
+			}
+		};
+		ex = (BadSqlGrammarException) sext.translate("task", "SQL2", sex);
+		assertEquals("SQL2", ex.getSql());
+		assertEquals(sex, ex.getSQLException());
+
+	}
+	
+	public void testErrorCodesProvidedInConstructor() {
+		runTests(new SQLErrorCodeSQLExceptionTranslator(ERROR_CODES));
+	}
+	
+	public void testCustomTranslation() {
+		final String TASK = "TASK";
+		final String SQL = "SQL SELECT *";
+		final DataAccessException customDex = new DataAccessException("") {};
+		SQLErrorCodeSQLExceptionTranslator sext = new SQLErrorCodeSQLExceptionTranslator() {
+
+			protected DataAccessException customTranslate(String task, String sql, SQLException sqlex) {
+				assertEquals(TASK, task);
+				assertEquals(SQL, sql);
+				return (sqlex == BAD_SQL_EX) ? customDex : null;
+			}
+		};
+		sext.setSqlErrorCodes(ERROR_CODES);
+		
+		// Shouldn't custom translate this
+		assertEquals(customDex, sext.translate(TASK, SQL, BAD_SQL_EX));
+		DataIntegrityViolationException diex = (DataIntegrityViolationException) sext.translate(TASK, SQL, INTEG_VIOLATION_EX);
+		assertEquals(INTEG_VIOLATION_EX, diex.getRootCause());
+	}
+	
+
+}
---
+++ test/org/springframework/jdbc/core/SQLErrorCodesFactoryTests.java
@@ -0,0 +1,215 @@
+
+package org.springframework.jdbc.core;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import junit.framework.TestCase;
+
+import org.easymock.MockControl;
+import org.springframework.util.ClassLoaderUtils;
+
+/**
+ * Tests for SQLErrorCode loading.
+ * @author Rod Johnson
+ */
+public class SQLErrorCodesFactoryTests extends TestCase {
+
+	/**
+	 * Constructor for SQLErrorCodesFactoryTests.
+	 * @param arg0
+	 */
+	public SQLErrorCodesFactoryTests(String arg0) {
+		super(arg0);
+	}
+	
+	/**
+	 * Check that a default instance returns empty error codes for an unknown
+	 * database
+	 */
+	public void testDefaultInstanceWithNoSuchDatabase() {
+		SQLErrorCodes sec = SQLErrorCodesFactory.getInstance().getErrorCodes("xx");
+		assertTrue(sec.getBadSqlGrammarCodes().length == 0);
+		assertTrue(sec.getDataIntegrityViolationCodes().length == 0);
+	}
+	
+	/**
+	 * Check that a known database produces recognizable codes
+	 */
+	public void testDefaultInstanceWithOracle() {
+		SQLErrorCodes sec = SQLErrorCodesFactory.getInstance().getErrorCodes("Oracle");
+		assertIsOracle(sec);
+	}
+
+	private void assertIsOracle(SQLErrorCodes sec) {
+		assertTrue(sec.getBadSqlGrammarCodes().length > 0);
+		assertTrue(sec.getDataIntegrityViolationCodes().length > 0);
+		// This had better be a Bad SQL Grammar code
+		assertTrue(Arrays.binarySearch(sec.getBadSqlGrammarCodes(), "942") >= 0);
+		// This had better NOT be
+		assertFalse(Arrays.binarySearch(sec.getBadSqlGrammarCodes(), "9xx42") >= 0);
+	}
+	
+	private void assertIsDB2(SQLErrorCodes sec) {
+		assertTrue(sec.getBadSqlGrammarCodes().length > 0);
+		assertTrue(sec.getDataIntegrityViolationCodes().length > 0);
+		
+		assertFalse(Arrays.binarySearch(sec.getBadSqlGrammarCodes(), "942") >= 0);
+		// This had better NOT be
+		assertTrue(Arrays.binarySearch(sec.getBadSqlGrammarCodes(), "-204") >= 0);
+	}
+	
+	public void testLookupOrder() {
+		class TestSQLErrorCodesFactory extends SQLErrorCodesFactory {
+			private int lookups = 0;
+			protected InputStream loadInputStream(String resourcePath) {
+				++lookups;
+				if (lookups == 1) {
+					assertEquals(SQLErrorCodesFactory.SQL_ERROR_CODE_OVERRIDE_PATH, resourcePath);
+					return null;
+				}
+				else {
+					// Should have only one more lookup
+					assertEquals(2, lookups);
+					assertEquals(SQLErrorCodesFactory.SQL_ERROR_CODE_DEFAULT_PATH, resourcePath);
+					return null;
+				}
+			}
+		};
+		
+		// Should have failed to load without error
+		TestSQLErrorCodesFactory sf = new TestSQLErrorCodesFactory();
+		assertTrue(sf.getErrorCodes("XX").getBadSqlGrammarCodes().length == 0);
+		assertTrue(sf.getErrorCodes("Oracle").getDataIntegrityViolationCodes().length == 0);
+	}
+	
+	/**
+	 * Check that custom error codes take precedence
+	 *
+	 */
+	public void testFindCustomCodes() {
+		class TestSQLErrorCodesFactory extends SQLErrorCodesFactory {
+			protected InputStream loadInputStream(String resourcePath) {
+				assertEquals(SQLErrorCodesFactory.SQL_ERROR_CODE_OVERRIDE_PATH, resourcePath);
+				return ClassLoaderUtils.getResourceAsStream(getClass(), "test-error-codes.xml");	
+			}
+		};
+	
+		// Should have failed to load without error
+		TestSQLErrorCodesFactory sf = new TestSQLErrorCodesFactory();
+		assertTrue(sf.getErrorCodes("XX").getBadSqlGrammarCodes().length == 0);
+		assertEquals(2, sf.getErrorCodes("Oracle").getBadSqlGrammarCodes().length);
+		assertEquals("1", sf.getErrorCodes("Oracle").getBadSqlGrammarCodes()[0]);
+	}
+	
+	public void testInvalidCustomCodeFormat() {
+		class TestSQLErrorCodesFactory extends SQLErrorCodesFactory {
+			protected InputStream loadInputStream(String resourcePath) {
+				assertEquals(SQLErrorCodesFactory.SQL_ERROR_CODE_OVERRIDE_PATH, resourcePath);
+				// Guaranteed to be on the classpath, but most certainly NOT XML
+				return ClassLoaderUtils.getResourceAsStream(getClass(), "JdbcTemplate.class");	
+			}
+		};
+
+		// Should have failed to load without error
+		TestSQLErrorCodesFactory sf = new TestSQLErrorCodesFactory();
+		assertTrue(sf.getErrorCodes("XX").getBadSqlGrammarCodes().length == 0);
+		assertEquals(0, sf.getErrorCodes("Oracle").getBadSqlGrammarCodes().length);
+	}
+	
+	
+	public void testDataSourceWithNullMetadata() throws Exception {
+		
+		MockControl ctrlConnection = MockControl.createControl(Connection.class);
+		Connection mockConnection = (Connection) ctrlConnection.getMock();
+		mockConnection.getMetaData();
+		ctrlConnection.setReturnValue(null);
+		mockConnection.close();
+		ctrlConnection.setVoidCallable();
+		ctrlConnection.replay();
+
+		MockControl ctrlDataSource = MockControl.createControl(DataSource.class);
+		DataSource mockDataSource = (DataSource) ctrlDataSource.getMock();
+		mockDataSource.getConnection();
+		ctrlDataSource.setDefaultReturnValue(mockConnection);
+		ctrlDataSource.replay();
+	
+		SQLErrorCodes sec = SQLErrorCodesFactory.getInstance().getErrorCodes(mockDataSource);
+		assertIsEmpty(sec);
+	
+		ctrlConnection.verify();
+		ctrlDataSource.verify();
+	}
+	
+	public void testGetFromDataSourceWithSQLException() throws Exception {
+		
+		SQLException expectedSQLException = new SQLException();
+
+		MockControl ctrlDataSource = MockControl.createControl(DataSource.class);
+		DataSource mockDataSource = (DataSource) ctrlDataSource.getMock();
+		mockDataSource.getConnection();
+		ctrlDataSource.setThrowable(expectedSQLException);
+		ctrlDataSource.replay();
+
+		SQLErrorCodes sec = SQLErrorCodesFactory.getInstance().getErrorCodes(mockDataSource);
+		assertIsEmpty(sec);
+
+		ctrlDataSource.verify();
+	}
+
+	private void assertIsEmpty(SQLErrorCodes sec) {
+		// Codes should be empty
+		assertEquals(0, sec.getBadSqlGrammarCodes().length);
+		assertEquals(0, sec.getDataIntegrityViolationCodes().length);
+	}
+	
+	private SQLErrorCodes getErrorCodesFromDataSourceWithGivenMetadata(String productName) throws Exception {
+		MockControl mdControl = MockControl.createControl(DatabaseMetaData.class);
+		DatabaseMetaData md = (DatabaseMetaData) mdControl.getMock();
+		md.getDatabaseProductName();
+		mdControl.setReturnValue(productName);
+		mdControl.replay();
+		
+		MockControl ctrlConnection = MockControl.createControl(Connection.class);
+		Connection mockConnection = (Connection) ctrlConnection.getMock();
+		mockConnection.getMetaData();
+		ctrlConnection.setReturnValue(md);
+		mockConnection.close();
+		ctrlConnection.setVoidCallable();
+		ctrlConnection.replay();
+
+		MockControl ctrlDataSource = MockControl.createControl(DataSource.class);
+		DataSource mockDataSource = (DataSource) ctrlDataSource.getMock();
+		mockDataSource.getConnection();
+		ctrlDataSource.setDefaultReturnValue(mockConnection);
+		ctrlDataSource.replay();
+
+		SQLErrorCodes sec = SQLErrorCodesFactory.getInstance().getErrorCodes(mockDataSource);
+
+		mdControl.verify();
+		ctrlConnection.verify();
+		ctrlDataSource.verify();
+		
+		return sec;
+	}
+	
+	public void testOracleRecognizedFromMetadata() throws Exception {
+		SQLErrorCodes sec = getErrorCodesFromDataSourceWithGivenMetadata("Oracle");
+		assertIsOracle(sec);
+	}
+	
+	public void testDB2RecognizedFromMetadata() throws Exception {
+		SQLErrorCodes sec = getErrorCodesFromDataSourceWithGivenMetadata("DB2");
+		assertIsDB2(sec);
+		sec = getErrorCodesFromDataSourceWithGivenMetadata("DB2/");
+		assertIsDB2(sec);
+		sec = getErrorCodesFromDataSourceWithGivenMetadata("DB2%");
+		assertIsEmpty(sec);
+	}
+
+}
