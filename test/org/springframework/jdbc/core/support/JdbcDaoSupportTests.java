53ad91be3262968a6eafdeab7f4cfd412001827c
Juergen Hoeller <jhoeller@interface21.com>
2003-11-04 23:03:02 +0000

    added setter methods for data access templates
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@431 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jdbc/core/support/JdbcDaoSupportTests.java
+++ test/org/springframework/jdbc/core/support/JdbcDaoSupportTests.java
@@ -1,15 +1,15 @@
 package org.springframework.jdbc.core.support;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import junit.framework.TestCase;
-
 import org.easymock.MockControl;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * @author Juergen Hoeller
@@ -17,19 +17,19 @@ import org.easymock.MockControl;
  */
 public class JdbcDaoSupportTests extends TestCase {
 
-	public void testJdbcDaoSupport() throws Exception {
-		MockControl conControl = MockControl.createControl(Connection.class);
-		Connection con = (Connection) conControl.getMock();
+	public void testJdbcDaoSupportWithDataSource() throws Exception {
 		MockControl dsControl = MockControl.createControl(DataSource.class);
 		DataSource ds = (DataSource) dsControl.getMock();
+		MockControl conControl = MockControl.createControl(Connection.class);
+		Connection con = (Connection) conControl.getMock();
 		ds.getConnection();
 		dsControl.setReturnValue(con, 1);
 		con.getMetaData();
 		conControl.setReturnValue(null, 1);
 		con.close();
 		conControl.setVoidCallable(1);
-		conControl.replay();
 		dsControl.replay();
+		conControl.replay();
 		final List test = new ArrayList();
 		JdbcDaoSupport dao = new JdbcDaoSupport() {
 			protected void initDao() {
@@ -38,8 +38,24 @@ public class JdbcDaoSupportTests extends TestCase {
 		};
 		dao.setDataSource(ds);
 		dao.afterPropertiesSet();
-		assertEquals("Correct SessionFactory", dao.getDataSource(), ds);
-		assertEquals("Correct HibernateTemplate", dao.getJdbcTemplate().getDataSource(), ds);
+		assertEquals("Correct DataSource", ds, dao.getDataSource());
+		assertEquals("Correct JdbcTemplate", ds, dao.getJdbcTemplate().getDataSource());
+		assertEquals("initDao called", test.size(), 1);
+		dsControl.verify();
+		conControl.verify();
+	}
+
+	public void testJdbcDaoSupportWithJdbcTemplate() throws Exception {
+		JdbcTemplate template = new JdbcTemplate();
+		final List test = new ArrayList();
+		JdbcDaoSupport dao = new JdbcDaoSupport() {
+			protected void initDao() {
+				test.add("test");
+			}
+		};
+		dao.setJdbcTemplate(template);
+		dao.afterPropertiesSet();
+		assertEquals("Correct JdbcTemplate", dao.getJdbcTemplate(), template);
 		assertEquals("initDao called", test.size(), 1);
 	}
 
