9e1c2333f9a69955855ff57ccdbc4b264267edd6
Juergen Hoeller <jhoeller@interface21.com>
2003-11-04 23:12:49 +0000

    now derives from ListableBeanFactoryImpl instead of using a delegate
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@435 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/core/support/JdbcBeanFactory.java
+++ src/org/springframework/jdbc/core/support/JdbcBeanFactory.java
@@ -1,16 +1,11 @@
-
 package org.springframework.jdbc.core.support;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.support.ListableBeanFactoryImpl;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
@@ -21,41 +16,50 @@ import org.springframework.jdbc.core.RowCallbackHandler;
  * as string. Formats for each are identical to the properties format
  * recognized by ListableBeanFactoryImpl.
  * @author Rod Johnson
- * @version $Id: JdbcBeanFactory.java,v 1.2 2003-10-31 17:01:24 jhoeller Exp $
+ * @version $Id: JdbcBeanFactory.java,v 1.3 2003-11-04 23:12:49 jhoeller Exp $
  */
-public class JdbcBeanFactory implements ListableBeanFactory {
-	
-	private ListableBeanFactory delegate;
-	
-	/**
-	 * SQL that produces the three columns.
-	 */
-	private String sql;
+public class JdbcBeanFactory extends ListableBeanFactoryImpl {
 	
 	private JdbcTemplate jdbcTemplate;
 
+	/**
+	 * Create a new JdbcBeanFactory.
+	 * @param dataSource DataSource to use to obtain database connections.
+	 * The DataSource will be cached during the life of this bean factory.
+	 * @param sql SQL query to use for loading bean definitions
+	 * @see #loadBeanDefinitions
+	 */
+	public JdbcBeanFactory(DataSource dataSource, String sql) {
+		setDataSource(dataSource);
+		loadBeanDefinitions(sql);
+	}
 
 	/**
-	 * Create a new JdbcBeanFactory
-	 * @param ds datasource to use to obtain connections. The datasource
-	 * will be cached during the life of this bean factory.
-	 * @param sql the first three columns must be bean name, property name and value. Any join
-	 * and any other columns are permitted: e.g.
-	 * SELECT BEAN_NAME, PROPERTY, VALUE FROM CONFIG WHERE CONFIG.APP_ID = 1
-	 * It's also possible to perform a join. Column names are not significant--only
-	 * the ordering of these first three columns.
+	 * Set the DataSource to use to obtain database connections.
+	 * Will implicitly create a new JdbcTemplate with the given DataSource.
 	 */
-	public JdbcBeanFactory(DataSource ds, String sql) {
-		this.sql = sql;
-		this.jdbcTemplate = new JdbcTemplate(ds);
-		refresh();
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	
+
 	/**
-	 * Refresh the factory. Uses copy-on-write for thread safety
+	 * Set the JdbcTemplate to be used by this bean factory.
+	 * Contains settings for DataSource, SQLExceptionTranslator, QueryExecutor, etc.
 	 */
-	public void refresh() {
-		ListableBeanFactoryImpl newDelegate =  new ListableBeanFactoryImpl();
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	/**
+	 * Load bean definitions from the database via the given SQL string.
+	 * @param sql SQL query to use for loading bean definitions.
+	 * The first three columns must be bean name, property name and value.
+	 * Any join and any other columns are permitted: e.g.
+	 * SELECT BEAN_NAME, PROPERTY, VALUE FROM CONFIG WHERE CONFIG.APP_ID = 1
+	 * It's also possible to perform a join. Column names are not significant --
+	 * only the ordering of these first three columns.
+	 */
+	public void loadBeanDefinitions(String sql) {
 		final Properties props = new Properties();
 		this.jdbcTemplate.query(sql, new RowCallbackHandler() {
 			public void processRow(ResultSet rs) throws SQLException {
@@ -66,41 +70,7 @@ public class JdbcBeanFactory implements ListableBeanFactory {
 				props.setProperty(beanName + "." + property, value);
 			}
 		});
-		
-		newDelegate.registerBeanDefinitions(props, null);
-		this.delegate = newDelegate;
-	}
-
-	public Object getBean(String name) {
-		return delegate.getBean(name);
-	}
-
-	public Object getBean(String name, Class requiredType) throws BeansException {
-		return delegate.getBean(name, requiredType);
-	}
-
-	public int getBeanDefinitionCount() {
-		return delegate.getBeanDefinitionCount();
-	}
-
-	public String[] getBeanDefinitionNames() {
-		return delegate.getBeanDefinitionNames();
-	}
-
-	public String[] getBeanDefinitionNames(Class type) {
-		return delegate.getBeanDefinitionNames(type);
-	}
-
-	public Map getBeansOfType(Class type) {
-		return delegate.getBeansOfType(type);
-	}
-
-	public boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
-		return delegate.isSingleton(name);
-	}
-
-	public String[] getAliases(String name) {
-		return null;
+		registerBeanDefinitions(props);
 	}
 
 }
