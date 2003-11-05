7b1916cdbbd9e39903b915004cba5563499e5ca4
Juergen Hoeller <jhoeller@interface21.com>
2003-11-05 09:55:31 +0000

    added registerAlias method to interface
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@444 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
+++ src/org/springframework/beans/factory/config/ConfigurableBeanFactory.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 /**
  * SPI interface to be implemented by most if not all bean factories.
@@ -39,8 +40,9 @@ public interface ConfigurableBeanFactory extends BeanFactory {
 	 * Return the registered PropertyValues for the given bean.
 	 * @param beanName name of the bean
 	 * @return the registered PropertyValues
+	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
 	 */
-	PropertyValues getPropertyValues(String beanName);
+	PropertyValues getPropertyValues(String beanName) throws NoSuchBeanDefinitionException;
 
 	/**
 	 * Register property value for a specific bean, overriding an existing value.
@@ -49,10 +51,21 @@ public interface ConfigurableBeanFactory extends BeanFactory {
 	 * certain property values after parsing the original bean definitions.
 	 * @param beanName name of the bean
 	 * @param pv property name and value
+	 * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException if there is no bean with the given name
 	 * @throws org.springframework.beans.BeansException if the property values of the specified bean are immutable
 	 * @see org.springframework.beans.factory.config.BeanFactoryPostProcessor
 	 */
 	void overridePropertyValue(String beanName, PropertyValue pv) throws BeansException;
+
+	/**
+	 * Given a bean name, create an alias. We typically use this method to
+	 * support names that are illegal within XML ids (used for bean names).
+	 * @param beanName name of the bean
+	 * @param alias alias that will behave the same as the bean name
+	 * @throws org.springframework.beans.factory.NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @throws org.springframework.beans.BeansException if the alias is already in use
+	 */
+	void registerAlias(String beanName, String alias) throws BeansException;
 
 	/**
 	 * Destroy all cached singletons in this factory.
