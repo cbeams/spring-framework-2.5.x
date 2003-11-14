a91a4e65057cc2eafe284bc8879ad6ca4ea12ecd
No Author <nobody@interface21.com>
2003-11-14 16:38:18 +0000

    This commit was manufactured by cvs2svn to create tag 'aop2'.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/tags/aop2@543 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/web/servlet/view/ResourceBundleViewResolverTestSuiteNoCache.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.web.servlet.view;
-
-public class ResourceBundleViewResolverTestSuiteNoCache extends ResourceBundleViewResolverTestSuite {
-	
-	protected boolean getCache() {
-		return false;
-	}
-}
- 
