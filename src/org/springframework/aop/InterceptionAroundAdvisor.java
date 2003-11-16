737e8a4bd349368f5d734bb7f50375e67f2b48b1
Rod Johnson <rjohnson@interface21.com>
2003-11-16 21:53:11 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@559 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/InterceptionAroundAdvisor.java
+++ src/org/springframework/aop/InterceptionAroundAdvisor.java
@@ -8,15 +8,19 @@ package org.springframework.aop;
 import org.aopalliance.intercept.Interceptor;
 
 
-
 /**
- *
+ * Advisor that delivers <b>around</b> advice via an AOP Alliance interceptor.
+ * Such advice is targeted by a pointcut.
  * @author Rod Johnson
  * @since 04-Apr-2003
- * @version $Id: InterceptionAroundAdvisor.java,v 1.1 2003-11-15 15:29:55 johnsonr Exp $
+ * @version $Id: InterceptionAroundAdvisor.java,v 1.2 2003-11-16 21:53:11 johnsonr Exp $
  */
 public interface InterceptionAroundAdvisor extends InterceptionAdvisor, PointcutAdvisor {
 	
+	/**
+	 * @return the pointcut that identifies joinpoints eligible for this
+	 * around advice.
+	 */
 	Interceptor getInterceptor(); 
 	
 
--- src/org/springframework/aop/InterceptionIntroductionAdvisor.java
+++ src/org/springframework/aop/InterceptionIntroductionAdvisor.java
@@ -5,21 +5,32 @@
  
 package org.springframework.aop;
 
-
-
-
 /**
- * TODO base Advice interface? ONLY WAY TO DO INTRODUCTION
+ * Advisor that performs an AOP <b>introduction</b> through an interceptor.
+ * Introduction is the implementation of additional interfaces 
+ * (not implemented by a target) via AOP advice.
  * @author Rod Johnson
  * @since 04-Apr-2003
- * @version $Id: InterceptionIntroductionAdvisor.java,v 1.1 2003-11-15 15:29:55 johnsonr Exp $
+ * @see org.springframework.aop.IntroductionInterceptor
+ * @version $Id: InterceptionIntroductionAdvisor.java,v 1.2 2003-11-16 21:51:04 johnsonr Exp $
  */
 public interface InterceptionIntroductionAdvisor extends InterceptionAdvisor {
 	
+	/**
+	 * @return the filter determining which target classes this introduction
+	 * should apply to. The class part of a pointcut. Note that method matching
+	 * doesn't make sense to introductions.
+	 */
 	ClassFilter getClassFilter();
 	
+	/**
+	 * @return the interceptor that handles the introduced interface(s)
+	 */
 	IntroductionInterceptor getIntroductionInterceptor();
 	
+	/**
+	 * @return the additional interfaces introduced by this Advisor
+	 */
 	Class[] getInterfaces();
 	
 
