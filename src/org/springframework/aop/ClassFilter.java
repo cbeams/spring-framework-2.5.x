fb2e9a156119adf05a00a038cf9383888e3f67eb
Rod Johnson <rjohnson@interface21.com>
2003-11-17 11:02:43 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@564 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/ClassFilter.java
+++ src/org/springframework/aop/ClassFilter.java
@@ -7,8 +7,14 @@ package org.springframework.aop;
 
 
 /**
+ * Filter that restricts matching of a pointcut or introduction to
+ * a given set of target classes.
+ * <br>
  * Can be used as part of a pointcut, or for the entire 
  * targeting of an IntroductionAdvice.
+ * @author Rod Johnson
+ * @see org.springframework.aop.Pointcut
+ * @version $Id: ClassFilter.java,v 1.2 2003-11-17 11:02:43 johnsonr Exp $
  */
 public interface ClassFilter {
 	
@@ -21,6 +27,11 @@ public interface ClassFilter {
 		}
 	};
 	
+	/**
+	 * Should the pointcut apply to the given interface or target class?
+	 * @param clazz candidate target class
+	 * @return whether the advice should apply to this candidate target class
+	 */
 	boolean matches(Class clazz);
 
 }
