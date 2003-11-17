f17afde484df197e28e6febedd7ea0a4ab2e0954
Rod Johnson <rjohnson@interface21.com>
2003-11-17 11:17:25 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@565 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/MethodMatcher.java
+++ src/org/springframework/aop/MethodMatcher.java
@@ -8,36 +8,67 @@ package org.springframework.aop;
 import java.lang.reflect.Method;
 
 /**
- * Part of a Pointcut.
+ * Part of a Pointcut. Checks whether the target method is eligible
+ * for advice. <br>
+ * A MethodMatcher may be evaluated <b>statically</b> or at <b>runtime</b> (dynamically).
+ * Static matching involves method and (possibly) method attributes. Dynamic matching
+ * also makes arguments for a particular call available, and any effects of running
+ * previous advice applying to the joinpoint.
+ * <br>
+ * If an implementation returns false in its isRuntime() method, evaluation can be performed
+ * statically, and the result will be the same for all invocations of this method,
+ * whatever their arguments. If the isRuntime() method returns false, the 3-arg matches()
+ * method will never be invoked.
+ * <br>
+ * If an implementation returns true in its 2-arg matches() method, and
+ * its isRuntime() method returns true, the 3-argument matches()
+ * method will be invoked <i>immediately before each potential execution of the related advice</i>,
+ * to decide whether the advice should run. All previous advice, such as earlier interceptors
+ * in an interceptor chain, will have run, so any state changes they have produced in parameters
+ * or ThreadLocal state, will be available at the time of evaluation.
+ * <br>
  * 
  * @author Rod Johnson
  * @since 11-Nov-2003
- * @version $Id: MethodMatcher.java,v 1.1 2003-11-11 18:31:51 johnsonr Exp $
+ * @version $Id: MethodMatcher.java,v 1.2 2003-11-17 11:17:25 johnsonr Exp $
  */
 public interface MethodMatcher {
 	
-	
+	/**
+	 * Perform static checking. If this returns false, or if the isRuntime() method
+	 * returns false, no runtime check will be made.
+	 * @param m candidate method
+	 * @param targetClass target class (may be null, in which case the candidate
+	 * class must be taken to be the method's declaring class)
+	 * @return whether or not this method matches statically
+	 */
 	boolean matches(Method m, Class targetClass);
 	
 	/**
+	 * Is this MethodMatcher dynamic?
 	 * Must a final call be made on the matches(Method, Class, Object[]) method at runtime
 	 * even if the 2-arg matches method returns true?
 	 * Can be invoked when an AOP proxy is created, and need not be invoked
 	 * again before each method invocation,
-	 * @return
-	 * TODO could be pulled up into a Matcher superinterface to apply to
+	 * @return whether or not a runtime matche via the 3-arg matches() method is
+	 * required if static matching passed.
+	 * <br>Note: could be pulled up into a Matcher superinterface to apply to
 	 * fields also
 	 */
 	boolean isRuntime();
 	
 	/**
-	 * Invoked only if the 2-arg matches method returns true for the given method
+	 * Is there a runtime (dynamic) match for this method, which must have matched
+	 * statically. This method is
+	 * invoked only if the 2-arg matches method returns true for the given method
 	 * and target class, and if the isRuntime() method returns true.
-	 * Invoked at the time of method invocation.
-	 * @param m
-	 * @param targetClass
-	 * @param args
-	 * @return
+	 * Invoked immediately before potentially running of the advice, after any
+	 * advice earlier in the advice chain has run.
+	 * @param m candidate method
+	 * @param targetClass target class
+	 * @param args arguments to the method
+	 * @return whether there's a runtime match
+	 * @see MethodMatcher#matches(Method, Class)
 	 */
 	boolean matches(Method m, Class targetClass, Object[] args);
 	
