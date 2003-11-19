6ec49c39c649a691607ce7ed95e1d3780f6115f8
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2003-11-19 15:31:09 +0000

    Refactored deprecated URLEncoder.encode(String) into URLEncoder.encode(String, String) with encoding scheme as a second argument
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@582 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/RedirectView.java
+++ src/org/springframework/web/servlet/view/RedirectView.java
@@ -14,27 +14,47 @@ import javax.servlet.http.HttpServletResponse;
  * exposing all model attributes as HTTP query parameters.
  * @author Rod Johnson
  * @author Juergen Hoeller
- * @version $Revision: 1.1.1.1 $
+ * @version $Id: RedirectView.java,v 1.2 2003-11-19 15:31:09 dkopylenko Exp $
  */
 public class RedirectView extends AbstractView {
-	
+
+	public static final String DEFAULT_ENCODING_SCHEME = "UTF-8";
+
+	private String encodingScheme = DEFAULT_ENCODING_SCHEME;
+
 	private String url;
 
 	public RedirectView() {
 	}
-	 
+
 	public RedirectView(String url) {
 		setUrl(url);
 	}
-	
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
 	protected String getUrl() {
 		return url;
 	}
-	
+
+	/**
+	 * Get encodingScheme
+	 * @return String representing encoding scheme
+     */
+	public String getEncodingScheme() {
+		return this.encodingScheme;
+	}
+
+	/**
+	 * Set encodingScheme
+	 * @param String representing encoding scheme
+	 */
+	public void setEncodingScheme(String encodingScheme) {
+		this.encodingScheme = encodingScheme;
+	}
+
 	/**
 	 * Subclasses can override this method to return name-value pairs for query strings,
 	 * which will be URLEncoded and formatted by this class.
@@ -47,15 +67,16 @@ public class RedirectView extends AbstractView {
 	/**
 	 * Convert model to request parameters and redirect to url.
 	 */
-	protected void renderMergedOutputModel(Map model, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	protected void renderMergedOutputModel(Map model, HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
 		if (getUrl() == null)
 			throw new ServletException("RedirectView is not configured: URL cannot be null");
-		
+
 		StringBuffer url = new StringBuffer(getUrl());
-		
+
 		// If there are not already some parameters, we need a ?
 		boolean first = (getUrl().indexOf('?') < 0);
-		
+
 		Iterator entries = queryProperties(model).entrySet().iterator();
 		while (entries.hasNext()) {
 			if (first) {
@@ -66,14 +87,14 @@ public class RedirectView extends AbstractView {
 				url.append("&");
 			}
 
-			Map.Entry entry = (Map.Entry) entries.next();
+			Map.Entry entry = (Map.Entry)entries.next();
 
-			url.append(URLEncoder.encode(entry.getKey().toString()));
+			url.append(URLEncoder.encode(entry.getKey().toString(), this.encodingScheme));
 			url.append("=");
-			url.append(URLEncoder.encode(entry.getValue().toString()));
+			url.append(URLEncoder.encode(entry.getValue().toString(), this.encodingScheme));
 		}
-		
+
 		response.sendRedirect(response.encodeRedirectURL(url.toString()));
 	}
-	
+
 }
