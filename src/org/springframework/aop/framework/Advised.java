34826b78725de67901e7c966ea7994450244da2e
Rod Johnson <rjohnson@interface21.com>
2003-11-20 11:05:14 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@586 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/Advised.java
+++ src/org/springframework/aop/framework/Advised.java
@@ -11,22 +11,31 @@ import org.springframework.aop.Advisor;
 /**
  * Interface to be implemented by classes that hold the configuration
  * of a factory of AOP proxies. This configuration includes
- * the interceptors and Advisors, and the proxied interfaces.
- * <p>Any AOP proxy obtained from Spring can be cast to ProxyConfig to allow
+ * the Interceptors and Advisors, and the proxied interfaces.
+ * <p>Any AOP proxy obtained from Spring can be cast to this interface to allow
  * manipulation of its AOP advice.
- * 
+ * @see org.springframework.aop.framework.AdvisedSupport
  * @author Rod Johnson
  * @since 13-Mar-2003
- * @version $Id: Advised.java,v 1.2 2003-11-15 16:20:16 johnsonr Exp $
+ * @version $Id: Advised.java,v 1.3 2003-11-20 11:05:14 johnsonr Exp $
  */
 public interface Advised {
 	
 	/**
 	 * Get whether the factory should expose the AOP Alliance method invocation as a ThreadLocal
 	 * @return whether the factory should expose the AOP Alliance method invocation as a ThreadLocal
+	 * @see AopContext
 	 */
 	boolean getExposeInvocation();
 	
+	/**
+	 * Get whether the factory should expose the proxy as a ThreadLocal. 
+	 * This can be necessary if a target object needs to invoke a method on itself
+	 * benefitting from advice. (If it invokes a method on <code>this</code> no advice
+	 * will apply.) Getting the proxy is analogous to an EJB calling getEJBObject().
+	 * @return whether the factory should expose the proxy as a ThreadLocal
+	 * @see AopContext
+	 */
 	boolean getExposeProxy();
 	
 	/**
@@ -52,48 +61,51 @@ public interface Advised {
 	
 	/**
 	 * 
-	 * Add the given interceptor to the tail of the interceptor chain.
-	 * This will be wrapped in an AlwaysInvoked method pointcut and returned
-	 * in an AlwaysInvoked wrapper from the getMethodPointcuts() method.
-	 * @param interceptor to add to the tail of the chain.
+	 * Add the given interceptor to the tail of the advice (interceptor) chain.
+	 * This will be wrapped in a DefaultInterceptionAroundAdvisor with a pointcut
+	 * that always applies, and returned from the getAdvisors() method in this
+	 * wrapped form.
+	 * @param interceptor to add to the tail of the chain
+	 * @see addInterceptor(int, Interceptor)
 	 */
 	void addInterceptor(Interceptor interceptor);
 	
 	
 	/**
 	 * Add an interceptor at the specified position in the interceptor chain.
-	 * @param pos index from 0 (head).
+	 * @param pos index from 0 (head)
 	 * @param interceptor interceptor to add at the specified position in the
-	 * interceptor chain.
+	 * interceptor chain
 	 */
 	void addInterceptor(int pos, Interceptor interceptor);
 	
 	
-	/** TODO fix Javadoc
-	 * Add an aspect ad the end of the interceptor chain
-	 * @param aspect aspect to add to the end of the chain
+	/** 
+	 * Add an Advisor at the end of the advisor chain
+	 * @param advisor Advisor to add to the end of the chain
 	 */
-	void addAdvisor(Advisor advice);
+	void addAdvisor(Advisor advisor);
 
-	/** TODO fix Javadoc
-	 * Add a pointcut at the specified position in the chain
-	 * @param aspect to add at the specified position in the chain
-	 * TODO does position parameter mean anything now?
+	/** 
+	 * Add an Advisor at the specified position in the chain
+	 * @param advisor advisor to add at the specified position in the chain
+	 * @param pos position in chain (0 is head). Must be valid.
 	 */
-	void addAdvisor(int pos, Advisor advice);
+	void addAdvisor(int pos, Advisor advisor);
 	
 	/**
 	 * Remove the interceptor
-	 * @param interceptor
-	 * @return if the interceptor was found and removed
+	 * @param interceptor to remove
+	 * @return true if the interceptor was found and removed,
+	 * otherwise false
 	 */
 	boolean removeInterceptor(Interceptor interceptor);
 	
 	/**
-	 * Can return null if now target. Returns true if we have
+	 * Can return null if there is no target, in which case interfaces and
+	 * advice supply all behaviour. Returns true if we have
 	 * a target interceptor. A target interceptor must be the last
-	 * interceptor. Implementations should be efficient, as this
-	 * will be invoked on each invocation.
+	 * interceptor in the chain.
 	 * @return Object
 	 */
 	Object getTarget();
@@ -108,7 +120,7 @@ public interface Advised {
 	/**
 	 * As toString() will normally pass to the target, 
 	 * this returns the equivalent for the ProxyConfig
-	 * @return
+	 * @return a string description of the proxy configuration
 	 */
 	String toProxyConfigString();
 
