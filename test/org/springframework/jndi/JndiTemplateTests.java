1cc2e0d41d156c204056841429a181ee760388ff
Rod Johnson <rjohnson@interface21.com>
2003-11-20 15:20:43 +0000

    Added 2 lookup tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@587 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jndi/JndiTemplateTests.java
+++ test/org/springframework/jndi/JndiTemplateTests.java
@@ -16,7 +16,7 @@ import org.easymock.MockControl;
  * 
  * @author Rod Johnson
  * @since 08-Jul-2003
- * @version $Id: JndiTemplateTests.java,v 1.2 2003-09-19 11:50:39 johnsonr Exp $
+ * @version $Id: JndiTemplateTests.java,v 1.3 2003-11-20 15:20:43 johnsonr Exp $
  */
 public class JndiTemplateTests extends TestCase {
 
@@ -26,6 +26,55 @@ public class JndiTemplateTests extends TestCase {
 	 */
 	public JndiTemplateTests(String arg0) {
 		super(arg0);
+	}
+	
+	public void testLookupSucceeds() throws Exception {
+		Object o = new Object();
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setReturnValue(o);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() throws NamingException {
+				return mock;
+			}
+		};
+
+		Object o2 = jt.lookup(name);
+		assertEquals(o, o2);
+		mc.verify();
+	}
+	
+	public void testLookupFails() throws Exception {
+		NamingException ne = new NamingException();
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setThrowable(ne);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() throws NamingException {
+				return mock;
+			}
+		};
+
+		try {
+			jt.lookup(name);
+			fail("Should have thrown NamingException");
+		}
+		catch (NamingException ex) {
+			// Ok
+		}
+		mc.verify();
 	}
 	
 	public void testBind() throws Exception {
