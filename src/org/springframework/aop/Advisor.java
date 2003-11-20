eb51a0b5cff40f1992fb5f3dd22dd068027abe84
Rod Johnson <rjohnson@interface21.com>
2003-11-20 10:43:59 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@585 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/Advisor.java
+++ src/org/springframework/aop/Advisor.java
@@ -6,11 +6,22 @@
 package org.springframework.aop;
 
 
-/**
- * Base interface for advice. InterceptionAdvice and IntroductionAdvice
- * are the allowed subclasses.
- * 
- * @version $Id: Advisor.java,v 1.1 2003-11-15 15:29:55 johnsonr Exp $
+/** 
+ * Base interface holding AOP <b>advice</b> (action to take at a joinpoint)
+ * and a filter determining the applicability of the advice (such as 
+ * a pointcut). <i>This interface is not for use by Spring users, but to
+ * allow for commonality in support for different types of advice.</i>
+ * <br>
+ * Spring currently supports <b>around advice</b> delivered via method
+ * <b>interception</b>. However, this interface is intended to allow support for
+ * different types of advice, such as <b>before</b> and <b>after</b> advice,
+ * which need not be implemented using interception.
+ * Note that these advice types can be implemented using around advice
+ * at present. 
+ * @see org.springframework.aop.InterceptionAroundAdvisor
+ * @see org.springframework.aop.InterceptionIntroductionAdvisor
+ * @author Rod Johnson
+ * @version $Id: Advisor.java,v 1.2 2003-11-20 10:43:59 johnsonr Exp $
  */
 public abstract interface Advisor {
 	
@@ -18,7 +29,12 @@ public abstract interface Advisor {
 	// Aspect getAspect();
 	
 	/**
-	 * Is this advice 
+	 * @return wheter this advice is associated with a particular instance
+	 * (for example, creating a mixin) or is it shared with all instances of
+	 * the advised class obtained from the same Spring bean factory?
+	 * <b>Note that this method is not currently used by the framework</b>. Use
+	 * singleton/prototype bean definitions or appropriate programmatic
+	 * proxy creation to ensure that Advisors have the correct lifecycle model. 
 	 */
 	boolean isPerInstance();
 
