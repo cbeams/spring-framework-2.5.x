49e9daa7fdb7dc4bcbb1e56366ce429c7a961e7e
Juergen Hoeller <jhoeller@interface21.com>
2003-11-28 16:39:36 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@719 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/interceptor/HotSwappableInvokerInterceptor.java
+++ src/org/springframework/aop/interceptor/HotSwappableInvokerInterceptor.java
@@ -2,50 +2,45 @@
  * The Spring Framework is published under the terms
  * of the Apache Software License.
  */
- 
+
 package org.springframework.aop.interceptor;
 
 import org.springframework.aop.framework.AopConfigException;
 
-
 /**
- * Implementation of Interceptor interface that 
- * invokes a local target object using reflection.
- * Unlike the simple InvokerInterceptor, this 
- * allows the target to be swapped while the application
- * is running.
- * This should always be the last interceptor in the chain.
- * It does not invoke proceed() on the MethodInvocation. 
+ * Implementation of Interceptor interface that invokes a local target object
+ * using reflection. Unlike the simple InvokerInterceptor, this allows the
+ * target to be swapped while the application is running.
+ *
+ * <P>This should always be the last interceptor in the chain.
+ * It does not invoke proceed() on the MethodInvocation.
+ *
  * @author Rod Johnson
- * @version $Id: HotSwappableInvokerInterceptor.java,v 1.1 2003-11-26 14:46:08 johnsonr Exp $
+ * @version $Id: HotSwappableInvokerInterceptor.java,v 1.2 2003-11-28 16:39:36 jhoeller Exp $
  */
 public class HotSwappableInvokerInterceptor extends AbstractReflectionInvokerInterceptor {
 
-	/** Target cached and invoked using reflection */	
+	/** Target cached and invoked using reflection */
 	private Object target;
-	
-	
+
 	/**
 	 * JavaBean constructor.
 	 * Set the initialTarget property before use.
 	 */
 	public HotSwappableInvokerInterceptor() {
-		
 	}
-	
+
 	public HotSwappableInvokerInterceptor(Object initialTarget) {
 		this.target = initialTarget;
 	}
-	
+
 	/**
 	 * Set the initial target. Construction time only.
-	 * @param target
 	 */
 	public void setInitialTarget(Object target) {
 		this.target = target;
 	}
-	
-	
+
 	/**
 	 * Swap the target, returning the old target
 	 * @param newTarget new target
@@ -53,14 +48,15 @@ public class HotSwappableInvokerInterceptor extends AbstractReflectionInvokerInt
 	 * @throws AopConfigException if the new target is invalid
 	 */
 	public synchronized Object swap(Object newTarget) throws AopConfigException {
-		if (newTarget == null)
+		if (newTarget == null) {
 			throw new AopConfigException("Cannot swap to null");
+		}
 		// TODO type checks
 		Object old = this.target;
 		this.target = newTarget;
 		return old;
 	}
-	
+
 	/**
 	 * Synchronization around something that takes so little time is fine
 	 * @see org.springframework.aop.ProxyInterceptor#getTarget()
@@ -68,7 +64,7 @@ public class HotSwappableInvokerInterceptor extends AbstractReflectionInvokerInt
 	public synchronized Object getTarget() {
 		return this.target;
 	}
-	
+
 	/**
 	 * Two invoker interceptors are equal if they have the same target or if the targets
 	 * are equal.
