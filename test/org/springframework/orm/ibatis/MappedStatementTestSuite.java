d4ce7bf8df82d37c36663ce87a6e94bb85d0ed5c
Juergen Hoeller <jhoeller@interface21.com>
2003-11-28 21:06:57 +0000

    added MappedStatementTemplate test
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@726 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/orm/ibatis/MappedStatementTestSuite.java
+++ test/org/springframework/orm/ibatis/MappedStatementTestSuite.java
@@ -2,9 +2,15 @@ package org.springframework.orm.ibatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.ibatis.db.sqlmap.MappedStatement;
 import com.ibatis.db.sqlmap.SqlMap;
 import junit.framework.TestCase;
+import org.easymock.MockControl;
 
 /**
  * @author Juergen Hoeller
@@ -35,6 +41,46 @@ public class MappedStatementTestSuite extends TestCase {
 		catch (IOException ex) {
 			// expected
 		}
+	}
+
+	public void testMappedStatementTemplate() throws SQLException {
+		MockControl dsControl = MockControl.createControl(DataSource.class);
+		DataSource ds = (DataSource) dsControl.getMock();
+		MockControl conControl = MockControl.createControl(Connection.class);
+		final Connection con = (Connection) conControl.getMock();
+		ds.getConnection();
+		dsControl.setReturnValue(con, 2);
+		con.getMetaData();
+		conControl.setReturnValue(null, 1);
+		con.close();
+		conControl.setVoidCallable(2);
+		dsControl.replay();
+		conControl.replay();
+
+		final MappedStatement stmt = new MappedStatement();
+		SqlMap map = new SqlMap() {
+			public MappedStatement getMappedStatement(String name) {
+				if ("stmt".equals(name)) {
+					return stmt;
+				}
+				return null;
+			}
+		};
+
+		MappedStatementTemplate template = new MappedStatementTemplate();
+		template.setDataSource(ds);
+		template.setSqlMap(map);
+		template.afterPropertiesSet();
+		Object result = template.execute("stmt", new MappedStatementCallback() {
+			public Object doInMappedStatement(MappedStatement s, Connection c) {
+				assertTrue(stmt == s);
+				assertTrue(con == c);
+				return "done";
+			}
+		});
+		assertEquals("done", result);
+		dsControl.verify();
+		conControl.verify();
 	}
 
 }
