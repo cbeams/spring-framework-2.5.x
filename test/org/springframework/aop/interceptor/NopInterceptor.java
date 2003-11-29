a59c8692eeb4d95d0e13fb4c9a947bbd00b63e50
Rod Johnson <rjohnson@interface21.com>
2003-11-29 21:25:56 +0000

    First version
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@735 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ test/org/springframework/aop/interceptor/NopInterceptor.java
@@ -0,0 +1,34 @@
+
+package org.springframework.aop.interceptor;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * Trivial interceptor that can be introduced in a chain to display it.
+ * 
+ * (c) Rod Johnson, 2003
+ * @author Rod Johnson
+ */
+public class NopInterceptor implements MethodInterceptor {
+	
+	
+	private int count;
+
+	/**
+	 * @see org.aopalliance.intercept.MethodInterceptor#invoke(MethodInvocation)
+	 */
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		++count;
+		//System.out.println("Debug interceptor: count=" + count +
+		//	" invocation=[" + invocation + "]");
+		Object rval = invocation.proceed();
+		//System.out.println("Debug interceptor: next returned");
+		return rval;
+	}
+	
+	public int getCount() {
+		return this.count;
+	}
+
+}
