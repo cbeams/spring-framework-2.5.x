d804667939692a60ab91f24dd4eecf1a561a7552
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2003-12-05 02:22:43 +0000

    Refactored rejectIfEmpty() into ValidationUtils
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@806 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/jpetstore/src/org/springframework/samples/jpetstore/domain/logic/OrderValidator.java
+++ samples/jpetstore/src/org/springframework/samples/jpetstore/domain/logic/OrderValidator.java
@@ -2,13 +2,14 @@ package org.springframework.samples.jpetstore.domain.logic;
 
 import org.springframework.samples.jpetstore.domain.Order;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
  * @author Juergen Hoeller
  * @since 01.12.2003
  */
-public class OrderValidator extends PetStoreAbstractValidator implements Validator {
+public class OrderValidator implements Validator {
 
 	public boolean supports(Class clazz) {
 		return Order.class.isAssignableFrom(clazz);
@@ -21,28 +22,28 @@ public class OrderValidator extends PetStoreAbstractValidator implements Validat
 	}
 
 	public void validateCreditCard(Order order, Errors errors) {
-		rejectIfEmpty(errors, "creditCard", "CCN_REQUIRED", "FAKE (!) credit card number required.");
-		rejectIfEmpty(errors, "expiryDate", "EXPIRY_DATE_REQUIRED", "Expiry date is required.");
-		rejectIfEmpty(errors, "cardType", "CARD_TYPE_REQUIRED", "Card type is required.");
+		ValidationUtils.rejectIfEmpty(errors, "creditCard", "CCN_REQUIRED", "FAKE (!) credit card number required.");
+		ValidationUtils.rejectIfEmpty(errors, "expiryDate", "EXPIRY_DATE_REQUIRED", "Expiry date is required.");
+		ValidationUtils.rejectIfEmpty(errors, "cardType", "CARD_TYPE_REQUIRED", "Card type is required.");
 	}
 
 	public void validateBillingAddress(Order order, Errors errors) {
-		rejectIfEmpty(errors, "billToFirstName", "FIRST_NAME_REQUIRED", "Billing Info: first name is required.");
-		rejectIfEmpty(errors, "billToLastName", "LAST_NAME_REQUIRED", "Billing Info: last name is required.");
-		rejectIfEmpty(errors, "billAddress1", "ADDRESS_REQUIRED", "Billing Info: address is required.");
-		rejectIfEmpty(errors, "billCity", "CITY_REQUIRED", "Billing Info: city is required.");
-		rejectIfEmpty(errors, "billState", "STATE_REQUIRED", "Billing Info: state is required.");
-		rejectIfEmpty(errors, "billZip", "ZIP_REQUIRED", "Billing Info: zip/postal code is required.");
-		rejectIfEmpty(errors, "billCountry", "COUNTRY_REQUIRED", "Billing Info: country is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billToFirstName", "FIRST_NAME_REQUIRED", "Billing Info: first name is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billToLastName", "LAST_NAME_REQUIRED", "Billing Info: last name is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billAddress1", "ADDRESS_REQUIRED", "Billing Info: address is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billCity", "CITY_REQUIRED", "Billing Info: city is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billState", "STATE_REQUIRED", "Billing Info: state is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billZip", "ZIP_REQUIRED", "Billing Info: zip/postal code is required.");
+		ValidationUtils.rejectIfEmpty(errors, "billCountry", "COUNTRY_REQUIRED", "Billing Info: country is required.");
 	}
 
 	public void validateShippingAddress(Order order, Errors errors) {
-		rejectIfEmpty(errors, "shipToFirstName", "FIRST_NAME_REQUIRED", "Shipping Info: first name is required.");
-		rejectIfEmpty(errors, "shipToLastName", "LAST_NAME_REQUIRED", "Shipping Info: last name is required.");
-		rejectIfEmpty(errors, "shipAddress1", "ADDRESS_REQUIRED", "Shipping Info: address is required.");
-		rejectIfEmpty(errors, "shipCity", "CITY_REQUIRED", "Shipping Info: city is required.");
-		rejectIfEmpty(errors, "shipState", "STATE_REQUIRED", "Shipping Info: state is required.");
-		rejectIfEmpty(errors, "shipZip", "ZIP_REQUIRED", "Shipping Info: zip/postal code is required.");
-		rejectIfEmpty(errors, "shipCountry", "COUNTRY_REQUIRED", "Shipping Info: country is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipToFirstName", "FIRST_NAME_REQUIRED", "Shipping Info: first name is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipToLastName", "LAST_NAME_REQUIRED", "Shipping Info: last name is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipAddress1", "ADDRESS_REQUIRED", "Shipping Info: address is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipCity", "CITY_REQUIRED", "Shipping Info: city is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipState", "STATE_REQUIRED", "Shipping Info: state is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipZip", "ZIP_REQUIRED", "Shipping Info: zip/postal code is required.");
+		ValidationUtils.rejectIfEmpty(errors, "shipCountry", "COUNTRY_REQUIRED", "Shipping Info: country is required.");
 	}
 }
