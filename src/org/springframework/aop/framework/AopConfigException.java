34d9095291b998ab193d81636a87a190edf5207f
Juergen Hoeller <jhoeller@interface21.com>
2003-12-06 12:38:28 +0000

    fixed exception base class
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@835 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/AopConfigException.java
+++ src/org/springframework/aop/framework/AopConfigException.java
@@ -5,13 +5,13 @@
 
 package org.springframework.aop.framework;
 
-import com.ibatis.common.exception.NestedRuntimeException;
+import org.springframework.core.NestedRuntimeException;
 
 /**
  * Exception that gets thrown on illegal AOP configuration arguments.
  * @author Rod Johnson
  * @since 13-Mar-2003
- * @version $Id: AopConfigException.java,v 1.3 2003-12-05 15:19:24 johnsonr Exp $ 
+ * @version $Id: AopConfigException.java,v 1.4 2003-12-06 12:38:28 jhoeller Exp $ 
  */
 public class AopConfigException extends NestedRuntimeException {
 
@@ -19,8 +19,8 @@ public class AopConfigException extends NestedRuntimeException {
 		super(msg);
 	}
 	
-	public AopConfigException(String msg, Throwable t) {
-		super(msg, t);
+	public AopConfigException(String msg, Throwable ex) {
+		super(msg, ex);
 	}
 
 }
