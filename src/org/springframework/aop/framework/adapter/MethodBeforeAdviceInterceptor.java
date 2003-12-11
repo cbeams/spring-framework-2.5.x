cc5527f234c1525b83477af07f8f4399b8b01047
Rod Johnson <rjohnson@interface21.com>
2003-12-11 15:00:24 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@912 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/aop/framework/adapter/MethodBeforeAdviceInterceptor.java
@@ -0,0 +1,38 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package org.springframework.aop.framework.adapter;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.MethodBeforeAdvice;
+
+
+/**
+ * Interceptor to wrap a MethodBeforeAdvice. In future we may also offer a more efficient alternative
+ * solution in cases where there is no interception advice and therefore no need to
+ * create a MethodInvocation object.
+ * <br>Used internally by the AOP framework: application developers should not need
+ * to use this class directly.
+ * @author Rod Johnson
+ * @version $Id: MethodBeforeAdviceInterceptor.java,v 1.1 2003-12-11 15:00:24 johnsonr Exp $
+ */
+final class MethodBeforeAdviceInterceptor implements MethodInterceptor {
+	
+	private MethodBeforeAdvice advice;
+	
+	public MethodBeforeAdviceInterceptor(MethodBeforeAdvice advice) {
+		this.advice = advice;
+	}
+
+	/**
+	 * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+	 */
+	public Object invoke(MethodInvocation mi) throws Throwable {
+		advice.before(mi.getMethod(), mi.getArguments(), mi.getThis() );
+		return mi.proceed();
+	}
+	
+}
