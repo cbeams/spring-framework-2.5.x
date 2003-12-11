ddebadbb2d8049e520ea734737e7aa1f7f8a59b0
Rod Johnson <rjohnson@interface21.com>
2003-12-11 16:41:44 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@915 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/adapter/GlobalAdvisorAdapterRegistry.java
+++ src/org/springframework/aop/framework/adapter/GlobalAdvisorAdapterRegistry.java
@@ -7,21 +7,25 @@ package org.springframework.aop.framework.adapter;
 
 
 /**
- * 
+ * Singleton to publish a shared DefaultAdvisorAdapterRegistry.
  * @author Rod Johnson
- * @version $Id: GlobalAdvisorAdapterRegistry.java,v 1.1 2003-12-11 14:51:37 johnsonr Exp $
+ * @version $Id: GlobalAdvisorAdapterRegistry.java,v 1.2 2003-12-11 16:41:44 johnsonr Exp $
  */
 public class GlobalAdvisorAdapterRegistry extends DefaultAdvisorAdapterRegistry {
 	
 	private static GlobalAdvisorAdapterRegistry instance = new GlobalAdvisorAdapterRegistry();
 	
+	/**
+	 * @return the per-VM AdapterRegistry instance.
+	 */
 	public static GlobalAdvisorAdapterRegistry getInstance() {
 		return instance;
 	}
 	
+	/**
+	 * Constructor to enforce the Singleton pattern.
+	 */
 	private GlobalAdvisorAdapterRegistry() {
-		
 	}
-
 	
 }
