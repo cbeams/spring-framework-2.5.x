7878944995d65a5da925348195e1f236c0122f00
Darren Davison <darren@davisononline.org>
2003-12-22 00:38:43 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1058 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- autobuilds/src/org/springframework/apptests/jpetstore/AllTests.java
+++ autobuilds/src/org/springframework/apptests/jpetstore/AllTests.java
@@ -18,12 +18,12 @@ import com.meterware.httpunit.WebResponse;
  * AllTests
  * 
  * Not every use case is covered here since the point is to exrcise the 
- * Spring code rather than the petclinic code.  Tests implemented here 
+ * Spring code rather than the application code.  Tests implemented here 
  * are sufficient to check all aspects of Spring that the sample app
  * makes use of.
  * 
  * @author Darren Davison
- * @version $Id: AllTests.java,v 1.1 2003-12-19 01:44:36 davison Exp $
+ * @version $Id: AllTests.java,v 1.2 2003-12-22 00:38:43 davison Exp $
  */
 public class AllTests extends AbstractTestCase {
 
