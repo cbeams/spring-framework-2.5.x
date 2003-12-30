1e06296c707638036b76c7f6bb915c380951c2c5
Juergen Hoeller <jhoeller@interface21.com>
2003-12-30 00:45:28 +0000

    fixed javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1107 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/metadata/AttributeException.java
+++ src/org/springframework/metadata/AttributeException.java
@@ -6,30 +6,19 @@ package org.springframework.metadata;
 
 import org.springframework.core.NestedRuntimeException;
 
-
 /**
- * A simple wrapper for exceptions that occur within the metadata
- * package.
+ * A simple wrapper for exceptions that occur within the metadata package.
  * @author Mark Pollack
  * @since Sep 28, 2003
- * @version $Id: AttributeException.java,v 1.1 2003-12-12 16:54:29 johnsonr Exp $
+ * @version $Id: AttributeException.java,v 1.2 2003-12-30 00:45:28 jhoeller Exp $
  * 
  */
 public class AttributeException extends NestedRuntimeException {
 
-	/**
-	 * {@inheritdoc}
-	 * @param msg {@inheritdoc}
-	 * @param ex {@inheritdoc}
-	 */
 	public AttributeException(String msg, Throwable ex) {
 		super(msg, ex);
 	}
 
-	/**
-	 * {@inheritdoc}
-	 * @param msg {@inheritdoc}
-	 */
 	public AttributeException(String msg) {
 		super(msg);
 	}
