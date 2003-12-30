c27f688629e8decd989c28f5b4a38d96e27cfb8b
Juergen Hoeller <jhoeller@interface21.com>
2003-12-30 02:09:22 +0000

    added OrderServiceClient as remoting test
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1141 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ samples/jpetstore/src/org/springframework/samples/jpetstore/service/client/AxisPortProxyFactoryBean.java
@@ -0,0 +1,49 @@
+package org.springframework.samples.jpetstore.service.client;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.Service;
+import javax.xml.rpc.encoding.TypeMapping;
+import javax.xml.rpc.encoding.TypeMappingRegistry;
+
+import org.apache.axis.encoding.ser.BeanDeserializerFactory;
+import org.apache.axis.encoding.ser.BeanSerializerFactory;
+
+import org.springframework.remoting.jaxrpc.JaxRpcPortProxyFactoryBean;
+import org.springframework.samples.jpetstore.domain.Item;
+import org.springframework.samples.jpetstore.domain.LineItem;
+import org.springframework.samples.jpetstore.domain.Order;
+import org.springframework.samples.jpetstore.domain.Product;
+
+/**
+ * Axis-specific subclass of JaxRpcPortProxyFactoryBean that registers bean
+ * mappings for JPetStore's domain objects. The same mappings are also
+ * registered at the server, in Axis' "server-config.wsdd" file.
+ *
+ * <p>Note: Without such explicit bean mappings, a complex type like Order
+ * cannot be transferred via SOAP.
+ *
+ * @author Juergen Hoeller
+ * @since 27.12.2003
+ * @see org.apache.axis.encoding.ser.BeanDeserializerFactory
+ * @see org.apache.axis.encoding.ser.BeanSerializerFactory
+ */
+public class AxisPortProxyFactoryBean extends JaxRpcPortProxyFactoryBean {
+
+	protected void postProcessJaxRpcService(Service service) {
+		TypeMappingRegistry registry = service.getTypeMappingRegistry();
+		TypeMapping mapping = registry.createTypeMapping();
+		registerBeanMapping(mapping, Product.class, "Product");
+		registerBeanMapping(mapping, Item.class, "Item");
+		registerBeanMapping(mapping, LineItem.class, "LineItem");
+		registerBeanMapping(mapping, Order.class, "Order");
+		registry.register("http://schemas.xmlsoap.org/soap/encoding/", mapping);
+	}
+
+	protected void registerBeanMapping(TypeMapping mapping, Class type, String name) {
+		QName qName = new QName("urn:BeanService", name);
+		mapping.register(type, qName,
+										 new BeanSerializerFactory(type, qName),
+										 new BeanDeserializerFactory(type, qName));
+	}
+
+}
