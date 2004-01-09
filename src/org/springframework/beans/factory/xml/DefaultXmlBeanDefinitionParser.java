c1b1d5e74bd916a79fd76e63dcd19e636c441eda
Juergen Hoeller <jhoeller@interface21.com>
2004-01-09 17:09:54 +0000

    changed all element constants to public
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1277 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/xml/DefaultXmlBeanDefinitionParser.java
+++ src/org/springframework/beans/factory/xml/DefaultXmlBeanDefinitionParser.java
@@ -46,49 +46,49 @@ public class DefaultXmlBeanDefinitionParser implements XmlBeanDefinitionParser {
 	 * Value of a T/F attribute that represents true.
 	 * Anything else represents false. Case seNsItive.
 	 */
-	private static final String TRUE_VALUE = "true";
-	private static final String DEFAULT_VALUE = "default";
+	public static final String TRUE_VALUE = "true";
+	public static final String DEFAULT_VALUE = "default";
 
-	private static final String DEFAULT_LAZY_INIT_ATTRIBUTE = "default-lazy-init";
-	private static final String DEFAULT_DEPENDENCY_CHECK_ATTRIBUTE = "default-dependency-check";
-	private static final String DEFAULT_AUTOWIRE_ATTRIBUTE = "default-autowire";
+	public static final String DEFAULT_LAZY_INIT_ATTRIBUTE = "default-lazy-init";
+	public static final String DEFAULT_DEPENDENCY_CHECK_ATTRIBUTE = "default-dependency-check";
+	public static final String DEFAULT_AUTOWIRE_ATTRIBUTE = "default-autowire";
 
-	private static final String BEAN_ELEMENT = "bean";
-	private static final String DESCRIPTION_ELEMENT = "description";
-	private static final String CLASS_ATTRIBUTE = "class";
-	private static final String PARENT_ATTRIBUTE = "parent";
-	private static final String ID_ATTRIBUTE = "id";
-	private static final String NAME_ATTRIBUTE = "name";
-	private static final String SINGLETON_ATTRIBUTE = "singleton";
-	private static final String DEPENDS_ON_ATTRIBUTE = "depends-on";
-	private static final String INIT_METHOD_ATTRIBUTE = "init-method";
-	private static final String DESTROY_METHOD_ATTRIBUTE = "destroy-method";
-	private static final String CONSTRUCTOR_ARG_ELEMENT = "constructor-arg";
-	private static final String INDEX_ATTRIBUTE = "index";
-	private static final String PROPERTY_ELEMENT = "property";
-	private static final String REF_ELEMENT = "ref";
-	private static final String LIST_ELEMENT = "list";
-	private static final String MAP_ELEMENT = "map";
-	private static final String KEY_ATTRIBUTE = "key";
-	private static final String ENTRY_ELEMENT = "entry";
-	private static final String BEAN_REF_ATTRIBUTE = "bean";
-	private static final String LOCAL_REF_ATTRIBUTE = "local";
-	private static final String EXTERNAL_REF_ATTRIBUTE = "external";
-	private static final String VALUE_ELEMENT = "value";
-	private static final String PROPS_ELEMENT = "props";
-	private static final String PROP_ELEMENT = "prop";
+	public static final String BEAN_ELEMENT = "bean";
+	public static final String DESCRIPTION_ELEMENT = "description";
+	public static final String CLASS_ATTRIBUTE = "class";
+	public static final String PARENT_ATTRIBUTE = "parent";
+	public static final String ID_ATTRIBUTE = "id";
+	public static final String NAME_ATTRIBUTE = "name";
+	public static final String SINGLETON_ATTRIBUTE = "singleton";
+	public static final String DEPENDS_ON_ATTRIBUTE = "depends-on";
+	public static final String INIT_METHOD_ATTRIBUTE = "init-method";
+	public static final String DESTROY_METHOD_ATTRIBUTE = "destroy-method";
+	public static final String CONSTRUCTOR_ARG_ELEMENT = "constructor-arg";
+	public static final String INDEX_ATTRIBUTE = "index";
+	public static final String PROPERTY_ELEMENT = "property";
+	public static final String REF_ELEMENT = "ref";
+	public static final String LIST_ELEMENT = "list";
+	public static final String MAP_ELEMENT = "map";
+	public static final String KEY_ATTRIBUTE = "key";
+	public static final String ENTRY_ELEMENT = "entry";
+	public static final String BEAN_REF_ATTRIBUTE = "bean";
+	public static final String LOCAL_REF_ATTRIBUTE = "local";
+	public static final String EXTERNAL_REF_ATTRIBUTE = "external";
+	public static final String VALUE_ELEMENT = "value";
+	public static final String PROPS_ELEMENT = "props";
+	public static final String PROP_ELEMENT = "prop";
 
-	private static final String LAZY_INIT_ATTRIBUTE = "lazy-init";
+	public static final String LAZY_INIT_ATTRIBUTE = "lazy-init";
 
-	private static final String DEPENDENCY_CHECK_ATTRIBUTE = "dependency-check";
-	private static final String DEPENDENCY_CHECK_ALL_ATTRIBUTE_VALUE = "all";
-	private static final String DEPENDENCY_CHECK_SIMPLE_ATTRIBUTE_VALUE = "simple";
-	private static final String DEPENDENCY_CHECK_OBJECTS_ATTRIBUTE_VALUE = "objects";
+	public static final String DEPENDENCY_CHECK_ATTRIBUTE = "dependency-check";
+	public static final String DEPENDENCY_CHECK_ALL_ATTRIBUTE_VALUE = "all";
+	public static final String DEPENDENCY_CHECK_SIMPLE_ATTRIBUTE_VALUE = "simple";
+	public static final String DEPENDENCY_CHECK_OBJECTS_ATTRIBUTE_VALUE = "objects";
 
-	private static final String AUTOWIRE_ATTRIBUTE = "autowire";
-	private static final String AUTOWIRE_BY_NAME_VALUE = "byName";
-	private static final String AUTOWIRE_BY_TYPE_VALUE = "byType";
-	private static final String AUTOWIRE_CONSTRUCTOR_VALUE = "constructor";
+	public static final String AUTOWIRE_ATTRIBUTE = "autowire";
+	public static final String AUTOWIRE_BY_NAME_VALUE = "byName";
+	public static final String AUTOWIRE_BY_TYPE_VALUE = "byType";
+	public static final String AUTOWIRE_CONSTRUCTOR_VALUE = "constructor";
 
 
 	protected final Log logger = LogFactory.getLog(getClass());
