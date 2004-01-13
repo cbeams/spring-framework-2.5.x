fec6e174433571c8c396c242b3c202d89713166d
Juergen Hoeller <jhoeller@interface21.com>
2004-01-13 18:12:47 +0000

    polished javadocs
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1318 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/ListableBeanFactory.java
+++ src/org/springframework/beans/factory/ListableBeanFactory.java
@@ -11,49 +11,58 @@ import org.springframework.beans.BeansException;
 
 /**
  * Extension of the BeanFactory interface to be implemented by bean factories
- * that can enumerate all their bean instances, rather than attempting bean
- * lookup by name one by one as requested by clients.
+ * that can enumerate all their bean instances, rather than attempting bean lookup
+ * by name one by one as requested by clients. BeanFactory implementations that
+ * preload all their beans (for example, DOM-based XML factories) may implement
+ * this interface. This interface is discussed in "Expert One-on-One J2EE Design
+ * and Development", by Rod Johnson.
  *
- * <p>If this is a HierarchicalBeanFactory, the return values will not take any
- * BeanFactory hierarchy into account, but will relate only to the beans defined
- * in the current factory. Use the BeanFactoryUtils helper class to get all.
+ * <p>If this is a HierarchicalBeanFactory, the return values will <i>not</i> take
+ * any BeanFactory hierarchy into account, but will relate only to the beans defined
+ * in the current factory. Use the BeanFactoryUtils helper class to get all beans.
  * 
+ * <p>The methods in this interface will just respect bean definitions of this factory.
+ * They will ignore any singleton beans that have been registered by other means like
+ * ConfigurableBeanFactory's registerSingleton. Use BeanFactory's methods to access such
+ * beans. In typical scenarios, all beans will be defined by bean definitions anyway.
+ *
  * <p>With the exception of getBeanDefinitionCount(), the methods in this interface
  * are not designed for frequent invocation. Implementations may be slow.
  *
- * <p>BeanFactory implementations that preload all their beans (for example,
- * DOM-based XML factories) may implement this interface.
- *
- * <p>This interface is discussed in "Expert One-on-One J2EE", by Rod Johnson.
- *
  * @author Rod Johnson
  * @since 16 April 2001
- * @version $Id: ListableBeanFactory.java,v 1.6 2003-12-11 18:20:44 jhoeller Exp $
+ * @version $Id: ListableBeanFactory.java,v 1.7 2004-01-13 18:12:47 jhoeller Exp $
  * @see org.springframework.beans.factory.support.BeanFactoryUtils
+ * @see org.springframework.beans.factory.config.ConfigurableBeanFactory#registerSingleton
  */
 public interface ListableBeanFactory extends BeanFactory {
 
 	/**
 	 * Return the number of beans defined in the factory.
 	 * Does not consider any hierarchy this factory may participate in.
+	 * <p>Note: Ignores any singleton beans that have been registered by
+	 * other means than bean definitions.
 	 * @return the number of beans defined in the factory
 	 */
 	int getBeanDefinitionCount();
 
 	/**
-	 * Return the names of all beans defined in this factory
+	 * Return the names of all beans defined in this factory.
 	 * Does not consider any hierarchy this factory may participate in.
+	 * <p>Note: Ignores any singleton beans that have been registered by
+	 * other means than bean definitions.
 	 * @return the names of all beans defined in this factory,
 	 * or an empty array if none defined
 	 */
 	String[] getBeanDefinitionNames();
 	
 	/**
-	 * Return the names of beans matching the given object type (including
-	 * subclasses), judging from the bean definitions. Will <i>not</i>
-	 * consider FactoryBeans as the type of their created objects is not
-	 * known before instantiation.
-	 * <p>Does not consider any hierarchy this factory may participate in.
+	 * Return the names of beans matching the given type (including subclasses),
+	 * judging from the bean definitions. Will <i>not</i> consider FactoryBeans,
+	 * as the type of their created objects is not known before instantiation.
+	 * Does not consider any hierarchy this factory may participate in.
+	 * <p>Note: Ignores any singleton beans that have been registered by
+	 * other means than bean definitions.
 	 * @param type class or interface to match, or null for all bean names
 	 * @return the names of beans matching the given object type 
 	 * (including subclasses), or an empty array if none
@@ -63,6 +72,8 @@ public interface ListableBeanFactory extends BeanFactory {
 	/**
 	 * Check if this bean factory contains a bean definition with the given name.
 	 * Does not consider any hierarchy this factory may participate in.
+	 * <p>Note: Ignores any singleton beans that have been registered by
+	 * other means than bean definitions.
 	 * @param name the name of the bean to look for
 	 * @return if this bean factory contains a bean definition with the given name
 	 */
@@ -72,10 +83,12 @@ public interface ListableBeanFactory extends BeanFactory {
 	 * Return the bean instances that match the given object type (including
 	 * subclasses), judging from either bean definitions or the value of
 	 * getObjectType() in the case of FactoryBeans.
+	 * Does not consider any hierarchy this factory may participate in.
 	 * <p>If FactoryBean's getObjectType() returns null and the bean is a
 	 * singleton, the type of the actually created objects should be evaluated.
 	 * Prototypes without explicit object type specification should be ignored.
-	 * <p>Does not consider any hierarchy this factory may participate in.
+	 * <p>Note: Ignores any singleton beans that have been registered by
+	 * other means than bean definitions.
 	 * @param type class or interface to match
 	 * @param includePrototypes whether to include prototype beans too
 	 * or just singletons (also applies to FactoryBeans)
