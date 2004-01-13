6678c2bb2269b8ef95ad248763a880e5e1c1e497
Rod Johnson <rjohnson@interface21.com>
2004-01-13 13:58:42 +0000

    Fixed canApply() method to work with any PointcutAdvisor, not just InterceptionAroundAdvisor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1302 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/support/AopUtils.java
+++ src/org/springframework/aop/framework/support/AopUtils.java
@@ -7,17 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.aopalliance.intercept.AspectException;
-
 import org.springframework.aop.Advisor;
-import org.springframework.aop.InterceptionAroundAdvisor;
 import org.springframework.aop.InterceptionIntroductionAdvisor;
 import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
 
 /**
  * Utility methods used by the AOP framework.
  * @author Rod Johnson
  * @author Juergen Hoeller
- * @version $Id: AopUtils.java,v 1.11 2003-12-30 01:07:11 jhoeller Exp $
+ * @version $Id: AopUtils.java,v 1.12 2004-01-13 13:58:42 johnsonr Exp $
  */
 public abstract class AopUtils {
 	
@@ -164,12 +163,18 @@ public abstract class AopUtils {
 		return false;
 	}
 	
-	public static boolean canApply(Advisor advice, Class targetClass, Class[] proxyInterfaces) {
-		if (advice instanceof InterceptionIntroductionAdvisor) {
-			return ((InterceptionIntroductionAdvisor) advice).getClassFilter().matches(targetClass);
+	public static boolean canApply(Advisor advisor, Class targetClass, Class[] proxyInterfaces) {
+		if (advisor instanceof InterceptionIntroductionAdvisor) {
+			return ((InterceptionIntroductionAdvisor) advisor).getClassFilter().matches(targetClass);
 		}
-		InterceptionAroundAdvisor interceptionAdvice = (InterceptionAroundAdvisor) advice;
-		return canApply(interceptionAdvice.getPointcut(), targetClass, proxyInterfaces);
+		else if (advisor instanceof PointcutAdvisor) {
+			PointcutAdvisor pca = (PointcutAdvisor) advisor;
+			return canApply(pca.getPointcut(), targetClass, proxyInterfaces);
+		}
+		else {
+			// It doesn't have a pointcut so we assume it applies
+			return true;
+		}
 	}
 
 }
