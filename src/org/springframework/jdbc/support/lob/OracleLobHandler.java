0a5c9bee1d0262d2cdd89b69160f3eecade740b9
Juergen Hoeller <jhoeller@interface21.com>
2004-01-15 18:37:09 +0000

    read Oracle constants via reflection to cover different values from different drivers
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1347 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/support/lob/OracleLobHandler.java
+++ src/org/springframework/jdbc/support/lob/OracleLobHandler.java
@@ -15,8 +15,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -45,16 +47,68 @@ import org.springframework.util.FileCopyUtils;
  * concerns, therefore they are represented by separate strategy interfaces.
  *
  * <p>Coded via reflection to avoid dependencies on Oracle classes.
+ * Even reads in Oracle constants via reflection because of different Oracle
+ * drivers (classes12, ojdbc14) having different constant values!
+ * As it initializes the Oracle classes on instantiation, do not define this
+ * as eager-initializing singleton if you do not want to depend on the Oracle
+ * JAR being in the class path: use "lazy-init=true" to avoid this issue.
  *
  * @author Juergen Hoeller
  * @since 04.12.2003
  * @see #setNativeJdbcExtractor
+ * @see oracle.sql.BLOB
+ * @see oracle.sql.CLOB
  */
 public class OracleLobHandler implements LobHandler {
 
+	private static final String BLOB_CLASS_NAME = "oracle.sql.BLOB";
+
+	private static final String CLOB_CLASS_NAME = "oracle.sql.CLOB";
+
+	private static final String DURATION_SESSION_FIELD_NAME = "DURATION_SESSION";
+
+	private static final String MODE_READWRITE_FIELD_NAME = "MODE_READWRITE";
+
+
 	protected final Log logger = LogFactory.getLog(getClass());
 
+	private final Class blobClass;
+
+	private final Class clobClass;
+
+	private final Map durationSessionConstants = new HashMap();
+
+	private final Map modeReadWriteConstants = new HashMap();
+
 	private NativeJdbcExtractor nativeJdbcExtractor;
+
+	private Boolean cache = Boolean.TRUE;
+
+
+	/**
+	 * This constructor retrieves the oracle.sql.BLOB and oracle.sql.CLOB
+	 * classes via reflection, and initializes the values for the
+	 * DURATION_SESSION and MODE_READWRITE constants defined there.
+	 * @see oracle.sql.BLOB#DURATION_SESSION
+	 * @see oracle.sql.BLOB#MODE_READWRITE
+	 * @see oracle.sql.CLOB#DURATION_SESSION
+	 * @see oracle.sql.CLOB#MODE_READWRITE
+	 */
+	public OracleLobHandler() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+		// initialize oracle.sql.BLOB class
+		this.blobClass = getClass().getClassLoader().loadClass(BLOB_CLASS_NAME);
+		this.durationSessionConstants.put(this.blobClass,
+		                                  new Integer(this.blobClass.getField(DURATION_SESSION_FIELD_NAME).getInt(null)));
+		this.modeReadWriteConstants.put(this.blobClass,
+		                                new Integer(this.blobClass.getField(MODE_READWRITE_FIELD_NAME).getInt(null)));
+
+		// initialize oracle.sql.CLOB class
+		this.clobClass = getClass().getClassLoader().loadClass(CLOB_CLASS_NAME);
+		this.durationSessionConstants.put(this.clobClass,
+		                                  new Integer(this.clobClass.getField(DURATION_SESSION_FIELD_NAME).getInt(null)));
+		this.modeReadWriteConstants.put(this.clobClass,
+		                                new Integer(this.clobClass.getField(MODE_READWRITE_FIELD_NAME).getInt(null)));
+	}
 
 	/**
 	 * Set an appropriate NativeJdbcExtractor to be able to retrieve the underlying
@@ -65,7 +119,17 @@ public class OracleLobHandler implements LobHandler {
 	public void setNativeJdbcExtractor(NativeJdbcExtractor extractor) {
 		this.nativeJdbcExtractor = extractor;
 	}
-	
+
+	/**
+	 * Set whether to cache the temporary LOB in the buffer cache.
+	 * This value will be passed into BLOB/CLOB.createTemporary.
+	 * @see oracle.sql.BLOB#createTemporary
+	 * @see oracle.sql.CLOB#createTemporary
+	 */
+	public void setCache(boolean cache) {
+		this.cache = new Boolean(cache);
+	}
+
 
 	public byte[] getBlobAsBytes(ResultSet rs, int columnIndex) throws SQLException {
 		logger.debug("Returning BLOB as bytes");
@@ -104,15 +168,11 @@ public class OracleLobHandler implements LobHandler {
 
 	protected class OracleLobCreator implements LobCreator {
 
-		private static final int DURATION_SESSION = 10;
-
-		private static final int MODE_READWRITE = 1;
-
 		private final List createdLobs = new ArrayList();
 
 		public void setBlobAsBytes(PreparedStatement ps, int parameterIndex, final byte[] content)
 				throws SQLException {
-			Blob blob = (Blob) createLob(ps, "oracle.sql.BLOB", new LobCallback() {
+			Blob blob = (Blob) createLob(ps, blobClass, new LobCallback() {
 				public void populateLob(Object lob) throws Exception {
 					Method methodToInvoke = lob.getClass().getMethod("getBinaryOutputStream", new Class[0]);
 					((OutputStream) methodToInvoke.invoke(lob, null)).write(content);
@@ -125,7 +185,7 @@ public class OracleLobHandler implements LobHandler {
 		public void setBlobAsBinaryStream(PreparedStatement ps, int parameterIndex,
 		                                  final InputStream binaryStream, int contentLength)
 				throws SQLException {
-			Blob blob = (Blob) createLob(ps, "oracle.sql.BLOB", new LobCallback() {
+			Blob blob = (Blob) createLob(ps, blobClass, new LobCallback() {
 				public void populateLob(Object lob) throws Exception {
 					Method methodToInvoke = lob.getClass().getMethod("getBinaryOutputStream", null);
 					FileCopyUtils.copy(binaryStream, ((OutputStream) methodToInvoke.invoke(lob, null)));
@@ -137,7 +197,7 @@ public class OracleLobHandler implements LobHandler {
 
 		public void setClobAsString(PreparedStatement ps, int parameterIndex, final String content)
 		    throws SQLException {
-			Clob clob = (Clob) createLob(ps, "oracle.sql.CLOB", new LobCallback() {
+			Clob clob = (Clob) createLob(ps, clobClass, new LobCallback() {
 				public void populateLob(Object lob) throws Exception {
 					Method methodToInvoke = lob.getClass().getMethod("getCharacterOutputStream", null);
 					Writer writer = ((Writer) methodToInvoke.invoke(lob, null));
@@ -152,7 +212,7 @@ public class OracleLobHandler implements LobHandler {
 		public void setClobAsAsciiStream(PreparedStatement ps, int parameterIndex,
 		                                 final InputStream asciiStream, int contentLength)
 		    throws SQLException {
-			Clob clob = (Clob) createLob(ps, "oracle.sql.CLOB", new LobCallback() {
+			Clob clob = (Clob) createLob(ps, clobClass, new LobCallback() {
 				public void populateLob(Object lob) throws Exception {
 					Method methodToInvoke = lob.getClass().getMethod("getAsciiOutputStream", null);
 					FileCopyUtils.copy(asciiStream, ((OutputStream) methodToInvoke.invoke(lob, null)));
@@ -165,7 +225,7 @@ public class OracleLobHandler implements LobHandler {
 		public void setClobAsCharacterStream(PreparedStatement ps, int parameterIndex,
 		                                     final Reader characterStream, int contentLength)
 		    throws SQLException {
-			Clob clob = (Clob) createLob(ps, "oracle.sql.CLOB", new LobCallback() {
+			Clob clob = (Clob) createLob(ps, clobClass, new LobCallback() {
 				public void populateLob(Object lob) throws Exception {
 					Method methodToInvoke = lob.getClass().getMethod("getCharacterOutputStream", null);
 					FileCopyUtils.copy(characterStream, ((Writer) methodToInvoke.invoke(lob, null)));
@@ -180,7 +240,7 @@ public class OracleLobHandler implements LobHandler {
 		 * Create a LOB instance for the given PreparedStatement,
 		 * populating it via the given callback.
 		 */
-		protected Object createLob(PreparedStatement ps, String lobClass, LobCallback callback) throws SQLException {
+		protected Object createLob(PreparedStatement ps, Class lobClass, LobCallback callback) throws SQLException {
 			try {
 				Object lob = prepareLob(getOracleConnection(ps), lobClass);
 				callback.populateLob(lob);
@@ -197,11 +257,11 @@ public class OracleLobHandler implements LobHandler {
 					throw (SQLException) ex.getTargetException();
 				}
 				else {
-					throw new DataAccessResourceFailureException("Could not create Oracle BLOB", ex.getTargetException());
+					throw new DataAccessResourceFailureException("Could not create Oracle LOB", ex.getTargetException());
 				}
 			}
 			catch (Exception ex) {
-				throw new DataAccessResourceFailureException("Could not create Oracle BLOB", ex);
+				throw new DataAccessResourceFailureException("Could not create Oracle LOB", ex);
 			}
 		}
 
@@ -222,18 +282,18 @@ public class OracleLobHandler implements LobHandler {
 		/**
 		 * Create and open an oracle.sql.BLOB/CLOB instance via reflection.
 		 */
-		protected Object prepareLob(Connection con, String lobClassName) throws Exception {
+		protected Object prepareLob(Connection con, Class lobClass) throws Exception {
 			/*
 			BLOB blob = BLOB.createTemporary(con, false, BLOB.DURATION_SESSION);
 			blob.open(BLOB.MODE_READWRITE);
 			return blob;
 			*/
-			Class lobClass = Class.forName(lobClassName);
 			Method createTemporary = lobClass.getMethod("createTemporary",
 			                                            new Class[] {Connection.class, boolean.class, int.class});
-			Object lob = createTemporary.invoke(null, new Object[] {con, Boolean.FALSE, new Integer(DURATION_SESSION)});
+			Object lob = createTemporary.invoke(null, new Object[] {con, cache,
+			                                                        durationSessionConstants.get(lobClass)});
 			Method open = lobClass.getMethod("open", new Class[] {int.class});
-			open.invoke(lob, new Object[] {new Integer(MODE_READWRITE)});
+			open.invoke(lob, new Object[] {modeReadWriteConstants.get(lobClass)});
 			return lob;
 		}
 
