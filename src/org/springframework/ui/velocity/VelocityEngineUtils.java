8e4449121053fb3bb7b886a7fd546f4f057e86e8
Juergen Hoeller <jhoeller@interface21.com>
2004-01-26 18:06:41 +0000

    added VelocityEngineUtils
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1386 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/ui/velocity/VelocityEngineUtils.java
@@ -0,0 +1,65 @@
+package org.springframework.ui.velocity;
+
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
+
+/**
+ * Utility class for working with a VelocityEngine.
+ * Provides convenience methods to merge a Velocity template with a model.
+ * @author Juergen Hoeller
+ * @since 22.01.2004
+ */
+public abstract class VelocityEngineUtils {
+
+	private static final Log logger = LogFactory.getLog(VelocityEngineUtils.class);
+
+	/**
+	 * Merge the specified Velocity template with the given model and write
+	 * the result to the given Writer.
+	 * @param velocityEngine VelocityEngine to work with
+	 * @param templateLocation the location of template, relative to Velocity's
+	 * resource loader path
+	 * @param model the Map that contains model names as keys and model objects
+	 * as values
+	 * @param writer the Writer to write the result to
+	 * @throws VelocityException if the template wasn't found or rendering failed
+	 */
+	public static void mergeTemplate(VelocityEngine velocityEngine, String templateLocation, Map model,
+	                                 Writer writer) throws VelocityException {
+		try {
+			velocityEngine.mergeTemplate(templateLocation, new VelocityContext(model), writer);
+		}
+		catch (VelocityException ex) {
+			throw ex;
+		}
+		catch (Exception ex) {
+			logger.error("Why does VelocityEngine throw a generic checked exception, after all?", ex);
+			throw new VelocityException(ex.getMessage());
+		}
+	}
+
+	/**
+	 * Merge the specified Velocity template with the given model into a String.
+	 * @param velocityEngine VelocityEngine to work with
+	 * @param templateLocation the location of template, relative to Velocity's
+	 * resource loader path
+	 * @param model the Map that contains model names as keys and model objects
+	 * as values
+	 * @return the result as String
+	 * @throws VelocityException if the template wasn't found or rendering failed
+	 */
+	public static String mergeTemplateIntoString(VelocityEngine velocityEngine, String templateLocation, Map model)
+	    throws VelocityException {
+		StringWriter result = new StringWriter();
+		mergeTemplate(velocityEngine, templateLocation, model, result);
+		return result.toString();
+	}
+
+}
