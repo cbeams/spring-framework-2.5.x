8970bf80e24f1e0a2d482dfd9459e6ac672c00de
Rod Johnson <rjohnson@interface21.com>
2004-01-31 10:34:58 +0000

    1.4 optimization using IdentityHashMap, suggested by Steven Bazyl
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1411 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/HashMapCachingAdvisorChainFactory.java
+++ src/org/springframework/aop/framework/HashMapCachingAdvisorChainFactory.java
@@ -8,16 +8,36 @@ package org.springframework.aop.framework;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.LogFactory;
 
 /**
  * AdvisorChainFactory implementation that caches by method.
+ * Uses IdentityHashMap in JVM 1.4, which skips expensive Method.hashCode()
+ * call. In 1.3, falls back to using HashMap.
  * @author Rod Johnson
- * @version $Id: HashMapCachingAdvisorChainFactory.java,v 1.2 2003-12-01 10:02:25 johnsonr Exp $
+ * @version $Id: HashMapCachingAdvisorChainFactory.java,v 1.3 2004-01-31 10:34:58 johnsonr Exp $
  */
 public final class HashMapCachingAdvisorChainFactory implements AdvisorChainFactory {
 	
-	private HashMap methodCache = new HashMap();
+	private Map methodCache = createMap();
 	
+	private Map createMap() {
+		// Use IdentityHashMap, introduced in Java 1.4, which is a lot faster
+		// as we want to compare Method keys by reference.
+		// The reason we do this via reflection rather than using new is to avoid a dependence in this
+		// class that will break it under 1.3
+		try {
+			Class clazz = Class.forName("java.util.IdentityHashMap");
+			return (Map) clazz.newInstance();
+		}
+		catch (Exception ex) {
+			// Shouldn't happen
+			LogFactory.getLog(getClass()).debug("Falling back to HashMap (JDK 1.3?): couldn't create an IdentityHashMap using reflection", ex);
+			return new HashMap();
+		}
+	}
 	
 	public List getInterceptorsAndDynamicInterceptionAdvice(Advised config, Object proxy, Method method, Class targetClass) {
 		List cached = (List) this.methodCache.get(method);
