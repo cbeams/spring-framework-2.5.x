acd861a50b7c61bd3c9a0784ac58990cec67a7c0
Darren Davison <darren@davisononline.org>
2004-02-04 23:03:00 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1474 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- autobuilds/apps/jpetstore/src/org/springframework/autobuilds/jpetstore/dao/AccountDao.java
+++ autobuilds/apps/jpetstore/src/org/springframework/autobuilds/jpetstore/dao/AccountDao.java
@@ -2,8 +2,8 @@ package org.springframework.autobuilds.jpetstore.dao;
 
 import java.util.List;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.autobuilds.jpetstore.domain.Account;
+import org.springframework.dao.DataAccessException;
 
 public interface AccountDao {
 
