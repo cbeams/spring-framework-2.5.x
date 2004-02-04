ea75d4753cdb50fb418ad637662e5a6168d8b955
Juergen Hoeller <jhoeller@interface21.com>
2004-02-04 17:09:56 +0000

    attempt to rollback on Error
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1449 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
+++ src/org/springframework/transaction/interceptor/DefaultTransactionAttribute.java
@@ -8,11 +8,11 @@ package org.springframework.transaction.interceptor;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
- * Transaction attribute that takes EJB approach to rolling
+ * Transaction attribute that takes the EJB approach to rolling
  * back on runtime, but not checked, exceptions.
  * @author Rod Johnson
  * @since 16-Mar-2003
- * @version $Id: DefaultTransactionAttribute.java,v 1.2 2003-08-18 16:16:35 jhoeller Exp $
+ * @version $Id: DefaultTransactionAttribute.java,v 1.3 2004-02-04 17:09:56 jhoeller Exp $
  */
 public class DefaultTransactionAttribute extends DefaultTransactionDefinition implements TransactionAttribute {
 
@@ -30,17 +30,18 @@ public class DefaultTransactionAttribute extends DefaultTransactionDefinition im
 	}
 
 	/**
-	 * Default behaviour is as with EJB: rollback on unchecked exception.
+	 * Default behavior is as with EJB: rollback on unchecked exception.
+	 * Additionally attempt to rollback on Error.
 	 * Consistent with TransactionTemplate's behavior.
 	 */
-	public boolean rollbackOn(Throwable t) {
-		return (t instanceof RuntimeException);
+	public boolean rollbackOn(Throwable ex) {
+		return (ex instanceof RuntimeException || ex instanceof Error);
 	}
 
 	/**
 	 * Return a description of this transaction attribute.
 	 * The format matches the one used by TransactionAttributeEditor,
-	 * to be able to feed toString results into TransactionAttribut properties.
+	 * to be able to feed toString results into TransactionAttribute properties.
 	 * @see org.springframework.transaction.interceptor.TransactionAttributeEditor
 	 */
 	public String toString() {
