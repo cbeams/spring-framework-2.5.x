c50023349dba51cbdb1c8a75667f7747860ea280
Juergen Hoeller <jhoeller@interface21.com>
2004-02-07 00:19:36 +0000

    removed View.setName
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1495 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/View.java
+++ src/org/springframework/web/servlet/View.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
  * by Rod Johnson (Wrox, 2002).
  *
  * <p>View implementations may differ widely. An obvious implementation would be
- * JSP-based. Other implementations might be XSLT based, or use an HTML generationlibrary.
+ * JSP-based. Other implementations might be XSLT-based, or use an HTML generation library.
  * This interface is designed to avoid restricting the range of possible implementations.
  *
  * <p>Views should be beans. They are likely to be instantiated as beans by a ViewResolver.
@@ -32,17 +32,9 @@ import javax.servlet.http.HttpServletResponse;
 public interface View {
 
 	/**
-	 * Set the view's name. Helpful for traceability.
-	 * Framework code must call this when constructing views.
-	 * @param name the view's name. May not be null.
-	 * Views should use this for log messages.
-	 */
-	void setName(String name);
-
-	/**
 	 * Render the view given the specified model.
 	 * The first step will be preparing the request: In the JSP case,
-	 * this means setting model objects as request attributes.
+	 * this would mean setting model objects as request attributes.
 	 * @param model Map of model attributes (model name String mapped to model object)
 	 * @param request current HTTP request
 	 * @param response HTTP response we are building
