31ad3db33986c67f7e3756172f3cf49630e284a8
Juergen Hoeller <jhoeller@interface21.com>
2004-02-09 10:24:20 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1508 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/context/support/MessageSourceResourceBundle.java
+++ src/org/springframework/context/support/MessageSourceResourceBundle.java
@@ -8,29 +8,44 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
 /**
- * Allows for accessing a MessageSource as a ResourceBundle.
+ * Helper class that allows for accessing a MessageSource as a ResourceBundle.
+ * Used for example to expose a Spring MessageSource to JSTL web views.
  * @author Juergen Hoeller
  * @since 27.02.2003
+ * @see org.springframework.context.MessageSource
+ * @see java.util.ResourceBundle
+ * @see org.springframework.web.servlet.support.JstlUtils#exposeLocalizationContext
  */
 public class MessageSourceResourceBundle extends ResourceBundle {
 
-	private MessageSource source;
-	private Locale locale;
+	private final MessageSource source;
+
+	private final Locale locale;
 
 	public MessageSourceResourceBundle(MessageSource source, Locale locale) {
 		this.source = source;
 		this.locale = locale;
 	}
 
+	/**
+	 * This implementation resolves the code in the MessageSource.
+	 * Returns null if the message could not be resolved.
+	 */
 	protected Object handleGetObject(String code) {
 		try {
-			return source.getMessage(code, null, locale);
-		} catch (NoSuchMessageException e) {
+			return this.source.getMessage(code, null, this.locale);
+		}
+		catch (NoSuchMessageException ex) {
 			return null;
 		}
 	}
 
+	/**
+	 * This implementation returns null, as a MessageSource does
+	 * not allow for enumerating the defined message codes.
+	 */
 	public Enumeration getKeys() {
 		return null;
 	}
+
 }
