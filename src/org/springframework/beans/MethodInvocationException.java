1e94932cadd4271ae05806dd921e250d0ab887cb
Juergen Hoeller <jhoeller@interface21.com>
2004-02-09 10:17:01 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1507 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/MethodInvocationException.java
+++ src/org/springframework/beans/MethodInvocationException.java
@@ -6,27 +6,27 @@ import java.beans.PropertyChangeEvent;
  * Thrown when a method getter or setter throws an exception,
  * analogous to an InvocationTargetException.
  * @author Rod Johnson
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class MethodInvocationException extends PropertyAccessException {
 
 	/**
 	 * Constructor to use when an exception results from a PropertyChangeEvent.
-	 * @param t Throwable raised by invoked method
+	 * @param ex Throwable raised by invoked method
 	 * @param propertyChangeEvent PropertyChangeEvent that resulted in an exception
 	 */
-	public MethodInvocationException(Throwable t, PropertyChangeEvent propertyChangeEvent) {
-		super("Property '" + propertyChangeEvent.getPropertyName() + "' threw exception", propertyChangeEvent, t);
+	public MethodInvocationException(Throwable ex, PropertyChangeEvent propertyChangeEvent) {
+		super("Property '" + propertyChangeEvent.getPropertyName() + "' threw exception", propertyChangeEvent, ex);
 	}
 
 	/**
 	 * Constructor to use when an exception results from a method invocation,
 	 * and there is no PropertyChangeEvent.
-	 * @param t Throwable raised by invoked method
+	 * @param ex Throwable raised by invoked method
 	 * @param methodName name of the method we were trying to invoke
 	 */
-	public MethodInvocationException(Throwable t, String methodName) {
-		super("Method '" + methodName + "' threw exception", null, t);
+	public MethodInvocationException(Throwable ex, String methodName) {
+		super("Method '" + methodName + "' threw exception", null, ex);
 	}
 
 	public String getErrorCode() {
