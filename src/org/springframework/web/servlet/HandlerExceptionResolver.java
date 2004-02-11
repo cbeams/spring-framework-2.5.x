fbf7d412cc926aacf16427bc8b1db8369531f669
Juergen Hoeller <jhoeller@interface21.com>
2004-02-11 22:31:42 +0000

    fixed javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1543 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/HandlerExceptionResolver.java
+++ src/org/springframework/web/servlet/HandlerExceptionResolver.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Interface to be implemented by objects than can resolve exceptions thrown
- * by handlers to error views. Implementors are typically registered as beans
- * in the application context.
+ * by handlers, in the typical case to error views. Implementors are typically
+ * registered as beans in the application context.
  *
  * <p>Error views are analogous to the error page JSPs, but can be used with
  * any kind of exception including any checked exception, with potentially
@@ -21,7 +21,7 @@ public interface HandlerExceptionResolver {
 	 * Try to resolve the given exception that got thrown during on handler execution,
 	 * returning a ModelAndView that represents a specific error page if appropriate.
 	 * @param request current HTTP request
-	 * @param request current HTTP response
+	 * @param response current HTTP response
 	 * @param handler the executed handler
 	 * @param ex the exception that got thrown during handler execution
 	 * @return a matching ModelAndView to forward to, or null for default processing
