c4d51a69d1e846438eb76995d8885bf6ca76650c
Juergen Hoeller <jhoeller@interface21.com>
2004-02-11 10:51:15 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1531 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/Advisor.java
+++ src/org/springframework/aop/Advisor.java
@@ -5,35 +5,31 @@
 
 package org.springframework.aop;
 
-
 /** 
  * Base interface holding AOP <b>advice</b> (action to take at a joinpoint)
  * and a filter determining the applicability of the advice (such as 
  * a pointcut). <i>This interface is not for use by Spring users, but to
  * allow for commonality in support for different types of advice.</i>
- * <br>
- * Spring currently supports <b>around advice</b> delivered via method
+ *
+ * <p>Spring currently supports <b>around advice</b> delivered via method
  * <b>interception</b>. However, this interface is intended to allow support for
  * different types of advice, such as <b>before</b> and <b>after</b> advice,
- * which need not be implemented using interception.
- * Note that these advice types can be implemented using around advice
- * at present. 
+ * which need not be implemented using interception. Note that these advice
+ * types can be implemented using around advice at present.
+ *
+ * @author Rod Johnson
+ * @version $Id: Advisor.java,v 1.3 2004-02-11 10:51:15 jhoeller Exp $
  * @see org.springframework.aop.InterceptionAroundAdvisor
  * @see org.springframework.aop.InterceptionIntroductionAdvisor
- * @author Rod Johnson
- * @version $Id: Advisor.java,v 1.2 2003-11-20 10:43:59 johnsonr Exp $
  */
-public abstract interface Advisor {
-	
-	
-	// Aspect getAspect();
+public interface Advisor {
 	
 	/**
-	 * @return wheter this advice is associated with a particular instance
+	 * Return whether this advice is associated with a particular instance
 	 * (for example, creating a mixin) or is it shared with all instances of
-	 * the advised class obtained from the same Spring bean factory?
-	 * <b>Note that this method is not currently used by the framework</b>. Use
-	 * singleton/prototype bean definitions or appropriate programmatic
+	 * the advised class obtained from the same Spring bean factory.
+	 * <b>Note that this method is not currently used by the framework</b>.
+	 * Use singleton/prototype bean definitions or appropriate programmatic
 	 * proxy creation to ensure that Advisors have the correct lifecycle model. 
 	 */
 	boolean isPerInstance();
