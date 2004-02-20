dbd0b23df2169849cbdee97ce8ec55fe58d87f8d
Juergen Hoeller <jhoeller@interface21.com>
2004-02-20 16:15:12 +0000

    added scheduling tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1603 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ test/org/springframework/scheduling/SchedulingTestSuite.java
@@ -0,0 +1,188 @@
+package org.springframework.scheduling;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import junit.framework.TestCase;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+
+import org.springframework.beans.TestBean;
+import org.springframework.scheduling.quartz.CronTriggerBean;
+import org.springframework.scheduling.quartz.JobDetailBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerBean;
+import org.springframework.scheduling.timer.MethodInvokingTimerTaskFactoryBean;
+import org.springframework.scheduling.timer.ScheduledTimerTask;
+import org.springframework.scheduling.timer.TimerFactoryBean;
+
+/**
+ * @author Juergen Hoeller
+ * @since 20.02.2004
+ */
+public class SchedulingTestSuite extends TestCase {
+
+	public void testTimerFactoryBean() throws Exception {
+		TestTimerTask timerTask0 = new TestTimerTask();
+		TestMethodInvokingTask task1 = new TestMethodInvokingTask();
+		MethodInvokingTimerTaskFactoryBean mittfb = new MethodInvokingTimerTaskFactoryBean();
+		mittfb.setTargetObject(task1);
+		mittfb.setTargetMethod("doSomething");
+		mittfb.afterPropertiesSet();
+		TimerTask timerTask1 = (TimerTask) mittfb.getObject();
+		ScheduledTimerTask[] tasks = new ScheduledTimerTask[2];
+		tasks[0] = new ScheduledTimerTask(timerTask0, 0, 10, false);
+		tasks[1] = new ScheduledTimerTask(timerTask1, 10, 20, true);
+		TimerFactoryBean timerFactoryBean = new TimerFactoryBean();
+		try {
+			timerFactoryBean.setScheduledTimerTasks(tasks);
+			assertTrue(timerFactoryBean.getObject() instanceof Timer);
+			Thread.sleep(100);
+		}
+		finally {
+			timerFactoryBean.destroy();
+		}
+		assertTrue(timerTask0.counter > 3);
+		assertTrue(task1.counter > 1);
+	}
+
+	public void testSchedulerFactoryBean() throws Exception {
+		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+
+		JobDetailBean jobDetail0 = new JobDetailBean();
+		jobDetail0.setJobClass(TestQuartzJob.class);
+		jobDetail0.setBeanName("myJob0");
+		Map jobData = new HashMap();
+		jobData.put("testBean", new TestBean("tb", 99));
+		jobDetail0.setJobDataAsMap(jobData);
+		jobDetail0.afterPropertiesSet();
+		CronTriggerBean trigger0 = new CronTriggerBean();
+		trigger0.setBeanName("myTrigger0");
+		trigger0.setJobDetail(jobDetail0);
+		trigger0.setCronExpression("0/1 * * * * ?");
+		trigger0.afterPropertiesSet();
+
+		TestMethodInvokingTask task1 = new TestMethodInvokingTask();
+		MethodInvokingJobDetailFactoryBean mijdfb = new MethodInvokingJobDetailFactoryBean();
+		mijdfb.setBeanName("myJob1");
+		mijdfb.setTargetObject(task1);
+		mijdfb.setTargetMethod("doSomething");
+		mijdfb.afterPropertiesSet();
+		JobDetail jobDetail1 = (JobDetail) mijdfb.getObject();
+		SimpleTriggerBean trigger1 = new SimpleTriggerBean();
+		trigger1.setBeanName("myTrigger1");
+		trigger1.setJobDetail(jobDetail1);
+		trigger1.setStartDelay(0);
+		trigger1.setRepeatInterval(20);
+		trigger1.afterPropertiesSet();
+
+		schedulerFactoryBean.setTriggers(new Trigger[] {trigger0, trigger1});
+		TestQuartzJob.counter = 0;
+		try {
+			schedulerFactoryBean.afterPropertiesSet();
+			Thread.sleep(200);
+		}
+		finally {
+			schedulerFactoryBean.destroy();
+		}
+		assertTrue(TestQuartzJob.counter > 0);
+		assertTrue(task1.counter > 6);
+	}
+
+
+	public void testSchedulerFactoryBeanWithPlainQuartzObjects() throws Exception {
+		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+
+		JobDetail jobDetail0 = new JobDetail();
+		jobDetail0.setJobClass(TestQuartzJob.class);
+		jobDetail0.setName("myJob0");
+		jobDetail0.setGroup(Scheduler.DEFAULT_GROUP);
+		jobDetail0.getJobDataMap().put("testBean", new TestBean("tb", 99));
+		CronTrigger trigger0 = new CronTrigger();
+		trigger0.setName("myTrigger0");
+		trigger0.setGroup(Scheduler.DEFAULT_GROUP);
+		trigger0.setJobName("myJob0");
+		trigger0.setJobGroup(Scheduler.DEFAULT_GROUP);
+		trigger0.setStartTime(new Date());
+		trigger0.setCronExpression("0/1 * * * * ?");
+
+		TestMethodInvokingTask task1 = new TestMethodInvokingTask();
+		MethodInvokingJobDetailFactoryBean mijdfb = new MethodInvokingJobDetailFactoryBean();
+		mijdfb.setName("myJob1");
+		mijdfb.setGroup(Scheduler.DEFAULT_GROUP);
+		mijdfb.setTargetObject(task1);
+		mijdfb.setTargetMethod("doSomething");
+		mijdfb.afterPropertiesSet();
+		JobDetail jobDetail1 = (JobDetail) mijdfb.getObject();
+		SimpleTrigger trigger1 = new SimpleTrigger();
+		trigger1.setName("myTrigger1");
+		trigger1.setGroup(Scheduler.DEFAULT_GROUP);
+		trigger1.setJobName("myJob1");
+		trigger1.setJobGroup(Scheduler.DEFAULT_GROUP);
+		trigger1.setStartTime(new Date());
+		trigger1.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+		trigger1.setRepeatInterval(20);
+
+		schedulerFactoryBean.setJobDetails(new JobDetail[] {jobDetail0, jobDetail1});
+		schedulerFactoryBean.setTriggers(new Trigger[] {trigger0, trigger1});
+		TestQuartzJob.counter = 0;
+		try {
+			schedulerFactoryBean.afterPropertiesSet();
+			Thread.sleep(200);
+		}
+		finally {
+			schedulerFactoryBean.destroy();
+		}
+		assertTrue(TestQuartzJob.counter > 0);
+		assertTrue(task1.counter > 6);
+	}
+
+
+	private static class TestTimerTask extends TimerTask {
+
+		private int counter = 0;
+
+		public void run() {
+			counter++;
+		}
+	}
+
+
+	public static class TestQuartzJob extends QuartzJobBean {
+
+		private static int counter = 0;
+
+		private TestBean testBean;
+
+		public void setTestBean(TestBean testBean) {
+			this.testBean = testBean;
+		}
+
+		protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+			if (this.testBean.getAge() == 99) {
+				counter++;
+			}
+		}
+	}
+
+
+	public static class TestMethodInvokingTask {
+
+		private int counter = 0;
+
+		public void doSomething() {
+			counter++;
+		}
+	}
+
+}
