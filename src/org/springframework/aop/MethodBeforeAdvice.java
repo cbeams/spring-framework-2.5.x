406635e365290ca0f55a0cd7c9fdea50af2b304e
Rod Johnson <rjohnson@interface21.com>
2004-02-22 09:59:59 +0000

    Better Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1610 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/MethodBeforeAdvice.java
+++ src/org/springframework/aop/MethodBeforeAdvice.java
@@ -8,12 +8,24 @@ package org.springframework.aop;
 import java.lang.reflect.Method;
 
 /**
- * 
+ * Advice invoked before a method is invoked. Such advices cannot
+ * prevent the method call proceeding, unless they throw a
+ * Throwable. 
  * @author Rod Johnson
- * @version $Id: MethodBeforeAdvice.java,v 1.1 2003-12-05 13:05:54 johnsonr Exp $
+ * @version $Id: MethodBeforeAdvice.java,v 1.2 2004-02-22 09:59:59 johnsonr Exp $
  */
 public interface MethodBeforeAdvice extends BeforeAdvice {
 	
+	/**
+	 * Callback before a given method is invoked
+	 * @param m method being invoked
+	 * @param args arguments to the method
+	 * @param target target of the method invocation. May be null
+	 * @throws Throwable if this object wishes to abort the
+	 * call. Any exception thrown will be returned to the caller
+	 * if it's allowed by the method signature. Otherwise
+	 * the exception will be wrapped as a runtime exception. 
+	 */
 	void before(Method m, Object[] args, Object target) throws Throwable;
 
 }
--- src/org/springframework/aop/TargetSource.java
+++ src/org/springframework/aop/TargetSource.java
@@ -6,9 +6,16 @@
 package org.springframework.aop;
 
 /**
- * 
+ * A TargetSource is used to obtain the current "target" of 
+ * an AOP invocation, which will be invoked via reflection if no
+ * around advice chooses to end the interceptor chain itself. 
+ * <br>If a TargetSource is "static", it will always
+ * return the same target, allowing optimizations in the AOP framework.
+ * Dynamic target sources can support pooling, hot swapping etc.
+ * <br>Application developers don't usually need to work with TargetSources
+ * directly: this is an AOP framework interface.
  * @author Rod Johnson
- * @version $Id: TargetSource.java,v 1.2 2003-11-30 18:10:53 johnsonr Exp $
+ * @version $Id: TargetSource.java,v 1.3 2004-02-22 09:59:59 johnsonr Exp $
  */
 public interface TargetSource {
 	
