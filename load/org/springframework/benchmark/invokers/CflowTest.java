9eb6a4b3c0c625bff309b6e31efed3ab5783f13f
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2004-02-23 18:23:02 +0000

    Reverted the addition of overloaded wrap() method in DefaultAdvisorAdapterRegistry
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1645 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- load/org/springframework/benchmark/invokers/CflowTest.java
+++ load/org/springframework/benchmark/invokers/CflowTest.java
@@ -7,19 +7,18 @@ package org.springframework.benchmark.invokers;
 
 import java.lang.reflect.Method;
 
-import org.springframework.aop.Advisor;
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.aop.framework.Advised;
-import org.springframework.aop.framework.adapter.GlobalAdvisorAdapterRegistry;
 import org.springframework.aop.support.ControlFlowPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.load.AbortTestException;
 import org.springframework.load.TestFailedException;
 
 /**
  * 
  * @author Rod Johnson
- * @version $Id: CflowTest.java,v 1.2 2004-02-23 15:51:05 dkopylenko Exp $
+ * @version $Id: CflowTest.java,v 1.3 2004-02-23 18:23:02 dkopylenko Exp $
  */
 public class CflowTest extends RandomWaitTest {
 
@@ -49,14 +48,8 @@ public class CflowTest extends RandomWaitTest {
 			};
 			ControlFlowPointcut cflow = new ControlFlowPointcut(getClass(), "runPass");
 
-			//DK
-			GlobalAdvisorAdapterRegistry advisorAdapterRegistry = GlobalAdvisorAdapterRegistry.getInstance();
-			//Created overloaded wrap() method to associate pointcut with Advisor
-			Advisor beforeAdvisor = advisorAdapterRegistry.wrap(ba, cflow);
-
-			//advised.addAdvisor(new DefaultBeforeAdvisor(cflow, ba));
-			advised.addAdvisor(beforeAdvisor);
-
+			advised.addAdvisor(new DefaultPointcutAdvisor(cflow, ba));
+			
 			System.out.println(advised.toProxyConfigString());
 			assertEquals("right number of advisors", 2, advised.getAdvisors().length);
 			System.out.println("Service bean class for group " + getGroup() + "=" + service.getClass());
