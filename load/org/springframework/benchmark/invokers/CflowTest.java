f713a38cf1a668f1ce21cdae3f41099325d2c098
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2004-02-23 15:51:05 +0000

    Refactored to use the  newest AOP API
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1639 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- load/org/springframework/benchmark/invokers/CflowTest.java
+++ load/org/springframework/benchmark/invokers/CflowTest.java
@@ -2,30 +2,31 @@
  * The Spring Framework is published under the terms
  * of the Apache Software License.
  */
- 
+
 package org.springframework.benchmark.invokers;
 
 import java.lang.reflect.Method;
 
+import org.springframework.aop.Advisor;
 import org.springframework.aop.BeforeAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.aop.framework.Advised;
+import org.springframework.aop.framework.adapter.GlobalAdvisorAdapterRegistry;
 import org.springframework.aop.support.ControlFlowPointcut;
-import org.springframework.aop.support.DefaultBeforeAdvisor;
 import org.springframework.load.AbortTestException;
 import org.springframework.load.TestFailedException;
 
 /**
  * 
  * @author Rod Johnson
- * @version $Id: CflowTest.java,v 1.1 2003-12-07 18:56:59 johnsonr Exp $
+ * @version $Id: CflowTest.java,v 1.2 2004-02-23 15:51:05 dkopylenko Exp $
  */
 public class CflowTest extends RandomWaitTest {
-	
+
 	protected void runPass(int i) throws TestFailedException, AbortTestException, Exception {
 		Service myService = getService();
 		myService.takeUpToMillis(maxMillis);
-	
+
 		for (int j = 0; j < notAdvised; j++) {
 			myService.notAdvised();
 		}
@@ -43,19 +44,25 @@ public class CflowTest extends RandomWaitTest {
 			Advised advised = (Advised) service;
 			BeforeAdvice ba = new MethodBeforeAdvice() {
 				public void before(Method m, Object[] args, Object target) throws Throwable {
-					//System.err.println(m);
-				}
+						//System.err.println(m);
+	}
 			};
 			ControlFlowPointcut cflow = new ControlFlowPointcut(getClass(), "runPass");
-			advised.addAdvisor(new DefaultBeforeAdvisor(cflow, ba));
-			
+
+			//DK
+			GlobalAdvisorAdapterRegistry advisorAdapterRegistry = GlobalAdvisorAdapterRegistry.getInstance();
+			//Created overloaded wrap() method to associate pointcut with Advisor
+			Advisor beforeAdvisor = advisorAdapterRegistry.wrap(ba, cflow);
+
+			//advised.addAdvisor(new DefaultBeforeAdvisor(cflow, ba));
+			advised.addAdvisor(beforeAdvisor);
+
 			System.out.println(advised.toProxyConfigString());
 			assertEquals("right number of advisors", 2, advised.getAdvisors().length);
 			System.out.println("Service bean class for group " + getGroup() + "=" + service.getClass());
-			
+
 			inited = true;
 		}
 	}
-	
-	
+
 }
