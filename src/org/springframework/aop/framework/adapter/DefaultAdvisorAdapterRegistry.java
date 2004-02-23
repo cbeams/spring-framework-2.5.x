8ee2c03e454103b6eec60466904d0c4c40feb621
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2004-02-23 18:21:08 +0000

    Reverted the addition of overloaded wrap() method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1644 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/adapter/DefaultAdvisorAdapterRegistry.java
+++ src/org/springframework/aop/framework/adapter/DefaultAdvisorAdapterRegistry.java
@@ -2,7 +2,7 @@
  * The Spring Framework is published under the terms
  * of the Apache Software License.
  */
-
+ 
 package org.springframework.aop.framework.adapter;
 
 import java.util.LinkedList;
@@ -10,17 +10,16 @@ import java.util.List;
 
 import org.aopalliance.intercept.Interceptor;
 import org.springframework.aop.Advisor;
-import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 
 /**
  * @author Rod Johnson
- * @version $Id: DefaultAdvisorAdapterRegistry.java,v 1.7 2004-02-23 15:49:56 dkopylenko Exp $
+ * @version $Id: DefaultAdvisorAdapterRegistry.java,v 1.8 2004-02-23 18:21:08 dkopylenko Exp $
  */
 public class DefaultAdvisorAdapterRegistry implements AdvisorAdapterRegistry {
-
+	
 	private List adapters = new LinkedList();
-
+	
 	public DefaultAdvisorAdapterRegistry() {
 		// register well-known adapters
 		registerAdvisorAdapter(new BeforeAdviceAdapter());
@@ -29,22 +28,18 @@ public class DefaultAdvisorAdapterRegistry implements AdvisorAdapterRegistry {
 	}
 
 	public Advisor wrap(Object advice) throws UnknownAdviceTypeException {
-		return wrap(advice, null);
-	}
-
-	public Advisor wrap(Object advice, Pointcut pointcut) throws UnknownAdviceTypeException {
 		if (advice instanceof Advisor) {
 			return (Advisor) advice;
 		}
 		if (advice instanceof Interceptor) {
 			// So well-known it doesn't even need an adapter
-			return getAdvisor(advice, pointcut);
+			return new DefaultPointcutAdvisor(advice);
 		}
 		for (int i = 0; i < this.adapters.size(); i++) {
 			// Check that it is supported
 			AdvisorAdapter adapter = (AdvisorAdapter) this.adapters.get(i);
 			if (adapter.supportsAdvice(advice)) {
-				return getAdvisor(advice, pointcut);
+				return new DefaultPointcutAdvisor(advice);
 			}
 		}
 		throw new UnknownAdviceTypeException(advice);
@@ -66,15 +61,6 @@ public class DefaultAdvisorAdapterRegistry implements AdvisorAdapterRegistry {
 
 	public void registerAdvisorAdapter(AdvisorAdapter adapter) {
 		this.adapters.add(adapter);
-	}
-
-	private Advisor getAdvisor(Object advice, Pointcut pointcut) {
-		if (pointcut == null) {
-			return new DefaultPointcutAdvisor(advice);
-		}
-		else {
-			return new DefaultPointcutAdvisor(pointcut, advice);
-		}
 	}
 
 }
