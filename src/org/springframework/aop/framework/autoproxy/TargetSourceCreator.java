5c0184390aafb9d88e454b2cf713aa073a829944
Juergen Hoeller <jhoeller@interface21.com>
2004-03-01 15:14:35 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1725 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/autoproxy/TargetSourceCreator.java
+++ src/org/springframework/aop/framework/autoproxy/TargetSourceCreator.java
@@ -12,10 +12,12 @@ import org.springframework.beans.factory.BeanFactory;
  * Implementations can create special target sources, such as pooling target
  * sources, for particular beans. For example, they may base their choice
  * on attributes, such as a pooling attribute, on the target class.
- * AbstractAutoProxyCreator can support a number of TargetSourceCreators,
+ *
+ * <p>AbstractAutoProxyCreator can support a number of TargetSourceCreators,
  * which will be applied in order.
+ *
  * @author Rod Johnson
- * @version $Id: TargetSourceCreator.java,v 1.2 2003-12-30 01:07:11 jhoeller Exp $
+ * @version $Id: TargetSourceCreator.java,v 1.3 2004-03-01 15:14:34 jhoeller Exp $
  */
 public interface TargetSourceCreator {
 	
@@ -23,6 +25,7 @@ public interface TargetSourceCreator {
 	 * Create a special TargetSource for the given bean, if any.
 	 * @param bean the bean to create a TargetSource for
 	 * @param beanName the name of the bean
+	 * @param factory the containing factory
 	 * @return a special TargetSource or null if this TargetSourceCreator isn't
 	 * interested in the particular bean
 	 */
