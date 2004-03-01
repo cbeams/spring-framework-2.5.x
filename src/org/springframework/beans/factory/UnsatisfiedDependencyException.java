e2578767e1de75ba65b657a1768724686309d032
Juergen Hoeller <jhoeller@interface21.com>
2004-03-01 09:47:11 +0000

    removed unused overloaded constructors
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1715 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/UnsatisfiedDependencyException.java
+++ src/org/springframework/beans/factory/UnsatisfiedDependencyException.java
@@ -7,18 +7,13 @@ package org.springframework.beans.factory;
 
 /**
  * Exception thrown when a bean depends on other beans or simple properties that were not
- * specified in the bean factory definition, although dependency checking was enabled
+ * specified in the bean factory definition, although dependency checking was enabled.
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @since September 3, 2003
- * @version $Id: UnsatisfiedDependencyException.java,v 1.5 2004-01-20 11:26:43 jhoeller Exp $
- * @see org.springframework.beans.factory.FactoryBean
+ * @version $Id: UnsatisfiedDependencyException.java,v 1.6 2004-03-01 09:47:11 jhoeller Exp $
  */
 public class UnsatisfiedDependencyException extends BeanDefinitionStoreException {
-
-	public UnsatisfiedDependencyException(String beanName, int ctorArgIndex, Class ctorArgType) {
-		this(beanName, ctorArgIndex, ctorArgType, null);
-	}
 
 	public UnsatisfiedDependencyException(String beanName, int ctorArgIndex, Class ctorArgType, String msg) {
 		super("Bean with name '" + beanName + "' has an unsatisfied dependency expressed through " +
@@ -26,13 +21,9 @@ public class UnsatisfiedDependencyException extends BeanDefinitionStoreException
 					(msg != null ? ": " + msg : ""));
 	}
 
-	public UnsatisfiedDependencyException(String beanName, String propertyName) {
-		this(beanName, propertyName, null);
-	}
-
 	public UnsatisfiedDependencyException(String beanName, String propertyName, String msg) {
-		super("Bean with name '" + beanName + "' has an unsatisfied dependency expressed through property '" + 
-				propertyName + "': set this property value or disable dependency checking for this bean" +
+		super("Bean with name '" + beanName + "' has an unsatisfied dependency expressed through property '" +
+					propertyName + "': set this property value or disable dependency checking for this bean" +
 					(msg != null ? ": " + msg : ""));
 	}
 
