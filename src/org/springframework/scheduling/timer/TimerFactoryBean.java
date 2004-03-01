9b4b83d68089046d055d8a2d7e093326ce9bb17d
Juergen Hoeller <jhoeller@interface21.com>
2004-03-01 12:47:22 +0000

    added createTimer hook
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1719 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/scheduling/timer/TimerFactoryBean.java
+++ src/org/springframework/scheduling/timer/TimerFactoryBean.java
@@ -7,19 +7,19 @@ import org.apache.commons.logging.LogFactory;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 
 /**
- * FactoryBean that sets up a J2SE Timer and exposes it for
- * bean references.
+ * FactoryBean that sets up a J2SE Timer and exposes it for bean references.
  *
- * <p>Allows registration of ScheduledTimerTasks, automatically
- * starting the timer on initialization and cancelling it on destruction.
+ * <p>Allows registration of ScheduledTimerTasks, automatically starting
+ * the timer on initialization and cancelling it on destruction.
  * In typical scenarios, there is no need to access the Timer instance
  * itself in application code.
  *
- * <p>Note that Timer uses a TimerTask instance that is shared
- * between repeated executions, in contrast to Quartz which
- * instantiates a new Job for each execution.
+ * <p>Note that Timer uses a TimerTask instance that is shared between
+ * repeated executions, in contrast to Quartz which instantiates a new Job
+ * for each execution.
  *
  * @author Juergen Hoeller
  * @since 19.02.2004
@@ -27,11 +27,15 @@ import org.springframework.beans.factory.FactoryBean;
  * @see java.util.Timer
  * @see java.util.TimerTask
  */
-public class TimerFactoryBean implements FactoryBean, DisposableBean {
+public class TimerFactoryBean implements FactoryBean, InitializingBean, DisposableBean {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private final Timer timer = new Timer();
+	private ScheduledTimerTask[] scheduledTimerTasks;
+
+	private boolean daemon = true;
+
+	private Timer timer;
 
 	/**
 	 * Register a list of ScheduledTimerTask objects with the Timer that
@@ -40,9 +44,25 @@ public class TimerFactoryBean implements FactoryBean, DisposableBean {
 	 * @see java.util.Timer#scheduleAtFixedRate(java.util.TimerTask, long, long)
 	 */
 	public void setScheduledTimerTasks(ScheduledTimerTask[] scheduledTimerTasks) {
+		this.scheduledTimerTasks = scheduledTimerTasks;
+	}
+
+	/**
+	 * Set whether the timer should use a daemon thread,
+	 * just executing as long as the application itself is running.
+	 * Default is true: In a J2EE environment, the container is in
+	 * control of the application lifecycle.
+	 * @see java.util.Timer#Timer(boolean)
+	 */
+	public void setDaemon(boolean daemon) {
+		this.daemon = daemon;
+	}
+
+	public void afterPropertiesSet() {
 		logger.info("Initializing Timer");
-		for (int i = 0; i < scheduledTimerTasks.length; i++) {
-			ScheduledTimerTask scheduledTask = scheduledTimerTasks[i];
+		this.timer = createTimer(this.daemon);
+		for (int i = 0; i < this.scheduledTimerTasks.length; i++) {
+			ScheduledTimerTask scheduledTask = this.scheduledTimerTasks[i];
 			if (scheduledTask.isFixedRate()) {
 				this.timer.scheduleAtFixedRate(scheduledTask.getTimerTask(), scheduledTask.getDelay(),
 																			 scheduledTask.getPeriod());
@@ -52,6 +72,17 @@ public class TimerFactoryBean implements FactoryBean, DisposableBean {
 														scheduledTask.getPeriod());
 			}
 		}
+	}
+
+	/**
+	 * Create a new Timer instance. Called by afterPropertiesSet.
+	 * Can be overridden in subclasses to provide custom Timer subclasses.
+	 * @return a new Timer instance
+	 * @see #afterPropertiesSet
+	 * @see java.util.Timer#Timer(boolean)
+	 */
+	protected Timer createTimer(boolean daemon) {
+		return new Timer(daemon);
 	}
 
 	public Object getObject() {
