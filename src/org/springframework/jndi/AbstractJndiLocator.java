b47ba6494a4dcaf9b9abd6a8dac49f128099c9fb
Juergen Hoeller <jhoeller@interface21.com>
2004-03-04 18:34:08 +0000

    added setJndiEnvironment
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1753 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jndi/AbstractJndiLocator.java
+++ src/org/springframework/jndi/AbstractJndiLocator.java
@@ -5,6 +5,8 @@
 
 package org.springframework.jndi;
 
+import java.util.Properties;
+
 import javax.naming.NamingException;
 
 import org.apache.commons.logging.Log;
@@ -34,7 +36,9 @@ import org.springframework.beans.factory.InitializingBean;
  * this class (i.e. lazy load it instead).<p> 
  *
  * @author Rod Johnson
- * @version $Id: AbstractJndiLocator.java,v 1.7 2004-02-27 14:59:00 luke_t Exp $
+ * @version $Id: AbstractJndiLocator.java,v 1.8 2004-03-04 18:34:08 jhoeller Exp $
+ * @see #setJndiTemplate
+ * @see #setJndiEnvironment
  * @see #setResourceRef
  */
 public abstract class AbstractJndiLocator implements InitializingBean {
@@ -49,6 +53,7 @@ public abstract class AbstractJndiLocator implements InitializingBean {
 	private String jndiName;
 
 	private boolean resourceRef = false;
+
 
 	/**
 	 * Create a new JNDI locator. The jndiName property must be set,
@@ -72,9 +77,11 @@ public abstract class AbstractJndiLocator implements InitializingBean {
 
 	/**
 	 * Set the JNDI template to use for the JNDI lookup.
+	 * You can also specify JNDI environment settings via setJndiEnvironment.
+	 * @see #setJndiEnvironment
 	 */
-	public final void setJndiTemplate(JndiTemplate template) {
-		jndiTemplate = template;
+	public final void setJndiTemplate(JndiTemplate jndiTemplate) {
+		this.jndiTemplate = jndiTemplate;
 	}
 
 	/**
@@ -82,6 +89,22 @@ public abstract class AbstractJndiLocator implements InitializingBean {
 	 */
 	public final JndiTemplate getJndiTemplate() {
 		return jndiTemplate;
+	}
+
+	/**
+	 * Set the JNDI environment to use for the JNDI lookup.
+	 * Creates a JndiTemplate with the given environment settings.
+	 * @see #setJndiTemplate
+	 */
+	public final void setJndiEnvironment(Properties jndiEnvironment) {
+		this.jndiTemplate = new JndiTemplate(jndiEnvironment);
+	}
+
+	/**
+	 * Return the JNDI enviromment to use for the JNDI lookup.
+	 */
+	public final Properties getJndiEnvironment() {
+		return jndiTemplate.getEnvironment();
 	}
 
 	/**
@@ -117,6 +140,7 @@ public abstract class AbstractJndiLocator implements InitializingBean {
 	public final boolean isResourceRef() {
 		return resourceRef;
 	}
+
 
 	public final void afterPropertiesSet() throws NamingException, IllegalArgumentException {
 		if (this.jndiName == null || this.jndiName.equals("")) {
