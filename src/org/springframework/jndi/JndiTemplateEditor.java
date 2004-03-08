bd9d0e471fdd406338f52c43562f7b2bafa93fef
Juergen Hoeller <jhoeller@interface21.com>
2004-03-08 16:59:13 +0000

    preinstantiate PropertiesEditor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1782 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jndi/JndiTemplateEditor.java
+++ src/org/springframework/jndi/JndiTemplateEditor.java
@@ -11,29 +11,28 @@ import java.util.Properties;
 import org.springframework.beans.propertyeditors.PropertiesEditor;
 
 /**
- * Properties editor for JndiTemplate objects. Allows properties-format strings to
- * be used to represent properties editors.
+ * Properties editor for JndiTemplate objects. Allows properties of type
+ * JndiTemplate to be populated with a properties-format string.
  * @author Rod Johnson
  * @since 09-May-2003
- * @version $Id: JndiTemplateEditor.java,v 1.1.1.1 2003-08-14 16:20:34 trisberg Exp $
+ * @version $Id: JndiTemplateEditor.java,v 1.2 2004-03-08 16:59:13 jhoeller Exp $
  */
 public class JndiTemplateEditor extends PropertyEditorSupport {
-	
-	/**
-	 * @see java.beans.PropertyEditor#setAsText(java.lang.String)
-	 */
+
+	private final PropertiesEditor propertiesEditor = new PropertiesEditor();
+
 	public void setAsText(String text) throws IllegalArgumentException {
-		if (text == null)
+		if (text == null) {
 			throw new IllegalArgumentException("JndiTemplate cannot be created from null string");
+		}
 		if ("".equals(text)) {
-			// Empty environment
+			// empty environment
 			setValue(new JndiTemplate());
 		}
 		else {
-			// We have a non-empty properties string
-			PropertiesEditor pe = new PropertiesEditor();
-			pe.setAsText(text);
-			Properties props = (Properties) pe.getValue();
+			// we have a non-empty properties string
+			this.propertiesEditor.setAsText(text);
+			Properties props = (Properties) this.propertiesEditor.getValue();
 			setValue(new JndiTemplate(props));
 		}
 	}
