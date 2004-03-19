93d78efb7f9e96f9c2a8307c6387ca2b2dd2a7b0
Juergen Hoeller <jhoeller@interface21.com>
2004-03-19 17:43:52 +0000

    derive from FatalBeanException rather than BeansException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1942 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/BeanDefinitionValidationException.java
+++ src/org/springframework/beans/factory/support/BeanDefinitionValidationException.java
@@ -16,15 +16,15 @@
 
 package org.springframework.beans.factory.support;
 
-import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
 
 /**
- * Exception if the validation of a bean definition failed.
+ * Exception thrown when the validation of a bean definition failed.
  * @author Juergen Hoeller
  * @since 21.11.2003
  * @see AbstractBeanDefinition#validate
  */
-public class BeanDefinitionValidationException extends BeansException {
+public class BeanDefinitionValidationException extends FatalBeanException {
 
 	public BeanDefinitionValidationException(String msg) {
 		super(msg);
