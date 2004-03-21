d55a5d2fb11bd4deabbf93398262d541890f69f6
Juergen Hoeller <jhoeller@interface21.com>
2004-03-21 15:15:40 +0000

    fixed javadoc error
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@1991 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/AfterReturningAdvice.java
+++ src/org/springframework/aop/AfterReturningAdvice.java
@@ -21,12 +21,10 @@ import java.lang.reflect.Method;
 import org.aopalliance.aop.Advice;
 
 /**
- * After returning advice is invoked
- * only on normal method return, not if an exception is thrown.
- * Such advice can see the return value, but cannot change it.
- * @see org.springframework.aop.MethodAfterReturningAdvice
+ * After returning advice is invoked only on normal method return, not if an
+ * exception is thrown. Such advice can see the return value, but cannot change it.
  * @author Rod Johnson
- * @version $Id: AfterReturningAdvice.java,v 1.4 2004-03-19 18:43:12 johnsonr Exp $
+ * @version $Id: AfterReturningAdvice.java,v 1.5 2004-03-21 15:15:40 jhoeller Exp $
  */
 public interface AfterReturningAdvice extends Advice {
 	
