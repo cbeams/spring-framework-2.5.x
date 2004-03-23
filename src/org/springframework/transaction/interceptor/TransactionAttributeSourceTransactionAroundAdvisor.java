c012b4f658add9766926423ef76c4b2ab2ce0e1f
Juergen Hoeller <jhoeller@interface21.com>
2004-03-23 14:33:30 +0000

    default ordering behavior
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2036 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/interceptor/TransactionAttributeSourceTransactionAroundAdvisor.java
+++ src/org/springframework/transaction/interceptor/TransactionAttributeSourceTransactionAroundAdvisor.java
@@ -20,53 +20,35 @@ import java.lang.reflect.Method;
 
 import org.springframework.aop.framework.AopConfigException;
 import org.springframework.aop.support.StaticMethodMatcherPointcutAdvisor;
-import org.springframework.core.Ordered;
 
 /**
  * InterceptionAroundAdvisor driven by a TransactionAttributeSource.
  * Used to exclude TransactionInterceptor from methods that are non transactional.
- * Because the AOP framework caches advice calculations, this is normally faster
- * than just letting the TransactionInterceptor run and find out itself
- * that it has no work to do.
+ *
+ * <p>Because the AOP framework caches advice calculations, this is normally faster
+ * than just letting the TransactionInterceptor run and find out itself that it
+ * has no work to do.
+ *
+ * @author Rod Johnson
+ * @version $Id: TransactionAttributeSourceTransactionAroundAdvisor.java,v 1.6 2004-03-23 14:33:30 jhoeller Exp $
  * @see org.springframework.transaction.interceptor.TransactionInterceptor
  * @see org.springframework.transaction.interceptor.TransactionProxyFactoryBean
- * @author Rod Johnson
- * @version $Id: TransactionAttributeSourceTransactionAroundAdvisor.java,v 1.5 2004-03-18 02:46:05 trisberg Exp $
  */
-public class TransactionAttributeSourceTransactionAroundAdvisor extends StaticMethodMatcherPointcutAdvisor implements Ordered {
-	
-	/**
-	 * Most advice will want to run within a transaction context,
-	 * so we set the order value to be quite high. Other advisors
-	 * can use a higher value than this if want to ensure that they
-	 * always run within a transaction context. 
-	 * <br>Order value applies only when ordering is not 
-	 * specified in configuration: for example,
-	 * it applies when using an AdvisorAutoProxyCreator.
-	 */
-	public static final int ORDER_VALUE = 10;
+public class TransactionAttributeSourceTransactionAroundAdvisor extends StaticMethodMatcherPointcutAdvisor {
 	
 	private TransactionAttributeSource transactionAttributeSource;
 	
 	public TransactionAttributeSourceTransactionAroundAdvisor(TransactionInterceptor ti) {
 		super(ti);
-		if (ti.getTransactionAttributeSource() == null)
-			throw new AopConfigException("Cannot construct a TransactionAttributeSourceTransactionAroundAdvisor using a TransactionInterceptor that has no TransactionAttributeSource configured");
+		if (ti.getTransactionAttributeSource() == null) {
+			throw new AopConfigException("Cannot construct a TransactionAttributeSourceTransactionAroundAdvisor " +
+																	 "using a TransactionInterceptor that has no TransactionAttributeSource configured");
+		}
 		this.transactionAttributeSource = ti.getTransactionAttributeSource();
 	}
 
-	/**
-	 * @see org.springframework.aop.MethodMatcher#matches(java.lang.reflect.Method, java.lang.Class)
-	 */
 	public boolean matches(Method m, Class targetClass) {
-		return transactionAttributeSource.getTransactionAttribute(m, targetClass) != null;
-	}
-
-	/**
-	 * @see org.springframework.core.Ordered#getOrder()
-	 */
-	public int getOrder() {
-		return ORDER_VALUE;
+		return (this.transactionAttributeSource.getTransactionAttribute(m, targetClass) != null);
 	}
 
 }
