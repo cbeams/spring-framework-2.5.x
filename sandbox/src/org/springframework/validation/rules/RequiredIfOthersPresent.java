5b8066cbbfdebaa94087963499422b93da30caa4
Keith Donald <kdonald@interface21.com>
2004-03-30 15:18:11 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2118 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/validation/rules/RequiredIfOthersPresent.java
+++ sandbox/src/org/springframework/validation/rules/RequiredIfOthersPresent.java
@@ -1,73 +1,64 @@
 /*
- * The Spring Framework is published under the terms of the Apache Software
- * License.
+ * $Header:
+ * /usr/local/cvs/product/project/src/java/com/csi/product/project/Type.java,v
+ * 1.1 2004/01/26 23:10:32 keith Exp $ $Revision: 1.4 $ $Date: 2004/01/26
+ * 23:10:32 $
+ * 
+ * Copyright Computer Science Innovations (CSI), 2003. All rights reserved.
  */
 package org.springframework.validation.rules;
 
 import java.util.Iterator;
 import java.util.Set;
 
-import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.functor.LogicalOperator;
-import org.springframework.functor.UnaryPredicate;
 import org.springframework.functor.predicates.CompoundUnaryPredicate;
 import org.springframework.functor.predicates.PropertyPresent;
-import org.springframework.functor.predicates.Required;
 import org.springframework.functor.predicates.UnaryAnd;
 import org.springframework.functor.predicates.UnaryOr;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 /**
- * Validates property value as 'required' if and only if some other properties
- * are also provided.
- * 
- * @author Seth Ladd
  * @author Keith Donald
  */
-public class RequiredIfOthersPresent implements UnaryPredicate {
-    private String propertyName;
-    private CompoundUnaryPredicate compoundPredicate;
+public class RequiredIfOthersPresent extends RequiredIfTrue {
 
+    /**
+     * Tests that the property is required if all "other properties" are
+     * present. Present means they are "non null."
+     * 
+     * @param otherPropertyNames
+     *            one or more other properties, delimited by commas.
+     */
     public RequiredIfOthersPresent(String otherPropertyNames) {
         this(otherPropertyNames, LogicalOperator.AND);
     }
 
+    /**
+     * Tests that the property is required if all or any of the "other
+     * properties" are present.
+     * 
+     * @param otherPropertyNames
+     *            one or more other properties, delimited by commas.
+     * @param operator
+     *            the logical operator, either AND or OR.
+     */
     public RequiredIfOthersPresent(String otherPropertyNames,
             LogicalOperator operator) {
+        super();
         Assert.notNull(otherPropertyNames);
         Assert.notNull(operator);
         Set set = StringUtils.commaDelimitedListToSet(otherPropertyNames);
         Assert.hasElements(set);
+        CompoundUnaryPredicate compoundPredicate;
         if (operator == LogicalOperator.AND) {
-            this.compoundPredicate = new UnaryAnd();
+            compoundPredicate = new UnaryAnd();
         } else {
-            this.compoundPredicate = new UnaryOr();
+            compoundPredicate = new UnaryOr();
         }
         for (Iterator i = set.iterator(); i.hasNext();) {
             compoundPredicate.add(new PropertyPresent((String)i.next()));
-        }
-    }
-
-    public RequiredIfOthersPresent(CompoundUnaryPredicate compoundPredicate) {
-        Assert.notNull(compoundPredicate);
-        compoundPredicate.validateTypeSafety(PropertyPresent.class);
-        this.compoundPredicate = compoundPredicate;
-    }
-
-    public void setPropertyName(String propertyName) {
-        Assert.notNull(propertyName);
-        this.propertyName = propertyName;
-    }
-
-    public boolean evaluate(Object bean) {
-        Assert.notNull(propertyName);
-        boolean present = compoundPredicate.evaluate(bean);
-        if (present) {
-            return Required.instance().evaluate(
-                    new BeanWrapperImpl(bean).getPropertyValue(propertyName));
-        } else {
-            return true;
         }
     }
 
---
+++ sandbox/src/org/springframework/validation/rules/RequiredIfTrue.java
@@ -0,0 +1,66 @@
+/*
+ * The Spring Framework is published under the terms of the Apache Software
+ * License.
+ */
+package org.springframework.validation.rules;
+
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.functor.UnaryPredicate;
+import org.springframework.functor.predicates.Required;
+import org.springframework.util.Assert;
+
+/**
+ * Validates a property value as 'required' if some other condition is true.
+ * 
+ * @author Seth Ladd
+ * @author Keith Donald
+ */
+public class RequiredIfTrue implements UnaryPredicate {
+    private String propertyName;
+    private UnaryPredicate predicate;
+
+    /**
+     * Tests that the property is required if the provided predicate is
+     * satisified.
+     * 
+     * @param otherPropertyNames
+     *            one or more other properties, delimited by commas.
+     * @param operator
+     *            the logical operator, either AND or OR.
+     */
+    public RequiredIfTrue(UnaryPredicate predicate) {
+        Assert.notNull(predicate);
+        setPredicate(predicate);
+    }
+
+    protected RequiredIfTrue() {
+
+    }
+
+    protected void setPredicate(UnaryPredicate predicate) {
+        Assert.notNull(predicate);
+        this.predicate = predicate;
+    }
+
+    /**
+     * The property name which may be required if other properties are present.
+     * 
+     * @param propertyName
+     *            The property name
+     */
+    public void setPropertyName(String propertyName) {
+        Assert.notNull(propertyName);
+        this.propertyName = propertyName;
+    }
+
+    public boolean evaluate(Object bean) {
+        Assert.notNull(propertyName);
+        if (predicate.evaluate(bean)) {
+            return Required.instance().evaluate(
+                    new BeanWrapperImpl(bean).getPropertyValue(propertyName));
+        } else {
+            return true;
+        }
+    }
+
+}
