502194de236ca38023313b16c68305073a3f95e1
Keith Donald <kdonald@interface21.com>
2004-03-30 00:34:41 +0000

    updated copyright
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2099 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/functor/UnaryFunction.java
+++ sandbox/src/org/springframework/functor/UnaryFunction.java
@@ -1,9 +1,17 @@
 /*
- * $Header: /var/local/springframework.cvs.sourceforge.net/spring/sandbox/src/org/springframework/functor/UnaryFunction.java,v 1.1 2004-03-30 00:32:19 kdonald Exp $
- * $Revision: 1.1 $
- * $Date: 2004-03-30 00:32:19 $
- *
- * Copyright Computer Science Innovations (CSI), 2003. All rights reserved.
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.functor;
 
