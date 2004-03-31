b2c43424baf38827636f1a24271ba0f7bfc05f6d
Keith Donald <kdonald@interface21.com>
2004-03-31 04:32:22 +0000

    new factory methods
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2134 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/functor/PredicateFactory.java
+++ sandbox/src/org/springframework/functor/PredicateFactory.java
@@ -2,22 +2,24 @@
  * Copyright 2002-2004 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 package org.springframework.functor;
 
 import org.springframework.functor.predicates.ParameterizedBinaryPredicate;
+import org.springframework.functor.predicates.UnaryAnd;
 import org.springframework.functor.predicates.UnaryFunctionTester;
 import org.springframework.functor.predicates.UnaryNot;
+import org.springframework.functor.predicates.UnaryOr;
 
 /**
  * A factory for easing the construction of predicates.
@@ -27,9 +29,9 @@ import org.springframework.functor.predicates.UnaryNot;
 public class PredicateFactory {
 
     /**
-     * Bind the specified parameter to the second argument of a BinaryPredicate,
-     * returning a UnaryPredicate which will test a single variable argument
-     * against the constant parameter.
+     * Bind the specified parameter to the second argument of a
+     * BinaryPredicate, returning a UnaryPredicate which will test a single
+     * variable argument against the constant parameter.
      * 
      * @param predicate
      *            the binary predicate to bind to
@@ -37,8 +39,9 @@ public class PredicateFactory {
      *            the parameter value (constant)
      * @return The unary predicate
      */
-    public static UnaryPredicate bind(BinaryPredicate predicate,
-            Object parameter) {
+    public static UnaryPredicate bind(
+        BinaryPredicate predicate,
+        Object parameter) {
         return new ParameterizedBinaryPredicate(predicate, parameter);
     }
 
@@ -53,8 +56,9 @@ public class PredicateFactory {
      * @return The testing predicate, which on the call to test(o) first
      *         evaluates 'o' using the function and then tests the result.
      */
-    public static UnaryPredicate attachResultTester(UnaryPredicate tester,
-            UnaryFunction function) {
+    public static UnaryPredicate attachResultTester(
+        UnaryPredicate tester,
+        UnaryFunction function) {
         return new UnaryFunctionTester(tester, function);
     }
 
@@ -70,6 +74,56 @@ public class PredicateFactory {
             throw new IllegalArgumentException("Predicate is already negated");
         }
         return new UnaryNot(predicate);
+    }
+
+    /**
+     * Returns a new, empty unary conjunction prototype, capable of composing
+     * individual predicates where 'ALL' must test true.
+     * 
+     * @return the UnaryAnd
+     */
+    public static UnaryAnd conjunction() {
+        return new UnaryAnd();
+    }
+
+    /**
+     * Returns a new, empty unary disjunction prototype, capable of composing
+     * individual predicates where 'ANY' must test true.
+     * 
+     * @return the UnaryAnd
+     */
+    public static UnaryOr disjunction() {
+        return new UnaryOr();
+    }
+
+    /**
+     * AND two predicates.
+     * 
+     * @param predicate1
+     *            the first predicate
+     * @param predicate2
+     *            the second predicate
+     * @return The compound AND predicate
+     */
+    public static UnaryPredicate and(
+        UnaryPredicate predicate1,
+        UnaryPredicate predicate2) {
+        return new UnaryAnd(predicate1, predicate2);
+    }
+
+    /**
+     * OR two predicates.
+     * 
+     * @param predicate1
+     *            the first predicate
+     * @param predicate2
+     *            the second predicate
+     * @return The compound OR predicate
+     */
+    public static UnaryPredicate or(
+        UnaryPredicate predicate1,
+        UnaryPredicate predicate2) {
+        return new UnaryOr(predicate1, predicate2);
     }
 
 }
