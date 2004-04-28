e7286d699dc4cb6d134c614d2e8cf6e3ae6adc15
Juergen Hoeller <jhoeller@interface21.com>
2004-04-28 07:21:14 +0000

    added convenience classes for LOB handling with JdbcTemplate
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2374 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/jdbc/core/support/AbstractLobCreatingPreparedStatementCallback.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.core.support;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+import org.springframework.jdbc.support.lob.LobCreator;
+import org.springframework.jdbc.support.lob.LobHandler;
+
+/**
+ * Abstract PreparedStatementCallback implementation that manages a LobCreator.
+ * Typically used as inner class, with access to surrounding method arguments.
+ *
+ * <p>Delegates to the <code>setValues</code> template method for setting values
+ * on the PreparedStatement, using a given LobCreator for BLOB/CLOB arguments.
+ *
+ * @author Juergen Hoeller
+ * @since 27.04.2004
+ * @see org.springframework.jdbc.support.lob.LobCreator
+ */
+public abstract class AbstractLobCreatingPreparedStatementCallback implements PreparedStatementCallback {
+
+	private final LobHandler lobHandler;
+
+	/**
+	 * Create a new AbstractLobCreatingPreparedStatementCallback for the
+	 * given LobHandler.
+	 * @param lobHandler the LobHandler to create LobCreators with
+	 */
+	public AbstractLobCreatingPreparedStatementCallback(LobHandler lobHandler) {
+		this.lobHandler = lobHandler;
+	}
+
+	public final Object doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
+		LobCreator lobCreator = this.lobHandler.getLobCreator();
+		try {
+			setValues(ps, lobCreator);
+			return new Integer(ps.executeUpdate());
+		}
+		finally {
+			lobCreator.close();
+		}
+	}
+
+	/**
+	 * Set values on the given PreparedStatement, using the given
+	 * LobCreator for BLOB/CLOB arguments.
+	 * @param ps the PreparedStatement to use
+	 * @param lobCreator the LobCreator to use
+	 * @throws SQLException if thrown by JDBC methods
+	 * @throws DataAccessException in case of custom exceptions
+	 */
+	protected abstract void setValues(PreparedStatement ps, LobCreator lobCreator)
+			throws SQLException, DataAccessException;
+
+}
---
+++ src/org/springframework/jdbc/core/support/AbstractLobStreamingResultSetExtractor.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.core.support;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.LobRetrievalFailureException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+/**
+ * Abstract ResultSetExtractor implementation that assumes streaming of LOB data.
+ * Typically used as inner class, with access to surrounding method arguments.
+ *
+ * <p>Delegates to the <code>streamData</code> template method for streaming LOB
+ * content to some OutputStream, typically using a LobHandler. Converts an
+ * IOException thrown during streaming to a LobRetrievalFailureException.
+ *
+ * @author Juergen Hoeller
+ * @since 28.04.2004
+ * @see org.springframework.jdbc.support.lob.LobHandler
+ * @see org.springframework.jdbc.LobRetrievalFailureException
+ */
+public abstract class AbstractLobStreamingResultSetExtractor implements ResultSetExtractor {
+
+	/**
+	 * Delegates to handleNoRowFound, handleMultipleRowsFound and streamData,
+	 * according to the ResultSet state. Converts an IOException thrown by
+	 * streamData to a LobRetrievalFailureException.
+	 * @see #handleNoRowFound
+	 * @see #handleMultipleRowsFound
+	 * @see #streamData
+	 * @see org.springframework.jdbc.LobRetrievalFailureException
+	 */
+	public final Object extractData(ResultSet rs) throws SQLException, DataAccessException {
+		if (!rs.next()) {
+			handleNoRowFound();
+		}
+		else {
+			try {
+				streamData(rs);
+				if (rs.next()) {
+					handleMultipleRowsFound();
+				}
+			}
+			catch (IOException ex) {
+				throw new LobRetrievalFailureException("Couldn't stream LOB content", ex);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Handle the case where the ResultSet does not contain a row.
+	 * @throws DataAccessException a corresponding exception,
+	 * by default a LobRetrievalFailureException
+	 * @see org.springframework.jdbc.LobRetrievalFailureException
+	 */
+	protected void handleNoRowFound() throws DataAccessException {
+		throw new LobRetrievalFailureException("No row found in database");
+	}
+
+	/**
+	 * Handle the case where the ResultSet contains multiple rows.
+	 * @throws DataAccessException a corresponding exception,
+	 * by default a LobRetrievalFailureException
+	 * @see org.springframework.jdbc.LobRetrievalFailureException
+	 */
+	protected void handleMultipleRowsFound() throws DataAccessException {
+		throw new LobRetrievalFailureException("Multiple rows found in database");
+	}
+
+	/**
+	 * Stream LOB content from the given ResultSet to some OutputStream.
+	 * <p>Typically used as inner class, with access to surrounding method arguments
+	 * and to a LobHandler instance variable of the surrounding class.
+	 * @param rs the ResultSet to take the LOB content from
+	 * @throws SQLException if thrown by JDBC methods
+	 * @throws IOException if thrown by stream access methods
+	 * @throws DataAccessException in case of custom exceptions
+	 * @see org.springframework.jdbc.support.lob.LobHandler#getBlobAsBinaryStream
+	 * @see org.springframework.util.FileCopyUtils
+	 */
+	protected abstract void streamData(ResultSet rs) throws SQLException, IOException, DataAccessException;
+
+}
