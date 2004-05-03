f6d93da7e481203faf39fb1d76440f3573455ea8
Juergen Hoeller <jhoeller@interface21.com>
2004-05-03 09:38:13 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2437 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/HandlerMapping.java
+++ src/org/springframework/web/servlet/HandlerMapping.java
@@ -32,7 +32,7 @@ import javax.servlet.http.HttpServletRequest;
  * instance, optionally accompanied by some HandlerInterceptor instances.
  * The DispatcherServlet will first call each HandlerInterceptor's preHandle
  * method in the given order, finally invoking the handler itself if all
- * preHandle method have returned "true".
+ * preHandle methods have returned "true".
  *
  * <p>The ability to parameterize this mapping is a powerful and unusual
  * capability of this MVC framework. For example, it is possible to write
@@ -44,6 +44,7 @@ import javax.servlet.http.HttpServletRequest;
  * DispatcherServlet. Non-Ordered instances get treated as lowest priority.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @see org.springframework.core.Ordered
  * @see org.springframework.web.servlet.handler.AbstractHandlerMapping
  * @see org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping
@@ -54,9 +55,10 @@ public interface HandlerMapping {
 	/**
 	 * Return a handler and any interceptors for this request. The choice may be made
 	 * on request URL, session state, or any factor the implementing class chooses.
-	 * <p>This returns Object, rather than even a tag interface, so that handlers
-	 * are not constrained in any way. For example, a HandlerAdapter could be
-	 * written to allow another framework's handler objects to be used.
+	 * <p>The returned HandlerExecutionChain contains a handler Object, rather than
+	 * even a tag interface, so that handlers are not constrained in any way.
+	 * For example, a HandlerAdapter could be written to allow another framework's
+	 * handler objects to be used.
 	 * <p>Returns null if no match was found. This is not an error. The
 	 * DispatcherServlet will query all registered HandlerMapping beans to find
 	 * a match, and only decide there is an error if none can find a handler.
