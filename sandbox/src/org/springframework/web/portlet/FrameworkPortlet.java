6ce14c75ff87e546ed0874bb3710ab2ab620fd3e
wgthom <wgthom@users.sourceforge.net>
2004-05-07 20:17:07 +0000

    move default servlet mapping to /WEB-INF/servlet/view
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2467 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/portlet/FrameworkPortlet.java
+++ sandbox/src/org/springframework/web/portlet/FrameworkPortlet.java
@@ -80,7 +80,7 @@ import org.springframework.web.portlet.context.support.XmlPortletApplicationCont
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @author William G. Thompson, Jr.
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * @see #doService
  * @see #initFrameworkPortlet
  * @see #setContextClass
@@ -115,7 +115,7 @@ public abstract class FrameworkPortlet extends PortletBean {
 	 * portlet render requests to servlet requests in order to leverage the view support
 	 * in org.springframework.web.view.
 	 */
-	public static final String DEFAULT_VIEW_RENDERER_SERVLET = "/view";
+	public static final String DEFAULT_VIEW_RENDERER_SERVLET = "/WEB-INF/servlet/view";
 
 	/** Custom PortletApplicationContext implementation class */
 	private Class contextClass = DEFAULT_CONTEXT_CLASS;
