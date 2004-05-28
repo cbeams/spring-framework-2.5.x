1e35af877270e0225601382b6dcbd488813a5956
Juergen Hoeller <jhoeller@interface21.com>
2004-05-28 10:19:29 +0000

    added support for valueOf/toString as alternative to NumberFormat
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2608 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/propertyeditors/CustomNumberEditor.java
+++ src/org/springframework/beans/propertyeditors/CustomNumberEditor.java
@@ -22,9 +22,13 @@ import java.math.BigInteger;
 import java.text.NumberFormat;
 import java.text.ParseException;
 
+import org.springframework.util.StringUtils;
+
 /**
  * Property editor for any Number subclass like Integer, Long, Float, Double.
- * Uses a given NumberFormat for (locale-specific) parsing and rendering.
+ * Can use a given NumberFormat for (locale-specific) parsing and rendering,
+ * or alternatively the default <code>valueOf</code> respectively
+ * <code>toString</code> methods.
  *
  * <p>This is not meant to be used as system PropertyEditor but rather as
  * locale-specific number editor within custom controller code, to parse
@@ -50,8 +54,31 @@ public class CustomNumberEditor extends PropertyEditorSupport {
 	private final boolean allowEmpty;
 
 	/**
-	 * Create a new instance, using the given NumberFormat for
-	 * parsing and rendering.
+	 * Create a new CustomNumberEditor instance, using the default
+	 * <code>valueOf</code> methods for parsing and <code>toString</code>
+	 * methods for rendering.
+	 * <p>The allowEmpty parameter states if an empty String should
+	 * be allowed for parsing, i.e. get interpreted as null value.
+	 * Else, an IllegalArgumentException gets thrown in that case.
+	 * @param numberClass Number subclass to generate
+	 * @param allowEmpty if empty strings should be allowed
+	 * @throws IllegalArgumentException if an invalid numberClass has been specified
+	 * @see Integer#valueOf
+	 * @see Integer#toString
+	 */
+	public CustomNumberEditor(Class numberClass, boolean allowEmpty)
+	    throws IllegalArgumentException {
+		if (numberClass == null || !Number.class.isAssignableFrom(numberClass)) {
+			throw new IllegalArgumentException("Property class must be a subclass of Number");
+		}
+		this.numberClass = numberClass;
+		this.numberFormat = null;
+		this.allowEmpty = allowEmpty;
+	}
+
+	/**
+	 * Create a new CustomNumberEditor instance, using the given NumberFormat
+	 * for parsing and rendering.
 	 * <p>The allowEmpty parameter states if an empty String should
 	 * be allowed for parsing, i.e. get interpreted as null value.
 	 * Else, an IllegalArgumentException gets thrown in that case.
@@ -62,7 +89,7 @@ public class CustomNumberEditor extends PropertyEditorSupport {
 	 */
 	public CustomNumberEditor(Class numberClass, NumberFormat numberFormat, boolean allowEmpty)
 	    throws IllegalArgumentException {
-		if (!Number.class.isAssignableFrom(numberClass)) {
+		if (numberClass == null || !Number.class.isAssignableFrom(numberClass)) {
 			throw new IllegalArgumentException("Property class must be a subclass of Number");
 		}
 		this.numberClass = numberClass;
@@ -71,10 +98,12 @@ public class CustomNumberEditor extends PropertyEditorSupport {
 	}
 
 	public void setAsText(String text) throws IllegalArgumentException {
-		if (this.allowEmpty && (text == null || "".equals(text.trim()))) {
+		if (this.allowEmpty && !StringUtils.hasText(text)) {
 			setValue(null);
 		}
-		else {
+
+		// use given NumberFormat for parsing text
+		else if (this.numberFormat != null) {
 			try {
 				Number number = this.numberFormat.parse(text);
 				if (this.numberClass.isInstance(number)) {
@@ -102,18 +131,56 @@ public class CustomNumberEditor extends PropertyEditorSupport {
 					setValue(new BigDecimal(Double.toString(number.doubleValue())));
 				}
 				else {
-					throw new IllegalArgumentException("Cannot convert [" + text + "] to [" + this.numberClass + "]");
+					throw new IllegalArgumentException("Cannot convert [" + text + "] to [" +
+																						 this.numberClass.getName() + "]");
 				}
 			}
 			catch (ParseException ex) {
 				throw new IllegalArgumentException("Cannot parse number: " + ex.getMessage());
 			}
 		}
+
+		// use default valueOf methods for parsing text
+		else {
+			if (this.numberClass.equals(Short.class)) {
+				setValue(Short.valueOf(text));
+			}
+			else if (this.numberClass.equals(Integer.class)) {
+				setValue(Integer.valueOf(text));
+			}
+			else if (this.numberClass.equals(Long.class)) {
+				setValue(Long.valueOf(text));
+			}
+			else if (this.numberClass.equals(BigInteger.class)) {
+				setValue(new BigInteger(text));
+			}
+			else if (this.numberClass.equals(Float.class)) {
+				setValue(Float.valueOf(text));
+			}
+			else if (this.numberClass.equals(Double.class)) {
+				setValue(Double.valueOf(text));
+			}
+			else if (this.numberClass.equals(BigDecimal.class)) {
+				setValue(new BigDecimal(text));
+			}
+			else {
+				throw new IllegalArgumentException("Cannot convert [" + text + "] to [" +
+																					 this.numberClass.getName() + "]");
+			}
+		}
 	}
 
 	public String getAsText() {
-		if (getValue() != null) {
-			return this.numberFormat.format(getValue());
+		Object value = getValue();
+		if (value != null) {
+			if (this.numberFormat != null) {
+				// use NumberFormat for rendering value
+				return this.numberFormat.format(value);
+			}
+			else {
+				// use toString method for rendering value
+				return value.toString();
+			}
 		}
 		else {
 			return (this.allowEmpty ? "" : "null");
