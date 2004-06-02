7e346d626df22b95e1c48d7fc88229f9f02ca48a
Juergen Hoeller <jhoeller@interface21.com>
2004-06-02 17:12:30 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2660 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/JdkVersion.java
+++ src/org/springframework/core/JdkVersion.java
@@ -17,11 +17,11 @@
 package org.springframework.core;
 
 /**
- * Class used to find the current JVM version.
+ * Class used to find the current Java/JDK version.
  * Usually we want to find if we're in a 1.4 or higher JVM.
  * (Spring does not support 1.2 JVMs.)
  * @author Rod Johnson
- * @version $Id: JdkVersion.java,v 1.2 2004-03-18 02:46:06 trisberg Exp $
+ * @version $Id: JdkVersion.java,v 1.3 2004-06-02 17:12:30 jhoeller Exp $
  */
 public class JdkVersion {
 	
@@ -47,9 +47,11 @@ public class JdkVersion {
 	
 	/**
 	 * Get the major version code. This means we can do things like
-	 * if getJavaVersion() >= JAVA_14
-	 * @return a code comparable to the JAVA_XX codes in this
-	 * class. 
+	 * <code>if getMajorJavaVersion() < JAVA_14</code>.
+	 * @return a code comparable to the JAVA_XX codes in this class
+	 * @see #JAVA_13
+	 * @see #JAVA_14
+	 * @see #JAVA_15
 	 */
 	public static int getMajorJavaVersion() {
 		return majorJavaVersion;
