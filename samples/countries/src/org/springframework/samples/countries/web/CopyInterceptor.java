39645cc708511222e473afc582ace3de45a2de1a
Juergen Hoeller <jhoeller@interface21.com>
2004-06-03 00:10:50 +0000

    reworked CopyInterceptor to work with CountriesController reference
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2671 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/countries/src/org/springframework/samples/countries/web/CopyInterceptor.java
+++ samples/countries/src/org/springframework/samples/countries/web/CopyInterceptor.java
@@ -6,12 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationContextException;
 import org.springframework.samples.countries.dao.IDaoCountry;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -21,41 +15,23 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * of the countries to a database.
  * @author Jean-Pierre Pawlak
  */
-public class CopyInterceptor extends HandlerInterceptorAdapter implements ApplicationContextAware {
+public class CopyInterceptor extends HandlerInterceptorAdapter {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private Boolean copyAvailable;
+	private boolean copyAvailable = false;
 
 	/**
 	 * Set <code>copyAvailable</code> to True if the <code>countriesController</code>
 	 * has a <code>secondDao</code> declared and if this one is of a DATABASE type.
 	 * Otherwise <code>copyAvailable</code> is set to False.
 	 */
-	public void setApplicationContext(ApplicationContext ctx)
-		throws ApplicationContextException {
-		Object o = ctx.getBean("countriesController");
-		if (null == o) {
-			copyAvailable = Boolean.FALSE;
+	public void setCountriesController(CountriesController countriesController) {
+		IDaoCountry dao = countriesController.getSecondDaoCountry();
+		if (dao != null) {
+			this.copyAvailable = IDaoCountry.DATABASE.equals(dao.getType());
 		}
-		else {
-			BeanWrapper bw = new BeanWrapperImpl(o);
-			Object dao;
-			try {
-				dao = bw.getPropertyValue("secondDaoCountry");
-				bw = new BeanWrapperImpl(dao);
-				if (IDaoCountry.DATABASE.equals(bw.getPropertyValue("type"))) {
-					copyAvailable = Boolean.TRUE;
-				}
-				else {
-					copyAvailable = Boolean.FALSE;
-				}
-			}
-			catch (BeansException e) {
-				copyAvailable = Boolean.FALSE;
-			}
-		}
-		if (copyAvailable.booleanValue()) {
+		if (this.copyAvailable) {
 			logger.info("The countriesController has a valid secondDao. Copy to the database is available.");
 		}
 		else {
@@ -64,11 +40,11 @@ public class CopyInterceptor extends HandlerInterceptorAdapter implements Applic
 	}
 
 	/**
-	 * Makes the <code>copyAvailable</code> value available to views.
+	 * Make the <code>copyAvailable</code> value available to views.
 	 */
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 												 ModelAndView modelAndView) {
-		modelAndView.addObject("copyAvailable",copyAvailable);
+		modelAndView.addObject("copyAvailable", new Boolean(this.copyAvailable));
 	}
 
 }
