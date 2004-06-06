6db0bf0b026c1cf34735fd684a94b8d88794b8a3
Mark Pollack <mpollack@interface21.com>
2004-06-06 19:04:20 +0000

    start 1.1 support
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2689 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ sandbox/src/org/springframework/jms/JmsSender11.java
@@ -0,0 +1,80 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+package org.springframework.jms;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.NamingException;
+
+/**
+ * @author <a href="mailto:mark.pollack@codestreet.com">Mark Pollack</a>
+ */
+public class JmsSender11 extends AbstractJmsSender
+{
+
+    public void send(String destinationName, MessageCreator messageCreator)
+        throws JmsException
+    {
+        Connection connection = null;
+        try
+        {
+            connection = getConnectionFactory().createConnection();
+            Session session =
+                connection.createSession(
+                    isSessionTransacted(),
+                    getSessionAcknowledgeMode());
+            Destination dest =
+                (Destination) super.lookupJndiResource(destinationName);
+            if (logger.isInfoEnabled())
+            {
+                logger.info(
+                    "Looked up destination with name ["
+                        + destinationName
+                        + "]");
+            }
+            MessageProducer producer = session.createProducer(dest);
+
+            Message message = messageCreator.createMessage(session);
+            if (logger.isInfoEnabled())
+            {
+                logger.info("Message created was [" + message + "]");
+            }
+            producer.send(dest, message);
+
+        } catch (NamingException e)
+        {
+            throw new JmsException(
+                "Couldn't get destination name ["
+                    + destinationName
+                    + "] from JNDI",
+                e);
+
+        } catch (JMSException e)
+        {
+            throw new JmsException(
+                "Couln't create JMS Connection or Session",
+                e);
+        } finally
+        {
+            if (connection != null)
+            {
+                try
+                {
+                    //session.close();
+                    connection.close();
+                } catch (JMSException e)
+                {
+                    logger.warn("Failed to close the connection", e);
+                }
+            }
+        }
+
+    }
+
+}
