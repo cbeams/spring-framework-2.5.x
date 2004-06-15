9be8fb86ac20c54cccc648e6ea96aa757ff51c24
Juergen Hoeller <jhoeller@interface21.com>
2004-06-15 16:56:38 +0000

    added char versions of the constants
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2796 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/PropertyAccessor.java
+++ src/org/springframework/beans/PropertyAccessor.java
@@ -32,18 +32,21 @@ public interface PropertyAccessor {
 	 * Follows normal Java conventions: getFoo().getBar() would be "foo.bar".
 	 */
 	String NESTED_PROPERTY_SEPARATOR = ".";
+	char NESTED_PROPERTY_SEPARATOR_CHAR = '.';
 
 	/**
 	 * Marker that indicates the start of a property key for an
 	 * indexed or mapped property like "person.addresses[0]".
 	 */
 	String PROPERTY_KEY_PREFIX = "[";
+	char PROPERTY_KEY_PREFIX_CHAR = '[';
 
 	/**
 	 * Marker that indicates the end of a property key for an
 	 * indexed or mapped property like "person.addresses[0]".
 	 */
 	String PROPERTY_KEY_SUFFIX = "]";
+	char PROPERTY_KEY_SUFFIX_CHAR = ']';
 
 
 	/**
