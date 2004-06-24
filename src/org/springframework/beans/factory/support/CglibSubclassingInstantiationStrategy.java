a573f7e3f1a2eeb7104a7186bd9091548591fc6c
Juergen Hoeller <jhoeller@interface21.com>
2004-06-24 08:43:53 +0000

    renamed DefaultInstantationStrategy to SimpleInstantiationStrategy
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@2900 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
+++ src/org/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -1,6 +1,17 @@
 /*
- * The Spring Framework is published under the terms
- * of the Apache Software License.
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.springframework.beans.factory.support;
@@ -14,21 +25,25 @@ import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import net.sf.cglib.proxy.NoOp;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.beans.factory.BeanFactory;
 
 /**
- * Default object instantiation strategy for use in BeanFactories. Uses CGLIB to generate subclasses
- * dynamically if methods need to be overridden by the container, to implement Method Injection.
- * Using Method Injection features requires CGLIB on the classpath. However, the core IoC container
- * will still run without CGLIB being available.
- * 
+ * Default object instantiation strategy for use in BeanFactories.
+ * Uses CGLIB to generate subclasses dynamically if methods need to be
+ * overridden by the container, to implement Method Injection.
+ *
+ * <p>Using Method Injection features requires CGLIB on the classpath.
+ * However, the core IoC container will still run without CGLIB being available.
+ *
  * @author Rod Johnson
- * @version $Id: CglibSubclassingInstantiationStrategy.java,v 1.2 2004-06-24 07:17:40 johnsonr Exp $
+ * @version $Id: CglibSubclassingInstantiationStrategy.java,v 1.3 2004-06-24 08:43:53 jhoeller Exp $
  */
-class CglibSubclassingInstantiationStrategy extends DefaultInstantiationStrategy {
+public class CglibSubclassingInstantiationStrategy extends SimpleInstantiationStrategy {
 
-	/** 
+	/**
 	 * Index in the CGLIB callback array for passthrough behaviour,
 	 * in which case the subclass won't override the original class.
 	 */
@@ -40,82 +55,83 @@ class CglibSubclassingInstantiationStrategy extends DefaultInstantiationStrategy
 	 */
 	private static final int LOOKUP_OVERRIDE = 1;
 
-	protected Object instantiateWithMethodInjection(RootBeanDefinition rbd, BeanFactory owner) {
-		// Must generate CGLIB subclass
-		return new CglibSubclassCreator(rbd, owner).instantiate();
+
+	protected Object instantiateWithMethodInjection(RootBeanDefinition beanDefinition, BeanFactory owner) {
+		// must generate CGLIB subclass
+		return new CglibSubclassCreator(beanDefinition, owner).instantiate();
 	}
 
-	protected Object instantiateWithMethodInjection(RootBeanDefinition rbd, BeanFactory owner, Constructor ctor,
-			Object[] args) {
-		return new CglibSubclassCreator(rbd, owner).instantiate(ctor, args);
+	protected Object instantiateWithMethodInjection(RootBeanDefinition beanDefinition, BeanFactory owner,
+																									Constructor ctor, Object[] args) {
+		return new CglibSubclassCreator(beanDefinition, owner).instantiate(ctor, args);
 	}
+
 
 	/**
-	 * An inner class so we don't have a CGLIB dependency in core
+	 * An inner class so we don't have a CGLIB dependency in core.
 	 */
-	private class CglibSubclassCreator {
+	private static class CglibSubclassCreator {
 
-		private RootBeanDefinition rbd;
+		private static final Log logger = LogFactory.getLog(CglibSubclassCreator.class);
+
+		private RootBeanDefinition beanDefinition;
+
 		private BeanFactory owner;
 
-		public CglibSubclassCreator(RootBeanDefinition rbd, BeanFactory owner) {
-			this.rbd = rbd;
+		public CglibSubclassCreator(RootBeanDefinition beanDefinition, BeanFactory owner) {
+			this.beanDefinition = beanDefinition;
 			this.owner = owner;
 		}
 
 		public Object instantiate() {
-			Enhancer e = new Enhancer();
-			e.setSuperclass(rbd.getBeanClass());
-			e.setCallbackFilter(new CallbackFilterImpl());
-			e.setCallbacks(new Callback[]{
-					NoOp.INSTANCE, 
+			Enhancer enhancer = new Enhancer();
+			enhancer.setSuperclass(this.beanDefinition.getBeanClass());
+			enhancer.setCallbackFilter(new CallbackFilterImpl());
+			enhancer.setCallbacks(new Callback[] {
+					NoOp.INSTANCE,
 					new LookupOverrideMethodInterceptor()
 			});
 
-			return e.create();
+			return enhancer.create();
 		}
 
 		public Object instantiate(Constructor ctor, Object[] args) {
 			throw new UnsupportedOperationException("Method overriding not yet supported with Constructor Injection");
 		}
 
-		
+
 		/**
-		 * CGLIB MethodInterceptor to override methods, replacing them with an implementation
-		 * that returns a bean looked up in the container. 
+		 * CGLIB MethodInterceptor to override methods, replacing them with an
+		 * implementation that returns a bean looked up in the container.
 		 */
 		private class LookupOverrideMethodInterceptor implements MethodInterceptor {
 
-			/**
-			 * @see net.sf.cglib.proxy.MethodInterceptor#intercept(java.lang.Object,
-			 *      java.lang.reflect.Method, java.lang.Object[], net.sf.cglib.proxy.MethodProxy)
-			 */
 			public Object intercept(Object o, Method m, Object[] args, MethodProxy mp) throws Throwable {
-				// Cast is safe as CallbackFilter filters are used selectively
-				LookupOverride lo = (LookupOverride) rbd.getMethodOverrides().getOverride(m);
+				// cast is safe as CallbackFilter filters are used selectively
+				LookupOverride lo = (LookupOverride) beanDefinition.getMethodOverrides().getOverride(m);
 				return owner.getBean(lo.getBeanName());
 			}
 		}
 
+
 		/**
-		 * CGLIB object to filter method interception behaviour.
+		 * CGLIB object to filter method interception behavior.
 		 */
 		private class CallbackFilterImpl implements CallbackFilter {
 
-			/**
-			 * @see net.sf.cglib.proxy.CallbackFilter#accept(java.lang.reflect.Method)
-			 */
-			public int accept(Method m) {
-				MethodOverride methodOverride = rbd.getMethodOverrides().getOverride(m);
-				log.info("Override for " + m.getName() + " is " + methodOverride);
+			public int accept(Method method) {
+				MethodOverride methodOverride = beanDefinition.getMethodOverrides().getOverride(method);
+				if (logger.isInfoEnabled()) {
+					logger.info("Override for '" + method.getName() + "' is [" + methodOverride + "]");
+				}
 				if (methodOverride == null) {
 					return PASSTHROUGH;
 				}
 				else if (methodOverride instanceof LookupOverride) {
 					return LOOKUP_OVERRIDE;
 				}
-				
-				throw new UnsupportedOperationException("Unexpected MethodOverride subclass: " + methodOverride.getClass());
+				throw new UnsupportedOperationException("Unexpected MethodOverride subclass: " +
+																								methodOverride.getClass().getName());
 			}
 		}
 	}
---
+++ src/org/springframework/beans/factory/support/SimpleInstantiationStrategy.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory.support;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.util.StringUtils;
+
+/**
+ * Simple object instantiation strategy for use in BeanFactories.
+ *
+ * <p>Does not support Method Injection, although it provides hooks for subclasses
+ * to override to add Method Injection  support, for example by overriding methods.
+ * 
+ * @author Rod Johnson
+ * @version $Id: SimpleInstantiationStrategy.java,v 1.1 2004-06-24 08:43:53 jhoeller Exp $
+ */
+public class SimpleInstantiationStrategy implements InstantiationStrategy {
+
+	public final Object instantiate(RootBeanDefinition beanDefinition, BeanFactory owner) {
+		// don't override the class with CGLIB if no overrides
+		if (beanDefinition.getMethodOverrides().isEmpty()) {
+			return BeanUtils.instantiateClass(beanDefinition.getBeanClass());
+		}
+		else {
+			// must generate CGLIB subclass
+			return instantiateWithMethodInjection(beanDefinition, owner);
+		}
+	}
+	
+	/**
+	 * Subclasses can override this method, which is implemented to throw
+	 * UnsupportedOperationException, if they can instantiate an object with
+	 * the Method Injection specified in the given RootBeanDefinition.
+	 * Instantiation should use a no-arg constructor.
+	 */
+	protected Object instantiateWithMethodInjection(RootBeanDefinition beanDefinition, BeanFactory owner) {
+		throw new UnsupportedOperationException("Method Injection not supported in SimpleInstantiationStrategy");
+	}
+
+	public final Object instantiate(RootBeanDefinition beanDefinition, BeanFactory owner,
+																	Constructor ctor, Object[] args) {
+		if (beanDefinition.getMethodOverrides().isEmpty()) {
+			return BeanUtils.instantiateClass(ctor, args);
+		}
+		else {
+			return instantiateWithMethodInjection(beanDefinition, owner, ctor, args);
+		}
+	}
+	
+	/**
+	 * Subclasses can override this method, which is implemented to throw
+	 * UnsupportedOperationException, if they can instantiate an object with
+	 * the Method Injection specified in the given RootBeanDefinition.
+	 * Instantiation should use the given constructor and parameters.
+	 */
+	protected Object instantiateWithMethodInjection(RootBeanDefinition beanDefinition, BeanFactory owner,
+																									Constructor ctor, Object[] args) {
+		throw new UnsupportedOperationException("Method Injection not supported in SimpleInstantiationStrategy");
+	}
+
+	public final Object instantiate(RootBeanDefinition beanDefinition, BeanFactory owner,
+																	Method factoryMethod, Object[] args) {
+		try {
+			// must be a static method
+			return factoryMethod.invoke(null, args);
+		}
+		catch (IllegalArgumentException ex) {
+			throw new BeanDefinitionStoreException("Illegal arguments to factory method " + factoryMethod + "; " +
+																						 "args=" + StringUtils.arrayToCommaDelimitedString(args));
+		}
+		catch (IllegalAccessException ex) {
+			throw new BeanDefinitionStoreException("Cannot access factory method " + factoryMethod + "; is it public?");
+		}
+		catch (InvocationTargetException ex) {
+			throw new BeanDefinitionStoreException("Factory method " + factoryMethod + " threw exception",
+																						 ex.getTargetException());
+		}
+	}
+	
+}
