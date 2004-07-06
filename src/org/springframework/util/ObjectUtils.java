b64a0f338686dbf5127b83b49341f04ec95c3ea8
Keith Donald <kdonald@interface21.com>
2004-07-06 21:17:44 +0000

    simple getBoolean method that converts a boolean primitive to Boolean.TRUE or Boolean.FALSE
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3060 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/ObjectUtils.java
+++ src/org/springframework/util/ObjectUtils.java
@@ -17,55 +17,75 @@
 package org.springframework.util;
 
 /**
- * Miscellaneous object utility methods. Mainly for internal use
- * within the framework; consider Jakarta's Commons Lang for a more
- * comprehensive suite of object utilities.
+ * Miscellaneous object utility methods. Mainly for internal use within the
+ * framework; consider Jakarta's Commons Lang for a more comprehensive suite of
+ * object utilities.
+ * 
  * @author Juergen Hoeller
  * @author Keith Donald
  * @author Rod Johnson
  * @since 19.03.2004
  * @see org.apache.commons.lang.ObjectUtils
- * @version $Id: ObjectUtils.java,v 1.4 2004-06-30 15:35:54 johnsonr Exp $
+ * @version $Id: ObjectUtils.java,v 1.5 2004-07-06 21:17:44 kdonald Exp $
  */
 public abstract class ObjectUtils {
 
-	/**
-	 * Determine if the given objects are equal, returning true if both
-	 * are null respectively false if only one is null.
-	 * @param o1 first Object to compare
-	 * @param o2 second Object to compare
-	 * @return whether the given objects are equal
-	 */
-	public static boolean nullSafeEquals(Object o1, Object o2) {
-		return (o1 == o2 || (o1 != null && o1.equals(o2)));
-	}
+    /**
+     * Determine if the given objects are equal, returning true if both are null
+     * respectively false if only one is null.
+     * 
+     * @param o1
+     *            first Object to compare
+     * @param o2
+     *            second Object to compare
+     * @return whether the given objects are equal
+     */
+    public static boolean nullSafeEquals(Object o1, Object o2) {
+        return (o1 == o2 || (o1 != null && o1.equals(o2)));
+    }
 
-	/**
-	 * Return a hex string form of an object's identity hash code.
-	 * @param o the object
-	 * @return the object's identity code in hex
-	 */
-	public static String getIdentityHexString(Object o) {
-		return Integer.toHexString(System.identityHashCode(o));
-	}
-	
-	public static boolean isCheckedException(Throwable t) {
-		return (t instanceof Exception) && (!(t instanceof RuntimeException));
-	}
-	
-	public static boolean isCompatibleWithThrowsClause(Throwable t, Class[] declaredExceptions) {
-		if (t instanceof RuntimeException) {
-			return true;
-		}
-		if (declaredExceptions != null) {
-			for (int i = 0; i < declaredExceptions.length; i++) {
-				if (declaredExceptions[i].isAssignableFrom(t.getClass())) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
+    /**
+     * Return a hex string form of an object's identity hash code.
+     * 
+     * @param o
+     *            the object
+     * @return the object's identity code in hex
+     */
+    public static String getIdentityHexString(Object o) {
+        return Integer.toHexString(System.identityHashCode(o));
+    }
+
+    /**
+     * Return the canoncialized Boolean object for the provided boolean
+     * primitive.  Useful when you must work with objects and wish to reuse 
+     * the static boolean constants.
+     * 
+     * @param b
+     *            the boolean
+     * @return Boolean.TRUE or Boolean.FALSE
+     */
+    public static Boolean getBoolean(boolean b) {
+        if (b) {
+            return Boolean.TRUE;
+        }
+        else {
+            return Boolean.FALSE;
+        }
+    }
+
+    public static boolean isCheckedException(Throwable t) {
+        return (t instanceof Exception) && (!(t instanceof RuntimeException));
+    }
+
+    public static boolean isCompatibleWithThrowsClause(Throwable t,
+            Class[] declaredExceptions) {
+        if (t instanceof RuntimeException) { return true; }
+        if (declaredExceptions != null) {
+            for (int i = 0; i < declaredExceptions.length; i++) {
+                if (declaredExceptions[i].isAssignableFrom(t.getClass())) { return true; }
+            }
+        }
+        return false;
+    }
 
 }
