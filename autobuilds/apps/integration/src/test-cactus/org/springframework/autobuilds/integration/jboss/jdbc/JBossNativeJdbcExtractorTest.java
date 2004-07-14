a958bc512b037b6398a54b53d9addb598c87b29d
Colin Sampaleanu <csampaleanu@interface21.com>
2004-07-14 21:41:45 +0000

    add comment
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3143 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- autobuilds/apps/integration/src/test-cactus/org/springframework/autobuilds/integration/jboss/jdbc/JBossNativeJdbcExtractorTest.java
+++ autobuilds/apps/integration/src/test-cactus/org/springframework/autobuilds/integration/jboss/jdbc/JBossNativeJdbcExtractorTest.java
@@ -1,8 +1,17 @@
 /*
- * Created on Jun 21, 2004
+ * Copyright 2002-2004 the original author or authors.
  *
- * @todo To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.springframework.autobuilds.integration.jboss.jdbc;
@@ -29,6 +38,8 @@ import org.springframework.jdbc.support.nativejdbc.JBossNativeJdbcExtractor;
 
 /**
  * Integration test for JBossNativeJdbcExtractor
+ * 
+ * @author Colin Sampaleanu
  */
 public class JBossNativeJdbcExtractorTest extends TestCase {
 
