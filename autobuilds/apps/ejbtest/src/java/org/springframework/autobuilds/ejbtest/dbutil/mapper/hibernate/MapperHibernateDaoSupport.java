beb4f622f7d2a0d098fbda23bf7d28a4eaf20299
Colin Sampaleanu <csampaleanu@interface21.com>
2004-07-15 13:01:00 +0000

    add base Mapper class
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3157 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ autobuilds/apps/ejbtest/src/java/org/springframework/autobuilds/ejbtest/dbutil/mapper/hibernate/MapperHibernateDaoSupport.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.autobuilds.ejbtest.dbutil.mapper.hibernate;
+
+import org.springframework.orm.hibernate.support.HibernateDaoSupport;
+
+/**
+ * Subclass of Spring's HibernateDaoSupport which defaults the defaultAllowCreate flag to 
+ * false. It also has a constructor variant which allows an instance to be created based
+ * on a passed in Mapper instance. InitializingBean's afterPropertiesSet must still be
+ * called when using this constructor! 
+ * 
+ * @version $Revision: 1.1 $
+ * @author colin sampaleanu
+ */
+
+public abstract class MapperHibernateDaoSupport extends HibernateDaoSupport {
+
+  // constructor for bean style creation
+  public MapperHibernateDaoSupport() {
+    defaultTemplateAllowCreateValue = false;  
+  }
+
+  // construction off an existing mapper instance
+  protected MapperHibernateDaoSupport(MapperImpl mapper) {
+    setSessionFactory(mapper.getSessionFactory());
+  }
+
+}
