5810fc8d59eee58c4dad3540bc31590ceaa762cb
Juergen Hoeller <jhoeller@interface21.com>
2004-07-28 12:55:56 +0000

    added client.sh
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3352 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ samples/jpetstore/client/client.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SET CLIENT_HOME directory
+
+CLIENT_HOME=`dirname $0`
+
+CPATH=$CLIENT_HOME/../../../lib/aopalliance/aopalliance.jar
+CPATH=$CLIENT_HOME/../../../lib/axis/axis.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/axis/saaj.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/axis/wsdl4j.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/caucho/hessian-2.1.12.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/caucho/burlap-2.1.12.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/j2ee/jaxrpc.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/jakarta-commons/commons-collections.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/jakarta-commons/commons-discovery.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/jakarta-commons/commons-logging.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../dist/spring.jar:$CPATH
+CPATH=$CLIENT_HOME/jpetstore.jar:$CPATH
+
+java -cp $CPATH org.springframework.samples.jpetstore.service.client.OrderServiceClient $1 $2
