4d67e31db098dd6e2179cef9efca65b0b51d44cb
Keith Donald <kdonald@interface21.com>
2004-08-26 05:14:19 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3629 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/test/org/springframework/rules/values/FormModelTests.java
+++ sandbox/test/org/springframework/rules/values/FormModelTests.java
@@ -1,6 +1,6 @@
 /*
  * $Header: /usr/local/cvs/module/src/java/File.java,v 1.7 2004/01/16 22:23:11
- * keith Exp $ $Revision: 1.1 $ $Date: 2004-08-26 03:29:35 $
+ * keith Exp $ $Revision: 1.2 $ $Date: 2004-08-26 05:14:19 $
  * 
  * Copyright Computer Science Innovations (CSI), 2004. All rights reserved.
  */
@@ -9,8 +9,88 @@ package org.springframework.rules.values;
 import junit.framework.TestCase;
 
 public class FormModelTests extends TestCase {
-    public void testGetPropertyTokens() {
-        DefaultFormModel form = new DefaultFormModel();
-        String propertyPath = "my.test.property.path[0]";
+    public void testNestedProperties() {
+        Company c = new Company();
+        Contact contact = new Contact();
+        contact.setAddress(new Address());
+        c.setPrimaryContact(contact);
+
+        ValidatingFormModel model = new ValidatingFormModel(c);
+        model.setBufferChangesDefault(false);
+        ValueModel city = model.add("primaryContact.address.city");
+        city.addValueListener(new ValueListener() {
+            public void valueChanged() {
+                System.out.println("city changed");
+            }
+        });
+        ValueModel addr = model.add("primaryContact.address");
+        addr.set(new Address());
+    }
+
+    public static class Company {
+        private String name;
+
+        private Contact primaryContact;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Contact getPrimaryContact() {
+            return primaryContact;
+        }
+
+        public void setPrimaryContact(Contact primaryContact) {
+            this.primaryContact = primaryContact;
+        }
+    }
+
+    public static class Contact {
+        private String name;
+
+        private Address address;
+
+        public Address getAddress() {
+            return address;
+        }
+
+        public void setAddress(Address address) {
+            System.out.println("Address set");
+            this.address = address;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class Address {
+        private String city;
+
+        private String state;
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
     }
 }
