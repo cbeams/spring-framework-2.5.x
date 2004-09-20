953d408404309ea040d81ec4433a9e79a7f9f6de
Rod Johnson <rjohnson@interface21.com>
2004-09-20 16:57:25 +0000

    Added FactoryBean created object advice test--now commented out
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3832 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/framework/autoproxy/BeanNameAutoProxyCreatorTests.java
+++ test/org/springframework/aop/framework/autoproxy/BeanNameAutoProxyCreatorTests.java
@@ -103,6 +103,46 @@ public class BeanNameAutoProxyCreatorTests extends TestCase {
 		}
 	}
 	
+	/*
+	public void testJdkIntroductionOnFactoryBean() {
+		ITestBean tb = (ITestBean) bf.getBean("factory-introductionUsingJdk");
+		NopInterceptor nop = (NopInterceptor) bf.getBean("introductionNopInterceptor");
+		assertEquals(0, nop.getCount());
+		assertTrue(AopUtils.isJdkDynamicProxy(tb));
+		int age = 5;
+		tb.setAge(age);
+		assertEquals(age, tb.getAge());
+		assertTrue("Introduction was made", tb instanceof TimeStamped);
+		assertEquals(0, ((TimeStamped) tb).getTimeStamp());
+		assertEquals(3, nop.getCount());		
+		assertEquals("introductionUsingJdk", tb.getName());
+	
+		ITestBean tb2 = (ITestBean) bf.getBean("second-introductionUsingJdk");
+			
+		// Check two per-instance mixins were distinct
+		Lockable lockable1 = (Lockable) tb;
+		Lockable lockable2 = (Lockable) tb2;
+		assertFalse(lockable1.locked());
+		assertFalse(lockable2.locked());
+		tb.setAge(65);
+		assertEquals(65, tb.getAge());
+		lockable1.lock();
+		assertTrue(lockable1.locked());
+		// Shouldn't affect second
+		assertFalse(lockable2.locked());
+		// Can still mod second object
+		tb2.setAge(12);
+		// But can't mod first
+		try {
+			tb.setAge(6);
+			fail("Mixin should have locked this object");
+		}
+		catch (LockedException ex) {
+			// Ok
+		}
+	}
+	*/
+	
 	public void testJdkProxyWithWildcardMatch() {
 		ITestBean tb = (ITestBean) bf.getBean("jdk1");
 		jdkAssertions(tb);
