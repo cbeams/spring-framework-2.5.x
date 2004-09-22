ee42387bcc315b778321fe687ea748ad7e604454
Rod Johnson <rjohnson@interface21.com>
2004-09-22 13:08:46 +0000

    Introduced IntroductionAdvice superinterface: other advices than interceptors can implement introductions
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3835 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/IntroductionAdvice.java
+++ src/org/springframework/aop/IntroductionAdvice.java
@@ -16,18 +16,23 @@
 
 package org.springframework.aop;
 
-import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.aop.Advice;
 
-/**
- * Subinterface of AOP Alliance MethodInterceptor that allows additional interfaces
- * to be implemented by the interceptor, and available via a proxy using that
+/** 
+ * Subinterface of AOP Alliance Advice that allows additional interfaces
+ * to be implemented by an Advice, and available via a proxy using that
  * interceptor. This is a fundamental AOP concept called <b>introduction</b>.
  *
  * <p>Introductions are often <b>mixins</b>, enabling the building of composite
  * objects that can achieve many of the goals of multiple inheritance in Java.
- *
  * @author Rod Johnson
  */
-public interface IntroductionInterceptor extends MethodInterceptor, IntroductionAdvice {
+public interface IntroductionAdvice extends Advice {
+	
+	/**
+	 * Does this IntroductionInterceptor implement the given interface?
+	 * @param intf interface to check
+	 */
+	boolean implementsInterface(Class intf);
 
 }
