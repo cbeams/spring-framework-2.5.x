91bab187c84f39f8ca4f8909d8960e36578c876c
Juergen Hoeller <jhoeller@interface21.com>
2004-09-30 21:09:37 +0000

    added debug logging for remote invocations
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@3950 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/support/RemoteInvocationBasedExporter.java
+++ src/org/springframework/remoting/support/RemoteInvocationBasedExporter.java
@@ -63,7 +63,30 @@ public abstract class RemoteInvocationBasedExporter extends RemoteExporter {
 	 */
 	protected Object invoke(RemoteInvocation invocation, Object targetObject)
 			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException{
-		return getRemoteInvocationExecutor().invoke(invocation, targetObject);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Applying " + invocation);
+		}
+		try {
+			return getRemoteInvocationExecutor().invoke(invocation, targetObject);
+		}
+		catch (NoSuchMethodException ex) {
+			if (logger.isDebugEnabled()) {
+				logger.warn("Could not find target method for " + invocation, ex);
+			}
+			throw ex;
+		}
+		catch (IllegalAccessException ex) {
+			if (logger.isDebugEnabled()) {
+				logger.warn("Could not access target method for " + invocation, ex);
+			}
+			throw ex;
+		}
+		catch (InvocationTargetException ex) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Target method failed for " + invocation, ex.getTargetException());
+			}
+			throw ex;
+		}
 	}
 
 	/**
