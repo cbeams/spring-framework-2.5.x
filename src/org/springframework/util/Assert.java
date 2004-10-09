22efc401ca1d793ddd358643a7d7e6c9b17d8cc3
Juergen Hoeller <jhoeller@interface21.com>
2004-10-09 21:09:49 +0000

    improved javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4066 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/Assert.java
+++ src/org/springframework/util/Assert.java
@@ -19,23 +19,28 @@ import java.util.Collection;
 import java.util.Map;
 
 /**
- * Assert utility class that assists in validating arguments and internal invariants.
+ * Assert utility class that assists in validating arguments.
  * Useful for identifying programmer errors early and obviously at runtime.
  *
- * <p>For example, if the contract of a public setter method states it does not
- * allow null arguments, Assert can be used to validate that contract. Doing
- * this clearly indicates a contract violation when it occurs and protects the
+ * <p>For example, if the contract of a public method states it does not allow
+ * null arguments, Assert can be used to validate that contract. Doing this
+ * clearly indicates a contract violation when it occurs and protects the
  * class's invariants.
  *
- * <p>This class is similiar to JUnit's assertion library. If an argument value
+ * <p>Typically used to validate method arguments rather than configuration
+ * properties, to check for cases that are usually programmer errors rather
+ * than configuration errors. In contrast to config initialization code,
+ * there is usally no point in falling back to defaults in such methods.
+ *
+ * <p>This class is similar to JUnit's assertion library. If an argument value
  * is deemed invalid, an IllegalArgumentException is thrown. For example:
  *
  * <pre>
  * Assert.notNull(clazz, "The class must not be null");
  * Assert.isTrue(i > 0, "The value must be greater than zero");</pre>
  *
- * <p>Mainly for internal use within the framework; consider Jakarta's Commons Lang
- * for a more comprehensive suite of assertion utilities.
+ * Mainly for internal use within the framework; consider Jakarta's Commons
+ * Lang >= 2.0 for a more comprehensive suite of assertion utilities.
  *
  * @author Keith Donald
  * @author Juergen Hoeller
