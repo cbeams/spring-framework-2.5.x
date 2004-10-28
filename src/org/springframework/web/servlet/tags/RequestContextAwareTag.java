38b88c3e4956bebd72576b22cc2445186d5bdcbb
Juergen Hoeller <jhoeller@interface21.com>
2004-10-28 15:18:49 +0000

    added HttpServletRequest check
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4206 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/tags/RequestContextAwareTag.java
+++ src/org/springframework/web/servlet/tags/RequestContextAwareTag.java
@@ -64,6 +64,9 @@ public abstract class RequestContextAwareTag extends TagSupport implements TryCa
 		this.requestContext = (RequestContext) pageContext.getAttribute(REQUEST_CONTEXT_PAGE_ATTRIBUTE);
 		try {
 			if (this.requestContext == null) {
+				if (!(this.pageContext.getRequest() instanceof HttpServletRequest)) {
+					throw new JspException("RequestContextAwareTag just supports HTTP requests");
+				}
 				this.requestContext =	new RequestContext((HttpServletRequest) this.pageContext.getRequest());
 				pageContext.setAttribute(REQUEST_CONTEXT_PAGE_ATTRIBUTE, this.requestContext);
 			}
