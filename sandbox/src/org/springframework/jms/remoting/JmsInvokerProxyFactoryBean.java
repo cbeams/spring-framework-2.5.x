28968857c0e0b69441670217f4da501ed8f14712
Juergen Hoeller <jhoeller@interface21.com>
2004-11-06 14:36:02 +0000

    moved JMS invoker from remoting.jms in the main sources to jms.remoting in the sandbox
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4255 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/jms/remoting/JmsInvokerProxyFactoryBean.java
+++ sandbox/src/org/springframework/jms/remoting/JmsInvokerProxyFactoryBean.java
@@ -1,28 +1,25 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
-package org.springframework.remoting.jms;
+package org.springframework.jms.remoting;
 
-import java.net.MalformedURLException;
+import javax.jms.JMSException;
 
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.remoting.caucho.BurlapClientInterceptor;
-
-import javax.jms.JMSException;
 
 /**
  * Factory bean for JMS proxies. Behaves like the proxied service when
@@ -32,10 +29,10 @@ import javax.jms.JMSException;
  * For details, see JmsClientInterceptor docs.
  *
  * @author James Strachan
- * @see org.springframework.remoting.jms.JmsClientInterceptor
- * @see org.springframework.remoting.jms.JmsServiceExporter
+ * @see org.springframework.jms.remoting.JmsInvokerClientInterceptor
+ * @see org.springframework.jms.remoting.JmsInvokerServiceExporter
  */
-public class JmsProxyFactoryBean extends JmsClientInterceptor implements FactoryBean {
+public class JmsInvokerProxyFactoryBean extends JmsInvokerClientInterceptor implements FactoryBean {
 
 	private Object serviceProxy;
 
