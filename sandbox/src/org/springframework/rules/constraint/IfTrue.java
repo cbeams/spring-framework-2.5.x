f7f26787f30d8f065168ceb0ec3060d90a3130b4
Keith Donald <kdonald@interface21.com>
2004-11-11 07:36:39 +0000

    moved IfTrue to constraint where it should be
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4297 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/rules/constraint/IfTrue.java
+++ sandbox/src/org/springframework/rules/constraint/IfTrue.java
@@ -1,11 +1,19 @@
 /*
- * $Header: /var/local/springframework.cvs.sourceforge.net/spring/sandbox/src/org/springframework/rules/constraints/IfTrue.java,v 1.1 2004-11-03 19:02:09 kdonald Exp $
- * $Revision: 1.1 $
- * $Date: 2004-11-03 19:02:09 $
+ * Copyright 2002-2004 the original author or authors.
  * 
- * Copyright Computer Science Innovations (CSI), 2004. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.springframework.rules.constraints;
+package org.springframework.rules.constraint;
 
 import org.springframework.util.Assert;
 import org.springframework.util.closure.Constraint;
