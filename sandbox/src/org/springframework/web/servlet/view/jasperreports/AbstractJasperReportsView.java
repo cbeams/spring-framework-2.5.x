673559d58da5d56a924b838ee638de9c4c391fd6
Rob Harrop <rharrop@interface21.com>
2004-11-16 14:49:57 +0000

    Updated JavaDocs for JasperReports view support
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4351 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/servlet/view/jasperreports/AbstractJasperReportsView.java
+++ sandbox/src/org/springframework/web/servlet/view/jasperreports/AbstractJasperReportsView.java
@@ -1,5 +1,17 @@
 /*
- * Created on Sep 16, 2004
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.web.servlet.view.jasperreports;
 
@@ -15,119 +27,130 @@ import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperManager;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.design.JRBshCompiler;
 
 import org.springframework.core.io.Resource;
+import org.springframework.ui.jasperreports.JasperReportsUtils;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
 
 /**
- * Base view class for all JasperReports views.
+ * Base view class for all JasperReports views. Controls on the fly compilation
+ * of report designs as required and is responsible for locating the report data source
+ * in the Spring model.
  * 
  * @author robh
  */
 public abstract class AbstractJasperReportsView extends AbstractUrlBasedView {
 
-	/**
-	 * The <code>JasperReport</code> that is used to render the view.
-	 */
-	private JasperReport report;
+    /**
+     * The <code>JasperReport</code> that is used to render the view.
+     */
+    private JasperReport report;
 
-	/**
-	 * A <code>Resource</code> instance that is the source of the
-	 * <code>JasperReport</code>.
-	 */
-	private Resource reportResource;
+    /**
+     * A <code>Resource</code> instance that is the source of the
+     * <code>JasperReport</code>.
+     */
+    private Resource reportResource;
 
-	/**
-	 * Finds the <code>JRDataSource</code> to use for rendering the report and
-	 * then invokes the renderView() method that should be implemented by the
-	 * subclass.
-	 * 
-	 * @param model
-	 *            A <code>Map</code> containing the model data. Should contain
-	 *            an instance of <code>JRDataSource</code>.
-	 * @throws NoDataSourceException
-	 *             Thrown if the <code>model</code> parameter does not contain
-	 *             an instance of <code>JRDataSource</code>
-	 */
-	protected final void renderMergedOutputModel(Map model,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception, NoDataSourceException {
+    /**
+     * Finds the <code>JRDataSource</code> to use for rendering the report and
+     * then invokes the renderView() method that should be implemented by the
+     * subclass.
+     * 
+     * @param model
+     *            A <code>Map</code> containing the model data. Should contain
+     *            an instance of <code>JRDataSource</code>.
+     * @throws NoDataSourceException
+     *             Thrown if the <code>model</code> parameter does not contain
+     *             an instance of <code>JRDataSource</code>
+     */
+    protected final void renderMergedOutputModel(Map model,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception, NoDataSourceException {
 
-		JRDataSource dataSource = locateDataSource(model);
+        JRDataSource dataSource = locateDataSource(model);
 
-		if (dataSource == null) {
-			throw new NoDataSourceException("No Report DataSource Supplied.");
-		}
+        if (dataSource == null) {
+            throw new NoDataSourceException("No Report DataSource Supplied.");
+        }
 
-		response.setContentType(getContentType());
-		renderView(report, model, dataSource, response);
-	}
+        response.setContentType(getContentType());
+        renderView(report, model, dataSource, response);
+    }
 
-	/**
-	 * 
-	 */
-	protected void initApplicationContext()
-			throws UnrecognizedReportExtensionException,
-			JasperReportsInitializationException, ReportFileNotFoundException {
-		super.initApplicationContext();
+    /**
+     * Checks to see that a valid report file URL is supplied in the
+     * configuration. Compiles the report file is necessary.
+     */
+    protected void initApplicationContext()
+            throws UnrecognizedReportExtensionException,
+            JasperReportsInitializationException, ReportFileNotFoundException {
+        super.initApplicationContext();
 
-		String reportPath = getUrl();
-		// we know the url is set
-		// now try to get the report
-		// and then compile it
-		reportResource = getApplicationContext().getResource(getUrl());
+        String reportPath = getUrl();
+        
+        // we know the url is set
+        // now try to get the report
+        // and then compile it
+        reportResource = getApplicationContext().getResource(getUrl());
 
-		try {
-			if (reportPath.endsWith(".jasper")) {
-				report = JasperManager.loadReport(reportResource
-						.getInputStream());
-			} else if (reportPath.endsWith(".jrxml")) {
-				// attempt a compile!
-				JRBshCompiler compiler = new JRBshCompiler();
-				report = compiler.compileReport(JasperManager
-						.loadXmlDesign(reportResource.getInputStream()));
-			} else {
-				throw new UnrecognizedReportExtensionException(
-						"Report URL must end in either .jasper or .jrxml");
-			}
-		} catch (JRException ex) {
-			throw new JasperReportsInitializationException(
-					"An exception occured in the Jasper Reports framework during initialization.",
-					ex);
-		} catch (FileNotFoundException ex) {
-			throw new ReportFileNotFoundException(
-					"Unable to load report at path: " + getUrl());
-		} catch (IOException ex) {
-			throw new JasperReportsInitializationException(
-					"An exception occured whilst reading report data.", ex);
-		}
-	}
+        try {
+            if (reportPath.endsWith(".jasper")) {
+                report = JasperManager.loadReport(reportResource
+                        .getInputStream());
+            } else if (reportPath.endsWith(".jrxml")) {
+                // attempt a compile
+                report = JasperReportsUtils.compileReport(reportResource
+                        .getInputStream());
+            } else {
+                throw new UnrecognizedReportExtensionException(
+                        "Report URL must end in either .jasper or .jrxml");
+            }
+        } catch (JRException ex) {
+            throw new JasperReportsInitializationException(
+                    "An exception occured in the Jasper Reports framework during initialization.",
+                    ex);
+        } catch (FileNotFoundException ex) {
+            throw new ReportFileNotFoundException(
+                    "Unable to load report at path: " + getUrl());
+        } catch (IOException ex) {
+            throw new JasperReportsInitializationException(
+                    "An exception occured whilst reading report data.", ex);
+        }
+    }
 
-	/**
-	 * Attempts to locate an instance of JRDataSource in a given Map instance.
-	 * 
-	 * @param model
-	 *            The Map to look in.
-	 * @return The JRDataSource if found, otherwise null.
-	 */
-	private JRDataSource locateDataSource(Map model) {
-		JRDataSource dataSource = null;
+    /**
+     * Attempts to locate an instance of JRDataSource in a given Map instance.
+     * 
+     * @param model
+     *            The Map to look in.
+     * @return The JRDataSource if found, otherwise null.
+     */
+    private JRDataSource locateDataSource(Map model) {
+        JRDataSource dataSource = null;
 
-		for (Iterator i = model.values().iterator(); i.hasNext();) {
-			Object o = i.next();
+        for (Iterator i = model.values().iterator(); i.hasNext();) {
+            Object o = i.next();
 
-			if (o instanceof JRDataSource) {
-				dataSource = (JRDataSource) o;
-				break;
-			}
-		}
+            if (o instanceof JRDataSource) {
+                dataSource = (JRDataSource) o;
+                break;
+            }
+        }
 
-		return dataSource;
-	}
+        return dataSource;
+    }
 
-	protected abstract void renderView(JasperReport report, Map model,
-			JRDataSource dataSource, HttpServletResponse response)
-			throws Exception;
+    /**
+     * Subclasses should implement this method to perform the actual rendering process.
+     * @param report The <code>JasperReport</code> to render.
+     * @param model The <code>Map</code> containg report parameters.
+     * @param dataSource The <code>JRDataSource</code> containing the report data.
+     * @param response The <code>HttpServletResponse</code> the report should be rendered to.
+     * @throws Exception
+     */
+    protected abstract void renderView(JasperReport report, Map model,
+            JRDataSource dataSource, HttpServletResponse response)
+            throws Exception;
 
 }
--- sandbox/src/org/springframework/web/servlet/view/jasperreports/JasperReportsCsvView.java
+++ sandbox/src/org/springframework/web/servlet/view/jasperreports/JasperReportsCsvView.java
@@ -1,5 +1,17 @@
 /*
- * Created on Sep 17, 2004
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.web.servlet.view.jasperreports;
 
@@ -7,15 +19,24 @@ import net.sf.jasperreports.engine.JRAbstractExporter;
 import net.sf.jasperreports.engine.export.JRCsvExporter;
 
 /**
+ * Implementation of AbstractJasperReportsView that renders
+ * report results in CSV format.
  * @author robh
- *
  */
 public class JasperReportsCsvView extends AbstractWriterDrivenJasperReportsView {
 
+    /**
+     * Sets the content type of this report to <code>text/csv</code>
+     *
+     */
 	public JasperReportsCsvView() {
 		setContentType("text/csv");
 	}
 
+    /**
+     * Returns an instance of <code>JRCsvExporter</code>
+     * @return A <code>JRCsvExporter</code>
+     */
 	protected JRAbstractExporter getExporter() {
 		return new JRCsvExporter();
 	}
