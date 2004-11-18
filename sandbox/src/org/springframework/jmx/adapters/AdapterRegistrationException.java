164b5cf50fe27a38e729bc0caa3337fd12096f38
Rob Harrop <rharrop@interface21.com>
2004-11-18 15:15:51 +0000

    Fixed incorrect superclass for AdapterRegistrationException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4360 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/jmx/adapters/AdapterRegistrationException.java
+++ sandbox/src/org/springframework/jmx/adapters/AdapterRegistrationException.java
@@ -3,44 +3,22 @@
  */
 package org.springframework.jmx.adapters;
 
-import com.ibatis.common.exception.NestedRuntimeException;
+import org.springframework.core.NestedRuntimeException;
+
 
 /**
  * @author robh
  */
 public class AdapterRegistrationException extends NestedRuntimeException {
 
-    /**
-     * 
-     */
-    public AdapterRegistrationException() {
-        super();
-        // TODO Auto-generated constructor stub
+   
+    public AdapterRegistrationException(String message) {
+        super(message);
     }
 
-    /**
-     * @param arg0
-     */
-    public AdapterRegistrationException(String arg0) {
-        super(arg0);
-        // TODO Auto-generated constructor stub
-    }
 
-    /**
-     * @param arg0
-     */
-    public AdapterRegistrationException(Throwable arg0) {
-        super(arg0);
-        // TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @param arg0
-     * @param arg1
-     */
-    public AdapterRegistrationException(String arg0, Throwable arg1) {
-        super(arg0, arg1);
-        // TODO Auto-generated constructor stub
+    public AdapterRegistrationException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
