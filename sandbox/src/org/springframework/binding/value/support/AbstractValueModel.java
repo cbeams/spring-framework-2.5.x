2f2be7a4e8229c0477e102328e3983a19f57cfde
Keith Donald <kdonald@interface21.com>
2004-11-23 13:53:34 +0000

    fixed concurrent modification bug
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4399 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/binding/value/support/AbstractValueModel.java
+++ sandbox/src/org/springframework/binding/value/support/AbstractValueModel.java
@@ -32,79 +32,85 @@ import org.springframework.util.closure.Closure;
  * @author Keith Donald
  */
 public abstract class AbstractValueModel extends AbstractPropertyChangePublisher implements BoundValueModel {
-	private Set listeners;
+    private Set listeners;
 
-	public void addValueChangeListener(ValueChangeListener l) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("[Adding value listener " + l + "]");
-		}
-		getOrCreateListeners().add(l);
-	}
+    public void addValueChangeListener(ValueChangeListener l) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("[Adding value listener " + l + "]");
+        }
+        getOrCreateListeners().add(l);
+    }
 
-	public void removeValueChangeListener(ValueChangeListener l) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("[Removing value listener " + l + "]");
-		}
-		getOrCreateListeners().remove(l);
-	}
+    public void removeValueChangeListener(ValueChangeListener l) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("[Removing value listener " + l + "]");
+        }
+        getOrCreateListeners().remove(l);
+    }
 
-	private Collection getOrCreateListeners() {
-		if (listeners == null) {
-			listeners = new LinkedHashSet(9);
-		}
-		return listeners;
-	}
+    private Collection getOrCreateListeners() {
+        if (listeners == null) {
+            listeners = new LinkedHashSet(9);
+        }
+        else {
+            // create a copy in order to prevent concurent modification problems
+            listeners = new LinkedHashSet(listeners);
+        }
+        return listeners;
+    }
 
-	protected void fireValueChanged() {
-		if (listeners != null) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("[Firing value changed event]");
-			}
-			Iterator it = listeners.iterator();
-			while (it.hasNext()) {
-			    try {
-			        ((ValueChangeListener)it.next()).valueChanged();
-			    } catch(ConcurrentModificationException e) {
-			        logger.equals("Concurrent access to value model listener list detected!  Subscribing listeners must not update the value model in their handler methods.");
-			        throw e;
-			    }
-			}
-		}
-	}
+    protected void fireValueChanged() {
+        if (listeners != null) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("[Firing value changed event]");
+            }
+            Iterator it = listeners.iterator();
+            while (it.hasNext()) {
+                try {
+                    ((ValueChangeListener)it.next()).valueChanged();
+                }
+                catch (ConcurrentModificationException e) {
+                    logger
+                            .equals("Concurrent access to value model listener list detected!  Subscribing listeners must not update the value model in their handler methods.");
+                    throw e;
+                }
+            }
+        }
+    }
 
-	protected void fireValueChanged(boolean oldValue, boolean newValue) {
-		fireValueChanged(new Boolean(oldValue), new Boolean(newValue));
-	}
+    protected void fireValueChanged(boolean oldValue, boolean newValue) {
+        fireValueChanged(Boolean.valueOf(oldValue), Boolean.valueOf(newValue));
+    }
 
-	protected void fireValueChanged(int oldValue, int newValue) {
-		fireValueChanged(new Integer(oldValue), new Integer(newValue));
-	}
+    protected void fireValueChanged(int oldValue, int newValue) {
+        fireValueChanged(new Integer(oldValue), new Integer(newValue));
+    }
 
-	protected void fireValueChanged(long oldValue, long newValue) {
-		fireValueChanged(new Long(oldValue), new Long(newValue));
-	}
+    protected void fireValueChanged(long oldValue, long newValue) {
+        fireValueChanged(new Long(oldValue), new Long(newValue));
+    }
 
-	protected void fireValueChanged(double oldValue, double newValue) {
-		fireValueChanged(new Double(oldValue), new Double(newValue));
-	}
+    protected void fireValueChanged(double oldValue, double newValue) {
+        fireValueChanged(new Double(oldValue), new Double(newValue));
+    }
 
-	protected void fireValueChanged(float oldValue, float newValue) {
-		fireValueChanged(new Float(oldValue), new Float(newValue));
-	}
+    protected void fireValueChanged(float oldValue, float newValue) {
+        fireValueChanged(new Float(oldValue), new Float(newValue));
+    }
 
-	protected void fireValueChanged(Object oldValue, Object newValue) {
-		fireValueChanged();
-		firePropertyChange(VALUE_PROPERTY, oldValue, newValue);
-	}
+    protected void fireValueChanged(Object oldValue, Object newValue) {
+        fireValueChanged();
+        firePropertyChange(VALUE_PROPERTY, oldValue, newValue);
+    }
 
-	protected void updateSilently(ValueModel valueModel, ValueChangeListener changeHandler, Closure updateBlock) {
-		try {
-			valueModel.removeValueChangeListener(changeHandler);
-			updateBlock.call(null);
-		}
-		finally {
-			valueModel.addValueChangeListener(changeHandler);
-		}
-	}
+    protected void updateSilently(ValueModel valueModel, ValueChangeListener changeHandler, Closure updateBlock) {
+        try {
+            valueModel.removeValueChangeListener(changeHandler);
+            updateBlock.call(null);
+        }
+        finally {
+            valueModel.addValueChangeListener(changeHandler);
+        }
+    }
 
 }
