c4cba96267d70bce9b6f1532bbfb35e4f5ceaf08
Dmitry Kopylenko <dmitriy.kopylenko@gmail.com>
2004-11-30 17:34:32 +0000

    Added JamonPerformanceMonitorInterceptor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4430 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/aop/interceptor/JamonPerformanceMonitorInterceptor.java
@@ -0,0 +1,37 @@
+package org.springframework.aop.interceptor;
+
+import java.io.Serializable;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
+
+/**
+ * Performance monitor interceptor that uses <b>Jamon </b> library to perfom the performance mesurment on the
+ * intercepted method and output the stats.
+ * <p>
+ * This code is inspired by Thierry Templier's blog
+ * 
+ * @author Dmitriy Kopylenko
+ * @version $Id: JamonPerformanceMonitorInterceptor.java,v 1.1 2004-11-30 17:34:32 dkopylenko Exp $
+ */
+public class JamonPerformanceMonitorInterceptor implements MethodInterceptor, Serializable {
+
+    protected static final Log logger = LogFactory.getLog(JamonPerformanceMonitorInterceptor.class);
+    
+    /**
+     * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
+     */
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        Monitor mon = MonitorFactory.start(invocation.toString());
+        Object rval = invocation.proceed();
+        mon.stop();
+        logger.info("The performance statistics for " + invocation +":\n" + mon);
+
+        return rval;
+    }
+}
