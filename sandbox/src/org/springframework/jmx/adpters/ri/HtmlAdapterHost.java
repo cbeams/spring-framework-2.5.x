5495d4cd9de6dec160a4b6ea38f9c046baf5400c
Rob Harrop <rharrop@interface21.com>
2004-12-06 17:25:40 +0000

    Moved HtmlAdapterHost
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4483 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/jmx/adpters/ri/HtmlAdapterHost.java
+++ sandbox/src/org/springframework/jmx/adpters/ri/HtmlAdapterHost.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.jmx.adpters.ri;
+package org.springframework.jmx.adapters.ri;
 
 import org.springframework.jmx.adapters.AbstractReflectionBasedAdapterHost;
 
