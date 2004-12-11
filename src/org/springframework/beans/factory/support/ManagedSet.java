69a179c2f04fc986e4c7af3f7954a62aa513ce80
Juergen Hoeller <jhoeller@interface21.com>
2004-12-11 23:42:47 +0000

    reworked ManagedSet to work with a target Set, preferably a LinkedHashSet
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4558 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/ManagedSet.java
+++ src/org/springframework/beans/factory/support/ManagedSet.java
@@ -16,21 +16,101 @@
 
 package org.springframework.beans.factory.support;
 
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.springframework.core.CollectionFactory;
 
 /**
- * Tag subclass used to hold managed Set elements, which may
- * include runtime bean references.
+ * Tag collection class used to hold managed Set values,
+ * which may include runtime bean references.
+ *
+ * <p>Wraps a target Set, which will be a linked set if possible
+ * (that is, if running on JDK 1.4 or if Commons Collections 3.x is available).
+ *
  * @author Juergen Hoeller
  * @since 21.01.2004
+ * @see org.springframework.core.CollectionFactory#createLinkedSetIfPossible
  */
-public class ManagedSet extends HashSet {
+public class ManagedSet implements Set {
+
+	private final Set targetSet;
 
 	public ManagedSet() {
+		this(16);
 	}
 
 	public ManagedSet(int initialCapacity) {
-		super(initialCapacity);
+		this.targetSet = CollectionFactory.createLinkedSetIfPossible(initialCapacity);
+	}
+
+	public ManagedSet(Set targetSet) {
+		this.targetSet = targetSet;
+	}
+
+	public int size() {
+		return this.targetSet.size();
+	}
+
+	public boolean isEmpty() {
+		return this.targetSet.isEmpty();
+	}
+
+	public boolean contains(Object obj) {
+		return this.targetSet.contains(obj);
+	}
+
+	public Iterator iterator() {
+		return this.targetSet.iterator();
+	}
+
+	public Object[] toArray() {
+		return this.targetSet.toArray();
+	}
+
+	public Object[] toArray(Object[] arr) {
+		return this.targetSet.toArray(arr);
+	}
+
+	public boolean add(Object obj) {
+		return this.targetSet.add(obj);
+	}
+
+	public boolean remove(Object obj) {
+		return this.targetSet.remove(obj);
+	}
+
+	public boolean containsAll(Collection coll) {
+		return this.targetSet.containsAll(coll);
+	}
+
+	public boolean addAll(Collection coll) {
+		return this.targetSet.addAll(coll);
+	}
+
+	public boolean retainAll(Collection coll) {
+		return this.targetSet.retainAll(coll);
+	}
+
+	public boolean removeAll(Collection coll) {
+		return this.targetSet.removeAll(coll);
+	}
+
+	public void clear() {
+		this.targetSet.clear();
+	}
+
+	public int hashCode() {
+		return this.targetSet.hashCode();
+	}
+
+	public boolean equals(Object obj) {
+		return this.targetSet.equals(obj);
+	}
+
+	public String toString() {
+		return this.targetSet.toString();
 	}
 
 }
