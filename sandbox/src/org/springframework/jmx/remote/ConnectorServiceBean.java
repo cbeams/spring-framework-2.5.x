1c0aa99c8568f4e42cd199e20410f19d8c40543e
Juergen Hoeller <jhoeller@interface21.com>
2004-12-30 10:33:40 +0000

    stop connector server on shutdown
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4649 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/jmx/remote/ConnectorServiceBean.java
+++ sandbox/src/org/springframework/jmx/remote/ConnectorServiceBean.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
 package org.springframework.jmx.remote;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.Map;
 
 import javax.management.JMException;
@@ -27,7 +26,9 @@ import javax.management.remote.JMXConnectorServer;
 import javax.management.remote.JMXConnectorServerFactory;
 import javax.management.remote.JMXServiceURL;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.jmx.ObjectNameManager;
 import org.springframework.jmx.exceptions.ObjectNamingException;
 import org.springframework.jmx.util.JmxUtils;
@@ -36,111 +37,132 @@ import org.springframework.jmx.util.JmxUtils;
  * @author Rob Harrop
  * @author Marcus Brito
  */
-public class ConnectorServiceBean implements InitializingBean {
+public class ConnectorServiceBean implements InitializingBean, DisposableBean {
 
-    private MBeanServer mbeanServer;
+	private MBeanServer mbeanServer;
 
-    private JMXConnectorServer connectorServer;
+	private boolean registerConnectorAsMBean = false;
 
-    private boolean registerConnectorAsMBean = false;
+	private String objectName;
 
-    private String objectName;
+	private String serviceUrl = "service:jmx:jmxmp://localhost:9876";
 
-    private String serviceUrl = "service:jmx:jmxmp://localhost:9876";
+	private Map environment;
 
-    private Map environment;
+	private boolean threaded = false;
 
-    private boolean threaded = false;
+	private boolean daemon = false;
 
-    private boolean daemon = false;
+	private JMXConnectorServer connectorServer;
 
-    public void setThreaded(boolean threaded) {
-        this.threaded = threaded;
-    }
 
-    public void setDaemon(boolean daemon) {
-        this.daemon = daemon;
-    }
+	public void setServer(MBeanServer mbeanServer) {
+		this.mbeanServer = mbeanServer;
+	}
 
-    public void setServer(MBeanServer mbeanServer) {
-        this.mbeanServer = mbeanServer;
-    }
+	public void setRegisterConnectorAsMBean(boolean registerConnectorAsMBean) {
+		this.registerConnectorAsMBean = registerConnectorAsMBean;
+	}
 
-    public void setRegisterConnectorAsMBean(boolean registerConnectorAsMBean) {
-        this.registerConnectorAsMBean = registerConnectorAsMBean;
-    }
+	public void setObjectName(String objectName) {
+		this.objectName = objectName;
+	}
 
-    public void setEnvironment(Map environment) {
-        this.environment = environment;
-    }
+	public void setServiceUrl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+	}
 
-    public void setObjectName(String objectName) {
-        this.objectName = objectName;
-    }
+	public void setEnvironment(Map environment) {
+		this.environment = environment;
+	}
 
-    public void setServiceUrl(String serviceUrl) {
-        this.serviceUrl = serviceUrl;
-    }
+	public String getServiceUrl() {
+		return this.serviceUrl;
+	}
 
-    public String getServiceUrl() {
-        return this.serviceUrl;
-    }
+	public void setThreaded(boolean threaded) {
+		this.threaded = threaded;
+	}
 
-    public void afterPropertiesSet() throws Exception {
-        start();
-    }
+	public void setDaemon(boolean daemon) {
+		this.daemon = daemon;
+	}
 
-    public void start() throws MalformedURLException, IOException, JMException {
-        if (mbeanServer == null) {
-            this.mbeanServer = JmxUtils.locateMBeanServer();
-        }
 
-        ObjectName oname = null;
+	public void afterPropertiesSet() throws IOException, JMException {
+		start();
+	}
 
-        // check that the object name is specified
-        if ((registerConnectorAsMBean)) {
-            if (objectName == null) {
-                throw new ObjectNamingException("Must set value for property, objectName, "
-                        + "when registerConnectorAsBean is true.");
-            } else {
-                try {
-                    oname = ObjectNameManager.getInstance(objectName);
-                } catch (MalformedObjectNameException ex) {
-                    throw new ObjectNamingException("ObjectName: " + objectName
-                            + " is malformed.", ex);
-                }
-            }
-        }
+	public void start() throws IOException, JMException {
+		if (this.mbeanServer == null) {
+			this.mbeanServer = JmxUtils.locateMBeanServer();
+		}
 
-        // create the service url
-        JMXServiceURL url = new JMXServiceURL(serviceUrl);
+		ObjectName oname = null;
 
-        // create the connector now
-        connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(url,
-                environment, mbeanServer);
+		// check that the object name is specified
+		if (this.registerConnectorAsMBean) {
+			if (this.objectName == null) {
+				throw new ObjectNamingException("objectName is required when registerConnectorAsBean is true");
+			}
+			else {
+				try {
+					oname = ObjectNameManager.getInstance(this.objectName);
+				}
+				catch (MalformedObjectNameException ex) {
+					throw new ObjectNamingException("ObjectName '" + this.objectName + "' is malformed", ex);
+				}
+			}
+		}
 
-        // do we want to register the connector with the mbean server?
-        if (registerConnectorAsMBean) {
-            mbeanServer.registerMBean(connectorServer, oname);
-        }
+		// Create the service URL.
+		JMXServiceURL url = new JMXServiceURL(this.serviceUrl);
 
-        if (threaded) {
-            // now start the connector
-            Thread connectorThread = new Thread(new Runnable() {
-                public void run() {
-                    try {
-                        connectorServer.start();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e.getMessage(), e);
-                    }
-                }
-            });
+		// Create the connector server now.
+		this.connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(url,
+				this.environment, this.mbeanServer);
 
-            connectorThread.setName("JMX Connector Thread [" + serviceUrl + "]");
-            connectorThread.setDaemon(daemon);
-            connectorThread.start();
-        } else {
-            connectorServer.start();
-        }
-    }
+		// Do we want to register the connector with the MBean server?
+		if (this.registerConnectorAsMBean) {
+			this.mbeanServer.registerMBean(this.connectorServer, oname);
+		}
+
+		if (this.threaded) {
+			// Start the connector via a thread.
+			Thread connectorThread = new Thread() {
+				public void run() {
+					try {
+						connectorServer.start();
+					}
+					catch (IOException ex) {
+						throw new DelayedConnectorStartException(ex);
+					}
+				}
+			};
+
+			connectorThread.setName("JMX Connector Thread [" + this.serviceUrl + "]");
+			connectorThread.setDaemon(this.daemon);
+			connectorThread.start();
+		}
+		else {
+			this.connectorServer.start();
+		}
+	}
+
+	public void destroy() throws IOException {
+		this.connectorServer.stop();
+	}
+
+
+	/**
+	 * Exception to be thrown if the JMX connector server cannot be started
+	 * (in a concurrent thread).
+	 */
+	public static class DelayedConnectorStartException extends NestedRuntimeException {
+
+		private DelayedConnectorStartException(IOException ex) {
+			super("Could not start JMX connector server after delay", ex);
+		}
+	}
+
 }
