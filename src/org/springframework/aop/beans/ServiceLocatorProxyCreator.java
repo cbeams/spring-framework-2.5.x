9035646625afa0fe0669e6f93433050ec88111e7
Colin Sampaleanu <csampaleanu@interface21.com>
2005-01-02 19:38:00 +0000

    ongoing ServiceLocatorProxyCreator work
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4658 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/beans/ServiceLocatorProxyCreator.java
+++ src/org/springframework/aop/beans/ServiceLocatorProxyCreator.java
@@ -32,12 +32,9 @@ import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 /**
- * 2004-12-30: Work in progress, but I need to check in to check out on another machine.
- * Will be complete (polished) in a day or two.
- * 
- * <p>FactoryBean which, given an interface which must have one or more methods
- * with the signatures:
- * <code>
+ * <p>
+ * FactoryBean which, given an interface which must have one or more methods
+ * with the signatures: <code>
  * <type> getXXX()
  * <code>
  * or
@@ -45,9 +42,11 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
  * <type> getXXX(String id)
  * <code>
  * creates a proxy which implements that interface.</p>
- * On invocation of one of these factory methods on the proxy, the proxy returns
- * the result of a beanfactory.getBean(id) call. If id is null, then if one bean
- * in the factory matches the return type of the 'get' method, that is returned.
+ * <p>On invocation of the no-arg factory method, or the String-arg factory method with an 
+ * id of null, if exactly one bean in the factory matches the return type of the 'get' method,
+ * that is returned, otherwise an exception is thrown. On invocation of the String-arg factory
+ * method with a non-null argument, the proxy returns the result of a beanfactory.getBean(id)
+ * call.</p>
  * 
  * If an id is specified, and no bean matches the specified id, or the id is
  * null and there is not exactly one bean matching the return type of the factory
@@ -57,108 +56,102 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
  */
 
 public class ServiceLocatorProxyCreator implements FactoryBean, InitializingBean,
-        BeanFactoryAware {
+		BeanFactoryAware {
 
-    private Class serviceLocatorInterface;
+	private Class serviceLocatorInterface;
 
-    private ListableBeanFactory beanFactory;
+	private ListableBeanFactory beanFactory;
 
-    private Object proxy;
+	private Object proxy;
 
-    public void setServiceLocatorInterface(Class interfaceName) {
-        this.serviceLocatorInterface = interfaceName;
-    }
+	public void setServiceLocatorInterface(Class interfaceName) {
+		this.serviceLocatorInterface = interfaceName;
+	}
 
-    // javadoc in superclass!
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        if (!(beanFactory instanceof ListableBeanFactory))
-            throw new FatalBeanException(
-                    "This class must be used in BeanFactory that is at least a ListableBeanFactory");
-        this.beanFactory = (ListableBeanFactory) beanFactory;
-    }
+	// javadoc in superclass!
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		if (!(beanFactory instanceof ListableBeanFactory))
+			throw new FatalBeanException(
+					"This class must be used in BeanFactory that is at least a ListableBeanFactory");
+		this.beanFactory = (ListableBeanFactory) beanFactory;
+	}
 
-    // javadoc in superclass!
-    public void afterPropertiesSet() throws BeansException {
-        if (serviceLocatorInterface == null)
-            throw new FatalBeanException("'factoryInterface' not set");
+	// javadoc in superclass!
+	public void afterPropertiesSet() throws BeansException {
+		if (serviceLocatorInterface == null)
+			throw new FatalBeanException("'factoryInterface' not set");
 
-        // create proxy which will delegate getXXX(String id) method to
-        // getBean() call
+		// create proxy which will delegate getXXX(String id) method to
+		// getBean() call
 
-        ProxyFactory proxyFactory = new ProxyFactory();
-        proxyFactory.addInterface(serviceLocatorInterface);
-        // add a target which will be called for base Object methods
-        // (toString(), etc.)
-        proxyFactory.setTarget(new TypedFactoryTarget());
+		ProxyFactory proxyFactory = new ProxyFactory();
+		proxyFactory.addInterface(serviceLocatorInterface);
+		// add a target which will be called for base Object methods
+		// (toString(), etc.)
+		proxyFactory.setTarget(new TypedFactoryTarget());
 
-        proxyFactory.addAdvice(new MethodInterceptor() {
-            public Object invoke(MethodInvocation mi) throws Throwable {
+		proxyFactory.addAdvice(new MethodInterceptor() {
+			public Object invoke(MethodInvocation mi) throws Throwable {
 
-                Method method = mi.getMethod();
-                Class[] paramTypes = method.getParameterTypes();
+				Method method = mi.getMethod();
+				Class[] paramTypes = method.getParameterTypes();
 
-                try {
-                    Method interfaceMethod = serviceLocatorInterface.getMethod(method
-                            .getName(), paramTypes);
+				try {
+					Method interfaceMethod = serviceLocatorInterface.getMethod(method
+							.getName(), paramTypes);
 
-                    if (!interfaceMethod.getName().startsWith("get")
-                            || (paramTypes.length != 1 && !String.class.equals(paramTypes[0]))) {
-                        throw new UnsupportedOperationException(
-                                "May only call methods with signature '<type> getXXXXXX(String id)' on factory interface, but tried to call: "
-                                        + interfaceMethod);
-                    }
-                    Class factoryReturnType = interfaceMethod.getReturnType();
+					if (!interfaceMethod.getName().startsWith("get")
+							|| !((paramTypes.length == 0) || (paramTypes.length == 1 && String.class
+									.equals(paramTypes[0])))) {
+						throw new UnsupportedOperationException(
+								"May only call methods with signature '<type> getXXXXXX()' or '<type> getXXXXXX(String id)' on factory interface, but tried to call: "
+										+ interfaceMethod);
+					}
+					Class factoryReturnType = interfaceMethod.getReturnType();
 
-                    Object[] args = mi.getArguments();
-                    if (args.length != 1 && !String.class.equals(args[0])) {
-                        // should actually be impossible to get here
-                        throw new IllegalArgumentException(
-                                "Expected one argument of type String");
-                    }
-                    String beanName = (String) args[0];
+					String beanName = null;
+					if (mi.getArguments().length == 1)
+						beanName = (String) mi.getArguments()[0];
 
-                    if (beanName != null) {
-                        return beanFactory.getBean(beanName);
-                    }
+					if (beanName != null)
+						return beanFactory.getBean(beanName);
 
-                    Map beans = beanFactory.getBeansOfType(factoryReturnType);
-                    if (beans.size() != 1) {
-                        throw new NoSuchBeanDefinitionException(
-                                factoryReturnType,
-                                "No bean named '"
-                                        + beanName
-                                        + "' found in factory, and there wasn't exactly one instance of a bean of type '"
-                                        + factoryReturnType.getClass() + "'");
-                    }
-                    return beans.values().iterator().next();
-                }
-                catch (NoSuchMethodException e) {
-                    // non factory interface method call (toString(), equals,
-                    // etc.)
-                    return mi.proceed();
-                }
-            }
-        });
-        proxy = proxyFactory.getProxy();
-    }
+					Map beans = beanFactory.getBeansOfType(factoryReturnType);
+					if (beans.size() != 1) {
+						throw new NoSuchBeanDefinitionException(factoryReturnType,
+								"Bean name not specified, and there isn't exactly one instance of a bean of type '"
+										+ factoryReturnType.getClass() + "'");
+					}
+					return beans.values().iterator().next();
+				}
+				catch (NoSuchMethodException e) {
+					// it's normal to get here for non factory interface method
+					// call (toString(), equals,
+					// etc.).
+					return mi.proceed();
+				}
+			}
+		});
+		proxy = proxyFactory.getProxy();
+	}
 
-    // javadoc in superclass!
-    public Object getObject() throws Exception {
-        return proxy;
-    }
+	// javadoc in superclass!
+	public Object getObject() throws Exception {
+		return proxy;
+	}
 
-    // javadoc in superclass!
-    public Class getObjectType() {
-        return serviceLocatorInterface;
-    }
+	// javadoc in superclass!
+	public Class getObjectType() {
+		return serviceLocatorInterface;
+	}
 
-    // javadoc in superclass!
-    public boolean isSingleton() {
-        return false;
-    }
+	// javadoc in superclass!
+	public boolean isSingleton() {
+		return false;
+	}
 
-    // just a type to use as the target for invocations which are not against
-    // factory interface
-    class TypedFactoryTarget {
-    }
+	// just a type to use as the target for invocations which are not against
+	// factory interface
+	class TypedFactoryTarget {
+	}
 }
