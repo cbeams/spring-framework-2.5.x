2f8faf275dc8ddd8a407e81d8cf58f23c8be5a79
Juergen Hoeller <jhoeller@interface21.com>
2005-01-13 17:54:54 +0000

    added "findPropertyForMethod" operation (for JMX support)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4741 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/BeanUtils.java
+++ src/org/springframework/beans/BeanUtils.java
@@ -317,10 +317,29 @@ public abstract class BeanUtils {
 	 * Retrieve the JavaBeans <code>PropertyDescriptor</code>s of a given class.
 	 * @param clazz the Class to retrieve the PropertyDescriptors for
 	 * @return an array of <code>PropertyDescriptors</code> for the given class
+	 * @throws BeansException if PropertyDescriptor look fails
 	 */
-	public static PropertyDescriptor[] getPropertyDescriptors(Class clazz) {
+	public static PropertyDescriptor[] getPropertyDescriptors(Class clazz) throws BeansException {
 		CachedIntrospectionResults cr = CachedIntrospectionResults.forClass(clazz);
 		return cr.getBeanInfo().getPropertyDescriptors();
+	}
+
+	/**
+	 * Find a JavaBeans <code>PropertyDescriptor</code> for the given method,
+	 * with the method either being the read method or the write method for
+	 * that bean property.
+	 * @param method the method to find a corresponding PropertyDescriptor for
+	 * @return the corresponding PropertyDescriptor, or null if none
+	 * @throws BeansException if PropertyDescriptor look fails
+	 */
+	public static PropertyDescriptor findPropertyForMethod(Method method) throws BeansException {
+		PropertyDescriptor[] pds = getPropertyDescriptors(method.getDeclaringClass());
+		for (int i = 0; i < pds.length; i++) {
+			if (method.equals(pds[i].getReadMethod()) || method.equals(pds[i].getWriteMethod())) {
+				return pds[i];
+			}
+		}
+		return null;
 	}
 
 }
