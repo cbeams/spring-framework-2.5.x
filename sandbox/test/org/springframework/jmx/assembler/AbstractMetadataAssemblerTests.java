6fac022180a206418351669e7dd76b9ea894a3ec
Rob Harrop <rharrop@interface21.com>
2005-01-19 21:04:01 +0000

    Modified MetatdaModelMBeanInfoAssembler to set descriptor fields as Strings for JBoss MX.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4825 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/test/org/springframework/jmx/assembler/AbstractMetadataAssemblerTests.java
+++ sandbox/test/org/springframework/jmx/assembler/AbstractMetadataAssemblerTests.java
@@ -107,9 +107,9 @@ public abstract class AbstractMetadataAssemblerTests extends AbstractJmxAssemble
 
 		assertEquals("Logging should be set to true", "true", desc.getFieldValue("log"));
 		assertEquals("Log file should be jmx.log", "jmx.log", desc.getFieldValue("logFile"));
-		assertEquals("Currency Time Limit should be 15", new Integer(15), desc.getFieldValue("currencyTimeLimit"));
+		assertEquals("Currency Time Limit should be 15", "15", desc.getFieldValue("currencyTimeLimit"));
 		assertEquals("Persist Policy should be OnUpdate", "OnUpdate", desc.getFieldValue("persistPolicy"));
-		assertEquals("Persist Period should be 200", new Integer(200), desc.getFieldValue("persistPeriod"));
+		assertEquals("Persist Period should be 200", "200", desc.getFieldValue("persistPeriod"));
 		assertEquals("Persist Location should be foo", "./foo", desc.getFieldValue("persistLocation"));
 		assertEquals("Persist Name should be bar", "bar.jmx", desc.getFieldValue("persistName"));
 	}
@@ -120,9 +120,9 @@ public abstract class AbstractMetadataAssemblerTests extends AbstractJmxAssemble
 		Descriptor desc = info.getAttribute("name").getDescriptor();
 
 		assertEquals("Default value should be foo", "foo", desc.getFieldValue("default"));
-		assertEquals("Currency Time Limit should be 20", new Integer(20), desc.getFieldValue("currencyTimeLimit"));
+		assertEquals("Currency Time Limit should be 20", "20", desc.getFieldValue("currencyTimeLimit"));
 		assertEquals("Persist Policy should be OnUpdate", "OnUpdate", desc.getFieldValue("persistPolicy"));
-		assertEquals("Persist Period should be 300", new Integer(300), desc.getFieldValue("persistPeriod"));
+		assertEquals("Persist Period should be 300", "300", desc.getFieldValue("persistPeriod"));
 	}
 
 	public void testOperationDescriptor() throws Exception {
@@ -130,7 +130,7 @@ public abstract class AbstractMetadataAssemblerTests extends AbstractJmxAssemble
 
 		Descriptor desc = info.getOperation("myOperation").getDescriptor();
 
-		assertEquals("Currency Time Limit should be 30", new Integer(30), desc.getFieldValue("currencyTimeLimit"));
+		assertEquals("Currency Time Limit should be 30", "30", desc.getFieldValue("currencyTimeLimit"));
 		assertEquals("Role should be \"operation\"", "operation", desc.getFieldValue("role"));
 	}
 
