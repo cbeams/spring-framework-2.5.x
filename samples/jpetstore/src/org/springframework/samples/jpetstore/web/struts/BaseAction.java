d74d172399d2669e1d755565b64cc0e33d7161b6
Juergen Hoeller <jhoeller@interface21.com>
2005-01-24 22:59:08 +0000

    added null check to "setServlet" implementation
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4856 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/jpetstore/src/org/springframework/samples/jpetstore/web/struts/BaseAction.java
+++ samples/jpetstore/src/org/springframework/samples/jpetstore/web/struts/BaseAction.java
@@ -10,10 +10,21 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
- * Superclass for Struts actions in this application. 
- * Looks up the Spring WebApplicationContext via the ServletContext,
- * and obtains the PetStoreFacade implementation from it, making it available
- * to subclasses via a protected getter method. 
+ * Superclass for Struts actions in JPetStore's web tier.
+ *
+ * <p>Looks up the Spring WebApplicationContext via the ServletContext
+ * and obtains the PetStoreFacade implementation from it, making it
+ * available to subclasses via a protected getter method.
+ *
+ * <p>As alternative to such a base class, consider using Spring's
+ * ActionSupport class for Struts, which pre-implements
+ * WebApplicationContext lookup in a generic fashion.
+ *
+ * @author Juergen Hoeller
+ * @since 30.11.2003
+ * @see #getPetStore
+ * @see org.springframework.web.context.support.WebApplicationContextUtils#getRequiredWebApplicationContext
+ * @see org.springframework.web.struts.ActionSupport
  */
 public abstract class BaseAction extends Action {
 
@@ -21,9 +32,11 @@ public abstract class BaseAction extends Action {
 
 	public void setServlet(ActionServlet actionServlet) {
 		super.setServlet(actionServlet);
-		ServletContext servletContext = actionServlet.getServletContext();
-		WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
-		this.petStore = (PetStoreFacade) wac.getBean("petStore");
+		if (actionServlet != null) {
+			ServletContext servletContext = actionServlet.getServletContext();
+			WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+			this.petStore = (PetStoreFacade) wac.getBean("petStore");
+		}
 	}
 
 	protected PetStoreFacade getPetStore() {
