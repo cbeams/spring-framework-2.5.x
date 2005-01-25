311475077570c8e384500ba866ec6845cdab520e
Juergen Hoeller <jhoeller@interface21.com>
2005-01-25 10:18:47 +0000

    added constructor that takes a ResultSetExtractor as argument
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4865 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/core/SqlReturnResultSet.java
+++ src/org/springframework/jdbc/core/SqlReturnResultSet.java
@@ -20,25 +20,51 @@ package org.springframework.jdbc.core;
  * Subclass of SqlOutParameter to represent a returned ResultSet
  * from a stored procedure call.
  *
- * <p>Must declare a RowCallbackHandler to handle any returned rows.
- * No additional properties: instanceof will be used to check
- * for such types.
+ * <p>Must declare a ResultSetExtractor, RowCallbackHandler or RowMapper
+ * to handle any returned rows. No additional properties: instanceof will
+ * be used to check for such types.
  *
  * <p>Returned ResultSets - like all stored procedure parameters -
  * must have names.
  *
  * @author Thomas Risberg
+ * @author Juergen Hoeller
  */
 public class SqlReturnResultSet extends ResultSetSupportingSqlParameter {
 
+	/**
+	 * Create a new SqlReturnResultSet.
+	 * @param name name of the parameter, as used in input and output maps
+	 * @param rse ResultSetExtractor to use for parsing the ResultSet
+	 */
+	public SqlReturnResultSet(String name, ResultSetExtractor rse) {
+		super(name, 0, rse);
+	}
+
+	/**
+	 * Create a new SqlReturnResultSet.
+	 * @param name name of the parameter, as used in input and output maps
+	 * @param rch RowCallbackHandler to use for parsing the ResultSet
+	 */
 	public SqlReturnResultSet(String name, RowCallbackHandler rch) {
 		super(name, 0, rch);
 	}
 
+	/**
+	 * Create a new SqlReturnResultSet.
+	 * @param name name of the parameter, as used in input and output maps
+	 * @param rm RowMapper to use for parsing the ResultSet
+	 */
 	public SqlReturnResultSet(String name, RowMapper rm) {
 		super(name, 0, rm);
 	}
 
+	/**
+	 * Create a new SqlReturnResultSet.
+	 * @param name name of the parameter, as used in input and output maps
+	 * @param rm RowMapper to use for parsing the ResultSet
+	 * @param rowsExpected number of expected rows
+	 */
 	public SqlReturnResultSet(String name, RowMapper rm, int rowsExpected) {
 		super(name, 0, rm, rowsExpected);
 	}
