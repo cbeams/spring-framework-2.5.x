3d3ed7c3033f8d78157db1264f4874b66f390ca3
Keith Donald <kdonald@interface21.com>
2005-01-27 21:17:41 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4922 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ sandbox/src/org/springframework/web/flow/BeanFactoryAwareFlow.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ */
+package org.springframework.web.flow;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.util.Assert;
+
+/**
+ * @author Keith Donald
+ */
+public class BeanFactoryAwareFlow extends Flow implements InitializingBean, BeanFactoryAware, BeanNameAware {
+
+	private BeanFactory beanFactory;
+
+	public BeanFactoryAwareFlow() {
+		super();
+	}
+
+	/**
+	 * @param id
+	 */
+	public BeanFactoryAwareFlow(String id) {
+		super(id);
+	}
+
+	/**
+	 * @param id
+	 * @param flowDao
+	 */
+	public BeanFactoryAwareFlow(String id, FlowDao flowDao) {
+		super(id, flowDao);
+	}
+
+	/**
+	 * @param id
+	 * @param startStateId
+	 * @param flowDao
+	 * @param states
+	 */
+	public BeanFactoryAwareFlow(String id, String startStateId, FlowDao flowDao, AbstractState[] states) {
+		super(id, startStateId, flowDao, states);
+	}
+
+	/**
+	 *  
+	 */
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
+
+	/**
+	 *  
+	 */
+	public void setBeanName(String name) {
+		if (getId() == null) {
+			setId(name);
+		}
+	}
+
+	public void afterPropertiesSet() {
+		initFlow();
+	}
+
+	protected BeanFactory getBeanFactory() {
+		return beanFactory;
+	}
+
+	protected AutowireCapableBeanFactory getAutowireCapableBeanFactory() {
+		Assert.isInstanceOf(AutowireCapableBeanFactory.class, getBeanFactory());
+		return (AutowireCapableBeanFactory)beanFactory;
+	}
+
+	protected ActionBean getActionBean(Class actionBeanClassName) {
+		return (ActionBean)getAutowireCapableBeanFactory().autowire(actionBeanClassName,
+				AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, true);
+	}
+
+}
