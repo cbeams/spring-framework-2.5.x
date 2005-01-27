3b48f4f0332ecbdc1027ba2612c66cc81cdb1439
Juergen Hoeller <jhoeller@interface21.com>
2005-01-27 10:59:57 +0000

    leveraged "getObjectById", added order-by clauses
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4907 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/petclinic/src/org/springframework/samples/petclinic/ojb/PersistenceBrokerClinic.java
+++ samples/petclinic/src/org/springframework/samples/petclinic/ojb/PersistenceBrokerClinic.java
@@ -3,10 +3,10 @@ package org.springframework.samples.petclinic.ojb;
 import java.util.Collection;
 
 import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
 import org.apache.ojb.broker.query.QueryByCriteria;
 
 import org.springframework.dao.DataAccessException;
-import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.orm.ojb.support.PersistenceBrokerDaoSupport;
 import org.springframework.samples.petclinic.Clinic;
 import org.springframework.samples.petclinic.Owner;
@@ -19,7 +19,7 @@ import org.springframework.samples.petclinic.Visit;
  * OJB PersistenceBroker implementation of the Clinic interface.
  *
  * <p>The mappings are defined in "OJB-repository.xml",
- * located in the root of the classpath.
+ * located in the root of the class path.
  *
  * @author Juergen Hoeller
  * @since 04.07.2004
@@ -27,37 +27,31 @@ import org.springframework.samples.petclinic.Visit;
 public class PersistenceBrokerClinic extends PersistenceBrokerDaoSupport implements Clinic {
 
 	public Collection getVets() throws DataAccessException {
-		return getPersistenceBrokerTemplate().getCollectionByQuery(new QueryByCriteria(Vet.class));
+		QueryByCriteria query = new QueryByCriteria(Vet.class);
+		query.addOrderByAscending("lastName");
+		query.addOrderByAscending("firstName");
+		return getPersistenceBrokerTemplate().getCollectionByQuery(query);
 	}
 
 	public Collection getPetTypes() throws DataAccessException {
-		return getPersistenceBrokerTemplate().getCollectionByQuery(new QueryByCriteria(PetType.class));
+		QueryByCriteria query = new QueryByCriteria(PetType.class);
+		query.addOrderByAscending("name");
+		return getPersistenceBrokerTemplate().getCollectionByQuery(query);
 	}
 
 	public Collection findOwners(String lastName) throws DataAccessException {
 		Criteria criteria = new Criteria();
 		criteria.addLike("lastName", lastName + "%");
-		return getPersistenceBrokerTemplate().getCollectionByQuery(new QueryByCriteria(Owner.class, criteria));
+		Query query = new QueryByCriteria(Owner.class, criteria);
+		return getPersistenceBrokerTemplate().getCollectionByQuery(query);
 	}
 
 	public Owner loadOwner(int id) throws DataAccessException {
-		Criteria criteria = new Criteria();
-		criteria.addLike("id", Integer.toString(id));
-		Owner owner = (Owner) getPersistenceBrokerTemplate().getObjectByQuery(new QueryByCriteria(Owner.class, criteria));
-		if (owner == null) {
-			throw new ObjectRetrievalFailureException(Owner.class, new Integer(id));
-		}
-		return owner;
+		return (Owner) getPersistenceBrokerTemplate().getObjectById(Owner.class, new Integer(id));
 	}
 
 	public Pet loadPet(int id) throws DataAccessException {
-		Criteria criteria = new Criteria();
-		criteria.addLike("id", Integer.toString(id));
-		Pet pet = (Pet) getPersistenceBrokerTemplate().getObjectByQuery(new QueryByCriteria(Pet.class, criteria));
-		if (pet == null) {
-			throw new ObjectRetrievalFailureException(Pet.class, new Integer(id));
-		}
-		return pet;
+		return (Pet) getPersistenceBrokerTemplate().getObjectById(Pet.class, new Integer(id));
 	}
 
 	public void storeOwner(Owner owner) throws DataAccessException {
