c617be0d5e7a0a54c04c6f0b4040106a0b112a7d
Keith Donald <kdonald@interface21.com>
2005-01-27 23:34:03 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4933 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/MultiActionBean.java
+++ sandbox/src/org/springframework/web/flow/MultiActionBean.java
@@ -1,5 +1,17 @@
 /*
- * Copyright 2004-2005 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.web.flow;
 
@@ -22,7 +34,7 @@ public class MultiActionBean extends AbstractActionBean {
 	private EventHandlerMethodNameResolver methodNameResolver = new DefaultEventHandlerMethodNameResolver();
 
 	private static class DefaultEventHandlerMethodNameResolver implements EventHandlerMethodNameResolver {
-		public String getMethodName(String eventId) {
+		public String getHandlerMethodName(String eventId) {
 			return "handle" + StringUtils.capitalize(eventId) + "Event";
 		}
 	}
@@ -31,7 +43,7 @@ public class MultiActionBean extends AbstractActionBean {
 			MutableAttributesAccessor model) throws RuntimeException, ServletRequestBindingException {
 		FlowSessionExecutionInfo sessionExecution = FlowUtils.getFlowSessionExecutionInfo(model);
 		String eventId = sessionExecution.getLastEventId();
-		String handlerMethodName = methodNameResolver.getMethodName(eventId);
+		String handlerMethodName = methodNameResolver.getHandlerMethodName(eventId);
 		try {
 			Method handlerMethod = getHandlerMethod(handlerMethodName);
 			return (ActionBeanEvent)handlerMethod.invoke(getDelegate(), new Object[] { request, response, model });
