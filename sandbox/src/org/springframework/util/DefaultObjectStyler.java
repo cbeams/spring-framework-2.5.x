747d6da2c16b1629fcf770ece3c75bee134ed5bd
Keith Donald <kdonald@interface21.com>
2005-01-28 05:16:42 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@4937 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/util/DefaultObjectStyler.java
+++ sandbox/src/org/springframework/util/DefaultObjectStyler.java
@@ -23,8 +23,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import net.sf.hibernate.collection.PersistentCollection;
-
 import org.springframework.util.visitor.ReflectiveVisitorSupport;
 import org.springframework.util.visitor.Visitor;
 
@@ -36,193 +34,166 @@ import org.springframework.util.visitor.Visitor;
  */
 public class DefaultObjectStyler implements Visitor, ObjectStyler {
 
-    private ReflectiveVisitorSupport visitorSupport = new ReflectiveVisitorSupport();
+	private ReflectiveVisitorSupport visitorSupport = new ReflectiveVisitorSupport();
 
-    private static final String EMPTY = "[empty]";
+	private static final String EMPTY = "[empty]";
 
-    private static final String NULL = "[null]";
+	private static final String NULL = "[null]";
 
-    private static final String COLLECTION = "collection";
+	private static final String COLLECTION = "collection";
 
-    private static final String SET = "set";
+	private static final String SET = "set";
 
-    private static final String LIST = "list";
+	private static final String LIST = "list";
 
-    private static final String MAP = "map";
+	private static final String MAP = "map";
 
-    private static final String ARRAY = "array";
+	private static final String ARRAY = "array";
 
-    private static final ObjectStyler INSTANCE = new DefaultObjectStyler();
+	private static final ObjectStyler INSTANCE = new DefaultObjectStyler();
 
-    /**
-     * Return the default object styler singleton instance, for convenient
-     * access.
-     * 
-     * @return The default object styler
-     */
-    public static ObjectStyler instance() {
-        return INSTANCE;
-    }
+	/**
+	 * Return the default object styler singleton instance, for convenient
+	 * access.
+	 * 
+	 * @return The default object styler
+	 */
+	public static ObjectStyler instance() {
+		return INSTANCE;
+	}
 
-    /**
-     * Static accessor that calls the default styler's style method.
-     * 
-     * @param o the argument to style
-     * @return The styled string.
-     */
-    public static String call(Object o) {
-        return INSTANCE.style(o);
-    }
+	/**
+	 * Static accessor that calls the default styler's style method.
+	 * 
+	 * @param o the argument to style
+	 * @return The styled string.
+	 */
+	public static String call(Object o) {
+		return INSTANCE.style(o);
+	}
 
-    /**
-     * Styles the string form of this object using the reflective visitor
-     * pattern. The reflective help removes the need to define a vistable class
-     * for each type of styled valued.
-     * 
-     * @param o The object to be styled.
-     * @return The styled string.
-     */
-    public String style(Object o) {
-        return (String)visitorSupport.invokeVisit(this, o);
-    }
+	/**
+	 * Styles the string form of this object using the reflective visitor
+	 * pattern. The reflective help removes the need to define a vistable class
+	 * for each type of styled valued.
+	 * 
+	 * @param o The object to be styled.
+	 * @return The styled string.
+	 */
+	public String style(Object o) {
+		return (String)visitorSupport.invokeVisit(this, o);
+	}
 
-    String visit(String value) {
-        return ('\'' + value + '\'');
-    }
+	String visit(String value) {
+		return ('\'' + value + '\'');
+	}
 
-    String visit(Number value) {
-        return NumberFormat.getInstance().format(value);
-    }
+	String visit(Number value) {
+		return NumberFormat.getInstance().format(value);
+	}
 
-    String visit(Class clazz) {
-        return ClassUtils.getShortName(clazz);
-    }
+	String visit(Class clazz) {
+		return ClassUtils.getShortName(clazz);
+	}
 
-    String visit(Method method) {
-        return method.getName() + "@" + ClassUtils.getShortName(method.getDeclaringClass());
-    }
+	String visit(Method method) {
+		return method.getName() + "@" + ClassUtils.getShortName(method.getDeclaringClass());
+	}
 
-    String visit(Map value) {
-        if (value instanceof PersistentCollection) {
-            return visit((PersistentCollection)value);
-        }
-        else {
-            return visitInternal(value);
-        }
-    }
+	String visit(Map value) {
+		return visitInternal(value);
+	}
 
-    private String visitInternal(Map value) {
-        StringBuffer buffer = new StringBuffer(value.size() * 8 + 16);
-        buffer.append(MAP + "[");
-        for (Iterator i = value.entrySet().iterator(); i.hasNext();) {
-            Map.Entry entry = (Map.Entry)i.next();
-            buffer.append(style(entry));
-            if (i.hasNext()) {
-                buffer.append(',').append(' ');
-            }
-        }
-        if (value.isEmpty()) {
-            buffer.append(EMPTY);
-        }
-        buffer.append("]");
-        return buffer.toString();
-    }
+	private String visitInternal(Map value) {
+		StringBuffer buffer = new StringBuffer(value.size() * 8 + 16);
+		buffer.append(MAP + "[");
+		for (Iterator i = value.entrySet().iterator(); i.hasNext();) {
+			Map.Entry entry = (Map.Entry)i.next();
+			buffer.append(style(entry));
+			if (i.hasNext()) {
+				buffer.append(',').append(' ');
+			}
+		}
+		if (value.isEmpty()) {
+			buffer.append(EMPTY);
+		}
+		buffer.append("]");
+		return buffer.toString();
+	}
 
-    String visit(Map.Entry value) {
-        return style(value.getKey()) + " -> " + style(value.getValue());
-    }
+	String visit(Map.Entry value) {
+		return style(value.getKey()) + " -> " + style(value.getValue());
+	}
 
-    String visit(Collection value) {
-        if (value instanceof PersistentCollection) {
-            return visit((PersistentCollection)value);
-        }
-        else {
-            return visitInternal(value);
-        }
-    }
+	String visit(Collection value) {
+		return visitInternal(value);
+	}
 
-    private String visitInternal(Collection value) {
-        StringBuffer buffer = new StringBuffer(value.size() * 8 + 16);
-        buffer.append(getCollectionTypeString(value) + "[");
-        for (Iterator i = value.iterator(); i.hasNext();) {
-            buffer.append(style(i.next()));
-            if (i.hasNext()) {
-                buffer.append(',').append(' ');
-            }
-        }
-        if (value.isEmpty()) {
-            buffer.append(EMPTY);
-        }
-        buffer.append("]");
-        return buffer.toString();
-    }
+	private String visitInternal(Collection value) {
+		StringBuffer buffer = new StringBuffer(value.size() * 8 + 16);
+		buffer.append(getCollectionTypeString(value) + "[");
+		for (Iterator i = value.iterator(); i.hasNext();) {
+			buffer.append(style(i.next()));
+			if (i.hasNext()) {
+				buffer.append(',').append(' ');
+			}
+		}
+		if (value.isEmpty()) {
+			buffer.append(EMPTY);
+		}
+		buffer.append("]");
+		return buffer.toString();
+	}
 
-    private String getCollectionTypeString(Collection value) {
-        if (value instanceof List) {
-            return LIST;
-        }
-        else if (value instanceof Set) {
-            return SET;
-        }
-        else {
-            return COLLECTION;
-        }
-    }
+	private String getCollectionTypeString(Collection value) {
+		if (value instanceof List) {
+			return LIST;
+		}
+		else if (value instanceof Set) {
+			return SET;
+		}
+		else {
+			return COLLECTION;
+		}
+	}
 
-    String visit(PersistentCollection hibernateCollection) {
-        if (hibernateCollection.wasInitialized()) {
-            if (hibernateCollection instanceof Collection) {
-                return visitInternal((Collection)hibernateCollection);
-            }
-            else if (hibernateCollection instanceof Map) {
-                return visitInternal((Map)hibernateCollection);
-            }
-            else {
-                return style("<initialized unknown hibernate collection>");
-            }
-        }
-        else {
-            return style("<uninitialized hibernate collection>");
-        }
-    }
+	String visit(Object value) {
+		if (value.getClass().isArray()) {
+			return styleArray(getObjectArray(value));
+		}
+		else {
+			return String.valueOf(value);
+		}
+	}
 
-    String visit(Object value) {
-        if (value.getClass().isArray()) {
-            return styleArray(getObjectArray(value));
-        }
-        else {
-            return String.valueOf(value);
-        }
-    }
+	String visitNull() {
+		return NULL;
+	}
 
-    String visitNull() {
-        return NULL;
-    }
+	private String styleArray(Object[] array) {
+		StringBuffer buffer = new StringBuffer(array.length * 8 + 16);
+		buffer.append(ARRAY + "<" + StringUtils.delete(ClassUtils.getShortName(array.getClass()), ";") + ">[");
+		for (int i = 0; i < array.length - 1; i++) {
+			buffer.append(style(array[i]));
+			buffer.append(',').append(' ');
+		}
+		if (array.length > 0) {
+			buffer.append(style(array[array.length - 1]));
+		}
+		else {
+			buffer.append(EMPTY);
+		}
+		buffer.append("]");
+		return buffer.toString();
+	}
 
-    private String styleArray(Object[] array) {
-        StringBuffer buffer = new StringBuffer(array.length * 8 + 16);
-        buffer.append(ARRAY + "<" + StringUtils.delete(ClassUtils.getShortName(array.getClass()), ";") + ">[");
-        for (int i = 0; i < array.length - 1; i++) {
-            buffer.append(style(array[i]));
-            buffer.append(',').append(' ');
-        }
-        if (array.length > 0) {
-            buffer.append(style(array[array.length - 1]));
-        }
-        else {
-            buffer.append(EMPTY);
-        }
-        buffer.append("]");
-        return buffer.toString();
-    }
-
-    private Object[] getObjectArray(Object value) {
-        if (value.getClass().getComponentType().isPrimitive()) {
-            return ObjectUtils.toObjectArray(value);
-        }
-        else {
-            return (Object[])value;
-        }
-    }
+	private Object[] getObjectArray(Object value) {
+		if (value.getClass().getComponentType().isPrimitive()) {
+			return ObjectUtils.toObjectArray(value);
+		}
+		else {
+			return (Object[])value;
+		}
+	}
 
 }
