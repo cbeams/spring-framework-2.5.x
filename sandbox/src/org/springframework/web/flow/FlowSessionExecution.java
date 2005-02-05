aef5f4414d2e165d69e4823bdaace2d3ee24650c
Keith Donald <kdonald@interface21.com>
2005-02-05 23:46:40 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5054 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/FlowSessionExecution.java
+++ sandbox/src/org/springframework/web/flow/FlowSessionExecution.java
@@ -4,14 +4,36 @@
 package org.springframework.web.flow;
 
 /**
- * Subinterface of info that exposes mutable operations - designed for use by
- * the lifecycle listenner, which is a bit more privleged.
+ * Subinterface of <code>FlowSessionExecutionInfo</code> that exposes mutable
+ * operations. Designed for use by the <code>FlowSessionExecutionListener</code>,
+ * which is a bit more privleged than pure for-management clients. This
+ * interface may also be used in situations where other privileged objects need
+ * access to flow definition configuration details during session execution.
+ * 
+ * Note: though these definitions are exposed to clients via this interface,
+ * they should *not* be modified post application startup. Mutable operations on
+ * mutable configuration operations should be treated as frozen after a flow's
+ * session execution commences.
+ * 
  * @author Keith Donald
  */
 public interface FlowSessionExecution extends FlowSessionExecutionInfo, MutableAttributesAccessor {
+
+	/**
+	 * Returns this session execution's active flow definition.
+	 * @return The active flow definition
+	 */
 	public Flow getActiveFlow();
-	
+
+	/**
+	 * Returns this session execution's root flow definition.
+	 * @return The root flow definition.
+	 */
 	public Flow getRootFlow();
-	
+
+	/**
+	 * Returns this session execution's current state definition.
+	 * @return the current state definition.
+	 */
 	public AbstractState getCurrentState();
 }
