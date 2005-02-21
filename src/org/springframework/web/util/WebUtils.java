f0f88df8cdede84e30ef6d6fdf4e47aeccf99765
Juergen Hoeller <jhoeller@interface21.com>
2005-02-21 21:05:51 +0000

    factored out ServletContextResource's handling of real paths into the new WebUtils.getRealPath method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5479 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/util/WebUtils.java
+++ src/org/springframework/web/util/WebUtils.java
@@ -17,6 +17,7 @@
 package org.springframework.web.util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Map;
@@ -134,6 +135,32 @@ public abstract class WebUtils {
 	 */
 	public static File getTempDir(ServletContext servletContext) {
 		return (File) servletContext.getAttribute(TEMP_DIR_CONTEXT_ATTRIBUTE);
+	}
+
+	/**
+	 * Return the real path of the given path within the web application,
+	 * as provided by the servlet container.
+	 * <p>Prepends a slash if the path does not already start with a slash,
+	 * and throws a FileNotFoundException if the path cannot be resolved to
+	 * a resource (in contrast to ServletContext's <code>getRealPath</code>,
+	 * which returns null).
+	 * @param servletContext the servlet context of the web application
+	 * @return the corresponding real path
+	 * @throws FileNotFoundException if the path cannot be resolved to a resource
+	 * @see javax.servlet.ServletContext#getRealPath
+	 */
+	public static String getRealPath(ServletContext servletContext, String path) throws FileNotFoundException {
+		// Interpret location as relative to the web application root directory.
+		if (!path.startsWith("/")) {
+			path = "/" + path;
+		}
+		String realPath = servletContext.getRealPath(path);
+		if (realPath == null) {
+			throw new FileNotFoundException(
+					"ServletContext resource [" + path + "] cannot be resolved to absolute file path - " +
+					"web application archive not expanded?");
+		}
+		return realPath;
 	}
 
 
