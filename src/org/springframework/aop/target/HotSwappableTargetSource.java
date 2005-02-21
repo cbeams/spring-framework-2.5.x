88e940105d3c28ae2ee161bd941934e631a2c962
Juergen Hoeller <jhoeller@interface21.com>
2005-02-21 15:17:46 +0000

    added "hashCode" implementation
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5446 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/target/HotSwappableTargetSource.java
+++ src/org/springframework/aop/target/HotSwappableTargetSource.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.aop.target;
 
@@ -82,20 +82,23 @@ public class HotSwappableTargetSource implements TargetSource, Serializable {
 		return old;
 	}
 
+	public String toString() {
+		return "HotSwappableTargetSource for target: " + this.target;
+	}
+
 	/**
-	 * Two invoker interceptors are equal if they have the same target or
-	 * if the targets are equal.
+	 * Two HotSwappableTargetSources are equal if the targets are equal.
 	 */
 	public boolean equals(Object other) {
 		if (!(other instanceof HotSwappableTargetSource)) {
 			return false;
 		}
 		HotSwappableTargetSource otherTargetSource = (HotSwappableTargetSource) other;
-		return otherTargetSource.target == this.target || otherTargetSource.target.equals(this.target);
+		return this.target.equals(otherTargetSource.target);
 	}
-	
-	public String toString() {
-		return "HotSwappableTargetSource for target: " + this.target;
+
+	public int hashCode() {
+		return this.target.hashCode();
 	}
 
 }
