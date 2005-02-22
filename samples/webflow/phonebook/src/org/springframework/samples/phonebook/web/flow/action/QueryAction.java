d8fdb43869953e3ebd986a614a430c387a7c6e3a
Erwin Vervaet <klr8@ervacon.com>
2005-02-22 20:57:18 +0000

    Cleanup.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5538 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/webflow/phonebook/src/org/springframework/samples/phonebook/web/flow/action/QueryAction.java
+++ samples/webflow/phonebook/src/org/springframework/samples/phonebook/web/flow/action/QueryAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.samples.phonebook.web.flow.action;
 
 import javax.servlet.http.HttpServletRequest;
@@ -5,21 +20,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.samples.phonebook.domain.PhoneBook;
 import org.springframework.samples.phonebook.domain.PhoneBookQuery;
-import org.springframework.web.flow.Action;
 import org.springframework.web.flow.MutableFlowModel;
+import org.springframework.web.flow.action.AbstractAction;
 
-public class QueryAction implements Action {
+public class QueryAction extends AbstractAction {
 
 	private PhoneBook phoneBook;
 
 	public void setPhoneBook(PhoneBook phoneBook) {
 		this.phoneBook = phoneBook;
 	}
-
-	public String execute(HttpServletRequest request, HttpServletResponse response, MutableFlowModel model)
+	
+	protected String doExecuteAction(HttpServletRequest request,
+			HttpServletResponse response, MutableFlowModel model)
 			throws Exception {
 		PhoneBookQuery query = (PhoneBookQuery)model.getAttribute("query");
 		model.setAttribute("persons", phoneBook.query(query));
-		return "success";
+		return success();
 	}
 }
