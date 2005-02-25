314a998a5f1d4bb451f587f984bfbbfc229176d2
Keith Donald <kdonald@interface21.com>
2005-02-25 02:28:19 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5610 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/action/SetAction.java
+++ sandbox/src/org/springframework/web/flow/action/SetAction.java
@@ -15,19 +15,13 @@
  */
 package org.springframework.web.flow.action;
 
-import java.util.Iterator;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.propertyeditors.ClassEditor;
 import org.springframework.binding.AttributeAccessor;
 import org.springframework.binding.AttributeMapper;
-import org.springframework.binding.convert.ConverterLocator;
 import org.springframework.binding.support.Mapping;
 import org.springframework.binding.support.ParameterizableAttributeMapper;
-import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import org.springframework.web.flow.MutableFlowModel;
 
@@ -41,36 +35,10 @@ public class SetAction extends AbstractAction {
 
 	private AttributeMapper requestParameterMapper;
 
-	private ConverterLocator converterLocator;
-
 	/**
 	 * Creates a set action with an initially empty mappings list.
 	 */
 	public SetAction() {
-	}
-
-	/**
-	 * Create a set action with the specified string mapping, where the string
-	 * is a request parameter name that should be mapped as a string attribute
-	 * in the flow model with the same name.
-	 * <p>
-	 * If the type to convert should be something other than a string, it maybe
-	 * be encoded within the mappings argument with a comma delimiter: e.g.
-	 * <code>myAttribute,java.lang.Short</code>
-	 * @param mapping the string mapping (e.g. "postalCode")
-	 */
-	public SetAction(String mapping) {
-		setMapping(mapping);
-	}
-
-	/**
-	 * Create a set action with the specified string mappings, where each string
-	 * is a request parameter name that should be mapped as a string attribute in
-	 * the flow model with the same name.
-	 * @param mappings the string mappings
-	 */
-	public SetAction(String[] mappings) {
-		setMappings(mappings);
 	}
 
 	/**
@@ -90,48 +58,6 @@ public class SetAction extends AbstractAction {
 	}
 
 	/**
-	 * Set the type converter registry
-	 * @param registry the registry
-	 */
-	public void setConverterLocator(ConverterLocator registry) {
-		this.converterLocator = registry;
-	}
-
-	/**
-	 * Set the encoded string mapping.
-	 * @param mapping the mapping
-	 */
-	public void setMapping(String mapping) {
-		setMappings(new String[] { mapping });
-	}
-
-	/**
-	 * Set the encoded string mappings.
-	 * @param mappings the mappings
-	 */
-	public void setMappings(String[] mappings) {
-		ClassEditor classEditor = new ClassEditor();
-		Mapping[] maps = new Mapping[mappings.length];
-		for (int i = 0; i < mappings.length; i++) {
-			String[] encodedMapping = StringUtils.commaDelimitedListToStringArray(mappings[i]);
-			if (encodedMapping.length == 2) {
-				classEditor.setAsText(encodedMapping[1]);
-				Class clazz = (Class)classEditor.getValue();
-				maps[i] = new Mapping(encodedMapping[0], getConverterLocator().getConverter(String.class, clazz));
-			}
-			else {
-				maps[i] = new Mapping(encodedMapping[0]);
-			}
-		}
-		setMappings(maps);
-	}
-
-	protected ConverterLocator getConverterLocator() {
-		Assert.notNull(this.converterLocator, "The converterLocator property was request but is not set");
-		return this.converterLocator;
-	}
-
-	/**
 	 * Set the single mapping for this set action.
 	 * @param mapping the mapping
 	 */
@@ -145,33 +71,6 @@ public class SetAction extends AbstractAction {
 	 */
 	public void setMappings(Mapping[] mappings) {
 		this.requestParameterMapper = new ParameterizableAttributeMapper(mappings);
-	}
-
-	/**
-	 * Set the mappings map.
-	 * @param mappingsMap the mappings map
-	 */
-	public void setMappingsMap(Map mappingsMap) {
-		ClassEditor classEditor = new ClassEditor();
-		Mapping[] maps = new Mapping[mappingsMap.size()];
-		Iterator it = mappingsMap.entrySet().iterator();
-		int i = 0;
-		while (it.hasNext()) {
-			Map.Entry entry = (Map.Entry)it.next();
-			String sourceAttributeName = (String)entry.getKey();
-			String[] encodedMapping = StringUtils.commaDelimitedListToStringArray((String)entry.getValue());
-			if (encodedMapping.length == 2) {
-				classEditor.setAsText(encodedMapping[1]);
-				Class clazz = (Class)classEditor.getValue();
-				maps[i] = new Mapping(sourceAttributeName, encodedMapping[0], getConverterLocator().getConverter(
-						String.class, clazz));
-			}
-			else {
-				maps[i] = new Mapping(sourceAttributeName, encodedMapping[0]);
-			}
-			i++;
-		}
-		setMappings(maps);
 	}
 
 	/**
@@ -195,11 +94,11 @@ public class SetAction extends AbstractAction {
 	 * @author Keith Donald
 	 */
 	public static class RequestParameterAttributeAccessorAdapter implements AttributeAccessor {
-		
+
 		private HttpServletRequest request;
 
 		/**
-		 * Create a new request parameter attribute accessor. 
+		 * Create a new request parameter attribute accessor.
 		 */
 		public RequestParameterAttributeAccessorAdapter(HttpServletRequest request) {
 			this.request = request;
