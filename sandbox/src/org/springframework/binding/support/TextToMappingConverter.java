102722ac0df580f265c68632fb0d6804d04198e0
Keith Donald <kdonald@interface21.com>
2005-02-28 06:27:33 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5709 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/binding/support/TextToMappingConverter.java
+++ sandbox/src/org/springframework/binding/support/TextToMappingConverter.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2002-2004 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.springframework.binding.support;
 
@@ -50,25 +50,26 @@ public class TextToMappingConverter extends AbstractConverter {
 	}
 
 	protected Object doConvert(Object o, Class targetClass) throws Exception {
-		String[] mappingInfo = StringUtils.commaDelimitedListToStringArray((String)o);
-		String[] sourceTarget = StringUtils.delimitedListToStringArray(mappingInfo[0], "->");
-		if (mappingInfo.length == 2) {
-			Class clazz = (Class)getConversionService().getConversionExecutor(String.class, Class.class).call(mappingInfo[1]);
-			if (sourceTarget.length == 2) {
-				return new Mapping(sourceTarget[0], sourceTarget[1], getConversionService().getConversionExecutor(String.class,
-						clazz));
-			}
-			else {
-				return new Mapping(sourceTarget[0], getConversionService().getConversionExecutor(String.class, clazz));
+		//format: <sourceAttributeName>[,class][->targetAttributeName[,class]]
+		String[] sourceTarget = StringUtils.delimitedListToStringArray((String)o, "->");
+		String[] sourceMappingInfo = StringUtils.commaDelimitedListToStringArray(sourceTarget[0]);
+		String sourceAttributeName = sourceMappingInfo[0];
+		String targetAttributeName = sourceMappingInfo[0];
+		Class sourceAttributeClass = String.class;
+		Class targetAttributeClass = String.class;
+		if (sourceMappingInfo.length == 2) {
+			sourceAttributeClass = (Class)getConversionService().getConversionExecutor(String.class, Class.class).call(
+					sourceMappingInfo[1]);
+		}
+		if (sourceTarget.length == 2) {
+			String[] targetMappingInfo = StringUtils.commaDelimitedListToStringArray(sourceTarget[1]);
+			targetAttributeName = targetMappingInfo[0];
+			if (targetMappingInfo.length == 2) {
+				targetClass = (Class)getConversionService().getConversionExecutor(String.class, Class.class).call(
+						targetMappingInfo[1]);
 			}
 		}
-		else {
-			if (sourceTarget.length == 2) {
-				return new Mapping(sourceTarget[0], sourceTarget[1]);
-			}
-			else {
-				return new Mapping(sourceTarget[0]);
-			}
-		}
+		return new Mapping(sourceAttributeName, targetAttributeName, getConversionService().getConversionExecutor(
+				sourceAttributeClass, targetAttributeClass));
 	}
 }
