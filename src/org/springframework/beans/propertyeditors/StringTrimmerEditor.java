cde968d62e356e604a8fbbad30e2d889248c0db8
Juergen Hoeller <jhoeller@interface21.com>
2005-02-28 09:41:00 +0000

    added overloaded StringTrimmerEditor constructor that takes a set of chars to delete (e.g. for unwanted line breaks)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5716 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/propertyeditors/StringTrimmerEditor.java
+++ src/org/springframework/beans/propertyeditors/StringTrimmerEditor.java
@@ -12,11 +12,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.beans.propertyeditors;
 
 import java.beans.PropertyEditorSupport;
+
+import org.springframework.util.StringUtils;
 
 /**
  * Property editor that trims Strings.
@@ -29,6 +31,8 @@ import java.beans.PropertyEditorSupport;
  */
 public class StringTrimmerEditor extends PropertyEditorSupport {
 
+	private String charsToDelete;
+
 	private final boolean emptyAsNull;
 
 	/**
@@ -39,12 +43,27 @@ public class StringTrimmerEditor extends PropertyEditorSupport {
 		this.emptyAsNull = emptyAsNull;
 	}
 
+	/**
+	 * Create a new StringTrimmerEditor instance.
+	 * @param charsToDelete a set of characters to delete, in addition to
+	 * trimming an input String. Useful for deleting unwanted line breaks.
+	 * E.g. "\r\n\f" will delete all new lines and line feeds in a String.
+	 * @param emptyAsNull whether to transform an empty string to null
+	 */
+	public StringTrimmerEditor(String charsToDelete, boolean emptyAsNull) {
+		this.charsToDelete = charsToDelete;
+		this.emptyAsNull = emptyAsNull;
+	}
+
 	public void setAsText(String text) {
 		if (text == null) {
 			setValue(null);
 		}
 		else {
 			String value = text.trim();
+			if (this.charsToDelete != null) {
+				value = StringUtils.deleteAny(value, this.charsToDelete);
+			}
 			if (this.emptyAsNull && "".equals(value)) {
 				setValue(null);
 			}
--- test/org/springframework/beans/CustomEditorTests.java
+++ test/org/springframework/beans/CustomEditorTests.java
@@ -435,7 +435,37 @@ public class CustomEditorTests extends TestCase {
 		editor.setAsText(" test ");
 		assertEquals("test", editor.getValue());
 		assertEquals("test", editor.getAsText());
+		editor.setAsText("  ");
+		assertEquals(null, editor.getValue());
+		assertEquals("", editor.getAsText());
+		editor.setValue(null);
+		assertEquals("", editor.getAsText());
+	}
+
+	public void testStringTrimmerEditorWithCharsToDelete() {
+		StringTrimmerEditor editor = new StringTrimmerEditor("\r\n\f", false);
+		editor.setAsText("te\ns\ft");
+		assertEquals("test", editor.getValue());
+		assertEquals("test", editor.getAsText());
+		editor.setAsText(" test ");
+		assertEquals("test", editor.getValue());
+		assertEquals("test", editor.getAsText());
 		editor.setAsText("");
+		assertEquals("", editor.getValue());
+		assertEquals("", editor.getAsText());
+		editor.setValue(null);
+		assertEquals("", editor.getAsText());
+	}
+
+	public void testStringTrimmerEditorWithCharsToDeleteAndEmptyAsNull() {
+		StringTrimmerEditor editor = new StringTrimmerEditor("\r\n\f", true);
+		editor.setAsText("te\ns\ft");
+		assertEquals("test", editor.getValue());
+		assertEquals("test", editor.getAsText());
+		editor.setAsText(" test ");
+		assertEquals("test", editor.getValue());
+		assertEquals("test", editor.getAsText());
+		editor.setAsText(" \n\f ");
 		assertEquals(null, editor.getValue());
 		assertEquals("", editor.getAsText());
 		editor.setValue(null);
