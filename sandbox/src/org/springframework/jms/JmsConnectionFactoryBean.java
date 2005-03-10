40a1f5d3a92da3b454e5ca1b89d0a15990f8ed0e
Juergen Hoeller <jhoeller@interface21.com>
2005-03-10 18:24:08 +0000

    support Connection creation without user credentials
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5778 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/jms/JmsConnectionFactoryBean.java
+++ sandbox/src/org/springframework/jms/JmsConnectionFactoryBean.java
@@ -19,6 +19,7 @@ package org.springframework.jms;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,94 +30,81 @@ import org.springframework.beans.factory.InitializingBean;
 
 /**
  * Sets up a JMS Connection and exposes it for bean references.
- *
  * @author Mark Pollack
  */
-public class JmsConnectionFactoryBean
-    implements FactoryBean, InitializingBean, DisposableBean {
+public class JmsConnectionFactoryBean implements FactoryBean, InitializingBean, DisposableBean {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private Connection _connection;
+	private ConnectionFactory connectionFactory;
 
-	private String _userName;
+	private String username;
 
-	private String _password;
+	private String password;
 
-	private ConnectionFactory _connectionFactory;
+	private String clientId;
 
-	private String _clientID;
+	private ExceptionListener exceptionListener;
 
-	private ExceptionListener _exceptionListener;
+	private Connection connection;
+
+
+	public void setConnectionFactory(ConnectionFactory connectionFactory) {
+		this.connectionFactory = connectionFactory;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public void setExceptionListener(ExceptionListener exceptionListener) {
+		this.exceptionListener = exceptionListener;
+	}
+
+
+	public void afterPropertiesSet() throws JMSException {
+		if (this.connectionFactory == null) {
+			throw new IllegalArgumentException("connectionFactory is required");
+		}
+
+		logger.info("Creating JMS Connection");
+		this.connection = (this.username != null) ?
+				this.connectionFactory.createConnection(this.username, this.password) :
+				this.connectionFactory.createConnection();
+
+		if (this.clientId != null) {
+			this.connection.setClientID(this.clientId);
+		}
+		if (this.exceptionListener != null) {
+			this.connection.setExceptionListener(this.exceptionListener);
+		}
+	}
 
 
 	public Object getObject() throws Exception {
-		return _connection;
+		return connection;
 	}
-
 
 	public Class getObjectType() {
 		return Connection.class;
 	}
-
 
 	public boolean isSingleton() {
 		return true;
 	}
 
 
-	public void afterPropertiesSet() throws Exception {
-		if (_connectionFactory == null) {
-			throw new IllegalArgumentException("Did not set required JMS connection factory property");
-		}
-		logger.info("Creating JMS Connection");
-		_connection = _connectionFactory.createConnection(_userName, _password);
-		if (_clientID != null) _connection.setClientID(_clientID);
-		if (_exceptionListener != null) _connection.setExceptionListener(_exceptionListener);
-
-	}
-
-
-	public void destroy() throws Exception {
+	public void destroy() throws JMSException {
 		logger.info("Closing JMS Connection");
-		_connection.close();
-	}
-
-
-	/**
-	 * Set the connection factory that will be used to create the connection.
-	 * @param factory
-	 */
-	public void setConnectionFactory(ConnectionFactory factory) {
-		_connectionFactory = factory;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setPassword(String string) {
-		_password = string;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setUserName(String string) {
-		_userName = string;
-	}
-
-	/**
-	 * @param string
-	 */
-	public void setClientID(String string) {
-		_clientID = string;
-	}
-
-	/**
-	 * @param listener
-	 */
-	public void setExceptionListener(ExceptionListener listener) {
-		_exceptionListener = listener;
+		this.connection.close();
 	}
 
 }
