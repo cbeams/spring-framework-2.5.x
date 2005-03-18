4532eb7db1beb3b0de26da0fe49a64f36ca4044a
Juergen Hoeller <jhoeller@interface21.com>
2005-03-18 22:39:01 +0000

    simplified XMLC view support
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5938 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/web/servlet/view/xmlc/XmlcViewTests.java
+++ test/org/springframework/web/servlet/view/xmlc/XmlcViewTests.java
@@ -1,51 +1,54 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.springframework.web.servlet.view.xmlc;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import junit.framework.TestCase;
+import org.enhydra.xml.io.OutputOptions;
+import org.enhydra.xml.xmlc.XMLObject;
 
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.web.context.support.StaticWebApplicationContext;
 
-import org.enhydra.xml.xmlc.servlet.XMLCContext;
-import org.enhydra.xml.xmlc.XMLObject;
-import org.enhydra.xml.io.OutputOptions;
-
 /**
- * @author robh
+ * @author Rob Harrop
  */
 public class XmlcViewTests extends TestCase {
 
 	private final MockServletContext servletContext = new MockServletContext();
-
 	private final MockHttpServletRequest request = new MockHttpServletRequest();
-
 	private final MockHttpServletResponse response = new MockHttpServletResponse();
-
-	private final XMLCContext xmlcContext = XMLCContext.getContext(servletContext);
 
 	private static final StaticWebApplicationContext context = new StaticWebApplicationContext();
 
 	public void testCreateXMLObject() throws Exception {
-
 		final Map model = new HashMap();
 
 		final AbstractXmlcView view = new AbstractXmlcView() {
-			protected XMLObject createXMLObject(Map innerModel, HttpServletRequest innerRequest,
-					HttpServletResponse innerResponse, XMLCContext innerContext) throws ServletException {
+			protected XMLObject createXMLObject(Map innerModel, HttpServletRequest innerRequest) {
 				assertEquals("Model is not the same", model, innerModel);
 				assertEquals("Request is not the same", request, innerRequest);
-				assertEquals("Response is not the same", response, innerResponse);
-				assertEquals("XMLCContext is not the same", xmlcContext, innerContext);
-
 				return new TestXmlObject();
 			}
 		};
@@ -77,9 +80,10 @@ public class XmlcViewTests extends TestCase {
 		view.renderMergedOutputModel(model, request, response);
 	}
 
+
 	private static class OutputTestView extends AbstractXmlcView {
 
-		protected XMLObject createXMLObject(Map model, HttpServletRequest request, HttpServletResponse response, XMLCContext context) throws ServletException {
+		protected XMLObject createXMLObject(Map model, HttpServletRequest request) {
 			TestXmlObject xmlObject = new TestXmlObject();
 			xmlObject.setTitle("My New Title");
 			xmlObject.setTextTest("My New Text");
@@ -87,11 +91,12 @@ public class XmlcViewTests extends TestCase {
 		}
 	}
 
+
 	private static class OutputOptionsTestView extends AbstractXmlcView {
 
 		public boolean getOutputOptionsCalled = false;
 
-		protected XMLObject createXMLObject(Map model, HttpServletRequest request, HttpServletResponse response, XMLCContext context) throws ServletException {
+		protected XMLObject createXMLObject(Map model, HttpServletRequest request) throws ServletException {
 			return new TestXmlObject();
 		}
 
@@ -100,6 +105,5 @@ public class XmlcViewTests extends TestCase {
 			return new OutputOptions();
 		}
 	}
-
 
 }
