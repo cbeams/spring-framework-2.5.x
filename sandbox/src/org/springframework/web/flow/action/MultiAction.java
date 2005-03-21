5a8d0e218d9bdee300dd8f8fcfa668665074828d
Keith Donald <kdonald@interface21.com>
2005-03-21 15:59:12 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5998 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/action/MultiAction.java
+++ sandbox/src/org/springframework/web/flow/action/MultiAction.java
@@ -17,35 +17,43 @@ package org.springframework.web.flow.action;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 import org.springframework.util.Assert;
+import org.springframework.util.CachingMapTemplate;
+import org.springframework.web.flow.ActionExecutionException;
 import org.springframework.web.flow.Event;
 import org.springframework.web.flow.RequestContext;
 
 /**
- * Action implementation that bundles many action execution methods into
- * a single action implementation class. Action execution methods defined
- * by subclasses should follow the following signature:
+ * Action implementation that bundles many action execution methods into a
+ * single action implementation class. Action execution methods defined by
+ * subclasses should follow the following signature:
+ * 
  * <pre>
- * 	public Event executeMethodName(RequestContext context)
+ *    public Event executeMethodName(RequestContext context)
  * </pre>
- * By default, the "executeMethodName" will be the name of the <b>current
- * state</b> of the flow, so the follow state definition
+ * 
+ * By default, the "executeMethodName" will be the name of the <b>current state</b>
+ * of the flow, so the follow state definition
+ * 
  * <pre>
- * 	&lt;action-state id="search"&gt;
- * 		&lt;action name="searchAct" bean="myMultiAction"/&gt;
- * 		&lt;transition name="searchAct.ok" to="results"/&gt;
- * 	&lt;/action-state&gt;
+ *     &lt;action-state id=&quot;search&quot;&gt;
+ *         &lt;action bean=&quot;my.search.action&quot;/&gt;
+ *         &lt;transition on=&quot;success&quot; to=&quot;results&quot;/&gt;
+ *     &lt;/action-state&gt;
  * </pre>
- * will execute the method
+ * 
+ * ... will execute the method:
+ * 
  * <pre>
- * 	public Event search(RequestContext context)
- * </pre> 
+ *     public Event search(RequestContext context)
+ * </pre>
+ * 
  * <p>
- * A typical use of the MultiAction is to combine all CRUD operations for
- * a certain domain object into a single controller. This action also
- * allows you to reduce the number of action beans you have to configure
- * in the application context. 
+ * A typical use of the MultiAction is to combine all CRUD operations for a
+ * certain domain object into a single controller. This action also allows you
+ * to reduce the number of action beans you have to define and configure.
  * <p>
  * <b>Exposed configuration properties:</b> <br>
  * <table border="1">
@@ -61,93 +69,70 @@ import org.springframework.web.flow.RequestContext;
  * </tr>
  * <tr>
  * <td>methodNameResolver</td>
- * <td><i>{@link MultiAction.CurrentStateActionExecutionMethodNameResolver current state}</i></td>
- * <td>Set the strategy used to resolve the name of an action execution
- * method.</td>
+ * <td><i>{@link MultiAction.CurrentStateActionExecuteMethodNameResolver current state}</i></td>
+ * <td>Set the strategy used to resolve the name of an action execution method.</td>
  * </tr>
  * </table>
  * 
- * @see MultiAction.ActionExecutionMethodNameResolver
+ * @see MultiAction.ActionExecuteMethodNameResolver
  * 
  * @author Keith Donald
  * @author Erwin Vervaet
  */
 public class MultiAction extends AbstractAction {
-	
-	/**
-	 * Strategy interface used by the MultiAction to map a request context
-	 * to the name of an action execution method.
-	 * 
-	 * @author Keith Donald
-	 * @author Erwin Vervaet
-	 */
-	public interface ActionExecutionMethodNameResolver {
-		
-		/**
-		 * Resolve a method name from given flow execution request context.
-		 * @param context the flow execution request context
-		 * @return the name of the method that should handle action execution
-		 */
-		public String getMethodName(RequestContext context);
-	}
 
 	/**
-	 * Action execution method name resolver that uses the name of the
-	 * current state of the flow execution as a method name.
-	 * This is the default method name resolver used by the MultiAction class.
-	 * 
-	 * @author Erwin Vervaet
+	 * A configurable delegate that may handle execution requests--useful for
+	 * invoking legacy code.
 	 */
-	public static class CurrentStateActionExecutionMethodNameResolver implements ActionExecutionMethodNameResolver {
-		public String getMethodName(RequestContext context) {
-			return context.getCurrentState().getId();
-		}
-	}
-	
 	private Object delegate = this;
 
-	private ActionExecutionMethodNameResolver methodNameResolver = new CurrentStateActionExecutionMethodNameResolver();
+	/**
+	 * The action execute method name resolver strategy.
+	 */
+	private ActionExecuteMethodNameResolver executeMethodNameResolver = new CurrentStateActionExecuteMethodNameResolver();
 
 	/**
-	 * Returns the delegate object holding the action execution methods.
-	 * Defaults to this object.
+	 * The resolved action execute method cache.
 	 */
-	protected Object getDelegate() {
-		return this;
-	}
+	private Map executeMethodCache = new CachingMapTemplate() {
+		public Object create(Object key) {
+			String executeMethodName = (String)key;
+			try {
+				return getDelegate().getClass().getMethod(executeMethodName, new Class[] { RequestContext.class });
+			}
+			catch (NoSuchMethodException e) {
+				throw new ActionExecutionException(
+						"Unable to resolve action execute method with signature 'public Event " + executeMethodName
+								+ "(RequestContext.class)' - make sure this method name is correct "
+								+ "and such a method is defined in this action implementation", e);
+			}
+		}
+	};
 
 	/**
 	 * Set the delegate object holding the action execution methods.
 	 * @param delegate the delegate to set
 	 */
-	protected void setDelegate(Object delegate) {
+	public void setDelegate(Object delegate) {
 		this.delegate = delegate;
 	}
-	
-	/**
-	 * Get the strategy used to resolve action execution method
-	 * names. Defaults to current state id based resolution.
-	 */
-	public ActionExecutionMethodNameResolver getMethodNameResolver() {
-		return methodNameResolver;
-	}
 
 	/**
-	 * Set the strategy used to resolve action execution method
-	 * names.
+	 * Set the strategy used to resolve action execution method names.
 	 */
-	public void setMethodNameResolver(ActionExecutionMethodNameResolver methodNameResolver) {
-		this.methodNameResolver = methodNameResolver;
+	public void setExecuteMethodNameResolver(ActionExecuteMethodNameResolver methodNameResolver) {
+		this.executeMethodNameResolver = methodNameResolver;
 	}
-
 
 	protected Event doExecuteAction(RequestContext context) throws Exception {
-		String actionExecutionMethodName = this.methodNameResolver.getMethodName(context);
+		String actionExecutionMethodName = this.executeMethodNameResolver.getMethodName(context);
 		try {
-			Method actionExecutionMethod = getActionExecutionMethod(actionExecutionMethodName);
-			Object result = actionExecutionMethod.invoke(getDelegate(), new Object[] { context });
+			Method actionExecuteMethod = getActionExecuteMethod(actionExecutionMethodName);
+			Object result = actionExecuteMethod.invoke(getDelegate(), new Object[] { context });
 			if (result != null) {
-				Assert.isInstanceOf(Event.class, result, "Event handler methods should return a result object of type Event");
+				Assert.isInstanceOf(Event.class, result,
+						"Event handler methods should return a result object of type Event");
 			}
 			return (Event)result;
 		}
@@ -163,12 +148,56 @@ public class MultiAction extends AbstractAction {
 	}
 
 	/**
+	 * Returns the delegate object holding the action execution methods.
+	 * Defaults to this object.
+	 */
+	public Object getDelegate() {
+		return this;
+	}
+
+	/**
+	 * Get the strategy used to resolve action execution method names. Defaults
+	 * to current state id based resolution.
+	 */
+	public ActionExecuteMethodNameResolver getExecuteMethodNameResolver() {
+		return executeMethodNameResolver;
+	}
+
+	/**
 	 * Find the action execution method with given name on the delegate object
 	 * using reflection.
 	 */
-	protected Method getActionExecutionMethod(String actionExecutionMethodName) throws NoSuchMethodException,
-			IllegalAccessException {
-		return getDelegate().getClass().getMethod(actionExecutionMethodName, new Class[] { RequestContext.class });
+	protected Method getActionExecuteMethod(String actionExecutionMethodName) {
+		return (Method)this.executeMethodCache.get(actionExecutionMethodName);
 	}
 
+	/**
+	 * Strategy interface used by the MultiAction to map a request context to
+	 * the name of an action execution method.
+	 * 
+	 * @author Keith Donald
+	 * @author Erwin Vervaet
+	 */
+	public interface ActionExecuteMethodNameResolver {
+
+		/**
+		 * Resolve a method name from given flow execution request context.
+		 * @param context the flow execution request context
+		 * @return the name of the method that should handle action execution
+		 */
+		public String getMethodName(RequestContext context);
+	}
+
+	/**
+	 * Action execution method name resolver that uses the name of the current
+	 * state of the flow execution as a method name. This is the default method
+	 * name resolver used by the MultiAction class.
+	 * 
+	 * @author Erwin Vervaet
+	 */
+	public static class CurrentStateActionExecuteMethodNameResolver implements ActionExecuteMethodNameResolver {
+		public String getMethodName(RequestContext context) {
+			return context.getCurrentState().getId();
+		}
+	}
 }
