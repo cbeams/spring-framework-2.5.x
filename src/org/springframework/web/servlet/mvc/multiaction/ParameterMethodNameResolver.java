8b5f6ce55dcb29e14637080d4aa6df17e0836041
Juergen Hoeller <jhoeller@interface21.com>
2005-03-21 09:18:30 +0000

    renamed "paramNameList" property to "methodParamNames"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@5980 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/mvc/multiaction/ParameterMethodNameResolver.java
+++ src/org/springframework/web/servlet/mvc/multiaction/ParameterMethodNameResolver.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.web.servlet.mvc.multiaction;
 
@@ -21,59 +21,99 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * <p>Implementation of MethodNameResolver which supports several strategies for
- * mapping parameter values to the names of methods to invoke.</p>
+ * Implementation of MethodNameResolver which supports several strategies for
+ * mapping parameter values to the names of methods to invoke.
  * 
  * <p>The simplest strategy looks for a specific named parameter, whose value is
  * considered the name of the method to invoke. The name of the parameter may be
  * specified as a JavaBean property, if the default <code>action</code> is not
- * acceptable.</p>
+ * acceptable.
  * 
  * <p>The second strategy uses the very existence of a request parameter (i.e.
  * a request parameter with a certain name is found) as an indication that a 
  * method with the same name should be dispatched to. In this case, the actual
- * request parameter value is ignored</p>
+ * request parameter value is ignored.
  * 
  * <p>The second resolution strategy is prmarilly expected to be used with web
  * pages containing multiple submit buttons. The 'name' attribute of each
  * button should be set to the mapped method name, while the 'value' attribute
  * is normally displayed as the button label by the browser, and will be
- * ignored by the resolver.</p>
+ * ignored by the resolver.
  * 
  * <p>For use with either strategy, the name of a default handler method to use
- * when there is no match, can be specified as JavaBean properties.</p> 
+ * when there is no match, can be specified as JavaBean properties.
  * 
  * <p>For both resolution strategies, the method name is of course coming from
  * some sort of view code, (such as a JSP page). While this may be acceptable,
  * it is sometimes desireable to treat this only as a 'logical' method name,
- * with a further mapping to a 'real' method name. As such, an optional 'logical'
- * mapping may be specified for this purpose.<p>
+ * with a further mapping to a 'real' method name. As such, an optional
+ * 'logical' mapping may be specified for this purpose.
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @author Colin Sampaleanu
  * @see #setParamName
+ * @see #setMethodParamNames
+ * @see #setLogicalMappings
  * @see #setDefaultMethodName
- * @see #setParamNameList(String[])
  */
 public class ParameterMethodNameResolver implements MethodNameResolver {
 
+	/**
+	 * Default name for the parameter whose value identifies the method to invoke:
+	 * "action".
+	 */
 	public static final String DEFAULT_PARAM_NAME = "action";
+
 
 	private String paramName = DEFAULT_PARAM_NAME;
 
+	private String[] methodParamNames;
+
+	private Properties logicalMappings;
+
 	private String defaultMethodName;
-	
-	private String[] paramNameList = {};
-	
-	private Properties logicalMappings = new Properties();
+
 
 	/**
-	 * Set the parameter name we're looking for.
-	 * Default is "action".
+	 * Set the name of the parameter whose <i>value</o> identifies the name of
+	 * the method to invoke. Default is "action".
+	 * <p>Alternatively, specify parameter names where the very existence of each
+	 * parameter means that a method of the same name should be invoked, via
+	 * the "methodParamNames" property.
+	 * @see #setMethodParamNames
 	 */
 	public void setParamName(String paramName) {
 		this.paramName = paramName;
+	}
+
+	/**
+	 * Set a String array of parameter names, where the <i>very existence of a
+	 * parameter</i> in the list (with value ignored) means that a method of the
+	 * same name should be invoked. This target method name may then be optionally
+	 * further mapped via the {@link #logicalMappings} property, in which case it
+	 * can be considered a logical name only.
+	 * @see #setParamName
+	 */
+	public void setMethodParamNames(String[] methodParamNames) {
+		this.methodParamNames = methodParamNames;
+	}
+
+	/**
+	 * Specifies a set of optional logical method name mappings. For both resolution
+	 * strategies, the method name initially comes in from the view layer. If that needs
+	 * to be treated as a 'logical' method name, and mapped to a 'real' method name, then
+	 * a name/value pair for that purpose should be added to this Properties instance.
+	 * Any method name not found in this mapping will be considered to already be the
+	 * real method name.
+	 * <p>Note that in the case of no match, where the {@link #defaultMethodName} property
+	 * is used if available, that method name is considered to already be the real method
+	 * name, and is not run through the logical mapping.
+	 * @param logicalMappings a Properties object mapping logical method names to real
+	 * method names
+	 */
+	public void setLogicalMappings(Properties logicalMappings) {
+		this.logicalMappings = logicalMappings;
 	}
 
 	/**
@@ -83,66 +123,45 @@ public class ParameterMethodNameResolver implements MethodNameResolver {
 	public void setDefaultMethodName(String defaultMethodName) {
 		this.defaultMethodName = defaultMethodName;
 	}
-	
-	/**
-	 * <p>A list (as a String array) of parameter names, where the very existence of
-	 * each parameter in the list (with value ignored) means that a method of the
-	 * same name should be resolved. This target method name may then be optionally
-	 * further mapped via the {@link #logicalMappings} property, in which case it
-	 * can be considered a logical name only.</p>
-	 * 
-	 * <p>Note that the value of any parameter matching the {@link #paramName} property
-	 * will always be applied first if such a parameter exists, in which case this list
-	 * will not be consulted.</p>
-	 * 
-	 * @param an array of mappings
-	 */
-	public void setParamNameList(String[] mappings) {
-		this.paramNameList = mappings;
-	}
-	
-	/**
-	 * <p>Specifies a set of optional logical method name mappings. For both resolution
-	 * strategies, the method name initially comes in from the view layer. If that needs
-	 * to be treated as a 'logical' method name, and mapped to a 'real' method name, then
-	 * a name/value pair for that purpose should be added to this Properties instance.
-	 * Any method name not found in this mapping will be considered to already be the
-	 * real method name.</p>
-	 * 
-	 * <p>Note that in the case of no match, where the {@link #defaultMethodName} property
-	 * is used if available, that method name is considered to already be the real method
-	 * name, and is not run through the logical mapping.</p>
-	 * 
-	 * @param logicalMappings a Properties object mapping logical method names to real method
-	 * names. 
-	 */
-	public void setLogicalMappings(Properties logicalMappings) {
-		this.logicalMappings = logicalMappings;
-	}
+
 
 	public String getHandlerMethodName(HttpServletRequest request) throws NoSuchRequestHandlingMethodException {
-		String methodName = request.getParameter(this.paramName);
-		
-		if (methodName == null) {
-			for (int i = 0; i < paramNameList.length; ++i) {
-				String candidate = paramNameList[i];
+		String methodName = null;
+
+		// Check parameter whose value identifies the method to invoke, if any.
+		if (this.paramName != null) {
+			methodName = request.getParameter(this.paramName);
+		}
+
+		// Check parameter names where the very existence of each parameter
+		// means that a method of the same name should be invoked, if any.
+		if (this.methodParamNames != null) {
+			for (int i = 0; i < this.methodParamNames.length; ++i) {
+				String candidate = this.methodParamNames[i];
 				if (request.getParameter(candidate) != null) {
 					methodName = candidate;
 					break;
 				}
 			}
 		}
-		
+
 		if (methodName != null) {
-			methodName = logicalMappings.getProperty(methodName, methodName);
+			// Resolve logical name into real method name, if appropriate.
+			if (this.logicalMappings != null) {
+				methodName = this.logicalMappings.getProperty(methodName, methodName);
+			}
 		}
 		else {
+			// No specific method resolved: use default method.
 			methodName = this.defaultMethodName;
 		}
-		
+
+		// If resolution failed completely, throw an exception.
 		if (methodName == null) {
 			throw new NoSuchRequestHandlingMethodException(request);
 		}
+
 		return methodName;
 	}
+
 }
