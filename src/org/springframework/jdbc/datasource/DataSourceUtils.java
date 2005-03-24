3e9e1cb5acfb4a748e5b65a1e4fa3c7f5b9ee2d7
Juergen Hoeller <jhoeller@interface21.com>
2005-03-24 12:23:55 +0000

    fixed "connectionEquals" to explicitly check for identity too (for Connection handles that do not implement "equals" properly, such as the ones Commons DBCP exposes)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6104 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/DataSourceUtils.java
+++ src/org/springframework/jdbc/datasource/DataSourceUtils.java
@@ -287,7 +287,10 @@ public abstract class DataSourceUtils {
 	 * @see #getTargetConnection
 	 */
 	private static boolean connectionEquals(Connection heldCon, Connection passedInCon) {
-		return (heldCon.equals(passedInCon) || getTargetConnection(heldCon).equals(passedInCon));
+		// Explicitly check for identity too: for Connection handles that do not implement
+		// "equals" properly, such as the ones Commons DBCP exposes).
+		return (heldCon == passedInCon || heldCon.equals(passedInCon) ||
+				getTargetConnection(heldCon).equals(passedInCon));
 	}
 
 	/**
