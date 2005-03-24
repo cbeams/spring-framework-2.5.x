10c3cd9888aabc084013087f19019b2139d85c82
Juergen Hoeller <jhoeller@interface21.com>
2005-03-24 15:08:58 +0000

    reworked WeakReferenceMonitor and moved it to general util package
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6109 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/ejb/support/AbstractEnterpriseBean.java
+++ src/org/springframework/ejb/support/AbstractEnterpriseBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,23 +18,21 @@ package org.springframework.ejb.support;
 
 import javax.ejb.EnterpriseBean;
 
-import org.springframework.aop.util.WeakReferenceMonitor;
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.access.BeanFactoryLocator;
 import org.springframework.beans.factory.access.BeanFactoryReference;
 import org.springframework.context.access.ContextJndiBeanFactoryLocator;
+import org.springframework.util.WeakReferenceMonitor;
 
 /**
- * <p>
  * Superclass for all EJBs. Package-visible: not intended for direct subclassing. Provides
  * a standard way of loading a BeanFactory. Subclasses act as a facade, with the business
  * logic deferred to beans in the BeanFactory.
- * </p>
- * 
- * <p>
- * Default is to use a ContextJndiBeanFactoryLocator, which will initialize an XML
+ *
+ * <p>Default is to use a ContextJndiBeanFactoryLocator, which will initialize an XML
  * ApplicationContext from the classpath (based on a JNDI name specified). For a slightly
  * lighter weight implementation when ApplicationContext usage is not required,
  * setBeanFactoryLocator may be called (<i>before</i> your EJB's ejbCreate method is
@@ -44,13 +42,10 @@ import org.springframework.context.access.ContextJndiBeanFactoryLocator;
  * you may instead use a ContextSingletonBeanFactoryLocator. Alternately,
  * setBeanFactoryLocator may be called with a completely custom implementation of
  * BeanFactoryLocator.
- * </p>
- * 
- * <p>
- * Note that we cannot use final for our implementation of EJB lifecycle methods, as this
- * violates the EJB specification.
- * </p>
- * 
+ *
+ * <p>Note that we cannot use final for our implementation of EJB lifecycle methods,
+ * as this violates the EJB specification.
+ *
  * @author Rod Johnson
  * @author Colin Sampaleanu
  * @see #setBeanFactoryLocator
@@ -60,6 +55,7 @@ import org.springframework.context.access.ContextJndiBeanFactoryLocator;
 abstract class AbstractEnterpriseBean implements EnterpriseBean {
 
 	public static final String BEAN_FACTORY_PATH_ENVIRONMENT_KEY = "java:comp/env/ejb/BeanFactoryPath";
+
 
 	/**
 	 * Helper strategy that knows how to locate a Spring BeanFactory (or
@@ -73,18 +69,16 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 	/** Spring BeanFactory that provides the namespace for this EJB */
 	private BeanFactoryReference beanFactoryReference;
 
+
 	/**
 	 * Set the BeanFactoryLocator to use for this EJB. Default is a
 	 * ContextJndiBeanFactoryLocator.
-	 * <p>
-	 * Can be invoked before loadBeanFactory, for example in constructor or
+	 * <p>Can be invoked before loadBeanFactory, for example in constructor or
 	 * setSessionContext if you want to override the default locator.
-	 * <p>
-	 * Note that the BeanFactory is automatically loaded by the ejbCreate
+	 * <p>Note that the BeanFactory is automatically loaded by the ejbCreate
 	 * implementations of AbstractStatelessSessionBean and
 	 * AbstractMessageDriverBean but needs to be explicitly loaded in custom
 	 * AbstractStatefulSessionBean ejbCreate methods.
-	 * 
 	 * @see AbstractStatelessSessionBean#ejbCreate
 	 * @see AbstractMessageDrivenBean#ejbCreate
 	 * @see AbstractStatefulSessionBean#loadBeanFactory
@@ -97,14 +91,11 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 
 	/**
 	 * Set the bean factory locator key.
-	 * <p>
-	 * In case of the default BeanFactoryLocator implementation,
+	 * <p>In case of the default BeanFactoryLocator implementation,
 	 * ContextJndiBeanFactoryLocator, this is the JNDI path. The default value
 	 * of this property is "java:comp/env/ejb/BeanFactoryPath".
-	 * <p>
-	 * Can be invoked before loadBeanFactory, for example in constructor or
+	 * <p>Can be invoked before loadBeanFactory, for example in constructor or
 	 * setSessionContext if you want to override the default locator key.
-	 * 
 	 * @see #BEAN_FACTORY_PATH_ENVIRONMENT_KEY
 	 */
 	public void setBeanFactoryLocatorKey(String factoryKey) {
@@ -113,51 +104,44 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 
 	/**
 	 * Load a Spring BeanFactory namespace. Subclasses must invoke this method.
-	 * <p>
-	 * Package-visible as it shouldn't be called directly by user-created
+	 * <p>Package-visible as it shouldn't be called directly by user-created
 	 * subclasses.
-	 * 
 	 * @see org.springframework.ejb.support.AbstractStatelessSessionBean#ejbCreate()
 	 */
 	void loadBeanFactory() throws BeansException {
-		if (beanFactoryLocator == null) {
-			beanFactoryLocator = new ContextJndiBeanFactoryLocator();
+		if (this.beanFactoryLocator == null) {
+			this.beanFactoryLocator = new ContextJndiBeanFactoryLocator();
 		}
-		if (beanFactoryLocatorKey == null) {
-			beanFactoryLocatorKey = BEAN_FACTORY_PATH_ENVIRONMENT_KEY;
+		if (this.beanFactoryLocatorKey == null) {
+			this.beanFactoryLocatorKey = BEAN_FACTORY_PATH_ENVIRONMENT_KEY;
 		}
 
-		BeanFactoryReference targetBeanFactoryRef = beanFactoryLocator
-				.useBeanFactory(this.beanFactoryLocatorKey);
+		BeanFactoryReference targetBeanFactoryRef =
+				this.beanFactoryLocator.useBeanFactory(this.beanFactoryLocatorKey);
 
-		// we can not rely on the container to call ejbRemove() (it's skipped in
+		// Create handle to target object this handle will be returned to the client.
+		ProxyFactory pf = new ProxyFactory();
+		pf.addInterface(BeanFactoryReference.class);
+		pf.setTarget(targetBeanFactoryRef);
+		this.beanFactoryReference = (BeanFactoryReference) pf.getProxy();
+
+		// We can not rely on the container to call ejbRemove() (it's skipped in
 		// the case of system exceptions), so ensure the the bean factory
-		// reference is eventually released
-		this.beanFactoryReference = (BeanFactoryReference) WeakReferenceMonitor.monitor(
-				targetBeanFactoryRef, false, new WeakReferenceMonitor.ReleaseListener() {
-					public void notifyRelease(Object object) {
-						BeanFactoryReference bfr = (BeanFactoryReference) object;
-						// this may or may not actually do anything, depending
-						// on whether or not the container calls ejbRemove()
-						bfr.release();
-					}
-				});
+		// reference is eventually released.
+		WeakReferenceMonitor.monitor(this.beanFactoryReference,
+				new BeanFactoryReferenceReleaseListener(targetBeanFactoryRef));
 	}
 
 	/**
-	 * <p>
 	 * Unload the Spring BeanFactory instance. The default ejbRemove method
 	 * invokes this method, but subclasses which override ejbRemove must invoke
 	 * this method themselves.
-	 * </p>
-	 * <p>
-	 * Package-visible as it shouldn't be called directly by user-created
+	 * <p>Package-visible as it shouldn't be called directly by user-created
 	 * subclasses.
-	 * </p>
 	 */
 	void unloadBeanFactory() throws FatalBeanException {
-		// we will not ever get here if the container skips calling ejbRemove, but the
-		// WeakReferenceMonitor will still clean up (later) in that case
+		// We will not ever get here if the container skips calling ejbRemove, but the
+		// WeakReferenceMonitor will still clean up (later) in that case.
 		if (this.beanFactoryReference != null) {
 			this.beanFactoryReference.release();
 			this.beanFactoryReference = null;
@@ -166,7 +150,6 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 
 	/**
 	 * May be called after ejbCreate().
-	 * 
 	 * @return the bean factory
 	 */
 	protected BeanFactory getBeanFactory() {
@@ -176,10 +159,8 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 	/**
 	 * EJB lifecycle method, implemented to invoke onEjbRemote and unload the
 	 * BeanFactory afterwards.
-	 * <p>
-	 * Don't override it (although it can't be made final): code your shutdown
+	 * <p>Don't override it (although it can't be made final): code your shutdown
 	 * in onEjbRemove.
-	 * 
 	 * @see #onEjbRemove
 	 */
 	public void ejbRemove() {
@@ -191,12 +172,30 @@ abstract class AbstractEnterpriseBean implements EnterpriseBean {
 	 * Subclasses must implement this method to do any initialization they would
 	 * otherwise have done in an ejbRemove() method. The BeanFactory will be
 	 * unloaded afterwards.
-	 * <p>
-	 * This implementation is empty, to be overridden in subclasses. The same
-	 * restrictions apply to the work of this method as to an ejbRemove()
-	 * method.
+	 * <p>This implementation is empty, to be overridden in subclasses. The same
+	 * restrictions apply to the work of this method as to an ejbRemove() method.
 	 */
 	protected void onEjbRemove() {
 		// empty
 	}
+
+
+	/**
+	 * Implementation of WeakReferenceMonitor's ReleaseListener callback interface.
+	 * Release the given BeanFactoryReference if the monitor detects that there
+	 * are no strong references to the handle anymore.
+	 */
+	private static class BeanFactoryReferenceReleaseListener implements WeakReferenceMonitor.ReleaseListener {
+
+		private final BeanFactoryReference beanFactoryReference;
+
+		public BeanFactoryReferenceReleaseListener(BeanFactoryReference beanFactoryReference) {
+			this.beanFactoryReference = beanFactoryReference;
+		}
+
+		public void released() {
+			this.beanFactoryReference.release();
+		}
+	}
+
 }
