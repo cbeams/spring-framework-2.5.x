8106c3c7ae7efa3defe1f41eb5a168519dc100b7
Keith Donald <kdonald@interface21.com>
2005-03-27 05:44:21 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6155 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/webflow/birthdate/src/org/springframework/samples/birthdate/web/flow/action/BirthDate.java
+++ samples/webflow/birthdate/src/org/springframework/samples/birthdate/web/flow/action/BirthDate.java
@@ -3,22 +3,23 @@ package org.springframework.samples.birthdate.web.flow.action;
 import java.util.Date;
 
 public class BirthDate {
-	
+
 	private String name;
+
 	private Date date;
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Date getDate() {
 		return date;
 	}
-	
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
