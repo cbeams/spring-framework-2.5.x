8e04372365004f68f6545a9e1654bc345219a67a
Keith Donald <kdonald@interface21.com>
2005-03-29 21:10:44 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6230 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/ActionState.java
+++ sandbox/src/org/springframework/web/flow/ActionState.java
@@ -47,10 +47,10 @@ import org.springframework.util.Styler;
  * the action is not named, a transition for the base <code>success</code>
  * event will be searched, and if found, executed.
  * <p>
- * By default, the 'executeMethodProperty' used by the <code>MultiAction</code>
- * implementation to dispatch calls on a target action instance to a particular
- * handler method. In this case the value of the name property will map to the
- * method name on the target action instance.
+ * By default, the 'executeMethodProperty' is used by the
+ * <code>MultiAction</code> implementation to dispatch calls on a target
+ * action instance to a particular handler method. In this case the value of the
+ * name property will map to the method name on the target action instance.
  * 
  * @author Keith Donald
  * @author Erwin Vervaet
