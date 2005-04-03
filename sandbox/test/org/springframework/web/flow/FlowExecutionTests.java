397b55e4e74c8ba309db0de0538f51e95a8b76f5
Keith Donald <kdonald@interface21.com>
2005-04-03 06:32:15 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6336 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/test/org/springframework/web/flow/FlowExecutionTests.java
+++ sandbox/test/org/springframework/web/flow/FlowExecutionTests.java
@@ -1,11 +1,11 @@
 package org.springframework.web.flow;
 
+import junit.framework.TestCase;
+
 import org.springframework.web.flow.action.AbstractAction;
 import org.springframework.web.flow.config.AbstractFlowBuilder;
 import org.springframework.web.flow.config.FlowBuilderException;
 import org.springframework.web.flow.config.FlowFactoryBean;
-
-import junit.framework.TestCase;
 
 /**
  * General flow execution tests.
@@ -13,85 +13,76 @@ import junit.framework.TestCase;
  * @author Erwin Vervaet
  */
 public class FlowExecutionTests extends TestCase {
-	
+
 	public void testLoopInFlow() {
-		AbstractFlowBuilder builder=new AbstractFlowBuilder() {
+		AbstractFlowBuilder builder = new AbstractFlowBuilder() {
 			protected String flowId() {
 				return "flow";
 			}
-			
+
 			public void buildStates() throws FlowBuilderException {
-				addViewState(
-						"viewState",
-						"viewName",
-						new Transition[] { on(submit(), "viewState"), on(finish(), "endState") });
+				addViewState("viewState", "viewName", new Transition[] { on(submit(), "viewState"),
+						on(finish(), "endState") });
 				addEndState("endState");
 			}
 		};
-		FlowExecution flowExecution=new FlowFactoryBean(builder).getFlow().createExecution();
-		ViewDescriptor vd=flowExecution.start(new SimpleEvent(this, "start"));
+		FlowExecution flowExecution = new FlowFactoryBean(builder).getFlow().createExecution();
+		ViewDescriptor vd = flowExecution.start(new SimpleEvent(this, "start"));
 		assertNotNull(vd);
 		assertEquals("viewName", vd.getViewName());
-		for (int i=0; i<10; i++) {
-			vd=flowExecution.signalEvent(new SimpleEvent(this, "submit"));
+		for (int i = 0; i < 10; i++) {
+			vd = flowExecution.signalEvent(new SimpleEvent(this, "submit"));
 			assertNotNull(vd);
 			assertEquals("viewName", vd.getViewName());
 		}
 		assertTrue(flowExecution.isActive());
-		vd=flowExecution.signalEvent(new SimpleEvent(this, "finish"));
+		vd = flowExecution.signalEvent(new SimpleEvent(this, "finish"));
 		assertNull(vd);
 		assertFalse(flowExecution.isActive());
 	}
-	
+
 	public void testLoopInFlowWithSubFlow() {
-		AbstractFlowBuilder childBuilder=new AbstractFlowBuilder() {
+		AbstractFlowBuilder childBuilder = new AbstractFlowBuilder() {
 			protected String flowId() {
 				return "childFlow";
 			}
+
 			public void buildStates() throws FlowBuilderException {
-				addActionState(
-						"doOtherStuff",
-						new AbstractAction() {
-							private int executionCount=0;
-							
-							protected Event doExecuteAction(RequestContext context)	throws Exception {
-								executionCount++;
-								if (executionCount<2) {
-									return success();
-								}
-								return error();
-							}
-						},
-						new Transition[] { on(success(), finish()), on(error(), "stopTest")});
+				addActionState("doOtherStuff", new AbstractAction() {
+					private int executionCount = 0;
+
+					protected Event doExecuteAction(RequestContext context) throws Exception {
+						executionCount++;
+						if (executionCount < 2) {
+							return success();
+						}
+						return error();
+					}
+				}, new Transition[] { on(success(), finish()), on(error(), "stopTest") });
 				addEndState(finish());
 				addEndState("stopTest");
 			}
 		};
-		final Flow childFlow=new FlowFactoryBean(childBuilder).getFlow();
-		AbstractFlowBuilder parentBuilder=new AbstractFlowBuilder() {
+		final Flow childFlow = new FlowFactoryBean(childBuilder).getFlow();
+		AbstractFlowBuilder parentBuilder = new AbstractFlowBuilder() {
 			protected String flowId() {
 				return "parentFlow";
 			}
-			
+
 			public void buildStates() throws FlowBuilderException {
-				addActionState(
-						"doStuff",
-						new AbstractAction() {
-							protected Event doExecuteAction(RequestContext context) throws Exception {
-								return success();
-							}
-						},
-						on(success(), "startSubFlow"));
-				addSubFlowState(
-						"startSubFlow",
-						childFlow,
-						new Transition[] { on(finish(), "startSubFlow"), on("stopTest", "stopTest") });
+				addActionState("doStuff", new AbstractAction() {
+					protected Event doExecuteAction(RequestContext context) throws Exception {
+						return success();
+					}
+				}, on(success(), "startSubFlow"));
+				addSubFlowState("startSubFlow", childFlow, new Transition[] { on(finish(), "startSubFlow"),
+						on("stopTest", "stopTest") });
 				addEndState("stopTest");
 			}
 		};
-		Flow parentFlow=new FlowFactoryBean(parentBuilder).getFlow();
-		
-		FlowExecution flowExecution=parentFlow.createExecution();
+		Flow parentFlow = new FlowFactoryBean(parentBuilder).getFlow();
+
+		FlowExecution flowExecution = parentFlow.createExecution();
 		flowExecution.start(new SimpleEvent(this, "start"));
 		assertFalse(flowExecution.isActive());
 	}
