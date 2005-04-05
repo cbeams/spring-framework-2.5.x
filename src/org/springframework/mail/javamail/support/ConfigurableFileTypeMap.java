b2eb7ced4d7fb1c450696b2f19dfb07711c2332d
Juergen Hoeller <jhoeller@interface21.com>
2005-04-05 09:50:25 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6421 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/mail/javamail/support/ConfigurableFileTypeMap.java
+++ src/org/springframework/mail/javamail/support/ConfigurableFileTypeMap.java
@@ -19,9 +19,8 @@ package org.springframework.mail.javamail.support;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -33,44 +32,49 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 /**
- * <p>Spring-configurable <code>FileTypeMap</code> implementation that will read
+ * Spring-configurable <code>FileTypeMap</code> implementation that will read
  * MIME type to file extension mappings from a standard JavaMail MIME type
- * mapping file.</p>
+ * mapping file.
+ *
  * <p>The mapping file should be in the following format:
+ *
  * <pre>
  * # map text/html to .htm and .html files
- * text/html  htm html
- * </pre>
+ * text/html  htm html</pre>
+ *
  * Lines starting with <code>#</code> are treated as comments and are ignored. All
  * other lines are treated as mappings. Each mapping line should contain the MIME
  * type as the first entry and then each file extension to map to that MIME type
  * as subsequent entries. Each entry is separated by spaces or tabs.
- * </p>
- * <p>
- * By default, the mappings in the mime.types file located in the same package
- * as this class are used. This can be overriden using the
+ *
+ * <p>By default, the mappings in the mime.types file located in the same package
+ * as this class are used. This can be overridden using the
  * <code>mappingLocation</code> property.
- * </p>
- * <p>
- * Additional mappings can be added via the <code>mappings</code> property with
+ *
+ * <p>Additional mappings can be added via the <code>mappings</code> property with
  * each key of an entry being a file extension and the value of the entry
  * being the MIME type mapped to that file extension.
- * </p>
- * @see #setMappingLocation(org.springframework.core.io.Resource)
- * @see #setMappings(java.util.Properties)
+ *
+ * <p><b>NOTE:</b> This class will only work on JDK 1.4+, due to the use of the
+ * <code>java.util.regex</code> package.
+ *
  * @author Rob Harrop
+ * @since 1.2
+ * @see #setMappingLocation
+ * @see #setMappings
  */
 public class ConfigurableFileTypeMap extends FileTypeMap implements InitializingBean {
 
 	/**
 	 * The default MIME to use when no mapping can be found.
 	 */
-	private static final String DEFAULT_MIME_TYPE = "application/octet-stream";
+	public static final String DEFAULT_MIME_TYPE = "application/octet-stream";
 
 	/**
 	 * <code>Pattern</code> for matching an entry part in the mapping file.
 	 */
 	private static final Pattern PATTERN = Pattern.compile("([\\S]+)");
+
 
 	/**
 	 * The <code>Resource</code> to load the mapping file from.
@@ -81,46 +85,13 @@ public class ConfigurableFileTypeMap extends FileTypeMap implements Initializing
 	 * The final mappings merged from those in the mapping file and the entries in
 	 * <code>mappings</code>.
 	 */
-	private Map mergedMappings;
+	private Properties mergedMappings;
 
 	/**
 	 * Used to configure additional mappings.
 	 */
 	private Properties mappings;
 
-
-	/**
-	 * Gets the content type of the supplied <code>File</code> based on the extension.
-	 */
-	public String getContentType(File file) {
-		return getContentType(file.getName());
-	}
-
-	/**
-	 * Gets the content type of the file pointed to by the supplied <code>String</code>.
-	 */
-	public String getContentType(String fileName) {
-		String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-
-		String mimeType = (String) mergedMappings.get(extension);
-
-		if (mimeType == null) {
-			mimeType = DEFAULT_MIME_TYPE;
-		}
-
-		return mimeType;
-	}
-
-	/**
-	 * Creates the final merged mapping set.
-	 */
-	public void afterPropertiesSet() throws Exception {
-		mergedMappings = parseMappingFile(mappingLocation);
-
-		if (mappings != null) {
-			mergedMappings.putAll(mappings);
-		}
-	}
 
 	/**
 	 * Sets the <code>Resource</code> from which mappings are loaded.
@@ -138,48 +109,63 @@ public class ConfigurableFileTypeMap extends FileTypeMap implements Initializing
 	}
 
 	/**
-	 * Parses the supplied mapping data and puts the mappings into a <code>Map</code>
-	 * instance. Keys in the map correspond to file extensions and values to MIME
-	 * types.
-	 * @param mappingResource the <code>Resource</code> to load the mapping data from.
+	 * Creates the final merged mapping set.
 	 */
-	private Map parseMappingFile(Resource mappingResource) throws IOException {
-		BufferedReader rdr = null;
+	public void afterPropertiesSet() throws IOException {
+		this.mergedMappings = parseMappingFile(this.mappingLocation);
+		if (this.mappings != null) {
+			this.mergedMappings.putAll(this.mappings);
+		}
+	}
 
+
+	/**
+	 * Get the content type of the supplied <code>File</code> based on the extension.
+	 */
+	public String getContentType(File file) {
+		return getContentType(file.getName());
+	}
+
+	/**
+	 * Get the content type of the file pointed to by the supplied <code>String</code>.
+	 */
+	public String getContentType(String fileName) {
+		String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+		String mimeType = this.mergedMappings.getProperty(extension);
+		return (mimeType != null ? mimeType : DEFAULT_MIME_TYPE);
+	}
+
+	/**
+	 * Parses the supplied mapping data and puts the mappings into a <code>Properties</code>
+	 * instance. Keys in the map correspond to file extensions and values to MIME types.
+	 * @param mappingResource the <code>Resource</code> to load the mapping data from
+	 * @return the parsed MIME type mappings as <code>Properties</code> instance
+	 */
+	private Properties parseMappingFile(Resource mappingResource) throws IOException {
+		InputStream is = mappingResource.getInputStream();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 		try {
-			rdr = new BufferedReader(new InputStreamReader(mappingResource.getInputStream()));
-
-			Map mimeTypes = new HashMap();
+			Properties mimeTypes = new Properties();
 			String line = null;
-
-			while ((line = rdr.readLine()) != null) {
-
-				// skip empty lines and comments
+			while ((line = reader.readLine()) != null) {
+				// Skip empty lines and comments.
 				if (line.length() > 0 && line.charAt(0) == '#') {
 					continue;
 				}
-
-				Matcher m = PATTERN.matcher(line);
-
-				// read the first match which is the
-				// content type
-				if (m.find()) {
-					String mimeType = m.group();
-
-					// read each subsequent match
-					// which is a file extension
-					while (m.find()) {
-						mimeTypes.put(m.group(), mimeType);
+				Matcher matcher = PATTERN.matcher(line);
+				// Read the first match which is the content type.
+				if (matcher.find()) {
+					String mimeType = matcher.group();
+					// Read each subsequent match which is a file extension.
+					while (matcher.find()) {
+						mimeTypes.put(matcher.group(), mimeType);
 					}
 				}
 			}
-
 			return mimeTypes;
 		}
 		finally {
-			if (rdr != null) {
-				rdr.close();
-			}
+			reader.close();
 		}
 	}
 
