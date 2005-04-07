18a3b99007154dcc32296ffb79e6ecd516e25ee2
Rob Harrop <rharrop@interface21.com>
2005-04-07 19:05:14 +0000

    Updated JavaDocs on OgnlTransitionCriteriaCreator
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6479 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/support/OgnlTransitionCriteriaCreator.java
+++ sandbox/src/org/springframework/web/flow/support/OgnlTransitionCriteriaCreator.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2002-2005 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.springframework.web.flow.support;
 
@@ -12,10 +27,25 @@ import org.springframework.web.flow.config.FlowBuilderException;
 import org.springframework.web.flow.config.SimpleTransitionCriteriaCreator;
 
 /**
- * @author robh
+ * When presented with expressions in the format <code>${expr}</code>, creates instances
+ * of <code>OgnlTransitionCriteria</code> that will evaluate <code>expr</code> against
+ * the <code>RequestContext</code> using OGNL during <code>test(RequestContext)</code>.
+ *
+ * If the <code>on</code> value passed to <code>create(String)</code> is not an expression
+ * this class delegates to the super class to get an instance of the default
+ * <code>TransitionCriteria</code> for the event name.
+ * @author Rob Harrop
+ * @see OgnlTransitionCriteria
+ * @see SimpleTransitionCriteriaCreator
  */
 public class OgnlTransitionCriteriaCreator extends SimpleTransitionCriteriaCreator {
 
+	/**
+	 * If the supplied <code>on</code> value is an expression, then an instance
+	 * of <code>OgnlTransitionCriteria</code> is created. Otherwise, the super
+	 * class is asked to provide the default implementation of <code>TransitionCriteria</code>.
+	 * @param on an event name or expression
+	 */
 	public TransitionCriteria create(String on) {
 
 		if (isExpression(on)) {
