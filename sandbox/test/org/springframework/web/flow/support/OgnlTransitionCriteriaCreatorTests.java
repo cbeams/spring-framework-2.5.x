7a31393d2fca8ba6f083628b079bbc6131fc17a4
Keith Donald <kdonald@interface21.com>
2005-04-12 14:53:43 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6580 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ sandbox/test/org/springframework/web/flow/support/OgnlTransitionCriteriaCreatorTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.web.flow.support;
+
+import junit.framework.TestCase;
+
+import org.springframework.mock.web.flow.MockRequestContext;
+import org.springframework.web.flow.RequestContext;
+
+/**
+ * @author robh
+ */
+public class OgnlTransitionCriteriaCreatorTests extends TestCase {
+
+	public void testTrueEvaluation() throws Exception {
+		String expr = "${foo}";
+		String expression = "flowScope.foo == 'bar'";
+		OgnlTransitionCriteriaCreator.OgnlTransitionCriteria criterion = new OgnlTransitionCriteriaCreator.OgnlTransitionCriteria(
+				expression);
+		RequestContext ctx = getRequestContext();
+		assertTrue("Criterion should evaluate to true", criterion.test(ctx));
+
+	}
+
+	public void testFalseEvaluation() throws Exception {
+		String expression = "flowScope.foo != 'bar'";
+		OgnlTransitionCriteriaCreator.OgnlTransitionCriteria criterion = new OgnlTransitionCriteriaCreator.OgnlTransitionCriteria(
+				expression);
+		RequestContext ctx = getRequestContext();
+		assertFalse("Criterion should evaluate to false", criterion.test(ctx));
+	}
+
+	public void testNonBooleanEvaluation() throws Exception {
+		String expression = "flowScope.foo";
+		OgnlTransitionCriteriaCreator.OgnlTransitionCriteria criterion = new OgnlTransitionCriteriaCreator.OgnlTransitionCriteria(
+				expression);
+		RequestContext ctx = getRequestContext();
+
+		try {
+			criterion.test(ctx);
+			fail("Non-boolean evaluations are not allowed");
+		}
+		catch (IllegalArgumentException e) {
+			// success
+		}
+	}
+
+	public void testInvalidSyntax() throws Exception {
+		try {
+			new OgnlTransitionCriteriaCreator.OgnlTransitionCriteria("&foo<<m");
+			fail("Syntax error should throw IllegalArgumentException");
+		}
+		catch (IllegalArgumentException ex) {
+			// success
+		}
+	}
+
+	private RequestContext getRequestContext() {
+		RequestContext ctx = new MockRequestContext();
+		ctx.getFlowScope().setAttribute("foo", "bar");
+		return ctx;
+	}
+}
