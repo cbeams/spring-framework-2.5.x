e681c9ec1e4349c1ae20e406d56b94a2b2a26b28
Juergen Hoeller <jhoeller@interface21.com>
2005-04-15 09:28:00 +0000

    added "commandName" property and "createBinder" and "initBinder" callbacks
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6618 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/mvc/throwaway/ThrowawayControllerHandlerAdapter.java
+++ src/org/springframework/web/servlet/mvc/throwaway/ThrowawayControllerHandlerAdapter.java
@@ -28,26 +28,103 @@ import org.springframework.web.servlet.ModelAndView;
  * generic DispatcherServlet. Does not support last-modified checks.
  *
  * <p>This is an SPI class, not used directly by application code.
+ * It can be explicitly configured in a DispatcherServlet context, to use a
+ * customized version instead of the default ThrowawayControllerHandlerAdapter.
  *
  * @author Juergen Hoeller
  * @since 08.12.2003
  */
 public class ThrowawayControllerHandlerAdapter implements HandlerAdapter {
 
-	public static final String THROWAWAY_CONTROLLER_NAME = "throwawayController";
+	public static final String DEFAULT_COMMAND_NAME = "throwawayController";
+
+	private String commandName = DEFAULT_COMMAND_NAME;
+
+
+	/**
+	 * Set the name of the command in the model.
+	 * The command object will be included in the model under this name.
+	 */
+	public final void setCommandName(String commandName) {
+		this.commandName = commandName;
+	}
+
+	/**
+	 * Return the name of the command in the model.
+	 */
+	public final String getCommandName() {
+		return this.commandName;
+	}
+
 
 	public boolean supports(Object handler) {
 		return (handler instanceof ThrowawayController);
 	}
 
-	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+	/**
+	 * This implementation binds request parameters to the ThrowawayController
+	 * instance and then calls <code>execute</code> on it.
+	 * @see #createBinder
+	 * @see ThrowawayController#execute
+	 */
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
 		ThrowawayController throwaway = (ThrowawayController) handler;
-		ServletRequestDataBinder binder = new ServletRequestDataBinder(throwaway, THROWAWAY_CONTROLLER_NAME);
+
+		ServletRequestDataBinder binder = createBinder(request, throwaway);
 		binder.bind(request);
 		binder.closeNoCatch();
+
 		return throwaway.execute();
 	}
 
+	/**
+	 * Create a new binder instance for the given command and request.
+	 * <p>Called by <code>bindAndValidate</code>. Can be overridden to plug in
+	 * custom ServletRequestDataBinder subclasses.
+	 * <p>Default implementation creates a standard ServletRequestDataBinder,
+	 * sets the specified MessageCodesResolver (if any), and invokes initBinder.
+	 * Note that <code>initBinder</code> will not be invoked if you override this method!
+	 * @param request current HTTP request
+	 * @param command the command to bind onto
+	 * @return the new binder instance
+	 * @throws Exception in case of invalid state or arguments
+	 * @see #initBinder
+	 * @see #getCommandName
+	 */
+	protected ServletRequestDataBinder createBinder(HttpServletRequest request, ThrowawayController command)
+	    throws Exception {
+
+		ServletRequestDataBinder binder = new ServletRequestDataBinder(command, getCommandName());
+		initBinder(request, binder);
+		return binder;
+	}
+
+	/**
+	 * Initialize the given binder instance, for example with custom editors.
+	 * Called by <code>createBinder</code>.
+	 * <p>This method allows you to register custom editors for certain fields of your
+	 * command class. For instance, you will be able to transform Date objects into a
+	 * String pattern and back, in order to allow your JavaBeans to have Date properties
+	 * and still be able to set and display them in an HTML interface.
+	 * <p>Default implementation is empty.
+	 * @param request current HTTP request
+	 * @param binder new binder instance
+	 * @throws Exception in case of invalid state or arguments
+	 * @see #createBinder
+	 * @see org.springframework.validation.DataBinder#registerCustomEditor
+	 * @see org.springframework.beans.propertyeditors.CustomDateEditor
+	 */
+	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder)
+	    throws Exception {
+	}
+
+
+	/**
+	 * This implementation always returns -1, as last-modified checks are not supported.
+	 */
 	public long getLastModified(HttpServletRequest request, Object handler) {
 		return -1;
 	}
