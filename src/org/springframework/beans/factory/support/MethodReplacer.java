4e3924ff0cd91934acf93ce29fb90c6ae13e3469
Rod Johnson <rjohnson@interface21.com>
2005-04-15 09:00:48 +0000

    Improved Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6615 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/MethodReplacer.java
+++ src/org/springframework/beans/factory/support/MethodReplacer.java
@@ -19,8 +19,12 @@ package org.springframework.beans.factory.support;
 import java.lang.reflect.Method;
 
 /**
- * Interface to be implemented by classes than can override
- * any method on an IoC-managed object.
+ * Interface to be implemented by classes that can override
+ * any method on an IoC-managed object: the <b>Method Injection</b>
+ * form of Dependency Injection. Such methods may be--
+ * but need not be--abstract, in which case the container will
+ * create a concrete subclass to instantiate.
+ * @since 1.1
  * @author Rod Johnson
  */
 public interface MethodReplacer {
@@ -29,7 +33,7 @@ public interface MethodReplacer {
 	 * Reimplement the given method.
 	 * @param o instance we're reimplementing the method for
 	 * @param m method to reimplement
-	 * @param args arguments
+	 * @param args arguments to the method
 	 * @return return value for the method
 	 */
 	Object reimplement(Object o, Method m, Object[] args) throws Throwable;
