af78fc40d414c6238500e4c79d56bfbd49cfa15a
Keith Donald <kdonald@interface21.com>
2005-04-17 01:29:06 +0000

    collapsed package for simplicity
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@6663 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ samples/webflow/birthdate/src/org/springframework/samples/birthdate/BirthDateValidator.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2002-2004 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.samples.birthdate;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class BirthDateValidator implements Validator {
+
+	public boolean supports(Class clazz) {
+		return clazz.equals(BirthDate.class);
+	}
+
+	public void validate(Object obj, Errors errors) {
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "noName", "Please specify your name.");
+		ValidationUtils.rejectIfEmpty(errors, "date", "noDate", "Please speicfy your birth date.");
+	}
+
+}
