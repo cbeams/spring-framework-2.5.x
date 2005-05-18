cc9083236108c7e429be32ba5efe87ec5fb49fd4
Erwin Vervaet <klr8@ervacon.com>
2005-05-18 10:31:37 +0000

    Code review.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7112 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/web/flow/action/portlet/SetPortletModeAction.java
+++ sandbox/src/org/springframework/web/flow/action/portlet/SetPortletModeAction.java
@@ -50,6 +50,7 @@ public class SetPortletModeAction extends AbstractAction {
 	 */
 	public static final String PORTLET_MODE_PROPERTY = "portletMode";
 	
+	
 	private PortletMode portletMode;
 	
 	/**
@@ -102,7 +103,8 @@ public class SetPortletModeAction extends AbstractAction {
 			// portlet mode and the window state can be changed through
 			// ActionResponse only, if this is not the case, it means that this
 			// action has been invoked directly in a RenderRequest
-			throw new ActionExecutionException(context.getFlowContext().getCurrentState(), this, context.getProperties(),
+			throw new ActionExecutionException(
+					context.getFlowContext().getCurrentState(), this, context.getProperties(),
 					"SetPortletModeAction can only work with 'ActionResponse' " +
 					"-- make sure you are not invoking it in a RenderRequest", null);
 		}
