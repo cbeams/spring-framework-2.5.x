51eddd477f67afb7e020111494416c039c27312d
Keith Donald <kdonald@interface21.com>
2005-05-22 07:25:46 +0000

    *** empty log message ***
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7195 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/test/org/springframework/web/flow/config/TextToViewDescriptorCreatorTests.java
+++ sandbox/test/org/springframework/web/flow/config/TextToViewDescriptorCreatorTests.java
@@ -3,12 +3,17 @@ package org.springframework.web.flow.config;
 import junit.framework.TestCase;
 
 import org.springframework.mock.web.flow.MockRequestContext;
+import org.springframework.test.JUnitAssertSupport;
 import org.springframework.web.flow.Event;
 import org.springframework.web.flow.RequestContext;
 import org.springframework.web.flow.ViewDescriptor;
 import org.springframework.web.flow.ViewDescriptorCreator;
 
 public class TextToViewDescriptorCreatorTests extends TestCase {
+	protected JUnitAssertSupport asserts() {
+		return new JUnitAssertSupport();
+	}
+	
 	public void testStaticView() {
 		TextToViewDescriptorCreator converter = new TextToViewDescriptorCreator();
 		ViewDescriptorCreator creator = (ViewDescriptorCreator)converter.convert("myView");
@@ -25,6 +30,8 @@ public class TextToViewDescriptorCreatorTests extends TestCase {
 		ViewDescriptor view = creator.createViewDescriptor(context);
 		assertEquals("myView", view.getViewName());
 		assertEquals(2, view.getModel().size());
+		asserts().assertAttributeEquals(view, "foo", "bar");
+		asserts().assertAttributeEquals(view, "bar", "mit");
 	}
 	
 	public void testCustom() {
