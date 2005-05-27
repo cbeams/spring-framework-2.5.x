19b641611fc6b38b9629f76b8e59ad1663183bc9
Juergen Hoeller <jhoeller@interface21.com>
2005-05-27 12:04:50 +0000

    added javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7281 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/petclinic/src/org/springframework/samples/petclinic/jdbc/CachingClinic.java
+++ samples/petclinic/src/org/springframework/samples/petclinic/jdbc/CachingClinic.java
@@ -1,10 +1,19 @@
-
 package org.springframework.samples.petclinic.jdbc;
 
 /**
+ * Interface that defines a cache refresh operation.
+ * To be exposed for management via JMX.
  * @author Rob Harrop
+ * @since 1.2
+ * @see AbstractJdbcClinic
  */
 public interface CachingClinic {
 
+	/**
+	 * Refresh the cache of Vets that the Clinic is holding.
+	 * @see org.springframework.samples.petclinic.Clinic#getVets()
+	 * @see AbstractJdbcClinic#refreshVetsCache()
+	 */
 	void refreshVetsCache();
+
 }
