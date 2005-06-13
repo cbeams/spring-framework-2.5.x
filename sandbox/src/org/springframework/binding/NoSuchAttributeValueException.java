75d58e9331600130e7df614c1eb7cf8d025f8b9b
Keith Donald <kdonald@interface21.com>
2005-06-13 06:01:51 +0000

    binding, webflow are their own modules
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7410 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/binding/NoSuchAttributeValueException.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.binding;
-
-import org.springframework.core.NestedRuntimeException;
-
-public class NoSuchAttributeValueException extends NestedRuntimeException {
-	public NoSuchAttributeValueException(String placeholder) {
-		super("No such attribute value to fill placeholder '" + placeholder + "'");
-	}
-}
