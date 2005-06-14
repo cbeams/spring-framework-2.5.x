6af62ee125768f1af91ae69a861aca300a849fd8
Juergen Hoeller <jhoeller@interface21.com>
2005-06-14 19:28:41 +0000

    renamed ReflectiveVisitorSupport into ReflectiveVisitorHelper
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7435 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/core/ReflectiveVisitorHelperTests.java
+++ test/org/springframework/core/ReflectiveVisitorHelperTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.core;
 
 import java.util.AbstractList;
@@ -9,16 +25,14 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 /**
- * Tests the various functionality of the ReflectiveVisitorSupport class.
- * 
- * @author R.J. Lorimer
+ * @author Keith Donald
  */
-public class ReflectiveVisitorSupportTests extends TestCase {
+public class ReflectiveVisitorHelperTests extends TestCase {
 
-	private ReflectiveVisitorSupport support;
+	private ReflectiveVisitorHelper support;
 
 	public void setUp() {
-		support = new ReflectiveVisitorSupport();
+		support = new ReflectiveVisitorHelper();
 	}
 
 	public void testDirectDefaultLookup() {
@@ -150,4 +164,5 @@ public class ReflectiveVisitorSupportTests extends TestCase {
 			visited = true;
 		}
 	}
+
 }
