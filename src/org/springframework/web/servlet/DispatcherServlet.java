73a611dba88725c9ab9aa920f812d31fdd343bda
Juergen Hoeller <jhoeller@interface21.com>
2005-06-14 21:10:44 +0000

    throw ServletException in case of Error during request processing, rather than the Error itself
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7439 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/DispatcherServlet.java
+++ src/org/springframework/web/servlet/DispatcherServlet.java
@@ -721,11 +721,11 @@ public class DispatcherServlet extends FrameworkServlet {
 			throw ex;
 		}
 		catch (Error err) {
-			// Trigger after-completion for thrown error (rare case).
-			// Better trigger callback without exception argument rather than no callback at all;
-			// after-completion callbacks offen perform finally-style cleanup.
-			triggerAfterCompletion(mappedHandler, interceptorIndex, processedRequest, response, null);
-			throw err;
+			ServletException ex =
+					new ServletException("Error occured during request processing: " + err.getMessage(), err);
+			// Trigger after-completion for thrown exception.
+			triggerAfterCompletion(mappedHandler, interceptorIndex, processedRequest, response, ex);
+			throw ex;
 		}
 
 		finally {
