c69027b7512ed2b6112395b7ea7c491ae3dbc040
Rob Harrop <rharrop@interface21.com>
2005-06-20 20:39:43 +0000

    Added aj5 pointcut tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7480 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ sandbox/test/org/springframework/aop/support/aspectj/AspectJExpressionPointcutAdvisorTests.java
@@ -0,0 +1,39 @@
+
+package org.springframework.aop.support.aspectj;
+
+import org.springframework.beans.ITestBean;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+
+/**
+ * @author robh
+ */
+public class AspectJExpressionPointcutAdvisorTests extends AbstractDependencyInjectionSpringContextTests {
+
+	private ITestBean testBean;
+
+	private CallCountingInterceptor interceptor;
+
+	public void setTestBean(ITestBean testBean) {
+		this.testBean = testBean;
+	}
+
+	public void setInterceptor(CallCountingInterceptor interceptor) {
+		this.interceptor = interceptor;
+	}
+
+	protected void onSetUp() throws Exception {
+		interceptor.reset();
+	}
+
+	public void testPoincutting() throws Exception {
+		assertEquals("Count should be 0", 0, interceptor.getCount());
+		testBean.getAge();
+		assertEquals("Count should be 1", 1, interceptor.getCount());
+		testBean.setAge(90);
+		assertEquals("Count should be 1", 1, interceptor.getCount());
+	}
+
+	protected String[] getConfigLocations() {
+		return new String[]{"org/springframework/aop/support/aspectj/aspectj.xml"};
+	}
+}
