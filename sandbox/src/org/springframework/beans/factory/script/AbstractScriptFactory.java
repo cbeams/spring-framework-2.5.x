3587106dc702142f01d9f032ef73144720a1d6cd
Rob Harrop <rharrop@interface21.com>
2005-06-23 12:14:43 +0000

    Updated bytecode proxy bits
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7502 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- sandbox/src/org/springframework/beans/factory/script/AbstractScriptFactory.java
+++ sandbox/src/org/springframework/beans/factory/script/AbstractScriptFactory.java
@@ -19,7 +19,6 @@ package org.springframework.beans.factory.script;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.sf.cglib.asm.Type;
 import net.sf.cglib.core.Signature;
 import net.sf.cglib.proxy.InterfaceMaker;
 
@@ -36,6 +35,8 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.io.ResourceLoader;
+
+import org.objectweb.asm.Type;
 
 /**
  * This superclass has the following responsibilities:
