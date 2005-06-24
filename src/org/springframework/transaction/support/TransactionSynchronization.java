9b70a4bff8be7a8e3c18621827fe3b58fc073ad4
Juergen Hoeller <jhoeller@interface21.com>
2005-06-24 15:25:31 +0000

    added further clarification of callback semantics
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7510 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/support/TransactionSynchronization.java
+++ src/org/springframework/transaction/support/TransactionSynchronization.java
@@ -62,7 +62,12 @@ public interface TransactionSynchronization {
 
 	/**
 	 * Invoked before transaction commit (before "beforeCompletion").
-	 * Can e.g. flush transactional sessions to the database.
+	 * Can e.g. flush transactional O/R Mapping sessions to the database.
+	 * <p>This callback does <i>not</i> mean that the transaction will actually
+	 * be committed. A rollback decision can still occur after this method has
+	 * been called. This callback is rather meant to perform work that's only
+	 * relevant if a commit still has a chance to happen, such as flushing SQL
+	 * statements to the database.
 	 * <p>Note that exceptions will get propagated to the commit caller
 	 * and cause a rollback of the transaction.
 	 * @param readOnly if the transaction is defined as read-only transaction 
@@ -73,8 +78,11 @@ public interface TransactionSynchronization {
 	void beforeCommit(boolean readOnly);
 
 	/**
-	 * Invoked before transaction commit/rollback (after "beforeCommit", even
-	 * if beforeCommit threw an exception). Can e.g. perform resource cleanup.
+	 * Invoked before transaction commit/rollback.
+	 * Can e.g. perform resource cleanup, <i>before</i> transaction completion.
+	 * <p>This method will be invoked after <code>beforeCommit</code>, even
+	 * if <code>beforeCommit</code> threw an exception). This callbacks allows
+	 * for closing resources before transaction completion, for any outcome.
 	 * @throws RuntimeException in case of errors; will be logged
 	 * (note: do not throw TransactionException subclasses here!)
 	 * @see #beforeCommit
@@ -83,8 +91,8 @@ public interface TransactionSynchronization {
 	void beforeCompletion();
 
 	/**
-	 * Invoked after transaction commit/rollback. Can e.g. perform resource
-	 * cleanup, in this case after transaction completion.
+	 * Invoked after transaction commit/rollback.
+	 * Can e.g. perform resource cleanup, <i>after</i> transaction completion.
 	 * @param status completion status according to the STATUS_* constants
 	 * @throws RuntimeException in case of errors; will be logged
 	 * (note: do not throw TransactionException subclasses here!)
