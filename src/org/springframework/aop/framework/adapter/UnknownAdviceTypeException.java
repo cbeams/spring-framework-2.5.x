68fa651e5f19daa482c35be04a01de9bdcc1b72b
Juergen Hoeller <jhoeller@interface21.com>
2005-06-24 17:22:16 +0000

    improved default error message, added overloaded constructor with arbitrary message
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7513 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/adapter/UnknownAdviceTypeException.java
+++ src/org/springframework/aop/framework/adapter/UnknownAdviceTypeException.java
@@ -21,11 +21,29 @@ package org.springframework.aop.framework.adapter;
  * Advisor or Advice type.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @see org.aopalliance.aop.Advice
+ * @see org.springframework.aop.Advisor
  */
 public class UnknownAdviceTypeException extends IllegalArgumentException {
-	
+
+	/**
+	 * Create a new UnknownAdviceTypeException for the given advice object.
+	 * Will create a message text that says that the object is neither a
+	 * subinterface of Advice nor an Advisor.
+	 * @param advice the advice object of unknown type
+	 */
 	public UnknownAdviceTypeException(Object advice) {
-		super("No adapter for Advice " + (advice != null ? "of class [" + advice.getClass().getName() + "]" : "[null]"));
+		super("Advice object [" + advice + "] is neither a supported subinterface of " +
+				"[org.aopalliance.aop.Advice] nor an [org.springframework.aop.Advisor]");
+	}
+
+	/**
+	 * Create a new UnknownAdviceTypeException with the given message.
+	 * @param message the message text
+	 */
+	public UnknownAdviceTypeException(String message) {
+		super(message);
 	}
 
 }
