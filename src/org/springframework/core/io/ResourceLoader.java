3377d9a2d1b57f4e59390e3cd7006e8b4353b95c
Juergen Hoeller <jhoeller@interface21.com>
2005-07-20 08:05:28 +0000

    refer to CLASSPATH_URL_PREFIX defined in util.ResourceUtils
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7696 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/io/ResourceLoader.java
+++ src/org/springframework/core/io/ResourceLoader.java
@@ -16,6 +16,8 @@
 
 package org.springframework.core.io;
 
+import org.springframework.util.ResourceUtils;
+
 /**
  * Interface to be implemented by objects that can load resources.
  * An ApplicationContext is required to provide this functionality,
@@ -39,7 +41,7 @@ package org.springframework.core.io;
 public interface ResourceLoader {
 
 	/** Pseudo URL prefix for loading from the class path: "classpath:" */
-	String CLASSPATH_URL_PREFIX = "classpath:";
+	String CLASSPATH_URL_PREFIX = ResourceUtils.CLASSPATH_URL_PREFIX;
 
 	/**
 	 * Return a Resource handle for the specified resource.
