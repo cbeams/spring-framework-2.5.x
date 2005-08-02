233453112151ec59cca197f891a21de86270db19
Juergen Hoeller <jhoeller@interface21.com>
2005-08-02 11:14:14 +0000

    apply context's class loader, if any, as bean class loader to the XmlBeanDefinitionReader
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7792 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/support/XmlWebApplicationContext.java
+++ src/org/springframework/web/context/support/XmlWebApplicationContext.java
@@ -77,9 +77,19 @@ public class XmlWebApplicationContext extends AbstractRefreshableWebApplicationC
 	 * @see #loadBeanDefinitions
 	 */
 	protected void loadBeanDefinitions(DefaultListableBeanFactory beanFactory) throws IOException {
+		// Create a new XmlBeanDefinitionReader for the given BeanFactory.
 		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+		// Configure the bean definition reader with this context's
+		// resource loading environment.
 		beanDefinitionReader.setResourceLoader(this);
+		if (getClassLoader() != null) {
+			beanDefinitionReader.setBeanClassLoader(getClassLoader());
+		}
 		beanDefinitionReader.setEntityResolver(new ResourceEntityResolver(this));
+
+		// Allow a subclass to provide custom initialization of the reader,
+		// then proceed with actually loading the bean definitions.
 		initBeanDefinitionReader(beanDefinitionReader);
 		loadBeanDefinitions(beanDefinitionReader);
 	}
