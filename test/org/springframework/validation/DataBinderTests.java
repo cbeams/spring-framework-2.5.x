8c32cb0050eae6e39f184f149a64914bc6507ad2
Juergen Hoeller <jhoeller@interface21.com>
2005-08-06 20:34:55 +0000

    check custom editors and takes registered type as hint for expected property type
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7844 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/validation/DataBinderTests.java
+++ test/org/springframework/validation/DataBinderTests.java
@@ -723,6 +723,62 @@ public class DataBinderTests extends TestCase {
 		assertEquals("NOT_NULL", errors.getFieldError("map[key0]").getCodes()[4]);
 	}
 
+	public void testDirectBindingToEmptyIndexedFieldWithRegisteredSpecificEditor() {
+		IndexedTestBean tb = new IndexedTestBean();
+		DataBinder binder = new DataBinder(tb, "tb");
+		binder.registerCustomEditor(TestBean.class, "map[key0]", new PropertyEditorSupport() {
+			public void setAsText(String text) throws IllegalArgumentException {
+				DerivedTestBean tb = new DerivedTestBean();
+				tb.setName("array" + text);
+				setValue(tb);
+			}
+			public String getAsText() {
+				return ((TestBean) getValue()).getName();
+			}
+		});
+		Errors errors = binder.getErrors();
+		errors.rejectValue("map[key0]", "NOT_NULL", "should not be null");
+
+		assertEquals(1, errors.getFieldErrorCount("map[key0]"));
+		assertEquals("NOT_NULL", errors.getFieldError("map[key0]").getCode());
+		assertEquals("NOT_NULL.tb.map[key0]", errors.getFieldError("map[key0]").getCodes()[0]);
+		assertEquals("NOT_NULL.tb.map", errors.getFieldError("map[key0]").getCodes()[1]);
+		assertEquals("NOT_NULL.map[key0]", errors.getFieldError("map[key0]").getCodes()[2]);
+		assertEquals("NOT_NULL.map", errors.getFieldError("map[key0]").getCodes()[3]);
+		// This next code is only generated because of the registered editor, using the
+		// registered type of the editor as guess for the content type of the collection.
+		assertEquals("NOT_NULL.org.springframework.beans.TestBean", errors.getFieldError("map[key0]").getCodes()[4]);
+		assertEquals("NOT_NULL", errors.getFieldError("map[key0]").getCodes()[5]);
+	}
+
+	public void testDirectBindingToEmptyIndexedFieldWithRegisteredGenericEditor() {
+		IndexedTestBean tb = new IndexedTestBean();
+		DataBinder binder = new DataBinder(tb, "tb");
+		binder.registerCustomEditor(TestBean.class, "map", new PropertyEditorSupport() {
+			public void setAsText(String text) throws IllegalArgumentException {
+				DerivedTestBean tb = new DerivedTestBean();
+				tb.setName("array" + text);
+				setValue(tb);
+			}
+			public String getAsText() {
+				return ((TestBean) getValue()).getName();
+			}
+		});
+		Errors errors = binder.getErrors();
+		errors.rejectValue("map[key0]", "NOT_NULL", "should not be null");
+
+		assertEquals(1, errors.getFieldErrorCount("map[key0]"));
+		assertEquals("NOT_NULL", errors.getFieldError("map[key0]").getCode());
+		assertEquals("NOT_NULL.tb.map[key0]", errors.getFieldError("map[key0]").getCodes()[0]);
+		assertEquals("NOT_NULL.tb.map", errors.getFieldError("map[key0]").getCodes()[1]);
+		assertEquals("NOT_NULL.map[key0]", errors.getFieldError("map[key0]").getCodes()[2]);
+		assertEquals("NOT_NULL.map", errors.getFieldError("map[key0]").getCodes()[3]);
+		// This next code is only generated because of the registered editor, using the
+		// registered type of the editor as guess for the content type of the collection.
+		assertEquals("NOT_NULL.org.springframework.beans.TestBean", errors.getFieldError("map[key0]").getCodes()[4]);
+		assertEquals("NOT_NULL", errors.getFieldError("map[key0]").getCodes()[5]);
+	}
+
 	public void testCustomEditorWithSubclass() {
 		IndexedTestBean tb = new IndexedTestBean();
 		DataBinder binder = new DataBinder(tb, "tb");
