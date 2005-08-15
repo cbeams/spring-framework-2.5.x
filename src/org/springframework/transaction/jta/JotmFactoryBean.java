91479ebddfcbf46ea971a4184047c3142c95fa58
Juergen Hoeller <jhoeller@interface21.com>
2005-08-15 18:45:26 +0000

    use Spring 1.2 shortcuts in XML bean definition example
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7881 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/jta/JotmFactoryBean.java
+++ src/org/springframework/transaction/jta/JotmFactoryBean.java
@@ -39,20 +39,20 @@ import org.springframework.beans.factory.FactoryBean;
  * &lt;bean id="jotm" class="org.springframework.transaction.jta.JotmFactoryBean"/&gt;
  *
  * &lt;bean id="transactionManager" class="org.springframework.transaction.jta.JtaTransactionManager"&gt;
- *   &lt;property name="userTransaction"&gt;&lt;ref local="jotm"/&gt;&lt;/property&gt;
+ *   &lt;property name="userTransaction" ref="jotm"/&gt;
  * &lt;/bean&gt;
  *
  * &lt;bean id="innerDataSource" class="org.enhydra.jdbc.standard.StandardXADataSource" destroy-method="shutdown"&gt;
- *   &lt;property name="transactionManager"&gt;&lt;ref local="jotm"/&gt;&lt;/property&gt;
- *   &lt;property name="driverName"&gt;...&lt;/property&gt;
- *   &lt;property name="url"&gt;...&lt;/property&gt;
+ *   &lt;property name="transactionManager" ref="jotm"/&gt;
+ *   &lt;property name="driverName" value="..."/&gt;
+ *   &lt;property name="url" value="..."/&gt;
  * &lt;/bean&gt;
  *
  * &lt;bean id="dataSource" class="org.enhydra.jdbc.pool.StandardXAPoolDataSource" destroy-method="shutdown"&gt;
- *   &lt;property name="dataSource"&gt;&lt;ref local="innerDataSource"/&gt;&lt;/property&gt;
- *   &lt;property name="user"&gt;...&lt;/property&gt;
- *   &lt;property name="password"&gt;...&lt;/property&gt;
- *   &lt;property name="maxSize"&gt;...&lt;/property&gt;
+ *   &lt;property name="dataSource" ref="innerDataSource"/&gt;
+ *   &lt;property name="user" value="..."/&gt;
+ *   &lt;property name="password" value="..."/&gt;
+ *   &lt;property name="maxSize" value="..."/&gt;
  * &lt;/bean&gt;</pre>
  *
  * Uses JOTM's static access method to obtain the JOTM Current object, which
@@ -70,12 +70,15 @@ public class JotmFactoryBean implements FactoryBean, DisposableBean {
 
 	private Jotm jotm;
 
+
 	public JotmFactoryBean() throws NamingException {
-		// check for already active JOTM instance
+		// Check for already active JOTM instance.
 		this.jotmCurrent = Current.getCurrent();
 
-		// if none found, create new local JOTM instance
+		// If none found, create new local JOTM instance.
 		if (this.jotmCurrent == null) {
+			// Only for use within the current Spring context:
+			// local, not bound to registry.
 			this.jotm = new Jotm(true, false);
 			this.jotmCurrent = Current.getCurrent();
 		}
@@ -90,6 +93,7 @@ public class JotmFactoryBean implements FactoryBean, DisposableBean {
 		return jotm;
 	}
 
+
 	public Object getObject() {
 		return this.jotmCurrent;
 	}
@@ -102,6 +106,10 @@ public class JotmFactoryBean implements FactoryBean, DisposableBean {
 		return true;
 	}
 
+
+	/**
+	 * Stop the local JOTM instance, if created by this FactoryBean.
+	 */
 	public void destroy() {
 		if (this.jotm != null) {
 			this.jotm.stop();
