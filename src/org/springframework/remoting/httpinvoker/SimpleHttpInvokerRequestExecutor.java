87e1252c329cd7663da860afafb59952acf3045e
Juergen Hoeller <jhoeller@interface21.com>
2005-08-26 20:21:20 +0000

    factored out protected "writeRequestBody" and "readResponseBody" template methods
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@7936 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/httpinvoker/SimpleHttpInvokerRequestExecutor.java
+++ src/org/springframework/remoting/httpinvoker/SimpleHttpInvokerRequestExecutor.java
@@ -18,6 +18,7 @@ package org.springframework.remoting.httpinvoker;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -25,50 +26,122 @@ import java.net.URLConnection;
 import org.springframework.remoting.support.RemoteInvocationResult;
 
 /**
- * HttpInvokerRequestExecutor implementation that uses J2SE facilities
- * to execute POST requests, without support for HTTP authentication
- * or advanced configuration options.
+ * HttpInvokerRequestExecutor implementation that uses standard J2SE facilities
+ * to execute POST requests, without support for HTTP authentication or
+ * advanced configuration options.
  *
- * <p>Consider CommonsHttpInvokerRequestExecutor for more sophisticated needs.
+ * <p>Designed for easy subclassing, customizing specific template methods.
+ * However, consider CommonsHttpInvokerRequestExecutor for more sophisticated
+ * needs: The J2SE HttpURLConnection is rather limited in its capabilities.
  *
  * @author Juergen Hoeller
  * @since 1.1
  * @see CommonsHttpInvokerRequestExecutor
+ * @see java.net.HttpURLConnection
  */
 public class SimpleHttpInvokerRequestExecutor extends AbstractHttpInvokerRequestExecutor {
 
+	/**
+	 * Execute the given request through a standard J2SE HttpURLConnection.
+	 * <p>This method implements the basic processing workflow:
+	 * The actual work happens in this class's template methods.
+	 * @see #openConnection
+	 * @see #prepareConnection
+	 * @see #writeRequestBody
+	 * @see #readResponseBody
+	 */
 	protected RemoteInvocationResult doExecuteRequest(
 			HttpInvokerClientConfiguration config, ByteArrayOutputStream baos)
 			throws IOException, ClassNotFoundException {
 
+		// open connection
+		HttpURLConnection con = openConnection(config);
+
+		// send request
+		prepareConnection(con, baos.size());
+		writeRequestBody(config, con, baos);
+
+		// parse response
+		InputStream responseBody = readResponseBody(config, con);
+		return readRemoteInvocationResult(responseBody, config.getCodebaseUrl());
+	}
+
+	/**
+	 * Open an HttpURLConnection for the given remote invocation request.
+	 * @param config the HTTP invoker configuration that specifies the
+	 * target service
+	 * @return the HttpURLConnection for the given request
+	 * @throws IOException if thrown by I/O methods
+	 * @see java.net.URL#openConnection()
+	 */
+	protected HttpURLConnection openConnection(HttpInvokerClientConfiguration config) throws IOException {
 		URLConnection con = new URL(config.getServiceUrl()).openConnection();
 		if (!(con instanceof HttpURLConnection)) {
 			throw new IOException("Service URL [" + config.getServiceUrl() + "] is not an HTTP URL");
 		}
-		HttpURLConnection httpCon = (HttpURLConnection) con;
-
-		// send request
-		prepareConnection(httpCon, baos.size());
-		baos.writeTo(httpCon.getOutputStream());
-
-		// parse response
-		return readRemoteInvocationResult(httpCon.getInputStream(), config.getCodebaseUrl());
+		return (HttpURLConnection) con;
 	}
 
 	/**
 	 * Prepare the given HTTP connection.
-	 * <p>Default implementation specifies POST as method,
+	 * <p>The default implementation specifies POST as method,
 	 * "application/x-java-serialized-object" as "Content-Type" header,
 	 * and the given content length as "Content-Length" header.
 	 * @param con the HTTP connection to prepare
 	 * @param contentLength the length of the content to send
 	 * @throws IOException if thrown by HttpURLConnection methods
+	 * @see java.net.HttpURLConnection#setRequestMethod
+	 * @see java.net.HttpURLConnection#setRequestProperty
 	 */
 	protected void prepareConnection(HttpURLConnection con, int contentLength) throws IOException {
 		con.setDoOutput(true);
 		con.setRequestMethod(HTTP_METHOD_POST);
 		con.setRequestProperty(HTTP_HEADER_CONTENT_TYPE, CONTENT_TYPE_SERIALIZED_OBJECT);
 		con.setRequestProperty(HTTP_HEADER_CONTENT_LENGTH, Integer.toString(contentLength));
+	}
+
+	/**
+	 * Set the given serialized remote invocation as request body.
+	 * <p>The default implementation simply write the serialized invocation
+	 * to the HttpURLConnection's OutputStream. This can be overridden,
+	 * for example, to write a specific encoding and potentially set
+	 * appropriate HTTP request headers.
+	 * @param config the HTTP invoker configuration that specifies the
+	 * target service
+	 * @param con the HttpURLConnection to write the request body to
+	 * @param baos the ByteArrayOutputStream that contains the serialized
+	 * RemoteInvocation object
+	 * @throws IOException if thrown by I/O methods
+	 * @see java.net.HttpURLConnection#getOutputStream()
+	 * @see java.net.HttpURLConnection#setRequestProperty
+	 */
+	protected void writeRequestBody(
+			HttpInvokerClientConfiguration config, HttpURLConnection con, ByteArrayOutputStream baos)
+			throws IOException {
+
+		baos.writeTo(con.getOutputStream());
+	}
+
+	/**
+	 * Extract the response body from the given executed remote invocation
+	 * request.
+	 * <p>The default implementation simply reads the serialized invocation
+	 * from the HttpURLConnection's InputStream. This can be overridden,
+	 * for example, to check for GZIP response encoding and wrap the
+	 * returned InputStream in a GZIPInputStream.
+	 * @param config the HTTP invoker configuration that specifies the
+	 * target service
+	 * @param con the HttpURLConnection to read the response body from
+	 * @return an InputStream for the response body
+	 * @throws IOException if thrown by I/O methods
+	 * @see java.net.HttpURLConnection#getInputStream()
+	 * @see java.net.HttpURLConnection#getHeaderField(int)
+	 * @see java.net.HttpURLConnection#getHeaderFieldKey(int)
+	 */
+	protected InputStream readResponseBody(HttpInvokerClientConfiguration config, HttpURLConnection con)
+			throws IOException {
+
+		return con.getInputStream();
 	}
 
 }
