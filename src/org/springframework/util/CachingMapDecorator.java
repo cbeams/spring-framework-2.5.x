2fc1a5f4cb7049a28a95084dda92a49bee121375
Juergen Hoeller <jhoeller@interface21.com>
2005-09-19 18:15:54 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8065 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/CachingMapDecorator.java
+++ src/org/springframework/util/CachingMapDecorator.java
@@ -79,7 +79,7 @@ public abstract class CachingMapDecorator implements Map, Serializable {
 	 * @param targetMap the Map to decoarate
 	 */
 	public CachingMapDecorator(Map targetMap) {
-		Assert.notNull(targetMap, "Map cannot be <code>null</code>");
+		Assert.notNull(targetMap, "Target Map is required");
 		this.targetMap = targetMap;
 	}
 
