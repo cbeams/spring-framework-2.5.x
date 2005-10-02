a032071fe64d8744251aa4e83da4ad8111b6b90e
Juergen Hoeller <jhoeller@interface21.com>
2005-10-02 19:35:07 +0000

    added "defaultTimeout" bean property, for specifying the default JOTM transaction timeout
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8100 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/jta/JotmFactoryBean.java
+++ src/org/springframework/transaction/jta/JotmFactoryBean.java
@@ -85,6 +85,15 @@ public class JotmFactoryBean implements FactoryBean, DisposableBean {
 	}
 
 	/**
+	 * Set the default transaction timeout for the JOTM instance.
+	 * <p>Should only be called for a local JOTM instance,
+	 * not when accessing an existing (shared) JOTM instance.
+	 */
+	public void setDefaultTimeout(int defaultTimeout) {
+		this.jotmCurrent.setDefaultTimeout(defaultTimeout);
+	}
+
+	/**
 	 * Return the JOTM instance created by this factory bean, if any.
 	 * Will be <code>null</code> if an already active JOTM instance is used.
 	 * <p>Application code should never need to access this.
