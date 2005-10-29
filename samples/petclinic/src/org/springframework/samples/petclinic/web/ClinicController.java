4a0e817f835f994c3ec75b5b3a8bba148763694a
Juergen Hoeller <jhoeller@interface21.com>
2005-10-29 17:00:16 +0000

    changed owner view to use straight "owner" attribute instead of "model" attribute of type Map
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8245 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/petclinic/src/org/springframework/samples/petclinic/web/ClinicController.java
+++ samples/petclinic/src/org/springframework/samples/petclinic/web/ClinicController.java
@@ -1,14 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.ApplicationContextException;
 import org.springframework.samples.petclinic.Clinic;
 import org.springframework.samples.petclinic.Owner;
 import org.springframework.web.bind.RequestUtils;
@@ -24,16 +20,17 @@ public class ClinicController extends MultiActionController implements Initializ
 
 	private Clinic clinic;
 
+
 	public void setClinic(Clinic clinic) {
 		this.clinic = clinic;
 	}
 
-	public void afterPropertiesSet() throws Exception {
-		if (clinic == null)
-			throw new ApplicationContextException("Must set clinic bean property on " + getClass());
+	public void afterPropertiesSet() {
+		if (this.clinic == null) {
+			throw new IllegalArgumentException("Must set clinic bean property on " + getClass());
+		}
 	}
 
-	// handlers
 
 	/**
 	 * Custom handler for welcome
@@ -52,7 +49,7 @@ public class ClinicController extends MultiActionController implements Initializ
 	 * @return a ModelAndView to render the response
 	 */
 	public ModelAndView vetsHandler(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		return new ModelAndView("vetsView", "vets", clinic.getVets());
+		return new ModelAndView("vetsView", "vets", this.clinic.getVets());
 	}
 
 	/**
@@ -62,13 +59,11 @@ public class ClinicController extends MultiActionController implements Initializ
 	 * @return a ModelAndView to render the response
 	 */
 	public ModelAndView ownerHandler(HttpServletRequest request, HttpServletResponse response) throws ServletException {
-		Owner owner = clinic.loadOwner(RequestUtils.getIntParameter(request, "ownerId", 0));
+		Owner owner = this.clinic.loadOwner(RequestUtils.getIntParameter(request, "ownerId", 0));
 		if (owner == null) {
 			return new ModelAndView("findOwnersRedirect");
 		}
-		Map model = new HashMap();
-		model.put("owner", owner);
-		return new ModelAndView("ownerView", "model", model);
+		return new ModelAndView("ownerView", "owner", owner);
 	}
 
 }
