aeec22815934068ac36b9c815e1f04b08c97abbc
Juergen Hoeller <jhoeller@interface21.com>
2005-11-06 16:11:27 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8275 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/StringUtils.java
+++ src/org/springframework/util/StringUtils.java
@@ -347,8 +347,8 @@ public abstract class StringUtils {
 		int separatorIndex = path.lastIndexOf(FOLDER_SEPARATOR);
 		if (separatorIndex != -1) {
 			String newPath = path.substring(0, separatorIndex);
-			if (!relativePath.startsWith("/")) {
-				newPath += "/";
+			if (!relativePath.startsWith(FOLDER_SEPARATOR)) {
+				newPath += FOLDER_SEPARATOR;
 			}
 			return newPath + relativePath;
 		}
