6603a9a33acdcc1c694f610c86f704742b7cac91
Juergen Hoeller <jhoeller@interface21.com>
2005-11-18 19:48:24 +0000

    moved TaskExecutor abstraction out of the sandbox
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8395 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/context/event/SimpleApplicationEventMulticaster.java
+++ src/org/springframework/context/event/SimpleApplicationEventMulticaster.java
@@ -20,6 +20,9 @@ import java.util.Iterator;
 
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.util.Assert;
 
 /**
  * Simple implementation of the ApplicationEventMulticaster interface.
@@ -29,20 +32,53 @@ import org.springframework.context.ApplicationListener;
  * Listeners will usually perform corresponding <code>instanceof</code>
  * checks on the passed-in event object.
  *
- * <p>All listeners are invoked in the calling thread. This allows the danger of
- * a rogue listener blocking the entire application, but adds minimal overhead.
- *
- * <p>An alternative implementation could be more sophisticated in both these respects.
+ * <p>By default, all listeners are invoked in the calling thread.
+ * This allows the danger of a rogue listener blocking the entire application,
+ * but adds minimal overhead. Specify an alternative TaskExecutor to have
+ * listeners executed in different threads, for example from a thread pool.
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
+ * @see #setTaskExecutor
  */
 public class SimpleApplicationEventMulticaster extends AbstractApplicationEventMulticaster {
 
-	public void multicastEvent(ApplicationEvent event) {
+	private TaskExecutor taskExecutor = new SyncTaskExecutor();
+
+
+	/**
+	 * Set the TaskExecutor to execute application listeners with.
+	 * <p>Default is a SyncTaskExecutor, executing the listeners synchronously
+	 * in the calling thread.
+	 * <p>Consider specifying an asynchronous TaskExecutor here to not block the
+	 * caller until all listeners have been executed. However, note that asynchronous
+	 * execution will not participate in the caller's thread context (class loader,
+	 * transaction association) unless the TaskExecutor explicitly supports this.
+	 * @see org.springframework.core.task.SyncTaskExecutor
+	 * @see org.springframework.core.task.SimpleAsyncTaskExecutor
+	 * @see org.springframework.scheduling.timer.TimerTaskExecutor
+	 */
+	public void setTaskExecutor(TaskExecutor taskExecutor) {
+		Assert.notNull(taskExecutor, "taskExecutor is required");
+		this.taskExecutor = taskExecutor;
+	}
+
+	/**
+	 * Return the current TaskExecutor for this multicaster.
+	 */
+	protected TaskExecutor getTaskExecutor() {
+		return taskExecutor;
+	}
+
+
+	public void multicastEvent(final ApplicationEvent event) {
 		for (Iterator it = getApplicationListeners().iterator(); it.hasNext();) {
-			ApplicationListener listener = (ApplicationListener) it.next();
-			listener.onApplicationEvent(event);
+			final ApplicationListener listener = (ApplicationListener) it.next();
+			getTaskExecutor().execute(new Runnable() {
+				public void run() {
+					listener.onApplicationEvent(event);
+				}
+			});
 		}
 	}
 
