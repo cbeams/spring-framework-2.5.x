6fe75d0d6de10a9039e6eb2c5e977589547ee8be
Juergen Hoeller <jhoeller@interface21.com>
2005-11-19 16:58:01 +0000

    explicitly declare BeansException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8410 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/InstantiationStrategy.java
+++ src/org/springframework/beans/factory/support/InstantiationStrategy.java
@@ -19,6 +19,7 @@ package org.springframework.beans.factory.support;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
@@ -36,43 +37,49 @@ public interface InstantiationStrategy {
 	 * Return an instance of the bean with the given name in this factory.
 	 * @param beanDefinition the bean definition
 	 * @param beanName name of the bean when it's created in this context.
-	 * The name can be <code>null</code> if we're autowiring a bean that doesn't belong to the factory.
+	 * The name can be <code>null</code> if we're autowiring a bean that
+	 * doesn't belong to the factory.
 	 * @param owner owning BeanFactory
 	 * @return a bean instance for this bean definition
+	 * @throws BeansException if the instantiation failed
 	 */
 	Object instantiate(
-			RootBeanDefinition beanDefinition, String beanName, BeanFactory owner);
+			RootBeanDefinition beanDefinition, String beanName, BeanFactory owner) throws BeansException;
 	
 	/**
 	 * Return an instance of the bean with the given name in this factory,
 	 * creating it via the given constructor.
 	 * @param beanDefinition the bean definition
 	 * @param beanName name of the bean when it's created in this context.
-	 * The name can be <code>null</code> if we're autowiring a bean that doesn't belong to the factory.
+	 * The name can be <code>null</code> if we're autowiring a bean
+	 * that doesn't belong to the factory.
 	 * @param owner owning BeanFactory
 	 * @param ctor the constructor to use
 	 * @param args the constructor arguments to apply
 	 * @return a bean instance for this bean definition
+	 * @throws BeansException if the instantiation failed
 	 */
 	Object instantiate(
 			RootBeanDefinition beanDefinition, String beanName, BeanFactory owner,
-			Constructor ctor, Object[] args);
+			Constructor ctor, Object[] args) throws BeansException;
 	
 	/**
 	 * Return an instance of the bean with the given name in this factory,
 	 * creating it via the given factory method.
 	 * @param beanDefinition bean definition
 	 * @param beanName name of the bean when it's created in this context.
-	 * The name can be <code>null</code> if we're autowiring a bean that doesn't belong to the factory.
+	 * The name can be <code>null</code> if we're autowiring a bean
+	 * that doesn't belong to the factory.
 	 * @param owner owning BeanFactory
 	 * @param factoryBean the factory bean instance to call the factory method on,
 	 * or <code>null</code> in case of a static factory method
 	 * @param factoryMethod the factory method to use
 	 * @param args the factory method arguments to apply
 	 * @return a bean instance for this bean definition
+	 * @throws BeansException if the instantiation failed
 	 */
 	Object instantiate(
 			RootBeanDefinition beanDefinition, String beanName, BeanFactory owner,
-			Object factoryBean, Method factoryMethod, Object[] args);
+			Object factoryBean, Method factoryMethod, Object[] args) throws BeansException;
 	
 }
