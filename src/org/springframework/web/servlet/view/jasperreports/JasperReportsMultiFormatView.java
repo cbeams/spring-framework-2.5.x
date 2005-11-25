cdf0be9108e004c628789a045afba93568b37f89
Juergen Hoeller <jhoeller@interface21.com>
2005-11-25 15:15:53 +0000

    removed unused "mergeExporterParameters" method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8461 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/jasperreports/JasperReportsMultiFormatView.java
+++ src/org/springframework/web/servlet/view/jasperreports/JasperReportsMultiFormatView.java
@@ -20,12 +20,10 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Iterator;
 
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JRExporterParameter;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.ApplicationContextException;
@@ -201,10 +199,9 @@ public class JasperReportsMultiFormatView extends AbstractJasperReportsView {
 		// Copy appropriate properties across.
 		view.setExporterParameters(getExporterParameters());
 
-		// Can skip most initialization since all relevant
-		// URL processing has been done - just need to convert
-		// parameters on the sub view..
-        view.convertExporterParameters();
+		// Can skip most initialization since all relevant URL processing
+		// has been done - just need to convert parameters on the sub view.
+		view.convertExporterParameters();
 
 		response.setContentType(view.getContentType());
 
@@ -232,27 +229,4 @@ public class JasperReportsMultiFormatView extends AbstractJasperReportsView {
 		}
 	}
 
-	/**
-	 * Merges the configured {@link net.sf.jasperreports.engine.JRExporterParameter JRExporterParameters} with any specified
-	 * in the supplied model data. {@link net.sf.jasperreports.engine.JRExporterParameter JRExporterParameters} in the model
-	 * override those specified in the configuration.
-	 * @see #setExporterParameters(java.util.Map)
-	 */
-	protected Map mergeExporterParameters(Map model) {
-		Map mergedParameters = new HashMap(getConvertedExporterParameters());
-		for (Iterator iterator = model.keySet().iterator(); iterator.hasNext();) {
-			Object key = iterator.next();
-
-			if (key instanceof JRExporterParameter) {
-				Object value = model.get(key);
-				if (value instanceof String) {
-					mergedParameters.put(key, value);
-				}
-				else {
-					logger.warn("Ignoring exporter parameter [" + key + "]. Value is not a String.");
-				}
-			}
-		}
-		return mergedParameters;
-	}
 }
