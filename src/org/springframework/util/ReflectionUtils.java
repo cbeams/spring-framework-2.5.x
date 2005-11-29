82658f9aee3a44400bc6150a0a022ba07cff19be
Rod Johnson <rjohnson@interface21.com>
2005-11-29 17:48:16 +0000

    Added field and method callbacks
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8503 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/ReflectionUtils.java
+++ src/org/springframework/util/ReflectionUtils.java
@@ -16,8 +16,15 @@
 
 package org.springframework.util;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Simple utility class for handling reflection exceptions.
@@ -25,9 +32,12 @@ import java.lang.reflect.Method;
  *
  * @author Juergen Hoeller
  * @author Rob Harrop
+ * @author Rod Johnson
  * @since 1.2.2
  */
 public abstract class ReflectionUtils {
+	
+	private static Log log = LogFactory.getLog(ReflectionUtils.class);
 
 	/**
 	 * Handle the given reflection exception.
@@ -99,5 +109,177 @@ public abstract class ReflectionUtils {
 			handleReflectionException(e);
 			throw new IllegalStateException("Unexpected reflection exception -" + e.getMessage());
 		}
+	}
+	
+	
+	public static void doWithFields(Class targetClass, FieldCallback fc) throws IllegalArgumentException {		
+		doWithFields(targetClass, fc, null);
+	}
+	
+	/**
+	 * Invoke the given callback on all private fields in the target class, going
+	 * up the class hierarchy to get all declared fields.
+	 * @param targetClass
+	 * @param fc
+	 * @throws IllegalArgumentException
+	 */
+	public static void doWithFields(Class targetClass, FieldCallback fc, FieldFilter ff) throws IllegalArgumentException {		
+		// Keep backing up the inheritance hierarchy
+		do {
+			// Copy each field declared on this class unless it's static or file
+			Field[] fields = targetClass.getDeclaredFields();
+			
+			log.debug("Found " + fields.length + " fields on " + targetClass);
+			for (int i = 0; i < fields.length; i++) {
+				//	Skip static and final fields
+				 if (ff != null && !ff.matches(fields[i]))
+					 continue;
+						 
+				try {
+					fields[i].setAccessible(true);
+					fc.doWith(fields[i]);
+				}
+				catch (IllegalAccessException ex) {
+					throw new IllegalStateException("Shouldn't be illegal to access field '" + fields[i].getName() + "': " + ex);
+				}
+			}
+			
+			targetClass = targetClass.getSuperclass();
+		} while (targetClass != null && targetClass != Object.class);
+		
+	}
+	
+	/**
+	 * Callback interface invoked on each field in the hierarchy.
+	 */
+	public interface FieldCallback {
+		
+		/**
+		 * Perform an operation using the given field.
+		 * @param f Field, which will have been made accessible before this invocation
+		 * @throws IllegalArgumentException
+		 * @throws IllegalAccessException
+		 */
+		void doWith(Field f) throws IllegalArgumentException, IllegalAccessException;
+	}
+	
+	
+	/**
+	 * Perform the given callback operation on all matching methods of the given class
+	 * and superclasses<br>.
+	 * The same named method occurring on subclass and superclass will
+	 * appear twice, unless excluded by the MethodFilter
+	 * @param targetClass class to start looking at
+	 * @param mc callback action to take on each method
+	 * @param mf filter to use to exclude methods
+	 * @throws IllegalArgumentException
+	 */
+	public static void doWithMethods(Class targetClass, MethodCallback mc, MethodFilter mf) throws IllegalArgumentException {		
+		// Keep backing up the inheritance hierarchy
+		do {
+			Method[] methods = targetClass.getDeclaredMethods();
+			log.debug("Found " + methods.length + " methods on " + targetClass);
+			for (int i = 0; i < methods.length; i++) {
+				 if (mf != null && !mf.matches(methods[i])) {
+					 continue;
+				 }
+						 
+				try {
+					if (!methods[i].isAccessible()) {
+						methods[i].setAccessible(true);					
+					}
+					mc.doWith(methods[i]);
+				}
+				catch (IllegalAccessException ex) {
+					throw new IllegalStateException("Shouldn't be illegal to access method '" + methods[i].getName() + "': " + ex);
+				}
+			}
+			
+			targetClass = targetClass.getSuperclass();
+		} while (targetClass != null);
+	}
+	
+	/**
+	 * Get all declared methods on the leaf class and all superclasses.
+	 * Leaf class methods are included first.
+	 * @param leafClass
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static Method[] getAllDeclaredMethods(Class leafClass) throws IllegalArgumentException {
+		final List l = new LinkedList();
+		doWithMethods(leafClass, new MethodCallback() {
+			public void doWith(Method m) throws IllegalArgumentException ,IllegalAccessException {
+				l.add(m);
+			}
+		}, DECLARED_METHODS);
+		return (Method[]) l.toArray(new Method[l.size()]);
+	}
+	
+	
+	/**
+	 * Action to take on each method
+	 */
+	public interface MethodCallback {
+		void doWith(Method m) throws IllegalArgumentException, IllegalAccessException;
+	}
+	
+	/**
+	 * Callback to exclude methods
+	 */
+	public interface MethodFilter {
+		boolean matches(Method m);
+	}
+	
+	/**
+	 * Callback optionally used to filter fields to be
+	 * operated on by field callback.
+	 */
+	public interface FieldFilter {
+		boolean matches(Field f);
+	}
+	
+	public static FieldFilter COPYABLE_FIELDS = new FieldFilter() {
+		public boolean matches(Field f) {
+			return !(Modifier.isStatic(f.getModifiers()) ||
+	 				Modifier.isFinal(f.getModifiers())	);
+		}
+	};
+	
+	/**
+	 * MethodFilter implementation that will return all declared methods
+	 * on entire class hierarchy above this object.
+	 */
+	public static MethodFilter DECLARED_METHODS = new MethodFilter() {
+		public boolean matches(Method m) {
+			return true;
+		}
+	};
+	
+	
+	/**
+	 * Given the source object and the destination, which must be the same class or a subclass,
+	 * copy all fields, including inherited fields. Designed to work on objects with public
+	 * no-arg constructors.
+	 * @param src
+	 * @param dest
+	 * @throws java.lang.IllegalArgumentException if arguments are incompatible or either is null
+	 */
+	public static void shallowCopyFieldState(final Object src, final Object dest) throws IllegalArgumentException {
+		if (src == null)
+			throw new IllegalArgumentException("Source for field copy cannot be null");
+		if (dest == null)
+			throw new IllegalArgumentException("Destination for field copy cannot be null");
+		if (!src.getClass().isAssignableFrom(dest.getClass()))
+			throw new IllegalArgumentException("Destination class '" + dest.getClass().getName() +
+						"' must be same or subclass as source class '" + src.getClass().getName() + "'");
+		
+		log.debug("Copying fields from instance of " + src.getClass() + " to instance of " + dest.getClass());
+		ReflectionUtils.doWithFields(src.getClass(), new ReflectionUtils.FieldCallback() {
+			public void doWith(Field f) throws IllegalArgumentException ,IllegalAccessException {
+				Object srcValue = f.get(src);
+				f.set(dest, srcValue);
+			}
+		}, ReflectionUtils.COPYABLE_FIELDS);
 	}
 }
--- test/org/springframework/util/ReflectionUtilsTests.java
+++ test/org/springframework/util/ReflectionUtilsTests.java
@@ -1,6 +1,10 @@
 package org.springframework.util;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
@@ -27,5 +31,150 @@ public class ReflectionUtilsTests extends TestCase {
 		ReflectionUtils.invokeMethod(setName, bean, new Object[]{juergen});
 		assertEquals("Incorrect name set", juergen, bean.getName());
 	}
+	
+	public void testCopySrcToDestinationOfIncorrectClass() {
+		TestBean src = new TestBean();
+		String dest = new String();
+		try {
+			ReflectionUtils.shallowCopyFieldState(src, dest);
+			fail();
+		}
+		catch (IllegalArgumentException ex) {
+			// Ok
+		}
+	}
+	
+	
+	public void testRejectsNullSrc() {
+		TestBean src = null;
+		String dest = new String();
+		try {
+			ReflectionUtils.shallowCopyFieldState(src, dest);
+			fail();
+		}
+		catch (IllegalArgumentException ex) {
+			// Ok
+		}
+	}
+	
+	public void testRejectsNullDest() {
+		TestBean src = new TestBean();
+		String dest = null;
+		try {
+			ReflectionUtils.shallowCopyFieldState(src, dest);
+			fail();
+		}
+		catch (IllegalArgumentException ex) {
+			// Ok
+		}
+	}
+	
+	public void testValidCopy() {
+		TestBean src = new TestBean();
+		TestBean dest = new TestBean();
+		testValidCopy(src, dest);
+	}
+	
+	public static class TestBeanSubclassWithNewField extends TestBean {
+		private int magic;
+		protected String prot = "foo";
+	}
+	
+	public static class TestBeanSubclassWithFinalField extends TestBean {
+		private final String foo = "will break naive copy that doesn't exclude statics";
+	}
+	
+	public void testValidCopyOnSubTypeWithNewField() {
+		TestBeanSubclassWithNewField src = new TestBeanSubclassWithNewField();
+		TestBeanSubclassWithNewField dest = new TestBeanSubclassWithNewField();
+		src.magic = 11;
+		
+		// Will check inherited fields are copied
+		testValidCopy(src, dest);
+		
+		// Check subclass fields were copied
+		assertEquals(src.magic, dest.magic);
+		assertEquals(src.prot, dest.prot);
+	}
+	
+	public void testValidCopyToSubType() {
+		TestBean src = new TestBean();
+		TestBeanSubclassWithNewField dest = new TestBeanSubclassWithNewField();
+		dest.magic = 11;
+		testValidCopy(src, dest);
+		// Should have left this one alone
+		assertEquals(11, dest.magic);
+	}
+	
+	public void testValidCopyToSubTypeWithFinalField() {
+		TestBeanSubclassWithFinalField src = new TestBeanSubclassWithFinalField();
+		TestBeanSubclassWithFinalField dest = new TestBeanSubclassWithFinalField();
+		// Check that this doesn't fail due to attempt to assign final
+		testValidCopy(src, dest);
+	}
+	
+	private void testValidCopy(TestBean src, TestBean dest) {
+		src.setName("freddie");
+		src.setAge(15);
+		src.setSpouse(new TestBean());
+		assertFalse(src.getAge() == dest.getAge());
+		
+		ReflectionUtils.shallowCopyFieldState(src, dest);
+		assertEquals(src.getAge(), dest.getAge());
+		assertEquals(src.getSpouse(), dest.getSpouse());
+		assertEquals(src.getDoctor(), dest.getDoctor());
+	}
+	
+	static class ListSavingMethodCallback implements ReflectionUtils.MethodCallback {
+		private List methodNames = new LinkedList();
+		private List methods = new LinkedList();
+		
+		public void doWith(Method m) throws IllegalArgumentException, IllegalAccessException {
+			methodNames.add(m.getName());
+			methods.add(m);
+		}
+		
+		public List getMethodNames() {
+			return methodNames;
+		}
+		
+		public List getMethods() {
+			return methods;
+		}
+	};
 
+
+	public void testDoWithProtectedMethods() {
+		ListSavingMethodCallback mc = new ListSavingMethodCallback();
+		ReflectionUtils.doWithMethods(TestBean.class, mc, 
+				new ReflectionUtils.MethodFilter() {
+					public boolean matches(Method m) {
+						return Modifier.isProtected(m.getModifiers());
+					}
+		});
+		assertFalse(mc.getMethodNames().isEmpty());
+		assertTrue("Must find protected method on Object", mc.getMethodNames().contains("clone"));
+		assertTrue("Must find protected method on Object", mc.getMethodNames().contains("finalize"));
+		assertFalse("Public, not protected", mc.getMethodNames().contains("hashCode"));
+		assertFalse("Public, not protected", mc.getMethodNames().contains("absquatulate"));
+	}
+	
+	public static class TestBeanSubclass extends TestBean {
+		public void absquatulate() {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	public void testDuplicatesFound() {
+		ListSavingMethodCallback mc = new ListSavingMethodCallback();
+		ReflectionUtils.doWithMethods(TestBeanSubclass.class, mc, ReflectionUtils.DECLARED_METHODS);
+		int absquatulateCount = 0;
+		for (Iterator it = mc.getMethodNames().iterator(); it.hasNext() ;) {
+			String name = (String) it.next();
+			if (name.equals("absquatulate")) {
+				++absquatulateCount;
+			}
+		}
+		assertEquals("Found 2 absquatulates", 2, absquatulateCount);
+	}
 }
