3044e52de5454e2d3fd49f1f01b04c83e98495d8
Rod Johnson <rjohnson@interface21.com>
2005-11-29 16:02:16 +0000

    First version
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8501 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ tiger/src/org/springframework/core/Order.java
@@ -0,0 +1,20 @@
+package org.springframework.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to define Ordering. 
+ * 
+ * @author Rod Johnson
+ * @see org.springframework.core.Ordered
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+public @interface Order {
+
+	int value();
+}
