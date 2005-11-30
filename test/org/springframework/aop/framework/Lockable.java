5996dc70428dce844c91bbb8358dbfdac88fb974
Rod Johnson <rjohnson@interface21.com>
2005-11-30 14:15:48 +0000

    Javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8516 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/framework/Lockable.java
+++ test/org/springframework/aop/framework/Lockable.java
@@ -17,8 +17,17 @@
 package org.springframework.aop.framework;
 
 
+/**
+ * Simple interface to use for mixins
+ * 
+ * @author Rod Johnson
+ *
+ */
 public interface Lockable {
+	
 	void lock();
+	
 	void unlock();
+	
 	boolean locked();
 }
