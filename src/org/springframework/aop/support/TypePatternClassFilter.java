10395be1175ff3e14a5d48099d73de0071ecc82b
Rod Johnson <rjohnson@interface21.com>
2005-11-30 14:36:26 +0000

    First version
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8517 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/aop/support/TypePatternClassFilter.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.support;
+
+import org.aspectj.weaver.tools.PointcutParser;
+import org.aspectj.weaver.tools.TypePatternMatcher;
+import org.springframework.aop.ClassFilter;
+
+/**
+ * Spring AOP ClassFilter implementation using AspectJ type matching.
+ * @author Rod Johnson
+ * @since 1.3
+ */
+public class TypePatternClassFilter implements ClassFilter {
+	
+	private String typePattern;
+	
+	private TypePatternMatcher aspectJTypePatternMatcher;
+	
+	/**
+	 * JavaBean constructor. Be sure to set the
+	 * typePattern property.
+	 */
+	public TypePatternClassFilter() {
+	}
+	
+	/**
+	 * Create a fully configured TypePatternClassFilter using the  
+	 * given typePattern
+	 * @param typePattern type pattern that AspectJ weaver should parse
+	 */
+	public TypePatternClassFilter(String typePattern) {
+		setTypePattern(typePattern);
+	}
+
+	public String getTypePattern() {
+		return typePattern;
+	}
+
+	/**
+	 * Set the AspectJ type pattern to match. Examples include
+	 * <code>
+	 * org.springframework.beans.*
+	 * </code>
+	 * This will match any class or interface in the given package, and
+	 * <br>
+	 * <code>
+	 * org.springframework.beans.ITestBean+
+	 * </code>
+	 * This will match the ITestBean interface and any class that implements it.
+	 * <br>These conventions are established by AspectJ, not Spring AOP.
+	 * @param typePattern
+	 */
+	public void setTypePattern(String typePattern) {
+		this.typePattern = typePattern;
+		this.aspectJTypePatternMatcher = PointcutParser.getPointcutParserSupportingAllPrimitivesAndUsingContextClassloaderForResolution().
+				parseTypePattern(typePattern);
+	}
+
+	public boolean matches(Class clazz) {
+		return this.aspectJTypePatternMatcher.matches(clazz);
+	}
+
+}
---
+++ test/org/springframework/aop/support/TypePatternClassFilterTests.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.support;
+
+import junit.framework.TestCase;
+
+import org.springframework.aop.framework.autoproxy.metadata.ThreadLocalTestBean;
+import org.springframework.beans.IOther;
+import org.springframework.beans.ITestBean;
+import org.springframework.beans.TestBean;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+/**
+ * 
+ * 
+ * @author Rod Johnson
+ *
+ */
+public class TypePatternClassFilterTests extends TestCase {
+
+	public void testInvalidPattern() {
+		try {
+			new TypePatternClassFilter("-");
+			fail("Pattern should be recognized as invalid");
+		}
+		catch (IllegalArgumentException ex) {
+			// Ok
+		}
+	}
+	
+	public void testValidPatternMatching() {
+		TypePatternClassFilter tpcf = new TypePatternClassFilter("org.springframework.beans.*");
+		assertTrue("Should match: in package", tpcf.matches(TestBean.class));
+		assertTrue("Should match: in package", tpcf.matches(ITestBean.class));
+		assertTrue("Should match: in package", tpcf.matches(IOther.class));
+		assertFalse("Should be excluded: in wrong package", tpcf.matches(ThreadLocalTestBean.class));
+		assertFalse("Should be excluded: in wrong package", tpcf.matches(BeanFactory.class));
+		assertFalse("Should be excluded: in wrong package", tpcf.matches(DefaultListableBeanFactory.class));
+	}
+	
+	public void testSubclassMatching() {
+		TypePatternClassFilter tpcf = new TypePatternClassFilter("org.springframework.beans.ITestBean+");
+		assertTrue("Should match: in package", tpcf.matches(TestBean.class));
+		assertTrue("Should match: in package", tpcf.matches(ITestBean.class));
+		assertTrue("Should match: in package", tpcf.matches(ThreadLocalTestBean.class));
+		assertFalse("Should be excluded: not subclass", tpcf.matches(IOther.class));
+		assertFalse("Should be excluded: not subclass", tpcf.matches(DefaultListableBeanFactory.class));
+	}
+}
