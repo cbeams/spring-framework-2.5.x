4c9c2cd89ab9c916a061cf7fa79e616543a1d8c4
Rod Johnson <rjohnson@interface21.com>
2005-12-07 02:08:26 +0000

    First version
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8581 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/aop/aspectj/JoinPointImpl.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.aspectj;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.reflect.SourceLocation;
+
+/**
+ * Spring AOP implementation of AspectJ JoinPoint.
+ * Does not require an AOP Alliance MethodInvocation.
+ * 
+ * @author Rod Johnson
+ */
+public class JoinPointImpl implements JoinPoint {
+	
+	private final Method method;
+	
+	private final Object target;
+	
+	private final Object[] args;
+	
+	public JoinPointImpl(Method method, Object target, Object[] args) {
+		this.method = method;
+		this.target = target;
+		this.args = args;
+	}
+
+	public String toShortString() {
+		return "execution of " + method.getName();
+	}
+
+	public String toLongString() {
+		return "execution of " + method.getName();
+	}
+
+	public Object getThis() {
+		return target;
+	}
+
+	public Object getTarget() {
+		return target;
+	}
+
+	public Object[] getArgs() {
+		return args;
+	}
+
+	public Signature getSignature() {
+		throw new UnsupportedOperationException();
+	}
+
+	public SourceLocation getSourceLocation() {
+		throw new UnsupportedOperationException();
+	}
+
+
+	public StaticPart getStaticPart() {
+		throw new UnsupportedOperationException();
+	}
+
+	public String getKind() {
+		return JoinPoint.METHOD_EXECUTION;
+	}
+
+}
