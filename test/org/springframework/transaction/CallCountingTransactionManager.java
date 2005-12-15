bd7cec1e2ae39b6f2e9ef196bb70132c1d9fa37a
Rod Johnson <rjohnson@interface21.com>
2005-12-15 15:02:29 +0000

    Clear method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8624 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/transaction/CallCountingTransactionManager.java
+++ test/org/springframework/transaction/CallCountingTransactionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,6 +47,10 @@ public class CallCountingTransactionManager extends AbstractPlatformTransactionM
 	protected void doRollback(DefaultTransactionStatus status) {
 		++rollbacks;
 		--inflight;
+	}
+	
+	public void clear() {
+		begun = commits = rollbacks = inflight = 0;
 	}
 
 }
