643222247a3dd3206e0e126e120b994447b89159
Rob Harrop <rharrop@interface21.com>
2005-12-17 13:32:25 +0000

    Added <aop:scope/> tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8632 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ test/org/springframework/aop/config/AopNamespaceHandlerScopeTests.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.aop.config;
+
+import junit.framework.TestCase;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.aop.framework.Advised;
+import org.springframework.beans.ITestBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.web.context.scope.RequestContextHolder;
+
+/**
+ * @author Rob Harrop
+ */
+public class AopNamespaceHandlerScopeTests extends TestCase {
+
+	private ApplicationContext context;
+
+	public void setUp() {
+		this.context = new ClassPathXmlApplicationContext("org/springframework/aop/config/aopNamespaceHandlerScopeTests.xml");
+	}
+
+	public void testRequestScoping() throws Exception {
+
+		MockHttpServletRequest oldRequest = new MockHttpServletRequest();
+		MockHttpServletRequest newRequest = new MockHttpServletRequest();
+
+		RequestContextHolder.setRequest(oldRequest);
+
+		ITestBean scoped = getTestBean("requestScoped");
+
+		String rob = "Rob Harrop";
+		String bram = "Bram Smeets";
+
+		assertTrue("Should be AOP proxy", AopUtils.isAopProxy(scoped));
+		assertEquals(rob, scoped.getName());
+		scoped.setName(bram);
+		RequestContextHolder.setRequest(newRequest);
+		assertEquals(rob, scoped.getName());
+		RequestContextHolder.setRequest(oldRequest);
+		assertEquals(bram, scoped.getName());
+
+		assertTrue("Should have advisors", ((Advised)scoped).getAdvisors().length > 0);
+	}
+
+	public void testSessionScoping() throws Exception {
+		MockHttpSession oldSession = new MockHttpSession();
+		MockHttpSession newSession = new MockHttpSession();
+
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setSession(oldSession);
+		RequestContextHolder.setRequest(request);
+
+		ITestBean scoped = getTestBean("sessionScoped");
+
+		String rob = "Rob Harrop";
+		String bram = "Bram Smeets";
+
+		assertTrue("Should be AOP proxy", AopUtils.isAopProxy(scoped));
+		assertEquals(rob, scoped.getName());
+		scoped.setName(bram);
+		request.setSession(newSession);
+		assertEquals(rob, scoped.getName());
+		request.setSession(oldSession);
+		assertEquals(bram, scoped.getName());
+
+		assertTrue("Should have advisors", ((Advised)scoped).getAdvisors().length > 0);
+	}
+
+	private ITestBean getTestBean(String name) {
+		return (ITestBean) this.context.getBean(name);
+	}
+}
