7757cc0d3ce74ff725cfe527b2e192b671630867
Juergen Hoeller <jhoeller@interface21.com>
2005-12-18 00:31:15 +0000

    cleaned up imports
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8652 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/config/NamespaceHandlerUtils.java
+++ src/org/springframework/aop/config/NamespaceHandlerUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,30 +17,29 @@
 package org.springframework.aop.config;
 
 import org.springframework.aop.framework.autoproxy.InvocationContextExposingAdvisorAutoProxyCreator;
-import org.springframework.aop.framework.autoproxy.target.ConfigurableTargetSourceCreator;
 import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.PropertyValue;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.util.ClassUtils;
 
 /**
  * @author Rob Harrop
+ * @since 2.0
  */
 public abstract class NamespaceHandlerUtils {
 
-	// TODO: can short-circuit these methods when previously registered via this class
+	// TODO: Can short-circuit these methods when previously registered via this class.
 	// TODO: How eagerly should this try to look for user-registered auto proxy creators?
-	public static final String AUTO_PROXY_CREATOR_BEAN_NAME = "org.springframework.aop.config.internalAutoProxyCreator";
+	public static final String AUTO_PROXY_CREATOR_BEAN_NAME =
+			"org.springframework.aop.config.internalAutoProxyCreator";
 
-	public static final String CONFIGURABLE_TARGET_SOURCE_CREATOR_NAME = "org.springframework.aop.framework.target.internalTargetSourceCreator";
+	public static final String CONFIGURABLE_TARGET_SOURCE_CREATOR_NAME =
+			"org.springframework.aop.framework.target.internalTargetSourceCreator";
 
-	public static final String ASPECTJ_AUTO_PROXY_CREATOR = "org.springframework.aop.framework.autoproxy.AspectJAutoProxyCreator";
+	public static final String ASPECTJ_AUTO_PROXY_CREATOR =
+			"org.springframework.aop.framework.autoproxy.AspectJAutoProxyCreator";
+
 
 	public static void registerAutoProxyCreatorIfNecessary(BeanDefinitionRegistry registry) {
 		if (registry.containsBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME)) {
@@ -58,13 +57,15 @@ public abstract class NamespaceHandlerUtils {
 		Class ajApcClass = getAspectJAutoProxyCreatorClassIfPossible();
 
 		if (ajApcClass == null) {
-			throw new IllegalStateException("Unable to register AspectJ AutoProxyCreator. Cannot find class ["
-					+ ASPECTJ_AUTO_PROXY_CREATOR + "]. Are you running on Java 5.0+?");
+			throw new IllegalStateException(
+					"Unable to register AspectJ AutoProxyCreator. Cannot find class [" +
+					ASPECTJ_AUTO_PROXY_CREATOR + "]. Are you running on Java 5.0+?");
 		}
 
 
 		if (registry.containsBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME)) {
-			AbstractBeanDefinition definition = (AbstractBeanDefinition) registry.getBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME);
+			AbstractBeanDefinition definition =
+					(AbstractBeanDefinition) registry.getBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME);
 			if (baseApcClass.equals(definition.getBeanClass())) {
 				// switch APC type
 				definition.setBeanClass(ajApcClass);
@@ -80,8 +81,9 @@ public abstract class NamespaceHandlerUtils {
 		try {
 			return ClassUtils.forName(ASPECTJ_AUTO_PROXY_CREATOR);
 		}
-		catch (ClassNotFoundException e) {
+		catch (ClassNotFoundException ex) {
 			return null;
 		}
 	}
+
 }
