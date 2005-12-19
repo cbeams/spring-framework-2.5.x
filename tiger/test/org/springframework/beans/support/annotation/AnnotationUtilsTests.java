fd8de15fb98ff6a255eace97d543eff536fb4380
Juergen Hoeller <jhoeller@interface21.com>
2005-12-19 23:01:57 +0000

    removed superfluous import
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8677 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/beans/support/annotation/AnnotationUtilsTests.java
+++ tiger/test/org/springframework/beans/support/annotation/AnnotationUtilsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,8 +21,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Method;
 
 import junit.framework.TestCase;
-
-import org.springframework.beans.support.annotation.AnnotationUtils;
 
 /**
  * @author Rob Harrop
