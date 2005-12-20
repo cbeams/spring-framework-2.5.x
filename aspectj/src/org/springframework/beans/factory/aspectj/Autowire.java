913b73b6315beb712a0d7210ac0dd903e3708070
Juergen Hoeller <jhoeller@interface21.com>
2005-12-20 16:54:32 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8702 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- aspectj/src/org/springframework/beans/factory/aspectj/Autowire.java
+++ aspectj/src/org/springframework/beans/factory/aspectj/Autowire.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,17 +23,32 @@ import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
  * have its dependencies automatically injected by the Spring container using
  * setter injection. This is a core concept in Spring DI.
  *
- * <p>Note that constructor autowiring is not available here,
- * as bean creation methods will themselves invoke bean constructors.
+ * <p>This enumeration is intended for use with Spring's Configurable annotation
+ * and its associated AnnotationBeanConfigurer aspect (for AspectJ). Note that
+ * constructor autowiring is not available here, as our aspect is not in control
+ * over bean instantiation.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @since 2.0
+ * @see Configurable
  * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory
  */
 public enum Autowire {
 	
+	/**
+	 * Constant that indicates no autowiring at all.
+	 */
 	NO(AutowireCapableBeanFactory.AUTOWIRE_NO),
+
+	/**
+	 * Constant that indicates autowiring bean properties by name.
+	 */
 	BY_NAME(AutowireCapableBeanFactory.AUTOWIRE_BY_NAME),
+
+	/**
+	 * Constant that indicates autowiring bean properties by type.
+	 */
 	BY_TYPE(AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE);
 
 
