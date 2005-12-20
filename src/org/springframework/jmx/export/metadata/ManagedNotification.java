d7dcb17fa5438aefd3bb8dcb05797eeed5bd20ef
Juergen Hoeller <jhoeller@interface21.com>
2005-12-20 11:36:29 +0000

    removed overloaded "setNotificationTypes" variant (for JavaBean compliance)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8687 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jmx/export/metadata/ManagedNotification.java
+++ src/org/springframework/jmx/export/metadata/ManagedNotification.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.jmx.export.metadata;
 
-import org.springframework.util.StringUtils;
-
 /**
+ * Metadata that indicates a JMX notification emitted by a bean.
+ *
  * @author Rob Harrop
+ * @since 2.0
  */
 public class ManagedNotification {
 
@@ -13,31 +30,29 @@ public class ManagedNotification {
 
 	private String description;
 
-	public String[] getNotificationTypes() {
-		return notificationTypes;
-	}
-
-	public void setNotificationTypes(String notificationTypes) {
-		setNotificationTypes(StringUtils.commaDelimitedListToStringArray(notificationTypes));
-	}
 
 	public void setNotificationTypes(String[] notificationTypes) {
 		this.notificationTypes = notificationTypes;
 	}
 
-	public String getName() {
-		return name;
+	public String[] getNotificationTypes() {
+		return notificationTypes;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getDescription() {
-		return description;
+	public String getName() {
+		return name;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
 }
