15c7215d76816562992c3734215f073984bb67b7
Juergen Hoeller <jhoeller@interface21.com>
2005-12-22 13:07:58 +0000

    rearranged
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8749 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/Assert.java
+++ src/org/springframework/util/Assert.java
@@ -79,31 +79,6 @@ public abstract class Assert {
 	}
 
 	/**
-	 * Assert that an object is not null.
-	 * <pre>
-	 * Assert.notNull(clazz, "The class must not be null");</pre>
-	 * @param object the object to check
-	 * @param message the exception message to use if the assertion fails
-	 * @throws IllegalArgumentException if the object is <code>null</code>
-	 */
-	public static void notNull(Object object, String message) {
-		if (object == null) {
-			throw new IllegalArgumentException(message);
-		}
-	}
-
-	/**
-	 * Assert that an object is not null.
-	 * <pre>
-	 * Assert.notNull(clazz);</pre>
-	 * @param object the object to check
-	 * @throws IllegalArgumentException if the object is <code>null</code>
-	 */
-	public static void notNull(Object object) {
-		notNull(object, "[Assertion failed] - this argument is required; it cannot be null");
-	}
-
-	/**
 	 * Assert that an object is null.
 	 * <pre>
 	 * Assert.isNull(value, "The value must be null");</pre>
@@ -126,6 +101,31 @@ public abstract class Assert {
 	 */
 	public static void isNull(Object object) {
 		isNull(object, "[Assertion failed] - the object argument must be null");
+	}
+
+	/**
+	 * Assert that an object is not null.
+	 * <pre>
+	 * Assert.notNull(clazz, "The class must not be null");</pre>
+	 * @param object the object to check
+	 * @param message the exception message to use if the assertion fails
+	 * @throws IllegalArgumentException if the object is <code>null</code>
+	 */
+	public static void notNull(Object object, String message) {
+		if (object == null) {
+			throw new IllegalArgumentException(message);
+		}
+	}
+
+	/**
+	 * Assert that an object is not null.
+	 * <pre>
+	 * Assert.notNull(clazz);</pre>
+	 * @param object the object to check
+	 * @throws IllegalArgumentException if the object is <code>null</code>
+	 */
+	public static void notNull(Object object) {
+		notNull(object, "[Assertion failed] - this argument is required; it cannot be null");
 	}
 
 	/**
