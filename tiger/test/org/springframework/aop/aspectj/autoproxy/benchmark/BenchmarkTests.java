a6530320ccc33fc3bce4eebe8aa0ff85b65d2576
Rod Johnson <rjohnson@interface21.com>
2005-12-23 23:25:01 +0000

    Stronger benchmarks
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8782 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/aop/aspectj/autoproxy/benchmark/BenchmarkTests.java
+++ tiger/test/org/springframework/aop/aspectj/autoproxy/benchmark/BenchmarkTests.java
@@ -18,6 +18,7 @@ package org.springframework.aop.aspectj.autoproxy.benchmark;
 
 import junit.framework.TestCase;
 
+import org.springframework.aop.framework.Advised;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.ITestBean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -32,23 +33,53 @@ import org.springframework.util.StopWatch;
  */
 public class BenchmarkTests extends TestCase {
 	
+	private static final String ASPECTJ_CONTEXT = "/org/springframework/aop/aspectj/autoproxy/benchmark/aspectj.xml";
+
 	protected int getCount() {
 		return 1000000;
 	}
 	
-	public void testRepeatedInvocationsWithAspectJ() {
-		testRepeatedInvocations("/org/springframework/aop/aspectj/autoproxy/aspects.xml", getCount(), "AspectJ");
+	public void testRepeatedAroundAdviceInvocationsWithAspectJ() {
+		testRepeatedAroundAdviceInvocations(ASPECTJ_CONTEXT, getCount(), "AspectJ");
 	}
 	
-	public void testRepeatedInvocationsWithSpringAop() {
-		testRepeatedInvocations("/org/springframework/aop/aspectj/autoproxy/benchmark/springAop.xml", getCount(), "Spring AOP");
+	public void testRepeatedAroundAdviceInvocationsWithSpringAop() {
+		testRepeatedAroundAdviceInvocations(SPRING_AOP_CONTEXT(), getCount(), "Spring AOP");
 	}
 	
-	private long testRepeatedInvocations(String file, int howmany, String technology) {
+	public void testRepeatedBeforeAdviceInvocationsWithAspectJ() {
+		testBeforeAdviceWithoutJoinPoint(ASPECTJ_CONTEXT, getCount(), "AspectJ");
+	}
+	
+	public void testRepeatedBeforeAdviceInvocationsWithSpringAop() {
+		testBeforeAdviceWithoutJoinPoint(SPRING_AOP_CONTEXT(), getCount(), "Spring AOP");
+	}
+	
+	public void testRepeatedAfterReturningAdviceInvocationsWithAspectJ() {
+		testAfterReturningAdviceWithoutJoinPoint(ASPECTJ_CONTEXT, getCount(), "AspectJ");
+	}
+	
+	public void testRepeatedAfterReturningAdviceInvocationsWithSpringAop() {
+		testAfterReturningAdviceWithoutJoinPoint(SPRING_AOP_CONTEXT(), getCount(), "Spring AOP");
+	}
+
+	public void testRepeatedMixWithAspectJ() {
+		testMix(ASPECTJ_CONTEXT, getCount(), "AspectJ");
+	}
+	
+	public void testRepeatedMixWithSpringAop() {
+		testMix(SPRING_AOP_CONTEXT(), getCount(), "Spring AOP");
+	}
+
+	private String SPRING_AOP_CONTEXT() {
+		return "/org/springframework/aop/aspectj/autoproxy/benchmark/springAop.xml";
+	}
+	
+	private long testRepeatedAroundAdviceInvocations(String file, int howmany, String technology) {
 		ClassPathXmlApplicationContext bf = new ClassPathXmlApplicationContext(file);
 
 		StopWatch sw = new StopWatch();
-		sw.start(howmany + " repeated invocations with " + technology);
+		sw.start(howmany + " repeated around advice invocations with " + technology);
 		ITestBean adrian = (ITestBean) bf.getBean("adrian");
 		
 		assertTrue(AopUtils.isAopProxy(adrian));
@@ -63,8 +94,86 @@ public class BenchmarkTests extends TestCase {
 		return sw.getLastTaskTimeMillis();
 	}
 	
+	private long testBeforeAdviceWithoutJoinPoint(String file, int howmany, String technology) {
+		ClassPathXmlApplicationContext bf = new ClassPathXmlApplicationContext(file);
+
+		StopWatch sw = new StopWatch();
+		sw.start(howmany + " repeated before advice invocations with " + technology);
+		ITestBean adrian = (ITestBean) bf.getBean("adrian");
+		
+		assertTrue(AopUtils.isAopProxy(adrian));
+		Advised a = (Advised) adrian;
+		assertTrue(a.getAdvisors().length >= 3);
+		assertEquals("adrian", adrian.getName());
+		
+		for (int i = 0; i < howmany; i++) {
+			adrian.getName();
+		}
+		
+		sw.stop();
+		System.out.println(sw.prettyPrint());
+		return sw.getLastTaskTimeMillis();
+	}
+	
+	private long testAfterReturningAdviceWithoutJoinPoint(String file, int howmany, String technology) {
+		ClassPathXmlApplicationContext bf = new ClassPathXmlApplicationContext(file);
+
+		StopWatch sw = new StopWatch();
+		sw.start(howmany + " repeated after returning advice invocations with " + technology);
+		ITestBean adrian = (ITestBean) bf.getBean("adrian");
+		
+		assertTrue(AopUtils.isAopProxy(adrian));
+		Advised a = (Advised) adrian;
+		assertTrue(a.getAdvisors().length >= 3);
+		// Hits joinpoint
+		adrian.setAge(25);
+		
+		for (int i = 0; i < howmany; i++) {
+			adrian.setAge(i);
+		}
+		
+		sw.stop();
+		System.out.println(sw.prettyPrint());
+		return sw.getLastTaskTimeMillis();
+	}
+	
+	private long testMix(String file, int howmany, String technology) {
+		ClassPathXmlApplicationContext bf = new ClassPathXmlApplicationContext(file);
+
+		StopWatch sw = new StopWatch();
+		sw.start(howmany + " repeated mixed invocations with " + technology);
+		ITestBean adrian = (ITestBean) bf.getBean("adrian");
+		
+		assertTrue(AopUtils.isAopProxy(adrian));
+		Advised a = (Advised) adrian;
+		assertTrue(a.getAdvisors().length >= 3);
+		
+		for (int i = 0; i < howmany; i++) {
+			// Hit all 3 joinpoints
+			adrian.getAge();
+			adrian.getName();
+			adrian.setAge(i);
+			
+			// Invoke three non-advised methods
+			adrian.getDoctor();
+			adrian.getLawyer();
+			adrian.getSpouse();
+		}
+		
+		sw.stop();
+		System.out.println(sw.prettyPrint());
+		return sw.getLastTaskTimeMillis();
+	}
+	
+	// TODO test mixed
+	
+	// TODO test regular
+	
+	// TODO test n proxy creation
+	
+	// Useful for profilers that don't allow profiling unit tests
 	public static void main(String[] args) {
-		new BenchmarkTests().testRepeatedInvocationsWithAspectJ();
+		new BenchmarkTests().testRepeatedAroundAdviceInvocationsWithAspectJ();
 	}
 	
 }
