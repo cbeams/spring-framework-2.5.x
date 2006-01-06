e690b888a1d776171353f491853e77c169e5bb0e
Juergen Hoeller <jhoeller@interface21.com>
2006-01-06 23:43:39 +0000

    fixed constructor that takes a "batchSize" argument to correctly interpret it as batch size
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8899 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/object/BatchSqlUpdate.java
+++ src/org/springframework/jdbc/object/BatchSqlUpdate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -106,7 +106,8 @@ public class BatchSqlUpdate extends SqlUpdate {
 	 * @see java.sql.Types
 	 */
 	public BatchSqlUpdate(DataSource ds, String sql, int[] types, int batchSize) {
-		super(ds, sql, types, batchSize);
+		super(ds, sql, types);
+		setBatchSize(batchSize);
 	}
 
 
