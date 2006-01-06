8b1a947367372385eae73a50599c083bb670895c
Juergen Hoeller <jhoeller@interface21.com>
2006-01-06 22:29:20 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8894 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/hibernate3/LocalTransactionManagerLookup.java
+++ src/org/springframework/orm/hibernate3/LocalTransactionManagerLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,6 +45,7 @@ import org.hibernate.transaction.TransactionManagerLookup;
 public class LocalTransactionManagerLookup implements TransactionManagerLookup {
 
 	private final TransactionManager transactionManager;
+
 
 	public LocalTransactionManagerLookup() {
 		TransactionManager tm = LocalSessionFactoryBean.getConfigTimeTransactionManager();
