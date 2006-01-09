d0cef4e9ea5939903e72160e8cba8b2a4d80b314
Juergen Hoeller <jhoeller@interface21.com>
2006-01-09 22:48:23 +0000

    applied license header
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@8945 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/util/CollectionUtilsTests.java
+++ test/org/springframework/util/CollectionUtilsTests.java
@@ -1,13 +1,29 @@
+/*
+ * Copyright 2002-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.util;
 
-import junit.framework.TestCase;
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
+
+import junit.framework.TestCase;
 
 /**
  * @author Rob Harrop
@@ -22,12 +38,11 @@ public class CollectionUtilsTests extends TestCase {
 
 		List list = new ArrayList();
 		list.add(new Object());
-
 		assertFalse(CollectionUtils.isEmpty(list));
 
 		Map map = new HashMap();
 		map.put("foo", "bar");
-
 		assertFalse(CollectionUtils.isEmpty(map));
 	}
+
 }
