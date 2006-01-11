8d2d999745865e327b3e0f2a510240ff62c89f5b
Juergen Hoeller <jhoeller@interface21.com>
2006-01-11 21:00:50 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8983 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/scope/RequestContextListener.java
+++ src/org/springframework/web/context/scope/RequestContextListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,19 +26,21 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 /**
- * Listener for Servlet 2.4 containers. Exposes the request to the current thread,
+ * Listener for Servlet 2.4+ containers. Exposes the request to the current thread,
  * through both LocaleContextHolder and RequestContextHolder.
+ * To be registered as listener in <code>web.xml</code>.
  *
  * <p>Alternatively, Spring's RequestContextFilter and Spring's DispatcherServlet
  * also expose the same request context to the current thread.
  *
  * @author Steven Devijver
+ * @author Juergen Hoeller
+ * @since 2.0
  * @see javax.servlet.ServletRequestListener
  * @see org.springframework.context.i18n.LocaleContextHolder
  * @see org.springframework.web.context.scope.RequestContextHolder
  * @see org.springframework.web.filter.RequestContextFilter
  * @see org.springframework.web.servlet.DispatcherServlet
- * @since 2.0
  */
 public class RequestContextListener implements ServletRequestListener {
 
