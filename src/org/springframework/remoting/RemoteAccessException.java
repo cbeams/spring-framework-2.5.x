a9009adb49283fa00702ed3a67803f56bbc99008
Juergen Hoeller <jhoeller@interface21.com>
2006-01-12 22:26:52 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@8996 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/RemoteAccessException.java
+++ src/org/springframework/remoting/RemoteAccessException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ public class RemoteAccessException extends NestedRuntimeException {
 	/**
 	 * Constructor for RemoteAccessException.
 	 * @param msg the detail message
-	 * @param ex root cause (usually from using a underlying
+	 * @param ex root cause (usually from using an underlying
 	 * remoting API such as RMI)
 	 */
 	public RemoteAccessException(String msg, Throwable ex) {
