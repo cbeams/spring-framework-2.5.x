ee95672e77c72c25ff212626a77be4d423e1f366
Rob Harrop <rharrop@interface21.com>
2006-01-13 17:37:50 +0000

    Extended DefaultRequestToViewNameTranslator and polished the JavaDoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9005 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/DefaultRequestToViewNameTranslator.java
+++ src/org/springframework/web/servlet/view/DefaultRequestToViewNameTranslator.java
@@ -23,46 +23,123 @@ import org.springframework.util.StringUtils;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Default implementation of the {@link RequestToViewNameTranslator} interface that
+ * simply transforms the URI of the incoming request into the view name.
+ * <p/>
+ * The default transformation simply strips the leading slash and file extension of
+ * the URI and returns the result as the view name with the configured
+ * {@link #setPrefix prefix} and {@link #setSuffix suffix} added as appropriate.
+ * <p/>
+ * Stripping of the leading slash and file extension can be disabled using the
+ * {@link #setStripLeadingSlashes} and {@link #setStripExtensions} properties
+ * respectively.
+ *
  * @author Rob Harrop
  * @since 2.0M2
  */
 public class DefaultRequestToViewNameTranslator implements RequestToViewNameTranslator {
 
+	/**
+	 * The <code>/</code> character.
+	 */
 	private static final String SLASH = "/";
 
-	private UrlPathHelper pathHelper = new UrlPathHelper();
-
-	private String separator =SLASH;
-
-	private boolean stripExtensions = true;
-
-	private boolean stripLeadingSlashes = true;
-
+	/**
+	 * The <code>.</code> character.
+	 */
 	private static final String DOT = ".";
 
+	/**
+	 * {@link UrlPathHelper} implementation used to split up the incoming request URI.
+	 */
+	private UrlPathHelper pathHelper = new UrlPathHelper();
+
+	/**
+	 * The separator to use in place of slashes when generating the view name.
+	 */
+	private String separator =SLASH;
+
+	/**
+	 * The prefix to add to any generated view names.
+	 */
+	private String prefix = "";
+
+	/**
+	 * The suffix to add to any generated view names.
+	 */
+	private String suffix = "";
+
+	/**
+	 * Should file extensions be stripped from the URL?
+	 */
+	private boolean stripExtensions = true;
+
+	/**
+	 * Should leading slashes be stripped from the URL?
+	 */
+	private boolean stripLeadingSlashes = true;
+
+	/**
+	 * Sets the prefix to prepend to generated view names.
+	 */
+	public void setPrefix(String prefix) {
+		this.prefix = (prefix == null ? "" : prefix);
+	}
+
+	/**
+	 * Sets the suffix to append to generated view names.
+	 */
+	public void setSuffix(String suffix) {
+		this.suffix = (suffix == null ? "" : suffix);
+	}
+
+	/**
+	 * Sets the value that will replace '<code>/</code>' as the separator
+	 * in the view name. The default behaviour simply leaves '<code>/</code>'
+	 * as the separator.
+	 */
 	public void setSeparator(String separator) {
 		this.separator = separator;
 	}
 
+	/**
+	 * Sets whether or not file extensions should be stripped from the URI when
+	 * generating the view name. Default is <code>true</code>.
+	 */
 	public void setStripExtensions(boolean stripExtensions) {
 		this.stripExtensions = stripExtensions;
 	}
 
+	/**
+	 * Sets whether or not leadng slashes should be stripped from the URI when
+	 * generating the view name. Default is <code>true</code>.
+	 */
 	public void setStripLeadingSlashes(boolean stripLeadingSlashes) {
 		this.stripLeadingSlashes = stripLeadingSlashes;
 	}
 
+	/**
+	 * Sets the {@link UrlPathHelper} to use when manipulating the request URI
+	 */
 	public void setPathHelper(UrlPathHelper pathHelper) {
 		this.pathHelper = pathHelper;
 	}
 
+	/**
+	 * Translates the request URI of the incoming {@link HttpServletRequest} to the
+	 * view name based on the configured parameters.
+	 */
 	public final String translate(HttpServletRequest request) {
-		String pathInMapping = this.pathHelper.getPathWithinServletMapping(request);
-		return transformPath(pathInMapping);
+		String pathInMapping = this.pathHelper.getPathWithinApplication(request);
+		return this.prefix + transformPath(pathInMapping) + this.suffix;
 	}
 
-	protected String transformPath(String pathInMapping) {
-		String path = pathInMapping;
+	/**
+	 * Transforms the request URI (in the context of the webapp) stripping
+	 * slashes and extensions, and replacing the separator as required.
+	 */
+	protected String transformPath(String pathInApplication) {
+		String path = pathInApplication;
 
 		if(this.stripLeadingSlashes && path.startsWith(SLASH)) {
 			path = path.substring(1);
