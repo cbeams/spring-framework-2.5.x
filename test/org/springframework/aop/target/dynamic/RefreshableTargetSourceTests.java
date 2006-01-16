edb9ccc2fc413f2ceb622299a20f40727003ea4d
Juergen Hoeller <jhoeller@interface21.com>
2006-01-16 14:12:27 +0000

    renamed AbstractRefreshableTargetSourceTests to RefreshableTargetSourceTests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9056 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/target/dynamic/RefreshableTargetSourceTests.java
+++ test/org/springframework/aop/target/dynamic/RefreshableTargetSourceTests.java
@@ -21,7 +21,7 @@ import junit.framework.TestCase;
 /**
  * @author Rob Harrop
  */
-public class AbstractRefreshableTargetSourceTests extends TestCase {
+public class RefreshableTargetSourceTests extends TestCase {
 
 	/**
 	 * Test what happens when checking for refresh but not refreshing object
@@ -74,30 +74,25 @@ public class AbstractRefreshableTargetSourceTests extends TestCase {
 
 		Object a = ts.getTarget();
 		Object b = ts.getTarget();
-
 		assertEquals("Objects should be same", a, b);
 
 		Thread.sleep(500);
 
 		Object c = ts.getTarget();
-
 		assertEquals("A and C should be same", a, c);
 
 		Thread.sleep(600);
 
 		Object d = ts.getTarget();
-
 		assertNotNull("D should not be null", d);
 		assertFalse("A and D should not be equal", a.equals(d));
 
 		Object e = ts.getTarget();
-
 		assertEquals("D and E should be equal", d, e);
 
 		Thread.sleep(1100);
 
 		Object f = ts.getTarget();
-
 		assertFalse("E and F should be different", e.equals(f));
 	}
 
