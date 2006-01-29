5aabe88817c3e6e94c7c81fd0a504637cce10425
Juergen Hoeller <jhoeller@interface21.com>
2006-01-29 18:53:36 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9192 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/StaticListableBeanFactory.java
+++ src/org/springframework/beans/factory/support/StaticListableBeanFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,12 +34,12 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.util.StringUtils;
 
 /**
- * Static factory that allows to register existing singleton instances
+ * Static bean factory that allows to register existing singleton instances
  * programmatically. Does not have support for prototype beans and aliases.
  *
  * <p>Serves as example for a simple implementation of the ListableBeanFactory
  * interface, managing existing bean instances rather than creating new ones
- * based on bean definitions.
+ * based on bean definitions. Useful for simple tests.
  *
  * <p>For a full-fledged bean factory based on bean definitions, have a look
  * at DefaultListableBeanFactory.
@@ -53,6 +53,7 @@ public class StaticListableBeanFactory implements ListableBeanFactory {
 
 	/** Map from bean name to bean instance */
 	private final Map beans = new HashMap();
+
 
 	/**
 	 * Add a new singleton bean.
