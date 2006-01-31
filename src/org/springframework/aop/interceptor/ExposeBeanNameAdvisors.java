fcaa1d63e60ad10b230611c545e2ca7878b757e6
Juergen Hoeller <jhoeller@interface21.com>
2006-01-31 21:32:30 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9214 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/interceptor/ExposeBeanNameAdvisors.java
+++ src/org/springframework/aop/interceptor/ExposeBeanNameAdvisors.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package org.springframework.aop.interceptor;
 import org.aopalliance.aop.AspectException;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+
 import org.springframework.aop.Advisor;
 import org.springframework.aop.framework.ReflectiveMethodInvocation;
 import org.springframework.aop.support.DefaultIntroductionAdvisor;
@@ -27,14 +28,12 @@ import org.springframework.aop.support.DelegatingIntroductionInterceptor;
 import org.springframework.beans.factory.NamedBean;
 
 /**
- * Convenient methods for creating advisors 
- * that may be used when autoproxying beans
- * created with the Spring IoC container, binding the bean name
- * to the current invocation. May support a bean()
- * pointcut designator with AspectJ.
- * <br>
- * Typically used in Spring autoproxying, where the bean name is
- * known at proxy creation time.
+ * Convenient methods for creating advisors that may be used when autoproxying beans
+ * created with the Spring IoC container, binding the bean name to the current
+ * invocation. May support a <code>bean()</code> pointcut designator with AspectJ.
+ *
+ * <p>Typically used in Spring autoproxying, where the bean name is known
+ * at proxy creation time.
  * 
  * @author Rod Johnson
  * @since 2.0
@@ -42,11 +41,9 @@ import org.springframework.beans.factory.NamedBean;
  */
 public abstract class ExposeBeanNameAdvisors extends DefaultPointcutAdvisor {
 
-	private static final long serialVersionUID = 1L;
-	
 	/**
 	 * Binding for the bean name of the bean which is currently being invoked
-	 * in the ReflectiveMethodInvocation userAttributes Map
+	 * in the ReflectiveMethodInvocation userAttributes Map.
 	 */
 	private static final String BEAN_NAME_ATTRIBUTE = ExposeBeanNameAdvisors.class.getName() + ".beanName";
 
@@ -74,10 +71,11 @@ public abstract class ExposeBeanNameAdvisors extends DefaultPointcutAdvisor {
 		if (!(mi instanceof ReflectiveMethodInvocation)) {
 			throw new IllegalArgumentException("Not a Spring AOP ReflectiveMethodInvocation");
 		}
-		String beanName = (String) ((ReflectiveMethodInvocation) mi).getUserAttributes().get(BEAN_NAME_ATTRIBUTE);
+		ReflectiveMethodInvocation rmi = (ReflectiveMethodInvocation) mi;
+		String beanName = (String) rmi.getUserAttributes().get(BEAN_NAME_ATTRIBUTE);
 		if (beanName == null) {
-			String mesg = "Cannot get bean name: not set on MethodInvocation. Include ExposeBeanNameAdvisor in interceptor chain";
-			throw new AspectException(mesg, new Throwable(mesg));
+			throw new AspectException("Cannot get bean name: not set on MethodInvocation. " +
+					"Include ExposeBeanNameAdvisor in interceptor chain.");
 		}
 		return beanName;
 	}
@@ -94,10 +92,9 @@ public abstract class ExposeBeanNameAdvisors extends DefaultPointcutAdvisor {
 	
 	
 	/**
-	 * Create a new advisor that will expose the given bean name,
-	 * introducing the NamedBean interface to make the bean name accessible
-	 * without forcing the target object to be aware of this Spring IoC
-	 * concept
+	 * Create a new advisor that will expose the given bean name, introducing
+	 * the NamedBean interface to make the bean name accessible without forcing
+	 * the target object to be aware of this Spring IoC concept.
 	 * @param beanName bean name to expose
 	 */
 	public static Advisor createAdvisorIntroducingNamedBean(String beanName) {
@@ -119,7 +116,8 @@ public abstract class ExposeBeanNameAdvisors extends DefaultPointcutAdvisor {
 			return mi.proceed();
 		}
 	}
-	
+
+
 	private static class ExposeBeanNameIntroduction extends DelegatingIntroductionInterceptor implements NamedBean {
 		
 		private final String beanName; 
