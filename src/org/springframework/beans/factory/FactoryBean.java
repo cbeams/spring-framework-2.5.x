594197192e91197e26493a8c851d50b4a0db732e
Juergen Hoeller <jhoeller@interface21.com>
2006-02-01 18:14:12 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9219 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/FactoryBean.java
+++ src/org/springframework/beans/factory/FactoryBean.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -67,8 +67,8 @@ public interface FactoryBean {
 	 * <p>This method can be called <i>before</i> this FactoryBean has
 	 * been fully initialized. It must not rely on state created during
 	 * initialization; of course, it can still use such state if available.
-	 * <p><b>NOTE:</b> Autowiring will simply ignore FactoryBeans that
-	 * return null here. Therefore it is highly recommended to implement
+	 * <p><b>NOTE:</b> Autowiring will simply ignore FactoryBeans that return
+	 * <code>null</code> here. Therefore it is highly recommended to implement
 	 * this method properly, using the current state of the FactoryBean.
 	 * @return the type of object that this FactoryBean creates,
 	 * or <code>null</code> if not known at the time of the call
