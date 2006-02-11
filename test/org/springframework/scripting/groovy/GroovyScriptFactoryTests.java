eb0e680019934d6571581e2d6aa7aa7707146eff
Rick Evans <revans@interface21.com>
2006-02-11 13:00:29 +0000

    Added tests for lots of corner cases (misconfiguration, etc).
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9274 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/scripting/groovy/GroovyScriptFactoryTests.java
+++ test/org/springframework/scripting/groovy/GroovyScriptFactoryTests.java
@@ -17,17 +17,24 @@
 package org.springframework.scripting.groovy;
 
 import junit.framework.TestCase;
-
+import org.easymock.MockControl;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.aop.target.dynamic.Refreshable;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.JdkVersion;
 import org.springframework.scripting.Calculator;
 import org.springframework.scripting.Messenger;
+import org.springframework.scripting.ScriptSource;
+
+import java.io.FileNotFoundException;
 
 /**
+ * Unit tests for the GroovyScriptFactory class.
+ *
  * @author Rob Harrop
+ * @author Rick Evans
  */
 public class GroovyScriptFactoryTests extends TestCase {
 
@@ -71,6 +78,95 @@ public class GroovyScriptFactoryTests extends TestCase {
 
 		assertEquals("Message is incorrect after refresh.", desiredMessage, messenger.getMessage());
 		assertEquals("Incorrect refresh count", 2, refreshable.getRefreshCount());
+	}
+
+	public void testWithTwoClassesDefinedInTheOneGroovyFile_CorrectClassFirst() throws Exception {
+		if (JdkVersion.getMajorJavaVersion() < JdkVersion.JAVA_14) {
+			return;
+		}
+
+		ApplicationContext ctx =
+				new ClassPathXmlApplicationContext("org/springframework/scripting/groovy/twoClassesCorrectOneFirst.xml");
+		Messenger messenger = (Messenger) ctx.getBean("messenger");
+		assertNotNull(messenger);
+		assertEquals("Hello World!", messenger.getMessage());
+	}
+
+	public void testWithTwoClassesDefinedInTheOneGroovyFile_WrongClassFirst() throws Exception {
+		if (JdkVersion.getMajorJavaVersion() < JdkVersion.JAVA_14) {
+			return;
+		}
+
+		try {
+			ApplicationContext ctx =
+					new ClassPathXmlApplicationContext("org/springframework/scripting/groovy/twoClassesWrongOneFirst.xml");
+			ctx.getBean("messenger", Messenger.class);
+			fail("Must have failed: two classes defined in GroovyScriptFactory source, non-Messenger class defined first.");
+		}
+		// just testing for failure here, hence catching Exception...
+		catch (Exception expected) {
+		}
+	}
+
+	public void testCtorWithNullScriptSourceLocator() throws Exception {
+		try {
+			new GroovyScriptFactory(null);
+		}
+		catch (IllegalArgumentException expected) {
+		}
+	}
+
+	public void testCtorWithEmptyScriptSourceLocator() throws Exception {
+		try {
+			new GroovyScriptFactory("");
+		}
+		catch (IllegalArgumentException expected) {
+		}
+	}
+
+	public void testCtorWithWhitespacedScriptSourceLocator() throws Exception {
+		try {
+			new GroovyScriptFactory("\n   ");
+		}
+		catch (IllegalArgumentException expected) {
+		}
+	}
+
+	public void testWithInlineScriptWithLeadingWhitespace() throws Exception {
+		if (JdkVersion.getMajorJavaVersion() < JdkVersion.JAVA_14) {
+			return;
+		}
+
+		try {
+			new ClassPathXmlApplicationContext("org/springframework/scripting/groovy/lwspBadGroovyContext.xml");
+			fail("Must have thrown a BeanCreationException ('inline:' prefix was preceded by whitespace");
+		}
+		catch (BeanCreationException expected) {
+			assertTrue(expected.contains(FileNotFoundException.class));
+		}
+	}
+
+	public void testGetScriptedObjectDoesNotChokeOnNullInterfacesBeingPassedIn() throws Exception {
+		MockControl mock = MockControl.createControl(ScriptSource.class);
+		ScriptSource scriptSource = (ScriptSource) mock.getMock();
+		scriptSource.getScriptAsString();
+		mock.setDefaultReturnValue("class Bar {}");
+		mock.replay();
+
+		GroovyScriptFactory factory = new GroovyScriptFactory("a script source locator (doesn't matter here)");
+		Object scriptedObject = factory.getScriptedObject(scriptSource, null);
+		assertNotNull(scriptedObject);
+		mock.verify();
+	}
+
+	public void testGetScriptedObjectDoesChokeOnNullScriptSourceBeingPassedIn() throws Exception {
+		GroovyScriptFactory factory = new GroovyScriptFactory("a script source locator (doesn't matter here)");
+		try {
+			factory.getScriptedObject(null, null);
+			fail("Must have thrown a NullPointerException as per contract ('null' ScriptSource supplied");
+		}
+		catch (NullPointerException expected) {
+		}
 	}
 
 }
