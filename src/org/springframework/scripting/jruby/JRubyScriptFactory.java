14c2c57fe276b62be01e9cfdb61fa46edc952d12
Rick Evans <revans@interface21.com>
2006-02-11 15:16:14 +0000

    Added assert check on the required 'script' member variable Updated Javadoc with @exception clause as appropriate
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9276 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/scripting/jruby/JRubyScriptFactory.java
+++ src/org/springframework/scripting/jruby/JRubyScriptFactory.java
@@ -23,12 +23,17 @@ import org.jruby.exceptions.JumpException;
 import org.springframework.scripting.ScriptCompilationException;
 import org.springframework.scripting.ScriptFactory;
 import org.springframework.scripting.ScriptSource;
+import org.springframework.util.Assert;
 
 /**
- * ScriptFactory implementation for a JRuby script.
+ * {@link org.springframework.scripting.ScriptFactory} implementation
+ * for a JRuby script.
  *
- * <p>Typically used in combination with ScriptFactoryPostProcessor;
- * see the latter's javadoc for a configuration example.
+ * <p>Typically used in combination with a
+ * {@link org.springframework.scripting.support.ScriptFactoryPostProcessor};
+ * see the latter's
+ * {@link org.springframework.scripting.support.ScriptFactoryPostProcessor Javadoc}
+ * for a configuration example.
  *
  * @author Juergen Hoeller
  * @author Rob Harrop
@@ -38,9 +43,9 @@ import org.springframework.scripting.ScriptSource;
  */
 public class JRubyScriptFactory implements ScriptFactory {
 
-	private String scriptSourceLocator;
+	private final String scriptSourceLocator;
 
-	private Class[] scriptInterfaces;
+	private final Class[] scriptInterfaces;
 
 
 	/**
@@ -49,8 +54,13 @@ public class JRubyScriptFactory implements ScriptFactory {
 	 * Interpreted by the post-processor that actually creates the script.
 	 * @param scriptInterfaces the Java interfaces that the scripted object
 	 * is supposed to implement
+	 * @throws IllegalArgumentException if either of the supplied arguments is <code>null</code>;
+	 * or the supplied <code>scriptSourceLocator</code> argument is composed wholly of whitespace;
+	 * or if the supplied <code>scriptInterfaces</code> argument array has no elements
 	 */
 	public JRubyScriptFactory(String scriptSourceLocator, Class[] scriptInterfaces) {
+		Assert.hasText(scriptSourceLocator);
+		Assert.notEmpty(scriptInterfaces);
 		this.scriptSourceLocator = scriptSourceLocator;
 		this.scriptInterfaces = scriptInterfaces;
 	}
@@ -65,7 +75,8 @@ public class JRubyScriptFactory implements ScriptFactory {
 	}
 
 	/**
-	 * BeanShell scripts do require a config interface.
+	 * JRuby scripts do require a config interface.
+	 * @return <code>true</code> always
 	 */
 	public boolean requiresConfigInterface() {
 		return true;
