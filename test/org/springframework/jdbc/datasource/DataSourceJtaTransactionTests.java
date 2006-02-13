720721ddbf7a74d02eeaeb4d174cb35a8a71280b
Juergen Hoeller <jhoeller@interface21.com>
2006-02-13 12:25:43 +0000

    fixed tests with open outer connection to correctly expect final close call
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9302 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jdbc/datasource/DataSourceJtaTransactionTests.java
+++ test/org/springframework/jdbc/datasource/DataSourceJtaTransactionTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -252,17 +252,17 @@ public class DataSourceJtaTransactionTests extends TestCase {
 
 				if (accessAfterResume) {
 					try {
-						dsControl.verify();
-						conControl.verify();
-						dsControl.reset();
-						conControl.reset();
 						if (!openOuterConnection) {
+							dsControl.verify();
+							dsControl.reset();
 							ds.getConnection();
 							dsControl.setReturnValue(con, 1);
+							dsControl.replay();
 						}
+						conControl.verify();
+						conControl.reset();
 						con.close();
 						conControl.setVoidCallable(1);
-						dsControl.replay();
 						conControl.replay();
 					}
 					catch (SQLException ex) {
@@ -281,6 +281,15 @@ public class DataSourceJtaTransactionTests extends TestCase {
 
 				else {
 					if (openOuterConnection) {
+						try {
+							conControl.verify();
+							conControl.reset();
+							con.close();
+							conControl.setVoidCallable(1);
+							conControl.replay();
+						}
+						catch (SQLException ex) {
+						}
 						DataSourceUtils.releaseConnection(c, ds);
 					}
 				}
