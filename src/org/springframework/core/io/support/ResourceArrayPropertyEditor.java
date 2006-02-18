78d63392ae7e526b89c735200fa5a3ce90fbb3a0
Juergen Hoeller <jhoeller@interface21.com>
2006-02-18 12:01:16 +0000

    translate a set/list of patterns into a Resource array (rather than just one pattern)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@9347 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/io/support/ResourceArrayPropertyEditor.java
+++ src/org/springframework/core/io/support/ResourceArrayPropertyEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,20 @@ package org.springframework.core.io.support;
 
 import java.beans.PropertyEditorSupport;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
+import org.springframework.core.CollectionFactory;
+import org.springframework.core.io.Resource;
 import org.springframework.util.SystemPropertyUtils;
 
 /**
  * Editor for Resource descriptor arrays, to automatically convert String
  * location patterns (e.g. "file:C:/my*.txt" or "classpath*:myfile.txt")
- * to Resource array properties.
+ * to Resource array properties. Can also translate a collection or array
+ * of location patterns into a merged Resource array.
  *
  * <p>The path may contain ${...} placeholders, to be resolved as
  * system properties: e.g. ${user.dir}.
@@ -63,14 +70,61 @@ public class ResourceArrayPropertyEditor extends PropertyEditorSupport {
 	}
 
 
+	/**
+	 * Treat the given text as location pattern and convert it to a Resource array.
+	 */
 	public void setAsText(String text) {
-		String patternToUse = resolvePath(text).trim();
+		String pattern = resolvePath(text).trim();
 		try {
-			setValue(this.resourcePatternResolver.getResources(patternToUse));
+			setValue(this.resourcePatternResolver.getResources(pattern));
 		}
 		catch (IOException ex) {
 			throw new IllegalArgumentException(
-			    "Could not resolve resource location pattern [" + patternToUse + "]: " + ex.getMessage());
+			    "Could not resolve resource location pattern [" + pattern + "]: " + ex.getMessage());
+		}
+	}
+
+	/**
+	 * Treat the given value as collection or array and convert it to a Resource array.
+	 * Considers String elements as location patterns, and takes Resource elements as-is.
+	 */
+	public void setValue(Object value) throws IllegalArgumentException {
+		if (value instanceof Collection || (value instanceof Object[] && !(value instanceof Resource[]))) {
+			Collection input = (value instanceof Collection ? (Collection) value : Arrays.asList((Object[]) value));
+			Set merged = CollectionFactory.createLinkedSetIfPossible(8);
+			for (Iterator it = input.iterator(); it.hasNext();) {
+				Object element = it.next();
+				if (element instanceof String) {
+					// A location pattern: resolve it into a Resource array.
+					// Might point to a single resource or to multiple resources.
+					String pattern = resolvePath((String) element).trim();
+					try {
+						Resource[] resources = this.resourcePatternResolver.getResources(pattern);
+						for (int i = 0; i < resources.length; i++) {
+							merged.add(resources[i]);
+						}
+					}
+					catch (IOException ex) {
+						throw new IllegalArgumentException(
+								"Could not resolve resource location pattern [" + pattern + "]: " + ex.getMessage());
+					}
+				}
+				else if (element instanceof Resource) {
+					// A Resource object: add it to the set.
+					merged.add(element);
+				}
+				else {
+					throw new IllegalArgumentException("Cannot convert element [" + element +
+							"] to Resource: only location Strings and Resource objects supported");
+				}
+			}
+			super.setValue(merged.toArray(new Resource[merged.size()]));
+		}
+
+		else {
+			// An arbitrary value: probably a String or a Resource array.
+			// setAsText will be called for a String; a Resource array will be used as-is.
+			super.setValue(value);
 		}
 	}
 
