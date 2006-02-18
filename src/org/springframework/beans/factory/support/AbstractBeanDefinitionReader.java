312724c35aff51d86692f9bd86f1f2e8756d9e1f
Juergen Hoeller <jhoeller@interface21.com>
2006-02-18 12:13:55 +0000

    added assertion
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@9354 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/AbstractBeanDefinitionReader.java
+++ src/org/springframework/beans/factory/support/AbstractBeanDefinitionReader.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
 /**
@@ -68,6 +69,7 @@ public abstract class AbstractBeanDefinitionReader implements BeanDefinitionRead
 	 * @see org.springframework.core.io.support.PathMatchingResourcePatternResolver
 	 */
 	protected AbstractBeanDefinitionReader(BeanDefinitionRegistry beanFactory) {
+		Assert.notNull(beanFactory, "Bean factory must not be null");
 		this.beanFactory = beanFactory;
 
 		// Determine ResourceLoader to use.
