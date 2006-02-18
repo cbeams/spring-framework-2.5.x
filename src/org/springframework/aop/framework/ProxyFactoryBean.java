c25cbafb1217c6772b7c1287dd37c8730fb809e2
Rob Harrop <rharrop@interface21.com>
2006-02-18 12:12:12 +0000

    Added warning when using non-singleton proxies with singleton targets.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9352 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/ProxyFactoryBean.java
+++ src/org/springframework/aop/framework/ProxyFactoryBean.java
@@ -248,6 +248,10 @@ public class ProxyFactoryBean extends AdvisedSupport
 			return getSingletonInstance();
 		}
 		else {
+			if (this.targetName == null) {
+				logger.warn("Using non-singleton proxies with singleton targets is often undesirable." +
+								"Enable prototype proxies by setting the 'targetName' property.");
+			}
 			return newPrototypeInstance();
 		}
 	}
