da3e2da20bbdc22239c777a849a28cc2a299b75e
Juergen Hoeller <jhoeller@interface21.com>
2006-02-20 11:44:27 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@9385 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/multipart/support/StringMultipartFileEditor.java
+++ src/org/springframework/web/multipart/support/StringMultipartFileEditor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,12 +35,14 @@ public class StringMultipartFileEditor extends PropertyEditorSupport {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private String charsetName;
+	private final String charsetName;
+
 
 	/**
 	 * Create a new StringMultipartFileEditor, using the default charset.
 	 */
 	public StringMultipartFileEditor() {
+		this.charsetName = null;
 	}
 
 	/**
@@ -51,6 +53,7 @@ public class StringMultipartFileEditor extends PropertyEditorSupport {
 	public StringMultipartFileEditor(String charsetName) {
 		this.charsetName = charsetName;
 	}
+
 
 	public void setAsText(String text) {
 		setValue(text);
