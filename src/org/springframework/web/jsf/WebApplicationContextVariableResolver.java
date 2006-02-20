62082dc1c9cd35992b903861c8591046d7af0bac
Juergen Hoeller <jhoeller@interface21.com>
2006-02-20 16:29:45 +0000

    added assertion
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@9389 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/jsf/WebApplicationContextVariableResolver.java
+++ src/org/springframework/web/jsf/WebApplicationContextVariableResolver.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,6 +20,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.el.EvaluationException;
 import javax.faces.el.VariableResolver;
 
+import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
@@ -67,7 +68,16 @@ public class WebApplicationContextVariableResolver extends VariableResolver {
 	 * @param originalVariableResolver the original VariableResolver
 	 */
 	public WebApplicationContextVariableResolver(VariableResolver originalVariableResolver) {
+		Assert.notNull(originalVariableResolver, "Original JSF VariableResolver must not be null");
 		this.originalVariableResolver = originalVariableResolver;
+	}
+
+	/**
+	 * Return the original JSF VariableResolver that this resolver delegates to.
+	 * Used to resolve standard JSF-managed beans.
+	 */
+	protected final VariableResolver getOriginalVariableResolver() {
+		return originalVariableResolver;
 	}
 
 
@@ -83,7 +93,7 @@ public class WebApplicationContextVariableResolver extends VariableResolver {
 			value = getWebApplicationContext(context);
 		}
 		if (value == null) {
-			value = this.originalVariableResolver.resolveVariable(context, name);
+			value = getOriginalVariableResolver().resolveVariable(context, name);
 		}
 		return value;
 	}
