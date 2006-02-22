cfc4f333e559af38e8cf28052285c1eb2b44e1d9
Juergen Hoeller <jhoeller@interface21.com>
2006-02-22 21:56:25 +0000

    correctly synchronize access to count field
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@9416 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/interceptor/DebugInterceptor.java
+++ src/org/springframework/aop/interceptor/DebugInterceptor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,8 @@ import org.aopalliance.intercept.MethodInvocation;
  */
 public class DebugInterceptor extends SimpleTraceInterceptor {
 
-	private int count;
+	private volatile int count;
+
 
 	/**
 	 * Create a new DebugInterceptor with a static logger.
@@ -52,14 +53,18 @@ public class DebugInterceptor extends SimpleTraceInterceptor {
 		setUseDynamicLogger(useDynamicLogger);
 	}
 
+
 	public Object invoke(MethodInvocation invocation) throws Throwable {
-		this.count++;
+		synchronized (this) {
+			this.count++;
+		}
 		return super.invoke(invocation);
 	}
 
 	protected String getInvocationDescription(MethodInvocation invocation) {
 		return invocation + "; count=" + this.count;
 	}
+
 
 	/**
 	 * Return the number of times this interceptor has been invoked.
@@ -71,7 +76,7 @@ public class DebugInterceptor extends SimpleTraceInterceptor {
 	/**
 	 * Reset the invocation count to zero.
 	 */
-	public void resetCount() {
+	public synchronized void resetCount() {
 		this.count = 0;
 	}
 
