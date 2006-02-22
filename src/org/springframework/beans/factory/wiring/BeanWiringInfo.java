db644a6f9cefdaf822022e973931f3f35891a576
Rick Evans <revans@interface21.com>
2006-02-22 14:12:47 +0000

    Added some basic predicate-style assertions to method arguments. Made (implicit) constants in class explicitly static and final. Added Javadoc for 'throws'
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9406 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/wiring/BeanWiringInfo.java
+++ src/org/springframework/beans/factory/wiring/BeanWiringInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,51 +21,60 @@ import org.springframework.util.Assert;
 
 /**
  * Class holding wiring metadata information about a bean definition.
- * 
+ *
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @since 2.0
  */
 public class BeanWiringInfo {
-	
+
 	/**
 	 * Constant that indicates autowiring bean properties by name.
+	 *
 	 * @see #BeanWiringInfo(int, boolean)
 	 */
-	int AUTOWIRE_BY_NAME = 1;
+	public static final int AUTOWIRE_BY_NAME = 1;
 
 	/**
 	 * Constant that indicates autowiring bean properties by type.
+	 *
 	 * @see #BeanWiringInfo(int, boolean)
 	 */
-	int AUTOWIRE_BY_TYPE = 2;
+	public static final int AUTOWIRE_BY_TYPE = 2;
 
 
 	private String beanName = null;
 
 	private int autowireMode = AutowireCapableBeanFactory.AUTOWIRE_NO;
-	
+
 	private boolean dependencyCheck = false;
 
 
 	/**
 	 * Create a new BeanWiringInfo that points to the given bean name.
+	 *
 	 * @param beanName the name of the bean definition to take the property values from
+	 * @throws IllegalArgumentException if the supplied beanName is <code>null</code>, is empty, or consists wholly of whitespace
 	 */
 	public BeanWiringInfo(String beanName) {
-		Assert.notNull(beanName, "Bean name must not be null");
+		Assert.hasText(beanName, "Bean name must not be null, empty, or consist wholly of whitespace");
 		this.beanName = beanName;
 	}
-	
+
 	/**
 	 * Create a new BeanWiringInfo that indicates autowiring.
-	 * @param autowireMode either constant AUTOWIRE_BY_NAME or AUTOWIRE_BY_TYPE
+	 *
+	 * @param autowireMode	either constant AUTOWIRE_BY_NAME or AUTOWIRE_BY_TYPE
 	 * @param dependencyCheck whether to perform a dependency check for object
-	 * references in the bean instance (after autowiring)
+	 *                        references in the bean instance (after autowiring)
+	 * @throws IllegalArgumentException if the supplied autowireMode is not one of the allowed values
 	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#AUTOWIRE_BY_NAME
 	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#AUTOWIRE_BY_TYPE
 	 */
 	public BeanWiringInfo(int autowireMode, boolean dependencyCheck) {
+		if (autowireMode != AUTOWIRE_BY_NAME && autowireMode != AUTOWIRE_BY_TYPE) {
+			throw new IllegalArgumentException("Just constants AUTOWIRE_BY_NAME and AUTOWIRE_BY_TYPE allowed");
+		}
 		this.autowireMode = autowireMode;
 		this.dependencyCheck = dependencyCheck;
 	}
