5a75aea002b67907c03486c32b4a4c0e7667885e
Juergen Hoeller <jhoeller@interface21.com>
2006-02-23 09:06:56 +0000

    fixed "findByNamedQuery" to correctly apply parameter indexes starting from 1
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9425 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/orm/jpa/JpaTemplate.java
+++ tiger/src/org/springframework/orm/jpa/JpaTemplate.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -388,7 +388,7 @@ public class JpaTemplate extends JpaAccessor implements JpaOperations {
 				Query queryObject = em.createNamedQuery(queryName);
 				if (values != null) {
 					for (int i = 0; i < values.length; i++) {
-						queryObject.setParameter(i, values[i]);
+						queryObject.setParameter(i + 1, values[i]);
 					}
 				}
 				return queryObject.getResultList();
