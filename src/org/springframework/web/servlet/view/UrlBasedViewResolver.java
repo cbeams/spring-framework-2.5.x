a8d81f5421abb52639851aef98f782f102f19314
Juergen Hoeller <jhoeller@interface21.com>
2006-02-23 20:06:55 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9433 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/UrlBasedViewResolver.java
+++ src/org/springframework/web/servlet/view/UrlBasedViewResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -305,9 +305,9 @@ public class UrlBasedViewResolver extends AbstractCachingViewResolver {
 
 	/**
 	 * Overridden to implement check for "redirect:" prefix.
-	 * <p>Not possible in loadView, as overridden loadView versions in
-	 * subclasses might rely on the superclass always creating instances
-	 * of the required view class.
+	 * <p>Not possible in <code>loadView</code>, since overridden
+	 * <code>loadView</code> versions in subclasses might rely on the
+	 * superclass always creating instances of the required view class.
 	 * @see #loadView
 	 * @see #requiredViewClass
 	 */
