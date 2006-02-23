425049f3a0b363c3e96c7c30ce822a7742cbdfb9
Juergen Hoeller <jhoeller@interface21.com>
2006-02-23 20:34:36 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9438 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/TrueMethodMatcher.java
+++ src/org/springframework/aop/TrueMethodMatcher.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,6 @@
 
 package org.springframework.aop;
 
-import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 
@@ -30,7 +29,7 @@ class TrueMethodMatcher implements MethodMatcher, Serializable {
 	public static final TrueMethodMatcher INSTANCE = new TrueMethodMatcher();
 	
 	/**
-	 * Enforce Singleton
+	 * Enforce Singleton pattern.
 	 */
 	private TrueMethodMatcher() {
 	}
@@ -44,17 +43,16 @@ class TrueMethodMatcher implements MethodMatcher, Serializable {
 	}
 
 	public boolean matches(Method method, Class targetClass, Object[] args) {
-		// should never be invoked as isRuntime returns false
+		// Should never be invoked as isRuntime returns false.
 		throw new UnsupportedOperationException();
 	}
 	
 	/**
-	 * Required to support serialization.
-	 * Replaces with canonical instance on deserialization,
-	 * protecting Singleton pattern. 
-	 * Alternative to overriding equals().
+	 * Required to support serialization. Replaces with canonical
+	 * instance on deserialization, protecting Singleton pattern.
+	 * Alternative to overriding <code>equals()</code>.
 	 */
-	private Object readResolve() throws ObjectStreamException {
+	private Object readResolve() {
 		return INSTANCE;
 	}
 	
@@ -62,4 +60,5 @@ class TrueMethodMatcher implements MethodMatcher, Serializable {
 	public String toString() {
 		return "MethodMatcher.TRUE";
 	}
+
 }
