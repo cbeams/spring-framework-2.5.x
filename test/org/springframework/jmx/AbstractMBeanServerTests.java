0e7c5da276075b903d9b2150e9d171208494120b
Rick Evans <revans@interface21.com>
2006-03-02 08:45:52 +0000

    Added Apache licence and assertIsNotRegistered(..) method.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9497 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jmx/AbstractMBeanServerTests.java
+++ test/org/springframework/jmx/AbstractMBeanServerTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.springframework.jmx;
 
 import javax.management.MBeanServer;
@@ -45,5 +61,9 @@ public abstract class AbstractMBeanServerTests extends TestCase {
 
 	protected void assertIsRegistered(String message, ObjectName objectName) {
 		assertTrue(message, getServer().isRegistered(objectName));
+	}
+
+	protected void assertIsNotRegistered(String message, ObjectName objectName) {
+		assertFalse(message, getServer().isRegistered(objectName));
 	}
 }
