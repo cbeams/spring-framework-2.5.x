e8cbab74ece9e765802fc4ec79a98efef35101b4
Juergen Hoeller <jhoeller@interface21.com>
2006-03-04 00:42:16 +0000

    changed "ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE" to actually start with the package name
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9524 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/WebApplicationContext.java
+++ src/org/springframework/web/context/WebApplicationContext.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -51,7 +51,8 @@ public interface WebApplicationContext extends ApplicationContext {
 	 * @see org.springframework.web.context.support.WebApplicationContextUtils#getWebApplicationContext
 	 * @see org.springframework.web.context.support.WebApplicationContextUtils#getRequiredWebApplicationContext
 	 */
-	String ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE = WebApplicationContext.class + ".ROOT";
+	String ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE = WebApplicationContext.class.getName() + ".ROOT";
+
 
 	/**
 	 * Return the standard Servlet API ServletContext for this application.
