f4e56f23c2cc9650af000cca4b752cf26b590b24
Juergen Hoeller <jhoeller@interface21.com>
2006-03-04 00:23:54 +0000

    moved ControllerClassNameHandlerMapping from package "web.servlet.mvc.mapping" to "web.servlet.mvc"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9522 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/mvc/ControllerClassNameHandlerMapping.java
+++ src/org/springframework/web/servlet/mvc/ControllerClassNameHandlerMapping.java
@@ -14,22 +14,21 @@
  * limitations under the License.
  */
 
-package org.springframework.web.servlet.mvc.mapping;
+package org.springframework.web.servlet.mvc;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.BeansException;
+
 import org.springframework.util.ClassUtils;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
-import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 /**
  * Implementation of {@link HandlerMapping} that follows a simple convention for generating
  * URL path mappings from the class names of registered {@link Controller} beans.
- * <p/>
- * For simple {@link Controller} implementations (those that handle a single request type),
+ *
+ * <p>For simple {@link Controller} implementations (those that handle a single request type),
  * the convention is to take the {@link ClassUtils#getShortName short name} of the <code>Class</code>,
  * remove the 'Controller' suffix if it exists and return the remaining text, lowercased, as
  * the mapping, with a leading <code>/</code>. For example:
@@ -37,43 +36,50 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
  * 	<li><code>WelcomeController</code> -> <code>/welcome</code></li>
  * 	<li><code>HomeController</code> -> <code>/home</code></li>
  * </ul>
- * <p/>
- * For {@link MultiActionController MultiActionControllers} then a similar mapping is registed
+ *
+ * <p>For {@link MultiActionController MultiActionControllers} then a similar mapping is registed
  * except that all sub-paths are registed using the trailing wildcard pattern <code>/*</code>.
  * For example:
  * <ul>
  * 	<li><code>AdminController</code> -> <code>/welcome/*</code></li>
  * 	<li><code>CatalogController</code> -> <code>/catalog/*</code></li>
  * </ul>
- * For {@link MultiActionController} it is often useful to use this mapping strategy in
- * conjunction with the {@link org.springframework.web.servlet.mvc.multiaction.InternalPathMethodNameResolver}.
+ *
+ * <p>For {@link MultiActionController} it is often useful to use
+ * this mapping strategy in conjunction with the
+ * {@link org.springframework.web.servlet.mvc.multiaction.InternalPathMethodNameResolver}.
  *
  * @author Rob Harrop
  * @since 2.0
+ * @see org.springframework.web.servlet.mvc.Controller
+ * @see org.springframework.web.servlet.mvc.multiaction.MultiActionController
  */
 public class ControllerClassNameHandlerMapping extends AbstractUrlHandlerMapping implements HandlerMapping {
 
-	private static final Log logger = LogFactory.getLog(ControllerClassNameHandlerMapping.class);
-
 	/**
-	 * Common suffix at the end of {@link Controller} implementation classes. Removed when
-	 * generating the URL path.
+	 * Common suffix at the end of {@link Controller} implementation classes.
+	 * Removed when generating the URL path.
 	 */
 	private static final String CONTROLLER_SUFFIX = "Controller";
 
 
+	/** Logger available to subclasses */
+	protected final Log logger = LogFactory.getLog(getClass());
+
+
 	/**
-	 * Simply invokes {@link #detectHandlers()} method. Sub-classes may choose to override this
-	 * but must remember to invoke the super method.
+	 * Simply invokes the {@link #detectHandlers()} method.
+	 * Subclasses may choose to override this but must remember to invoke the super method.
 	 */
-	protected void initApplicationContext() throws BeansException {
+	protected void initApplicationContext() {
 		detectHandlers();
 	}
 
 	/**
-	 * Detects all the {@link Controller} beans registered in the
-	 * {@link org.springframework.context.ApplicationContext} and registers a
-	 * URL path mapping for each one based on rules defined {@link ControllerClassNameHandlerMapping here}.
+	 * Detect all the {@link Controller} beans registered in the
+	 * {@link org.springframework.context.ApplicationContext} and
+	 * registers a URL path mapping for each one based on rules defined
+	 * {@link ControllerClassNameHandlerMapping here}.
 	 * @see #generatePathMapping(Class)
 	 */
 	private void detectHandlers() {
@@ -82,12 +88,10 @@ public class ControllerClassNameHandlerMapping extends AbstractUrlHandlerMapping
 			String controllerBeanName = beanNames[i];
 			Class controllerClass = getApplicationContext().getType(controllerBeanName);
 			String urlPath = generatePathMapping(controllerClass);
-
 			if (logger.isDebugEnabled()) {
-				logger.debug("Registering Controller '" + controllerBeanName
-								+ "' as handler for URL path '" + urlPath + "'.");
+				logger.debug("Registering Controller '" + controllerBeanName +
+						"' as handler for URL path [" + urlPath + "]");
 			}
-
 			registerHandler(urlPath, controllerBeanName);
 		}
 	}
@@ -98,14 +102,16 @@ public class ControllerClassNameHandlerMapping extends AbstractUrlHandlerMapping
 	 */
 	protected String generatePathMapping(Class controllerClass) {
 		StringBuffer pathMapping = new StringBuffer("/");
-		String className = ClassUtils.getShortName(controllerClass.getName());
-		String path = className.endsWith(CONTROLLER_SUFFIX) ? className.substring(0, className.indexOf(CONTROLLER_SUFFIX)) : className;
 
+		String className = ClassUtils.getShortName(controllerClass.getName());
+		String path = className.endsWith(CONTROLLER_SUFFIX) ?
+				className.substring(0, className.indexOf(CONTROLLER_SUFFIX)) : className;
 		pathMapping.append(path.toLowerCase());
 
 		if (MultiActionController.class.isAssignableFrom(controllerClass)) {
 			pathMapping.append("/*");
 		}
+
 		return pathMapping.toString();
 	}
 
