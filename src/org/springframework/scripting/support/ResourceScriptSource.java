66ad6597032b8a9b3a33d8f352730effdad1bc63
Rick Evans <revans@interface21.com>
2006-03-06 12:55:46 +0000

    Removed redundant import.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9534 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/scripting/support/ResourceScriptSource.java
+++ src/org/springframework/scripting/support/ResourceScriptSource.java
@@ -18,7 +18,6 @@ package org.springframework.scripting.support;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.FileReader;
