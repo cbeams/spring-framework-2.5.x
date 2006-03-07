46b53f667cd6cf57f3992897340398e1770c88fb
Juergen Hoeller <jhoeller@interface21.com>
2006-03-07 22:43:19 +0000

    added overloaded constructors
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9549 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/NotWritablePropertyException.java
+++ src/org/springframework/beans/NotWritablePropertyException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,6 +31,27 @@ public class NotWritablePropertyException extends InvalidPropertyException {
 
 	private String[] possibleMatches = null;
 
+
+	/**
+	 * Create a new NotWritablePropertyException.
+	 * @param beanClass the offending bean class
+	 * @param propertyName the offending property name
+	 */
+	public NotWritablePropertyException(Class beanClass, String propertyName) {
+		super(beanClass, propertyName,
+				"Bean property '" + propertyName + "' is not writable or has an invalid setter method: " +
+				"Does the return type of the getter match the parameter type of the setter?");
+	}
+
+	/**
+	 * Create a new NotWritablePropertyException.
+	 * @param beanClass the offending bean class
+	 * @param propertyName the offending property name
+	 * @param msg the detail message
+	 */
+	public NotWritablePropertyException(Class beanClass, String propertyName, String msg) {
+		super(beanClass, propertyName, msg);
+	}
 
 	/**
 	 * Create a new NotWritablePropertyException.
