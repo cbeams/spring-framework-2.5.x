d6f2f5fd7837aae1784c776fc080210470bcd7d3
Juergen Hoeller <jhoeller@interface21.com>
2006-03-07 21:29:30 +0000

    turned non-abstract
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9548 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/DelegatingDataSource.java
+++ src/org/springframework/jdbc/datasource/DelegatingDataSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ import org.springframework.beans.factory.InitializingBean;
  * @see #getConnection()
  * @see #getConnection(String, String)
  */
-public abstract class DelegatingDataSource implements DataSource, InitializingBean {
+public class DelegatingDataSource implements DataSource, InitializingBean {
 
 	private DataSource targetDataSource;
 
