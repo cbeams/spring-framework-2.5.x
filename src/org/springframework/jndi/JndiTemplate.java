b6e05751afe3c35b72faa911ec49066d985d155b
Juergen Hoeller <jhoeller@interface21.com>
2006-03-08 10:45:19 +0000

    throw specific NameNotFoundExceptio and TypeMismatchNamingException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9558 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jndi/JndiTemplate.java
+++ src/org/springframework/jndi/JndiTemplate.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
 import javax.naming.NamingException;
 
 import org.apache.commons.logging.Log;
@@ -122,7 +123,7 @@ public class JndiTemplate {
 			public Object doInContext(Context ctx) throws NamingException {
 				Object located = ctx.lookup(name);
 				if (located == null) {
-					throw new NamingException(
+					throw new NameNotFoundException(
 							"JNDI object with [" + name + "] not found: JNDI implementation returned null");
 				}
 				return located;
@@ -145,7 +146,7 @@ public class JndiTemplate {
 	public Object lookup(String name, Class requiredType) throws NamingException {
 		Object jndiObject = lookup(name);
 		if (requiredType != null && !requiredType.isInstance(jndiObject)) {
-			throw new NamingException(
+			throw new TypeMismatchNamingException(
 					"Object [" + jndiObject + "] available at JNDI location [" +
 					name + "] is not assignable to [" + requiredType.getName() + "]");
 		}
--- test/org/springframework/jndi/JndiTemplateTests.java
+++ test/org/springframework/jndi/JndiTemplateTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,7 @@
 package org.springframework.jndi;
 
 import javax.naming.Context;
-import javax.naming.NamingException;
+import javax.naming.NameNotFoundException;
 
 import junit.framework.TestCase;
 import org.easymock.MockControl;
@@ -41,7 +41,7 @@ public class JndiTemplateTests extends TestCase {
 		mc.replay();
 
 		JndiTemplate jt = new JndiTemplate() {
-			protected Context createInitialContext() throws NamingException {
+			protected Context createInitialContext() {
 				return mock;
 			}
 		};
@@ -52,7 +52,7 @@ public class JndiTemplateTests extends TestCase {
 	}
 	
 	public void testLookupFails() throws Exception {
-		NamingException ne = new NamingException();
+		NameNotFoundException ne = new NameNotFoundException();
 		String name = "foo";
 		MockControl mc = MockControl.createControl(Context.class);
 		final Context mock = (Context) mc.getMock();
@@ -63,7 +63,7 @@ public class JndiTemplateTests extends TestCase {
 		mc.replay();
 
 		JndiTemplate jt = new JndiTemplate() {
-			protected Context createInitialContext() throws NamingException {
+			protected Context createInitialContext() {
 				return mock;
 			}
 		};
@@ -72,12 +72,65 @@ public class JndiTemplateTests extends TestCase {
 			jt.lookup(name);
 			fail("Should have thrown NamingException");
 		}
-		catch (NamingException ex) {
+		catch (NameNotFoundException ex) {
 			// Ok
 		}
 		mc.verify();
 	}
 	
+	public void testLookupReturnsNull() throws Exception {
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setReturnValue(null);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() {
+				return mock;
+			}
+		};
+
+		try {
+			jt.lookup(name);
+			fail("Should have thrown NamingException");
+		}
+		catch (NameNotFoundException ex) {
+			// Ok
+		}
+		mc.verify();
+	}
+
+	public void testLookupFailsWithTypeMismatch() throws Exception {
+		Object o = new Object();
+		String name = "foo";
+		MockControl mc = MockControl.createControl(Context.class);
+		final Context mock = (Context) mc.getMock();
+		mock.lookup(name);
+		mc.setReturnValue(o);
+		mock.close();
+		mc.setVoidCallable(1);
+		mc.replay();
+
+		JndiTemplate jt = new JndiTemplate() {
+			protected Context createInitialContext() {
+				return mock;
+			}
+		};
+
+		try {
+			jt.lookup(name, String.class);
+			fail("Should have thrown TypeMismatchNamingException");
+		}
+		catch (TypeMismatchNamingException ex) {
+			// Ok
+		}
+		mc.verify();
+	}
+
 	public void testBind() throws Exception {
 		Object o = new Object();
 		String name = "foo";
@@ -90,7 +143,7 @@ public class JndiTemplateTests extends TestCase {
 		mc.replay();
 		
 		JndiTemplate jt = new JndiTemplate() {
-			protected Context createInitialContext() throws NamingException {
+			protected Context createInitialContext() {
 				return mock;
 			}
 		};
@@ -111,7 +164,7 @@ public class JndiTemplateTests extends TestCase {
 		mc.replay();
 
 		JndiTemplate jt = new JndiTemplate() {
-			protected Context createInitialContext() throws NamingException {
+			protected Context createInitialContext() {
 				return mock;
 			}
 		};
@@ -131,7 +184,7 @@ public class JndiTemplateTests extends TestCase {
 		mc.replay();
 	
 		JndiTemplate jt = new JndiTemplate() {
-			protected Context createInitialContext() throws NamingException {
+			protected Context createInitialContext() {
 				return mock;
 			}
 		};
