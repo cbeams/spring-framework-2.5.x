04d0a844d63ad9c06479fd3dfa2c8884012817d0
Juergen Hoeller <jhoeller@interface21.com>
2006-03-08 10:51:36 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9560 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jndi/JndiObjectLocator.java
+++ src/org/springframework/jndi/JndiObjectLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,10 +82,7 @@ public abstract class JndiObjectLocator extends JndiLocatorSupport implements In
 		return expectedType;
 	}
 
-	/**
-	 * Check the "jndiName" property.
-	 */
-	public void afterPropertiesSet() throws NamingException {
+	public void afterPropertiesSet() throws IllegalArgumentException, NamingException {
 		if (!StringUtils.hasLength(getJndiName())) {
 			throw new IllegalArgumentException("jndiName is required");
 		}
