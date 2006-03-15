fc811b53b36ec9923bb64e865cf1bcd96e2992c4
Juergen Hoeller <jhoeller@interface21.com>
2006-03-15 20:53:26 +0000

    added null check
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9651 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/toplink/TopLinkOptimisticLockingFailureException.java
+++ src/org/springframework/orm/toplink/TopLinkOptimisticLockingFailureException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ import org.springframework.orm.ObjectOptimisticLockingFailureException;
 public class TopLinkOptimisticLockingFailureException extends ObjectOptimisticLockingFailureException {
 
 	public TopLinkOptimisticLockingFailureException(OptimisticLockException ex) {
-		super(ex.getObject().getClass(), "unknown", ex.getMessage(), ex);
+		super(ex.getObject() != null ? ex.getObject().getClass() : null, null, ex.getMessage(), ex);
 	}
 
 }
