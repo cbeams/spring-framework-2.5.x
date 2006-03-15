2c224e257627944d07b39f1ba6f17d39281a0c5b
Rick Evans <revans@interface21.com>
2006-03-15 13:54:00 +0000

    Added Javadoc.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9634 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/support/ParsedSql.java
+++ src/org/springframework/jdbc/support/ParsedSql.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,61 +19,111 @@ package org.springframework.jdbc.support;
 import java.util.List;
 
 /**
- * Class to hold information for any parsed SQL statements.
+ * Holds information for parsed SQL statements.
  *
  * @author Thomas Risberg
  * @since 2.0
  */
 public class ParsedSql {
+
 	private List parameters;
 	private String newSql;
 	private String sql;
 	private int parameterCount;
 	private int namedParameterCount;
 
+
+	/**
+	 * Creates a new instance of the {@link ParsedSql} class.
+	 */
 	public ParsedSql() {
 	}
 
+	/**
+	 * Creates a new instance of the {@link ParsedSql} class.
+	 * @param sql the SQL statement that is being (or is to be) parsed
+	 */
 	public ParsedSql(String sql) {
 		this.sql = sql;
 	}
 
+
+	/**
+	 * Gets all of the parameters (bind variables) to (and for) the parsed SQL statement.
+	 * @return the parameters to (and for) the parsed SQL statement
+	 */
 	public List getParameters() {
 		return parameters;
 	}
 
+	/**
+	 *  Sets the parameters (bind variables) to (and for) the parsed SQL statement.
+	 * @param parameters the parameters to (and for) the parsed SQL statement
+	 */
 	public void setParameters(List parameters) {
 		this.parameters = parameters;
 	}
 
+	/**
+	 * Gets the new (parsed) SQL.
+	 * @return the new (parsed) SQL
+	 */
 	public String getNewSql() {
 		return newSql;
 	}
 
+	/**
+	 * Sets the new (parsed) SQL.
+	 * @param newSql the new (parsed) SQL
+	 */
 	public void setNewSql(String newSql) {
 		this.newSql = newSql;
 	}
 
+	/**
+	 * Gets the SQL statement that is being (or is to be) parsed.
+	 * @return the SQL statement that is being (or is to be) parsed
+	 */
 	public String getSql() {
 		return sql;
 	}
 
+	/**
+	 * Sets the SQL statement that is being (or is to be) parsed.
+	 * @param sql the SQL statement that is being (or is to be) parsed
+	 */
 	public void setSql(String sql) {
 		this.sql = sql;
 	}
 
+	/**
+	 * Gets the count of all of the parameters to the SQL statement.
+	 * @return the count of all of the parameters to the SQL statement
+	 */
 	public int getParameterCount() {
 		return parameterCount;
 	}
 
+	/**
+	 * Sets the count of all of the parameters to the SQL statement.
+	 * @param parameterCount the count of all of the parameters to the SQL statement
+	 */
 	public void setParameterCount(int parameterCount) {
 		this.parameterCount = parameterCount;
 	}
 
+	/**
+	 * Gets the count of all of the named parameters to the SQL statement.
+	 * @return the count of all of the named parameters to the SQL statement
+	 */
 	public int getNamedParameterCount() {
 		return namedParameterCount;
 	}
 
+	/**
+	 * Sets the count of all of the named parameters to the SQL statement.
+	 * @param namedParameterCount the count of all of the (named) parameters to the SQL statement
+	 */
 	public void setNamedParameterCount(int namedParameterCount) {
 		this.namedParameterCount = namedParameterCount;
 	}
