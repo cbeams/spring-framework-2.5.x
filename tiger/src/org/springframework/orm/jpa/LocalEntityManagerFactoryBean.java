22c5989928b13d49e239f593de203f3b0fa23262
Juergen Hoeller <jhoeller@interface21.com>
2006-03-15 17:47:29 +0000

    removed support for obsolete "EntityManagerFactory.getEntityManager()" method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9641 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/orm/jpa/LocalEntityManagerFactoryBean.java
+++ tiger/src/org/springframework/orm/jpa/LocalEntityManagerFactoryBean.java
@@ -16,10 +16,6 @@
 
 package org.springframework.orm.jpa;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Properties;
 
 import javax.persistence.EntityManagerFactory;
@@ -34,7 +30,6 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.ClassUtils;
 
 /**
  * FactoryBean that creates a local JPA EntityManagerFactory instance.
@@ -68,8 +63,6 @@ public class LocalEntityManagerFactoryBean implements FactoryBean, InitializingB
 
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private EntityManagerFactory targetEntityManagerFactory;
-
 	private Class persistenceProviderClass;
 
 	private String entityManagerName;
@@ -78,22 +71,8 @@ public class LocalEntityManagerFactoryBean implements FactoryBean, InitializingB
 
 	private Properties jpaProperties;
 
-	private boolean exposeTransactionAwareEntityManagerFactory = true;
-
 
 	/**
-	 * Specify a target EntityManagerFactory to delegate to.
-	 * <p>This property will override any local factory settings, not
-	 * letting this FactoryBean create its own EntityManagerFactory.
-	 * This is mainly useful to create a Spring transaction-aware
-	 * EntityManagerFactory proxy for an existing EntityManagerFactory
-	 * as fetched from an application server's JNDI environment.
-	 * @see #setExposeTransactionAwareEntityManagerFactory
-	 */
-	public void setTargetEntityManagerFactory(EntityManagerFactory targetEntityManagerFactory) {
-		this.targetEntityManagerFactory = targetEntityManagerFactory;
-	}
-
 	/**
 	 * Set the PersistenceProvider implementation class to use for creating
 	 * the EntityManagerFactory. If not specified (which is the default),
@@ -131,48 +110,18 @@ public class LocalEntityManagerFactoryBean implements FactoryBean, InitializingB
 		this.jpaProperties = jpaProperties;
 	}
 
-	/**
-	 * Set whether to expose a transaction-aware proxy for the EntityManagerFactory,
-	 * returning the EntityManager that's associated with the current Spring-managed
-	 * transaction on <code>doGetEntityManager()</code>, if any.
-	 * <p>Default is "true", letting data access code work with the plain
-	 * JPA EntityManagerFactory and its <code>doGetEntityManager()</code> method,
-	 * while still being able to participate in current Spring-managed transactions:
-	 * with any transaction management strategy, either local or JTA / EJB CMT,
-	 * and any transaction synchronization mechanism, either Spring or JTA.
-	 * Furthermore, <code>doGetEntityManager()</code> will also seamlessly work with a
-	 * request-scoped EntityManager managed by OpenEntityManagerInViewFilter/Interceptor.
-	 * <p>Turn this flag off to expose the plain JPA EntityManagerFactory with
-	 * JPA's default <code>doGetEntityManager()</code> behavior, which usually only
-	 * supports plain JTA synchronization through the JTA TransactionManager.
-	 * @see javax.persistence.EntityManagerFactory#getEntityManager()
-	 * @see org.springframework.transaction.jta.JtaTransactionManager
-	 * @see JpaTransactionManager
-	 * @see org.springframework.orm.hibernate3.support.OpenSessionInViewFilter
-	 * @see org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor
-	 */
-	public void setExposeTransactionAwareEntityManagerFactory(boolean exposeTransactionAwareEntityManagerFactory) {
-		this.exposeTransactionAwareEntityManagerFactory = exposeTransactionAwareEntityManagerFactory;
-	}
-
 
 	/**
 	 * Initialize the EntityManagerFactory for the given configuration.
 	 * @throws javax.persistence.PersistenceException in case of JPA initialization errors
 	 */
 	public final void afterPropertiesSet() throws PersistenceException {
-		EntityManagerFactory emf = null;
-
-		if (this.targetEntityManagerFactory != null) {
-			// Use specified target EntityManagerFactory.
-			emf = this.targetEntityManagerFactory;
-		}
-		else if (this.persistenceProviderClass != null) {
+		if (this.persistenceProviderClass != null) {
 			// Create EntityManagerFactory directly through PersistenceProvider.
 			PersistenceProvider pp =
 					(PersistenceProvider) BeanUtils.instantiateClass(this.persistenceProviderClass);
-			emf = pp.createEntityManagerFactory(this.entityManagerName, this.jpaProperties);
-			if (emf == null) {
+			this.entityManagerFactory = pp.createEntityManagerFactory(this.entityManagerName, this.jpaProperties);
+			if (this.entityManagerFactory == null) {
 				throw new IllegalStateException(
 						"PersistenceProvider [" + this.persistenceProviderClass.getName() +
 						"] did not return an EntityManagerFactory for name '" + this.entityManagerName + "'");
@@ -180,31 +129,9 @@ public class LocalEntityManagerFactoryBean implements FactoryBean, InitializingB
 		}
 		else {
 			// Let JPA perform its PersistenceProvider autodetection.
-			emf = Persistence.createEntityManagerFactory(this.entityManagerName, this.jpaProperties);
+			this.entityManagerFactory =
+					Persistence.createEntityManagerFactory(this.entityManagerName, this.jpaProperties);
 		}
-
-		// Wrap EntityManagerFactory with transaction-aware proxy, if demanded.
-		if (this.exposeTransactionAwareEntityManagerFactory) {
-			this.entityManagerFactory = getTransactionAwareEntityManagerFactory(emf);
-		}
-		else {
-			this.entityManagerFactory = emf;
-		}
-	}
-
-	/**
-	 * Wrap the given EntityManagerFactory with a proxy that delegates every method call
-	 * to it but delegates <code>doGetEntityManager</code> calls to EntityManagerFactoryUtils,
-	 * for participating in Spring-managed transactions.
-	 * @param target the original EntityManagerFactory to wrap
-	 * @return the wrapped EntityManagerFactory
-	 * @see javax.persistence.EntityManagerFactory#getEntityManager()
-	 * @see EntityManagerFactoryUtils#doGetEntityManager(javax.persistence.EntityManagerFactory)
-	 */
-	protected EntityManagerFactory getTransactionAwareEntityManagerFactory(EntityManagerFactory target) {
-		Class[] ifcs = ClassUtils.getAllInterfaces(target);
-		return (EntityManagerFactory) Proxy.newProxyInstance(
-				getClass().getClassLoader(), ifcs, new TransactionAwareInvocationHandler(target));
 	}
 
 
@@ -231,60 +158,6 @@ public class LocalEntityManagerFactoryBean implements FactoryBean, InitializingB
 	public void destroy() {
 		logger.info("Closing JPA EntityManagerFactory");
 		this.entityManagerFactory.close();
-	}
-
-
-	/**
-	 * Invocation handler that delegates <code>doGetEntityManager()</code> calls
-	 * to EntityManagerFactoryUtils, for being aware of thread-bound transactions.
-	 */
-	private static class TransactionAwareInvocationHandler implements InvocationHandler {
-
-		private final EntityManagerFactory target;
-
-		public TransactionAwareInvocationHandler(EntityManagerFactory target) {
-			this.target = target;
-		}
-
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			// Invocation on EntityManagerFactory interface coming in...
-
-			if (method.getName().equals("getEntityManager")) {
-				// Handle getEntityManager method: return transactional EntityManager, if any.
-				try {
-					return EntityManagerFactoryUtils.doGetEntityManager((EntityManagerFactory) proxy);
-				}
-				catch (IllegalStateException springEx) {
-					// Not within a Spring-managed transaction with Spring synchronization:
-					// delegate to persistence provider's own synchronization mechanism.
-					// Will throw IllegalStateException if not supported by persistence provider.
-					try {
-						return this.target.getEntityManager();
-					}
-					catch (IllegalStateException jpaEx) {
-						throw new IllegalStateException(
-								"Could not obtain transactional EntityManager: neither from Spring [" +
-								springEx.getMessage() + "] nor from JPA provider [" + jpaEx.getMessage() + "]");
-					}
-				}
-			}
-			else if (method.getName().equals("equals")) {
-				// Only consider equal when proxies are identical.
-				return (proxy == args[0] ? Boolean.TRUE : Boolean.FALSE);
-			}
-			else if (method.getName().equals("hashCode")) {
-				// Use hashCode of SessionFactory proxy.
-				return new Integer(hashCode());
-			}
-
-			// Invoke method on target EntityManagerFactory.
-			try {
-				return method.invoke(this.target, args);
-			}
-			catch (InvocationTargetException ex) {
-				throw ex.getTargetException();
-			}
-		}
 	}
 
 }
