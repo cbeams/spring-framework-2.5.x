c28d514daa51b0a2e9b0244fc28992d374524c2c
Juergen Hoeller <jhoeller@interface21.com>
2006-03-15 20:53:12 +0000

    fixed typo
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9650 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/toplink/TopLinkQueryException.java
+++ src/org/springframework/orm/toplink/TopLinkQueryException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import org.springframework.dao.InvalidDataAccessResourceUsageException;
 
 /**
  * TopLink-specific subclass of InvalidDataAccessResourceUsageException,
- * thrown on invalid TopLik query syntax or behavior.
+ * thrown on invalid TopLink query syntax or behavior.
  *
  * @author Juergen Hoeller
  * @since 1.2
