cc2db04066c3fe0d213d8a12202bccdd3b675211
Juergen Hoeller <jhoeller@interface21.com>
2006-03-21 08:47:43 +0000

    fixed copyright notice
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9662 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/filter/DelegatingFilterProxy.java
+++ src/org/springframework/web/filter/DelegatingFilterProxy.java
@@ -1,10 +1,11 @@
-/* Copyright 2004, 2005 Acegi Technology Pty Limited
+/*
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -91,7 +92,7 @@ public class DelegatingFilterProxy extends GenericFilterBean {
 	 * Set whether to invoke the <code>Filter.init</code> and
 	 * <code>Filter.destroy</code> lifecycle methods on the target bean.
 	 * <p>Default is "false"; target beans usually rely on the Spring application
-	 * context for managing their lifecycle. Setting this flag to true means
+	 * context for managing their lifecycle. Setting this flag to "true" means
 	 * that the servlet container will control the lifecycle of the target
 	 * Filter, with this proxy delegating the corresponding calls.
 	 */
