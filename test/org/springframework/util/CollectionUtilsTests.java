8f565ed13c227a73306f2123025776e88687df22
Juergen Hoeller <jhoeller@interface21.com>
2006-03-21 09:38:14 +0000

    fixed "hasUniqueObject" to correctly handle null values
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9667 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/util/CollectionUtilsTests.java
+++ test/org/springframework/util/CollectionUtilsTests.java
@@ -64,4 +64,37 @@ public class CollectionUtilsTests extends TestCase {
 		assertTrue(CollectionUtils.contains(ht.keys(), "myElement"));
 	}
 
+	public void testHasUniqueObject() {
+		List list = new LinkedList();
+		list.add("myElement");
+		list.add("myOtherElement");
+		assertFalse(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		list.add("myElement");
+		assertTrue(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		list.add("myElement");
+		list.add(null);
+		assertFalse(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		list.add(null);
+		list.add("myElement");
+		assertFalse(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		list.add(null);
+		list.add(null);
+		assertTrue(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		list.add(null);
+		assertTrue(CollectionUtils.hasUniqueObject(list));
+
+		list = new LinkedList();
+		assertFalse(CollectionUtils.hasUniqueObject(list));
+	}
+
 }
