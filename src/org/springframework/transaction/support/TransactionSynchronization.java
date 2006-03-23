759e99b2a55abc843c6f1a79e5e1de48d0e414b8
Rick Evans <revans@interface21.com>
2006-03-23 19:40:39 +0000

    Minor Javadoc - corrected typo.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9701 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/support/TransactionSynchronization.java
+++ src/org/springframework/transaction/support/TransactionSynchronization.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ public interface TransactionSynchronization {
 	 * Invoked before transaction commit/rollback.
 	 * Can e.g. perform resource cleanup, <i>before</i> transaction completion.
 	 * <p>This method will be invoked after <code>beforeCommit</code>, even
-	 * if <code>beforeCommit</code> threw an exception). This callbacks allows
+	 * if <code>beforeCommit</code> threw an exception). This callback allows
 	 * for closing resources before transaction completion, for any outcome.
 	 * @throws RuntimeException in case of errors; will be logged
 	 * (note: do not throw TransactionException subclasses here!)
