0bdb303c350af8e2f38f6362c46784a8795928e5
Rick Evans <revans@interface21.com>
2006-03-23 19:38:29 +0000

    Minoir Javadoc - Added <pre class="code"> marker to class level example.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9699 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/hibernate3/HibernateInterceptor.java
+++ src/org/springframework/orm/hibernate3/HibernateInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
  * allowCreate=false, if the code relies on the interceptor to provide proper
  * Session handling. Typically, the code will look as follows:
  *
- * <pre>
+ * <pre class="code">
  * public void doHibernateAction() {
  *   Session session = SessionFactoryUtils.getSession(this.sessionFactory, false);
  *   try {
