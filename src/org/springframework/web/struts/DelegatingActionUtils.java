8625c53e6e2fbab2d37585bf92ffba77ee1f5b98
Juergen Hoeller <jhoeller@interface21.com>
2006-03-26 17:54:34 +0000

    added Autowiring(Tiles)RequestProcessor, autowiring created Actions (configurable through ActionServlet init-params)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9736 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/struts/DelegatingActionUtils.java
+++ src/org/springframework/web/struts/DelegatingActionUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionServlet;
 import org.apache.struts.config.ModuleConfig;
 
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -43,7 +44,32 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  */
 public abstract class DelegatingActionUtils {
 
-	protected static final Log logger = LogFactory.getLog(DelegatingActionUtils.class);
+	/**
+	 * The name of the autowire init-param specified on the Struts ActionServlet:
+	 * "spring.autowire"
+	 */
+	public static final String PARAM_AUTOWIRE = "spring.autowire";
+
+	/**
+	 * The name of the dependency check init-param specified on the Struts ActionServlet:
+	 * "spring.dependencyCheck"
+	 */
+	public static final String PARAM_DEPENDENCY_CHECK = "spring.dependencyCheck";
+
+	/**
+	 * Value of the autowire init-param that indicates autowiring by name:
+	 * "byName"
+	 */
+	public static final String AUTOWIRE_BY_NAME = "byName";
+
+	/**
+	 * Value of the autowire init-param that indicates autowiring by type:
+	 * "byType"
+	 */
+	public static final String AUTOWIRE_BY_TYPE = "byType";
+
+
+	private static final Log logger = LogFactory.getLog(DelegatingActionUtils.class);
 
 
 	/**
@@ -141,6 +167,44 @@ public abstract class DelegatingActionUtils {
 					prefix + "' delegating to Spring bean with name [" + beanName + "]");
 		}
 		return beanName;
+	}
+
+	/**
+	 * Determine the autowire mode from the "autowire" init-param of the
+	 * Struts ActionServlet, falling back to "AUTOWIRE_BY_TYPE" as default.
+	 * @param actionServlet the Struts ActionServlet
+	 * @return the autowire mode to use
+	 * @see #PARAM_AUTOWIRE
+	 * @see #AUTOWIRE_BY_NAME
+	 * @see #AUTOWIRE_BY_TYPE
+	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#autowireBeanProperties
+	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#AUTOWIRE_BY_TYPE
+	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#AUTOWIRE_BY_NAME
+	 */
+	public static int getAutowireMode(ActionServlet actionServlet) {
+		String autowire = actionServlet.getInitParameter(PARAM_AUTOWIRE);
+		if (autowire != null) {
+			if (AUTOWIRE_BY_NAME.equals(autowire)) {
+				return AutowireCapableBeanFactory.AUTOWIRE_BY_NAME;
+			}
+			else if (!AUTOWIRE_BY_TYPE.equals(autowire)) {
+				throw new IllegalArgumentException("ActionServlet 'autowire' parameter must be 'byName' or 'byType'");
+			}
+		}
+		return AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE;
+	}
+
+	/**
+	 * Determine the dependency check to use from the "dependencyCheck" init-param
+	 * of the Struts ActionServlet, falling back to no dependency check as default.
+	 * @param actionServlet the Struts ActionServlet
+	 * @return whether to enforce a dependency check or not
+	 * @see #PARAM_DEPENDENCY_CHECK
+	 * @see org.springframework.beans.factory.config.AutowireCapableBeanFactory#autowireBeanProperties
+	 */
+	public static boolean getDependencyCheck(ActionServlet actionServlet) {
+		String dependencyCheck = actionServlet.getInitParameter(PARAM_DEPENDENCY_CHECK);
+		return Boolean.valueOf(dependencyCheck).booleanValue();
 	}
 
 }
