b69ceac96e6d87681a74cac80e8d3710a1ac62cf
Juergen Hoeller <jhoeller@interface21.com>
2006-03-29 11:44:28 +0000

    keep and expose the offending method name
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9751 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/mvc/multiaction/NoSuchRequestHandlingMethodException.java
+++ src/org/springframework/web/servlet/mvc/multiaction/NoSuchRequestHandlingMethodException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,13 @@ import javax.servlet.http.HttpServletRequest;
  * Exception thrown when there's no request handling method for a request.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  */
 public class NoSuchRequestHandlingMethodException extends ServletException {
-	
+
+	private String methodName;
+
+
 	/**
 	 * Create a new NoSuchRequestHandlingMethodException for the given request.
 	 * @param request the offending HTTP request
@@ -42,6 +46,15 @@ public class NoSuchRequestHandlingMethodException extends ServletException {
 	public NoSuchRequestHandlingMethodException(String methodName, Class controllerClass) {
 		super("No request handling method with name '" + methodName +
 				"' in class [" + controllerClass.getName() + "]");
+		this.methodName = methodName;
+	}
+
+
+	/**
+	 * Return the name of the offending method, if known.
+	 */
+	public String getMethodName() {
+		return methodName;
 	}
 
 }
