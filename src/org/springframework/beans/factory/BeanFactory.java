0e987075836278df4231e46b3fa3ddd3f4134f9b
Juergen Hoeller <jhoeller@interface21.com>
2006-04-05 08:09:12 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9764 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/BeanFactory.java
+++ src/org/springframework/beans/factory/BeanFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -156,17 +156,18 @@ public interface BeanFactory {
 	boolean containsBean(String name);
 
 	/**
-	 * Is this bean a singleton? That is, will getBean() always return the same object?
+	 * Is this bean a singleton? That is, will <code>getBean</code> always return the same object?
 	 * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
 	 * @param name the name of the bean to query
 	 * @return is this bean a singleton
 	 * @throws NoSuchBeanDefinitionException if there is no bean with the given name
+	 * @see #getBean
 	 */
 	boolean isSingleton(String name) throws NoSuchBeanDefinitionException;
 
 	/**
 	 * Determine the type of the bean with the given name.
-	 * More specifically, checks the type of object that getBean would return.
+	 * More specifically, checks the type of object that <code>getBean</code> would return.
 	 * For a FactoryBean, returns the type of object that the FactoryBean creates.
 	 * @param name the name of the bean to query
 	 * @return the type of the bean, or <code>null</code> if not determinable
