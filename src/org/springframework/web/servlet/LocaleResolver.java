ac4fa9ee73f102e67d5851f01029ad916f5c4e33
Juergen Hoeller <jhoeller@interface21.com>
2006-04-05 07:58:09 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9760 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/LocaleResolver.java
+++ src/org/springframework/web/servlet/LocaleResolver.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,8 +30,8 @@ import javax.servlet.http.HttpServletResponse;
  * cookies, etc. The default implementation is AcceptHeaderLocaleResolver,
  * simply using the request's locale provided by the respective HTTP header.
  *
- * <p>Use RequestContext.getLocale() to retrieve the current locale in
- * controllers or views, independent of the actual resolution strategy.
+ * <p>Use <code>RequestContext.getLocale()</code> to retrieve the current locale
+ * in controllers or views, independent of the actual resolution strategy.
  *
  * @author Juergen Hoeller
  * @since 27.02.2003
