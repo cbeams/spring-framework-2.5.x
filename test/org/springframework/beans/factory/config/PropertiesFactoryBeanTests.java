de01bf992842cc893d9cb8b07d081961bd80f757
Juergen Hoeller <jhoeller@interface21.com>
2006-04-19 19:43:58 +0000

    added "propertiesArray" bean property, for merging multiple local Properties instances
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9966 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/config/PropertiesFactoryBeanTests.java
+++ test/org/springframework/beans/factory/config/PropertiesFactoryBeanTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -71,6 +71,34 @@ public class PropertiesFactoryBeanTests extends TestCase {
 		Properties props = (Properties) pfb.getObject();
 		assertEquals("99", props.getProperty("tb.array[0].age"));
 		assertEquals("value2", props.getProperty("key2"));
+	}
+
+	public void testWithPropertiesFileAndMultipleLocalProperties() throws Exception {
+		PropertiesFactoryBean pfb = new PropertiesFactoryBean();
+		pfb.setLocation(new ClassPathResource("/org/springframework/beans/factory/config/test.properties"));
+
+		Properties props1 = new Properties();
+		props1.setProperty("key2", "value2");
+		props1.setProperty("tb.array[0].age", "0");
+
+		Properties props2 = new Properties();
+		props2.setProperty("spring", "framework");
+		props2.setProperty("Don", "Mattingly");
+
+		Properties props3 = new Properties();
+		props3.setProperty("spider", "man");
+		props3.setProperty("bat", "man");
+
+		pfb.setPropertiesArray(new Properties[] {props1, props2, props3});
+		pfb.afterPropertiesSet();
+
+		Properties props = (Properties) pfb.getObject();
+		assertEquals("99", props.getProperty("tb.array[0].age"));
+		assertEquals("value2", props.getProperty("key2"));
+		assertEquals("framework", props.getProperty("spring"));
+		assertEquals("Mattingly", props.getProperty("Don"));
+		assertEquals("man", props.getProperty("spider"));
+		assertEquals("man", props.getProperty("bat"));
 	}
 
 	public void testWithPropertiesFileAndLocalPropertiesAndLocalOverride() throws Exception {
