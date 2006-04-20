0f834c4334559658bb7c66be05ff28d36fc2b3fc
Juergen Hoeller <jhoeller@interface21.com>
2006-04-20 10:33:29 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@9980 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/support/GeneratedKeyHolder.java
+++ src/org/springframework/jdbc/support/GeneratedKeyHolder.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,6 +40,7 @@ public class GeneratedKeyHolder implements KeyHolder {
 
 	private final List keyList;
 
+
 	/**
 	 * Create a new GeneratedKeyHolder with a default list.
 	 */
@@ -54,6 +55,7 @@ public class GeneratedKeyHolder implements KeyHolder {
 	public GeneratedKeyHolder(List keyList) {
 		this.keyList = keyList;
 	}
+
 
 	public Number getKey() throws InvalidDataAccessApiUsageException, DataRetrievalFailureException {
 		if (this.keyList.size() == 0) {
