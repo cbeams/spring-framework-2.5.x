65f352dd9748c05af94846642797fb332ce479a0
Juergen Hoeller <jhoeller@interface21.com>
2006-04-23 09:31:04 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10033 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/beans/factory/annotation/RequiredBeanFactoryPostProcessor.java
+++ tiger/src/org/springframework/beans/factory/annotation/RequiredBeanFactoryPostProcessor.java
@@ -16,6 +16,12 @@
 
 package org.springframework.beans.factory.annotation;
 
+import java.beans.PropertyDescriptor;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -24,17 +30,11 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.util.Assert;
 
-import java.beans.PropertyDescriptor;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * A {@link BeanFactoryPostProcessor} implementation that ensures that JavaBean
  * properties on beans that are marked with an (arbitrary) {@link Annotation}
  * are actually (configured to be) dependency-injected with a value.
- * 
+ *
  * <p>The motivation for the existence of this {@link BeanFactoryPostProcessor}
  * is to allow developers to annotate the setter properties of their own classes
  * with an arbitrary {@link Annotation} to indicate that the container must check
@@ -42,7 +42,7 @@ import java.util.List;
  * responsibility for such checking onto the container (where it arguably belongs),
  * and obviates the need (<b>in part</b>) for a developer to code a method that
  * simply checks that all required properties have actually been set.
- * 
+ *
  * <p>Please note that a 'checking' method may still need to implemented (and may
  * still be desirable), because all that this class does is enforce that a
  * 'required' property has actually been configured with a value. It does
@@ -57,90 +57,89 @@ import java.util.List;
  */
 public class RequiredBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
-    private Class<? extends Annotation> requiredAnnotationType = Required.class;
+	private Class<? extends Annotation> requiredAnnotationType = Required.class;
 
 
-    /**
-     * Sets the required annotation type.
-     * <p>This setter property exists so that developers can provide their own
-     * (non-Spring-specific) annotation type to indicate that a property value
-     * is required.
-     * <p>The default required annotation type is the {@link Required} annotation.
-     * @param requiredAnnotationType the desired annotation type
-     * @throws IllegalArgumentException if the supplied <code>requiredAnnotationType</code> argument is <code>null</code>
-     */
-    public void setRequiredAnnotationType(Class<? extends Annotation> requiredAnnotationType) {
-        Assert.notNull(requiredAnnotationType, "'requiredAnnotationType' cannot be null.");
-        this.requiredAnnotationType = requiredAnnotationType;
-    }
+	/**
+	 * Set the required annotation type.
+	 * The default required annotation type is the {@link Required} annotation.
+	 * <p>This setter property exists so that developers can provide their own
+	 * (non-Spring-specific) annotation type to indicate that a property value
+	 * is required.
+	 * @param requiredAnnotationType the desired annotation type
+	 */
+	public void setRequiredAnnotationType(Class<? extends Annotation> requiredAnnotationType) {
+		Assert.notNull(requiredAnnotationType, "'requiredAnnotationType' cannot be null");
+		this.requiredAnnotationType = requiredAnnotationType;
+	}
 
 
-    public final void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
-            processBeanDefinition(beanDefinition, beanDefinitionName);
-        }
-    }
+	public final void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		String[] beanDefinitionNames = beanFactory.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+			processBeanDefinition(beanDefinition, beanDefinitionName);
+		}
+	}
 
 
-    /**
-     * Is the supplied property required to have a value (i.e. be dependency injected)?
-     * <p>This implementation looks for the existence of a
-     * {@link #setRequiredAnnotationType(Class<? extends java.lang.annotation.Annotation>) 'required' annotation}
-     * on the supplied {@link PropertyDescriptor property}.
-     * @param propertyDescriptor the target {@link PropertyDescriptor property} (will never be <code>null</code>).
-     * @return <code>true</code> if the supplied property has been marked as being required; <code>false</code> if not,
-     *         or if the supplied property does not have a setter method.
-     */
-    protected boolean isRequired(PropertyDescriptor propertyDescriptor) {
-        Method setter = propertyDescriptor.getWriteMethod();
-        return (setter != null
-                && setter.getAnnotation(this.requiredAnnotationType) != null);
-    }
+	/**
+	 * Is the supplied property required to have a value (i.e. be dependency injected)?
+	 * <p>This implementation looks for the existence of a
+	 * {@link #setRequiredAnnotationType(Class<? extends java.lang.annotation.Annotation>) "required" annotation}
+	 * on the supplied {@link PropertyDescriptor property}.
+	 * @param propertyDescriptor the target {@link PropertyDescriptor property}
+	 * (will never be <code>null</code>)
+	 * @return <code>true</code> if the supplied property has been marked as being required;
+	 * <code>false</code> if not, or if the supplied property does not have a setter method.
+	 */
+	protected boolean isRequired(PropertyDescriptor propertyDescriptor) {
+		Method setter = propertyDescriptor.getWriteMethod();
+		return (setter != null && setter.getAnnotation(this.requiredAnnotationType) != null);
+	}
 
 
-    private void processBeanDefinition(BeanDefinition beanDefinition, String beanDefinitionName) {
-        Class targetType = ((AbstractBeanDefinition) beanDefinition).getBeanClass();
-        PropertyDescriptor[] propertyDescriptors = BeanUtils.getPropertyDescriptors(targetType);
-        List<String> invalidProperties = new ArrayList<String>();
-        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
-            if (isRequired(propertyDescriptor)
-                    && hasNotBeenDefined(propertyDescriptor, beanDefinition)) {
-                invalidProperties.add(propertyDescriptor.getName());
-            }
-        }
-        if (!invalidProperties.isEmpty()) {
-            throw new IllegalArgumentException(createExceptionMessage(invalidProperties, beanDefinitionName));
-        }
-    }
+	private void processBeanDefinition(BeanDefinition beanDefinition, String beanDefinitionName) {
+		Class targetType = ((AbstractBeanDefinition) beanDefinition).getBeanClass();
+		PropertyDescriptor[] propertyDescriptors = BeanUtils.getPropertyDescriptors(targetType);
+		List<String> invalidProperties = new ArrayList<String>();
+		for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+			if (isRequired(propertyDescriptor) && hasNotBeenDefined(propertyDescriptor, beanDefinition)) {
+				invalidProperties.add(propertyDescriptor.getName());
+			}
+		}
+		if (!invalidProperties.isEmpty()) {
+			throw new IllegalArgumentException(createExceptionMessage(invalidProperties, beanDefinitionName));
+		}
+	}
 
-    private String createExceptionMessage(List<String> invalidProperties, String beanDefinitionName) {
-        StringBuilder sb = new StringBuilder();
-        int size = invalidProperties.size();
-        sb.append(size == 1 ? "Property" : "Properties");
-        for (int i = 0; i < size; i++) {
-            String propertyName = invalidProperties.get(i);
-            if (i > 0) {
-                if (i == (size - 1)) {
-                    sb.append(" and");
-                } else {
-                    sb.append(",");
-                }
-            }
-            sb.append(" '");
-            sb.append(propertyName);
-            sb.append('\'');
-        }
-        sb.append(size == 1 ? " is required" : " are required");
-        sb.append(" for bean '");
-        sb.append(beanDefinitionName);
-        sb.append("'.");
-        return sb.toString();
-    }
+	private boolean hasNotBeenDefined(PropertyDescriptor propertyDescriptor, BeanDefinition beanDefinition) {
+		return (beanDefinition.getPropertyValues().getPropertyValue(propertyDescriptor.getName()) == null);
+	}
 
-    private boolean hasNotBeenDefined(PropertyDescriptor propertyDescriptor, BeanDefinition beanDefinition) {
-        return (beanDefinition.getPropertyValues().getPropertyValue(propertyDescriptor.getName()) == null);
-    }
+	private String createExceptionMessage(List<String> invalidProperties, String beanDefinitionName) {
+		StringBuilder sb = new StringBuilder();
+		int size = invalidProperties.size();
+		sb.append(size == 1 ? "Property" : "Properties");
+		for (int i = 0; i < size; i++) {
+			String propertyName = invalidProperties.get(i);
+			if (i > 0) {
+				if (i == (size - 1)) {
+					sb.append(" and");
+				}
+				else {
+					sb.append(",");
+				}
+			}
+			sb.append(" '");
+			sb.append(propertyName);
+			sb.append('\'');
+		}
+		sb.append(size == 1 ? " is required" : " are required");
+		sb.append(" for bean '");
+		sb.append(beanDefinitionName);
+		sb.append("'.");
+		return sb.toString();
+	}
 
 }
