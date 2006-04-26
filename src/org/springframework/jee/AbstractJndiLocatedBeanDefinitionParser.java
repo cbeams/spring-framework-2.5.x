cfdeaa3bc22a7e12d36b999ed63a7636c02d0d22
Rob Harrop <rharrop@interface21.com>
2006-04-26 19:48:26 +0000

    Missing file  - doh!
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10064 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/jee/AbstractJndiLocatedBeanDefinitionParser.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jee;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * @author Rob Harrop
+ * @since 2.0
+ */
+abstract class AbstractJndiLocatedBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+
+	public static final String ENVIRONMENT = "environment";
+
+	public static final String JNDI_ENVIRONMENT = "jndiEnvironment";
+
+	protected void postProcess(BeanDefinitionBuilder definitionBuilder, Element element) {
+		NodeList childNodes = element.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node node = childNodes.item(i);
+			if (ENVIRONMENT.equals(node.getLocalName())) {
+				definitionBuilder.addPropertyValue(JNDI_ENVIRONMENT, DomUtils.getTextValue((Element) node));
+			}
+		}
+	}
+}
