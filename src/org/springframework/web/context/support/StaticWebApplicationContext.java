fa357740854cf649efbf75c5850d6a996d857a87
Rick Evans <revans@interface21.com>
2006-05-02 18:22:54 +0000

    [SPR-1959] Calrified the JavaDoc for the setConfigLocations(..) method is response to JIRA issue.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10085 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/support/StaticWebApplicationContext.java
+++ src/org/springframework/web/context/support/StaticWebApplicationContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,7 +95,11 @@ public class StaticWebApplicationContext extends StaticApplicationContext
 		return this.namespace;
 	}
 
-	public void setConfigLocations(String[] configLocations) {
+    /**
+     * The {@link StaticWebApplicationContext} class does not support this method.
+     * @throws UnsupportedOperationException <b>always</b>
+     */
+    public void setConfigLocations(String[] configLocations) {
 		throw new UnsupportedOperationException("StaticWebApplicationContext does not support configLocations");
 	}
 
