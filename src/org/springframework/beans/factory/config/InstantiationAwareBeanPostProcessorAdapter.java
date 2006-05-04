c3731b20e26e69b8baff3b5d1e7fe1c5217491f4
Rod Johnson <rjohnson@interface21.com>
2006-05-04 13:09:26 +0000

    First version.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10113 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ src/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessorAdapter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+
+/**
+ * Adapter implementing all methods on InstantiationAwareBeanPostProcessor
+ * as no ops, which will not change normal processing of each bean
+ * instantiated by the container. 
+ * Subclasses can override merely those methods that they are interested in.
+ * 
+ * @author Rod Johnson
+ * @since 2.0
+ */
+public class InstantiationAwareBeanPostProcessorAdapter implements InstantiationAwareBeanPostProcessor {
+
+	public Object postProcessBeforeInstantiation(Class beanClass, String beanName) throws BeansException {
+		return null;
+	}
+
+	public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+		return true;
+	}
+
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
+
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
+
+}
