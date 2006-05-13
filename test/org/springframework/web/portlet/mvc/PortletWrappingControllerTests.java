7ca4c4a57a90e5905708ef55b4ae109e8f5fc90d
Mark Fisher <mfisher@interface21.com>
2006-05-13 18:02:13 +0000

    Added tests for ParameterMappingInterceptor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10182 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/web/portlet/mvc/PortletWrappingControllerTests.java
+++ test/org/springframework/web/portlet/mvc/PortletWrappingControllerTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.web.portlet.mvc;
 
 import java.io.IOException;
@@ -23,6 +39,9 @@ import org.springframework.mock.web.portlet.MockRenderResponse;
 import org.springframework.web.portlet.context.ConfigurablePortletApplicationContext;
 import org.springframework.web.portlet.context.StaticPortletApplicationContext;
 
+/**
+ * @author Mark Fisher
+ */
 public class PortletWrappingControllerTests extends TestCase {
 
 	private PortletWrappingController controller;
