27a7b3d56787ed85cc0fe8887f0055d37d793381
Rick Evans <revans@interface21.com>
2006-05-15 20:42:10 +0000

    Minor - corrected Javadoc typo ('metdata' -> 'metadata').
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10233 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/xml/BeanDefinitionDecorator.java
+++ src/org/springframework/beans/factory/xml/BeanDefinitionDecorator.java
@@ -17,31 +17,37 @@
 package org.springframework.beans.factory.xml;
 
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 /**
  * Interface used by the {@link org.springframework.beans.factory.xml.DefaultXmlBeanDefinitionParser}
- * to handle custom, nested (directly under a <code>&lt;bean&gt;</code>) tags. Decoration may also occur
- * based on custom attributes applied to the <code>&lt;bean&gt;</code> tag. Implementations are
- * free to turn the metdata in the custom tag into as many <code>BeanDefinitions</code> as required and
- * to transform the <code>BeanDefinition</code> of the enclosing <code>&lt;bean&gt;</code> tag,
- * potentially even returning a completely different <code>BeanDefinition</code> to replace
- * the original.
+ * to handle custom, nested (directly under a <code>&lt;bean&gt;</code>) tags.
+ * 
+ * <p>Decoration may also occur based on custom attributes applied to the
+ * <code>&lt;bean&gt;</code> tag. Implementations are free to turn the metadata in the
+ * custom tag into as many
+ * {@link org.springframework.beans.factory.config.BeanDefinition BeanDefinitions} as
+ * required and to transform the
+ * {@link org.springframework.beans.factory.config.BeanDefinition} of the enclosing
+ * <code>&lt;bean&gt;</code> tag, potentially even returning a completely different
+ * {@link org.springframework.beans.factory.config.BeanDefinition} to replace the
+ * original.
  *
- * <p>{@link BeanDefinitionDecorator BeanDefinitionDecorators} should be aware that they may be part of
- * a chain. In particular, a {@link BeanDefinitionDecorator} should be aware that a previous
- * {@link BeanDefinitionDecorator} may have replaced the original <code>BeanDefinition</code> with
- * a {@link org.springframework.aop.framework.ProxyFactoryBean} definition allowing for custom
- * {@link org.aopalliance.intercept.MethodInterceptor interceptors} to be added.
+ * <p>{@link BeanDefinitionDecorator BeanDefinitionDecorators} should be aware that
+ * they may be part of a chain. In particular, a {@link BeanDefinitionDecorator} should
+ * be aware that a previous {@link BeanDefinitionDecorator} may have replaced the
+ * original {@link org.springframework.beans.factory.config.BeanDefinition} with a
+ * {@link org.springframework.aop.framework.ProxyFactoryBean} definition allowing for
+ * custom {@link org.aopalliance.intercept.MethodInterceptor interceptors} to be added.
  *
- * <p>{@link BeanDefinitionDecorator BeanDefinitionDecorators} that wish to add an interceptor to the
- * enclosing bean should extend {@link org.springframework.aop.config.AbstractInterceptorDrivenBeanDefinitionDecorator} which handles
- * the chaining ensuring that only one proxy is created and that it contains all interceptors from the
- * chain.
+ * <p>{@link BeanDefinitionDecorator BeanDefinitionDecorators} that wish to add an
+ * interceptor to the enclosing bean should extend
+ * {@link org.springframework.aop.config.AbstractInterceptorDrivenBeanDefinitionDecorator}
+ * which handles the chaining ensuring that only one proxy is created and that it
+ * contains all interceptors from the chain.
  *
- * <p>The parser locates a {@link BeanDefinitionDecorator} from the {@link NamespaceHandler} for the
- * namespace in which the custom tag resides.
+ * <p>The parser locates a {@link BeanDefinitionDecorator} from the
+ * {@link NamespaceHandler} for the namespace in which the custom tag resides.
  *
  * @author Rob Harrop
  * @since 2.0
@@ -52,11 +58,16 @@ public interface BeanDefinitionDecorator {
 
 	/**
 	 * Parse the specified {@link Node} (either an element or an attribute) and decorate
-	 * the supplied <code>BeanDefinition</code>, returning the decorated definition.
-	 * <p>Implementations may choose to return a completely new definition, which will replace
-	 * the original definition in the resulting <code>BeanFactory</code>.
-	 * <p>The supplied {@link ParserContext} can be used to register any additional beans
-	 * needed to support the main definition.
+	 * the supplied {@link org.springframework.beans.factory.config.BeanDefinition},
+     * returning the decorated definition.
+	 * <p>Implementations may choose to return a completely new definition, which will
+     * replace the original definition in the resulting
+     * {@link org.springframework.beans.factory.BeanFactory}.
+	 * <p>The supplied {@link ParserContext} can be used to register any additional
+     * beans needed to support the main definition.
+     * @param node
+     * @param definition
+     * @param parserContext
 	 */
 	BeanDefinitionHolder decorate(Node node, BeanDefinitionHolder definition, ParserContext parserContext);
 
--- src/org/springframework/beans/factory/xml/BeanDefinitionParser.java
+++ src/org/springframework/beans/factory/xml/BeanDefinitionParser.java
@@ -17,31 +17,41 @@
 package org.springframework.beans.factory.xml;
 
 import org.w3c.dom.Element;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.config.BeanDefinition;
 
 /**
- * Interface used by the {@link org.springframework.beans.factory.xml.DefaultXmlBeanDefinitionParser}
- * to handle custom, top-level (directly under <code>&lt;beans&gt;</code>) tags. Implementations are
- * free to turn the metdata in the custom tag into as many <code>BeanDefinitions</code> as required.
- * <p>The parser locates a {@link BeanDefinitionParser} from the {@link NamespaceHandler} for the
- * namespace in which the custom tag resides.
+ * Interface used by the
+ * {@link org.springframework.beans.factory.xml.DefaultXmlBeanDefinitionParser} to
+ * handle custom, top-level (directly under <code>&lt;beans&gt;</code>) tags.
+ * 
+ * <p>Implementations are free to turn the metadata in the custom tag into as many
+ * {@link BeanDefinition BeanDefinitions} as required.
+ * 
+ * <p>The parser locates a {@link BeanDefinitionParser} from the associated
+ * {@link NamespaceHandler} for the namespace in which the custom tag resides.
  *
  * @author Rob Harrop
  * @since 2.0
  * @see NamespaceHandler
  * @see org.springframework.beans.factory.xml.BeanDefinitionDecorator
+ * @see AbstractBeanDefinitionParser
  */
 public interface BeanDefinitionParser {
 
 	/**
-	 * Parse the specified {@link Element} and register resulting <code>BeanDefinitions</code>
-	 * with the {@link BeanDefinitionRegistry} embedded in the supplied {@link ParserContext}.
-	 * <p>Implementations should return the primary <code>BeanDefinition</code> that results
-	 * from the parse phase if they which to be used nested inside <code>&lt;property&gt;</code> tag.
-	 * Implementations may return <code>null</code> if they will <strong>not</strong> be used in
-	 * a nested scenario.
-	 * @return the primary <code>BeanDefinition</code>
+	 * Parse the specified {@link Element} and register the resulting
+     * {@link BeanDefinition BeanDefinition(s)} with the
+     * {@link org.springframework.beans.factory.xml.ParserContext#getRegistry()} BeanDefinitionRegistry}
+     * embedded in the supplied {@link ParserContext}.
+	 * <p>Implementations must return the primary {@link BeanDefinition} that results
+	 * from the parse if they will ever be used in a nested fashion (for example as
+     * an inner tag in a <code>&lt;property/&gt;</code> tag). Implementations may
+     * return <code>null</code> if they will <strong>not</strong> be used in a nested
+     * fashion.
+     * @param element the element that is to be parsed into one or more {@link BeanDefinition BeanDefinitions}
+     * @param parserContext the object encapsulating the current state of the parse;
+     * provides access to a {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}
+	 * @return the primary {@link BeanDefinition}
 	 */
 	BeanDefinition parse(Element element, ParserContext parserContext);
 
