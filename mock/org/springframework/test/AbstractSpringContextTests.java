2100eb0a3d6158403e480a10c2be3be43205aca2
Juergen Hoeller <jhoeller@interface21.com>
2006-05-16 09:06:20 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10240 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- mock/org/springframework/test/AbstractSpringContextTests.java
+++ mock/org/springframework/test/AbstractSpringContextTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,7 @@ import org.springframework.util.StringUtils;
  * a LocalSessionFactoryBean for working with Hibernate, may take time to
  * initialize. Hence it often makes sense to do that initializing once.
  *
- * <p>Normally you won't extend this class directly, but extend one
+ * <p>Normally you won't extend this class directly but rather extend one
  * of its subclasses.
  *
  * @author Rod Johnson
@@ -45,7 +45,7 @@ public abstract class AbstractSpringContextTests extends ConditionalTestCase {
 
 	/**
 	 * Map of context keys returned by subclasses of this class, to
-	 * Spring Contexts. This needs to be static, as JUnit tests are
+	 * Spring contexts. This needs to be static, as JUnit tests are
 	 * destroyed and recreated between running individual test methods.
 	 */
 	private static Map contextKeyToContextMap = new HashMap();
