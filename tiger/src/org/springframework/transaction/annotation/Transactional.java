e5a90830dbdb234f2da2fe5e132147468bca792e
Rick Evans <revans@interface21.com>
2006-05-17 22:13:25 +0000

    Small change so that only Throwable types can be used for rollback rules.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10276 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/transaction/annotation/Transactional.java
+++ tiger/src/org/springframework/transaction/annotation/Transactional.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,15 +24,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * JDK 1.5+ annotation for describing transaction attributes on a method or class.
+ * Describes transaction attributes on a method or class.
  *
  * <p>This annotation type is generally directly comparable to Spring's
  * {@link org.springframework.transaction.interceptor.RuleBasedTransactionAttribute}
  * class, and in fact {@link AnnotationTransactionAttributeSource} will directly
  * convert the data to the latter class, so that Spring's transaction support code
- * does not have to know about Annotations. If no rules are relevant to the exception,
- * it will be treated like DefaultTransactionAttribute (rolling back on runtime
- * exceptions).
+ * does not have to know about annotations. If no rules are relevant to the exception,
+ * it will be treated like
+ * {@link org.springframework.transaction.interceptor.DefaultTransactionAttribute}
+ * (rolling back on runtime exceptions).
  * 
  * @author Colin Sampaleanu
  * @author Juergen Hoeller
@@ -47,63 +48,67 @@ import java.lang.annotation.Target;
 public @interface Transactional {
 	
 	/**
-	 * The transaction propagation type 
+	 * The transaction propagation type.
+     * <p>Defaults to {@link Propagation#REQUIRED}.
 	 */
 	Propagation propagation() default Propagation.REQUIRED;
 	
     /**
-     * The transaction isolation level
+     * The transaction isolation level.
+     * <p>Defaults to {@link Isolation#DEFAULT}.
      */
 	Isolation isolation() default Isolation.DEFAULT;
 	
 	/**
-	 * True if the transaction is read-only 
+	 * <code>true</code> if the transaction is read-only.
+     * <p>Defaults to <code>false</code>. 
 	 */
 	boolean readOnly() default false;
 	
 	/**
-	 * 0 or more exception Classes, which must be a subclass of Throwable, indicating
-	 * which exception types should cause a transaction rollback. This is the preferred
-	 * way to construct a rollback rule, matching the exception class and subclasses.
-	 * <p>Similar to {@link
-	 * org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(Class clazz)}
+	 * Defines zero (0) or more exception {@link Class classes}, which must be a
+     * subclass of {@link Throwable}, indicating which exception types must cause
+     * a transaction rollback.
+     * <p>This is the preferred way to construct a rollback rule, matching the
+     * exception class and subclasses.
+     * <p>Similar to {@link org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(Class clazz)}
 	 */
-	Class[] rollbackFor() default {};
+	Class<? extends Throwable>[] rollbackFor() default {};
 	
 	/**
-	 * 0 or more exception names (for exceptions which must be a subclass of Throwable)
-	 * which indicate exception types which should cause a transaction rollback.
+	 * Defines zero (0) or more exception names (for exceptions which must be a
+     * subclass of {@link Throwable}), indicating which exception types must cause
+     * a transaction rollback.
 	 * <p>This can be a substring, with no wildcard support at present.
-	 * A value of "ServletException" would match ServletException and
-	 * subclasses, for example.
+	 * A value of "ServletException" would match
+     * {@link javax.servlet.ServletException} and subclasses, for example.
 	 * <p><b>NB: </b>Consider carefully how specific the pattern is, and whether
 	 * to include package information (which isn't mandatory). For example,
 	 * "Exception" will match nearly anything, and will probably hide other rules.
 	 * "java.lang.Exception" would be correct if "Exception" was meant to define
-	 * a rule for all checked exceptions. With more unusual Exception
-	 * names such as "BaseBusinessException" there's no need to use a FQN.
-	 * <p>Similar to {@link
-	 * org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(String exceptionName)}
+	 * a rule for all checked exceptions. With more unusual {@link Exception}
+	 * names such as "BaseBusinessException" there is no need to use a FQN.
+	 * <p>Similar to {@link org.springframework.transaction.interceptor.RollbackRuleAttribute#RollbackRuleAttribute(String exceptionName)}
 	 */
 	String[] rollbackForClassName() default {};
     
 	/**
-	 * 0 or more exception Classes, which must be a subclass of Throwable, indicating
-	 * which exception types should not cause a transaction rollback. This is the preferred
-	 * way to construct a rollback rule, matching the exception class and subclasses.
-	 * <p>Similar to {@link
-	 * org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(Class clazz)}
+	 * Defines zero (0) or more exception {@link Class Classes}, which must be a
+     * subclass of {@link Throwable}, indicating which exception types must <b>not</b>
+     * cause a transaction rollback.
+     * <p>This is the preferred way to construct a rollback rule, matching the
+     * exception class and subclasses.
+	 * <p>Similar to {@link org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(Class clazz)}
 	 */
-	Class[] noRollbackFor() default {};
+	Class<? extends Throwable>[] noRollbackFor() default {};
 	
 	/**
-	 * 0 or more exception names (for exceptions which must be a subclass of
-	 * Throwable) which indicate exception types which should not cause a
-	 * transaction rollback.
-	 * <p>See description of {@link #rollbackForClassName()} for more info on how
+	 * Defines zero (0) or more exception names (for exceptions which must be a
+     * subclass of {@link Throwable}) indicating which exception types must <b>not</b>
+	 * cause a transaction rollback.
+	 * <p>See the description of {@link #rollbackForClassName()} for more info on how
 	 * the specified names are treated.
-	 * <p>Similar to {@link
-	 * org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(String exceptionName)}
+	 * <p>Similar to {@link org.springframework.transaction.interceptor.NoRollbackRuleAttribute#NoRollbackRuleAttribute(String exceptionName)}
 	 */
 	String[] noRollbackForClassName() default {};
 
