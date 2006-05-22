83f38d1c98e2bf64952d31f1231d4126ee0b261d
Rick Evans <revans@interface21.com>
2006-05-22 07:48:20 +0000

    [SPR-2045] Minor - fixed incorrect Javadoc.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10304 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/core/SqlReturnResultSet.java
+++ src/org/springframework/jdbc/core/SqlReturnResultSet.java
@@ -17,15 +17,13 @@
 package org.springframework.jdbc.core;
 
 /**
- * Subclass of SqlOutParameter to represent a returned ResultSet
- * from a stored procedure call.
+ * Represents a returned {@link java.sql.ResultSet} from a stored procedure call.
  *
- * <p>Must declare a ResultSetExtractor, RowCallbackHandler or RowMapper
- * to handle any returned rows. No additional properties: instanceof will
- * be used to check for such types.
+ * <p>A {@link ResultSetExtractor}, {@link RowCallbackHandler} or {@link RowMapper}
+ * must be provided to handle any returned rows.
  *
- * <p>Returned ResultSets - like all stored procedure parameters -
- * must have names.
+ * <p>Returned {@link java.sql.ResultSet ResultSets} - like all stored procedure
+ * parameters - <b>must</b> have names.
  *
  * @author Thomas Risberg
  * @author Juergen Hoeller
@@ -33,30 +31,30 @@ package org.springframework.jdbc.core;
 public class SqlReturnResultSet extends ResultSetSupportingSqlParameter {
 
 	/**
-	 * Create a new SqlReturnResultSet.
+	 * Create a new instance of the {@link SqlReturnResultSet} class.
 	 * @param name name of the parameter, as used in input and output maps
-	 * @param rse ResultSetExtractor to use for parsing the ResultSet
+	 * @param extractor ResultSetExtractor to use for parsing the {@link java.sql.ResultSet}
 	 */
-	public SqlReturnResultSet(String name, ResultSetExtractor rse) {
-		super(name, 0, rse);
+	public SqlReturnResultSet(String name, ResultSetExtractor extractor) {
+		super(name, 0, extractor);
 	}
 
 	/**
-	 * Create a new SqlReturnResultSet.
+	 * Create a new instance of the {@link SqlReturnResultSet} class.
 	 * @param name name of the parameter, as used in input and output maps
-	 * @param rch RowCallbackHandler to use for parsing the ResultSet
+	 * @param handler RowCallbackHandler to use for parsing the {@link java.sql.ResultSet}
 	 */
-	public SqlReturnResultSet(String name, RowCallbackHandler rch) {
-		super(name, 0, rch);
+	public SqlReturnResultSet(String name, RowCallbackHandler handler) {
+		super(name, 0, handler);
 	}
 
 	/**
-	 * Create a new SqlReturnResultSet.
+	 * Create a new instance of the {@link SqlReturnResultSet} class.
 	 * @param name name of the parameter, as used in input and output maps
-	 * @param rm RowMapper to use for parsing the ResultSet
+	 * @param mapper RowMapper to use for parsing the {@link java.sql.ResultSet}
 	 */
-	public SqlReturnResultSet(String name, RowMapper rm) {
-		super(name, 0, rm);
+	public SqlReturnResultSet(String name, RowMapper mapper) {
+		super(name, 0, mapper);
 	}
 
 }
