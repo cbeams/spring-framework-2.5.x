9a1a2301af44ad70817ad6c553555b4125be0908
Juergen Hoeller <jhoeller@interface21.com>
2006-05-29 21:29:59 +0000

    moved ControllerClassNameHandlerMapping from "servlet.mvc" to "servlet.mvc.support"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10389 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/mvc/support/ControllerClassNameHandlerMapping.java
+++ src/org/springframework/web/servlet/mvc/support/ControllerClassNameHandlerMapping.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.web.servlet.mvc;
+package org.springframework.web.servlet.mvc.support;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,22 +22,27 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.AbstractUrlHandlerMapping;
+import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+import org.springframework.web.servlet.mvc.throwaway.ThrowawayController;
 
 /**
- * Implementation of {@link HandlerMapping} that follows a simple convention for generating
- * URL path mappings from the class names of registered {@link Controller} beans.
+ * Implementation of {@link HandlerMapping} that follows a simple convention for
+ * generating URL path mappings from the class names of registered
+ * {@link org.springframework.web.servlet.mvc.Controller} and
+ * {@link org.springframework.web.servlet.mvc.throwaway.ThrowawayController} beans.
  *
- * <p>For simple {@link Controller} implementations (those that handle a single request type),
- * the convention is to take the {@link ClassUtils#getShortName short name} of the <code>Class</code>,
- * remove the 'Controller' suffix if it exists and return the remaining text, lowercased, as
- * the mapping, with a leading <code>/</code>. For example:
+ * <p>For simple {@link org.springframework.web.servlet.mvc.Controller} implementations
+ * (those that handle a single request type), the convention is to take the
+ * {@link ClassUtils#getShortName short name} of the <code>Class</code>,
+ * remove the 'Controller' suffix if it exists and return the remaining text, lowercased,
+ * as the mapping, with a leading <code>/</code>. For example:
  * <ul>
  * 	<li><code>WelcomeController</code> -> <code>/welcome</code></li>
  * 	<li><code>HomeController</code> -> <code>/home</code></li>
  * </ul>
  *
- * <p>For {@link MultiActionController MultiActionControllers} then a similar mapping is registed
+ * <p>For {@link MultiActionController MultiActionControllers} then a similar mapping is registered,
  * except that all sub-paths are registed using the trailing wildcard pattern <code>/*</code>.
  * For example:
  * <ul>
@@ -52,12 +57,13 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
  * @author Rob Harrop
  * @since 2.0
  * @see org.springframework.web.servlet.mvc.Controller
+ * @see org.springframework.web.servlet.mvc.throwaway.ThrowawayController
  * @see org.springframework.web.servlet.mvc.multiaction.MultiActionController
  */
 public class ControllerClassNameHandlerMapping extends AbstractUrlHandlerMapping implements HandlerMapping {
 
 	/**
-	 * Common suffix at the end of {@link Controller} implementation classes.
+	 * Common suffix at the end of controller implementation classes.
 	 * Removed when generating the URL path.
 	 */
 	private static final String CONTROLLER_SUFFIX = "Controller";
@@ -76,28 +82,41 @@ public class ControllerClassNameHandlerMapping extends AbstractUrlHandlerMapping
 	}
 
 	/**
-	 * Detect all the {@link Controller} beans registered in the
-	 * {@link org.springframework.context.ApplicationContext} and
-	 * registers a URL path mapping for each one based on rules defined
-	 * {@link ControllerClassNameHandlerMapping here}.
+	 * Detect all the {@link org.springframework.web.servlet.mvc.Controller} and
+	 * {@link org.springframework.web.servlet.mvc.throwaway.ThrowawayController}
+	 * beans registered in the {@link org.springframework.context.ApplicationContext}
+	 * and register a URL path mapping for each one based on rules defined here.
 	 * @see #generatePathMapping(Class)
 	 */
 	private void detectHandlers() {
 		String[] beanNames = getApplicationContext().getBeanNamesForType(Controller.class);
 		for (int i = 0; i < beanNames.length; i++) {
-			String controllerBeanName = beanNames[i];
-			Class controllerClass = getApplicationContext().getType(controllerBeanName);
-			String urlPath = generatePathMapping(controllerClass);
-			if (logger.isDebugEnabled()) {
-				logger.debug("Registering Controller '" + controllerBeanName +
-						"' as handler for URL path [" + urlPath + "]");
-			}
-			registerHandler(urlPath, controllerBeanName);
+			registerController(beanNames[i]);
+		}
+		beanNames = getApplicationContext().getBeanNamesForType(ThrowawayController.class);
+		for (int i = 0; i < beanNames.length; i++) {
+			registerController(beanNames[i]);
 		}
 	}
 
 	/**
-	 * Generates the actual URL path for the given {@link Controller} class. Sub-classes
+	 * Register the controller with the given name, as defined
+	 * in the current application context.
+	 * @param beanName the name of the controller bean
+	 * @see #getApplicationContext()
+	 */
+	protected void registerController(String beanName) {
+		Class controllerClass = getApplicationContext().getType(beanName);
+		String urlPath = generatePathMapping(controllerClass);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Registering Controller '" + beanName +
+					"' as handler for URL path [" + urlPath + "]");
+		}
+		registerHandler(urlPath, beanName);
+	}
+
+	/**
+	 * Generate the actual URL path for the given {@link Controller} class. Sub-classes
 	 * may choose to customize the paths that are generated by overriding this method.
 	 */
 	protected String generatePathMapping(Class controllerClass) {
