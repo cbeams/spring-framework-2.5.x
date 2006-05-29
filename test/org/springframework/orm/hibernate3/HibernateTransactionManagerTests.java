f4910fa271a7351c0e279e6089ed16b70f6a7124
Juergen Hoeller <jhoeller@interface21.com>
2006-05-29 19:13:32 +0000

    avoid EHCache init failures on Hibernate 3.2
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10379 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/orm/hibernate3/HibernateTransactionManagerTests.java
+++ test/org/springframework/orm/hibernate3/HibernateTransactionManagerTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,6 +35,7 @@ import org.hibernate.JDBCException;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cache.NoCacheProvider;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
 import org.hibernate.dialect.HSQLDialect;
@@ -1415,6 +1416,7 @@ public class HibernateTransactionManagerTests extends TestCase {
 		lsfb.setDataSource(ds);
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", HSQLDialect.class.getName());
+		props.setProperty("hibernate.cache.provider_class", NoCacheProvider.class.getName());
 		lsfb.setHibernateProperties(props);
 		lsfb.afterPropertiesSet();
 		final SessionFactory sf = (SessionFactory) lsfb.getObject();
@@ -1460,6 +1462,7 @@ public class HibernateTransactionManagerTests extends TestCase {
 		lsfb.setDataSource(ds);
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", HSQLDialect.class.getName());
+		props.setProperty("hibernate.cache.provider_class", NoCacheProvider.class.getName());
 		lsfb.setHibernateProperties(props);
 		lsfb.afterPropertiesSet();
 		final SessionFactory sf = (SessionFactory) lsfb.getObject();
