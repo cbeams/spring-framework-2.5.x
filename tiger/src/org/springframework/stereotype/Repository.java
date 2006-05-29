f915b2e526aa7ed82428667d199d3e03fc71bcbe
Juergen Hoeller <jhoeller@interface21.com>
2006-05-29 21:08:46 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10388 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/stereotype/Repository.java
+++ tiger/src/org/springframework/stereotype/Repository.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,11 +24,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation indicating that an annotated class is a 
- * Repository or DAO, eligible for Spring DataAccessException
- * translation and clarifying its role in the overall application
- * architecture for the purpose of tools, aspects etc.
- * 
+ * Annotation indicating that an annotated class is a "Repository" (or "DAO"),
+ * eligible for Spring DataAccessException translation and clarifying its role
+ * in the overall application architecture for the purpose of tools, aspects etc.
+ *
  * @author Rod Johnson
  * @since 2.0
  */
