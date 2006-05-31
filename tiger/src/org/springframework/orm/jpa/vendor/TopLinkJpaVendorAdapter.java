e7c531a7fdf85da279134ecd9718dfba8d786e6a
Juergen Hoeller <jhoeller@interface21.com>
2006-05-31 00:03:50 +0000

    updated to TopLink Essentials v2 b03; replaced databasePlatformClass with databaseName
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10414 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/orm/jpa/vendor/TopLinkJpaVendorAdapter.java
+++ tiger/src/org/springframework/orm/jpa/vendor/TopLinkJpaVendorAdapter.java
@@ -16,38 +16,15 @@
 
 package org.springframework.orm.jpa.vendor;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.logging.Level;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-
+import oracle.toplink.essentials.config.TargetDatabase;
+import oracle.toplink.essentials.config.TopLinkProperties;
 import oracle.toplink.essentials.ejb.cmp3.EntityManagerFactoryProvider;
-import oracle.toplink.essentials.platform.database.DB2Platform;
-import oracle.toplink.essentials.platform.database.HSQLPlatform;
-import oracle.toplink.essentials.platform.database.InformixPlatform;
-import oracle.toplink.essentials.platform.database.MySQL4Platform;
-import oracle.toplink.essentials.platform.database.PostgreSQLPlatform;
-import oracle.toplink.essentials.platform.database.SQLServerPlatform;
-import oracle.toplink.essentials.platform.database.SybasePlatform;
-import oracle.toplink.essentials.platform.database.oracle.OraclePlatform;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
-import org.springframework.jdbc.datasource.ConnectionHandle;
-import org.springframework.jdbc.support.JdbcUtils;
 import org.springframework.orm.jpa.JpaDialect;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * TopLink-specific JpaVendorAdapter implementation.
@@ -58,13 +35,7 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
  */
 public class TopLinkJpaVendorAdapter extends AbstractJpaVendorAdapter {
 
-	private final Log logger = LogFactory.getLog(getClass());
-
 	private final JpaDialect jpaDialect = new TopLinkJpaDialect();
-
-	private File dropDdlTempFile;
-
-	private File createDdlTempFile;
 
 
 	public Class getPersistenceProviderClass() {
@@ -73,54 +44,40 @@ public class TopLinkJpaVendorAdapter extends AbstractJpaVendorAdapter {
 
 	public Map getJpaPropertyMap() {
 		Properties jpaProperties = new Properties();
+
 		if (isShowSql()) {
-			jpaProperties.setProperty(EntityManagerFactoryProvider.TOPLINK_LOGGING_LEVEL, "FINE");
+			jpaProperties.setProperty(TopLinkProperties.LOGGING_LEVEL, Level.FINE.toString());
 		}
 		if (isGenerateDdl()) {
 			jpaProperties.setProperty(EntityManagerFactoryProvider.DDL_GENERATION,
 					EntityManagerFactoryProvider.DROP_AND_CREATE);
-			try {
-				this.dropDdlTempFile = File.createTempFile("toplink-drop", ".ddl", new File(""));
-				this.createDdlTempFile = File.createTempFile("toplink-create", ".ddl", new File(""));
-				jpaProperties.setProperty(EntityManagerFactoryProvider.DROP_JDBC_DDL_FILE,
-						this.dropDdlTempFile.getPath());
-				jpaProperties.setProperty(EntityManagerFactoryProvider.CREATE_JDBC_DDL_FILE,
-						this.createDdlTempFile.getPath());
-				this.dropDdlTempFile = new File(new File("").getAbsolutePath(), this.dropDdlTempFile.getPath());
-				this.createDdlTempFile = new File(new File("").getAbsolutePath(), this.createDdlTempFile.getPath());
-				this.dropDdlTempFile.deleteOnExit();
-				this.createDdlTempFile.deleteOnExit();
-			}
-			catch (IOException ex) {
-				logger.warn("Could not create temporary DDL files", ex);
-			}
+			jpaProperties.setProperty(EntityManagerFactoryProvider.DDL_GENERATION_MODE,
+					EntityManagerFactoryProvider.DDL_DATABASE_GENERATION);
 		}
-		
-		if (getDatabasePlatformClass() != null) {
-			jpaProperties.setProperty(EntityManagerFactoryProvider.TOPLINK_PLATFORM_PROPERTY,
-					getDatabasePlatformClass().getName());
-		}		
+
+		if (getDatabaseName() != null) {
+			jpaProperties.setProperty(TopLinkProperties.TARGET_DATABASE, getDatabaseName());
+		}
 		else if (getDatabase() != null) {
-			Class databasePlatformClass = getDatabasePlatformClass(getDatabase());
-			if (databasePlatformClass != null) {
-				jpaProperties.setProperty(EntityManagerFactoryProvider.TOPLINK_PLATFORM_PROPERTY,
-						databasePlatformClass.getName());
+			String targetDatabase = determineTargetDatabaseName(getDatabase());
+			if (targetDatabase != null) {
+				jpaProperties.setProperty(TopLinkProperties.TARGET_DATABASE, targetDatabase);
 			}
 		}
 
 		return jpaProperties;
 	}
 
-	protected Class getDatabasePlatformClass(Database database) {
+	protected String determineTargetDatabaseName(Database database) {
 		switch (database) {
-			case DB2: return DB2Platform.class;
-			case HSQL: return HSQLPlatform.class;
-			case INFORMIX: return InformixPlatform.class;
-			case MYSQL: return MySQL4Platform.class;
-			case ORACLE: return OraclePlatform.class;
-			case POSTGRESQL: return PostgreSQLPlatform.class;
-			case SQL_SERVER: return SQLServerPlatform.class;
-			case SYBASE: return SybasePlatform.class;
+			case DB2: return TargetDatabase.DB2;
+			case HSQL: return TargetDatabase.HSQL;
+			case INFORMIX: return TargetDatabase.Informix;
+			case MYSQL: return TargetDatabase.MySQL4;
+			case ORACLE: return TargetDatabase.Oracle;
+			case POSTGRESQL: return TargetDatabase.PostgreSQL;
+			case SQL_SERVER: return TargetDatabase.SQLServer;
+			case SYBASE: return TargetDatabase.Sybase;
 			default: return null;
 		}
 	}
@@ -131,102 +88,6 @@ public class TopLinkJpaVendorAdapter extends AbstractJpaVendorAdapter {
 
 	public JpaDialect getJpaDialect() {
 		return jpaDialect;
-	}
-
-
-	public void postProcessEntityManagerFactory(EntityManagerFactory emf) {
-		if (isGenerateDdl()) {
-			EntityManager em = emf.createEntityManager();
-			try {
-				this.jpaDialect.beginTransaction(em, new DefaultTransactionDefinition());
-				ConnectionHandle conHandle = this.jpaDialect.getJdbcConnection(em, false);
-				Connection con = conHandle.getConnection();
-				executeDdl(con, this.dropDdlTempFile);
-				executeDdl(con, this.createDdlTempFile);
-				em.getTransaction().commit();
-			}
-			catch (IOException ex) {
-				throw new IllegalArgumentException("Failed to read DDL script", ex);
-			}
-			catch (SQLException ex) {
-				logger.warn("Could not execute DDL statements", ex);
-			}
-			finally {
-				em.close();
-			}
-		}
-	}
-
-	private void executeDdl(Connection con, File ddlFile) throws SQLException, IOException {
-		List<String> statements = new LinkedList<String>();
-		LineNumberReader lnr = new LineNumberReader(new InputStreamReader(new FileInputStream(ddlFile)));
-		try {
-			String lastLine = lnr.readLine();
-			while (lastLine != null) {
-				statements.add(lastLine);
-				lastLine = lnr.readLine();
-			}
-			executeSchemaScript(con, statements.toArray(new String[statements.size()]));
-		}
-		finally {
-			lnr.close();
-		}
-	}
-
-	/**
-	 * Execute the given schema script on the given JDBC Connection.
-	 * <p>Note that the default implementation will log unsuccessful statements
-	 * and continue to execute. Override the <code>executeSchemaStatement</code>
-	 * method to treat failures differently.
-	 * @param con the JDBC Connection to execute the script on
-	 * @param sql the SQL statements to execute
-	 * @throws SQLException if thrown by JDBC methods
-	 * @see #executeSchemaStatement
-	 */
-	private void executeSchemaScript(Connection con, String[] sql) throws SQLException {
-		if (sql != null && sql.length > 0) {
-			boolean oldAutoCommit = con.getAutoCommit();
-			if (!oldAutoCommit) {
-				con.setAutoCommit(true);
-			}
-			try {
-				Statement stmt = con.createStatement();
-				try {
-					for (int i = 0; i < sql.length; i++) {
-						executeSchemaStatement(stmt, sql[i]);
-					}
-				}
-				finally {
-					JdbcUtils.closeStatement(stmt);
-				}
-			}
-			finally {
-				if (!oldAutoCommit) {
-					con.setAutoCommit(false);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Execute the given schema SQL on the given JDBC Statement.
-	 * <p>Note that the default implementation will log unsuccessful statements
-	 * and continue to execute. Override this method to treat failures differently.
-	 * @param stmt the JDBC Statement to execute the SQL on
-	 * @param sql the SQL statement to execute
-	 */
-	protected void executeSchemaStatement(Statement stmt, String sql) {
-		if (logger.isDebugEnabled()) {
-			logger.debug("Executing schema statement: " + sql);
-		}
-		try {
-			stmt.executeUpdate(sql);
-		}
-		catch (SQLException ex) {
-			if (logger.isWarnEnabled()) {
-				logger.warn("Unsuccessful schema statement: " + sql, ex);
-			}
-		}
 	}
 
 }
