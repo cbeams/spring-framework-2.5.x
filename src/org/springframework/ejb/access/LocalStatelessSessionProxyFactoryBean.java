8f49cbbf4cdf53dbb77622531ffcfee7d505c982
Juergen Hoeller <jhoeller@interface21.com>
2006-06-07 20:13:39 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10512 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBean.java
+++ src/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBean.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -62,6 +62,7 @@ public class LocalStatelessSessionProxyFactoryBean extends LocalSlsbInvokerInter
 	 */
 	private Class businessInterface;
 
+
 	/**
 	 * Set the business interface of the EJB we're proxying.
 	 * This will normally be a super-interface of the EJB local component interface.
@@ -86,6 +87,7 @@ public class LocalStatelessSessionProxyFactoryBean extends LocalSlsbInvokerInter
 		}
 		this.proxy = ProxyFactory.getProxy(this.businessInterface, this);
 	}
+
 
 	public Object getObject() {
 		return this.proxy;
