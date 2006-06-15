37b969da11e6a22b66802e506a54801b41ccfbad
Juergen Hoeller <jhoeller@interface21.com>
2006-06-15 14:22:19 +0000

    catch all RuntimeException/Errors and rethrows them as PortletException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10573 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/portlet/FrameworkPortlet.java
+++ src/org/springframework/web/portlet/FrameworkPortlet.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -411,7 +411,8 @@ public abstract class FrameworkPortlet extends PortletBean {
 			throws PortletException, IOException {
 
 		long startTime = System.currentTimeMillis();
-		Exception failureCause = null;
+		Throwable failureCause = null;
+
 		try {
 			if (request instanceof ActionRequest) {
 				doActionService((ActionRequest) request, (ActionResponse) response);
@@ -428,14 +429,11 @@ public abstract class FrameworkPortlet extends PortletBean {
 			failureCause = ex;
 			throw ex;
 		}
-		catch (RuntimeException ex) {
+		catch (Throwable ex) {
 			failureCause = ex;
-			throw ex;
+			throw new PortletException("Request processing failed", ex);
 		}
-		catch (Exception ex) {
-			failureCause = ex;
-			throw new PortletException(ex.getMessage(), ex);
-		}
+
 		finally {
 			if (failureCause != null) {
 				logger.error("Could not complete request", failureCause);
