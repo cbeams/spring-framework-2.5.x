5301c4b5c8ee35330e309f2a07a6f582b77e0560
Juergen Hoeller <jhoeller@interface21.com>
2006-06-27 15:05:40 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10701 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/ObjectRetrievalFailureException.java
+++ src/org/springframework/orm/ObjectRetrievalFailureException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,8 @@ import org.springframework.dao.DataRetrievalFailureException;
 
 /**
  * Exception thrown if a mapped object could not be retrieved via its identifier.
- * Provides information about the persistent class and the identifier. 
+ * Provides information about the persistent class and the identifier.
+ *
  * @author Juergen Hoeller
  * @since 13.10.2003
  */
