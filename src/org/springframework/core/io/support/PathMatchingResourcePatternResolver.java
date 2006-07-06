e545c3531efe82eeb5713938c5916a6135b878a7
Colin Sampaleanu <csampaleanu@interface21.com>
2006-07-06 13:48:34 +0000

    polishing javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10858 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/io/support/PathMatchingResourcePatternResolver.java
+++ src/org/springframework/core/io/support/PathMatchingResourcePatternResolver.java
@@ -144,7 +144,7 @@ import org.springframework.util.StringUtils;
  * </pre>is used to try to resolve it, the resolver will work off the (first) URL 
  * returned by <code>getResource("com/mycompany");</code>. If this base package
  * node exists in multiple classloader locations, the actual end resource may
- * not be underneath. Therefore, preferably, use "classpath*:" with the same
+ * not be underneath. Therefore, preferably, use "<code>classpath*:<code>" with the same
  * Ant-style pattern in such a case, which will search <i>all</i> class path
  * locations that contain the root package.
  * <p>
