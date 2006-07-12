d666222be0345426153c8756f5a3c89efdebfa0c
Rick Evans <revans@interface21.com>
2006-07-12 15:24:08 +0000

    Added extra condition check for NPE.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10912 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/propertyeditors/CharacterEditor.java
+++ src/org/springframework/beans/propertyeditors/CharacterEditor.java
@@ -21,22 +21,34 @@ import java.beans.PropertyEditorSupport;
 import org.springframework.util.StringUtils;
 
 /**
- * Editor for a <code>java.lang.Character</code>, to populate a property
- * of type Character or <code>char</code> from a String value.
+ * Editor for a {@link java.lang.Character}, to populate a property
+ * of type <code>Character</code> or <code>char</code> from a String value.
  *
- * <p>Note that the JDK does not contain a default property editor for
- * <code>char</code>! BeanWrapperImpl will register this editor by default.
+ * <p>Note that the JDK does not contain a default
+ * {@link java.beans.PropertyEditor property editor} for <code>char</code>!
+ * {@link org.springframework.beans.BeanWrapperImpl} will register this
+ * editor by default.
+ * 
+ * <p>Also supports conversion from a Unicode character sequence; e.g.
+ * <code>u0041</code> ('A').
  *
  * @author Juergen Hoeller
  * @author Rob Harrop
+ * @author Rick Evans
  * @since 1.2
  * @see java.lang.Character
  * @see org.springframework.beans.BeanWrapperImpl
  */
 public class CharacterEditor extends PropertyEditorSupport {
 
+	/**
+	 * The prefix that identifies a string as being a Unicode character sequence.
+	 */
 	private static final String UNICODE_PREFIX = "\\u";
 
+	/**
+	 * The length of a Unicode character sequence.
+	 */
 	private static final int UNICODE_LENGTH = 6;
 
 
@@ -46,9 +58,10 @@ public class CharacterEditor extends PropertyEditorSupport {
 	/**
 	 * Create a new CharacterEditor instance.
 	 * <p>The "allowEmpty" parameter controls whether an empty String is
-	 * to be allowed in parsing, i.e. be interpreted as 
-	 * the <code>null</code> value. Else, an IllegalArgumentException gets
-	 * thrown in that case.
+	 * to be allowed in parsing, i.e. be interpreted as the <code>null</code>
+	 * value when {@link #setAsText(String) text is being converted}. If
+	 * <code>false</code>, an {@link IllegalArgumentException} will be thrown
+	 * at that time.
 	 * @param allowEmpty if empty strings are to be allowed
 	 */
 	public CharacterEditor(boolean allowEmpty) {
@@ -61,9 +74,11 @@ public class CharacterEditor extends PropertyEditorSupport {
 			// Treat empty String as null value.
 			setValue(null);
 		}
-		else if (text.startsWith(UNICODE_PREFIX) && text.length() == UNICODE_LENGTH) {
-			int code = Integer.parseInt(text.substring(UNICODE_PREFIX.length()), 16);
-			setValue(new Character((char) code));
+		else if (text == null) {
+			throw new IllegalArgumentException("null String cannot be converted to char type");
+		}
+		else if (isUnicodeCharacterSequence(text)) {
+			setAsUnicode(text);
 		}
 		else if (text.length() != 1) {
 			throw new IllegalArgumentException("String [" + text + "] with length " +
@@ -77,6 +92,18 @@ public class CharacterEditor extends PropertyEditorSupport {
 	public String getAsText() {
 		Object value = getValue();
 		return (value != null ? value.toString() : "");
+	}
+
+
+	private void setAsUnicode(String text) {
+		int code = Integer.parseInt(text.substring(UNICODE_PREFIX.length()), 16);
+		setValue(new Character((char) code));
+	}
+
+
+	private static boolean isUnicodeCharacterSequence(String sequence) {
+		return sequence.startsWith(UNICODE_PREFIX)
+				&& sequence.length() == UNICODE_LENGTH;
 	}
 
 }
