4cc5f0f2bf496b35e17f2ce27f21a5d619746b08
Rick Evans <revans@interface21.com>
2006-07-13 12:02:29 +0000

    Corrected Javadoc typo ('me' -> 'be')
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10913 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/context/event/AbstractApplicationEventMulticaster.java
+++ src/org/springframework/context/event/AbstractApplicationEventMulticaster.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ import org.springframework.core.CollectionFactory;
  * <p>Implementing ApplicationEventMulticaster's actual <code>multicastEvent</code>
  * method is left to subclasses. SimpleApplicationEventMulticaster simply multicasts
  * all events to all registered listeners, invoking them in the calling thread.
- * Alternative implementations could me more sophisticated in those respects.
+ * Alternative implementations could be more sophisticated in those respects.
  *
  * @author Juergen Hoeller
  * @since 1.2.3
