4c6a8fe47955d0e13898f7f60d2516466c31303b
Juergen Hoeller <jhoeller@interface21.com>
2006-07-21 09:26:43 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10966 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/AopProxyUtils.java
+++ src/org/springframework/aop/framework/AopProxyUtils.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -54,7 +54,7 @@ public abstract class AopProxyUtils {
 
 	/**
 	 * Get complete set of interfaces to proxy. This will always add the Advised interface
-	 * unless the AdvisedSupport's "opaque" flag is true.
+	 * unless the AdvisedSupport's "opaque" flag is on.
 	 * @return the complete set of interfaces to proxy
 	 */
 	public static Class[] completeProxiedInterfaces(AdvisedSupport advised) {
