9cc61f27cecdfc31153ed5e6204e28588dd9f77d
Juergen Hoeller <jhoeller@interface21.com>
2006-07-23 22:00:46 +0000

    added dedicated "prepare()" method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10981 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/rmi/JndiRmiServiceExporter.java
+++ src/org/springframework/remoting/rmi/JndiRmiServiceExporter.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,11 +100,16 @@ public class JndiRmiServiceExporter extends RmiBasedExporter implements Initiali
 	}
 
 
-	/**
-	 * Register the service as RMI object.
-	 * Creates an RMI registry on the specified port if none exists.
-	 */
 	public void afterPropertiesSet() throws NamingException, RemoteException {
+		prepare();
+	}
+
+	/**
+	 * Initialize this service exporter, binding the specified service to JNDI.
+	 * @throws NamingException if service binding failed
+	 * @throws RemoteException if service export failed
+	 */
+	public void prepare() throws NamingException, RemoteException {
 		if (this.jndiName == null) {
 			throw new IllegalArgumentException("jndiName is required");
 		}
