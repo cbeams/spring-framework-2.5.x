ceebda044ea22dc0cd6893f790673e7e41c90735
Juergen Hoeller <jhoeller@interface21.com>
2006-07-23 22:02:40 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@10982 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/rmi/RmiServiceExporter.java
+++ src/org/springframework/remoting/rmi/RmiServiceExporter.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -198,9 +198,9 @@ public class RmiServiceExporter extends RmiBasedExporter implements Initializing
 	}
 
 	/**
-	 * Initialize this service exporter.
-	 * <p>Registers the service as RMI object.
-	 * Creates an RMI registry on the specified port if none exists.
+	 * Initialize this service exporter, registering the service as RMI object.
+	 * <p>Creates an RMI registry on the specified port if none exists.
+	 * @throws RemoteException if service registration failed
 	 */
 	public void prepare() throws RemoteException {
 		checkService();
