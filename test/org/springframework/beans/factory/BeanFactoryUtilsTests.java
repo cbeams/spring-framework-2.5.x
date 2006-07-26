a2a00fadc97b5bc6ca16b810cb83a527e75d6c17
Juergen Hoeller <jhoeller@interface21.com>
2006-07-26 00:25:11 +0000

    fixed typo
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11006 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/BeanFactoryUtilsTests.java
+++ test/org/springframework/beans/factory/BeanFactoryUtilsTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,7 +56,7 @@ public class BeanFactoryUtilsTests extends TestCase {
 	}
 
 	/**
-	 * Check that override doesn't count as too separate beans.
+	 * Check that override doesn't count as two separate beans.
 	 */
 	public void testHierarchicalCountBeansWithOverride() throws Exception {
 		// Leaf count
