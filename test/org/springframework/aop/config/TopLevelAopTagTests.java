c3b619cbd36def6614c1788c94f7d6b0d9f4b88d
Rick Evans <revans@interface21.com>
2006-07-31 16:48:58 +0000

    Added missing license header.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11045 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/config/TopLevelAopTagTests.java
+++ test/org/springframework/aop/config/TopLevelAopTagTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.aop.config;
 
 import junit.framework.TestCase;
@@ -6,9 +22,11 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
 /**
+ * Tests that the &lt;aop:config/&gt; element can be used as a top level element.
+ * 
  * @author Rob Harrop
  */
-public class TopLevelAopTagTests extends TestCase {
+public final class TopLevelAopTagTests extends TestCase {
 
 	public void testParse() throws Exception {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
@@ -17,4 +35,5 @@ public class TopLevelAopTagTests extends TestCase {
 
 		assertTrue(beanFactory.containsBeanDefinition("testPointcut"));
 	}
+
 }
