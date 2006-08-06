3181295270eb910f735b5daebd2dbe7cb0fd8c0f
Juergen Hoeller <jhoeller@interface21.com>
2006-08-06 18:39:20 +0000

    added assertion
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11103 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jms/support/destination/DynamicDestinationResolver.java
+++ src/org/springframework/jms/support/destination/DynamicDestinationResolver.java
@@ -57,6 +57,7 @@ public class DynamicDestinationResolver implements DestinationResolver {
 	public Destination resolveDestinationName(Session session, String destinationName, boolean pubSubDomain)
 			throws JMSException {
 
+		Assert.notNull(session, "Session must not be null");
 		Assert.notNull(destinationName, "Destination name must not be null");
 		if (pubSubDomain) {
 			return resolveTopic(session, destinationName);
