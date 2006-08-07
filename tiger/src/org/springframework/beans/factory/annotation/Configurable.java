c5d76ab0c33fe5a8ef47423f8d3f91f5b400aa52
Rick Evans <revans@interface21.com>
2006-08-07 08:26:11 +0000

    Javadoc polishing.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11112 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/beans/factory/annotation/Configurable.java
+++ tiger/src/org/springframework/beans/factory/annotation/Configurable.java
@@ -16,17 +16,12 @@
 
 package org.springframework.beans.factory.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
- * Annotation that marks a class as eligible for Spring-driven configuration.
- *
- * <p>Typically used with the AspectJ AnnotationBeanConfigurerAspect.
+ * Marks a class as being eligible for Spring-driven configuration.
+ * 
+ * <p>Typically used with the AspectJ <code>AnnotationBeanConfigurerAspect</code>.
  *
  * @author Rod Johnson
  * @author Rob Harrop
@@ -39,10 +34,19 @@ import java.lang.annotation.Target;
 @Inherited
 public @interface Configurable {
 
+	/**
+	 * The name of the bean definition that serves as the configuration template.
+	 */
 	String value() default "";
 
+	/**
+	 * Are dependencies to be injected via autowiring?
+	 */
 	Autowire autowire() default Autowire.NO;
-	
+
+	/**
+	 * Is dependency checking to be performed for configured objects?
+	 */
 	boolean dependencyCheck() default false;
 
 }
