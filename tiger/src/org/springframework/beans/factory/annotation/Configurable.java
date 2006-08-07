1e7bb03fd5a1072c6d67f0a366d5d30ea685eeee
Rick Evans <revans@interface21.com>
2006-08-07 08:27:21 +0000

    Organised imports (are now all explicit).
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11113 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/beans/factory/annotation/Configurable.java
+++ tiger/src/org/springframework/beans/factory/annotation/Configurable.java
@@ -16,7 +16,12 @@
 
 package org.springframework.beans.factory.annotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marks a class as being eligible for Spring-driven configuration.
