8599c0efaf38c8f85827799a1edf2091867f1206
Juergen Hoeller <jhoeller@interface21.com>
2006-08-10 17:33:33 +0000

    refined javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11175 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/AttributeAccessor.java
+++ src/org/springframework/core/AttributeAccessor.java
@@ -26,31 +26,41 @@ package org.springframework.core;
 public interface AttributeAccessor {
 
 	/**
-	 * Sets the attribute defined by <code>name</code> to the supplied	<code>value</code>.
+	 * Set the attribute defined by <code>name</code> to the supplied	<code>value</code>.
 	 * If <code>value</code> is <code>null</code>, the attribute is {@link #removeAttribute removed}.
+	 * <p>In general, users should take care to prevent overlaps with other
+	 * metadata attributes by using fully-qualified names, perhaps using
+	 * class or package names as prefix.
+	 * @param name the unique attribute key
+	 * @param value the attribute value to be attached
 	 */
 	void setAttribute(String name, Object value);
 
 	/**
-	 * Gets the value of the attribute identified by <code>name</code>. Returns <code>null</code> if
-	 * the attribute doesn't exist.
+	 * Get the value of the attribute identified by <code>name</code>.
+	 * Return <code>null</code> if the attribute doesn't exist.
+	 * @param name the unique attribute key
+	 * @return the current value of the attribute, if any
 	 */
 	Object getAttribute(String name);
 
 	/**
-	 * Removes the attribute identified by <code>name</code> and returns its value. Returns <code>null</code>
-	 * if no attribute under <code>name</code> is found.
+	 * Remove the attribute identified by <code>name</code> and return its value.
+	 * Return <code>null</code> if no attribute under <code>name</code> is found.
+	 * @param name the unique attribute key
+	 * @return the last value of the attribute, if any
 	 */
 	Object removeAttribute(String name);
 
 	/**
-	 * Returns <code>true</code> if the attribute identified by <code>name</code> exists, otherwise
-	 * returns <code>false</code>.
+	 * Return <code>true</code> if the attribute identified by <code>name</code> exists.
+	 * Otherwise return <code>false</code>.
+	 * @param name the unique attribute key
 	 */
 	boolean hasAttribute(String name);
 
 	/**
-	 * Returns the names of all attributes.
+	 * Return the names of all attributes.
 	 */
 	String[] attributeNames();
 
