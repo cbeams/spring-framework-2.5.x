b9645909909e8dc01c2a0b76261a83ff5c157a91
Juergen Hoeller <jhoeller@interface21.com>
2006-08-15 00:08:10 +0000

    added warning to javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11204 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/SpringVersion.java
+++ src/org/springframework/core/SpringVersion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,12 @@ package org.springframework.core;
 /**
  * Class that exposes the Spring version. Fetches the
  * "Implementation-Version" manifest attribute from the jar file.
+ *
+ * <p>Note that some ClassLoaders do not expose the package metadata,
+ * hence this class might not be able to determine the Spring version
+ * in all environments. Consider using a reflection-based check instead:
+ * For example, checking for the presence of a specific Spring 2.0
+ * method that you intend to call.
  *
  * @author Juergen Hoeller
  * @since 1.1
