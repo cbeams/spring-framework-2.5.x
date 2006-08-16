593ffe76e0a1c4de09b546ed847a97fcf4dd2b3d
Juergen Hoeller <jhoeller@interface21.com>
2006-08-16 15:00:59 +0000

    added message-only constructor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11225 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/RemoteLookupFailureException.java
+++ src/org/springframework/remoting/RemoteLookupFailureException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,6 +24,14 @@ package org.springframework.remoting;
  * @since 1.1
  */
 public class RemoteLookupFailureException extends RemoteAccessException {
+
+	/**
+	 * Constructor for RemoteLookupFailureException.
+	 * @param msg message
+	 */
+	public RemoteLookupFailureException(String msg) {
+		super(msg);
+	}
 
 	/**
 	 * Constructor for RemoteLookupFailureException.
