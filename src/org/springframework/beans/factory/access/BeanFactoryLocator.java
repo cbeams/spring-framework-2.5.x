680c4c582d6fa475848fd47a68427a8f5e008c74
Rick Evans <revans@interface21.com>
2006-08-16 09:49:25 +0000

    [SPR-2429] Javadoc polishing and reformatting.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11222 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/access/BeanFactoryLocator.java
+++ src/org/springframework/beans/factory/access/BeanFactoryLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,27 +19,32 @@ package org.springframework.beans.factory.access;
 import org.springframework.beans.BeansException;
 
 /**
- * An interface for a class used to lookup/use, and optionally allow the
- * release of a BeanFactory, or BeanFactory subclass such as ApplicationContext.
- *
+ * Defines a contract for the lookup, use, and release of a
+ * {@link org.springframework.beans.factory.BeanFactory}, or
+ * <code>BeanFactory</code> subclass such as
+ * {@link org.springframework.context.ApplicationContext}.
+ * 
  * <p>Where this interface is implemented as a singleton class such as
- * SingletonBeanFactoryLocator, the Spring team <strong>strongly</strong>
+ * {@link SingletonBeanFactoryLocator}, the Spring team <strong>strongly</strong>
  * suggests that it be used sparingly and with caution. By far the vast majority
  * of the code inside an application is best written in a Dependency Injection
- * style, where that code is served out of a BeanFactory/ApplicationContext
- * container, and has its own dependencies supplied by the container when it is
- * created. However, even such a singleton implementation sometimes has its use
- * in the small glue layers of code that is sometimes needed to tie other code
- * together. For example, third party code may try to construct new objects
- * directly, without the ability to force it to get these objects out of a 
- * beanfactory. If the object constructed by the third party code is just a
- * small stub or proxy, which then uses an implementation of this class to get a
- * beanfactory from which it gets the real object, to which it delegates, then 
- * proper Dependency Injection has been achieved. As another example, in a complex
- * J2EE app with multiple layers, with each layer having its own
- * ApplicationContext definition (in a hierarchy), a class like
- * SingletonBeanFactoryLocator may be used to demand load these contexts.
+ * style, where that code is served out of a
+ * <code>BeanFactory</code>/<code>ApplicationContext</code> container, and has
+ * its own dependencies supplied by the container when it is created. However,
+ * even such a singleton implementation sometimes has its use in the small glue
+ * layers of code that is sometimes needed to tie other code together. For
+ * example, third party code may try to construct new objects directly, without
+ * the ability to force it to get these objects out of a <code>BeanFactory</code>.
+ * If the object constructed by the third party code is just a small stub or
+ * proxy, which then uses an implementation of this class to get a
+ * <code>BeanFactory</code> from which it gets the real object, to which it
+ * delegates, then proper Dependency Injection has been achieved.
  * 
+ * <p>As another example, in a complex J2EE app with multiple layers, with each
+ * layer having its own <code>ApplicationContext</code> definition (in a
+ * hierarchy), a class like <code>SingletonBeanFactoryLocator</code> may be used
+ * to demand load these contexts.
+ *
  * @author Colin Sampaleanu
  * @see org.springframework.beans.factory.BeanFactory
  * @see org.springframework.context.access.DefaultLocatorFactory
@@ -47,15 +52,18 @@ import org.springframework.beans.BeansException;
  */
 public interface BeanFactoryLocator {
 
-  /**
-   * Use the BeanFactory (or derived class such as ApplicationContext) specified
-   * by the factoryKey parameter. The definition is possibly loaded/created as needed.
-   * @param factoryKey a resource name specifying which BeanFactory the BeanFactoryLocator
-   * should return for usage. The actual meaning of the resource name is specific to the
-   * actual implementation of BeanFactoryLocator.
-   * @return the BeanFactory instance, wrapped as a BeanFactoryReference object
-   * @throws BeansException if there is an error loading or accessing the BeanFactory
-   */
-  BeanFactoryReference useBeanFactory(String factoryKey) throws BeansException;
+	/**
+	 * Use the {@link org.springframework.beans.factory.BeanFactory} (or derived
+	 * interface such as {@link org.springframework.context.ApplicationContext})
+	 * specified by the <code>factoryKey</code> parameter.
+	 * <p>The definition is possibly loaded/created as needed.
+	 * @param factoryKey a resource name specifying which <code>BeanFactory</code> the
+	 *                   <code>BeanFactoryLocator</code> must return for usage.
+	 *                   The actual meaning of the resource name is specific to the
+	 *                   actual implementation of <code>BeanFactoryLocator</code>.
+	 * @return the <code>BeanFactory</code> instance, wrapped as a {@link BeanFactoryReference} object
+	 * @throws BeansException if there is an error loading or accessing the <code>BeanFactory</code>
+	 */
+	BeanFactoryReference useBeanFactory(String factoryKey) throws BeansException;
 
 }
--- src/org/springframework/beans/factory/access/BeanFactoryReference.java
+++ src/org/springframework/beans/factory/access/BeanFactoryReference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,9 @@ import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.BeanFactory;
 
 /**
- * Class used to track a reference to a BeanFactory obtained through
- * a BeanFactoryLocator.
- *
+ * Used to track a reference to a {@link BeanFactory} obtained through
+ * a {@link BeanFactoryLocator}.
+ * 
  * <p>It is safe to call {@link #release()} multiple times, but
  * {@link #getFactory()} must not be called after calling release.
  *
@@ -31,28 +31,32 @@ import org.springframework.beans.factory.BeanFactory;
  * @see org.springframework.context.access.ContextBeanFactoryReference
  */
 public interface BeanFactoryReference {
-  
-  /**
-   * Returns the BeanFactory instance held by this reference.
-   * @throws IllegalStateException if invoked after release() has been called
-   */
-  BeanFactory getFactory();
-  
-  /**
-   * Indicate that the BeanFactory instance referred to by this object is not
-   * needed any longer by the client code which obtained the ref object. Depending
-   * on the actual implementation of BeanFactoryLocator, and the actual type of
-   * BeanFactory, this may possibly not actually do anything; alternately in the
-   * case of a 'closeable' BeanFactory or derived class (such as ApplicationContext)
-   * may 'close' it, or may 'close' it once no more references remain.
-   * <p>In an EJB usage scenario this would normally be called from ejbRemove and
-   * ejbPassivate.
-   * <p>This is safe to call multiple times.
-   * @throws FatalBeanException if the BeanFactory cannot be released
-   * @see org.springframework.beans.factory.access.BeanFactoryLocator
+
+	/**
+	 * Returns the {@link BeanFactory} instance held by this reference.
+	 *
+	 * @throws IllegalStateException if invoked after release() has been called
+	 */
+	BeanFactory getFactory();
+
+	/**
+	 * Indicates that the {@link BeanFactory} instance referred to by this object is
+	 * not needed any longer by the client code which obtained the
+	 * {@link BeanFactoryReference}.
+	 * <p>Depending on the actual implementation of {@link BeanFactoryLocator}, and
+	 * the actual type of <code>BeanFactory</code>, this may possibly not actually
+	 * do anything; alternately in the case of a 'closeable' <code>BeanFactory</code>
+	 * or derived class (such as {@link org.springframework.context.ApplicationContext})
+	 * may 'close' it, or may 'close' it once no more references remain.
+	 * <p>In an EJB usage scenario this would normally be called from
+	 * <code>ejbRemove()</code> and <code>ejbPassivate()</code>.
+	 * <p>This is safe to call multiple times.
+	 *
+	 * @throws FatalBeanException if the <code>BeanFactory</code> cannot be released
+	 * @see BeanFactoryLocator
 	 * @see org.springframework.context.access.ContextBeanFactoryReference
 	 * @see org.springframework.context.ConfigurableApplicationContext#close
-   */
-  void release() throws FatalBeanException;
+	 */
+	void release() throws FatalBeanException;
 
 }
