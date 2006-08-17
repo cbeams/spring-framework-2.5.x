b36c2909931bb44313b4814bd07ce7a83055c746
Rick Evans <revans@interface21.com>
2006-08-17 10:05:55 +0000

    Initial testing and Javadoc polishing.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11238 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/scope/DefaultScopedObject.java
+++ src/org/springframework/aop/scope/DefaultScopedObject.java
@@ -20,8 +20,12 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.util.Assert;
 
 /**
- * Default implementation of the ScopedObject interface. Simply delegates the calls
- * to the underlying BeanFactory (<code>getBean</code>/<code>destroyScopedBean</code>).
+ * Default implementation of the {@link ScopedObject} interface.
+ * 
+ * <p>Simply delegates the calls to the underlying
+ * {@link ConfigurableBeanFactory bean factory}
+ * ({@link ConfigurableBeanFactory#getBean(String)}/
+ * {@link ConfigurableBeanFactory#destroyScopedBean(String)}).
  *
  * @author Juergen Hoeller
  * @since 2.0
@@ -36,13 +40,15 @@ public class DefaultScopedObject implements ScopedObject {
 
 
 	/**
-	 * Create a new DefaultScopedObject.
-	 * @param beanFactory the BeanFactory that holds the scoped target object
+	 * Creates a new instance of the {@link DefaultScopedObject} class.
+	 * @param beanFactory the {@link ConfigurableBeanFactory} that holds the scoped target object
 	 * @param targetBeanName the name of the target bean
+	 * @throws IllegalArgumentException if either of the parameters is <code>null</code>; or
+	 * if the <code>targetBeanName</code> consists wholly of whitespace
 	 */
 	public DefaultScopedObject(ConfigurableBeanFactory beanFactory, String targetBeanName) {
 		Assert.notNull(beanFactory, "BeanFactory must not be null");
-		Assert.notNull(targetBeanName, "Target bean name must not be null");
+		Assert.hasText(targetBeanName, "Target bean name must not be null or consist wholly of whitespace");
 		this.beanFactory = beanFactory;
 		this.targetBeanName = targetBeanName;
 	}
