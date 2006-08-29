2caa0ab6235b03a378132180f7ea6c3422f87176
Juergen Hoeller <jhoeller@interface21.com>
2006-08-29 22:38:34 +0000

    use "CacheManager.create(InputStream)" instead of "CacheManager.create(URL)"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11358 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/cache/ehcache/EhCacheManagerFactoryBean.java
+++ src/org/springframework/cache/ehcache/EhCacheManagerFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class EhCacheManagerFactoryBean implements FactoryBean, InitializingBean,
 	public void afterPropertiesSet() throws IOException, CacheException {
 		logger.info("Initializing EHCache CacheManager");
 		if (this.configLocation != null) {
-			this.cacheManager = CacheManager.create(this.configLocation.getURL());
+			this.cacheManager = CacheManager.create(this.configLocation.getInputStream());
 		}
 		else {
 			this.cacheManager = CacheManager.create();
