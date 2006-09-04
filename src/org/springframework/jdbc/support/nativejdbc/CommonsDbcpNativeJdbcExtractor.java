81e931c53951878cdbadcc814e1aab938b020099
Juergen Hoeller <jhoeller@interface21.com>
2006-09-04 14:20:26 +0000

    support original Commons DBCP as well as Tomcat 5.5's relocated version
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11398 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/support/nativejdbc/CommonsDbcpNativeJdbcExtractor.java
+++ src/org/springframework/jdbc/support/nativejdbc/CommonsDbcpNativeJdbcExtractor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
 package org.springframework.jdbc.support.nativejdbc;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,22 +25,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.apache.commons.dbcp.DelegatingCallableStatement;
-import org.apache.commons.dbcp.DelegatingConnection;
-import org.apache.commons.dbcp.DelegatingPreparedStatement;
-import org.apache.commons.dbcp.DelegatingResultSet;
-import org.apache.commons.dbcp.DelegatingStatement;
-
-import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * Implementation of the NativeJdbcExtractor interface for the
  * Jakarta Commons DBCP connection pool.
  *
- * <p>Returns the underlying native Connection, Statement, etc to
- * application code instead of DBCP's wrapper implementations.
- * The returned JDBC classes can then safely be cast, e.g. to
- * <code>oracle.jdbc.OracleConnection</code>.
+ * <p>Returns the underlying native Connection, Statement, etc to application
+ * code instead of DBCP's wrapper implementations. The returned JDBC classes
+ * can then safely be cast, e.g. to <code>oracle.jdbc.OracleConnection</code>.
  *
  * <p>This NativeJdbcExtractor can be set just to <i>allow</i> working with a
  * Commons DBCP DataSource: If a given object is not a Commons DBCP wrapper,
@@ -51,6 +45,11 @@ import org.springframework.dao.DataAccessResourceFailureException;
  * <code>getNativeCallableStatement</code> and <code>getNativeResultSet</code>
  * will not work with Commons DBCP 1.0.
  *
+ * <p>Note that this version of CommonsDbcpNativeJdbcExtractor will work
+ * against the original Commons DBCP in <code>org.apache.commons.dbcp</code>
+ * as well as against Tomcat 5.5's relocated Commons DBCP version in the
+ * <code>org.apache.tomcat.dbcp.dbcp</code> package.
+ *
  * @author Juergen Hoeller
  * @since 25.08.2003
  */
@@ -59,60 +58,56 @@ public class CommonsDbcpNativeJdbcExtractor extends NativeJdbcExtractorAdapter {
 	private static final String GET_INNERMOST_DELEGATE_METHOD_NAME = "getInnermostDelegate";
 
 
-	protected Connection doGetNativeConnection(Connection con) throws SQLException {
-		if (con instanceof DelegatingConnection) {
-			Connection nativeCon = ((DelegatingConnection) con).getInnermostDelegate();
-			// For some reason, the innermost delegate can be <code>null</code>: not for a
-			// Statement's Connection but for the Connection handle returned by the pool.
-			// We'll fall back to the MetaData's Connection in this case, which is
-			// a native unwrapped Connection with Commons DBCP 1.1 and 1.2.
-			return (nativeCon != null ? nativeCon : con.getMetaData().getConnection());
+	/**
+	 * Extracts the innermost delegate from the given Commons DBCP object.
+	 * Falls back to the given object if no underlying object found.
+	 * @param obj the Commons DBCP Connection/Statement/ResultSet
+	 * @return the underlying native Connection/Statement/ResultSet
+	 */
+	private static Object getInnermostDelegate(Object obj) {
+		if (obj == null) {
+			return null;
 		}
-		return con;
+		try {
+			Class classToAnalyze = obj.getClass();
+			while (!Modifier.isPublic(classToAnalyze.getModifiers())) {
+				classToAnalyze = classToAnalyze.getSuperclass();
+				if (classToAnalyze == null) {
+					// No public provider class found -> fall back to given object.
+					return obj;
+				}
+			}
+			Method getInnermostDelegate = classToAnalyze.getMethod(GET_INNERMOST_DELEGATE_METHOD_NAME, (Class[]) null);
+			Object delegate = ReflectionUtils.invokeMethod(getInnermostDelegate, obj);
+			return (delegate != null ? delegate : obj);
+		}
+		catch (NoSuchMethodException ex) {
+			return obj;
+		}
+		catch (SecurityException ex) {
+			throw new IllegalStateException("Commons DBCP getInnermostDelegate method is not accessible: " + ex);
+		}
+	}
+
+
+	protected Connection doGetNativeConnection(Connection con) throws SQLException {
+		return (Connection) getInnermostDelegate(con);
 	}
 
 	public Statement getNativeStatement(Statement stmt) throws SQLException {
-		if (stmt instanceof DelegatingStatement) {
-			return ((DelegatingStatement) stmt).getInnermostDelegate();
-		}
-		return stmt;
+		return (Statement) getInnermostDelegate(stmt);
 	}
 
 	public PreparedStatement getNativePreparedStatement(PreparedStatement ps) throws SQLException {
-		if (ps instanceof DelegatingPreparedStatement) {
-			// We need to use reflection here, as the "getInnermostDelegate"
-			// method signature varies between Commons DBCP 1.1 and 1.2.
-			try {
-				Method getInnermostDelegate = ps.getClass().getMethod(GET_INNERMOST_DELEGATE_METHOD_NAME, (Class[]) null);
-				return (PreparedStatement) getInnermostDelegate.invoke(ps, (Object[]) null);
-			}
-			catch (Exception ex) {
-				throw new DataAccessResourceFailureException("Could not retrieve innermost delegate", ex);
-			}
-		}
-		return ps;
+		return (PreparedStatement) getNativeStatement(ps);
 	}
 
 	public CallableStatement getNativeCallableStatement(CallableStatement cs) throws SQLException {
-		if (cs instanceof DelegatingCallableStatement) {
-			// We need to use reflection here, as the "getInnermostDelegate"
-			// method signature varies between Commons DBCP 1.1 and 1.2.
-			try {
-				Method getInnermostDelegate = cs.getClass().getMethod(GET_INNERMOST_DELEGATE_METHOD_NAME, (Class[]) null);
-				return (CallableStatement) getInnermostDelegate.invoke(cs, (Object[]) null);
-			}
-			catch (Exception ex) {
-				throw new DataAccessResourceFailureException("Could not retrieve innermost delegate", ex);
-			}
-		}
-		return cs;
+		return (CallableStatement) getNativeStatement(cs);
 	}
 
 	public ResultSet getNativeResultSet(ResultSet rs) throws SQLException {
-		if (rs instanceof DelegatingResultSet) {
-			return ((DelegatingResultSet) rs).getInnermostDelegate();
-		}
-		return rs;
+		return (ResultSet) getInnermostDelegate(rs);
 	}
 
 }
