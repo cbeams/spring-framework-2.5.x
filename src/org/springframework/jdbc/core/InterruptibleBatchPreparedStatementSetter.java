8ba4da802ec0ae511676d9a645943a5fcf697fb7
Juergen Hoeller <jhoeller@interface21.com>
2006-09-06 13:53:32 +0000

    added AbstractInterruptibleBatchPreparedStatementSetter base class, with single "setValuesIfAvailable" callback method
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11407 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/core/InterruptibleBatchPreparedStatementSetter.java
+++ src/org/springframework/jdbc/core/InterruptibleBatchPreparedStatementSetter.java
@@ -1,34 +1,56 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.jdbc.core;
 
 /**
- * Callback interface used by the JdbcTemplate class.
- *
- * <p>This interface sets values on a PreparedStatement provided by the
- * JdbcTemplate class for each of a number of updates in a batch using the
- * same SQL. Implementations are responsible for setting any necessary
- * parameters. SQL with placeholders will already have been supplied.
+ * Extension of the BatchPreparedStatementSetter interface that adds
+ * a batch exhaustion check.
  *
  * <p>This interface allows you to signal the end of a batch rather than
- * having to determine the exact batch size upfront.  Batch size is still being
- * honored but it is now the maximum size of the batch.
+ * having to determine the exact batch size upfront. Batch size is still
+ * being honored but it is now the maximum size of the batch.
  *
- * <p>The isBatchExhausted method is called after each call to setValues to determine whether
- * there were some values added or if the batch was determined to be complete and no additional
- * values were provided during the last call to setValues.
+ * <p>The <code>isBatchExhausted</code> method is called after each call to
+ * <code>setValues</code> to determine whether there were some values added
+ * or if the batch was determined to be complete and no additional values
+ * were provided during the last call to <code>setValues</code>.
  *
- * <p>Implementations <i>do not</i> need to concern themselves with
- * SQLExceptions that may be thrown from operations they attempt.
- * The JdbcTemplate class will catch and handle SQLExceptions appropriately.
+ * <p>Consider extending the AbstractInterruptibleBatchPreparedStatementSetter
+ * base class instead of implementing this interface directly, having a
+ * single callback method that combines the setting of values and the
+ * checking whether further vlaues exist.
  *
  * @author Thomas Risberg
  * @since 2.0
- * @see org.springframework.jdbc.core.JdbcTemplate#batchUpdate(String, org.springframework.jdbc.core.BatchPreparedStatementSetter)
+ * @see JdbcTemplate#batchUpdate(String, BatchPreparedStatementSetter)
+ * @see org.springframework.jdbc.core.support.AbstractInterruptibleBatchPreparedStatementSetter
  */
 public interface InterruptibleBatchPreparedStatementSetter extends BatchPreparedStatementSetter {
 
 	/**
-	 * Return the whether batch is complete, there were no additional values to be added.
+	 * Return whether the batch is complete, that is, whether there were no
+	 * additional values added during the last <code>setValues</code> call.
+	 * <p><b>NOTE:</b> If this method returns <code>true</code>, any parameters
+	 * set during the last <code>setValues</code> call will be ignored!
+	 * Make sure that you set a corresponding internal flag if you detect
+	 * exhaustion in your <code>setValues</code> implementation, letting
+	 * this method return <code>true</code> based on the flag.
 	 * @param i index of the statement we're issuing in the batch, starting from 0
+	 * @see #setValues
 	 */
 	boolean isBatchExhausted(int i);
 
--- test/org/springframework/jdbc/core/JdbcTemplateTests.java
+++ test/org/springframework/jdbc/core/JdbcTemplateTests.java
@@ -40,6 +40,7 @@ import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.SQLWarningException;
 import org.springframework.jdbc.UncategorizedSQLException;
+import org.springframework.jdbc.core.support.AbstractInterruptibleBatchPreparedStatementSetter;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLStateSQLExceptionTranslator;
@@ -764,7 +765,7 @@ public class JdbcTemplateTests extends AbstractJdbcTests {
 		ctrlDatabaseMetaData.verify();
 	}
 
-	public void testInterruptibleBatchUpdateWithPreparedStatement() throws Exception {
+	public void testInterruptibleBatchUpdate() throws Exception {
 		final String sql = "UPDATE NOSUCHTABLE SET DATE_DISPATCHED = SYSDATE WHERE ID = ?";
 		final int[] ids = new int[] { 100, 200 };
 		final int[] rowsAffected = new int[] { 1, 2 };
@@ -805,22 +806,83 @@ public class JdbcTemplateTests extends AbstractJdbcTests {
 		replay();
 
 		BatchPreparedStatementSetter setter =
-			new InterruptibleBatchPreparedStatementSetter() {
-			public void setValues(PreparedStatement ps, int i)
-				throws SQLException {
-				if (i < ids.length)
-					ps.setInt(1, ids[i]);
-			}
-			public int getBatchSize() {
-				return 1000;
-			}
-			public boolean isBatchExhausted(int i) {
-				if (i < ids.length)
-				    return false;
-				else
-					return true;
-			}
-		};
+				new InterruptibleBatchPreparedStatementSetter() {
+					public void setValues(PreparedStatement ps, int i) throws SQLException {
+						if (i < ids.length) {
+							ps.setInt(1, ids[i]);
+						}
+					}
+					public int getBatchSize() {
+						return 1000;
+					}
+					public boolean isBatchExhausted(int i) {
+						return (i >= ids.length);
+					}
+				};
+
+		JdbcTemplate template = new JdbcTemplate(mockDataSource, false);
+
+		int[] actualRowsAffected = template.batchUpdate(sql, setter);
+		assertTrue("executed 2 updates", actualRowsAffected.length == 2);
+		assertEquals(rowsAffected[0], actualRowsAffected[0]);
+		assertEquals(rowsAffected[1], actualRowsAffected[1]);
+
+		ctrlPreparedStatement.verify();
+		ctrlDatabaseMetaData.verify();
+	}
+
+	public void testInterruptibleBatchUpdateWithBaseClass() throws Exception {
+		final String sql = "UPDATE NOSUCHTABLE SET DATE_DISPATCHED = SYSDATE WHERE ID = ?";
+		final int[] ids = new int[] { 100, 200 };
+		final int[] rowsAffected = new int[] { 1, 2 };
+
+		MockControl ctrlPreparedStatement = MockControl.createControl(PreparedStatement.class);
+		PreparedStatement mockPreparedStatement = (PreparedStatement) ctrlPreparedStatement.getMock();
+		mockPreparedStatement.getConnection();
+		ctrlPreparedStatement.setReturnValue(mockConnection);
+		mockPreparedStatement.setInt(1, ids[0]);
+		ctrlPreparedStatement.setVoidCallable();
+		mockPreparedStatement.addBatch();
+		ctrlPreparedStatement.setVoidCallable();
+		mockPreparedStatement.setInt(1, ids[1]);
+		ctrlPreparedStatement.setVoidCallable();
+		mockPreparedStatement.addBatch();
+		ctrlPreparedStatement.setVoidCallable();
+		mockPreparedStatement.executeBatch();
+		ctrlPreparedStatement.setReturnValue(rowsAffected);
+		mockPreparedStatement.getWarnings();
+		ctrlPreparedStatement.setReturnValue(null);
+		mockPreparedStatement.close();
+		ctrlPreparedStatement.setVoidCallable();
+
+		MockControl ctrlDatabaseMetaData = MockControl.createControl(DatabaseMetaData.class);
+		DatabaseMetaData mockDatabaseMetaData = (DatabaseMetaData) ctrlDatabaseMetaData.getMock();
+		mockDatabaseMetaData.getDatabaseProductName();
+		ctrlDatabaseMetaData.setReturnValue("MySQL");
+		mockDatabaseMetaData.supportsBatchUpdates();
+		ctrlDatabaseMetaData.setReturnValue(true);
+
+		mockConnection.prepareStatement(sql);
+		ctrlConnection.setReturnValue(mockPreparedStatement);
+		mockConnection.getMetaData();
+		ctrlConnection.setReturnValue(mockDatabaseMetaData, 2);
+
+		ctrlPreparedStatement.replay();
+		ctrlDatabaseMetaData.replay();
+		replay();
+
+		BatchPreparedStatementSetter setter =
+				new AbstractInterruptibleBatchPreparedStatementSetter() {
+					protected boolean setValuesIfAvailable(PreparedStatement ps, int i) throws SQLException {
+						if (i < ids.length) {
+							ps.setInt(1, ids[i]);
+							return true;
+						}
+						else {
+							return false;
+						}
+					}
+				};
 
 		JdbcTemplate template = new JdbcTemplate(mockDataSource, false);
 
