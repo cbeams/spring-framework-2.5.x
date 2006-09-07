02c90cb839ab125be1a2fc932a53f858855b8dad
Juergen Hoeller <jhoeller@interface21.com>
2006-09-07 10:44:46 +0000

    factored out concrete loading of a single context from specified config locations to AbstractSingleSpringContextTests; restricted visibilities and stronger use of final; call ConfigurableApplicationContext's "registerShutdownHook()" for each context instance
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11430 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- mock/org/springframework/test/AbstractSpringContextTests.java
+++ mock/org/springframework/test/AbstractSpringContextTests.java
@@ -20,22 +20,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.util.StringUtils;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
 
 /**
  * Superclass for JUnit test cases using a Spring context.
  *
  * <p>Maintains a static cache of contexts by key. This has significant performance
- * benefit if initializing the context would take time. While initializing a 
+ * benefit if initializing the context would take time. While initializing a
  * Spring context itself is very quick, some beans in a context, such as
- * a LocalSessionFactoryBean for working with Hibernate, may take time to
- * initialize. Hence it often makes sense to do that initializing once.
+ * a LocalSessionFactoryBean for working with Hibernate, may take some time
+ * to initialize. Hence it often makes sense to do that initializing once.
  *
- * <p>Normally you won't extend this class directly but rather extend one
- * of its subclasses.
+ * <p>Any ApplicationContext created by this class will be asked to register a JVM
+ * shutdown hook for itself. Unless the context gets closed early, all context
+ * instances will be automatically closed on JVM shutdown. This allows for freeing
+ * external resources held by beans within the context, e.g. temporary files.
+ *
+ * <p>Normally you won't extend this class directly but rather extend one of
+ * its subclasses.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @since 1.1.1
  * @see AbstractDependencyInjectionSpringContextTests
  * @see AbstractTransactionalSpringContextTests
@@ -66,13 +72,50 @@ public abstract class AbstractSpringContextTests extends ConditionalTestCase {
 
 
 	/**
-	 * Set custom locations dirty. This will cause them to be reloaded
-	 * from the cache before the next test case is executed.
+	 * Explicitly add an ApplicationContext instance under a given key.
+	 * <p>This is not meant to be used by subclasses. It is rather exposed
+	 * for special test suite environments.
+	 * @param key the context key
+	 * @param context the ApplicationContext instance
+	 */
+	public final void addContext(Object key, ConfigurableApplicationContext context) {
+		Assert.notNull(context, "ApplicationContext must not be null");
+		contextKeyToContextMap.put(contextKeyString(key), context);
+	}
+
+	/**
+	 * Return whether there is a cached context for the given key.
+	 * @param contextKey the context key
+	 */
+	protected final boolean hasCachedContext(Object contextKey) {
+		return contextKeyToContextMap.containsKey(contextKey);
+	}
+
+	/**
+	 * Obtain an ApplicationContext for the given key, potentially cached.
+	 * @param key the context key
+	 * @return the corresponding ApplicationContext instance (potentially cached)
+	 */
+	protected final ConfigurableApplicationContext getContext(Object key) {
+		String keyString = contextKeyString(key);
+		ConfigurableApplicationContext ctx =
+				(ConfigurableApplicationContext) contextKeyToContextMap.get(keyString);
+		if (ctx == null) {
+			ctx = loadContext(key);
+			ctx.registerShutdownHook();
+			contextKeyToContextMap.put(keyString, ctx);
+		}
+		return ctx;
+	}
+
+	/**
+	 * Mark the context with the given key as dirty. This will cause the
+	 * cached context to be reloaded before the next test case is executed.
 	 * <p>Call this method only if you change the state of a singleton
 	 * bean, potentially affecting future tests.
 	 */
-	protected void setDirty(String[] locations) {
-		String keyString = contextKeyString(locations);
+	protected final void setDirty(Object contextKey) {
+		String keyString = contextKeyString(contextKey);
 		ConfigurableApplicationContext ctx =
 				(ConfigurableApplicationContext) contextKeyToContextMap.remove(keyString);
 		if (ctx != null) {
@@ -80,58 +123,22 @@ public abstract class AbstractSpringContextTests extends ConditionalTestCase {
 		}
 	}
 
-	protected boolean hasCachedContext(Object contextKey) {
-		return contextKeyToContextMap.containsKey(contextKey);
-	}
 
 	/**
 	 * Subclasses can override this to return a String representation of
-	 * their contextKey for use in logging
+	 * their context key for use in logging.
+	 * @param contextKey the context key
 	 */
 	protected String contextKeyString(Object contextKey) {
-		if (contextKey instanceof String[]) {
-			return StringUtils.arrayToCommaDelimitedString((String[]) contextKey);
-		}
-		else {
-			return contextKey.toString();
-		}
+		return ObjectUtils.nullSafeToString(contextKey);
 	}
-
-	public final void addContext(Object key, ConfigurableApplicationContext context) {
-		contextKeyToContextMap.put(contextKeyString(key), context);
-	}
-
-	protected ConfigurableApplicationContext getContext(Object key) {
-		String keyString = contextKeyString(key);
-		ConfigurableApplicationContext ctx =
-				(ConfigurableApplicationContext) contextKeyToContextMap.get(keyString);
-		if (ctx == null) {
-			if (key instanceof String[]) {
-				ctx = loadContextLocations((String[]) key);
-			}
-			else {
-				ctx = loadContext(key);
-			}
-			contextKeyToContextMap.put(keyString, ctx);
-		}
-		return ctx;
-	}
-
 
 	/**
-	 * Subclasses can invoke this to get a context key for the given location.
-	 * This doesn't affect the applicationContext instance variable in this class.
-	 * Dependency Injection cannot be applied from such contexts.
+	 * Load a new ApplicationContext for the given key.
+	 * <p>To be implemented by subclasses.
+	 * @param key the context key
+	 * @return the corresponding ApplicationContext instance (new)
 	 */
-	protected ConfigurableApplicationContext loadContextLocations(String[] locations) {
-		if (logger.isInfoEnabled()) {
-			logger.info("Loading config for: " + StringUtils.arrayToCommaDelimitedString(locations));
-		}
-		return new ClassPathXmlApplicationContext(locations);
-	}
-
-	protected ConfigurableApplicationContext loadContext(Object key) {
-		throw new UnsupportedOperationException("Subclasses may override this");
-	}
+	protected abstract ConfigurableApplicationContext loadContext(Object key);
 
 }
