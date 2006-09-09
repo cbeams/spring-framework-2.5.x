0deca6160c062d509dae82212e82acf92d38b53d
Juergen Hoeller <jhoeller@interface21.com>
2006-09-09 21:40:25 +0000

    fall back to SyncTaskExecutor in case of null
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11483 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/context/event/SimpleApplicationEventMulticaster.java
+++ src/org/springframework/context/event/SimpleApplicationEventMulticaster.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,6 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.task.SyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.util.Assert;
 
 /**
  * Simple implementation of the ApplicationEventMulticaster interface.
@@ -59,8 +58,7 @@ public class SimpleApplicationEventMulticaster extends AbstractApplicationEventM
 	 * @see org.springframework.scheduling.timer.TimerTaskExecutor
 	 */
 	public void setTaskExecutor(TaskExecutor taskExecutor) {
-		Assert.notNull(taskExecutor, "taskExecutor is required");
-		this.taskExecutor = taskExecutor;
+		this.taskExecutor = (taskExecutor != null ? taskExecutor : new SyncTaskExecutor());
 	}
 
 	/**
