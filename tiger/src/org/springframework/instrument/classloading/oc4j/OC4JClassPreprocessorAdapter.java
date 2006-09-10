748a0eecaf88459cdd8b559314e115a3bf7a82fe
Juergen Hoeller <jhoeller@interface21.com>
2006-09-10 08:28:14 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11488 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/instrument/classloading/oc4j/OC4JClassPreprocessorAdapter.java
+++ tiger/src/org/springframework/instrument/classloading/oc4j/OC4JClassPreprocessorAdapter.java
@@ -1,18 +1,19 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.instrument.classloading.oc4j;
 
 import java.lang.instrument.ClassFileTransformer;
@@ -21,48 +22,59 @@ import java.security.ProtectionDomain;
 
 import oracle.classloader.util.ClassPreprocessor;
 
+import org.springframework.util.Assert;
+
 /**
- * ClassPreprocessor adapter for OC4J. Many thanks to <a
- * href="mailto:mike.keith@oracle.com">Mike Keith</a> for assistance.
- * 
+ * ClassPreprocessor adapter for OC4J, delegating to a standard
+ * JDK ClassFileTransformer underneath.
+ *
+ * <p>Many thanks to <a
+ * href="mailto:mike.keith@oracle.com">Mike Keith</a>
+ * for his assistance.
+ *
  * @author Costin Leau
  * @since 2.0
- * 
  */
-public class OC4JClassPreprocessorAdapter implements ClassPreprocessor {
+class OC4JClassPreprocessorAdapter implements ClassPreprocessor {
 
-	private ClassFileTransformer transformer;
+	private final ClassFileTransformer transformer;
 
+
+	/**
+	 * Create a new OC4JClassPreprocessorAdapter for the given transformer.
+	 * @param transformer the standard JDK ClassFileTransformer to wrap
+	 */
 	public OC4JClassPreprocessorAdapter(ClassFileTransformer transformer) {
+		Assert.notNull(transformer, "Transformer must not be null");
 		this.transformer = transformer;
 	}
+
 
 	public ClassPreprocessor initialize(ClassLoader loader) {
 		return this;
 	}
 
-	public byte[] processClass(String className, byte origClassBytes[], int offset, int length, ProtectionDomain pd,
-			ClassLoader loader) {
+	public byte[] processClass(
+			String className, byte origClassBytes[], int offset, int length, ProtectionDomain pd, ClassLoader loader) {
 		try {
-			// extract the useful info
 			byte[] tempArray = new byte[length];
 			System.arraycopy(origClassBytes, offset, tempArray, 0, length);
 
-			// NB: oc4j passes className as "." without class while the
+			// NB: OC4J passes className as "." without class while the
 			// transformer expects a VM, "/" format
-			byte[] result = transformer.transform(loader, className.replace('.', '/'), null, pd, tempArray);
-			return (result == null ? origClassBytes : result);
+			byte[] result = this.transformer.transform(loader, className.replace('.', '/'), null, pd, tempArray);
+			return (result != null ? result : origClassBytes);
 		}
 		catch (IllegalClassFormatException ex) {
-			throw new RuntimeException("Cannot transform", ex);
+			throw new IllegalStateException("Cannot transform because of illegal class format", ex);
 		}
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("OC4JClassPreProcessorAdapter for:");
-		builder.append(transformer);
+		StringBuilder builder = new StringBuilder(getClass().getName());
+		builder.append(" for transformer: ");
+		builder.append(this.transformer);
 		return builder.toString();
 	}
 
