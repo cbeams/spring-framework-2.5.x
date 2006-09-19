26231fca2dad8c4e1bf624dd52b0c4f0b66a19c0
Juergen Hoeller <jhoeller@interface21.com>
2006-09-19 13:17:37 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11633 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/aspectj/AspectJProxyUtils.java
+++ src/org/springframework/aop/aspectj/AspectJProxyUtils.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,18 +24,17 @@ import org.springframework.aop.interceptor.ExposeInvocationInterceptor;
 
 /**
  * Utility methods for working with AspectJ proxies.
- * 
+ *
  * @author Rod Johnson
  * @since 2.0
  */
 public abstract class AspectJProxyUtils {
 	
 	/**
-	 * Add special advisors if necessary to work with a proxy chain
-	 * that contains AspectJ advisors. This will expose the current Spring AOP invocation
-	 * (necessary for some AspectJ pointcut matching) and make available the current AspectJ
-	 * JoinPoint. The call will have no effect if there are no AspectJ advisors
-	 * in the advisor chain.
+	 * Add special advisors if necessary to work with a proxy chain that contains AspectJ advisors.
+	 * This will expose the current Spring AOP invocation (necessary for some AspectJ pointcut matching)
+	 * and make available the current AspectJ JoinPoint. The call will have no effect if there are no
+	 * AspectJ advisors in the advisor chain.
 	 * @param advisors Advisors available
 	 * @return <code>true</code> if any special {@link Advisor Advisors} were added, otherwise <code>false</code>.
 	 */
@@ -47,8 +46,8 @@ public abstract class AspectJProxyUtils {
 				Advisor advisor = (Advisor) it.next();
 				// Be careful not to get the Advice without a guard, as
 				// this might eagerly instantiate a non-singleton AspectJ aspect
-				if ( 	(advisor instanceof InstantiationModelAwarePointcutAdvisor) || 
-						(advisor.getAdvice() instanceof AbstractAspectJAdvice)) {
+				if (advisor instanceof InstantiationModelAwarePointcutAdvisor ||
+						advisor.getAdvice() instanceof AbstractAspectJAdvice) {
 					foundAspectJAdvice = true;
 				}
 			}
