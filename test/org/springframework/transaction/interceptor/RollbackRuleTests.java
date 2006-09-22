3c3dd87fc6590d56c626ef1d3d6c32ae7fdf5f23
Juergen Hoeller <jhoeller@interface21.com>
2006-09-22 09:27:23 +0000

    throw IllegalArgumentException instead of AopConfigException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11677 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/transaction/interceptor/RollbackRuleTests.java
+++ test/org/springframework/transaction/interceptor/RollbackRuleTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,12 +20,10 @@ import javax.servlet.ServletException;
 
 import junit.framework.TestCase;
 
-import org.springframework.aop.framework.AopConfigException;
 import org.springframework.beans.FatalBeanException;
 import org.springframework.mail.MailSendException;
 
 /**
- * 
  * @author Rod Johnson
  * @since 09.04.2003
  */
@@ -65,7 +63,7 @@ public class RollbackRuleTests extends TestCase {
 			new RollbackRuleAttribute(StringBuffer.class);
 			fail("Can't construct a RollbackRuleAttribute without a throwable");
 		}
-		catch (AopConfigException ex) {
+		catch (IllegalArgumentException ex) {
 			// OK
 		}
 	}
