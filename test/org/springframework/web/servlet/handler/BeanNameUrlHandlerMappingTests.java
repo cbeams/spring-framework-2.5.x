616c9b765510f0d1fa31b2315ceaf272066e2ab1
Juergen Hoeller <jhoeller@interface21.com>
2006-09-27 14:20:17 +0000

    throw IllegalStateException in case of conflicting handler registration
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11744 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/web/servlet/handler/BeanNameUrlHandlerMappingTests.java
+++ test/org/springframework/web/servlet/handler/BeanNameUrlHandlerMappingTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,6 @@ import javax.servlet.ServletException;
 
 import junit.framework.TestCase;
 
-import org.springframework.context.ApplicationContextException;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
@@ -167,9 +166,9 @@ public class BeanNameUrlHandlerMappingTests extends TestCase {
 		BeanNameUrlHandlerMapping hm = (BeanNameUrlHandlerMapping) wac.getBean("handlerMapping");
 		try {
 			hm.registerHandler("/mypath/welcome.html", new Object());
-			fail("Should have thrown ApplicationContextException");
+			fail("Should have thrown IllegalStateException");
 		}
-		catch (ApplicationContextException ex) {
+		catch (IllegalStateException ex) {
 			// expected
 		}
 	}
