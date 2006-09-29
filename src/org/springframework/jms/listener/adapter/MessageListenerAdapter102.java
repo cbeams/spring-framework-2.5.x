5a71b1431b8f7ddf2b58dece81eeb56c59a80920
Juergen Hoeller <jhoeller@interface21.com>
2006-09-29 23:33:02 +0000

    fixed formatting
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11799 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jms/listener/adapter/MessageListenerAdapter102.java
+++ src/org/springframework/jms/listener/adapter/MessageListenerAdapter102.java
@@ -16,7 +16,17 @@
 
 package org.springframework.jms.listener.adapter;
 
-import javax.jms.*;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.QueueSender;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.jms.Topic;
+import javax.jms.TopicPublisher;
+import javax.jms.TopicSession;
 
 import org.springframework.jms.support.JmsUtils;
 import org.springframework.jms.support.converter.SimpleMessageConverter102;
@@ -24,34 +34,33 @@ import org.springframework.jms.support.converter.SimpleMessageConverter102;
 /**
  * A {@link MessageListenerAdapter} subclass that uses the JMS 1.0.2 specification,
  * rather than the JMS 1.1 methods used by MessageListenerAdapter itself.
- * 
+ *
  * <p>This class can be used for JMS 1.0.2 providers, offering the same facility
  * as MessageListenerAdapter does for JMS 1.1 providers.
- *
  * @author Juergen Hoeller
  * @author Rick Evans
  * @since 2.0
  */
 public class MessageListenerAdapter102 extends MessageListenerAdapter {
-    
+
 	/**
 	 * Create a new instance of the {@link MessageListenerAdapter102} class
-     * with the default settings.
+	 * with the default settings.
 	 */
 	public MessageListenerAdapter102() {
 	}
 
 	/**
 	 * Create a new instance of the {@link MessageListenerAdapter102} class
-     * for the given delegate.
-     * @param delegate the target object to delegate message listening to 
+	 * for the given delegate.
+	 * @param delegate the target object to delegate message listening to
 	 */
 	public MessageListenerAdapter102(Object delegate) {
-        super(delegate);
+		super(delegate);
 	}
 
 
-    /**
+	/**
 	 * Initialize the default implementations for the adapter's strategies:
 	 * SimpleMessageConverter102.
 	 * @see #setMessageConverter
@@ -67,22 +76,22 @@ public class MessageListenerAdapter102 extends MessageListenerAdapter {
 	 * else uses the JMS queue API.
 	 */
 	protected void sendResponse(Session session, Destination destination, Message response) throws JMSException {
-        MessageProducer producer = null;
-        try {
-            if (destination instanceof Topic) {
-                producer = ((TopicSession) session).createPublisher((Topic) destination);
-                postProcessProducer(producer, response);
-                ((TopicPublisher) producer).publish(response);
-            } else {
-                producer = ((QueueSession) session).createSender((Queue) destination);
-                postProcessProducer(producer, response);
+		MessageProducer producer = null;
+		try {
+			if (destination instanceof Topic) {
+				producer = ((TopicSession) session).createPublisher((Topic) destination);
+				postProcessProducer(producer, response);
+				((TopicPublisher) producer).publish(response);
+			}
+			else {
+				producer = ((QueueSession) session).createSender((Queue) destination);
+				postProcessProducer(producer, response);
 				((QueueSender) producer).send(response);
-            }
-        } finally {
-            if(producer != null) {
-                JmsUtils.closeMessageProducer(producer);
-            }
-        }
-    }
+			}
+		}
+		finally {
+			JmsUtils.closeMessageProducer(producer);
+		}
+	}
 
 }
