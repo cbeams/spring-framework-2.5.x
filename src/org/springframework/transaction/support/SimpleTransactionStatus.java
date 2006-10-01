a9595222e545e1e72b17c5bb706ac0a79880efea
Rick Evans <revans@interface21.com>
2006-10-01 09:27:51 +0000

    Javadoc polishing.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11805 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/support/SimpleTransactionStatus.java
+++ src/org/springframework/transaction/support/SimpleTransactionStatus.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,17 +16,19 @@
 
 package org.springframework.transaction.support;
 
-
-
 /**
- * Simple, generic implementation of the TransactionStatus interface.
- * Derives from AbstractTransactionStatus and adds an explicit "newTransaction" flag.
+ * A simple {@link org.springframework.transaction.TransactionStatus}
+ * implementation.
+ * 
+ * <p>Derives from {@link AbstractTransactionStatus} and adds an explicit
+ * {@link #isNewTransaction() "newTransaction"} flag.
  *
- * <p>This class is not used by any of Spring's pre-built PlatformTransactionManager
- * implementations. It is mainly provided as a start for custom transaction manager
- * implementations and as a static mock for testing transactional code (either
- * as part of a mock PlatformTransactionManager or as argument passed into a
- * TransactionCallback to be tested).
+ * <p>This class is not used by any of Spring's pre-built
+ * {@link org.springframework.transaction.PlatformTransactionManager}
+ * implementations. It is mainly provided as a start for custom transaction
+ * manager implementations and as a static mock for testing transactional
+ * code (either as part of a mock <code>PlatformTransactionManager</code> or
+ * as argument passed into a {@link TransactionCallback} to be tested).
  *
  * @author Juergen Hoeller
  * @since 1.2.3
@@ -37,20 +39,23 @@ public class SimpleTransactionStatus extends AbstractTransactionStatus {
 
 	private final boolean newTransaction;
 
+
 	/**
-	 * Create a new SimpleTransactionStatus, indicating a new transaction.
+	 * Creates a new instance of the {@link SimpleTransactionStatus} class,
+	 * indicating a new transaction.
 	 */
 	public SimpleTransactionStatus() {
-		this.newTransaction = true;
+		this(true);
 	}
 
 	/**
-	 * Create a new SimpleTransactionStatus.
-	 * @param newTransaction whether to indicate a new transaction.
+	 * Creates a new instance of the {@link SimpleTransactionStatus} class.
+	 * @param newTransaction whether to indicate a new transaction
 	 */
 	public SimpleTransactionStatus(boolean newTransaction) {
 		this.newTransaction = newTransaction;
 	}
+
 
 	public boolean isNewTransaction() {
 		return newTransaction;
