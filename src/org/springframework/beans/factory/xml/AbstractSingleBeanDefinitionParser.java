2e60a67ec7fa53889d46de3ef6fb2e410f9b04cc
Juergen Hoeller <jhoeller@interface21.com>
2006-10-02 13:21:42 +0000

    added overloaded "doParse" template method with a ParserContext argument
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11814 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/xml/AbstractSingleBeanDefinitionParser.java
+++ src/org/springframework/beans/factory/xml/AbstractSingleBeanDefinitionParser.java
@@ -33,11 +33,11 @@ import org.springframework.util.Assert;
  *
  * <p>The resulting <code>BeanDefinition</code> will be automatically registered
  * with the {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
- * Your job simply is to
- * {@link #doParse(org.w3c.dom.Element, org.springframework.beans.factory.support.BeanDefinitionBuilder) parse}
- * the custom XML {@link Element} into a single <code>BeanDefinition</code>.
+ * Your job simply is to {@link #doParse parse} the custom XML {@link Element}
+ * into a single <code>BeanDefinition</code>.
  *
  * @author Rob Harrop
+ * @author Juergen Hoeller
  * @author Rick Evans
  * @since 2.0
  */
@@ -48,11 +48,11 @@ public abstract class AbstractSingleBeanDefinitionParser extends AbstractBeanDef
 	 * {@link #getBeanClass bean Class} and passes it to the
 	 * {@link #doParse} strategy method.
 	 * @param element the element that is to be parsed into a single BeanDefinition
-	 * @param parserContext the object encapsulating the current state of the parse
+	 * @param parserContext the object encapsulating the current state of the parsing process
 	 * @return the BeanDefinition resulting from the parsing of the supplied {@link Element}
 	 * @throws IllegalStateException if the bean {@link Class} returned from
 	 * {@link #getBeanClass(org.w3c.dom.Element)} is <code>null</code>
-	 * @see #doParse(org.w3c.dom.Element, org.springframework.beans.factory.support.BeanDefinitionBuilder)
+	 * @see #doParse
 	 */
 	protected final AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
 		Class beanClass = getBeanClass(element);
@@ -62,7 +62,7 @@ public abstract class AbstractSingleBeanDefinitionParser extends AbstractBeanDef
 			// Inner bean definition must receive same singleton status as containing bean.
 			builder.setSingleton(parserContext.getContainingBeanDefinition().isSingleton());
 		}
-		doParse(element, builder);
+		doParse(element, parserContext, builder);
 		return builder.getBeanDefinition();
 	}
 
@@ -74,6 +74,20 @@ public abstract class AbstractSingleBeanDefinitionParser extends AbstractBeanDef
 	 * @see #parseInternal(org.w3c.dom.Element, ParserContext)   
 	 */
 	protected abstract Class getBeanClass(Element element);
+
+	/**
+	 * Parse the supplied {@link Element} and populate the supplied
+	 * {@link BeanDefinitionBuilder} as required.
+	 * <p>The default implementation delegates to the <code>doParse</code>
+	 * version without ParserContext argument.
+	 * @param element the XML element being parsed
+	 * @param parserContext the object encapsulating the current state of the parsing process
+	 * @param builder used to define the <code>BeanDefinition</code>
+	 * @see #doParse(Element, BeanDefinitionBuilder)
+	 */
+	protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
+		doParse(element, builder);
+	}
 
 	/**
 	 * Parse the supplied {@link Element} and populate the supplied
