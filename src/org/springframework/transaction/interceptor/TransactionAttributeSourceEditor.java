e2906308deb622131eef2a429744759b4c98ea5a
Juergen Hoeller <jhoeller@interface21.com>
2006-10-12 12:45:38 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11930 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/interceptor/TransactionAttributeSourceEditor.java
+++ src/org/springframework/transaction/interceptor/TransactionAttributeSourceEditor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,15 +20,13 @@ import java.beans.PropertyEditorSupport;
 import java.util.Iterator;
 import java.util.Properties;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import org.springframework.beans.propertyeditors.PropertiesEditor;
+import org.springframework.util.StringUtils;
 
 /**
- * Property editor that can convert String into TransactionAttributeSource.
+ * Property editor that converts a String into a {@link TransactionAttributeSource}.
  * The transaction attribute string must be parseable by the
- * TransactionAttributeEditor in this package.
+ * {@link TransactionAttributeEditor} in this package.
  *
  * <p>Strings are in property syntax, with the form:<br>
  * <code>FQCN.methodName=&lt;transaction attribute string&gt;</code>
@@ -50,30 +48,25 @@ import org.springframework.beans.propertyeditors.PropertiesEditor;
  */
 public class TransactionAttributeSourceEditor extends PropertyEditorSupport {
 
-	protected final Log logger = LogFactory.getLog(getClass());
-
-	public void setAsText(String s) throws IllegalArgumentException {
+	public void setAsText(String text) throws IllegalArgumentException {
 		MethodMapTransactionAttributeSource source = new MethodMapTransactionAttributeSource();
-		if (s == null || "".equals(s)) {
-			// Leave value in property editor <code>null</code>
-		}
-		else {
-			// Use properties editor to tokenize the hold string
+		if (StringUtils.hasLength(text)) {
+			// Use properties editor to tokenize the hold string.
 			PropertiesEditor propertiesEditor = new PropertiesEditor();
-			propertiesEditor.setAsText(s);
+			propertiesEditor.setAsText(text);
 			Properties props = (Properties) propertiesEditor.getValue();
 
-			// Now we have properties, process each one individually
+			// Now we have properties, process each one individually.
 			TransactionAttributeEditor tae = new TransactionAttributeEditor();
 			for (Iterator iter = props.keySet().iterator(); iter.hasNext();) {
 				String name = (String) iter.next();
 				String value = props.getProperty(name);
 
-				// Convert value to a transaction attribute
+				// Convert value to a transaction attribute.
 				tae.setAsText(value);
 				TransactionAttribute attr = (TransactionAttribute) tae.getValue();
 
-				// Register name and attribute
+				// Register name and attribute.
 				source.addTransactionalMethod(name, attr);
 			}
 		}
