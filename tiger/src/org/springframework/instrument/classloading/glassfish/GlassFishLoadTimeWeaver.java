859f5d97e154195d3ded21bbb96eeab755ba73ac
Juergen Hoeller <jhoeller@interface21.com>
2006-10-14 00:08:26 +0000

    refined javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@11944 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/instrument/classloading/glassfish/GlassFishLoadTimeWeaver.java
+++ tiger/src/org/springframework/instrument/classloading/glassfish/GlassFishLoadTimeWeaver.java
@@ -1,100 +1,83 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.instrument.classloading.glassfish;
 
 import java.lang.instrument.ClassFileTransformer;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import com.sun.enterprise.loader.InstrumentableClassLoader;
+
 import org.springframework.instrument.classloading.LoadTimeWeaver;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
-import com.sun.enterprise.loader.InstrumentableClassLoader;
-
 /**
- * Glassfish specific LTW. Uses the application server enhancing capabilities,
- * to execute
- * 
+ * {@link LoadTimeWeaver} implementation for GlassFish's instrumentable ClassLoader.
+ *
  * @author Costin Leau
- * 
+ * @author Juergen Hoeller
+ * @since 2.0.1
+ * @see com.sun.enterprise.loader.InstrumentableClassLoader
  */
 public class GlassFishLoadTimeWeaver implements LoadTimeWeaver {
 
-	private static final Log log = LogFactory.getLog(GlassFishLoadTimeWeaver.class);
-	private InstrumentableClassLoader classLoader;
+	private final InstrumentableClassLoader classLoader;
+
 
 	public GlassFishLoadTimeWeaver() {
 		this(ClassUtils.getDefaultClassLoader());
 	}
 
-	public GlassFishLoadTimeWeaver(ClassLoader loader) {
-		Assert.notNull(loader, "parameter cannot be null");
-		InstrumentableClassLoader cl = determineClassLoader(loader);
-
-		if (cl == null)
-			throw new IllegalArgumentException(loader + " and its parents are not suitable classloaders; an "
-					+ InstrumentableClassLoader.class.getName() + " implementation required");
-
-		this.classLoader = cl;
-
+	public GlassFishLoadTimeWeaver(ClassLoader classLoader) {
+		Assert.notNull(classLoader, "ClassLoader must not be null");
+		InstrumentableClassLoader icl = determineClassLoader(classLoader);
+		if (icl == null) {
+			throw new IllegalArgumentException(classLoader + " and its parents are not suitable ClassLoaders: " +
+					"An [" + InstrumentableClassLoader.class.getName() + "] implementation is required.");
+		}
+		this.classLoader = icl;
 	}
 
-	protected InstrumentableClassLoader determineClassLoader(ClassLoader loader) {
-		// detect transformation aware classloader by traversing the hierarchy
-		// (as in GlassFish, Spring can be loaded by the WebappClassLoader)
-		for (ClassLoader cl = loader; cl != null; cl = cl.getParent()) {
-
-			if (log.isDebugEnabled())
-				log.debug("inspecting classloader " + cl);
-
+	/**
+	 * Determine the GlassFish InstrumentableClassLoader for the given ClassLoader.
+	 * @param classLoader the ClassLoader to check
+	 * @return the InstrumentableClassLoader, or <code>null</code> if none found
+	 */
+	protected InstrumentableClassLoader determineClassLoader(ClassLoader classLoader) {
+		// Detect transformation-aware ClassLoader by traversing the hierarchy
+		// (as in GlassFish, Spring can be loaded by the WebappClassLoader).
+		for (ClassLoader cl = classLoader; cl != null; cl = cl.getParent()) {
 			if (cl instanceof InstrumentableClassLoader) {
-				if (log.isDebugEnabled())
-					log.debug("found instrumentable classloader " + cl);
 				return (InstrumentableClassLoader) cl;
 			}
 		}
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.instrument.classloading.LoadTimeWeaver#addTransformer(java.lang.instrument.ClassFileTransformer)
-	 */
+
 	public void addTransformer(ClassFileTransformer transformer) {
-		classLoader.addTransformer(new ClassTransformerAdapter(transformer));
+		this.classLoader.addTransformer(new ClassTransformerAdapter(transformer));
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.instrument.classloading.LoadTimeWeaver#getInstrumentableClassLoader()
-	 */
 	public ClassLoader getInstrumentableClassLoader() {
-		return (ClassLoader) classLoader;
+		return (ClassLoader) this.classLoader;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.instrument.classloading.LoadTimeWeaver#getThrowawayClassLoader()
-	 */
 	public ClassLoader getThrowawayClassLoader() {
-		return classLoader.copy();
+		return this.classLoader.copy();
 	}
 
 }
