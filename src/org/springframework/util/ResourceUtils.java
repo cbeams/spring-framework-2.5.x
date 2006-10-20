a0df2e339ae7799e68b7554f732f6363113537cb
Juergen Hoeller <jhoeller@interface21.com>
2006-10-20 15:07:18 +0000

    detect jar file URL in WebLogic jar entry URLs correctly
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12019 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/ResourceUtils.java
+++ src/org/springframework/util/ResourceUtils.java
@@ -220,7 +220,15 @@ public abstract class ResourceUtils {
 		String urlFile = jarUrl.getFile();
 		int separatorIndex = urlFile.indexOf(JAR_URL_SEPARATOR);
 		if (separatorIndex != -1) {
-			return new URL(urlFile.substring(0, separatorIndex));
+			String jarFile = urlFile.substring(0, separatorIndex);
+			try {
+				return new URL(jarFile);
+			}
+			catch (MalformedURLException ex) {
+				// Probably no protocol in original jar URL, like "jar:C:/mypath/myjar.jar".
+				// This usually indicates that the jar file resides in the file system.
+				return new URL(FILE_URL_PREFIX + jarFile);
+			}
 		}
 		else {
 			return jarUrl;
--- test/org/springframework/util/ResourceUtilsTests.java
+++ test/org/springframework/util/ResourceUtilsTests.java
@@ -40,6 +40,8 @@ public class ResourceUtilsTests extends TestCase {
 		assertEquals(new URL("file:myjar.jar"),
 				ResourceUtils.extractJarFileURL(new URL("jar:file:myjar.jar!/mypath")));
 		assertEquals(new URL("file:myjar.jar"),
+				ResourceUtils.extractJarFileURL(new URL(null, "jar:myjar.jar!/mypath", new DummyURLStreamHandler())));
+		assertEquals(new URL("file:myjar.jar"),
 				ResourceUtils.extractJarFileURL(new URL(null, "zip:file:myjar.jar!/mypath", new DummyURLStreamHandler())));
 		assertEquals(new URL("file:myjar.jar"),
 				ResourceUtils.extractJarFileURL(new URL(null, "wsjar:file:myjar.jar!/mypath", new DummyURLStreamHandler())));
