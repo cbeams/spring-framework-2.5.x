025c70bd766d860c1d2771d13010f20b7c36db05
Juergen Hoeller <jhoeller@interface21.com>
2006-10-30 13:47:31 +0000

    javadoc polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12099 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/ConstantException.java
+++ src/org/springframework/core/ConstantException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +17,8 @@
 package org.springframework.core;
 
 /**
- * Exception thrown when the Constants class is asked for an invalid
- * constant name.
+ * Exception thrown when the {@link Constants} class is asked for
+ * an invalid constant name.
  *
  * @author Rod Johnson
  * @since 28.04.2003
