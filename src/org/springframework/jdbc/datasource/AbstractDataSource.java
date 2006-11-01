887e8f0c33c6dd69d7ee0d3aebfd69c38f5324c9
Juergen Hoeller <jhoeller@interface21.com>
2006-11-01 15:07:33 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12130 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/AbstractDataSource.java
+++ src/org/springframework/jdbc/datasource/AbstractDataSource.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,7 +26,7 @@ import org.apache.commons.logging.LogFactory;
 
 /**
  * Abstract base class for Spring's DataSource implementations,
- * taking care of the "uninteresting" glue.
+ * taking care of the padding.
  *
  * @author Juergen Hoeller
  * @since 07.05.2003
@@ -34,28 +34,33 @@ import org.apache.commons.logging.LogFactory;
  */
 public abstract class AbstractDataSource implements DataSource {
 
+	/** Logger available to subclasses */
 	protected final Log logger = LogFactory.getLog(getClass());
 
+
 	/**
-	 * Returns 0: means use default system timeout.
+	 * Returns 0, indicating to use the default system timeout.
 	 */
 	public int getLoginTimeout() throws SQLException {
 		return 0;
 	}
 
+	/**
+	 * Setting a login timeout is not supported.
+	 */
 	public void setLoginTimeout(int timeout) throws SQLException {
 		throw new UnsupportedOperationException("setLoginTimeout");
 	}
 
 	/**
-	 * LogWriter methods are unsupported.
+	 * LogWriter methods are not supported.
 	 */
 	public PrintWriter getLogWriter() {
 		throw new UnsupportedOperationException("getLogWriter");
 	}
 
 	/**
-	 * LogWriter methods are unsupported.
+	 * LogWriter methods are not supported.
 	 */
 	public void setLogWriter(PrintWriter pw) throws SQLException {
 		throw new UnsupportedOperationException("setLogWriter");
