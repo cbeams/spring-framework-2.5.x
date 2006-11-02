ba730af9ef538628a3ea9c27b4100a2cf3dcd342
Juergen Hoeller <jhoeller@interface21.com>
2006-11-02 16:07:50 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12145 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/RequestToViewNameTranslator.java
+++ src/org/springframework/web/servlet/RequestToViewNameTranslator.java
@@ -19,8 +19,9 @@ package org.springframework.web.servlet;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Translates an incoming {@link javax.servlet.http.HttpServletRequest}
- * into a logical view name when no view name is explicitly supplied.
+ * Strategy interface for translating an incoming
+ * {@link javax.servlet.http.HttpServletRequest} into a
+ * logical view name when no view name is explicitly supplied.
  *
  * @author Rob Harrop
  * @since 2.0
@@ -29,8 +30,8 @@ public interface RequestToViewNameTranslator {
 
 	/**
 	 * Translate the given {@link HttpServletRequest} into a view name.
-	 * @param request the incoming {@link HttpServletRequest} providing the
-	 *                context from which a view name is to be resolved
+	 * @param request the incoming {@link HttpServletRequest} providing
+	 * the context from which a view name is to be resolved
 	 * @return the view name (never <code>null</code>)
 	 * @throws Exception if view name translation fails
 	 */
