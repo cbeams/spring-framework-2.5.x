a674a04fa7c3d44aeb27d9d4f4343d968e03b286
Rick Evans <revans@interface21.com>
2006-11-04 14:30:39 +0000

    Added Javadoc missing from the 'extractPathWithinPattern' method declaration.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12160 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/PathMatcher.java
+++ src/org/springframework/util/PathMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,40 +17,46 @@
 package org.springframework.util;
 
 /**
- * Strategy interface for String-based path matching.
- * Used by PathMatchingResourcePatternResolver, AbstractUrlHandlerMapping,
- * PropertiesMethodNameResolver, WebContentInterceptor.
+ * Strategy interface for <code>String</code>-based path matching.
+ * 
+ * <p>Used by {@link org.springframework.core.io.support.PathMatchingResourcePatternResolver},
+ * {@link org.springframework.web.servlet.handler.AbstractUrlHandlerMapping},
+ * {@link org.springframework.web.servlet.mvc.multiaction.PropertiesMethodNameResolver},
+ * and {@link org.springframework.web.servlet.mvc.WebContentInterceptor}.
  *
- * <p>The default implementation is AntPathMatcher,
- * supporting Ant-style pattern syntax.
+ * <p>The default implementation is {@link AntPathMatcher}, supporting the
+ * Ant-style pattern syntax.
  *
  * @author Juergen Hoeller
  * @since 1.2
  * @see AntPathMatcher
- * @see org.springframework.core.io.support.PathMatchingResourcePatternResolver
- * @see org.springframework.web.servlet.handler.AbstractUrlHandlerMapping
- * @see org.springframework.web.servlet.mvc.multiaction.PropertiesMethodNameResolver
- * @see org.springframework.web.servlet.mvc.WebContentInterceptor
  */
 public interface PathMatcher {
 
 	/**
-	 * Return if the given string represents a pattern to be matched
-	 * via this class: If not, the "match" method does not have to be
-	 * used because direct equality comparisons are sufficient.
-	 * @param str the string to check
-	 * @return whether the given string represents a pattern
-	 * @see #match
+	 * Does the given <code>string</code> represent a pattern that can be matched
+	 * by an implementation of this interface?
+	 * <p>If the return value is <code>false</code>, then the {@link #match}
+	 * method does not have to be used because direct equality comparisons will
+	 * be sufficient.
+	 * @param text the <code>string</code> to check
+	 * @return <code>true</code> if the given <code>string</code> represents a pattern
 	 */
-	boolean isPattern(String str);
+	boolean isPattern(String text);
 
 	/**
-	 * Match a string against the given pattern.
+	 * Match the given <code>text</code> against the given <code>pattern</code>.
 	 * @param pattern the pattern to match against
-	 * @param str the string to test
-	 * @return whether the arguments matched
+	 * @param text the string to test
+	 * @return <code>true</code> if the supplied <code>text</code> matched
 	 */
-	boolean match(String pattern, String str);
+	boolean match(String pattern, String text);
 
+	/**
+	 * Given a pattern and a full path, returns the non-pattern mapped part.
+	 * @param pattern the path pattern
+	 * @param path the full path
+	 * @return the non-pattern mapped part of the given <code>path</code>
+	 */
 	String extractPathWithinPattern(String pattern, String path);
 }
