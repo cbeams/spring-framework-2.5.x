4c5569bf42d527e004865ff2e69ff43e450d930b
Rick Evans <revans@interface21.com>
2006-11-16 09:45:10 +0000

    Corrected Javadoc typos.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12219 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
+++ src/org/springframework/beans/factory/config/PropertyPlaceholderConfigurer.java
@@ -194,7 +194,7 @@ public class PropertyPlaceholderConfigurer extends PropertyResourceConfigurer
 
 	/**
 	 * Set whether to ignore unresolvable placeholders. Default is "false":
-	 * An exception will be thrown if a placeholder cannot not be resolved.
+	 * An exception will be thrown if a placeholder cannot be resolved.
 	 */
 	public void setIgnoreUnresolvablePlaceholders(boolean ignoreUnresolvablePlaceholders) {
 		this.ignoreUnresolvablePlaceholders = ignoreUnresolvablePlaceholders;
@@ -387,8 +387,8 @@ public class PropertyPlaceholderConfigurer extends PropertyResourceConfigurer
 
 	/**
 	 * BeanDefinitionVisitor that resolves placeholders in String values,
-	 * deleagating to the <code>parseStringValue</code> method of the
-	 * containing clas.
+	 * delegating to the <code>parseStringValue</code> method of the
+	 * containing class.
 	 */
 	private class PlaceholderResolvingBeanDefinitionVisitor extends BeanDefinitionVisitor {
 
