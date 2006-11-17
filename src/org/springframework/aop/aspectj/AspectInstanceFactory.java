9a9248b77d2d08113717620d38f0aabaacb0583f
Juergen Hoeller <jhoeller@interface21.com>
2006-11-17 22:22:22 +0000

    removed "getInstantiationCount()" method; AspectInstanceFactory expresses order
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12243 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/aspectj/AspectInstanceFactory.java
+++ src/org/springframework/aop/aspectj/AspectInstanceFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,24 +16,26 @@
 
 package org.springframework.aop.aspectj;
 
+import org.springframework.core.Ordered;
+
 /**
- * Interface implemented to provide instance of the AspectJ aspect on which the
- * aspect method exists. Decouples from Spring's IoC container.
+ * Interface implemented to provide an instance of an AspectJ aspect.
+ * Decouples from Spring's bean factory.
+ *
+ * <p>Extends the {@link org.springframework.core.Ordered} interface
+ * to express an order value for the underlying aspect in a chain.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @since 2.0
+ * @see org.springframework.beans.factory.BeanFactory#getBean
  */
-public interface AspectInstanceFactory {
+public interface AspectInstanceFactory extends Ordered {
 
 	/**
 	 * Create an instance of this factory's aspect.
+	 * @return the aspect instance (never <code>null</code>)
 	 */
 	Object getAspectInstance();
-	
-	/**
-	 * Return the number of instantiations from this factory.
-	 * @return the number of aspect instances created
-	 */
-	int getInstantiationCount();
-	
+
 }
--- src/org/springframework/aop/aspectj/SingletonAspectInstanceFactory.java
+++ src/org/springframework/aop/aspectj/SingletonAspectInstanceFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2006 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,14 @@
 
 package org.springframework.aop.aspectj;
 
+import org.springframework.core.Ordered;
+import org.springframework.util.Assert;
+
 /**
- * Simple implementation of AspectInstanceFactory backed
- * by a singleton.
+ * Simple implementation of AspectInstanceFactory backed by a singleton object.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @since 2.0
  */
 public class SingletonAspectInstanceFactory implements AspectInstanceFactory {
@@ -28,20 +31,25 @@ public class SingletonAspectInstanceFactory implements AspectInstanceFactory {
 	private final Object aspectInstance;
 
 
+	/**
+	 * Create a new SingletonAspectInstanceFactory for the given aspect instance.
+	 * @param aspectInstance the singleton aspect instance
+	 */
 	public SingletonAspectInstanceFactory(Object aspectInstance) {
-		if (aspectInstance == null) {
-			throw new IllegalStateException("AspectInstance may not be null");
-		}
+		Assert.notNull(aspectInstance, "Aspect instance must not be null");
 		this.aspectInstance = aspectInstance;
 	}
 
 
 	public final Object getAspectInstance() {
-		return aspectInstance;
+		return this.aspectInstance;
 	}
 
-	public final int getInstantiationCount() {
-		return 1;
+	public int getOrder() {
+		if (this.aspectInstance instanceof Ordered) {
+			return ((Ordered) this.aspectInstance).getOrder();
+		}
+		return Ordered.LOWEST_PRECEDENCE;
 	}
-	
+
 }
