505ff7d702289d7a7bca4a55a415ebfca42350f1
Juergen Hoeller <jhoeller@interface21.com>
2006-11-20 22:19:12 +0000

    added event tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12287 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ test/org/springframework/transaction/TxNamespaceHandlerEventTests.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.transaction;
+
+import junit.framework.TestCase;
+
+import org.springframework.beans.factory.parsing.BeanComponentDefinition;
+import org.springframework.beans.factory.parsing.CollectingReaderEventListener;
+import org.springframework.beans.factory.parsing.ComponentDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @author Torsten Juergeleit
+ * @author Juergen Hoeller
+ */
+public class TxNamespaceHandlerEventTests extends TestCase {
+
+	private CollectingReaderEventListener eventListener = new CollectingReaderEventListener();
+
+	private XmlBeanDefinitionReader reader;
+
+	private DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+
+	public void setUp() throws Exception {
+		this.reader = new XmlBeanDefinitionReader(this.beanFactory);
+		this.reader.setEventListener(this.eventListener);
+		this.reader.loadBeanDefinitions(new ClassPathResource("transactionNamespaceHandlerTests.xml", getClass()));
+	}
+
+	public void testComponentEventReceived() {
+		ComponentDefinition component = this.eventListener.getComponentDefinition("txAdvice");
+		assertTrue(component instanceof BeanComponentDefinition);
+		/*
+		ComponentDefinition component = this.eventListener.getComponentDefinition("tx:advice");
+		assertTrue("No holder with nested component 'tx:advice'", component instanceof CompositeComponentDefinition);
+		CompositeComponentDefinition compositeDef = (CompositeComponentDefinition) component;
+
+		ComponentDefinition[] nestedComponentDefs = compositeDef.getNestedComponents();
+		assertEquals("Incorrect number of inner components", 1, nestedComponentDefs.length);
+		assertTrue(compositeDef.getNestedComponents()[0] instanceof BeanComponentDefinition);
+		*/
+	}
+
+}
