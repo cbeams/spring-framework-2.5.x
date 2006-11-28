e76eeb0d2951928ad7abd947122fedc65ae661fd
Juergen Hoeller <jhoeller@interface21.com>
2006-11-28 09:13:00 +0000

    switch FlushMode to MANUAL in case of read-only, and enforce COMMIT or AUTO for read-write transaction
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12326 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/orm/jpa/vendor/HibernateJpaDialect.java
+++ tiger/src/org/springframework/orm/jpa/vendor/HibernateJpaDialect.java
@@ -22,12 +22,15 @@ import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceException;
 
+import org.hibernate.FlushMode;
 import org.hibernate.Session;
 import org.hibernate.ejb.HibernateEntityManager;
 
 import org.springframework.jdbc.datasource.ConnectionHandle;
 import org.springframework.jdbc.datasource.SimpleConnectionHandle;
 import org.springframework.orm.jpa.DefaultJpaDialect;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
 
 /**
  * {@link org.springframework.orm.jpa.JpaDialect} implementation for
@@ -38,6 +41,32 @@ import org.springframework.orm.jpa.DefaultJpaDialect;
  * @since 2.0
  */
 public class HibernateJpaDialect extends DefaultJpaDialect {
+
+	public Object beginTransaction(EntityManager entityManager, TransactionDefinition definition)
+			throws PersistenceException, SQLException, TransactionException {
+
+		super.beginTransaction(entityManager, definition);
+		Session session = getSession(entityManager);
+		FlushMode flushMode = session.getFlushMode();
+		FlushMode previousFlushMode = null;
+		if (definition.isReadOnly()) {
+			// We should suppress flushing for a read-only transaction.
+			session.setFlushMode(FlushMode.MANUAL);
+			previousFlushMode = flushMode;
+		}
+		else {
+			// We need AUTO or COMMIT for a non-read-only transaction.
+			if (flushMode.lessThan(FlushMode.COMMIT)) {
+				session.setFlushMode(FlushMode.AUTO);
+				previousFlushMode = flushMode;
+			}
+		}
+		return new SessionTransactionData(session, previousFlushMode);
+	}
+
+	public void cleanupTransaction(Object transactionData) {
+		((SessionTransactionData) transactionData).resetFlushMode();
+	}
 
 	@Override
 	public ConnectionHandle getJdbcConnection(EntityManager entityManager, boolean readOnly)
@@ -50,6 +79,25 @@ public class HibernateJpaDialect extends DefaultJpaDialect {
 
 	protected Session getSession(EntityManager em) {
 		return ((HibernateEntityManager) em).getSession();
+	}
+
+
+	private static class SessionTransactionData {
+
+		private final Session session;
+
+		private final FlushMode previousFlushMode;
+
+		public SessionTransactionData(Session session, FlushMode previousFlushMode) {
+			this.session = session;
+			this.previousFlushMode = previousFlushMode;
+		}
+
+		public void resetFlushMode() {
+			if (this.previousFlushMode != null) {
+				this.session.setFlushMode(this.previousFlushMode);
+			}
+		}
 	}
 
 }
