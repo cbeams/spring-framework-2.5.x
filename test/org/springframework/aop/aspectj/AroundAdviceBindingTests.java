d58b61ba8a5785349ff50dc659d2100b5013ffd9
Juergen Hoeller <jhoeller@interface21.com>
2006-12-11 15:13:30 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12386 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/aspectj/AroundAdviceBindingTests.java
+++ test/org/springframework/aop/aspectj/AroundAdviceBindingTests.java
@@ -1,29 +1,31 @@
 /*
  * Copyright 2002-2006 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.aop.aspectj;
 
 import org.easymock.MockControl;
+
 import org.springframework.aop.aspectj.AroundAdviceBindingTestAspect.AroundAdviceBindingCollaborator;
 
 /**
- * Tests for various parameter binding scenarios with before advice
+ * Tests for various parameter binding scenarios with before advice.
+ *
  * @author Adrian Colyer
  */
-public class AroundAdviceBindingTests extends
-		AbstractAdviceBindingTests {
+public class AroundAdviceBindingTests extends AbstractAdviceBindingTests {
 
 	private MockControl mockControl;
 	private AroundAdviceBindingCollaborator mockCollaborator;
