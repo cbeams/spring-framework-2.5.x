f5ff5364e6baee0a823bdf6c421823be5ea9679e
Juergen Hoeller <jhoeller@interface21.com>
2006-12-24 12:23:02 +0000

    polished javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@12462 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/PropertyEditorRegistry.java
+++ src/org/springframework/beans/PropertyEditorRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,11 @@ package org.springframework.beans;
 import java.beans.PropertyEditor;
 
 /**
- * Interface that encapsulates methods for registering JavaBeans PropertyEditors.
- * This is the interface that a PropertyEditorRegistrar operates on.
+ * Encapsulates methods for registering JavaBeans {@link PropertyEditor PropertyEditors}.
+ * This is the central interface that a {@link PropertyEditorRegistrar} operates on.
  *
- * <p>Implemented by BeanWrapper/BeanWrapperImpl and by the DataBinder class
- * in the validation package.
+ * <p>Extended by {@link BeanWrapper}; implemented by {@link BeanWrapperImpl}
+ * and  {@link org.springframework.validation.DataBinder}.
  *
  * @author Juergen Hoeller
  * @since 1.2.6
@@ -47,16 +47,19 @@ public interface PropertyEditorRegistry {
 	 * property, or for all properties of the given type.
 	 * <p>If the property path denotes an array or Collection property,
 	 * the editor will get applied either to the array/Collection itself
-	 * (the PropertyEditor has to create an array or Collection value) or
-	 * to each element (the PropertyEditor has to create the element type),
+	 * (the {@link PropertyEditor} has to create an array or Collection value) or
+	 * to each element (the <code>PropertyEditor</code> has to create the element type),
 	 * depending on the specified required type.
 	 * <p>Note: Only one single registered custom editor per property path
-	 * is supported. In case of a Collection/array, do not register an editor
+	 * is supported. In the case of a Collection/array, do not register an editor
 	 * for both the Collection/array and each element on the same property.
+	 * <p>For example, if you wanted to register an editor for "items[n].quantity"
+	 * (for all values n), you would use "items.quality" as the value of the
+	 * 'propertyPath' argument to this method.
 	 * @param requiredType type of the property (can be <code>null</code> if a property
 	 * is given but should be specified in any case for consistency checking)
 	 * @param propertyPath path of the property (name or nested path), or
-	 * null if registering an editor for all properties of the given type
+	 * <code>null</code> if registering an editor for all properties of the given type
 	 * @param propertyEditor editor to register
 	 */
 	void registerCustomEditor(Class requiredType, String propertyPath, PropertyEditor propertyEditor);
@@ -66,7 +69,7 @@ public interface PropertyEditorRegistry {
 	 * @param requiredType type of the property (can be <code>null</code> if a property
 	 * is given but should be specified in any case for consistency checking)
 	 * @param propertyPath path of the property (name or nested path), or
-	 * null if looking for an editor for all properties of the given type
+	 * <code>null</code> if looking for an editor for all properties of the given type
 	 * @return the registered editor, or <code>null</code> if none
 	 */
 	PropertyEditor findCustomEditor(Class requiredType, String propertyPath);
