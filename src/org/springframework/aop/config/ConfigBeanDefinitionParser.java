38f5d85acffbea75f0d0016802069fd1abf41dfc
Juergen Hoeller <jhoeller@interface21.com>
2007-01-03 15:20:39 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12550 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/config/ConfigBeanDefinitionParser.java
+++ src/org/springframework/aop/config/ConfigBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -516,8 +516,7 @@ class ConfigBeanDefinitionParser implements BeanDefinitionParser {
 	 * the supplied pointcut expression.
 	 */
 	protected AbstractBeanDefinition createPointcutDefinition(String expression) {
-		RootBeanDefinition beanDefinition = new RootBeanDefinition();
-		beanDefinition.setBeanClass(AspectJExpressionPointcut.class);
+		RootBeanDefinition beanDefinition = new RootBeanDefinition(AspectJExpressionPointcut.class);
 		beanDefinition.setSingleton(false);
 		beanDefinition.setSynthetic(true);
 		beanDefinition.getPropertyValues().addPropertyValue(EXPRESSION, expression);
