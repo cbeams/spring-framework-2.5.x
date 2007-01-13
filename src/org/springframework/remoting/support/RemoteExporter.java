18b14c253f3da020b857788a356863db00f6fa24
Juergen Hoeller <jhoeller@interface21.com>
2007-01-13 19:42:14 +0000

    explicitly use the service interface's ClassLoader for proxy generation
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12647 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/remoting/support/RemoteExporter.java
+++ src/org/springframework/remoting/support/RemoteExporter.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ public abstract class RemoteExporter {
 	 * Return the service to export.
 	 */
 	protected Object getService() {
-		return service;
+		return this.service;
 	}
 
 	/**
@@ -77,7 +77,7 @@ public abstract class RemoteExporter {
 	 * Return the interface of the service to export.
 	 */
 	protected Class getServiceInterface() {
-		return serviceInterface;
+		return this.serviceInterface;
 	}
 
 	/**
@@ -99,7 +99,7 @@ public abstract class RemoteExporter {
 	 * exported services.
 	 */
 	protected boolean isRegisterTraceInterceptor() {
-		return registerTraceInterceptor;
+		return this.registerTraceInterceptor;
 	}
 
 
@@ -151,7 +151,7 @@ public abstract class RemoteExporter {
 			proxyFactory.addAdvice(new RemoteInvocationTraceInterceptor(getExporterName()));
 		}
 		proxyFactory.setTarget(getService());
-		return proxyFactory.getProxy();
+		return proxyFactory.getProxy(getServiceInterface().getClassLoader());
 	}
 
 	/**
