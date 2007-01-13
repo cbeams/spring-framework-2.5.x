27c3f1161be02b0c8df56d97223eca8e81f3d1f6
Juergen Hoeller <jhoeller@interface21.com>
2007-01-13 22:54:16 +0000

    added overloaded ProxyFactory constructors for single-interceptor and TargetSource-only proxies; turned target null check into standard assertion
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12661 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/framework/ProxyFactory.java
+++ src/org/springframework/aop/framework/ProxyFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2006 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package org.springframework.aop.framework;
 import org.aopalliance.intercept.Interceptor;
 
 import org.springframework.aop.TargetSource;
+import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
 /**
@@ -28,6 +29,7 @@ import org.springframework.util.ClassUtils;
  *
  * @author Rod Johnson
  * @author Rob Harrop
+ * @author Juergen Hoeller
  * @since 14.03.2003
  */
 public class ProxyFactory extends AdvisedSupport implements AopProxy {
@@ -41,24 +43,44 @@ public class ProxyFactory extends AdvisedSupport implements AopProxy {
 	/**
 	 * Create a new ProxyFactory.
 	 * <p>Will proxy all interfaces of the given target.
-     * @param target the target object that is to be proxied
-     * @throws AopConfigException if the supplied target object is <code>null</code> 
+	 * @param target the target object that is to be proxied
 	 */
-	public ProxyFactory(Object target) throws AopConfigException {
-		if (target == null) {
-			throw new AopConfigException("Can't proxy null object");
-		}
+	public ProxyFactory(Object target) {
+		Assert.notNull(target, "Target object must not be null");
 		setInterfaces(ClassUtils.getAllInterfaces(target));
 		setTarget(target);
 	}
 
 	/**
 	 * Create a new ProxyFactory.
-	 * No target, only interfaces. Must add interceptors.
+	 * <p>No target, only interfaces. Must add interceptors.
 	 */
 	public ProxyFactory(Class[] interfaces) {
 		setInterfaces(interfaces);
 	}
+
+	/**
+	 * Create a new ProxyFactory for the given interface and interceptor.
+	 * <p>Convenience method for creating a proxy for a single interceptor,
+	 * assuming that the interceptor handles all calls itself rather than
+	 * delegating to a target, like in the case of remoting proxies.
+	 * @param proxyInterface the interface that the proxy should implement
+	 * @param interceptor the interceptor that the proxy should invoke
+	 */
+	public ProxyFactory(Class proxyInterface, Interceptor interceptor) {
+		addInterface(proxyInterface);
+		addAdvice(interceptor);
+	}
+
+	/**
+	 * Create a ProxyFactory for the specified <code>TargetSource</code>,
+	 * making the proxy implement the specified interface.
+	 */
+	public ProxyFactory(Class proxyInterface, TargetSource targetSource) {
+		addInterface(proxyInterface);
+		setTargetSource(targetSource);
+	}
+
 
 	/**
 	 * Create a new proxy according to the settings in this factory.
@@ -78,6 +100,7 @@ public class ProxyFactory extends AdvisedSupport implements AopProxy {
 	 * or removed interfaces. Can add and remove interceptors.
 	 * <p>Uses the given class loader (if necessary for proxy creation).
 	 * @param classLoader the class loader to create the proxy with
+	 * (or <code>null</code> for the low-level proxy facility's default)
 	 * @return the new proxy
 	 */
 	public Object getProxy(ClassLoader classLoader) {
@@ -93,28 +116,24 @@ public class ProxyFactory extends AdvisedSupport implements AopProxy {
 	 * @param proxyInterface the interface that the proxy should implement
 	 * @param interceptor the interceptor that the proxy should invoke
 	 * @return the new proxy
+	 * @see #ProxyFactory(Class, org.aopalliance.intercept.Interceptor)
 	 */
 	public static Object getProxy(Class proxyInterface, Interceptor interceptor) {
-		ProxyFactory proxyFactory = new ProxyFactory();
-		proxyFactory.addInterface(proxyInterface);
-		proxyFactory.addAdvice(interceptor);
-		return proxyFactory.getProxy();
+		return new ProxyFactory(proxyInterface, interceptor).getProxy();
 	}
 
 	/**
-	 * Create a proxy for the specified <code>TargetSource</code> that implements the
-	 * specified interface.
+	 * Create a proxy for the specified <code>TargetSource</code>,
+	 * implementing the specified interface.
+	 * @see #ProxyFactory(Class, org.springframework.aop.TargetSource)
 	 */
 	public static Object getProxy(Class proxyInterface, TargetSource targetSource) {
-		ProxyFactory proxyFactory = new ProxyFactory();
-		proxyFactory.addInterface(proxyInterface);
-		proxyFactory.setTargetSource(targetSource);
-		return proxyFactory.getProxy();
+		return new ProxyFactory(proxyInterface, targetSource).getProxy();
 	}
 
 	/**
-	 * Create a proxy for the specified <code>TargetSource</code> that extends the
-	 * target class of the <code>TargetSource</code>.
+	 * Create a proxy for the specified <code>TargetSource</code> that extends
+	 * the target class of the <code>TargetSource</code>.
 	 */
 	public static Object getProxy(TargetSource targetSource) {
 		if (targetSource.getTargetClass() == null) {
--- test/org/springframework/aop/framework/ProxyFactoryTests.java
+++ test/org/springframework/aop/framework/ProxyFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,16 +38,6 @@ import org.springframework.transaction.interceptor.TransactionInterceptor;
  */
 public class ProxyFactoryTests extends TestCase {
 
-	public void testNullTarget() {
-		try {
-			// Use the constructor taking Object
-			new ProxyFactory((Object) null);
-			fail("Should't allow proxy with null target");
-		} 
-		catch (AopConfigException ex) {
-		}
-	}
-	
 	public void testIndexOfMethods() {
 		TestBean target = new TestBean();
 		ProxyFactory pf = new ProxyFactory(target);
