0da8b1fe0f866816aad46080ecc5f4ee5d1a8826
Juergen Hoeller <jhoeller@interface21.com>
2007-01-14 13:56:05 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12673 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/request/RequestContextHolder.java
+++ src/org/springframework/web/context/request/RequestContextHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,7 +80,7 @@ public abstract class RequestContextHolder  {
 	/**
 	 * Return the RequestAttributes currently bound to the thread.
 	 * @return the RequestAttributes currently bound to the thread,
-	 * or <code>null</code>
+	 * or <code>null</code> if none bound
 	 */
 	public static RequestAttributes getRequestAttributes() {
 		RequestAttributes attributes = (RequestAttributes) requestAttributesHolder.get();
