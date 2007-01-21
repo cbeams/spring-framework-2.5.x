98b0c3284498eed832ca69fb502dbf7eaa0c29ad
Juergen Hoeller <jhoeller@interface21.com>
2007-01-21 17:01:41 +0000

    turned JRubyException into JRubyExecutionException, as inner class of JRubyScriptUtils
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12746 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/scripting/jruby/JRubyScriptUtils.java
+++ src/org/springframework/scripting/jruby/JRubyScriptUtils.java
@@ -16,7 +16,14 @@
 
 package org.springframework.scripting.jruby;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.List;
+
 import org.jruby.IRuby;
+import org.jruby.RubyException;
 import org.jruby.RubyNil;
 import org.jruby.ast.ClassNode;
 import org.jruby.ast.Colon2Node;
@@ -26,19 +33,15 @@ import org.jruby.exceptions.JumpException;
 import org.jruby.exceptions.RaiseException;
 import org.jruby.javasupport.JavaEmbedUtils;
 import org.jruby.runtime.builtin.IRubyObject;
+
 import org.springframework.aop.support.AopUtils;
+import org.springframework.core.NestedRuntimeException;
 import org.springframework.scripting.ScriptCompilationException;
 import org.springframework.util.ClassUtils;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * Utility methods for handling JRuby-scripted objects.
- * <p/>
+ *
  * <p>Note: As of Spring 2.0.2, this class requires JRuby 0.9.2 or higher.
  *
  * @author Rob Harrop
@@ -51,10 +54,8 @@ public abstract class JRubyScriptUtils {
 	/**
 	 * Create a new JRuby-scripted object from the given script source,
 	 * using the default {@link ClassLoader}.
-	 *
 	 * @param scriptSource the script source text
-	 * @param interfaces   the interfaces that the scripted Java object
-	 *                     is to implement
+	 * @param interfaces the interfaces that the scripted Java object is to implement
 	 * @return the scripted Java object
 	 * @throws JumpException in case of JRuby parsing failure
 	 * @see ClassUtils#getDefaultClassLoader()
@@ -65,11 +66,9 @@ public abstract class JRubyScriptUtils {
 
 	/**
 	 * Create a new JRuby-scripted object from the given script source.
-	 *
 	 * @param scriptSource the script source text
-	 * @param interfaces   the interfaces that the scripted Java object
-	 *                     is to implement
-	 * @param classLoader  the {@link ClassLoader} to create the script proxy with
+	 * @param interfaces the interfaces that the scripted Java object is to implement
+	 * @param classLoader the {@link ClassLoader} to create the script proxy with
 	 * @return the scripted Java object
 	 * @throws JumpException in case of JRuby parsing failure
 	 */
@@ -100,17 +99,15 @@ public abstract class JRubyScriptUtils {
 		return JavaEmbedUtils.initialize(Collections.EMPTY_LIST);
 	}
 
-
 	/**
 	 * Given the root {@link Node} in a JRuby AST will locate the name of the
 	 * class defined by that AST.
-	 *
 	 * @throws IllegalArgumentException if no class is defined by the supplied AST
 	 */
 	private static String findClassName(Node rootNode) {
 		ClassNode classNode = findClassNode(rootNode);
 		if (classNode == null) {
-			throw new IllegalArgumentException("Unable to determine class name for root node '" + rootNode + "'.");
+			throw new IllegalArgumentException("Unable to determine class name for root node '" + rootNode + "'");
 		}
 		Colon2Node node = (Colon2Node) classNode.getCPath();
 		return node.getName();
@@ -118,9 +115,8 @@ public abstract class JRubyScriptUtils {
 
 	/**
 	 * Find the first {@link ClassNode} under the supplied {@link Node}.
-	 *
 	 * @return the found <code>ClassNode</code>, or <code>null</code>
-	 *         if no {@link ClassNode} is found.
+	 * if no {@link ClassNode} is found
 	 */
 	private static ClassNode findClassNode(Node node) {
 		if (node instanceof ClassNode) {
@@ -159,27 +155,25 @@ public abstract class JRubyScriptUtils {
 
 		private final IRuby ruby;
 
-
 		public RubyObjectInvocationHandler(IRubyObject rubyObject, IRuby ruby) {
 			this.rubyObject = rubyObject;
 			this.ruby = ruby;
 		}
 
-
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 			if (AopUtils.isToStringMethod(method)) {
 				return "JRuby object [" + this.rubyObject + "]";
 			}
-
 			try {
 				IRubyObject[] rubyArgs = convertToRuby(args);
-				IRubyObject rubyResult = this.rubyObject.callMethod(this.ruby.getCurrentContext(), method.getName(), rubyArgs);
+				IRubyObject rubyResult =
+						this.rubyObject.callMethod(this.ruby.getCurrentContext(), method.getName(), rubyArgs);
 				return JavaEmbedUtils.rubyToJava(this.ruby, rubyResult, method.getReturnType());
-			} catch (RaiseException ex) {
-				throw new JRubyException(ex);
+			}
+			catch (RaiseException ex) {
+				throw new JRubyExecutionException(ex);
 			}
 		}
-
 
 		private IRubyObject[] convertToRuby(Object[] javaArgs) {
 			if (javaArgs == null || javaArgs.length == 0) {
@@ -193,5 +187,29 @@ public abstract class JRubyScriptUtils {
 		}
 	}
 
+
+	/**
+	 * Exception thrown in response to a JRuby {@link RaiseException}
+	 * being thrown from a JRuby method invocation.
+	 * <p>Introduced because the <code>RaiseException</code> class does not
+	 * have useful {@link Object#toString()}, {@link Throwable#getMessage()},
+	 * and {@link Throwable#printStackTrace} implementations.
+	 */
+	public static class JRubyExecutionException extends NestedRuntimeException {
+
+		/**
+		 * Create a new <code>JRubyException</code>,
+		 * wrapping the given JRuby <code>RaiseException</code>.
+		 * @param exception the cause (must not be <code>null</code>)
+		 */
+		public JRubyExecutionException(RaiseException exception) {
+			super(buildMessage(exception), exception);
+		}
+
+		private static String buildMessage(RaiseException exception) {
+			RubyException rubyEx = exception.getException();
+			return (rubyEx != null && rubyEx.message != null) ? rubyEx.message.toString() : "Unexpected JRuby error";
+		}
+	}
 
 }
