74a229e37f386dba2e9ac8b9fbc3fa04b1848580
Juergen Hoeller <jhoeller@interface21.com>
2007-01-22 22:53:50 +0000

    added warning regarding Quartz 1.6 trigger persistence
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@12760 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/scheduling/quartz/SimpleTriggerBean.java
+++ src/org/springframework/scheduling/quartz/SimpleTriggerBean.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,18 +28,23 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.Constants;
 
 /**
- * Convenience subclass of Quartz' SimpleTrigger class that eases
- * bean-style usage.
+ * Convenience subclass of Quartz's {@link org.quartz.SimpleTrigger}
+ * class, making bean-style usage easier.
  *
- * <p>SimpleTrigger itself is already a JavaBean but lacks sensible
- * defaults. This class uses the Spring bean name as job name, the
- * Quartz default group ("DEFAULT") as job group, the current time
- * as start time, and indefinite repetition, if not specified.
+ * <p>SimpleTrigger itself is already a JavaBean but lacks sensible defaults.
+ * This class uses the Spring bean name as job name, the Quartz default group
+ * ("DEFAULT") as job group, the current time as start time, and indefinite
+ * repetition, if not specified.
  *
- * <p>This class will also register the trigger with the job name
- * and group of a given JobDetail. This allows SchedulerFactoryBean
- * to automatically register a trigger for the respective JobDetail,
+ * <p>This class will also register the trigger with the job name and group of
+ * a given {@link org.quartz.JobDetail}. This allows {@link SchedulerFactoryBean}
+ * to automatically register a trigger for the corresponding JobDetail,
  * instead of registering the JobDetail separately.
+ *
+ * <p><b>NOTE:</b> This convenience subclass does not work with trigger
+ * persistence in Quartz 1.6, due to a change in Quartz's trigger handling.
+ * Use Quartz 1.5 if you rely on trigger persistence based on this class,
+ * or the standard Quartz {@link org.quartz.SimpleTrigger} class instead.
  *
  * @author Juergen Hoeller
  * @since 18.02.2004
@@ -51,11 +56,14 @@ import org.springframework.core.Constants;
  * @see #setJobDetail
  * @see SchedulerFactoryBean#setTriggers
  * @see SchedulerFactoryBean#setJobDetails
+ * @see CronTriggerBean
  */
 public class SimpleTriggerBean extends SimpleTrigger
     implements JobDetailAwareTrigger, BeanNameAware, InitializingBean {
 
+	/** Constants for the SimpleTrigger class */
 	private static final Constants constants = new Constants(SimpleTrigger.class);
+
 
 	private long startDelay = 0;
 
@@ -70,8 +78,8 @@ public class SimpleTriggerBean extends SimpleTrigger
 
 	/**
 	 * Set the misfire instruction via the name of the corresponding
-	 * constant in the SimpleTrigger class. Default is
-	 * MISFIRE_INSTRUCTION_SMART_POLICY.
+	 * constant in the {@link org.quartz.SimpleTrigger} class.
+	 * Default is <code>MISFIRE_INSTRUCTION_SMART_POLICY</code>.
 	 * @see org.quartz.SimpleTrigger#MISFIRE_INSTRUCTION_FIRE_NOW
 	 * @see org.quartz.SimpleTrigger#MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_EXISTING_COUNT
 	 * @see org.quartz.SimpleTrigger#MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_REMAINING_COUNT
@@ -124,7 +132,7 @@ public class SimpleTriggerBean extends SimpleTrigger
 	}
 
 	public JobDetail getJobDetail() {
-		return jobDetail;
+		return this.jobDetail;
 	}
 
 	public void setBeanName(String beanName) {
