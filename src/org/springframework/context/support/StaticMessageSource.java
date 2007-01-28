ff685363b06984b3523756e237a89741b468c6f3
Juergen Hoeller <jhoeller@interface21.com>
2007-01-28 20:03:42 +0000

    log registered messages at debug level instead of info level
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12809 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/context/support/StaticMessageSource.java
+++ src/org/springframework/context/support/StaticMessageSource.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,17 +22,18 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- * Simple implementation of MessageSource that allows messages
- * to be held in a Java object, and added programmatically.
- * This MessageSource supports internationalization.
+ * Simple implementation of {@link org.springframework.context.MessageSource}
+ * which allows messages to be registered programmatically.
+ * This MessageSource supports basic internationalization.
  *
- * <p>Intended for testing rather than use in production systems.
+ * <p>Intended for testing rather than for use in production systems.
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
  */
 public class StaticMessageSource extends AbstractMessageSource {
 
+	/** Map from 'code + locale' keys to message Strings */
 	private final Map messages = new HashMap();
 
 
@@ -48,8 +49,8 @@ public class StaticMessageSource extends AbstractMessageSource {
 	 */
 	public void addMessage(String code, Locale locale, String msg) {
 		this.messages.put(code + "_" + locale.toString(), createMessageFormat(msg, locale));
-		if (logger.isInfoEnabled()) {
-			logger.info("Added message [" + msg + "] for code [" + code + "] and Locale [" + locale + "]");
+		if (logger.isDebugEnabled()) {
+			logger.debug("Added message [" + msg + "] for code [" + code + "] and Locale [" + locale + "]");
 		}
 	}
 
