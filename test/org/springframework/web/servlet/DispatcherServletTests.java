07c8c2e21ece3f4846ed2cab20ae937c9322af0c
Juergen Hoeller <jhoeller@interface21.com>
2007-01-28 20:44:48 +0000

    added "destroy()" calls
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12812 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/web/servlet/DispatcherServletTests.java
+++ test/org/springframework/web/servlet/DispatcherServletTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,6 +111,9 @@ public class DispatcherServletTests extends TestCase {
 				(FrameworkServlet.SERVLET_CONTEXT_PREFIX + "complex").equals(complexDispatcherServlet.getServletContextAttributeName()));
 		assertTrue("Context not published",
 				getServletContext().getAttribute(FrameworkServlet.SERVLET_CONTEXT_PREFIX + "complex") == null);
+
+		simpleDispatcherServlet.destroy();
+		complexDispatcherServlet.destroy();
 	}
 
 	public void testInvalidRequest() throws Exception {
