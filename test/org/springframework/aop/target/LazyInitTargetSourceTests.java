a1be3f43cd3c3268426757d8a77d8fb3afb4bbf4
Juergen Hoeller <jhoeller@interface21.com>
2007-02-02 12:51:40 +0000

    re-added lazy-init FactoryBean test
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12873 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/target/LazyInitTargetSourceTests.java
+++ test/org/springframework/aop/target/LazyInitTargetSourceTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,17 @@
 
 package org.springframework.aop.target;
 
+import java.util.Set;
+
 import junit.framework.TestCase;
 
-import org.springframework.beans.TestBean;
 import org.springframework.beans.ITestBean;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
 /**
  * @author Juergen Hoeller
+ * @author Rob Harrop
  * @since 07.01.2005
  */
 public class LazyInitTargetSourceTests extends TestCase {
@@ -32,6 +34,7 @@ public class LazyInitTargetSourceTests extends TestCase {
 	public void testLazyInitSingletonTargetSource() {
 		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("lazyInitSingletonTests.xml", getClass()));
 		bf.preInstantiateSingletons();
+
 		ITestBean tb = (ITestBean) bf.getBean("proxy");
 		assertFalse(bf.containsSingleton("target"));
 		assertEquals(10, tb.getAge());
@@ -41,17 +44,34 @@ public class LazyInitTargetSourceTests extends TestCase {
 	public void testCustomLazyInitSingletonTargetSource() {
 		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("customLazyInitTarget.xml", getClass()));
 		bf.preInstantiateSingletons();
+
 		ITestBean tb = (ITestBean) bf.getBean("proxy");
 		assertFalse(bf.containsSingleton("target"));
 		assertEquals("Rob Harrop", tb.getName());
 		assertTrue(bf.containsSingleton("target"));
 	}
 
+	public void testLazyInitFactoryBeanTargetSource() {
+		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("lazyInitFactoryBean.xml", getClass()));
+		bf.preInstantiateSingletons();
+
+		Set set1 = (Set) bf.getBean("proxy1");
+		assertFalse(bf.containsSingleton("target1"));
+		assertTrue(set1.contains("10"));
+		assertTrue(bf.containsSingleton("target1"));
+
+		Set set2 = (Set) bf.getBean("proxy2");
+		assertFalse(bf.containsSingleton("target2"));
+		assertTrue(set2.contains("20"));
+		assertTrue(bf.containsSingleton("target2"));
+	}
+
 
 	public static class CustomLazyInitTargetSource extends LazyInitTargetSource {
 
 		protected void postProcessTargetObject(Object targetObject) {
-			((ITestBean)targetObject).setName("Rob Harrop");
+			((ITestBean) targetObject).setName("Rob Harrop");
 		}
 	}
+
 }
