e4fd637559d5e07d301b5a8ea047d2e28b53b636
Juergen Hoeller <jhoeller@interface21.com>
2007-02-03 00:55:25 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12898 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- mock/org/springframework/test/web/AbstractModelAndViewTests.java
+++ mock/org/springframework/test/web/AbstractModelAndViewTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,9 +29,9 @@ import junit.framework.TestCase;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Convenient base class for tests dealing with
- * Spring web MVC ModelAndView objects.
- * 
+ * Convenient base class for tests dealing with Spring web MVC
+ * {@link org.springframework.web.servlet.ModelAndView} objects.
+ *
  * @author Alef Arendsen
  * @author Bram Smeets
  * @since 2.0
