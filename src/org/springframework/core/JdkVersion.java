8610d89814b14f629633dbb28ffb66d85103d80f
Juergen Hoeller <jhoeller@interface21.com>
2007-02-06 15:55:29 +0000

    properly detect JDK 1.6/1.7 (Java 6/7) as higher than 1.5 (Java 5)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@12917 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/JdkVersion.java
+++ src/org/springframework/core/JdkVersion.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,39 +17,72 @@
 package org.springframework.core;
 
 /**
- * Helper class used to find the current Java/JDK version.
- * Usually we want to find if we're in a 1.4 or higher JVM.
- * (Spring does not support 1.2 JVMs.)
+ * Internal helper class used to find the current Java/JDK version.
+ *
+ * <p>Note that Spring does not support 1.2 JVMs.
+ *
  * @author Rod Johnson
  * @author Juergen Hoeller
  */
 public class JdkVersion {
-	
+
+	/**
+	 * Constants identifying the 1.3.x JVM (JDK 1.3).
+	 */
 	public static final int JAVA_13 = 0;
-	
+
+	/**
+	 * Constant identifying the 1.4.x JVM (J2SE 1.4).
+	 */
 	public static final int JAVA_14 = 1;
-	
+
+	/**
+	 * Constant identifying the 1.5 JVM (Java 5).
+	 */
 	public static final int JAVA_15 = 2;
 
-	private static String javaVersion;
+	/**
+	 * Constant identifying the 1.6 JVM (Java 6).
+	 */
+	public static final int JAVA_16 = 3;
 
-	private static int majorJavaVersion = JAVA_13;
-	
+	/**
+	 * Constant identifying the 1.7 JVM (Java 7).
+	 */
+	public static final int JAVA_17 = 4;
+
+
+	private static final String javaVersion;
+
+	private static final int majorJavaVersion;
+
 	static {
 		javaVersion = System.getProperty("java.version");
-		// should look like "1.4.1_02"
-		if (javaVersion.indexOf("1.4.") != -1) {
-			majorJavaVersion = JAVA_14;
+		// version String should look like "1.4.2_10"
+		if (javaVersion.indexOf("1.7.") != -1) {
+			majorJavaVersion = JAVA_17;
+		}
+		else if (javaVersion.indexOf("1.6.") != -1) {
+			majorJavaVersion = JAVA_16;
 		}
 		else if (javaVersion.indexOf("1.5.") != -1) {
 			majorJavaVersion = JAVA_15;
 		}
-		// else leave as 1.3 default
+		else if (javaVersion.indexOf("1.4.") != -1) {
+			majorJavaVersion = JAVA_14;
+		}
+		else {
+			// else leave 1.3 as default (it's either 1.3 or unknown)
+			majorJavaVersion = JAVA_13;
+		}
 	}
+
 
 	/**
 	 * Return the full Java version string, as returned by
 	 * <code>System.getProperty("java.version")</code>.
+	 * @return the full Java version string
+	 * @see System#getProperty(String)
 	 */
 	public static String getJavaVersion() {
 		return javaVersion;
@@ -62,6 +95,8 @@ public class JdkVersion {
 	 * @see #JAVA_13
 	 * @see #JAVA_14
 	 * @see #JAVA_15
+	 * @see #JAVA_16
+	 * @see #JAVA_17
 	 */
 	public static int getMajorJavaVersion() {
 		return majorJavaVersion;
