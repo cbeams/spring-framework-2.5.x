41795da89a6bc6fa40243ba04e61939fb124b6b2
Juergen Hoeller <jhoeller@interface21.com>
2007-02-06 18:18:56 +0000

    refactored SimpleAsyncTaskExecutor to allow for reuse of the new CustomizableThreadCreator class
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12923 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/task/SimpleAsyncTaskExecutor.java
+++ src/org/springframework/core/task/SimpleAsyncTaskExecutor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.io.Serializable;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.ConcurrencyThrottleSupport;
+import org.springframework.util.CustomizableThreadCreator;
 
 /**
  * TaskExecutor implementation that fires up a new Thread for each task,
@@ -41,31 +42,38 @@ import org.springframework.util.ConcurrencyThrottleSupport;
  * @see org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
  * @see org.springframework.scheduling.commonj.WorkManagerTaskExecutor
  */
-public class SimpleAsyncTaskExecutor extends ConcurrencyThrottleSupport
-		implements TaskExecutor, Serializable {
+public class SimpleAsyncTaskExecutor extends CustomizableThreadCreator implements TaskExecutor, Serializable {
 
 	/**
 	 * Default thread name prefix: "SimpleAsyncTaskExecutor-".
+	 * @deprecated as of Spring 2.0.3, since the default thread name prefix
+	 * is now taken from the concrete class (could be a subclass)
 	 */
 	public static final String DEFAULT_THREAD_NAME_PREFIX =
 			ClassUtils.getShortName(SimpleAsyncTaskExecutor.class) + "-";
 
+	/**
+	 * Permit any number of concurrent invocations: that is, don't throttle concurrency.
+	 */
+	public static final int UNBOUNDED_CONCURRENCY = ConcurrencyThrottleSupport.UNBOUNDED_CONCURRENCY;
 
-	private final Object monitor = new Object();
+	/**
+	 * Switch concurrency 'off': that is, don't allow any concurrent invocations.
+	 */
+	public static final int NO_CONCURRENCY = ConcurrencyThrottleSupport.NO_CONCURRENCY;
 
-	private String threadNamePrefix = DEFAULT_THREAD_NAME_PREFIX;
 
-	private int threadPriority = Thread.NORM_PRIORITY;
-
-	private boolean daemon = false;
-
-	private int threadCount = 0;
+	/**
+	 * Internal concurrency throttle used by this executor.
+	 */
+	private final ConcurrencyThrottleAdapter concurrencyThrottle = new ConcurrencyThrottleAdapter();
 
 
 	/**
 	 * Create a new SimpleAsyncTaskExecutor with default thread name prefix.
 	 */
 	public SimpleAsyncTaskExecutor() {
+		super();
 	}
 
 	/**
@@ -73,75 +81,56 @@ public class SimpleAsyncTaskExecutor extends ConcurrencyThrottleSupport
 	 * @param threadNamePrefix the prefix to use for the names of newly created threads
 	 */
 	public SimpleAsyncTaskExecutor(String threadNamePrefix) {
-		setThreadNamePrefix(threadNamePrefix);
+		super(threadNamePrefix);
 	}
 
 
 	/**
-	 * Specify the prefix to use for the names of newly created threads.
-	 * Default is "SimpleAsyncTaskExecutor-".
+	 * Set the maximum number of parallel accesses allowed.
+	 * -1 indicates no concurrency limit at all.
+	 * <p>In principle, this limit can be changed at runtime,
+	 * although it is generally designed as a config time setting.
+	 * NOTE: Do not switch between -1 and any concrete limit at runtime,
+	 * as this will lead to inconsistent concurrency counts: A limit
+	 * of -1 effectively turns off concurrency counting completely.
+	 * @see #UNBOUNDED_CONCURRENCY
 	 */
-	public void setThreadNamePrefix(String threadNamePrefix) {
-		this.threadNamePrefix = (threadNamePrefix != null ? threadNamePrefix : DEFAULT_THREAD_NAME_PREFIX);
+	public void setConcurrencyLimit(int concurrencyLimit) {
+		this.concurrencyThrottle.setConcurrencyLimit(concurrencyLimit);
 	}
 
 	/**
-	 * Return the thread name prefix to use for the names of newly
-	 * created threads.
+	 * Return the maximum number of parallel accesses allowed.
 	 */
-	protected String getThreadNamePrefix() {
-		return threadNamePrefix;
+	public int getConcurrencyLimit() {
+		return this.concurrencyThrottle.getConcurrencyLimit();
 	}
 
 	/**
-	 * Set the priority of the threads that this executor creates.
-	 * Default is 5.
-	 * @see java.lang.Thread#NORM_PRIORITY
+	 * Return whether this throttle is currently active.
+	 * @return <code>true</code> if the concurrency limit for this instance is active
+	 * @see #getConcurrencyLimit()
+	 * @see #setConcurrencyLimit
 	 */
-	public void setThreadPriority(int threadPriority) {
-		this.threadPriority = threadPriority;
-	}
-
-	/**
-	 * Return the priority of the threads that this executor creates.
-	 */
-	protected int getThreadPriority() {
-		return threadPriority;
-	}
-
-	/**
-	 * Set whether this executor should create daemon threads,
-	 * just executing as long as the application itself is running.
-	 * <p>Default is "false": Tasks passed to this executor should be either
-	 * short-lived or support explicit cancelling. Hence, if the application
-	 * shuts down, tasks will by default finish their execution. Specify
-	 * "true" for eager shutdown of threads that execute tasks.
-	 * @see java.lang.Thread#setDaemon
-	 */
-	public void setDaemon(boolean daemon) {
-		this.daemon = daemon;
-	}
-
-	/**
-	 * Return whether this executor should create daemon threads.
-	 */
-	protected boolean isDaemon() {
-		return daemon;
+	public boolean isThrottleActive() {
+		return this.concurrencyThrottle.isThrottleActive();
 	}
 
 
 	/**
 	 * Executes the given task, within a concurrency throttle
 	 * if configured (through the superclass's settings).
-	 * @throws NullPointerException if the supplied task is <code>null</code>
-	 * @see #beforeAccess()
 	 * @see #doExecute(Runnable)
-	 * @see #afterAccess()
 	 */
 	public final void execute(Runnable task) {
 		Assert.notNull(task, "Runnable must not be null");
-		beforeAccess();
-		doExecute(new ConcurrencyThrottlingRunnable(task));
+		if (isThrottleActive()) {
+			this.concurrencyThrottle.beforeAccess();
+			doExecute(new ConcurrencyThrottlingRunnable(task));
+		}
+		else {
+			doExecute(task);
+		}
 	}
 
 	/**
@@ -155,34 +144,21 @@ public class SimpleAsyncTaskExecutor extends ConcurrencyThrottleSupport
 		createThread(task).start();
 	}
 
-	/**
-	 * Template method for creation of a Thread.
-	 * <p>Default implementation creates a new Thread for the given
-	 * Runnable, applying an appropriate thread name.
-	 * @param task the Runnable to execute
-	 * @see #nextThreadName()
-	 */
-	protected Thread createThread(Runnable task) {
-		Thread thread = new Thread(task, nextThreadName());
-		thread.setPriority(getThreadPriority());
-		thread.setDaemon(isDaemon());
-		return thread;
-	}
 
 	/**
-	 * Return the thread name to use for a newly created thread.
-	 * <p>Default implementation returns the specified thread name prefix
-	 * with an increasing thread count appended: for example,
-	 * "SimpleAsyncTaskExecutor-0".
-	 * @see #getThreadNamePrefix()
+	 * Subclass of the general ConcurrencyThrottleSupport class,
+	 * making <code>beforeAccess()</code> and <code>afterAccess()</code>
+	 * visible to the surrounding class.
 	 */
-	protected String nextThreadName() {
-		int threadNumber = 0;
-		synchronized (this.monitor) {
-			this.threadCount++;
-			threadNumber = this.threadCount;
+	private static class ConcurrencyThrottleAdapter extends ConcurrencyThrottleSupport {
+
+		protected void beforeAccess() {
+			super.beforeAccess();
 		}
-		return getThreadNamePrefix() + threadNumber;
+
+		protected void afterAccess() {
+			super.afterAccess();
+		}
 	}
 
 
@@ -203,7 +179,7 @@ public class SimpleAsyncTaskExecutor extends ConcurrencyThrottleSupport
 				this.target.run();
 			}
 			finally {
-				afterAccess();
+				concurrencyThrottle.afterAccess();
 			}
 		}
 	}
