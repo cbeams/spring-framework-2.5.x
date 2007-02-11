120a26e8e9f6a8d61fd95451b1b91cacd5daaad8
Juergen Hoeller <jhoeller@interface21.com>
2007-02-11 23:03:25 +0000

    revised javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@12993 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/AbstractCachingViewResolver.java
+++ src/org/springframework/web/servlet/view/AbstractCachingViewResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,12 +25,13 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 
 /**
- * Convenient superclass for view resolvers.
- * Caches views once resolved: This means that view resolution won't be a
- * performance problem, no matter how costly initial view retrieval is.
+ * Convenient base class for {@link org.springframework.web.servlet.ViewResolver}
+ * implementations. Caches {@link org.springframework.web.servlet.View} objects
+ * once resolved: This means that view resolution won't be a performance problem,
+ * no matter how costly initial view retrieval is.
  *
- * <p>View retrieval is deferred to subclasses via the <code>loadView</code>
- * template method.
+ * <p>Subclasses need to implement the {@link #loadView} template method,
+ * building the View object for a specific view name and locale.
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
@@ -46,11 +47,10 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 
 
 	/**
-	 * Enable or disable caching. Disable this only for debugging and development.
-	 * Default is for caching to be enabled.
+	 * Enable or disable caching.
+	 * <p>Default is "true": caching is enabled.
+	 * Disable this only for debugging and development.
 	 * <p><b>Warning: Disabling caching can severely impact performance.</b>
-	 * Tests indicate that turning caching off reduces performance by at least 20%.
-	 * Increased object churn probably eventually makes the problem even worse.
 	 */
 	public void setCache(boolean cache) {
 		this.cache = cache;
@@ -71,7 +71,6 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 		}
 		else {
 			Object cacheKey = getCacheKey(viewName, locale);
-			// No synchronization, as we can live with occasional double caching.
 			synchronized (this.viewCache) {
 				View view = (View) this.viewCache.get(cacheKey);
 				if (view == null) {
@@ -89,9 +88,10 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 
 	/**
 	 * Return the cache key for the given view name and the given locale.
-	 * Default is a String consisting of view name and locale suffix.
-	 * <p>Needs to regard the locale in general, as a different locale can lead to a
-	 * different view! Can be overridden in subclasses.
+	 * <p>Default is a String consisting of view name and locale suffix.
+	 * Can be overridden in subclasses.
+	 * <p>Needs to respect the locale in general, as a different locale can
+	 * lead to a different view resource.
 	 */
 	protected Object getCacheKey(String viewName, Locale locale) {
 		return viewName + "_" + locale;
@@ -99,7 +99,7 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 
 	/**
 	 * Provides functionality to clear the cache for a certain view.
-	 * This can be handy in case developer are able to modify views
+	 * <p>This can be handy in case developer are able to modify views
 	 * (e.g. Velocity templates) at runtime after which you'd need to
 	 * clear the cache for the specified view.
 	 * @param viewName the view name for which the cached view object
@@ -144,8 +144,8 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 
 	/**
 	 * Create the actual View object.
-	 * Default implementation delegates to <code>loadView</code>.
-	 * <p>Can be overridden to resolve certain view names in a special fashion,
+	 * <p>The default implementation delegates to {@link #loadView}.
+	 * This can be overridden to resolve certain view names in a special fashion,
 	 * before delegating to the actual <code>loadView</code> implementation
 	 * provided by the subclass.
 	 * @param viewName the name of the view to retrieve
@@ -160,13 +160,11 @@ public abstract class AbstractCachingViewResolver extends WebApplicationObjectSu
 	}
 
 	/**
-	 * Subclasses must implement this method. There need be no concern
-	 * for efficiency, as this class will cache views.
-	 * <p>Not all subclasses may support internationalization:
-	 * A subclass that doesn't can simply ignore the locale parameter.
-	 * <p><b>NOTE:</b> As of Spring 1.2, subclasses are supposed to
-	 * fully initialize the View objects before returning them,
-	 * also applying bean container initialization callbacks.
+	 * Subclasses must implement this method, building a View object
+	 * for the specified view. The returned View objects will be
+	 * cached by this ViewResolver base class.
+	 * <p>Subclasses are not forced to support internationalization:
+	 * A subclass that doesn't may simply ignore the locale parameter.
 	 * @param viewName the name of the view to retrieve
 	 * @param locale the Locale to retrieve the view for
 	 * @return the View instance, or <code>null</code> if not found
