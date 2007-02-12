16596e449362615fed0de2886bbabde381c4af21
Juergen Hoeller <jhoeller@interface21.com>
2007-02-12 21:55:57 +0000

    "hashCode" implementation returns a constant value even if the target object changes
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-1-2@12999 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/aop/target/HotSwappableTargetSource.java
+++ src/org/springframework/aop/target/HotSwappableTargetSource.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,32 +22,44 @@ import org.springframework.aop.TargetSource;
 import org.springframework.util.Assert;
 
 /**
- * Implementation of TargetSource interface that caches a local target object,
- * but allows the target to be swapped while the application is running.
+ * {@link org.springframework.aop.TargetSource} implementation that
+ * caches a local target object, but allows the target to be swapped
+ * while the application is running.
  *
  * <p>If configuring an object of this class in a Spring IoC container,
  * use constructor injection.
- * 
+ *
  * <p>This TargetSource is serializable if the target is at the time
  * of serialization.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  */
 public class HotSwappableTargetSource implements TargetSource, Serializable {
 
-	/** Target cached and invoked using reflection */
+	/** use serialVersionUID from Spring 1.2 for interoperability */
+	private static final long serialVersionUID = 7497929212653839187L;
+
+
+	/** The current target object */
 	private Object target;
+
 
 	/**
 	 * Create a new HotSwappableTargetSource with the given initial target object.
 	 * @param initialTarget the initial target object
 	 */
 	public HotSwappableTargetSource(Object initialTarget) {
-		Assert.notNull(initialTarget, "initialTarget is required");
+		Assert.notNull(initialTarget, "Target object must not be null");
 		this.target = initialTarget;
 	}
 
-	public Class getTargetClass() {
+
+	/**
+	 * Return the type of the current target object.
+	 * <p>The returned type should usually be constant across all target objects.
+	 */
+	public synchronized Class getTargetClass() {
 		return this.target.getClass();
 	}
 
@@ -55,50 +67,44 @@ public class HotSwappableTargetSource implements TargetSource, Serializable {
 		return false;
 	}
 
-	/**
-	 * Synchronization around something that takes so little time is fine.
-	 */
 	public synchronized Object getTarget() {
 		return this.target;
 	}
 
-	public void releaseTarget(Object o) {
-		// No implementation needed
+	public void releaseTarget(Object target) {
+		// nothing to do
 	}
 
+
 	/**
-	 * Swap the target, returning the old target.
-	 * @param newTarget new target
-	 * @return the old target
+	 * Swap the target, returning the old target object.
+	 * @param newTarget the new target object
+	 * @return the old target object
 	 * @throws IllegalArgumentException if the new target is invalid
 	 */
 	public synchronized Object swap(Object newTarget) throws IllegalArgumentException {
-		if (newTarget == null) {
-			throw new IllegalArgumentException("Cannot swap to null");
-		}
-		// TODO type checks
+		Assert.notNull(newTarget, "Target object must not be null");
 		Object old = this.target;
 		this.target = newTarget;
 		return old;
 	}
 
-	public String toString() {
-		return "HotSwappableTargetSource for target: " + this.target;
-	}
 
 	/**
-	 * Two HotSwappableTargetSources are equal if the targets are equal.
+	 * Two HotSwappableTargetSources are equal if the current target
+	 * objects are equal.
 	 */
 	public boolean equals(Object other) {
-		if (!(other instanceof HotSwappableTargetSource)) {
-			return false;
-		}
-		HotSwappableTargetSource otherTargetSource = (HotSwappableTargetSource) other;
-		return this.target.equals(otherTargetSource.target);
+		return (this == other || (other instanceof HotSwappableTargetSource &&
+				this.target.equals(((HotSwappableTargetSource) other).target)));
 	}
 
 	public int hashCode() {
-		return this.target.hashCode();
+		return HotSwappableTargetSource.class.hashCode();
+	}
+
+	public String toString() {
+		return "HotSwappableTargetSource for target: " + this.target;
 	}
 
 }
