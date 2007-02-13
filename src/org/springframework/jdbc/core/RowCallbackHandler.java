aeb2fefd193869fe1493f9c27499c356049768b6
Juergen Hoeller <jhoeller@interface21.com>
2007-02-13 16:33:08 +0000

    revised javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13013 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/core/RowCallbackHandler.java
+++ src/org/springframework/jdbc/core/RowCallbackHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,30 +20,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * Callback interface used by JdbcTemplate's query methods.
- * Implementations of this interface perform the actual work of extracting
- * results, but don't need to worry about exception handling. SQLExceptions
- * will be caught and handled correctly by the JdbcTemplate class.
+ * An interface used by {@link JdbcTemplate} for processing rows of a
+ * {@link java.sql.ResultSet} on a per-row basis. Implementations of
+ * this interface perform the actual work of processing each row
+ * but don't need to worry about exception handling.
+ * {@link java.sql.SQLException SQLExceptions} will be caught and handled
+ * by the calling JdbcTemplate.
  *
- * <p>In contrast to a ResultSetExtractor, a RowCallbackHandler object is
- * typically stateful: It keeps the result state within the object, to be
- * available for later inspection. See RowCountCallbackHandler's javadoc
- * for a usage example with JdbcTemplate.
+ * <p>In contrast to a {@link ResultSetExtractor}, a RowCallbackHandler
+ * object is typically stateful: It keeps the result state within the
+ * object, to be available for later inspection. See
+ * {@link RowCountCallbackHandler} for a usage example.
+ *
+ * <p>Consider using a {@link RowMapper} instead if you need to map
+ * exactly one result object per row, assembling them into a List.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
+ * @see JdbcTemplate
+ * @see RowMapper
  * @see ResultSetExtractor
  * @see RowCountCallbackHandler
- * @see RowMapper
  */
 public interface RowCallbackHandler {
 
 	/**
 	 * Implementations must implement this method to process each row of data
-	 * in the ResultSet. This method should not call next() on the ResultSet,
-	 * but extract the current values. Exactly what the implementation chooses
-	 * to do is up to it; a trivial implementation might simply count rows,
-	 * while another implementation might build an XML document.
-	 * @param rs the ResultSet to process
+	 * in the ResultSet. This method should not call <code>next()</code> on
+	 * the ResultSet; it is only supposed to extract values of the current row.
+	 * <p>Exactly what the implementation chooses to do is up to it:
+	 * A trivial implementation might simply count rows, while another
+	 * implementation might build an XML document.
+	 * @param rs the ResultSet to process (pre-initialized for the current row)
 	 * @throws SQLException if a SQLException is encountered getting
 	 * column values (that is, there's no need to catch SQLException)
 	 */
