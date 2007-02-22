47832c47a08acfd26a37bffa4c06e5a4000d9c91
Juergen Hoeller <jhoeller@interface21.com>
2007-02-22 23:33:48 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13079 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/aspectj/DeclareParentsTests.java
+++ test/org/springframework/aop/aspectj/DeclareParentsTests.java
@@ -27,26 +27,25 @@ public class DeclareParentsTests extends AbstractAdviceBindingTests {
 	protected String getConfigPath() {
 		return "declare-parents-tests.xml";
 	}
-	
+
 	public void testIntroductionWasMade() {
 		assertTrue("Introduction must have been made", testBeanProxy instanceof Lockable);
 	}
-	
+
 	// TODO if you change type pattern from org.springframework.beans..*
 	// to org.springframework..* it also matches introduction.
 	// Perhaps generated advisor bean definition could be made to depend
-	// on the introduction, in which case this would not be a problem
-	
+	// on the introduction, in which case this would not be a problem.
 	public void testLockingWorks() {
 		Object introductionObject = applicationContext.getBean("introduction");
 		assertFalse("Introduction should not be proxied", AopUtils.isAopProxy(introductionObject));
-		
+
 		Lockable lockable = (Lockable) testBeanProxy;
 		assertFalse(lockable.locked());
-		
+
 		// Invoke a non-advised method
 		testBeanProxy.getAge();
-		
+
 		testBeanProxy.setName("");
 		lockable.lock();
 		try {
@@ -54,7 +53,7 @@ public class DeclareParentsTests extends AbstractAdviceBindingTests {
 			fail("Should be locked");
 		}
 		catch (IllegalStateException ex) {
-			
+			// expected
 		}
 	}
 
