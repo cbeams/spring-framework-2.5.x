cbfba71cc7f16fe06838c803caf6c9c50000779b
Juergen Hoeller <jhoeller@interface21.com>
2007-02-28 20:33:41 +0000

    added javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13142 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/jmx/export/annotation/ManagedNotification.java
+++ tiger/src/org/springframework/jmx/export/annotation/ManagedNotification.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,20 @@
 
 package org.springframework.jmx.export.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
 
 /**
+ * JDK 1.5+ method-level annotation that indicates a JMX notification
+ * emitted by a bean.
+ *
  * @author Rob Harrop
+ * @since 2.0
+ * @see org.springframework.jmx.export.metadata.ManagedNotification
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
--- tiger/src/org/springframework/jmx/export/annotation/ManagedNotifications.java
+++ tiger/src/org/springframework/jmx/export/annotation/ManagedNotifications.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,15 +16,19 @@
 
 package org.springframework.jmx.export.annotation;
 
-import java.lang.annotation.Target;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Documented;
+import java.lang.annotation.Target;
 
 /**
+ * JDK 1.5+ method-level annotation that indicates JMX notifications emitted by
+ * a bean, containing multiple {@link ManagedNotification ManagedNotifications}
+ *
  * @author Rob Harrop
+ * @since 2.0
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -33,4 +37,5 @@ import java.lang.annotation.Documented;
 public @interface ManagedNotifications {
 
 	ManagedNotification[] value();
+
 }
