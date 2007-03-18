95659480b41c401f34a78165b0d69fbb669b134a
Juergen Hoeller <jhoeller@interface21.com>
2007-03-18 17:26:44 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13325 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/LifecycleBean.java
+++ test/org/springframework/beans/factory/LifecycleBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,6 @@ public class LifecycleBean implements BeanNameAware, BeanFactoryAware, Initializ
 	}
 	
 	public void declaredInitMethod() {
-		
 		if (!this.inited) {
 			throw new RuntimeException("Factory didn't call afterPropertiesSet before declared init method");
 		}		
