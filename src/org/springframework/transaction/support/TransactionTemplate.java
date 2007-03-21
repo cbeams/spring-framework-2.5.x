352f7b548c76abe7acf14b518952405f74e93e8b
Juergen Hoeller <jhoeller@interface21.com>
2007-03-21 21:18:01 +0000

    factored out TransactionOperations interface from TransactionTemplate, defining the central "execute" operation
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13330 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/support/TransactionTemplate.java
+++ src/org/springframework/transaction/support/TransactionTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,14 +29,15 @@ import org.springframework.transaction.TransactionStatus;
  * Template class that simplifies programmatic transaction demarcation and
  * transaction exception handling.
  *
- * <p>The central method is "execute", supporting transactional code implementing
- * the TransactionCallback interface. It handles the transaction lifecycle and
- * possible exceptions such that neither the TransactionCallback implementation
- * nor the calling code needs to explicitly handle transactions.
+ * <p>The central method is {@link #execute}, supporting transactional code that
+ * implements the {@link TransactionCallback} interface. This template handles
+ * the transaction lifecycle and possible exceptions such that neither the
+ * TransactionCallback implementation nor the calling code needs to explicitly
+ * handle transactions.
  *
  * <p>Typical usage: Allows for writing low-level data access objects that use
- * (JNDI) resources but are not transaction-aware themselves. Instead, they
- * can implicitly participate in (JTA) transactions handled by higher-level
+ * resources such as JDBC DataSources but are not transaction-aware themselves.
+ * Instead, they can implicitly participate in transactions handled by higher-level
  * application services utilizing this class, making calls to the low-level
  * services via an inner-class callback object.
  *
@@ -52,45 +53,47 @@ import org.springframework.transaction.TransactionStatus;
  * @author Juergen Hoeller
  * @since 17.03.2003
  * @see #execute
- * @see org.springframework.transaction.support.TransactionCallback
+ * @see #setTransactionManager
  * @see org.springframework.transaction.PlatformTransactionManager
- * @see org.springframework.transaction.jta.JtaTransactionManager
  */
-public class TransactionTemplate extends DefaultTransactionDefinition implements InitializingBean {
+public class TransactionTemplate extends DefaultTransactionDefinition
+		implements TransactionOperations, InitializingBean {
 
 	/** Logger available to subclasses */
 	protected final Log logger = LogFactory.getLog(getClass());
 
-	private PlatformTransactionManager transactionManager = null;
+	private PlatformTransactionManager transactionManager;
 
 
 	/**
-	 * Create a new TransactionTemplate instance.
-	 * Mainly targetted at configuration by a bean factory.
-	 * Note: Transaction manager property needs to be set before any execute calls.
+	 * Construct a new TransactionTemplate for bean usage.
+	 * <p>Note: The PlatformTransactionManager needs to be set before
+	 * any <code>execute</code> calls.
 	 * @see #setTransactionManager
 	 */
 	public TransactionTemplate() {
 	}
 
 	/**
-	 * Create a new TransactionTemplate instance.
-	 * @param transactionManager transaction management strategy to be used
+	 * Construct a new TransactionTemplate using the given transaction manager.
+	 * @param transactionManager the transaction management strategy to be used
 	 */
 	public TransactionTemplate(PlatformTransactionManager transactionManager) {
 		this.transactionManager = transactionManager;
 	}
-	
+
 	/**
-	 * Create a new TransactionTemplate instance.
-	 * @param transactionManager transaction management strategy to be used
-	 * @param transactionDefinition transaction definition to copy from.
-	 * Properties can still be set to change values
+	 * Construct a new TransactionTemplate using the given transaction manager,
+	 * taking its default settings from the given transaction definition.
+	 * @param transactionManager the transaction management strategy to be used
+	 * @param transactionDefinition the transaction definition to copy the
+	 * default settings from. Local properties can still be set to change values.
 	 */
 	public TransactionTemplate(PlatformTransactionManager transactionManager, TransactionDefinition transactionDefinition) {
 		super(transactionDefinition);
 		this.transactionManager = transactionManager;
 	}
+
 
 	/**
 	 * Set the transaction management strategy to be used.
@@ -103,26 +106,16 @@ public class TransactionTemplate extends DefaultTransactionDefinition implements
 	 * Return the transaction management strategy to be used.
 	 */
 	public PlatformTransactionManager getTransactionManager() {
-		return transactionManager;
+		return this.transactionManager;
 	}
 
 	public void afterPropertiesSet() {
 		if (this.transactionManager == null) {
-			throw new IllegalArgumentException("transactionManager is required");
+			throw new IllegalArgumentException("Property 'transactionManager' is required");
 		}
 	}
 
 
-	/**
-	 * Execute the action specified by the given callback object within a transaction.
-	 * <p>Allows for returning a result object created within the transaction, that is,
-	 * a domain object or a collection of domain objects. A RuntimeException thrown
-	 * by the callback is treated as a fatal exception that enforces a rollback.
-	 * Such an exception gets propagated to the caller of the template.
-	 * @param action the callback object that specifies the transactional action
-	 * @return a result object returned by the callback, or <code>null</code> if none
-	 * @throws TransactionException in case of initialization, rollback, or system errors
-	 */
 	public Object execute(TransactionCallback action) throws TransactionException {
 		if (this.transactionManager instanceof CallbackPreferringPlatformTransactionManager) {
 			return ((CallbackPreferringPlatformTransactionManager) this.transactionManager).execute(this, action);
