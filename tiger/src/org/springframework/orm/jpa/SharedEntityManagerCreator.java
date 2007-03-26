ee1a2794d691e7a3ab18dc38a1882510f8483524
Juergen Hoeller <jhoeller@interface21.com>
2007-03-26 21:32:17 +0000

    SharedEntityManagerCreator's proxies deliver a "toString()" result without touching a target EntityManager
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13355 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/orm/jpa/SharedEntityManagerCreator.java
+++ tiger/src/org/springframework/orm/jpa/SharedEntityManagerCreator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -136,8 +136,12 @@ public abstract class SharedEntityManagerCreator {
 				return (proxy == args[0]);
 			}
 			else if (method.getName().equals("hashCode")) {
-				// Use hashCode of SessionFactory proxy.
+				// Use hashCode of EntityManager proxy.
 				return hashCode();
+			}
+			else if (method.getName().equals("toString")) {
+				// Deliver toString without touching a target EntityManager.
+				return "Shared EntityManager proxy for target factory [" + this.targetFactory + "]";
 			}
 			else if (method.getName().equals("isOpen")) {
 				// Handle isOpen method: always return true.
