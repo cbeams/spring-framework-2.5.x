0073f3c969d738d72891b7582a687ca032b52490
Juergen Hoeller <jhoeller@interface21.com>
2007-03-31 10:11:43 +0000

    use a shared PropertiesEditor instance
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13414 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/PropertyValuesEditor.java
+++ src/org/springframework/beans/PropertyValuesEditor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,32 +19,28 @@ package org.springframework.beans;
 import java.beans.PropertyEditorSupport;
 import java.util.Properties;
 
-import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.propertyeditors.PropertiesEditor;
 
 /**
- * Editor for PropertyValues objects. Not
- * a GUI editor.
- * <br>NB: this editor must be registered with the JavaBeans API before it
- * will be available. Editors in this package are
- * registered by BeanWrapperImpl.
- * <br>The required format is defined in java.util.Properties documentation.
- * Each property must be on a new line.
- * <br>
- * The present implementation relies on a PropertiesEditor.
+ * {@link java.beans.PropertyEditor Editor} for a {@link PropertyValues} object.
+ *
+ * <p>The required format is defined in the {@link java.util.Properties}
+ * documentation. Each property must be on a new line.
+ *
+ * <p>The present implementation relies on a
+ * {@link org.springframework.beans.propertyeditors.PropertiesEditor}
+ * underneath.
  *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  */
 public class PropertyValuesEditor extends PropertyEditorSupport {
-	
-	
-	/**
-	 * @see java.beans.PropertyEditor#setAsText(java.lang.String)
-	 */
-	public void setAsText(String s) throws IllegalArgumentException {
-		PropertiesEditor pe = new PropertiesEditor();
-		pe.setAsText(s);
-		Properties props = (Properties) pe.getValue();
+
+	private final PropertiesEditor propertiesEditor = new PropertiesEditor();
+
+	public void setAsText(String text) throws IllegalArgumentException {
+		this.propertiesEditor.setAsText(text);
+		Properties props = (Properties) this.propertiesEditor.getValue();
 		setValue(new MutablePropertyValues(props));
 	}
 
