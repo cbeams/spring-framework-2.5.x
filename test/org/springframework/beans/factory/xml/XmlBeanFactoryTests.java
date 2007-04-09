c198ca29eceaa0cd42424e859ff4cf48666e16b6
Juergen Hoeller <jhoeller@interface21.com>
2007-04-09 18:15:38 +0000

    strip repeated factory prefixes if found (for compatibility with previous Spring versions)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13492 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/xml/XmlBeanFactoryTests.java
+++ test/org/springframework/beans/factory/xml/XmlBeanFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -494,6 +494,11 @@ public class XmlBeanFactoryTests extends TestCase {
 		TestBean tb = (TestBean) xbf.getBean("singletonFactory");
 		DummyFactory db = (DummyFactory) xbf.getBean("&singletonFactory");
 		assertTrue(tb == db.getOtherTestBean());
+	}
+
+	public void testFactoryReferenceWithDoublePrefix() {
+		XmlBeanFactory xbf = new XmlBeanFactory(new ClassPathResource("factoryCircle.xml", getClass()));
+		DummyFactory db = (DummyFactory) xbf.getBean("&&singletonFactory");
 	}
 
 	public void testComplexFactoryReferenceCircle() {
