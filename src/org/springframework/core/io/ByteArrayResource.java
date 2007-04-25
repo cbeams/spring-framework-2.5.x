dc0cb1bbe0cba69b5d90abfcbe16a2d1470320be
Juergen Hoeller <jhoeller@interface21.com>
2007-04-25 19:06:07 +0000

    fixed "hashCode()" javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13592 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/io/ByteArrayResource.java
+++ src/org/springframework/core/io/ByteArrayResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,8 @@ public class ByteArrayResource extends AbstractResource {
 	}
 
 	/**
-	 * This implementation returns the hash code of the underlying byte array.
+	 * This implementation returns the hash code based on the
+	 * underlying byte array.
 	 */
 	public int hashCode() {
 		return (byte[].class.hashCode() * 29 * this.byteArray.length);
