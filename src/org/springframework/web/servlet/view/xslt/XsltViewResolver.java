9ada2ff783df5eb6757be4f998b24400f44eb6f8
Juergen Hoeller <jhoeller@interface21.com>
2007-04-27 13:58:24 +0000

    properly apply the "cacheTemplates" flag to created XsltView instances
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13608 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/xslt/XsltViewResolver.java
+++ src/org/springframework/web/servlet/view/xslt/XsltViewResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,83 +16,106 @@
 
 package org.springframework.web.servlet.view.xslt;
 
-import org.springframework.util.Assert;
-import org.springframework.util.xml.SimpleTransformErrorListener;
-import org.springframework.web.servlet.view.AbstractUrlBasedView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import java.util.Properties;
 
 import javax.xml.transform.ErrorListener;
 import javax.xml.transform.URIResolver;
-import java.util.Properties;
+
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 /**
  * {@link org.springframework.web.servlet.ViewResolver} implementation that
- * resolves instances of {@link XsltView} by translating the supplied view name into
- * the URL of the XSLT stylesheet.
+ * resolves instances of {@link XsltView} by translating the supplied view name
+ * into the URL of the XSLT stylesheet.
  *
  * @author Rob Harrop
+ * @author Juergen Hoeller
  * @since 2.0
  */
 public class XsltViewResolver extends UrlBasedViewResolver {
 
-	private ErrorListener errorListener = new SimpleTransformErrorListener(logger);
+	private String sourceKey;
 
 	private URIResolver uriResolver;
 
-	private boolean cacheTemplates = true;
+	private ErrorListener errorListener;
 
-	private String sourceKey;
+	private boolean indent = true;
 
 	private Properties outputProperties;
 
-	private boolean indent = true;
+	private boolean cacheTemplates = true;
+
 
 	public XsltViewResolver() {
 		setViewClass(XsltView.class);
 	}
 
-	/**
-	 * @see XsltView#setCacheTemplates
-	 */
-	public void setCacheTemplates(boolean cacheTemplates) {
-		this.cacheTemplates = cacheTemplates;
-	}
 
 	/**
-	 * @see XsltView#setUriResolver
-	 */
-	public void setUriResolver(URIResolver uriResolver) {
-		this.uriResolver = uriResolver;
-	}
-
-	/**
-	 * @see XsltView#setErrorListener
-	 */
-	public void setErrorListener(ErrorListener errorListener) {
-		Assert.notNull(errorListener, "'errorListener' cannot be null.");
-		this.errorListener = errorListener;
-	}
-
-	/**
-	 * @see XsltView#setSourceKey
+	 * Set the name of the model attribute that represents the XSLT Source.
+	 * If not specified, the model map will be searched for a matching value type.
+	 * <p>The following source types are supported out of the box:
+	 * {@link javax.xml.transform.Source}, {@link org.w3c.dom.Document},
+	 * {@link org.w3c.dom.Node}, {@link java.io.Reader}, {@link java.io.InputStream}
+	 * and {@link org.springframework.core.io.Resource}.
 	 */
 	public void setSourceKey(String sourceKey) {
 		this.sourceKey = sourceKey;
 	}
 
 	/**
-	 * @see XsltView#setOutputProperties
+	 * Set the URIResolver used in the transform.
+	 * <p>The URIResolver handles calls to the XSLT <code>document()</code> function.
+	 */
+	public void setUriResolver(URIResolver uriResolver) {
+		this.uriResolver = uriResolver;
+	}
+
+	/**
+	 * Set an implementation of the {@link javax.xml.transform.ErrorListener}
+	 * interface for custom handling of transformation errors and warnings.
+	 * <p>If not set, a default
+	 * {@link org.springframework.util.xml.SimpleTransformErrorListener} is
+	 * used that simply logs warnings using the logger instance of the view class,
+	 * and rethrows errors to discontinue the XML transformation.
+	 * @see org.springframework.util.xml.SimpleTransformErrorListener
+	 */
+	public void setErrorListener(ErrorListener errorListener) {
+		this.errorListener = errorListener;
+	}
+
+	/**
+	 * Set whether the XSLT transformer may add additional whitespace when
+	 * outputting the result tree.
+	 * <p>Default is <code>true</code> (on); set this to <code>false</code> (off)
+	 * to not specify an "indent" key, leaving the choice up to the stylesheet.
+	 * @see javax.xml.transform.OutputKeys#INDENT
+	 */
+	public void setIndent(boolean indent) {
+		this.indent = indent;
+	}
+
+	/**
+	 * Set arbitrary transformer output properties to be applied to the stylesheet.
+	 * <p>Any values specified here will override defaults that this view sets
+	 * programmatically.
+	 * @see javax.xml.transform.Transformer#setOutputProperty
 	 */
 	public void setOutputProperties(Properties outputProperties) {
 		this.outputProperties = outputProperties;
 	}
 
 	/**
-	 * @see XsltView#setIndent(boolean)
+	 * Turn on/off the caching of the XSLT templates.
+	 * <p>The default value is "true". Only set this to "false" in development,
+	 * where caching does not seriously impact performance.
 	 */
-	public void setIndent(boolean indent) {
-		this.indent = indent;
+	public void setCacheTemplates(boolean cacheTemplates) {
+		this.cacheTemplates = cacheTemplates;
 	}
+
 
 	protected Class requiredViewClass() {
 		return XsltView.class;
@@ -100,11 +123,17 @@ public class XsltViewResolver extends UrlBasedViewResolver {
 
 	protected AbstractUrlBasedView buildView(String viewName) throws Exception {
 		XsltView view = (XsltView) super.buildView(viewName);
-		view.setUriResolver(this.uriResolver);
-		view.setErrorListener(this.errorListener);
 		view.setSourceKey(this.sourceKey);
-		view.setOutputProperties(this.outputProperties);
+		if (this.uriResolver != null) {
+			view.setUriResolver(this.uriResolver);
+		}
+		if (this.errorListener != null) {
+			view.setErrorListener(this.errorListener);
+		}
 		view.setIndent(this.indent);
+		view.setOutputProperties(this.outputProperties);
+		view.setCacheTemplates(this.cacheTemplates);
 		return view;
 	}
+
 }
