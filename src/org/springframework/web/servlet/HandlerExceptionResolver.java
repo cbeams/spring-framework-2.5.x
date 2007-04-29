f421201e62247d9f51223ca74c75e23b738ddc7a
Juergen Hoeller <jhoeller@interface21.com>
2007-04-29 15:56:00 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13618 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/HandlerExceptionResolver.java
+++ src/org/springframework/web/servlet/HandlerExceptionResolver.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,8 +38,8 @@ public interface HandlerExceptionResolver {
 	 * returning a ModelAndView that represents a specific error page if appropriate.
 	 * @param request current HTTP request
 	 * @param response current HTTP response
-	 * @param handler the executed handler, or <code>null</code> if none chosen at the time of
-	 * the exception (for example, if multipart resolution failed)
+	 * @param handler the executed handler, or <code>null</code> if none chosen at the
+	 * time of the exception (for example, if multipart resolution failed)
 	 * @param ex the exception that got thrown during handler execution
 	 * @return a corresponding ModelAndView to forward to, or <code>null</code> for default processing
 	 */
