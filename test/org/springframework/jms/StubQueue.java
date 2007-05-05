47c75f548f030dbe7eb5602fcca51bbef64bdfd7
Juergen Hoeller <jhoeller@interface21.com>
2007-05-05 09:26:46 +0000

    reformatted
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13672 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jms/StubQueue.java
+++ test/org/springframework/jms/StubQueue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,22 +25,22 @@ import javax.jms.Queue;
  */
 public class StubQueue implements Queue {
 
-    public static final String DEFAULT_QUEUE_NAME = "banjo";
+	public static final String DEFAULT_QUEUE_NAME = "banjo";
 
 
-    private String queueName = DEFAULT_QUEUE_NAME;
+	private String queueName = DEFAULT_QUEUE_NAME;
 
 
-    public StubQueue() {
-    }
+	public StubQueue() {
+	}
 
-    public StubQueue(String queueName) {
-        this.queueName = queueName;
-    }
+	public StubQueue(String queueName) {
+		this.queueName = queueName;
+	}
 
 
-    public String getQueueName() {
-        return this.queueName;
-    }
+	public String getQueueName() {
+		return this.queueName;
+	}
 
 }
