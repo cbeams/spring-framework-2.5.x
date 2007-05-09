5a10e6e08243836056b38a246f26387f9bf32348
Juergen Hoeller <jhoeller@interface21.com>
2007-05-09 14:14:32 +0000

    revised javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13737 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/stereotype/Component.java
+++ tiger/src/org/springframework/stereotype/Component.java
@@ -24,13 +24,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that an annotated class is a "Component".
- * 
- * <p>Instances of such classes may be considered as autowire candidates 
+ * Indicates that an annotated class is a "component".
+ * Such classes are considered as candidates for autodetected Spring beans
  * when using annotation-based configuration and classpath scanning.
- * 
+ *
+ * <p>Other class-level annotations may be considered as identifying
+ * a component as well, typically a special kind of component:
+ * e.g. the {@link Repository} annotation or AspectJ's
+ * {@link org.aspectj.lang.annotation.Aspect} annotation.
+ *
  * @author Mark Fisher
  * @since 2.1
+ * @see org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -38,6 +43,11 @@ import java.lang.annotation.Target;
 @Documented
 public @interface Component {
 
+	/**
+	 * The value may indicate a suggestion for a logical component name,
+	 * to be turned into a Spring bean in case of an autodetected component.
+	 * @return the suggested component name, if any
+	 */
 	String value() default "";
 
 }
