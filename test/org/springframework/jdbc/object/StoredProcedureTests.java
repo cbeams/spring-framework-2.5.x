03943960097091faa74e984819f7d7dc8e725dc0
Juergen Hoeller <jhoeller@interface21.com>
2007-05-12 09:46:33 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13774 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jdbc/object/StoredProcedureTests.java
+++ test/org/springframework/jdbc/object/StoredProcedureTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -448,8 +448,7 @@ public class StoredProcedureTests extends AbstractJdbcTests {
 		ctrlConnection.setReturnValue(mockCallable);
 
 		replay();
-		ParameterMapperStoredProcedure pmsp =
-			new ParameterMapperStoredProcedure(mockDataSource);
+		ParameterMapperStoredProcedure pmsp = new ParameterMapperStoredProcedure(mockDataSource);
 		Map out = pmsp.executeTest();
 		assertEquals("OK", out.get("out"));
 	}
@@ -478,8 +477,7 @@ public class StoredProcedureTests extends AbstractJdbcTests {
 		ctrlConnection.setReturnValue(mockCallable);
 
 		replay();
-		SqlTypeValueStoredProcedure stvsp =
-			new SqlTypeValueStoredProcedure(mockDataSource);
+		SqlTypeValueStoredProcedure stvsp = new SqlTypeValueStoredProcedure(mockDataSource);
 		Map out = stvsp.executeTest(testVal);
 		assertEquals("OK", out.get("out"));
 	}
