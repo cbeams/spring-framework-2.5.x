a5e3357e11df45c390eab002bf141ec54e0d9384
Thomas Risberg <trisberg@interface21.com>
2007-05-14 03:02:29 +0000

    added ParameterizedBeanPropertyRowMapper and tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13804 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ tiger/src/org/springframework/jdbc/core/simple/ParameterizedBeanPropertyRowMapper.java
@@ -0,0 +1,62 @@
+package org.springframework.jdbc.core.simple;
+
+import org.springframework.jdbc.core.AbstractBeanPropertyRowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Generic ParameterizedRowMapper implementation that converts a row into a new instance
+ * of the specified mapped target class.  The mapped target class must be a top-level class
+ * and it must have a default or no-arg constructor.
+ *
+ * Uses Java 5 covariant return types to override the return type of the {@link #mapRow} method
+ * to be the type parameter <code>T</code>.
+ *
+ * Column values are mapped based on matching the column name as obtained from result set
+ * metadata to public setters for the corresponding properties.  The names are matched either
+ * directly or by transforming a name separating the parts with underscores to the same name
+ * using "camel" case.
+ *
+ * Mapping is provided for fields in the target class that are defined as any of the
+ * following types: String, byte, Byte, short, Short, int, Integer, long, Long, float, Float,
+ * double, Double, BigDecimal, boolean, Boolean and java.util.Date.
+ *
+ * To facilitate mapping between columns and fields that don't have matching names, try using column
+ * aliases in the SQL statement like "select fname as first_name from customer".
+ *
+ * Please note that this class is designed to provide convenience rather than high performance.
+ * For best performance consider using a custom RowMapper.
+ *
+ * @author trisberg
+ * @since 2.1
+ * @see ParameterizedRowMapper
+ */
+public class ParameterizedBeanPropertyRowMapper<T> extends AbstractBeanPropertyRowMapper implements ParameterizedRowMapper<T> {
+
+	/**
+	 * Create a new ParameterizedBeanPropertyRowMapper.
+	 * @see #setMappedClass
+	 */
+	public ParameterizedBeanPropertyRowMapper() {
+	}
+
+	/**
+	 * Create a new ParameterizedBeanPropertyRowMapper.
+	 * @param mappedClass the class that each row should be mapped to.
+	 */
+	public ParameterizedBeanPropertyRowMapper(Class mappedClass) {
+		initialize(mappedClass);
+	}
+
+    /**
+	 * Extract the values for all columns in the current row.
+	 * <p>Utilizes public setters and result set metadata.
+	 * @see java.sql.ResultSetMetaData
+	 */
+    @SuppressWarnings("unchecked")
+	public T mapRow(ResultSet rs, int rowNumber) throws SQLException {
+		return (T)doMapRow(rs, rowNumber);
+	}
+
+}
---
+++ tiger/test/org/springframework/jdbc/core/simple/ParameterizedBeanPropertyRowMapperTests.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.core.simple;
+
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.jdbc.core.AbstractRowMapperTests;
+import org.springframework.jdbc.core.test.Person;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Mock object based tests for ParameterizedBeanPropertyRowMapper.
+ *
+ * @author trisberg
+ */
+public class ParameterizedBeanPropertyRowMapperTests extends AbstractRowMapperTests {
+
+	private SimpleJdbcTemplate simpleJdbcTemplate;
+
+	protected void setUp() throws SQLException {
+		super.setUp();
+		simpleJdbcTemplate = new SimpleJdbcTemplate(jdbcTemplate);
+	}
+
+	public void testOverridingClassDefinedForMapping() {
+		ParameterizedBeanPropertyRowMapper<Person> mapper =
+				new ParameterizedBeanPropertyRowMapper<Person>(Person.class);
+		try {
+			mapper.setMappedClass(Long.class);
+			fail("Setting new class should have thrown InvalidDataAccessApiUsageException");
+		}
+		catch (InvalidDataAccessApiUsageException ex) {
+		}
+		try {
+			mapper.setMappedClass(Person.class);
+		}
+		catch (InvalidDataAccessApiUsageException ex) {
+			fail("Setting same class should not have thrown InvalidDataAccessApiUsageException");
+		}
+	}
+
+	public void testStaticQueryWithRowMapper() throws SQLException {
+
+		List<Person> result = simpleJdbcTemplate.query("select name, age, birth_date, balance from people",
+				new ParameterizedBeanPropertyRowMapper<Person>(Person.class));
+
+		assertEquals(1, result.size());
+
+		Person bean = result.get(0);
+
+		verify(bean);
+
+	}
+
+}
