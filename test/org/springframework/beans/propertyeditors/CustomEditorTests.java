ce6d0f35eebfa057e2b1b010ac29c5f8f19a8ccf
Juergen Hoeller <jhoeller@interface21.com>
2007-05-17 18:05:08 +0000

    trim text before comparing against true/false Strings
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13814 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/propertyeditors/CustomEditorTests.java
+++ test/org/springframework/beans/propertyeditors/CustomEditorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -178,6 +178,12 @@ public class CustomEditorTests extends TestCase {
 
 		bw.setPropertyValue("bool1", "false");
 		assertTrue("Correct bool1 value", Boolean.FALSE.equals(bw.getPropertyValue("bool1")));
+		assertTrue("Correct bool1 value", !tb.isBool1());
+
+		bw.setPropertyValue("bool1", "  true  ");
+		assertTrue("Correct bool1 value", tb.isBool1());
+
+		bw.setPropertyValue("bool1", "  false  ");
 		assertTrue("Correct bool1 value", !tb.isBool1());
 
 		bw.setPropertyValue("bool1", "on");
