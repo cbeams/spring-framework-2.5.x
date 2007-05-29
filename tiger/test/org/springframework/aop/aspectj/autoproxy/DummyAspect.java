6178ffd36ffa590616bb62b5cee80a59158e6a22
Juergen Hoeller <jhoeller@interface21.com>
2007-05-29 15:41:19 +0000

    added @author tag
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@13946 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/aop/aspectj/autoproxy/DummyAspect.java
+++ tiger/test/org/springframework/aop/aspectj/autoproxy/DummyAspect.java
@@ -20,6 +20,9 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+/**
+ * @author Juergen Hoeller
+ */
 @Aspect
 public class DummyAspect {
 
