91418cca1049bfadf50c62903cd3692a1fe1b67c
Juergen Hoeller <jhoeller@interface21.com>
2007-06-12 18:49:47 +0000

    fixed javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@14026 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/core/NestedIOException.java
+++ src/org/springframework/core/NestedIOException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ public class NestedIOException extends IOException {
 
 
 	/**
-	 * Construct a <code>NestedServletException</code> with the specified detail message.
+	 * Construct a <code>NestedIOException</code> with the specified detail message.
 	 * @param msg the detail message
 	 */
 	public NestedIOException(String msg) {
@@ -50,7 +50,7 @@ public class NestedIOException extends IOException {
 	}
 
 	/**
-	 * Construct a <code>NestedServletException</code> with the specified detail message
+	 * Construct a <code>NestedIOException</code> with the specified detail message
 	 * and nested exception.
 	 * @param msg the detail message
 	 * @param cause the nested exception
