e8b62b9b547b76da483cbb8df191a2078900bc2c
Juergen Hoeller <jhoeller@interface21.com>
2007-06-13 20:27:32 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14038 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/hibernate3/SessionHolder.java
+++ src/org/springframework/orm/hibernate3/SessionHolder.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,8 +29,8 @@ import org.springframework.util.Assert;
 
 /**
  * Session holder, wrapping a Hibernate Session and a Hibernate Transaction.
- * HibernateTransactionManager binds instances of this class
- * to the thread, for a given SessionFactory.
+ * HibernateTransactionManager binds instances of this class to the thread,
+ * for a given SessionFactory.
  *
  * <p>Note: This is an SPI class, not intended to be used by applications.
  *
@@ -132,7 +132,7 @@ public class SessionHolder extends ResourceHolderSupport {
 	}
 
 	public Transaction getTransaction() {
-		return transaction;
+		return this.transaction;
 	}
 
 	public void setPreviousFlushMode(FlushMode previousFlushMode) {
@@ -140,7 +140,7 @@ public class SessionHolder extends ResourceHolderSupport {
 	}
 
 	public FlushMode getPreviousFlushMode() {
-		return previousFlushMode;
+		return this.previousFlushMode;
 	}
 
 
--- tiger/src/org/springframework/orm/jpa/EntityManagerHolder.java
+++ tiger/src/org/springframework/orm/jpa/EntityManagerHolder.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2006 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,8 +23,8 @@ import org.springframework.util.Assert;
 
 /**
  * Holder wrapping a JPA EntityManager.
- * JpaTransactionManager binds instances of this class
- * to the thread, for a given EntityManagerFactory.
+ * JpaTransactionManager binds instances of this class to the thread,
+ * for a given EntityManagerFactory.
  *
  * <p>Note: This is an SPI class, not intended to be used by applications.
  *
@@ -37,13 +37,15 @@ public class EntityManagerHolder extends ResourceHolderSupport {
 
 	private final EntityManager entityManager;
 
+
 	public EntityManagerHolder(EntityManager entityManager) {
-		Assert.notNull(entityManager, "EntityManager must not be <code>null</code>");
+		Assert.notNull(entityManager, "EntityManager must not be null");
 		this.entityManager = entityManager;
 	}
 
+
 	public EntityManager getEntityManager() {
-		return entityManager;
+		return this.entityManager;
 	}
 
 }
