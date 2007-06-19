fb5a7def393fef2d07c50d4d9dc07eca752fa433
Rick Evans <revans@interface21.com>
2007-06-19 10:15:18 +0000

    Explained what the use of the term 'padding' in the class-level Javadoc means.
    Helpful for non-English speakers for whom padding means the stuff you stick around your china vases in a removal box so that they don't break in transit.
    
    Motivated in part by this blog post:
    
    http://jroller.com/page/nanik?entry=eyeballing_spring_jdbc
    
    
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14082 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/AbstractDataSource.java
+++ src/org/springframework/jdbc/datasource/AbstractDataSource.java
@@ -30,6 +30,10 @@ import org.springframework.util.Assert;
  * Abstract base class for Spring's {@link javax.sql.DataSource}
  * implementations, taking care of the padding.
  *
+ * <p>'Padding' in the context of this class means default implementations
+ * for certain methods from the <code>DataSource</code> interface, such as
+ * {@link #getLoginTimeout()}, {@link #setLoginTimeout(int)}, and so forth.
+ *
  * @author Juergen Hoeller
  * @since 07.05.2003
  * @see DriverManagerDataSource
@@ -41,7 +45,7 @@ public abstract class AbstractDataSource implements DataSource {
 
 
 	/**
-	 * Returns 0, indicating to use the default system timeout.
+	 * Returns 0, indicating the default system timeout is to be used.
 	 */
 	public int getLoginTimeout() throws SQLException {
 		return 0;
