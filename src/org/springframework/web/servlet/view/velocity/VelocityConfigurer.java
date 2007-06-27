83b7cb4be603dcb095c9ff9992cad333a80e80c5
Juergen Hoeller <jhoeller@interface21.com>
2007-06-27 16:25:09 +0000

    revised javadoc for "setVelocityEngine"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@14118 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/view/velocity/VelocityConfigurer.java
+++ src/org/springframework/web/servlet/view/velocity/VelocityConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,27 +32,30 @@ import org.springframework.ui.velocity.VelocityEngineFactory;
  * The simplest way to use this class is to specify just a "resourceLoaderPath";
  * you do not need any further configuration then.
  *
- * <pre>
+ * <pre class="code">
  * &lt;bean id="velocityConfig" class="org.springframework.web.servlet.view.velocity.VelocityConfigurer"&gt;
  *   &lt;property name="resourceLoaderPath">&lt;value&gt;/WEB-INF/velocity/&lt;/value>&lt;/property&gt;
  * &lt;/bean&gt;</pre>
  *
  * This bean must be included in the application context of any application
- * using Spring's VelocityView for web MVC. It exists purely to configure Velocity;
- * it is not meant to be referenced by application components but just internally
- * by VelocityView. Implements VelocityConfig to be found by VelocityView without
- * depending on the bean name of the configurer. Each DispatcherServlet can define
- * its own VelocityConfigurer if desired.
+ * using Spring's {@link VelocityView} for web MVC. It exists purely to configure
+ * Velocity; it is not meant to be referenced by application components (just
+ * internally by VelocityView). This class implements {@link VelocityConfig}
+ * in order to be found by VelocityView without depending on the bean name of
+ * this configurer. Each DispatcherServlet may define its own VelocityConfigurer
+ * if desired, potentially with different template loader paths.
  *
- * <p>Note that you can also refer to a preconfigured VelocityEngine instance, for
- * example one set up by VelocityEngineFactoryBean, via the "velocityEngine" property.
+ * <p>Note that you can also refer to a pre-configured VelocityEngine
+ * instance via the "velocityEngine" property, e.g. set up by
+ * {@link org.springframework.ui.velocity.VelocityEngineFactoryBean},
  * This allows to share a VelocityEngine for web and email usage, for example.
  *
  * <p>This configurer registers the "spring.vm" Velocimacro library for web views
- * (contained in this package and thus in spring.jar), which makes all macros
- * defined in it implicitly available:
+ * (contained in this package and thus in <code>spring.jar</code>), which makes
+ * all of Spring's default Velocity macros available to the views.
+ * This allows for using the Spring-provided macros such as follows:
  *
- * <pre>
+ * <pre class="code">
  * #springBind("person.age")
  * age is ${status.value}</pre>
  *
@@ -63,7 +66,6 @@ import org.springframework.ui.velocity.VelocityEngineFactory;
  * @see #setVelocityProperties
  * @see #setResourceLoaderPath
  * @see #setVelocityEngine
- * @see org.springframework.ui.velocity.VelocityEngineFactoryBean
  * @see VelocityView
  */
 public class VelocityConfigurer extends VelocityEngineFactory
@@ -81,12 +83,17 @@ public class VelocityConfigurer extends VelocityEngineFactory
 
 	private VelocityEngine velocityEngine;
 
+
 	/**
-	 * Set a preconfigured VelocityEngine to use for the Velocity web config, e.g.
-	 * a shared one for web and email usage, set up via VelocityEngineFactoryBean.
-	 * If this is not set, VelocityEngineFactory's properties (inherited by this
-	 * class) have to be specified.
-	 * @see org.springframework.ui.velocity.VelocityEngineFactoryBean
+	 * Set a pre-configured VelocityEngine to use for the Velocity web
+	 * configuration: e.g. a shared one for web and email usage, set up via
+	 * {@link org.springframework.ui.velocity.VelocityEngineFactoryBean}.
+	 * <p>Note that the Spring macros will <i>not</i> be enabled automatically in
+	 * case of an external VelocityEngine passed in here. Make sure to include
+	 * <code>spring.vm</code> in your template loader path in such a scenario
+	 * (if there is an actual need to use those macros).
+	 * <p>If this is not set, VelocityEngineFactory's properties
+	 * (inherited by this class) have to be specified.
 	 */
 	public void setVelocityEngine(VelocityEngine velocityEngine) {
 		this.velocityEngine = velocityEngine;
@@ -94,7 +101,7 @@ public class VelocityConfigurer extends VelocityEngineFactory
 
 	/**
 	 * Initialize VelocityEngineFactory's VelocityEngine
-	 * if not overridden by a preconfigured VelocityEngine.
+	 * if not overridden by a pre-configured VelocityEngine.
 	 * @see #createVelocityEngine
 	 * @see #setVelocityEngine
 	 */
