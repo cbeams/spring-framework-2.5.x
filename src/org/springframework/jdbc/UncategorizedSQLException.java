59b1b04a4bd3905a40b33c8559d6563b290d2422
Juergen Hoeller <jhoeller@interface21.com>
2007-08-02 08:25:37 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14384 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/UncategorizedSQLException.java
+++ src/org/springframework/jdbc/UncategorizedSQLException.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,7 @@ import org.springframework.dao.UncategorizedDataAccessException;
  * @author Juergen Hoeller
  */
 public class UncategorizedSQLException extends UncategorizedDataAccessException {
-	
+
 	/** SQL that led to the problem */
 	private final String sql;
 
@@ -44,7 +44,7 @@ public class UncategorizedSQLException extends UncategorizedDataAccessException 
 				ex.getSQLState() + "]; error code [" + ex.getErrorCode() + "]; " + ex.getMessage(), ex);
 		this.sql = sql;
 	}
-	
+
 
 	/**
 	 * Return the underlying SQLException.
@@ -52,12 +52,12 @@ public class UncategorizedSQLException extends UncategorizedDataAccessException 
 	public SQLException getSQLException() {
 		return (SQLException) getCause();
 	}
-	
+
 	/**
 	 * Return the SQL that led to the problem.
 	 */
 	public String getSql() {
-		return sql;
+		return this.sql;
 	}
 
 }
