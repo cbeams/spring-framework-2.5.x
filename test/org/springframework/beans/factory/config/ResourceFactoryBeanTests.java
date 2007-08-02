1432a79557bb43d0b8968dddd0e750b9cfaff5f7
Juergen Hoeller <jhoeller@interface21.com>
2007-08-02 07:43:28 +0000

    fixed formatting
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@14382 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/config/ResourceFactoryBeanTests.java
+++ test/org/springframework/beans/factory/config/ResourceFactoryBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,100 +16,98 @@
 
 package org.springframework.beans.factory.config;
 
-import junit.framework.TestCase;
-import org.easymock.MockControl;
-import org.springframework.core.io.Resource;
-import org.springframework.test.AssertThrows;
-
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 
+import junit.framework.TestCase;
+import org.easymock.MockControl;
+
+import org.springframework.core.io.Resource;
+import org.springframework.test.AssertThrows;
+
 /**
- * Unit tests for the {@link ResourceFactoryBean} class.
- *
  * @author Rick Evans
  */
-public final class ResourceFactoryBeanTests extends TestCase {
+public class ResourceFactoryBeanTests extends TestCase {
 
-    public void testIsSingleton() throws Exception {
-        ResourceFactoryBean factory = new ResourceFactoryBean();
-        assertTrue(factory.isSingleton());
-    }
+	public void testIsSingleton() throws Exception {
+		ResourceFactoryBean factory = new ResourceFactoryBean();
+		assertTrue(factory.isSingleton());
+	}
 
-    public void testGetObjectTypeDefaultsToPlainResourceInterfaceifLookupResourceIsNotSupplied() throws Exception {
-        ResourceFactoryBean factory = new ResourceFactoryBean();
-        assertEquals(Resource.class, factory.getObjectType());
-    }
+	public void testGetObjectTypeDefaultsToPlainResourceInterfaceifLookupResourceIsNotSupplied() throws Exception {
+		ResourceFactoryBean factory = new ResourceFactoryBean();
+		assertEquals(Resource.class, factory.getObjectType());
+	}
 
-    public void testGetObjectTypeReturnsActualResourceLocationTypeWhenLookupResourceIsSupplied() throws Exception {
-        ResourceFactoryBean factory = new ResourceFactoryBean();
-        factory.setLocation(new StubResource());
-        assertEquals(StubResource.class, factory.getObjectType());
-    }
+	public void testGetObjectTypeReturnsActualResourceLocationTypeWhenLookupResourceIsSupplied() throws Exception {
+		ResourceFactoryBean factory = new ResourceFactoryBean();
+		factory.setLocation(new StubResource());
+		assertEquals(StubResource.class, factory.getObjectType());
+	}
 
-    public void testReturnsResourceAsIsInSunnyDayCase() throws Exception {
-        MockControl mockResource = MockControl.createControl(Resource.class);
-        Resource resource = (Resource) mockResource.getMock();
-        mockResource.replay();
+	public void testReturnsResourceAsIsInSunnyDayCase() throws Exception {
+		MockControl mockResource = MockControl.createControl(Resource.class);
+		Resource resource = (Resource) mockResource.getMock();
+		mockResource.replay();
 
-        ResourceFactoryBean factory = new ResourceFactoryBean();
-        factory.setLocation(resource);
-        factory.afterPropertiesSet();
-        Object object = factory.getObject();
+		ResourceFactoryBean factory = new ResourceFactoryBean();
+		factory.setLocation(resource);
+		factory.afterPropertiesSet();
+		Object object = factory.getObject();
 
-        assertNotNull("As per FactoryBean contract, the return value of getObject() cannot be null.", object);
-        assertTrue("Obviously not getting a Resource back", Resource.class.isAssignableFrom(object.getClass()));
-        assertSame("Not passing Resource location back as-is", resource, object);
+		assertNotNull("As per FactoryBean contract, the return value of getObject() cannot be null.", object);
+		assertTrue("Obviously not getting a Resource back", Resource.class.isAssignableFrom(object.getClass()));
+		assertSame("Not passing Resource location back as-is", resource, object);
 
-        mockResource.verify();
-    }
+		mockResource.verify();
+	}
 
-    public void testWhenResourceLocationIsMissing() throws Exception {
-        new AssertThrows(IllegalArgumentException.class) {
-            public void test() throws Exception {
-                ResourceFactoryBean factory = new ResourceFactoryBean();
-                factory.afterPropertiesSet();
-            }
-        }.runTest();
-    }
-
-
-    private static final class StubResource implements Resource {
-
-        public boolean exists() {
-            throw new UnsupportedOperationException();
-        }
-
-        public boolean isOpen() {
-            throw new UnsupportedOperationException();
-        }
-
-        public URL getURL() {
-            throw new UnsupportedOperationException();
-        }
-
-        public File getFile() {
-            throw new UnsupportedOperationException();
-        }
-
-        public Resource createRelative(String relativePath) {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getFilename() {
-            throw new UnsupportedOperationException();
-        }
-
-        public String getDescription() {
-            throw new UnsupportedOperationException();
-        }
+	public void testWhenResourceLocationIsMissing() throws Exception {
+		new AssertThrows(IllegalArgumentException.class) {
+			public void test() throws Exception {
+				ResourceFactoryBean factory = new ResourceFactoryBean();
+				factory.afterPropertiesSet();
+			}
+		}.runTest();
+	}
 
 
-        public InputStream getInputStream() {
-            throw new UnsupportedOperationException();
-        }
+	private static class StubResource implements Resource {
 
-    }
+		public boolean exists() {
+			throw new UnsupportedOperationException();
+		}
+
+		public boolean isOpen() {
+			throw new UnsupportedOperationException();
+		}
+
+		public URL getURL() {
+			throw new UnsupportedOperationException();
+		}
+
+		public File getFile() {
+			throw new UnsupportedOperationException();
+		}
+
+		public Resource createRelative(String relativePath) {
+			throw new UnsupportedOperationException();
+		}
+
+		public String getFilename() {
+			throw new UnsupportedOperationException();
+		}
+
+		public String getDescription() {
+			throw new UnsupportedOperationException();
+		}
+
+		public InputStream getInputStream() {
+			throw new UnsupportedOperationException();
+		}
+
+	}
 
 }
