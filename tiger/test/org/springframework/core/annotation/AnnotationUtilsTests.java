f9f142b86514788e334075ed4199e5df1314717c
Sam Brannen <sam@sambrannen.com>
2007-08-02 15:05:48 +0000

    Added unit tests for AnnotationUtils:  - findAnnotationDeclaringClass(Class<? extends Annotation>, Class<?>)  - isAnnotationDeclaredLocally(Class<? extends Annotation>, Class<?>)  - isAnnotationInherited(Class<? extends Annotation>, Class<?>)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14392 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/core/annotation/AnnotationUtilsTests.java
+++ tiger/test/org/springframework/core/annotation/AnnotationUtilsTests.java
@@ -16,6 +16,12 @@
 
 package org.springframework.core.annotation;
 
+import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
+import static org.springframework.core.annotation.AnnotationUtils.findAnnotationDeclaringClass;
+import static org.springframework.core.annotation.AnnotationUtils.getAnnotation;
+import static org.springframework.core.annotation.AnnotationUtils.isAnnotationDeclaredLocally;
+import static org.springframework.core.annotation.AnnotationUtils.isAnnotationInherited;
+
 import java.lang.reflect.Method;
 
 import junit.framework.TestCase;
@@ -25,90 +31,177 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * @author Rod Johnson
  * @author Juergen Hoeller
+ * @author Sam Brannen
  */
 public class AnnotationUtilsTests extends TestCase {
 
 	public void testFindMethodAnnotationOnLeaf() throws SecurityException, NoSuchMethodException {
-		Method m = Leaf.class.getMethod("annotatedOnLeaf", (Class[]) null);
+
+		final Method m = Leaf.class.getMethod("annotatedOnLeaf", (Class[]) null);
 		assertNotNull(m.getAnnotation(Order.class));
-		assertNotNull(AnnotationUtils.getAnnotation(m, Order.class));
-		assertNotNull(AnnotationUtils.findAnnotation(m, Order.class));
+		assertNotNull(getAnnotation(m, Order.class));
+		assertNotNull(findAnnotation(m, Order.class));
 	}
-	
+
 	public void testFindMethodAnnotationOnRoot() throws SecurityException, NoSuchMethodException {
-		Method m = Leaf.class.getMethod("annotatedOnRoot", (Class[]) null);
+
+		final Method m = Leaf.class.getMethod("annotatedOnRoot", (Class[]) null);
 		assertNotNull(m.getAnnotation(Order.class));
-		assertNotNull(AnnotationUtils.getAnnotation(m, Order.class));
-		assertNotNull(AnnotationUtils.findAnnotation(m, Order.class));
+		assertNotNull(getAnnotation(m, Order.class));
+		assertNotNull(findAnnotation(m, Order.class));
 	}
-	
+
 	public void testFindMethodAnnotationOnRootButOverridden() throws SecurityException, NoSuchMethodException {
-		Method m = Leaf.class.getMethod("overrideWithoutNewAnnotation", (Class[]) null);
+
+		final Method m = Leaf.class.getMethod("overrideWithoutNewAnnotation", (Class[]) null);
 		assertNull(m.getAnnotation(Order.class));
-		assertNull(AnnotationUtils.getAnnotation(m, Order.class));
-		assertNotNull(AnnotationUtils.findAnnotation(m, Order.class));
+		assertNull(getAnnotation(m, Order.class));
+		assertNotNull(findAnnotation(m, Order.class));
 	}
-	
+
 	public void testFindMethodAnnotationNotAnnotated() throws SecurityException, NoSuchMethodException {
-		Method m = Leaf.class.getMethod("notAnnotated", (Class[]) null);
-		assertNull(AnnotationUtils.findAnnotation(m, Order.class));
+
+		final Method m = Leaf.class.getMethod("notAnnotated", (Class[]) null);
+		assertNull(findAnnotation(m, Order.class));
 	}
 
 	public void testFindMethodAnnotationOnBridgeMethod() throws Exception {
-		Method m = SimpleFoo.class.getMethod("something", Object.class);
+
+		final Method m = SimpleFoo.class.getMethod("something", Object.class);
 		assertTrue(m.isBridge());
 		assertNull(m.getAnnotation(Order.class));
-		assertNull(AnnotationUtils.getAnnotation(m, Order.class));
-		assertNotNull(AnnotationUtils.findAnnotation(m, Order.class));
+		assertNull(getAnnotation(m, Order.class));
+		assertNotNull(findAnnotation(m, Order.class));
 		assertNull(m.getAnnotation(Transactional.class));
-		assertNotNull(AnnotationUtils.getAnnotation(m, Transactional.class));
-		assertNotNull(AnnotationUtils.findAnnotation(m, Transactional.class));
+		assertNotNull(getAnnotation(m, Transactional.class));
+		assertNotNull(findAnnotation(m, Transactional.class));
 	}
 
-// TODO consider whether we want this to handle annotations on interfaces
-//	public void testFindMethodAnnotationFromInterfaceImplementedByRoot() throws Exception {
-//		Method m = Leaf.class.getMethod("fromInterfaceImplementedByRoot", (Class[]) null);
-//		Order o = AnnotationUtils.findAnnotation(Order.class, m, Leaf.class);
-//		assertNotNull(o);
-//	}
+	// TODO consider whether we want this to handle annotations on interfaces
+	// public void testFindMethodAnnotationFromInterfaceImplementedByRoot()
+	// throws Exception {
+	// Method m = Leaf.class.getMethod("fromInterfaceImplementedByRoot",
+	// (Class[]) null);
+	// Order o = findAnnotation(Order.class, m, Leaf.class);
+	// assertNotNull(o);
+	// }
 
+	public void testFindAnnotationDeclaringClass() throws Exception {
 
-	public interface AnnotatedInterface {
+		// no class-level annotation
+		assertNull(findAnnotationDeclaringClass(Transactional.class, NonAnnotatedInterface.class));
+		assertNull(findAnnotationDeclaringClass(Transactional.class, NonAnnotatedClass.class));
+
+		// inherited class-level annotation; note: @Transactional is inherited
+		assertEquals(InheritedAnnotationInterface.class, findAnnotationDeclaringClass(Transactional.class,
+				InheritedAnnotationInterface.class));
+		assertNull(findAnnotationDeclaringClass(Transactional.class, SubInheritedAnnotationInterface.class));
+		assertEquals(InheritedAnnotationClass.class, findAnnotationDeclaringClass(Transactional.class,
+				InheritedAnnotationClass.class));
+		assertEquals(InheritedAnnotationClass.class, findAnnotationDeclaringClass(Transactional.class,
+				SubInheritedAnnotationClass.class));
+
+		// non-inherited class-level annotation; note: @Order is not inherited,
+		// but findAnnotationDeclaringClass() should still find it.
+		assertEquals(NonInheritedAnnotationInterface.class, findAnnotationDeclaringClass(Order.class,
+				NonInheritedAnnotationInterface.class));
+		assertNull(findAnnotationDeclaringClass(Order.class, SubNonInheritedAnnotationInterface.class));
+		assertEquals(NonInheritedAnnotationClass.class, findAnnotationDeclaringClass(Order.class,
+				NonInheritedAnnotationClass.class));
+		assertEquals(NonInheritedAnnotationClass.class, findAnnotationDeclaringClass(Order.class,
+				SubNonInheritedAnnotationClass.class));
+	}
+
+	public void testIsAnnotationDeclaredLocally() throws Exception {
+
+		// no class-level annotation
+		assertFalse(isAnnotationDeclaredLocally(Transactional.class, NonAnnotatedInterface.class));
+		assertFalse(isAnnotationDeclaredLocally(Transactional.class, NonAnnotatedClass.class));
+
+		// inherited class-level annotation; note: @Transactional is inherited
+		assertTrue(isAnnotationDeclaredLocally(Transactional.class, InheritedAnnotationInterface.class));
+		assertFalse(isAnnotationDeclaredLocally(Transactional.class, SubInheritedAnnotationInterface.class));
+		assertTrue(isAnnotationDeclaredLocally(Transactional.class, InheritedAnnotationClass.class));
+		assertFalse(isAnnotationDeclaredLocally(Transactional.class, SubInheritedAnnotationClass.class));
+
+		// non-inherited class-level annotation; note: @Order is not inherited
+		assertTrue(isAnnotationDeclaredLocally(Order.class, NonInheritedAnnotationInterface.class));
+		assertFalse(isAnnotationDeclaredLocally(Order.class, SubNonInheritedAnnotationInterface.class));
+		assertTrue(isAnnotationDeclaredLocally(Order.class, NonInheritedAnnotationClass.class));
+		assertFalse(isAnnotationDeclaredLocally(Order.class, SubNonInheritedAnnotationClass.class));
+	}
+
+	public void testIsAnnotationInherited() throws Exception {
+
+		// no class-level annotation
+		assertFalse(isAnnotationInherited(Transactional.class, NonAnnotatedInterface.class));
+		assertFalse(isAnnotationInherited(Transactional.class, NonAnnotatedClass.class));
+
+		// inherited class-level annotation; note: @Transactional is inherited
+		assertFalse(isAnnotationInherited(Transactional.class, InheritedAnnotationInterface.class));
+		// isAnnotationInherited() does not currently traverse interface
+		// hierarchies. Thus the following, though perhaps counter intuitive,
+		// must be false:
+		assertFalse(isAnnotationInherited(Transactional.class, SubInheritedAnnotationInterface.class));
+		assertFalse(isAnnotationInherited(Transactional.class, InheritedAnnotationClass.class));
+		assertTrue(isAnnotationInherited(Transactional.class, SubInheritedAnnotationClass.class));
+
+		// non-inherited class-level annotation; note: @Order is not inherited
+		assertFalse(isAnnotationInherited(Order.class, NonInheritedAnnotationInterface.class));
+		assertFalse(isAnnotationInherited(Order.class, SubNonInheritedAnnotationInterface.class));
+		assertFalse(isAnnotationInherited(Order.class, NonInheritedAnnotationClass.class));
+		assertFalse(isAnnotationInherited(Order.class, SubNonInheritedAnnotationClass.class));
+	}
+
+	public static interface AnnotatedInterface {
 
 		@Order(0)
 		void fromInterfaceImplementedByRoot();
 	}
 
-
 	public static class Root implements AnnotatedInterface {
 
 		@Order(27)
-		public void annotatedOnRoot() {}
+		public void annotatedOnRoot() {
 
-		public void overrideToAnnotate() {}
+		}
+
+		public void overrideToAnnotate() {
+
+		}
 
 		@Order(27)
-		public void overrideWithoutNewAnnotation() {}
+		public void overrideWithoutNewAnnotation() {
 
-		public void notAnnotated() {}
+		}
 
-		public void fromInterfaceImplementedByRoot() {}
+		public void notAnnotated() {
+
+		}
+
+		public void fromInterfaceImplementedByRoot() {
+
+		}
 	}
-
 
 	public static class Leaf extends Root {
 
 		@Order(25)
-		public void annotatedOnLeaf() {}
+		public void annotatedOnLeaf() {
+
+		}
 
 		@Override
 		@Order(1)
-		public void overrideToAnnotate() { }
+		public void overrideToAnnotate() {
+
+		}
 
 		@Override
-		public void overrideWithoutNewAnnotation() {}
-	}
+		public void overrideWithoutNewAnnotation() {
 
+		}
+	}
 
 	public static abstract class Foo<T> {
 
@@ -116,12 +209,46 @@ public class AnnotationUtilsTests extends TestCase {
 		public abstract void something(T arg);
 	}
 
-
 	public static class SimpleFoo extends Foo<String> {
 
 		@Transactional
-		public void something(String arg) {
+		public void something(final String arg) {
+
 		}
+	}
+
+	@Transactional
+	public static interface InheritedAnnotationInterface {
+	}
+
+	public static interface SubInheritedAnnotationInterface extends InheritedAnnotationInterface {
+	}
+
+	@Order
+	public static interface NonInheritedAnnotationInterface {
+	}
+
+	public static interface SubNonInheritedAnnotationInterface extends NonInheritedAnnotationInterface {
+	}
+
+	public static class NonAnnotatedClass {
+	}
+
+	public static interface NonAnnotatedInterface {
+	}
+
+	@Transactional
+	public static class InheritedAnnotationClass {
+	}
+
+	public static class SubInheritedAnnotationClass extends InheritedAnnotationClass {
+	}
+
+	@Order
+	public static class NonInheritedAnnotationClass {
+	}
+
+	public static class SubNonInheritedAnnotationClass extends NonInheritedAnnotationClass {
 	}
 
 }
