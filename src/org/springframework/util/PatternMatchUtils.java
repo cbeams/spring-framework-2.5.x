c8519beb2a5fac43bc1169d1cb14d376b6ad4c2f
Juergen Hoeller <jhoeller@interface21.com>
2007-08-06 13:21:01 +0000

    support multi-part patterns as well ("xxx*yyy")
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14428 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/PatternMatchUtils.java
+++ src/org/springframework/util/PatternMatchUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,34 +27,47 @@ public abstract class PatternMatchUtils {
 
 	/**
 	 * Match a String against the given pattern, supporting the following simple
-	 * pattern styles: "xxx*", "*xxx" and "*xxx*" matches, as well as direct equality.
+	 * pattern styles: "xxx*", "*xxx", "*xxx*" and "xxx*yyy" matches (with an
+	 * arbitrary number of pattern parts), as well as direct equality.
 	 * @param pattern the pattern to match against
 	 * @param str the String to match
 	 * @return whether the String matches the given pattern
 	 */
 	public static boolean simpleMatch(String pattern, String str) {
-		if (ObjectUtils.nullSafeEquals(pattern, str) || "*".equals(pattern)) {
-			return true;
-		}
 		if (pattern == null || str == null) {
 			return false;
 		}
-		if (pattern.startsWith("*") && pattern.endsWith("*") &&
-				str.indexOf(pattern.substring(1, pattern.length() - 1)) != -1) {
-			return true;
+		int firstIndex = pattern.indexOf('*');
+		if (firstIndex == -1) {
+			return pattern.equals(str);
 		}
-		if (pattern.startsWith("*") && str.endsWith(pattern.substring(1, pattern.length()))) {
-			return true;
+		if (firstIndex == 0) {
+			if (pattern.length() == 1) {
+				return true;
+			}
+			int nextIndex = pattern.indexOf('*', firstIndex + 1);
+			if (nextIndex == -1) {
+				return str.endsWith(pattern.substring(1));
+			}
+			String part = pattern.substring(1, nextIndex);
+			int partIndex = str.indexOf(part);
+			while (partIndex != -1) {
+				if (simpleMatch(pattern.substring(nextIndex), str.substring(partIndex + part.length()))) {
+					return true;
+				}
+				partIndex = str.indexOf(part, partIndex + 1);
+			}
+			return false;
 		}
-		if (pattern.endsWith("*") && str.startsWith(pattern.substring(0, pattern.length() - 1))) {
-			return true;
-		}
-		return false;
+		return (str.length() >= firstIndex &&
+				pattern.substring(0, firstIndex).equals(str.substring(0, firstIndex)) &&
+				simpleMatch(pattern.substring(firstIndex), str.substring(firstIndex)));
 	}
 
 	/**
 	 * Match a String against the given patterns, supporting the following simple
-	 * pattern styles: "xxx*", "*xxx" and "*xxx*" matches, as well as direct equality.
+	 * pattern styles: "xxx*", "*xxx", "*xxx*" and "xxx*yyy" matches (with an
+	 * arbitrary number of pattern parts), as well as direct equality.
 	 * @param patterns the patterns to match against
 	 * @param str the String to match
 	 * @return whether the String matches any of the given patterns
