32dd48213a4a32a90bb74293b982c5a61a534ba9
Juergen Hoeller <jhoeller@interface21.com>
2007-08-08 11:20:56 +0000

    refined assertion
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14466 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/lookup/BeanFactoryDataSourceLookup.java
+++ src/org/springframework/jdbc/datasource/lookup/BeanFactoryDataSourceLookup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ public class BeanFactoryDataSourceLookup implements DataSourceLookup, BeanFactor
 
 
 	public DataSource getDataSource(String dataSourceName) throws DataSourceLookupFailureException {
-		Assert.notNull(this.beanFactory, "BeanFactory is required");
+		Assert.state(this.beanFactory != null, "BeanFactory is required");
 		try {
 			return (DataSource) this.beanFactory.getBean(dataSourceName, DataSource.class);
 		}
