24044adb372b72f3014b4c9aefed1a91e0874e6e
Juergen Hoeller <jhoeller@interface21.com>
2007-08-12 14:28:31 +0000

    minimized assertions
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@14505 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ src/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -124,7 +124,6 @@ public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 	}
 
 	public Object getSingleton(String beanName) {
-		Assert.notNull(beanName, "'beanName' must not be null");
 		Object singletonObject = this.singletonObjects.get(beanName);
 		return (singletonObject != NULL_OBJECT ? singletonObject : null);
 	}
@@ -170,13 +169,11 @@ public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
 	 * @param beanName the name of the bean
 	 */
 	protected void removeSingleton(String beanName) {
-		Assert.notNull(beanName, "'beanName' must not be null");
 		this.singletonObjects.remove(beanName);
 		this.registeredSingletons.remove(beanName);
 	}
 
 	public boolean containsSingleton(String beanName) {
-		Assert.notNull(beanName, "'beanName' must not be null");
 		return (this.singletonObjects.containsKey(beanName));
 	}
 
