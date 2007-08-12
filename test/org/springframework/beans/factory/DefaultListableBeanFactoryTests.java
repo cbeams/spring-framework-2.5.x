2a9295a6f7dbfce5c34e05b694db8e3dfe15e669
Juergen Hoeller <jhoeller@interface21.com>
2007-08-12 15:12:06 +0000

    adapted performance limits in bean creation tests
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14511 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/beans/factory/DefaultListableBeanFactoryTests.java
+++ test/org/springframework/beans/factory/DefaultListableBeanFactoryTests.java
@@ -1447,7 +1447,7 @@ public class DefaultListableBeanFactoryTests extends TestCase {
 		}
 		sw.stop();
 		System.out.println(sw.getTotalTimeMillis());
-		assertTrue("Prototype creation took too long: " + sw.getTotalTimeMillis(), sw.getTotalTimeMillis() < 3000);
+		assertTrue("Prototype creation took too long: " + sw.getTotalTimeMillis(), sw.getTotalTimeMillis() < 2000);
 	}
 
 	/**
@@ -1564,7 +1564,7 @@ public class DefaultListableBeanFactoryTests extends TestCase {
 		}
 		sw.stop();
 		System.out.println(sw.getTotalTimeMillis());
-		assertTrue("Prototype creation took too long: " + sw.getTotalTimeMillis(), sw.getTotalTimeMillis() < 3000);
+		assertTrue("Prototype creation took too long: " + sw.getTotalTimeMillis(), sw.getTotalTimeMillis() < 4000);
 	}
 
 	public void testBeanPostProcessorWithWrappedObjectAndDisposableBean() {
