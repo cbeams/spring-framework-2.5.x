6cc5ceaee6566be2785479cf7a59ae56a2a26ffd
Sam Brannen <sam@sambrannen.com>
2007-08-21 14:44:53 +0000

    New abstract base test classes which integrate Spring's annotation-based ApplicationContext and transactional testing support in a JUnit 4.4 environment.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14614 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ tiger/mock/org/springframework/test/junit4/AbstractTransactionalJUnit4SpringContextTests.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.test.junit4;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.test.annotation.ContextConfiguration;
+import org.springframework.test.annotation.NotTransactional;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.annotation.TestExecutionListeners;
+import org.springframework.test.annotation.TransactionConfiguration;
+import org.springframework.test.context.listeners.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.listeners.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.listeners.TransactionalTestExecutionListener;
+import org.springframework.test.jdbc.SimpleJdbcTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <p>
+ * Abstract {@link Transactional transactional} extension of
+ * {@link AbstractJUnit4SpringContextTests} that also adds some convenience
+ * functionality for JDBC access. Expects a {@link DataSource} bean to be
+ * defined in the Spring {@link ApplicationContext application context}.
+ * </p>
+ * <p>
+ * This class exposes a {@link SimpleJdbcTemplate} and provides an easy way to
+ * delete from the database in a new transaction.
+ * </p>
+ * <p>
+ * Concrete subclasses must fulfill the same requirements outlined in
+ * {@link AbstractJUnit4SpringContextTests}.
+ * </p>
+ *
+ * @see AbstractJUnit4SpringContextTests
+ * @see ContextConfiguration
+ * @see TestExecutionListeners
+ * @see TransactionConfiguration
+ * @see Transactional
+ * @see NotTransactional
+ * @see Rollback
+ * @see SimpleJdbcTestUtils
+ * @author Sam Brannen
+ * @version $Revision: 1.1 $
+ * @since 2.1
+ */
+@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+		TransactionalTestExecutionListener.class })
+@Transactional
+public class AbstractTransactionalJUnit4SpringContextTests extends AbstractJUnit4SpringContextTests {
+
+	// ------------------------------------------------------------------------|
+	// --- INSTANCE VARIABLES -------------------------------------------------|
+	// ------------------------------------------------------------------------|
+
+	protected SimpleJdbcTemplate	simpleJdbcTemplate;
+
+	// ------------------------------------------------------------------------|
+	// --- INSTANCE METHODS ---------------------------------------------------|
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * Set the DataSource, typically provided via Dependency Injection.
+	 *
+	 * @param dataSource The DataSource to inject.
+	 */
+	@Autowired
+	public void setDataSource(final DataSource dataSource) {
+
+		this.simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
+	}
+
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * Get the SimpleJdbcTemplate that this base class manages.
+	 *
+	 * @return the SimpleJdbcTemplate
+	 */
+	public final SimpleJdbcTemplate getSimpleJdbcTemplate() {
+
+		return this.simpleJdbcTemplate;
+	}
+
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * Count the rows in the given table.
+	 *
+	 * @param tableName table name to count rows in
+	 * @return the number of rows in the table
+	 */
+	protected int countRowsInTable(final String tableName) {
+
+		return SimpleJdbcTestUtils.countRowsInTable(getSimpleJdbcTemplate(), tableName);
+	}
+
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * <p>
+	 * Convenience method for deleting all rows from the specified tables.
+	 * </p>
+	 * <p>
+	 * Use with caution outside of a transaction!
+	 * </p>
+	 *
+	 * @param names The names of the tables from which to delete.
+	 * @return The total number of rows deleted from all specified tables.
+	 */
+	protected int deleteFromTables(final String... names) {
+
+		return SimpleJdbcTestUtils.deleteFromTables(getSimpleJdbcTemplate(), names);
+	}
+
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * <p>
+	 * Execute the given SQL script.
+	 * </p>
+	 * <p>
+	 * Use with caution outside of a transaction!
+	 * </p>
+	 *
+	 * @param sqlResourcePath Spring resource path for the SQL script. Should
+	 *        normally be loaded by classpath. There should be one statement per
+	 *        line. Any semicolons will be removed. <b>Do not use this method to
+	 *        execute DDL if you expect rollback.</b>
+	 * @param continueOnError whether or not to continue without throwing an
+	 *        exception in the event of an error.
+	 * @throws DataAccessException if there is an error executing a statement
+	 *         and continueOnError was <code>false</code>.
+	 */
+	protected void executeSqlScript(final String sqlResourcePath, final boolean continueOnError)
+			throws DataAccessException {
+
+		SimpleJdbcTestUtils.executeSqlScript(getSimpleJdbcTemplate(), getApplicationContext(), sqlResourcePath,
+				continueOnError);
+	}
+
+	// ------------------------------------------------------------------------|
+
+}
