0f7a1232dfecf40058b119cb0d5e5f1fbf18f09d
Sam Brannen <sam@sambrannen.com>
2007-08-21 22:10:47 +0000

    ConcreteTransactionalJUnit38SpringContextTests is now a combined unit test for AbstractJUnit38SpringContextTests and AbstractTransactionalJUnit38SpringContextTests.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14618 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/test/junit38/ConcreteTransactionalJUnit38SpringContextTests.java
+++ tiger/test/org/springframework/test/junit38/ConcreteTransactionalJUnit38SpringContextTests.java
@@ -15,41 +15,71 @@
  */
 package org.springframework.test.junit38;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.junit.Test;
+import org.springframework.beans.Employee;
+import org.springframework.beans.Pet;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.test.annotation.AfterTransaction;
 import org.springframework.test.annotation.BeforeTransaction;
 import org.springframework.test.annotation.ContextConfiguration;
+import org.springframework.test.annotation.NotTransactional;
 import org.springframework.test.jdbc.SimpleJdbcTestUtils;
 
 /**
- * Unit test for {@link AbstractTransactionalJUnit38SpringContextTests}.
+ * Combined unit test for {@link AbstractJUnit38SpringContextTests} and
+ * {@link AbstractTransactionalJUnit38SpringContextTests}.
  *
  * @author Sam Brannen
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * @since 2.1
  */
 @ContextConfiguration
-public class ConcreteTransactionalJUnit38SpringContextTests extends AbstractTransactionalJUnit38SpringContextTests {
+public class ConcreteTransactionalJUnit38SpringContextTests extends AbstractTransactionalJUnit38SpringContextTests
+		implements BeanNameAware, InitializingBean {
 
 	// ------------------------------------------------------------------------|
 	// --- CONSTANTS ----------------------------------------------------------|
 	// ------------------------------------------------------------------------|
 
-	protected static final String	BOB		= "bob";
+	protected static final String	BOB				= "bob";
 
-	protected static final String	JANE	= "jane";
+	protected static final String	JANE			= "jane";
 
-	protected static final String	SUE		= "sue";
+	protected static final String	SUE				= "sue";
 
-	protected static final String	LUKE	= "luke";
+	protected static final String	LUKE			= "luke";
 
-	protected static final String	LEIA	= "leia";
+	protected static final String	LEIA			= "leia";
 
-	protected static final String	YODA	= "yoda";
+	protected static final String	YODA			= "yoda";
+
+	// ------------------------------------------------------------------------|
+	// --- INSTANCE VARIABLES -------------------------------------------------|
+	// ------------------------------------------------------------------------|
+
+	private boolean					beanInitialized	= false;
+
+	private String					beanName		= "replace me with null";
+
+	private Employee				employee;
+
+	@Autowired
+	private Pet						pet;
+
+	@Autowired(required = false)
+	protected Long					nonrequiredLong;
+
+	@Resource()
+	protected String				foo;
+
+	protected String				bar;
 
 	// ------------------------------------------------------------------------|
 	// --- CONSTRUCTORS -------------------------------------------------------|
@@ -101,6 +131,94 @@ public class ConcreteTransactionalJUnit38SpringContextTests extends AbstractTran
 
 	// ------------------------------------------------------------------------|
 	// --- INSTANCE METHODS ---------------------------------------------------|
+	// ------------------------------------------------------------------------|
+
+	/**
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	public final void afterPropertiesSet() throws Exception {
+
+		this.beanInitialized = true;
+	}
+
+	/**
+	 * @see org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang.String)
+	 */
+	public final void setBeanName(final String beanName) {
+
+		this.beanName = beanName;
+	}
+
+	@Autowired
+	protected final void setEmployee(final Employee employee) {
+
+		this.employee = employee;
+	}
+
+	@Resource
+	protected final void setBar(final String bar) {
+
+		this.bar = bar;
+	}
+
+	// ------------------------------------------------------------------------|
+
+	@Test
+	@NotTransactional
+	public final void verifyApplicationContext() {
+
+		assertNotNull("The application context should have been set due to ApplicationContextAware semantics.",
+				getApplicationContext());
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyBeanInitialized() {
+
+		assertTrue("This test bean should have been initialized due to InitializingBean semantics.",
+				this.beanInitialized);
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyBeanNameSet() {
+
+		assertNull("The bean name of this test instance should have been set to NULL "
+				+ "due to BeanNameAware semantics, since the testing support classes "
+				+ "currently do not provide a bean name for dependency injected test instances.", this.beanName);
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyAnnotationAutowiredFields() {
+
+		assertNull("The nonrequiredLong property should NOT have been autowired.", this.nonrequiredLong);
+		assertNotNull("The pet field should have been autowired.", this.pet);
+		assertEquals("Fido", this.pet.getName());
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyAnnotationAutowiredMethods() {
+
+		assertNotNull("The employee setter method should have been autowired.", this.employee);
+		assertEquals("John Smith", this.employee.getName());
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyResourceAnnotationWiredFields() {
+
+		assertEquals("The foo field should have been wired via @Resource.", "Foo", this.foo);
+	}
+
+	@Test
+	@NotTransactional
+	public final void verifyResourceAnnotationWiredMethods() {
+
+		assertEquals("The bar method should have been wired via @Resource.", "Bar", this.bar);
+	}
+
 	// ------------------------------------------------------------------------|
 
 	@BeforeTransaction
