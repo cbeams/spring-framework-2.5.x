ae2d050ad0755888ca3f7f150bb4868737ff34d7
Thomas Risberg <trisberg@interface21.com>
2007-09-01 04:54:16 +0000

    moved SimpleJdbcInsert and SimpleJdbcCall from sandbox to tiger (SPR-2072)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14717 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

---
+++ tiger/test/org/springframework/jdbc/core/simple/SimpleJdbcUtilsTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.jdbc.core.simple;
+
+import junit.framework.TestCase;
+import org.springframework.jdbc.core.simple.SimpleJdbcUtils;
+import org.springframework.beans.BeanWrapperImpl;
+
+/**
+ * Unit tests for SimpleJdbcUtils.
+ *
+ * @author Thomas Risberg
+ */
+public class SimpleJdbcUtilsTests extends TestCase {
+
+	public void testcommonDatabaseName() {
+		assertEquals("Wrong db name", "Oracle", SimpleJdbcUtils.commonDatabaseName("Oracle"));
+		assertEquals("Wrong db name", "DB2", SimpleJdbcUtils.commonDatabaseName("DB2-for-Spring"));
+		assertEquals("Wrong db name", "Sybase", SimpleJdbcUtils.commonDatabaseName("Sybase SQL Server"));
+		assertEquals("Wrong db name", "Sybase", SimpleJdbcUtils.commonDatabaseName("Adaptive Server Enterprise"));
+		assertEquals("Wrong db name", "MySQL", SimpleJdbcUtils.commonDatabaseName("MySQL"));
+	}
+
+	public void testConvertUnderscoreNameToPropertyName() {
+		assertEquals("Wrong property name", "myName", SimpleJdbcUtils.convertUnderscoreNameToPropertyName("MY_NAME"));
+		assertEquals("Wrong property name", "yourName", SimpleJdbcUtils.convertUnderscoreNameToPropertyName("yOUR_nAME"));
+		assertEquals("Wrong property name", "AName", SimpleJdbcUtils.convertUnderscoreNameToPropertyName("a_name"));
+		assertEquals("Wrong property name", "someoneElsesName", SimpleJdbcUtils.convertUnderscoreNameToPropertyName("someone_elses_name"));
+	}
+
+}
