7ba72346aa9a4ba13acbf83212d6be5cce637bf8
Sam Brannen <sam@sambrannen.com>
2007-09-06 20:07:55 +0000

    Verifying the number of times the parameterized test method was executed.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14792 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/test/context/junit4/ParameterizedDependencyInjectionTests.java
+++ tiger/test/org/springframework/test/context/junit4/ParameterizedDependencyInjectionTests.java
@@ -19,12 +19,16 @@ package org.springframework.test.context.junit4;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import junit.framework.JUnit4TestAdapter;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -55,6 +59,8 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 @TestExecutionListeners( { DependencyInjectionTestExecutionListener.class })
 public class ParameterizedDependencyInjectionTests implements ApplicationContextAware {
 
+	private static final List<Employee> employees = new ArrayList<Employee>();
+
 	private ApplicationContext applicationContext;
 
 	private final String employeeBeanName;
@@ -84,6 +90,11 @@ public class ParameterizedDependencyInjectionTests implements ApplicationContext
 		return new JUnit4TestAdapter(ParameterizedDependencyInjectionTests.class);
 	}
 
+	@BeforeClass
+	public static void clearEmployees() {
+		employees.clear();
+	}
+
 	@Before
 	public void injectDependencies() throws Exception {
 
@@ -102,8 +113,15 @@ public class ParameterizedDependencyInjectionTests implements ApplicationContext
 
 		// Verifying 'parameterized' support:
 		final Employee employee = (Employee) this.applicationContext.getBean(this.employeeBeanName);
+		employees.add(employee);
 		assertEquals("Verifying the name of the employee configured as bean [" + this.employeeBeanName + "].",
 		        this.employeeName, employee.getName());
+	}
+
+	@AfterClass
+	public static void verifyNumParameterizedRuns() {
+		assertEquals("Verifying the number of times the parameterized test method was executed.",
+		        employeeData().size(), employees.size());
 	}
 
 }
