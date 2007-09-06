0666d06822ab9e24c110630040545a2b8aee825f
Sam Brannen <sam@sambrannen.com>
2007-09-06 11:03:41 +0000

    Fixed typo: underscores
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14777 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/support/JdbcUtils.java
+++ src/org/springframework/jdbc/support/JdbcUtils.java
@@ -331,7 +331,7 @@ public abstract class JdbcUtils {
 	}
 
 	/**
-	 * Convert a column name with undercores to the corresponding property name using "camel case".  A name
+	 * Convert a column name with underscores to the corresponding property name using "camel case".  A name
 	 * like "customer_number" would match a "customerNumber" property name.
 	 * @param name the column name to be converted
 	 * @return the name using "camel case"
@@ -340,10 +340,12 @@ public abstract class JdbcUtils {
 		StringBuffer result = new StringBuffer();
 		boolean nextIsUpper = false;
 		if (name != null && name.length() > 0) {
-			if (name.length() > 1 && name.substring(1,2).equals("_"))
+			if (name.length() > 1 && name.substring(1,2).equals("_")) {
 				result.append(name.substring(0, 1).toUpperCase());
-			else
+			}
+			else {
 				result.append(name.substring(0, 1).toLowerCase());
+			}
 			for (int i = 1; i < name.length(); i++) {
 				String s = name.substring(i, i + 1);
 				if (s.equals("_")) {
