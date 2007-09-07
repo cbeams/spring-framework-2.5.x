9caf2865e8fa3521abe91420183314a168bb9b8d
Sam Brannen <sam@sambrannen.com>
2007-09-07 15:09:27 +0000

    Introduced the new ModelAndViewAssert class. AbstractModelAndViewTests now delegates to ModelAndViewAssert as well.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@14809 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- mock/org/springframework/test/web/AbstractModelAndViewTests.java
+++ mock/org/springframework/test/web/AbstractModelAndViewTests.java
@@ -16,165 +16,163 @@
 
 package org.springframework.test.web;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Convenient base class for tests dealing with Spring web MVC
- * {@link org.springframework.web.servlet.ModelAndView} objects.
+ * <p>
+ * Convenient JUnit 3.8 base class for tests dealing with Spring web MVC
+ * {@link org.springframework.web.servlet.ModelAndView ModelAndView} objects.
+ * </p>
+ * <p>
+ * All <code>assert*()</code> methods throw {@link AssertionFailedError}s.
+ * </p>
  *
  * @author Alef Arendsen
  * @author Bram Smeets
+ * @author Sam Brannen
  * @since 2.0
  * @see org.springframework.web.servlet.ModelAndView
+ * @see ModelAndViewAssert
  */
 public abstract class AbstractModelAndViewTests extends TestCase {
-	
+
+	/**
+	 * Checks whether the model value under the given <code>modelName</code>
+	 * exists and checks it type, based on the <code>expectedType</code>. If
+	 * the model entry exists and the type matches, the model value is returned.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param modelName name of the object to add to the model (never
+	 *        <code>null</code>)
+	 * @param expectedType expected type of the model value
+	 * @return the model value
+	 */
+	protected Object assertAndReturnModelAttributeOfType(ModelAndView mav, Object modelName, Class expectedType) {
+
+		try {
+			return ModelAndViewAssert.assertAndReturnModelAttributeOfType(mav, modelName, expectedType);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
+	}
+
+	/**
+	 * Compare each individual entry in a list, without first sorting the lists.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param modelName name of the object to add to the model (never
+	 *        <code>null</code>)
+	 * @param expectedList the expected list
+	 */
+	protected void assertCompareListModelAttribute(ModelAndView mav, Object modelName, List expectedList) {
+
+		try {
+			ModelAndViewAssert.assertCompareListModelAttribute(mav, modelName, expectedList);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
+	}
+
 	/**
 	 * Assert whether or not a model attribute is available.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param modelName name of the object to add to the model (never
+	 *        <code>null</code>)
 	 */
-	protected void assertModelAttributeAvailable(ModelAndView mav, Object key) {
-		assertNotNull("Model is null", mav.getModel());
-		assertTrue("Model attribute with name '" + key + "' is not available",
-				mav.getModel().containsKey(key));
-	}	
-	
-	/**
-	 * Compare each individual entry in a list, not sorting the lists first.
-	 */
-	protected void assertCompareListModelAttribute(ModelAndView mav, Object key, List assertionList) {
-		List modelList = (List)assertAndReturnModelAttributeOfType(mav, key, List.class);
-		assertEquals("Size of model list is '" + modelList.size() +
-				"' while size of assertion list is '" + assertionList.size() + "'",
-				assertionList.size(), modelList.size());		
-		assertEquals("List in model under key '" + key + "' is not equals to given list", assertionList, modelList);
+	protected void assertModelAttributeAvailable(ModelAndView mav, Object modelName) {
+
+		try {
+			ModelAndViewAssert.assertModelAttributeAvailable(mav, modelName);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
 	}
-	
+
+	/**
+	 * Compare a given <code>expectedValue</code> to the value from the model
+	 * bound under the given <code>modelName</code>.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param modelName name of the object to add to the model (never
+	 *        <code>null</code>)
+	 * @param expectedValue the model value
+	 */
+	protected void assertModelAttributeValue(ModelAndView mav, Object modelName, Object expectedValue) {
+
+		try {
+			ModelAndViewAssert.assertModelAttributeValue(mav, modelName, expectedValue);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
+	}
+
+	/**
+	 * Inspect the <code>expectedModel</code> to see if all elements in the
+	 * model appear and are equal.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param expectedModel the expected model
+	 */
+	protected void assertModelAttributeValues(ModelAndView mav, Map expectedModel) {
+
+		try {
+			ModelAndViewAssert.assertModelAttributeValues(mav, expectedModel);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
+	}
+
 	/**
 	 * Compare each individual entry in a list after having sorted both lists
 	 * (optionally using a comparator).
-	 * @param comp the comparator to use. If not specifying the comparator,
-	 * both lists will be sorted not using any comparator.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param modelName name of the object to add to the model (never
+	 *        <code>null</code>)
+	 * @param expectedList the expected list
+	 * @param comparator the comparator to use (may be <code>null</code>). If
+	 *        not specifying the comparator, both lists will be sorted not using
+	 *        any comparator.
 	 */
-	protected void assertSortAndCompareListModelAttribute(
-			ModelAndView mav, Object key, List assertionList, Comparator comp) {
+	protected void assertSortAndCompareListModelAttribute(ModelAndView mav, Object modelName, List expectedList,
+			Comparator comparator) {
 
-		List modelList = (List)assertAndReturnModelAttributeOfType(mav, key, List.class);
-		assertEquals("Size of model list is '" + modelList.size() +
-				"' while size of assertion list is '" + assertionList.size() + "'",
-				assertionList.size(), modelList.size());
-
-		if (comp != null) {
-			Collections.sort(modelList, comp);
-			Collections.sort(assertionList, comp);
+		try {
+			ModelAndViewAssert.assertSortAndCompareListModelAttribute(mav, modelName, expectedList, comparator);
 		}
-		else {
-			Collections.sort(modelList);
-			Collections.sort(assertionList);
-		}		
-		assertEquals("List in model under key '" + key + "' is not equals to given list", assertionList, modelList);
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
+		}
 	}
-	
+
 	/**
-	 * Checks whether the given model key exists and checks it type, based
-	 * on the given type. If the model entry exists and the type matches,
-	 * the given model value is returned.
-	 */
-	protected Object assertAndReturnModelAttributeOfType(ModelAndView mav, Object key, Class type) {
-		assertNotNull("Model is null", mav.getModel());
-		assertNotNull("Model attribute with key '" + key + "' is null", mav.getModel().get(key));
-		Object obj = mav.getModel().get(key);
-		assertTrue("Model attribute is not of type '" + type.getName() + "' but is a '" +
-				obj.getClass().getName() + "'", type.isAssignableFrom(obj.getClass()));
-		return obj;
-	}
-	
-	/**
-	 * Check to see if the view name in the ModelAndView matches the given String.
+	 * Check to see if the view name in the ModelAndView matches the given
+	 * <code>expectedName</code>.
+	 *
+	 * @param mav ModelAndView to test against (never <code>null</code>)
+	 * @param expectedName the name of the model value
 	 */
 	protected void assertViewName(ModelAndView mav, String name) {
-		assertEquals("View name is not equal to '" + name + "' but was '" + mav.getViewName() + "'",
-				name, mav.getViewName());
-	}
-	
-	/**
-	 * Compare a given Object to the value from the model bound under the given key.
-	 */
-	protected void assertModelAttributeValue(ModelAndView mav, Object key, Object value) {
-		Object modelValue = assertAndReturnModelAttributeOfType(mav, key, Object.class);
-		assertEquals("Model value with key '" + key + "' is not the same as given value which was '" + value + "'", 
-				value, modelValue);				
-	}
-	
-	/**
-	 * Inspect the given model to see if all elements in the model appear and are equal
-	 */
-	protected void assertModelAttributeValues(ModelAndView mav, Map assertionModel) {
-		assertNotNull(mav.getModel());
-		
-		if (!mav.getModel().keySet().equals(assertionModel.keySet())) {
-			StringBuffer buf = new StringBuffer("Keyset of given model does not match.\n");
-			appendNonMatchingSetsErrorMessage(assertionModel.keySet(), mav.getModel().keySet(), buf);
-			fail(buf.toString());
-		}
-		
-		StringBuffer buf = new StringBuffer();
-		Iterator it = mav.getModel().keySet().iterator();
-		while (it.hasNext()) {
-			Object key = (Object) it.next();
-			Object assertionValue = assertionModel.get(key);
-			Object mavValue = mav.getModel().get(key);
-			if (!assertionValue.equals(mavValue)) {
-				buf.append("Value under key '" + key + "' differs, should have been '" +
-						assertionValue + "' but was '" + mavValue +"'\n");
-			}
-		}
 
-		if (buf.length() != 0) {
-			 buf.insert(0, "Values of given model do not match.\n");
-			 fail(buf.toString());
+		try {
+			ModelAndViewAssert.assertViewName(mav, name);
+		}
+		catch (AssertionError e) {
+			throw new AssertionFailedError(e.getMessage());
 		}
 	}
 
-	private void appendNonMatchingSetsErrorMessage(Set assertionSet, Set incorrectSet, StringBuffer buf) {
-		Set tempSet = new HashSet();
-		tempSet.addAll(incorrectSet);
-		tempSet.removeAll(assertionSet);
-		
-		if (tempSet.size() > 0) {
-			buf.append("Set has too many elements:\n");
-			Iterator it = tempSet.iterator();
-			while (it.hasNext()) {
-				Object o = (Object) it.next();
-				buf.append('-');
-				buf.append(o.toString());
-				buf.append('\n');
-			}
-		}
-		
-		tempSet = new HashSet();
-		tempSet.addAll(assertionSet);
-		tempSet.removeAll(incorrectSet);
-		
-		if (tempSet.size() > 0) {
-			buf.append("Set is missing elements:\n");
-			Iterator it = tempSet.iterator();
-			while (it.hasNext()) {
-				Object o = (Object) it.next();
-				buf.append('-');
-				buf.append(o.toString());
-				buf.append('\n');
-			}
-		}	 
-	}
-	
 }
