eff030caa4520b42729ee66abef60995e4ae6ca3
Juergen Hoeller <jhoeller@interface21.com>
2007-09-28 15:55:54 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@15021 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/util/UrlPathHelper.java
+++ src/org/springframework/web/util/UrlPathHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,15 +31,14 @@ import org.springframework.util.StringUtils;
  * Helper class for URL path matching. Provides support for URL paths in
  * RequestDispatcher includes and support for consistent URL decoding.
  *
- * <p>Used by AbstractUrlHandlerMapping, AbstractUrlMethodNameResolver
- * and RequestContext for path matching and/or URI determination.
+ * <p>Used by {@link org.springframework.web.servlet.handler.AbstractUrlHandlerMapping},
+ * {@link org.springframework.web.servlet.mvc.multiaction.AbstractUrlMethodNameResolver}
+ * and {@link org.springframework.web.servlet.support.RequestContext} for path matching
+ * and/or URI determination.
  *
  * @author Juergen Hoeller
  * @author Rob Harrop
  * @since 14.01.2004
- * @see org.springframework.web.servlet.handler.AbstractUrlHandlerMapping
- * @see org.springframework.web.servlet.mvc.multiaction.AbstractUrlMethodNameResolver
- * @see org.springframework.web.servlet.support.RequestContext
  */
 public class UrlPathHelper {
 
@@ -94,7 +93,7 @@ public class UrlPathHelper {
 	 * @see #getContextPath
 	 * @see #getRequestUri
 	 * @see WebUtils#DEFAULT_CHARACTER_ENCODING
-	 * @see javax.servlet.ServletRequest#getCharacterEncoding
+	 * @see javax.servlet.ServletRequest#getCharacterEncoding()
 	 * @see java.net.URLDecoder#decode(String, String)
 	 * @see java.net.URLDecoder#decode(String)
 	 */
@@ -108,11 +107,11 @@ public class UrlPathHelper {
 	 * <p>If the request specifies a character encoding itself, the request
 	 * encoding will override this setting. This also allows for generically
 	 * overriding the character encoding in a filter that invokes the
-	 * ServletRequest.setCharacterEncoding method.
+	 * <code>ServletRequest.setCharacterEncoding</code> method.
 	 * @param defaultEncoding the character encoding to use
 	 * @see #determineEncoding
-	 * @see javax.servlet.ServletRequest#getCharacterEncoding
-	 * @see javax.servlet.ServletRequest#setCharacterEncoding
+	 * @see javax.servlet.ServletRequest#getCharacterEncoding()
+	 * @see javax.servlet.ServletRequest#setCharacterEncoding(String)
 	 * @see WebUtils#DEFAULT_CHARACTER_ENCODING
 	 */
 	public void setDefaultEncoding(String defaultEncoding) {
@@ -236,8 +235,8 @@ public class UrlPathHelper {
 	/**
 	 * Return the servlet path for the given request, regarding an include request
 	 * URL if called within a RequestDispatcher include.
-	 * <p>As the value returned by request.getServletPath() is already decoded by
-	 * the servlet container, this method will not attempt to decode it.
+	 * <p>As the value returned by <code>request.getServletPath()</code> is already
+	 * decoded by the servlet container, this method will not attempt to decode it.
 	 * @param request current HTTP request
 	 * @return the servlet path
 	 */
@@ -254,7 +253,7 @@ public class UrlPathHelper {
 	 * Return the request URI for root of the given request. If this is a forwarded request,
 	 * correctly resolves to the request URI of the original request. Relies on the Servlet
 	 * 2.4 'forward' attributes. These attributes may be set by other components when
-	 * running in a Servlet 2.3- environment.
+	 * running in a Servlet 2.3 environment.
 	 */
 	public String getOriginatingRequestUri(HttpServletRequest request) {
 		String uri = (String) request.getAttribute(WebUtils.FORWARD_REQUEST_URI_ATTRIBUTE);
@@ -284,7 +283,7 @@ public class UrlPathHelper {
 	 * Return the request URI for root of the given request. If this is a forwarded request,
 	 * correctly resolves to the request URI of the original request. Relies on the Servlet
 	 * 2.4 'forward' attributes. These attributes may be set by other components when
-	 * running in a Servlet 2.3- environment.
+	 * running in a Servlet 2.3 environment.
 	 */
 	public String getOriginatingQueryString(HttpServletRequest request) {
 		String queryString = (String) request.getAttribute(WebUtils.FORWARD_QUERY_STRING_ATTRIBUTE);
@@ -314,7 +313,7 @@ public class UrlPathHelper {
 	 * @param source the String to decode
 	 * @return the decoded String
 	 * @see WebUtils#DEFAULT_CHARACTER_ENCODING
-	 * @see javax.servlet.ServletRequest#getCharacterEncoding
+	 * @see javax.servlet.ServletRequest#getCharacterEncoding()
 	 * @see java.net.URLDecoder#decode(String, String)
 	 * @see java.net.URLDecoder#decode(String)
 	 */
@@ -345,7 +344,7 @@ public class UrlPathHelper {
 	 * falling back to the default encoding specified for this resolver.
 	 * @param request current HTTP request
 	 * @return the encoding for the request (never <code>null</code>)
-	 * @see javax.servlet.ServletRequest#getCharacterEncoding
+	 * @see javax.servlet.ServletRequest#getCharacterEncoding()
 	 * @see #setDefaultEncoding
 	 */
 	protected String determineEncoding(HttpServletRequest request) {
