2bb5302f25c9b629005083710a402e020105337c
Ramnivas Laddad <rladdad@interface21.com>
2007-09-29 15:02:45 +0000

    Added a few more test pointcuts
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15029 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/aop/aspectj/BeanNamePointcutMatchingTests.java
+++ test/org/springframework/aop/aspectj/BeanNamePointcutMatchingTests.java
@@ -30,9 +30,16 @@ public class BeanNamePointcutMatchingTests extends TestCase {
 	public void testMatchingPointcuts() {
 		assertMatch("someName", "bean(someName)");
 
-		// Spring beans are less restrictive 
-		assertMatch("someName/someOtherName", "bean(someName/someOtherName)"); // MVC Controller-kind 
-		assertMatch("service:name=traceService", "bean(service:name=traceService)"); // JMX-kind
+		// Spring bean names are less restrictive compared to AspectJ names (methods, types etc.) 
+		// MVC Controller-kind 
+		assertMatch("someName/someOtherName", "bean(someName/someOtherName)");
+		assertMatch("someName/foo/someOtherName", "bean(someName/*/someOtherName)");
+		assertMatch("someName/foo/bar/someOtherName", "bean(someName/*/someOtherName)");
+		assertMatch("someName/*/**", "bean(someName/*)");
+		// JMX-kind
+		assertMatch("service:name=traceService", "bean(service:name=traceService)");
+		assertMatch("service:name=traceService", "bean(service:name=*)");
+		assertMatch("service:name=traceService", "bean(*:name=traceService)");
 
 		// Wildcards
 		assertMatch("someName", "bean(*someName)");
