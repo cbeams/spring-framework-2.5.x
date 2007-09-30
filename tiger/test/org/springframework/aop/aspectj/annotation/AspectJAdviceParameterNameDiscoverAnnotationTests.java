721d42543abcdd2786883d890b400dc464c7a51d
Juergen Hoeller <jhoeller@interface21.com>
2007-09-30 21:38:19 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15043 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/aop/aspectj/annotation/AspectJAdviceParameterNameDiscoverAnnotationTests.java
+++ tiger/test/org/springframework/aop/aspectj/annotation/AspectJAdviceParameterNameDiscoverAnnotationTests.java
@@ -24,10 +24,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.aop.aspectj.AspectJAdviceParameterNameDiscovererTests;
 
 /**
- * Additional parameter name discover tests that need Java5.
+ * Additional parameter name discover tests that need Java 5.
  * Yes this will re-run the tests from the superclass, but that
  * doesn't matter in the grand scheme of things...
-
+ *
  * @author Adrian Colyer
  */
 public class AspectJAdviceParameterNameDiscoverAnnotationTests extends AspectJAdviceParameterNameDiscovererTests {
