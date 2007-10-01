b1fe4530c505e35e33a390bbdac89913ba7d4143
Juergen Hoeller <jhoeller@interface21.com>
2007-10-01 17:24:37 +0000

    reduce JMX API dependency to ModelMBeanNotificationBroadcaster
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15069 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jmx/export/notification/ModelMBeanNotificationPublisher.java
+++ src/org/springframework/jmx/export/notification/ModelMBeanNotificationPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import javax.management.MBeanException;
 import javax.management.Notification;
 import javax.management.ObjectName;
 import javax.management.modelmbean.ModelMBean;
+import javax.management.modelmbean.ModelMBeanNotificationBroadcaster;
 
 import org.springframework.util.Assert;
 
@@ -31,9 +32,10 @@ import org.springframework.util.Assert;
  * and send {@link Notification Notifications} to those listeners.
  *
  * @author Rob Harrop
+ * @author Juergen Hoeller
  * @author Rick Evans
  * @since 2.0
- * @see ModelMBean
+ * @see javax.management.modelmbean.ModelMBeanNotificationBroadcaster
  * @see NotificationPublisherAware
  */
 public class ModelMBeanNotificationPublisher implements NotificationPublisher {
@@ -42,7 +44,7 @@ public class ModelMBeanNotificationPublisher implements NotificationPublisher {
 	 * The {@link ModelMBean} instance wrapping the managed resource into which this
 	 * <code>NotificationPublisher</code> will be injected.
 	 */
-	private final ModelMBean modelMBean;
+	private final ModelMBeanNotificationBroadcaster modelMBean;
 
 	/**
 	 * The {@link ObjectName} associated with the {@link ModelMBean modelMBean}.
@@ -56,7 +58,7 @@ public class ModelMBeanNotificationPublisher implements NotificationPublisher {
 
 
 	/**
-	 * Creates a new instance of the {@link ModelMBeanNotificationPublisher} class
+	 * Create a new instance of the {@link ModelMBeanNotificationPublisher} class
 	 * that will publish all {@link javax.management.Notification Notifications}
 	 * to the supplied {@link ModelMBean}.
 	 * @param modelMBean the target {@link ModelMBean}; must not be <code>null</code>
@@ -64,10 +66,12 @@ public class ModelMBeanNotificationPublisher implements NotificationPublisher {
 	 * @param managedResource the managed resource exposed by the supplied {@link ModelMBean}
 	 * @throws IllegalArgumentException if any of the parameters is <code>null</code> 
 	 */
-	public ModelMBeanNotificationPublisher(ModelMBean modelMBean, ObjectName objectName, Object managedResource) {
-		Assert.notNull(modelMBean, "The 'modelMBean' parameter must not be null.");
-		Assert.notNull(objectName, "The 'objectName' parameter must not be null.");
-		Assert.notNull(managedResource, "The 'managedResource' parameter must not be null.");
+	public ModelMBeanNotificationPublisher(
+			ModelMBeanNotificationBroadcaster modelMBean, ObjectName objectName, Object managedResource) {
+
+		Assert.notNull(modelMBean, "'modelMBean' must not be null");
+		Assert.notNull(objectName, "'objectName' must not be null");
+		Assert.notNull(managedResource, "'managedResource' must not be null");
 		this.modelMBean = modelMBean;
 		this.objectName = objectName;
 		this.managedResource = managedResource;
@@ -75,7 +79,7 @@ public class ModelMBeanNotificationPublisher implements NotificationPublisher {
 
 
 	/**
-	 * Sends the supplied {@link Notification} using the wrapped
+	 * Send the supplied {@link Notification} using the wrapped
 	 * {@link ModelMBean} instance.
 	 * @param notification the {@link Notification} to be sent
 	 * @throws IllegalArgumentException if the supplied <code>notification</code> is <code>null</code>
@@ -98,15 +102,14 @@ public class ModelMBeanNotificationPublisher implements NotificationPublisher {
 	}
 
 	/**
-	 * From the {@link Notification class level Javadoc}:
-	 * <p><i>'It is strongly recommended that notification senders use the object name
-	 * rather than a reference to the MBean object as the source.'</i>
-	 *
-	 * @param notification the {@link Notification} whose {@link javax.management.Notification#getSource()} might need massaging
+	 * From the {@link Notification javadoc}:
+	 * <p><i>"It is strongly recommended that notification senders use the object name
+	 * rather than a reference to the MBean object as the source."</i>
+	 * @param notification the {@link Notification} whose
+	 * {@link javax.management.Notification#getSource()} might need massaging
 	 */
 	private void replaceNotificationSourceIfNecessary(Notification notification) {
-		if (notification.getSource() == null
-				|| notification.getSource().equals(this.managedResource)) {
+		if (notification.getSource() == null || notification.getSource().equals(this.managedResource)) {
 			notification.setSource(this.objectName);
 		}
 	}
