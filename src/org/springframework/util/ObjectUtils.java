f707da44f591023750bffda06b69b8b207c0151e
Juergen Hoeller <jhoeller@interface21.com>
2007-10-01 11:13:31 +0000

    optimized nullSafeEquals/nullSafeHashCode through early isArray check
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15052 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/ObjectUtils.java
+++ src/org/springframework/util/ObjectUtils.java
@@ -170,32 +170,34 @@ public abstract class ObjectUtils {
 		if (o1.equals(o2)) {
 			return true;
 		}
-		if (o1 instanceof Object[] && o2 instanceof Object[]) {
-			return Arrays.equals((Object[]) o1, (Object[]) o2);
-		}
-		if (o1 instanceof boolean[] && o2 instanceof boolean[]) {
-			return Arrays.equals((boolean[]) o1, (boolean[]) o2);
-		}
-		if (o1 instanceof byte[] && o2 instanceof byte[]) {
-			return Arrays.equals((byte[]) o1, (byte[]) o2);
-		}
-		if (o1 instanceof char[] && o2 instanceof char[]) {
-			return Arrays.equals((char[]) o1, (char[]) o2);
-		}
-		if (o1 instanceof double[] && o2 instanceof double[]) {
-			return Arrays.equals((double[]) o1, (double[]) o2);
-		}
-		if (o1 instanceof float[] && o2 instanceof float[]) {
-			return Arrays.equals((float[]) o1, (float[]) o2);
-		}
-		if (o1 instanceof int[] && o2 instanceof int[]) {
-			return Arrays.equals((int[]) o1, (int[]) o2);
-		}
-		if (o1 instanceof long[] && o2 instanceof long[]) {
-			return Arrays.equals((long[]) o1, (long[]) o2);
-		}
-		if (o1 instanceof short[] && o2 instanceof short[]) {
-			return Arrays.equals((short[]) o1, (short[]) o2);
+		if (o1.getClass().isArray() && o2.getClass().isArray()) {
+			if (o1 instanceof Object[] && o2 instanceof Object[]) {
+				return Arrays.equals((Object[]) o1, (Object[]) o2);
+			}
+			if (o1 instanceof boolean[] && o2 instanceof boolean[]) {
+				return Arrays.equals((boolean[]) o1, (boolean[]) o2);
+			}
+			if (o1 instanceof byte[] && o2 instanceof byte[]) {
+				return Arrays.equals((byte[]) o1, (byte[]) o2);
+			}
+			if (o1 instanceof char[] && o2 instanceof char[]) {
+				return Arrays.equals((char[]) o1, (char[]) o2);
+			}
+			if (o1 instanceof double[] && o2 instanceof double[]) {
+				return Arrays.equals((double[]) o1, (double[]) o2);
+			}
+			if (o1 instanceof float[] && o2 instanceof float[]) {
+				return Arrays.equals((float[]) o1, (float[]) o2);
+			}
+			if (o1 instanceof int[] && o2 instanceof int[]) {
+				return Arrays.equals((int[]) o1, (int[]) o2);
+			}
+			if (o1 instanceof long[] && o2 instanceof long[]) {
+				return Arrays.equals((long[]) o1, (long[]) o2);
+			}
+			if (o1 instanceof short[] && o2 instanceof short[]) {
+				return Arrays.equals((short[]) o1, (short[]) o2);
+			}
 		}
 		return false;
 	}
@@ -220,32 +222,34 @@ public abstract class ObjectUtils {
 		if (obj == null) {
 			return 0;
 		}
-		if (obj instanceof Object[]) {
-			return nullSafeHashCode((Object[]) obj);
-		}
-		if (obj instanceof boolean[]) {
-			return nullSafeHashCode((boolean[]) obj);
-		}
-		if (obj instanceof byte[]) {
-			return nullSafeHashCode((byte[]) obj);
-		}
-		if (obj instanceof char[]) {
-			return nullSafeHashCode((char[]) obj);
-		}
-		if (obj instanceof double[]) {
-			return nullSafeHashCode((double[]) obj);
-		}
-		if (obj instanceof float[]) {
-			return nullSafeHashCode((float[]) obj);
-		}
-		if (obj instanceof int[]) {
-			return nullSafeHashCode((int[]) obj);
-		}
-		if (obj instanceof long[]) {
-			return nullSafeHashCode((long[]) obj);
-		}
-		if (obj instanceof short[]) {
-			return nullSafeHashCode((short[]) obj);
+		if (obj.getClass().isArray()) {
+			if (obj instanceof Object[]) {
+				return nullSafeHashCode((Object[]) obj);
+			}
+			if (obj instanceof boolean[]) {
+				return nullSafeHashCode((boolean[]) obj);
+			}
+			if (obj instanceof byte[]) {
+				return nullSafeHashCode((byte[]) obj);
+			}
+			if (obj instanceof char[]) {
+				return nullSafeHashCode((char[]) obj);
+			}
+			if (obj instanceof double[]) {
+				return nullSafeHashCode((double[]) obj);
+			}
+			if (obj instanceof float[]) {
+				return nullSafeHashCode((float[]) obj);
+			}
+			if (obj instanceof int[]) {
+				return nullSafeHashCode((int[]) obj);
+			}
+			if (obj instanceof long[]) {
+				return nullSafeHashCode((long[]) obj);
+			}
+			if (obj instanceof short[]) {
+				return nullSafeHashCode((short[]) obj);
+			}
 		}
 		return obj.hashCode();
 	}
