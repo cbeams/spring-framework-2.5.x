98547103dd06d711993145b95f16cc0c719f3423
Juergen Hoeller <jhoeller@interface21.com>
2007-10-01 17:26:17 +0000

    added comment on simple beans versus MBeans
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@15070 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jmx/export/notification/NotificationPublisherAware.java
+++ src/org/springframework/jmx/export/notification/NotificationPublisherAware.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,14 @@ package org.springframework.jmx.export.notification;
  *
  * <p>Provides Spring-created managed resources with a {@link NotificationPublisher}
  * as soon as they are registered with the {@link javax.management.MBeanServer}.
+ *
+ * <p><b>NOTE:</b> This interface only applies to simple Spring-managed
+ * beans which happen to get exported through Spring's
+ * {@link org.springframework.jmx.export.MBeanExporter}.
+ * It does not apply to any non-exported beans; neither does it apply
+ * to standard MBeans exported by Spring. For standard JMX MBeans,
+ * consider implementing the {@link javax.management.modelmbean.ModelMBeanNotificationBroadcaster}
+ * interface (or implementing a full {@link javax.management.modelmbean.ModelMBean}).
  *
  * @author Rob Harrop
  * @since 2.0
