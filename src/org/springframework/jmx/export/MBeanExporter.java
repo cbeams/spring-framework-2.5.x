4355e7cc07bea23ecae30c3cb718d1346ee0473f
Juergen Hoeller <jhoeller@interface21.com>
2007-10-04 21:34:32 +0000

    MBeanExporter applies NotificationPublisherAware callbacks to objects registered via "registerManagedResource" as well
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15086 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jmx/export/MBeanExporter.java
+++ src/org/springframework/jmx/export/MBeanExporter.java
@@ -431,15 +431,15 @@ public class MBeanExporter extends MBeanRegistrationSupport
 	public void registerManagedResource(Object managedResource, ObjectName objectName) throws MBeanExportException {
 		Assert.notNull(managedResource, "Managed resource must not be null");
 		Assert.notNull(objectName, "ObjectName must not be null");
-		Object mbean = null;
-		if (isMBean(managedResource.getClass())) {
-			mbean = managedResource;
-		}
-		else {
-			mbean = createAndConfigureMBean(managedResource, managedResource.getClass().getName());
-		}
 		try {
-			doRegister(mbean, objectName);
+			if (isMBean(managedResource.getClass())) {
+				doRegister(managedResource, objectName);
+			}
+			else {
+				ModelMBean mbean = createAndConfigureMBean(managedResource, managedResource.getClass().getName());
+				doRegister(mbean, objectName);
+				injectNotificationPublisherIfNecessary(managedResource, mbean, objectName);
+			}
 		}
 		catch (JMException ex) {
 			throw new UnableToRegisterMBeanException(
--- test/org/springframework/jmx/export/NotificationPublisherTests.java
+++ test/org/springframework/jmx/export/NotificationPublisherTests.java
@@ -60,6 +60,19 @@ public class NotificationPublisherTests extends AbstractMBeanServerTests {
 		assertEquals("Notification not sent", 1, listener.count);
 	}
 
+	public void testSimpleBeanRegisteredManually() throws Exception {
+		// start the MBeanExporter
+		ConfigurableApplicationContext ctx = loadContext("org/springframework/jmx/export/notificationPublisherTests.xml");
+		MBeanExporter exporter = (MBeanExporter) ctx.getBean("exporter");
+		MyNotificationPublisher publisher = new MyNotificationPublisher();
+		exporter.registerManagedResource(publisher, ObjectNameManager.getInstance("spring:type=Publisher2"));
+		this.server.addNotificationListener(ObjectNameManager.getInstance("spring:type=Publisher2"), listener, null, null);
+
+		assertNotNull("NotificationPublisher should not be null", publisher.getNotificationPublisher());
+		publisher.sendNotification();
+		assertEquals("Notification not sent", 1, listener.count);
+	}
+
 	public void testMBean() throws Exception {
 		// start the MBeanExporter
 		ConfigurableApplicationContext ctx = loadContext("org/springframework/jmx/export/notificationPublisherTests.xml");
