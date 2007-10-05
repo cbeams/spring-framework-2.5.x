abfdbdb71820d82f2ce107500791ca397470be99
Juergen Hoeller <jhoeller@interface21.com>
2007-10-05 19:54:22 +0000

    EJB support tests only run on JDK 1.5+ due to the EJB API being version 3.0 in the classpath now
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15095 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBeanTests.java
+++ tiger/test/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,6 @@ public class LocalStatelessSessionProxyFactoryBeanTests extends TestCase {
 		mc.verify();	
 		ec.verify();	
 	}
-	
 	
 	public void testCreateException() throws Exception {
 		final String jndiName = "foo";
--- tiger/test/org/springframework/ejb/support/EjbSupportTests.java
+++ tiger/test/org/springframework/ejb/support/EjbSupportTests.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,10 +38,8 @@ import org.springframework.beans.factory.support.StaticListableBeanFactory;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
 /**
- * Most of the value of the tests here is in being forced
- * to implement ejbCreate() methods.
- *
  * @author Rod Johnson
+ * @author Juergen Hoeller
  * @since 21.05.2003
  */
 public class EjbSupportTests extends TestCase {
@@ -161,7 +159,6 @@ public class EjbSupportTests extends TestCase {
 			// Ok
 		}
 	}
-
 
 	public void testJmsMdb() throws Exception {
 		MockControl mc = MockControl.createControl(MessageDrivenContext.class);
