ce2de2abdea4d6be06ed045cf8e711f9004607d2
Juergen Hoeller <jhoeller@interface21.com>
2007-10-11 14:55:43 +0000

    DefaultListableBeanFactory resets the caches for all derived beans as well if a bean definition gets replaced
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15148 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
+++ src/org/springframework/beans/factory/support/DefaultListableBeanFactory.java
@@ -432,15 +432,7 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
 			}
 			this.beanDefinitionMap.put(beanName, beanDefinition);
 
-			// Remove the merged bean definition for the given bean, if already created.
-			clearMergedBeanDefinition(beanName);
-
-			// Remove corresponding bean from singleton cache, if any. Shouldn't usually
-			// be necessary, rather just meant for overriding a context's default beans
-			// (e.g. the default StaticMessageSource in a StaticApplicationContext).
-			synchronized (getSingletonMutex()) {
-				destroySingleton(beanName);
-			}
+			resetBeanDefinition(beanName);
 		}
 	}
 
@@ -457,14 +449,35 @@ public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFacto
 			}
 			this.beanDefinitionNames.remove(beanName);
 
-			// Remove the merged bean definition for the given bean, if already created.
-			clearMergedBeanDefinition(beanName);
+			resetBeanDefinition(beanName);
+		}
+	}
 
-			// Remove corresponding bean from singleton cache, if any. Shouldn't usually
-			// be necessary, rather just meant for overriding a context's default beans
-			// (e.g. the default StaticMessageSource in a StaticApplicationContext).
-			synchronized (getSingletonMutex()) {
-				destroySingleton(beanName);
+	/**
+	 * Reset all bean definition caches for the given bean,
+	 * including the caches of beans that are derived from it.
+	 * @param beanName the name of the bean to reset
+	 */
+	protected void resetBeanDefinition(String beanName) {
+		// Remove the merged bean definition for the given bean, if already created.
+		clearMergedBeanDefinition(beanName);
+
+		// Remove corresponding bean from singleton cache, if any. Shouldn't usually
+		// be necessary, rather just meant for overriding a context's default beans
+		// (e.g. the default StaticMessageSource in a StaticApplicationContext).
+		synchronized (getSingletonMutex()) {
+			destroySingleton(beanName);
+		}
+
+		// Reset all bean definitions that have the given bean as parent
+		// (recursively).
+		for (Iterator it = this.beanDefinitionNames.iterator(); it.hasNext();) {
+			String bdName = (String) it.next();
+			if (!beanName.equals(bdName)) {
+				BeanDefinition bd = (BeanDefinition) this.beanDefinitionMap.get(bdName);
+				if (beanName.equals(bd.getParentName())) {
+					resetBeanDefinition(bdName);
+				}
 			}
 		}
 	}
