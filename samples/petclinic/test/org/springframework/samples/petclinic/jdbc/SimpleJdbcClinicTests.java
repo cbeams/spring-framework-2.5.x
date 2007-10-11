3738013079b946d04447b3d11ab5d336d280858e
Sam Brannen <sam@sambrannen.com>
2007-10-11 21:30:12 +0000

    Updated Javadoc and removed suite() since we've upgraded to Ant 1.7 which provides explicit JUnit 4 support.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15160 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/petclinic/test/org/springframework/samples/petclinic/jdbc/SimpleJdbcClinicTests.java
+++ samples/petclinic/test/org/springframework/samples/petclinic/jdbc/SimpleJdbcClinicTests.java
@@ -1,27 +1,19 @@
 
 package org.springframework.samples.petclinic.jdbc;
 
-import junit.framework.JUnit4TestAdapter;
-
 import org.springframework.samples.petclinic.AbstractClinicTests;
 import org.springframework.test.context.ContextConfiguration;
 
 /**
  * <p>
- * Integration tests for the {@link HsqlJdbcClinic} implementation.
+ * Integration tests for the {@link SimpleJdbcClinic} implementation.
  * </p>
  * <p>
- * "JdbcClinicTests-context.xml" determines the actual beans to test.
+ * "SimpleJdbcClinicTests-context.xml" determines the actual beans to test.
  * </p>
  *
- * @author Juergen Hoeller
- * @author Sam Brannen
+ * @author Thomas Risberg
  */
 @ContextConfiguration
 public class SimpleJdbcClinicTests extends AbstractClinicTests {
-
-	// XXX Remove suite() once we've migrated to Ant 1.7 with JUnit 4 support.
-	public static junit.framework.Test suite() {
-		return new JUnit4TestAdapter(SimpleJdbcClinicTests.class);
-	}
 }
