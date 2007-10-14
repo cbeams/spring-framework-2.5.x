ff5c97218cb1a5b9a3312d60efd957215a5466bf
Sam Brannen <sam@sambrannen.com>
2007-10-14 14:07:17 +0000

    Refactored to use @RunWith(Parameterized.class).
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15206 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/test/context/junit4/RepeatedSpringMethodRoadieTests.java
+++ tiger/test/org/springframework/test/context/junit4/RepeatedSpringMethodRoadieTests.java
@@ -19,17 +19,20 @@ package org.springframework.test.context.junit4;
 import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.easymock.MockControl;
 import org.easymock.classextension.MockClassControl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.internal.runners.TestClass;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import org.springframework.test.annotation.Repeat;
 import org.springframework.test.annotation.Timed;
@@ -42,46 +45,50 @@ import org.springframework.test.context.TestContextManager;
  * @author Sam Brannen
  * @since 2.5
  */
-@RunWith(JUnit4ClassRunner.class)
+@RunWith(Parameterized.class)
 public class RepeatedSpringMethodRoadieTests {
 
-	protected final MockControl notifierMockControl = MockClassControl.createNiceControl(RunNotifier.class);
+	protected final String methodName;
+	protected final int expectedNumInvocations;
 
+	protected final MockControl notifierMockControl = MockClassControl.createNiceControl(RunNotifier.class);
 	protected final RunNotifier notifier = (RunNotifier) this.notifierMockControl.getMock();
 
 	protected final MockControl descriptionMockControl = MockClassControl.createNiceControl(Description.class);
-
 	protected final Description description = (Description) this.descriptionMockControl.getMock();
 
 	protected final MockControl testContextManagerMockControl = MockClassControl.createNiceControl(TestContextManager.class);
-
 	protected final TestContextManager testContextManager = (TestContextManager) this.testContextManagerMockControl.getMock();
 
 
-	private void assertRepetitions(final String methodName, final int expectedNumInvocations) throws Exception {
+	public RepeatedSpringMethodRoadieTests(final String methodName, final int expectedNumInvocations) throws Exception {
+		this.methodName = methodName;
+		this.expectedNumInvocations = expectedNumInvocations;
+	}
+
+	@Parameters
+	public static Collection<Object[]> repetitionData() {
+		return Arrays.asList(new Object[][] { { "testNonAnnotated", 1 }, { "testNegativeRepeatValue", 1 },
+			{ "testDefaultRepeatValue", 1 }, { "testRepeatedFiveTimes", 5 } });
+	}
+
+	@Test
+	public void assertRepetitions() throws Exception {
 
 		final Class<RepeatedTestCase> clazz = RepeatedTestCase.class;
 		final TestClass testClass = new TestClass(clazz);
 		final RepeatedTestCase testInstance = clazz.newInstance();
-		final Method method = clazz.getMethod(methodName, (Class[]) null);
+		final Method method = clazz.getMethod(this.methodName, (Class[]) null);
 		final SpringTestMethod testMethod = new SpringTestMethod(method, testClass);
 
 		new SpringMethodRoadie(this.testContextManager, testInstance, testMethod, this.notifier, this.description).run();
 
-		assertEquals("Verifying number of @Test invocations for test method [" + methodName + "].",
-				expectedNumInvocations, testInstance.invocationCount);
-		assertEquals("Verifying number of @Before invocations for test method [" + methodName + "].",
-				expectedNumInvocations, testInstance.beforeCount);
-		assertEquals("Verifying number of @After invocations for test method [" + methodName + "].",
-				expectedNumInvocations, testInstance.afterCount);
-	}
-
-	@Test
-	public void testRepeatAnnotationSupport() throws Exception {
-		assertRepetitions("testNonAnnotated", 1);
-		assertRepetitions("testNegativeRepeatValue", 1);
-		assertRepetitions("testDefaultRepeatValue", 1);
-		assertRepetitions("testRepeatedFiveTimes", 5);
+		assertEquals("Verifying number of @Test invocations for test method [" + this.methodName + "].",
+				this.expectedNumInvocations, testInstance.invocationCount);
+		assertEquals("Verifying number of @Before invocations for test method [" + this.methodName + "].",
+				this.expectedNumInvocations, testInstance.beforeCount);
+		assertEquals("Verifying number of @After invocations for test method [" + this.methodName + "].",
+				this.expectedNumInvocations, testInstance.afterCount);
 	}
 
 
