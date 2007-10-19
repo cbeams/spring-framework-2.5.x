1a58b18f4f25800e9e44894330422d9af0e8607c
Sam Brannen <sam@sambrannen.com>
2007-10-19 14:54:24 +0000

    Updated comments to reflect recent changes to the PetClinic implementation and configuration.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15250 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/petclinic/src/org/springframework/samples/petclinic/web/ClinicController.java
+++ samples/petclinic/src/org/springframework/samples/petclinic/web/ClinicController.java
@@ -1,3 +1,4 @@
+
 package org.springframework.samples.petclinic.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * <code>MultiActionController</code> that handles all non-form URL's.
+ * Annotation-driven <em>MultiActionController</em> that handles all non-form
+ * URL's.
  *
  * @author Juergen Hoeller
  * @author Mark Fisher
@@ -19,6 +21,7 @@ public class ClinicController {
 
 	private final Clinic clinic;
 
+
 	@Autowired
 	public ClinicController(Clinic clinic) {
 		this.clinic = clinic;
@@ -27,21 +30,21 @@ public class ClinicController {
 	/**
 	 * Custom handler for the welcome view.
 	 * <p>
-	 * Note that this handler relies on the RequestToViewNameTranslator to come up
-	 * with a view name based on the request URL: "/welcome.do" -> "welcome".
+	 * Note that this handler relies on the RequestToViewNameTranslator to
+	 * determine the logical view name based on the request URL: "/welcome.do" -&gt;
+	 * "welcome".
 	 */
 	@RequestMapping("/welcome.do")
 	public void welcomeHandler() {
 	}
 
 	/**
-	 * Custom handler for vets display.
+	 * Custom handler for displaying vets.
 	 * <p>
-	 * Note that this handler returns a plain ModelMap object instead of a
-	 * ModelAndView, also leveraging convention-based model attribute names. It
-	 * relies on the RequestToViewNameTranslator to come up with a view name
-	 * based on the request URL: "/vetsl" -> "vets", plus configured "View"
-	 * suffix -> "vetsView".
+	 * Note that this handler returns a plain {@link ModelMap} object instead of
+	 * a ModelAndView, thus leveraging convention-based model attribute names.
+	 * It relies on the RequestToViewNameTranslator to determine the logical
+	 * view name based on the request URL: "/vets.do" -&gt; "vets".
 	 *
 	 * @return a ModelMap with the model attributes for the view
 	 */
@@ -51,19 +54,19 @@ public class ClinicController {
 	}
 
 	/**
-	 * Custom handler for owner display.
+	 * Custom handler for displaying an owner.
 	 * <p>
-	 * Note that this handler usually returns a ModelAndView object without
-	 * view, also leveraging convention-based model attribute names. It relies
-	 * on the RequestToViewNameTranslator to come up with a view name based on
-	 * the request URL: "/ownerl" -> "owner", plus configured "View" suffix ->
-	 * "ownerView".
+	 * Note that this handler returns a plain {@link ModelMap} object instead of
+	 * a ModelAndView, thus leveraging convention-based model attribute names.
+	 * It relies on the RequestToViewNameTranslator to determine the logical
+	 * view name based on the request URL: "/owner.do" -&gt; "owner".
 	 *
-	 * @param ownerId the id of the owner to show
+	 * @param ownerId the ID of the owner to display
 	 * @return a ModelMap with the model attributes for the view
 	 */
 	@RequestMapping("/owner.do")
-	public ModelMap ownerHandler(@RequestParam("ownerId") int ownerId) {
+	public ModelMap ownerHandler(@RequestParam("ownerId")
+	int ownerId) {
 		return new ModelMap(this.clinic.loadOwner(ownerId));
 	}
 
