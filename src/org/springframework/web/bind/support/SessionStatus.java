3b556adf3f11a81b9fa1434c8da287eb5260a386
Sam Brannen <sam@sambrannen.com>
2007-10-31 11:22:33 +0000

    Javadoc: corrected grammar.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15341 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/bind/support/SessionStatus.java
+++ src/org/springframework/web/bind/support/SessionStatus.java
@@ -17,12 +17,13 @@
 package org.springframework.web.bind.support;
 
 /**
- * Simple interface that can be injected into handler methods,
- * allowing them to signal that their session processing is complete.
- * The handler invoker may then follow up with appropriate cleanup,
- * e.g. of session attributes which have been implicit created
- * during this handler's processing (according to the
- * {@link org.springframework.web.bind.annotation.SessionAttributes} annotation).
+ * Simple interface that can be injected into handler methods, allowing them to
+ * signal that their session processing is complete. The handler invoker may
+ * then follow up with appropriate cleanup, e.g. of session attributes which
+ * have been implicitly created during this handler's processing (according to
+ * the
+ * {@link org.springframework.web.bind.annotation.SessionAttributes @SessionAttributes}
+ * annotation).
  *
  * @author Juergen Hoeller
  * @since 2.5
@@ -32,14 +33,14 @@ package org.springframework.web.bind.support;
 public interface SessionStatus {
 
 	/**
-	 * Mark the current handler's session processing as complete,
-	 * allowing for cleanup of session attributes.
+	 * Mark the current handler's session processing as complete, allowing for
+	 * cleanup of session attributes.
 	 */
 	void setComplete();
 
 	/**
-	 * Return whether the current handler's session processing
-	 * has been marked as complete.
+	 * Return whether the current handler's session processing has been marked
+	 * as complete.
 	 */
 	boolean isComplete();
 
