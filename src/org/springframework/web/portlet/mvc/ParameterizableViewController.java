aab84e3a59efde8beef2f8f72db6dd07f70c7cb4
Juergen Hoeller <jhoeller@interface21.com>
2007-11-16 12:38:19 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@15517 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/portlet/mvc/ParameterizableViewController.java
+++ src/org/springframework/web/portlet/mvc/ParameterizableViewController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import org.springframework.web.portlet.ModelAndView;
  * controller offers an alternative to sending a request straight to a view
  * such as a JSP. The advantage here is that the client is not exposed to
  * the concrete view technology but rather just to the controller URL;
- * the concrete view will be determined by the ViewResolver.
+ * the concrete view will be determined by the ViewResolver.</p>
  *
  * <p><b><a name="workflow">Workflow
  * (<a href="AbstractController.html#workflow">and that defined by superclass</a>):</b><br>
@@ -78,12 +78,12 @@ public class ParameterizableViewController extends AbstractController {
 	 * Return the name of the view to delegate to.
 	 */
 	public String getViewName() {
-		return viewName;
+		return this.viewName;
 	}
 
 	protected void initApplicationContext() {
 		if (this.viewName == null) {
-			throw new IllegalArgumentException("viewName is required");
+			throw new IllegalArgumentException("Property 'viewName' is required");
 		}
 	}
 
