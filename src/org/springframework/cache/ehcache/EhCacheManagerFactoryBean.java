b75b53912a1ab8c8610c43a2cb76f95005ef3a1f
Juergen Hoeller <jhoeller@interface21.com>
2007-11-27 21:12:22 +0000

    added "cacheManagerName" property to EhCacheManagerFactoryBean
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15626 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/cache/ehcache/EhCacheManagerFactoryBean.java
+++ src/org/springframework/cache/ehcache/EhCacheManagerFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,9 +42,7 @@ import org.springframework.core.io.Resource;
  * also necessary for loading EHCache configuration from a non-default config location.
  *
  * <p>Note: As of Spring 2.0, this FactoryBean will by default create an independent
- * CacheManager instance, which requires EHCache 1.2 or higher. Set the "shared"
- * flag to "true" to create a CacheManager instance that is shared at the VM level
- * (which is also compatible with EHCache 1.1).
+ * CacheManager instance, which requires EHCache 1.2 or higher.
  *
  * @author Dmitriy Kopylenko
  * @author Juergen Hoeller
@@ -62,6 +60,8 @@ public class EhCacheManagerFactoryBean implements FactoryBean, InitializingBean,
 
 	private boolean shared = false;
 
+	private String cacheManagerName;
+
 	private CacheManager cacheManager;
 
 
@@ -69,6 +69,8 @@ public class EhCacheManagerFactoryBean implements FactoryBean, InitializingBean,
 	 * Set the location of the EHCache config file. A typical value is "/WEB-INF/ehcache.xml".
 	 * <p>Default is "ehcache.xml" in the root of the class path, or if not found,
 	 * "ehcache-failsafe.xml" in the EHCache jar (default EHCache initialization).
+	 * @see net.sf.ehcache.CacheManager#create(java.io.InputStream)
+	 * @see net.sf.ehcache.CacheManager#CacheManager(java.io.InputStream)
 	 */
 	public void setConfigLocation(Resource configLocation) {
 		this.configLocation = configLocation;
@@ -78,11 +80,19 @@ public class EhCacheManagerFactoryBean implements FactoryBean, InitializingBean,
 	 * Set whether the EHCache CacheManager should be shared (as a singleton at the VM level)
 	 * or independent (typically local within the application). Default is "false", creating
 	 * an independent instance.
-	 * <p>Note that independent CacheManager instances are only available on EHCache 1.2 and
-	 * higher. Switch this flag to "true" if you intend to run against an EHCache 1.1 jar.
+	 * @see net.sf.ehcache.CacheManager#create()
+	 * @see net.sf.ehcache.CacheManager#CacheManager()
 	 */
 	public void setShared(boolean shared) {
 		this.shared = shared;
+	}
+
+	/**
+	 * Set the name of the EHCache CacheManager (if a specific name is desired).
+	 * @see net.sf.ehcache.CacheManager#setName(String)
+	 */
+	public void setCacheManagerName(String cacheManagerName) {
+		this.cacheManagerName = cacheManagerName;
 	}
 
 
@@ -105,6 +115,9 @@ public class EhCacheManagerFactoryBean implements FactoryBean, InitializingBean,
 			else {
 				this.cacheManager = new CacheManager();
 			}
+		}
+		if (this.cacheManagerName != null) {
+			this.cacheManager.setName(this.cacheManagerName);
 		}
 	}
 
