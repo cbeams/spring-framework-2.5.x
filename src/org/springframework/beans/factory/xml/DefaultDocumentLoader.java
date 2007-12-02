9c0520085e78fe483f4bbc674a65eb84cab456b3
Juergen Hoeller <jhoeller@interface21.com>
2007-12-02 23:19:29 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@15686 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/xml/DefaultDocumentLoader.java
+++ src/org/springframework/beans/factory/xml/DefaultDocumentLoader.java
@@ -32,9 +32,9 @@ import org.xml.sax.InputSource;
  *
  * <p>Simply loads {@link Document documents} using the standard JAXP-configured
  * XML parser. If you want to change the {@link DocumentBuilder} that is used to
- * load documents, then one strategy is to define a corresponding Java system
- * property when starting your application. For example, to use the Oracle
- * {@link DocumentBuilder}, one might start one's application like as follows:
+ * load documents, then one strategy is to define a corresponding Java system property
+ * when starting your JVM. For example, to use the Oracle {@link DocumentBuilder},
+ * you might start your application like as follows:
  *
  * <pre code="class">java -Djavax.xml.parsers.DocumentBuilderFactory=oracle.xml.jaxp.JXDocumentBuilderFactory MyMainClass</pre>
  *
@@ -81,6 +81,7 @@ public class DefaultDocumentLoader implements DocumentLoader {
 	 * @param validationMode the type of validation: {@link XmlBeanDefinitionReader#VALIDATION_NONE none},
 	 * {@link XmlBeanDefinitionReader#VALIDATION_DTD DTD} or {@link XmlBeanDefinitionReader#VALIDATION_XSD XSD}
 	 * @param namespaceAware <code>true</code> if the returned factory is to provide support for XML namespaces
+	 * @return the JAXP DocumentBuilderFactory
 	 * @throws ParserConfigurationException if we failed to build a proper DocumentBuilderFactory
 	 */
 	protected DocumentBuilderFactory createDocumentBuilderFactory(int validationMode, boolean namespaceAware)
@@ -117,6 +118,8 @@ public class DefaultDocumentLoader implements DocumentLoader {
 	 * adding further initialization of the builder.
 	 * @param factory the JAXP DocumentBuilderFactory that the DocumentBuilder
 	 * should be created with
+	 * @param entityResolver the SAX EntityResolver to use
+	 * @param errorHandler the SAX ErrorHandler to use
 	 * @return the JAXP DocumentBuilder
 	 * @throws ParserConfigurationException if thrown by JAXP methods
 	 */
