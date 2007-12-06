42b14b7e5b633925964a4349b41e1609fd2a49cb
Juergen Hoeller <jhoeller@interface21.com>
2007-12-06 13:09:42 +0000

    moved SimpleHttpServerFactoryBean to the support root package
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15712 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/remoting/support/SimpleHttpServerFactoryBean.java
+++ tiger/src/org/springframework/remoting/support/SimpleHttpServerFactoryBean.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.remoting.support.server;
+package org.springframework.remoting.support;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
