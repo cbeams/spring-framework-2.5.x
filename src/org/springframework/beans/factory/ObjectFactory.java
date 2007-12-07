4656626258cd2c9bfe0a1f82b21f1da807ffca29
Juergen Hoeller <jhoeller@interface21.com>
2007-12-07 11:53:35 +0000

    revised javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@15733 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/ObjectFactory.java
+++ src/org/springframework/beans/factory/ObjectFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2006 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,18 +21,18 @@ import org.springframework.beans.BeansException;
 /**
  * Defines a factory which can return an Object instance
  * (possibly shared or independent) when invoked.
- * 
+ *
  * <p>This interface is typically used to encapsulate a generic factory which
  * returns a new instance (prototype) of some target object on each invocation.
  *
- * <p>This interface is similar to FactoryBean, but implementations of the latter
- * are normally meant to be defined as instances by the user in a BeanFactory,
- * while implementations of this class are normally meant to be fed as a property
- * to other beans. As such, the <code>getObject</code> method has different
- * exception handling behavior.
- * 
+ * <p>This interface is similar to {@link FactoryBean}, but implementations
+ * of the latter are normally meant to be defined as SPI instances in a
+ * {@link BeanFactory}, while implementations of this class are normally meant
+ * to be fed as an API to other beans (through injection). As such, the
+ * <code>getObject()</code> method has different exception handling behavior.
+ *
  * @author Colin Sampaleanu
- * @since 11.05.2004
+ * @since 1.0.2
  * @see FactoryBean
  */
 public interface ObjectFactory {
