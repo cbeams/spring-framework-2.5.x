0bb1117181fab2d312e3155f8394bb2ee392d1f8
Juergen Hoeller <jhoeller@interface21.com>
2007-12-07 11:45:14 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15731 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/ObjectFactory.java
+++ src/org/springframework/beans/factory/ObjectFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2006 the original author or authors.
- * 
+ * Copyright 2002-2007 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,7 +32,7 @@ import org.springframework.beans.BeansException;
  * exception handling behavior.
  * 
  * @author Colin Sampaleanu
- * @since 11.05.2004
+ * @since 1.0.2
  * @see FactoryBean
  */
 public interface ObjectFactory {
