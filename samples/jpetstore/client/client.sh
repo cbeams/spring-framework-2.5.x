c71367eb8789fbd6cc42bd069d5aecd226642901
Juergen Hoeller <jhoeller@interface21.com>
2007-12-31 14:35:10 +0000

    updated classpath for new Hessian jar
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15844 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- samples/jpetstore/client/client.sh
+++ samples/jpetstore/client/client.sh
@@ -5,8 +5,7 @@ CLIENT_HOME=`dirname $0`
 
 CPATH=$CLIENT_HOME/../../../lib/axis/axis.jar:$CPATH
 CPATH=$CLIENT_HOME/../../../lib/axis/wsdl4j.jar:$CPATH
-CPATH=$CLIENT_HOME/../../../lib/caucho/hessian-2.1.12.jar:$CPATH
-CPATH=$CLIENT_HOME/../../../lib/caucho/burlap-2.1.12.jar:$CPATH
+CPATH=$CLIENT_HOME/../../../lib/caucho/hessian-3.1.3.jar:$CPATH
 CPATH=$CLIENT_HOME/../../../lib/j2ee/jaxrpc.jar:$CPATH
 CPATH=$CLIENT_HOME/../../../lib/jakarta-commons/commons-collections.jar:$CPATH
 CPATH=$CLIENT_HOME/../../../lib/jakarta-commons/commons-discovery.jar:$CPATH
