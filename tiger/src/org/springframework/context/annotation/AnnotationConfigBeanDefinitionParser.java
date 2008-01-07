fc68a4cbe2c04fe55b381f0d9737088ea31312fe
Christian Dupuis <christian.dupuis@springsource.com>
2008-01-07 00:19:31 +0000

    annotation config processor bean components are now nested within a CompositeComponentDefinition for the root context:annotation-config element to make the origin of the processor beans more obvious in tools
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15903 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/context/annotation/AnnotationConfigBeanDefinitionParser.java
+++ tiger/src/org/springframework/context/annotation/AnnotationConfigBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,30 +18,42 @@ package org.springframework.context.annotation;
 
 import java.util.Set;
 
-import org.w3c.dom.Element;
-
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
+import org.springframework.beans.factory.parsing.CompositeComponentDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
 
 /**
  * Parser for the &lt;context:annotation-config/&gt; element.
  *
  * @author Mark Fisher
  * @author Juergen Hoeller
+ * @author Christian Dupuis
  * @since 2.5
  * @see AnnotationConfigUtils
  */
 public class AnnotationConfigBeanDefinitionParser implements BeanDefinitionParser {
 
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		Object source = parserContext.extractSource(element);
 		Set<BeanDefinitionHolder> processorDefinitions = AnnotationConfigUtils.registerAnnotationConfigProcessors(
-				parserContext.getRegistry(), parserContext.extractSource(element));
+				parserContext.getRegistry(), source);
+
+		// register component for the surrounding context:annotation-config element 
+		CompositeComponentDefinition compDefinition = new CompositeComponentDefinition(element.getTagName(), source);
+		parserContext.pushContainingComponent(compDefinition);
+		
+		// nest the concrete beans in the surrounding component 
 		for (BeanDefinitionHolder processorDefinition : processorDefinitions) {
 			parserContext.registerComponent(new BeanComponentDefinition(processorDefinition));
 		}
+		
+		// finally register the composite component
+		parserContext.popAndRegisterContainingComponent();
+		
 		return null;
 	}
 
