26bc7de707bf7e0a87759de537a03816095e34ec
Juergen Hoeller <jhoeller@interface21.com>
2008-01-08 00:20:29 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15915 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/context/annotation/AnnotationConfigBeanDefinitionParser.java
+++ tiger/src/org/springframework/context/annotation/AnnotationConfigBeanDefinitionParser.java
@@ -18,13 +18,14 @@ package org.springframework.context.annotation;
 
 import java.util.Set;
 
+import org.w3c.dom.Element;
+
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.parsing.BeanComponentDefinition;
 import org.springframework.beans.factory.parsing.CompositeComponentDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
-import org.w3c.dom.Element;
 
 /**
  * Parser for the &lt;context:annotation-config/&gt; element.
@@ -39,21 +40,23 @@ public class AnnotationConfigBeanDefinitionParser implements BeanDefinitionParse
 
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
 		Object source = parserContext.extractSource(element);
-		Set<BeanDefinitionHolder> processorDefinitions = AnnotationConfigUtils.registerAnnotationConfigProcessors(
-				parserContext.getRegistry(), source);
 
-		// register component for the surrounding context:annotation-config element 
+		// Obtain bean definitions for all relevant BeanPostProcessors.
+		Set<BeanDefinitionHolder> processorDefinitions =
+				AnnotationConfigUtils.registerAnnotationConfigProcessors(parserContext.getRegistry(), source);
+
+		// Register component for the surrounding <context:annotation-config> element.
 		CompositeComponentDefinition compDefinition = new CompositeComponentDefinition(element.getTagName(), source);
 		parserContext.pushContainingComponent(compDefinition);
-		
-		// nest the concrete beans in the surrounding component 
+
+		// Nest the concrete beans in the surrounding component.
 		for (BeanDefinitionHolder processorDefinition : processorDefinitions) {
 			parserContext.registerComponent(new BeanComponentDefinition(processorDefinition));
 		}
-		
-		// finally register the composite component
+
+		// Finally register the composite component.
 		parserContext.popAndRegisterContainingComponent();
-		
+
 		return null;
 	}
 
