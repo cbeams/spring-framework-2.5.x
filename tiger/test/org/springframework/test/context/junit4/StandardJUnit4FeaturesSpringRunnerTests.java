ba999feee656651b07f84735031d80d6ab369252
Juergen Hoeller <jhoeller@interface21.com>
2008-01-14 14:57:03 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@15945 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/test/context/junit4/StandardJUnit4FeaturesSpringRunnerTests.java
+++ tiger/test/org/springframework/test/context/junit4/StandardJUnit4FeaturesSpringRunnerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,7 +36,9 @@ import org.springframework.test.context.TestExecutionListeners;
  * @see StandardJUnit4FeaturesTests
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@TestExecutionListeners( {})
+@TestExecutionListeners({})
 public class StandardJUnit4FeaturesSpringRunnerTests extends StandardJUnit4FeaturesTests {
-	/* all tests are in the parent class. */
+
+	/* All tests are in the parent class... */
+
 }
