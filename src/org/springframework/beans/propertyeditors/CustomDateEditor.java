6d616d835047a6836e3f6f0e22dbd466d184216f
Juergen Hoeller <jhoeller@interface21.com>
2008-01-26 23:24:37 +0000

    CustomDateEditor and CustomNumberEditor preserve original ParseException as root cause of IllegalArgumentException
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16014 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/propertyeditors/CustomDateEditor.java
+++ src/org/springframework/beans/propertyeditors/CustomDateEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -106,7 +106,10 @@ public class CustomDateEditor extends PropertyEditorSupport {
 				setValue(this.dateFormat.parse(text));
 			}
 			catch (ParseException ex) {
-				throw new IllegalArgumentException("Could not parse date: " + ex.getMessage());
+				IllegalArgumentException iae =
+						new IllegalArgumentException("Could not parse date: " + ex.getMessage());
+				iae.initCause(ex);
+				throw iae;
 			}
 		}
 	}
