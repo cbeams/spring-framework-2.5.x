9c821ff509d5cb193a453a195bc1e2ff38a49f6d
Juergen Hoeller <jhoeller@interface21.com>
2008-01-26 23:27:47 +0000

    made exception message consistent with CustomDateEditor
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16015 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/NumberUtils.java
+++ src/org/springframework/util/NumberUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -190,7 +190,7 @@ public abstract class NumberUtils {
 				return convertNumberToTargetClass(number, targetClass);
 			}
 			catch (ParseException ex) {
-				throw new IllegalArgumentException(ex.getMessage());
+				throw new IllegalArgumentException("Could not parse number: " + ex.getMessage());
 			}
 		}
 		else {
