0404e57800f16e6726265217acc4d23ab6dca207
Juergen Hoeller <jhoeller@interface21.com>
2008-01-28 15:38:38 +0000

    consistently send "no-cache" value instead of "No-cache"
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16031 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/support/WebContentGenerator.java
+++ src/org/springframework/web/servlet/support/WebContentGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -243,7 +243,7 @@ public abstract class WebContentGenerator extends WebApplicationObjectSupport {
 	 * See <code>http://www.mnot.net/cache_docs</code>.
 	 */
 	protected final void preventCaching(HttpServletResponse response) {
-		response.setHeader(HEADER_PRAGMA, "No-cache");
+		response.setHeader(HEADER_PRAGMA, "no-cache");
 		if (this.useExpiresHeader) {
 			// HTTP 1.0 header
 			response.setDateHeader(HEADER_EXPIRES, 1L);
