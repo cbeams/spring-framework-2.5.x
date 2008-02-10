f73d6a4a50d68b5739e338e03b9b791491e2109e
Juergen Hoeller <jhoeller@interface21.com>
2008-02-10 21:08:13 +0000

    revised javadoc
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16135 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/instrument/classloading/LoadTimeWeaver.java
+++ tiger/src/org/springframework/instrument/classloading/LoadTimeWeaver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,40 +20,43 @@ import java.lang.instrument.ClassFileTransformer;
 
 /**
  * Defines the contract for adding one or more
- * {@link ClassFileTransformer ClassFileTransformers} to a
- * {@link ClassLoader} - typically the current context class loader.
- * 
- * <p>Implementations may of course provide their own class loader as well.
+ * {@link ClassFileTransformer ClassFileTransformers} to a {@link ClassLoader}.
+ *
+ * <p>Implementations may operate on the current context <code>ClassLoader</code>
+ * or expose their own instrumentable <code>ClassLoader</code>.
  *
  * @author Rod Johnson
  * @author Costin Leau
  * @since 2.0
+ * @see java.lang.instrument.ClassFileTransformer
  */
 public interface LoadTimeWeaver {
 
 	/**
-	 * Add a class file transformer to be applied by this load-time weaver.
-	 * @param transformer the class file transformer to add
+	 * Add a <code>ClassFileTransformer</code> to be applied by this
+	 * <code>LoadTimeWeaver</code>.
+	 * @param transformer the <code>ClassFileTransformer</code> to add
 	 */
 	void addTransformer(ClassFileTransformer transformer);
-	
+
 	/**
-	 * Return a class loader that supports instrumentation through AspectJ-style load-time
-	 * weaving based on user-defined {@link ClassFileTransformer ClassFileTransformers}.
-	 * <p>May be the current class loader, or a class loader created by this
-	 * {@link LoadTimeWeaver} instance.
-	 * @return a class loader that supports instrumentation through AspectJ-style load-time
-	 * weaving based on user-defined {@link ClassFileTransformer ClassFileTransformers}
+	 * Return a <code>ClassLoader</code> that supports instrumentation
+	 * through AspectJ-style load-time weaving based on user-defined
+	 * {@link ClassFileTransformer ClassFileTransformers}.
+	 * <p>May be the current <code>ClassLoader</code>, or a <code>ClassLoader</code>
+	 * created by this {@link LoadTimeWeaver} instance.
+	 * @return the <code>ClassLoader</code> which will expose
+	 * instrumented classes according to the registered transformers
 	 */
 	ClassLoader getInstrumentableClassLoader();
-	
+
 	/**
-	 * Return a throwaway class loader, enabling classes to be loaded and
-	 * inspected without affecting the parent class loader.
+	 * Return a throwaway <code>ClassLoader</code>, enabling classes to be
+	 * loaded and inspected without affecting the parent <code>ClassLoader</code>.
 	 * <p>Should <i>not</i> return the same instance of the {@link ClassLoader}
 	 * returned from an invocation of {@link #getInstrumentableClassLoader()}.
-	 * @return a temporary throwaway class loader; should return a new
-	 * instance for each call, with no existing state
+	 * @return a temporary throwaway <code>ClassLoader</code>; should return
+	 * a new instance for each call, with no existing state
 	 */
 	ClassLoader getThrowawayClassLoader();
 
