2ca37294b2f5cc26facb149d5004080fbe4ffa9f
Juergen Hoeller <jhoeller@interface21.com>
2008-02-16 22:11:52 +0000

    deprecated ClassLoaderAnalyzerInterceptor and associated ClassLoaderUtils class
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16164 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/ClassLoaderUtils.java
+++ src/org/springframework/util/ClassLoaderUtils.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2008 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +23,7 @@ package org.springframework.util;
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @since 02 April 2001
+ * @deprecated as of Spring 2.5, to be removed in Spring 3.0
  * @see java.lang.ClassLoader
  */
 public abstract class ClassLoaderUtils {
