b55fa79ae1f0dccb9803696386de3395d1396040
Juergen Hoeller <jhoeller@interface21.com>
2008-02-16 22:06:52 +0000

    deprecated RefreshablePagedListHolder class and associated PagedListSourceProvider interface
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16163 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/support/PagedListSourceProvider.java
+++ src/org/springframework/beans/support/PagedListSourceProvider.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2008 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,10 +21,11 @@ import java.util.Locale;
 
 /**
  * Callback that provides the source for a reloadable List.
- * Used by RefreshablePagedListHolder.
+ * Used by {@link RefreshablePagedListHolder}.
  *
- * @author Jean-Pierre PAWLAK
+ * @author Jean-Pierre Pawlak
  * @author Juergen Hoeller
+ * @deprecated as of Spring 2.5, to be removed in Spring 3.0
  * @see org.springframework.beans.support.RefreshablePagedListHolder#setSourceProvider
  */
 public interface PagedListSourceProvider {
@@ -42,6 +43,6 @@ public interface PagedListSourceProvider {
 	 * @see org.springframework.beans.support.RefreshablePagedListHolder#setLocale
 	 * @see org.springframework.beans.support.RefreshablePagedListHolder#setFilter
 	 */
-	public List loadList(Locale locale, Object filter);
+	List loadList(Locale locale, Object filter);
 
 }
