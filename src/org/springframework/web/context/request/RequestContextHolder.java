b8d19288a08ed2b0b44cbbd1971faf2073cac9b0
Juergen Hoeller <jhoeller@interface21.com>
2008-02-19 21:29:10 +0000

    refined exception message
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16195 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/context/request/RequestContextHolder.java
+++ src/org/springframework/web/context/request/RequestContextHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,8 +100,9 @@ public abstract class RequestContextHolder  {
 		RequestAttributes attributes = getRequestAttributes();
 		if (attributes == null) {
 			throw new IllegalStateException("No thread-bound request found: " +
-					"Are you referring to request attributes outside of an actual web request? " +
-					"If you are actually operating within a web request and still receive this message," +
+					"Are you referring to request attributes outside of an actual web request, " +
+					"or processing a request outside of the originally receiving thread? " +
+					"If you are actually operating within a web request and still receive this message, " +
 					"your code is probably running outside of DispatcherServlet/DispatcherPortlet: " +
 					"In this case, use RequestContextListener or RequestContextFilter to expose the current request.");
 		}
