077485c3d6396073ae3e8395e011302ae5d5667e
Juergen Hoeller <jhoeller@interface21.com>
2008-02-19 15:57:58 +0000

    check ActionResponse (safer bet) for action/render logging
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16194 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/portlet/DispatcherPortlet.java
+++ src/org/springframework/web/portlet/DispatcherPortlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -944,7 +944,7 @@ public class DispatcherPortlet extends FrameworkPortlet {
 			pageNotFoundLogger.warn("No mapping found for current request " +
 					"in DispatcherPortlet with name '" + getPortletName() + "'" +
 					", mode '" + request.getPortletMode() + "'" +
-					", type '" + (request instanceof ActionRequest ? "action" : "render") + "'" +
+					", type '" + (response instanceof ActionResponse ? "action" : "render") + "'" +
 					", session '" + request.getRequestedSessionId() + "'" +
 					", user '" + getUsernameForRequest(request) + "'");
 		}
