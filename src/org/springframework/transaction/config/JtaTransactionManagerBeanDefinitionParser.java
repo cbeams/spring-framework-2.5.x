3851433e6da8b955f368d6bc4d14645c20157a52
Juergen Hoeller <jhoeller@interface21.com>
2008-02-25 20:25:38 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16224 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/transaction/config/JtaTransactionManagerBeanDefinitionParser.java
+++ src/org/springframework/transaction/config/JtaTransactionManagerBeanDefinitionParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,26 +16,22 @@
 
 package org.springframework.transaction.config;
 
+import org.w3c.dom.Element;
+
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.util.ClassUtils;
-import org.w3c.dom.Element;
 
 /**
  * Parser for the &lt;tx:jta-transaction-manager/&gt; element,
  * autodetecting BEA WebLogic, IBM WebSphere and Oracle OC4J.
  *
  * @author Juergen Hoeller
+ * @author Christian Dupuis
  * @since 2.5
  */
 public class JtaTransactionManagerBeanDefinitionParser extends AbstractSingleBeanDefinitionParser  {
-
-	private static final String JTA_TRANSACTION_MANAGER_CLASS_NAME =
-			"org.springframework.transaction.jta.JtaTransactionManager";
-
-	private static final String OC4J_TRANSACTION_MANAGER_CLASS_NAME =
-			"org.springframework.transaction.jta.OC4JJtaTransactionManager";
 
 	private static final String WEBLOGIC_JTA_TRANSACTION_MANAGER_CLASS_NAME =
 			"org.springframework.transaction.jta.WebLogicJtaTransactionManager";
@@ -43,8 +39,15 @@ public class JtaTransactionManagerBeanDefinitionParser extends AbstractSingleBea
 	private static final String WEBSPHERE_TRANSACTION_MANAGER_CLASS_NAME =
 			"org.springframework.transaction.jta.WebSphereUowTransactionManager";
 
+	private static final String OC4J_TRANSACTION_MANAGER_CLASS_NAME =
+			"org.springframework.transaction.jta.OC4JJtaTransactionManager";
+
+	private static final String JTA_TRANSACTION_MANAGER_CLASS_NAME =
+			"org.springframework.transaction.jta.JtaTransactionManager";
+
 	public static final String DEFAULT_TRANSACTION_MANAGER_BEAN_NAME =
 			AnnotationDrivenBeanDefinitionParser.DEFAULT_TRANSACTION_MANAGER_BEAN_NAME;
+
 
 	private static final boolean weblogicPresent = ClassUtils.isPresent("weblogic.transaction.UserTransaction");
 
