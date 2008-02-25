1276806bb147c0eb2be3e91e237005af51c56c7c
Juergen Hoeller <jhoeller@interface21.com>
2008-02-25 14:18:06 +0000

    reset thread-bound request attributes
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16218 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/context/annotation/ClassPathBeanDefinitionScannerScopeTests.java
+++ tiger/test/org/springframework/context/annotation/ClassPathBeanDefinitionScannerScopeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,8 +62,12 @@ public class ClassPathBeanDefinitionScannerScopeTests extends TestCase {
 		newRequestWithSession.setSession(new MockHttpSession());
 		this.newRequestAttributesWithSession = new ServletRequestAttributes(newRequestWithSession);
 	}
-	
-	
+
+	protected void tearDown() throws Exception {
+		RequestContextHolder.setRequestAttributes(null);
+	}
+
+
 	public void testSingletonScopeWithNoProxy() {
 		RequestContextHolder.setRequestAttributes(oldRequestAttributes);
 		ApplicationContext context = createContext(ScopedProxyMode.NO);
