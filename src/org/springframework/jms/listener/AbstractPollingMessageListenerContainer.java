928ac2773c58e84366c79f5e988027e77eec955e
Juergen Hoeller <jhoeller@interface21.com>
2008-03-08 17:06:05 +0000

    propagate JMSExceptions thrown from a listener, potentially triggering recovery
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16303 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jms/listener/AbstractPollingMessageListenerContainer.java
+++ src/org/springframework/jms/listener/AbstractPollingMessageListenerContainer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -316,6 +316,11 @@ public abstract class AbstractPollingMessageListenerContainer extends AbstractMe
 						status.setRollbackOnly();
 					}
 					handleListenerException(ex);
+					// Rethrow JMSException to indicate an infrastructure problem
+					// that may have to trigger recovery...
+					if (ex instanceof JMSException) {
+						throw (JMSException) ex;
+					}
 				}
 				return true;
 			}
