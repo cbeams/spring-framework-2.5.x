c117a0883923604e82a2950584573f98b8808231
Juergen Hoeller <jhoeller@interface21.com>
2008-03-20 11:52:47 +0000

    fixed "testNoBusinessInterfaceSpecified" to actually use LocalStatelessSessionProxyFactoryBean
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16392 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBeanTests.java
+++ test/org/springframework/ejb/access/LocalStatelessSessionProxyFactoryBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -135,7 +135,7 @@ public class LocalStatelessSessionProxyFactoryBeanTests extends TestCase {
 			} 
 		};
 
-		SimpleRemoteStatelessSessionProxyFactoryBean fb = new SimpleRemoteStatelessSessionProxyFactoryBean();
+		LocalStatelessSessionProxyFactoryBean fb = new LocalStatelessSessionProxyFactoryBean();
 		fb.setJndiName(jndiName);
 		fb.setResourceRef(true);
 		// Don't set business interface
