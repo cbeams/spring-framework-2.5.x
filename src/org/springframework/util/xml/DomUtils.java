05432d81525e05902e3520e5e41b565c82f3a223
Juergen Hoeller <jhoeller@interface21.com>
2008-03-23 22:41:08 +0000

    added "getChildElementsByTagNames" method with tag names Collection argument to DomUtils
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16407 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/util/xml/DomUtils.java
+++ src/org/springframework/util/xml/DomUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
 package org.springframework.util.xml;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import org.w3c.dom.CharacterData;
@@ -42,6 +44,31 @@ import org.springframework.util.Assert;
 public abstract class DomUtils {
 
 	/**
+	 * Retrieve all child elements of the given DOM element that match any of
+	 * the given element names. Only look at the direct child level of the
+	 * given element; do not go into further depth (in contrast to the
+	 * DOM API's <code>getElementsByTagName</code> method).
+	 * @param ele the DOM element to analyze
+	 * @param childEleNames the child element names to look for
+	 * @return a List of child <code>org.w3c.dom.Element</code> instances
+	 * @see org.w3c.dom.Element
+	 * @see org.w3c.dom.Element#getElementsByTagName
+	 */
+	public static List getChildElementsByTagNames(Element ele, Collection childEleNames) {
+		Assert.notNull(ele, "Element must not be null");
+		Assert.notNull(childEleNames, "Element names collection must not be null");
+		NodeList nl = ele.getChildNodes();
+		List childEles = new ArrayList();
+		for (int i = 0; i < nl.getLength(); i++) {
+			Node node = nl.item(i);
+			if (node instanceof Element && nodeNameMatch(node, childEleNames)) {
+				childEles.add(node);
+			}
+		}
+		return childEles;
+	}
+
+	/**
 	 * Retrieve all child elements of the given DOM element that match
 	 * the given element name. Only look at the direct child level of the
 	 * given element; do not go into further depth (in contrast to the
@@ -53,15 +80,7 @@ public abstract class DomUtils {
 	 * @see org.w3c.dom.Element#getElementsByTagName
 	 */
 	public static List getChildElementsByTagName(Element ele, String childEleName) {
-		NodeList nl = ele.getChildNodes();
-		List childEles = new ArrayList();
-		for (int i = 0; i < nl.getLength(); i++) {
-			Node node = nl.item(i);
-			if (node instanceof Element && nodeNameEquals(node, childEleName)) {
-				childEles.add(node);
-			}
-		}
-		return childEles;
+		return getChildElementsByTagNames(ele, Collections.singleton(childEleName));
 	}
 
 	/**
@@ -73,10 +92,12 @@ public abstract class DomUtils {
 	 * or <code>null</code> if none found
 	 */
 	public static Element getChildElementByTagName(Element ele, String childEleName) {
+		Assert.notNull(ele, "Element must not be null");
+		Assert.notNull(childEleName, "Element name must not be null");
 		NodeList nl = ele.getChildNodes();
 		for (int i = 0; i < nl.getLength(); i++) {
 			Node node = nl.item(i);
-			if (node instanceof Element && nodeNameEquals(node, childEleName)) {
+			if (node instanceof Element && nodeNameMatch(node, childEleName)) {
 				return (Element) node;
 			}
 		}
@@ -97,17 +118,6 @@ public abstract class DomUtils {
 	}
 
 	/**
-	 * Namespace-aware equals comparison. Returns <code>true</code> if either
-	 * {@link Node#getLocalName} or {@link Node#getNodeName} equals <code>desiredName</code>,
-	 * otherwise returns <code>false</code>.
-	 */
-	public static boolean nodeNameEquals(Node node, String desiredName) {
-		Assert.notNull(node, "Node must not be null");
-		Assert.notNull(desiredName, "Desired name must not be null");
-		return (desiredName.equals(node.getNodeName()) || desiredName.equals(node.getLocalName()));
-	}
-
-	/**
 	 * Extract the text value from the given DOM element, ignoring XML comments.
 	 * <p>Appends all CharacterData nodes and EntityReference nodes
 	 * into a single String value, excluding Comment nodes.
@@ -116,6 +126,7 @@ public abstract class DomUtils {
 	 * @see Comment
 	 */
 	public static String getTextValue(Element valueEle) {
+		Assert.notNull(valueEle, "Element must not be null");
 		StringBuffer value = new StringBuffer();
 		NodeList nl = valueEle.getChildNodes();
 		for (int i = 0; i < nl.getLength(); i++) {
@@ -125,6 +136,31 @@ public abstract class DomUtils {
 			}
 		}
 		return value.toString();
+	}
+
+	/**
+	 * Namespace-aware equals comparison. Returns <code>true</code> if either
+	 * {@link Node#getLocalName} or {@link Node#getNodeName} equals <code>desiredName</code>,
+	 * otherwise returns <code>false</code>.
+	 */
+	public static boolean nodeNameEquals(Node node, String desiredName) {
+		Assert.notNull(node, "Node must not be null");
+		Assert.notNull(desiredName, "Desired name must not be null");
+		return nodeNameMatch(node, desiredName);
+	}
+
+	/**
+	 * Matches the given node's name and local name against the given desired name.
+	 */
+	private static boolean nodeNameMatch(Node node, String desiredName) {
+		return (desiredName.equals(node.getNodeName()) || desiredName.equals(node.getLocalName()));
+	}
+
+	/**
+	 * Matches the given node's name and local name against the given desired names.
+	 */
+	private static boolean nodeNameMatch(Node node, Collection desiredNames) {
+		return (desiredNames.contains(node.getNodeName()) || desiredNames.contains(node.getLocalName()));
 	}
 
 }
