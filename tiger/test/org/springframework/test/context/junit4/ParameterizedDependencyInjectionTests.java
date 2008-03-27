75815c16adc011983af4c4af55565b4fbcbf22f6
Sam Brannen <sam@sambrannen.com>
2008-03-27 23:23:58 +0000

    Now using @Autowired in favor of implementing ApplicationContextAware.
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16446 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/test/context/junit4/ParameterizedDependencyInjectionTests.java
+++ tiger/test/org/springframework/test/context/junit4/ParameterizedDependencyInjectionTests.java
@@ -36,7 +36,6 @@ import org.springframework.beans.Employee;
 import org.springframework.beans.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContextManager;
 import org.springframework.test.context.TestExecutionListeners;
@@ -56,18 +55,18 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 @RunWith(Parameterized.class)
 @ContextConfiguration
 @TestExecutionListeners( { DependencyInjectionTestExecutionListener.class })
-public class ParameterizedDependencyInjectionTests implements ApplicationContextAware {
+public class ParameterizedDependencyInjectionTests {
 
 	private static final List<Employee> employees = new ArrayList<Employee>();
 
+	@Autowired
 	private ApplicationContext applicationContext;
-
-	private final String employeeBeanName;
-
-	private final String employeeName;
 
 	@Autowired
 	private Pet pet;
+
+	private final String employeeBeanName;
+	private final String employeeName;
 
 	private final TestContextManager testContextManager;
 
@@ -94,10 +93,6 @@ public class ParameterizedDependencyInjectionTests implements ApplicationContext
 		this.testContextManager.prepareTestInstance(this);
 	}
 
-	public final void setApplicationContext(final ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
-
 	@Test
 	public final void verifyPetAndEmployee() {
 
@@ -108,13 +103,13 @@ public class ParameterizedDependencyInjectionTests implements ApplicationContext
 		final Employee employee = (Employee) this.applicationContext.getBean(this.employeeBeanName);
 		employees.add(employee);
 		assertEquals("Verifying the name of the employee configured as bean [" + this.employeeBeanName + "].",
-				this.employeeName, employee.getName());
+			this.employeeName, employee.getName());
 	}
 
 	@AfterClass
 	public static void verifyNumParameterizedRuns() {
 		assertEquals("Verifying the number of times the parameterized test method was executed.",
-				employeeData().size(), employees.size());
+			employeeData().size(), employees.size());
 	}
 
 }
