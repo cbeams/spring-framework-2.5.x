43fe711e9977b04ded3c2b99708048ee25b1e2c8
Juergen Hoeller <jhoeller@interface21.com>
2008-04-01 11:52:52 +0000

    JndiDataSourceLookup uses "resourceRef"=true by default (being able to resolve J2EE "jdbc/myDb" ENC names by default)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16476 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/test/org/springframework/orm/jpa/persistenceunit/PersistenceXmlParsingTests.java
+++ tiger/test/org/springframework/orm/jpa/persistenceunit/PersistenceXmlParsingTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,7 +91,7 @@ public class PersistenceXmlParsingTests extends TestCase {
 	public void testExample4() throws Exception {
 		SimpleNamingContextBuilder builder = SimpleNamingContextBuilder.emptyActivatedContextBuilder();
 		DataSource ds = new DriverManagerDataSource();
-		builder.bind("jdbc/MyDB", ds);
+		builder.bind("java:comp/env/jdbc/MyDB", ds);
 
 		PersistenceUnitReader reader = new PersistenceUnitReader(
 				new PathMatchingResourcePatternResolver(), new JndiDataSourceLookup());
