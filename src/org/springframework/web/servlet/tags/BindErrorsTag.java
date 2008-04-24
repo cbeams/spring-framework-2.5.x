836c3ca809879f73c38928add40400864e67304e
Juergen Hoeller <jhoeller@interface21.com>
2008-04-24 09:34:50 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16562 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/web/servlet/tags/BindErrorsTag.java
+++ src/org/springframework/web/servlet/tags/BindErrorsTag.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002-2005 the original author or authors.
- * 
+ * Copyright 2002-2008 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,8 +24,9 @@ import org.springframework.validation.Errors;
 import org.springframework.web.util.ExpressionEvaluationUtils;
 
 /**
- * Evaluates content if there are bind errors for a certain bean.
- * Exports an "errors" variable of type Errors for the given bean.
+ * JSP tag that evaluates content if there are binding errors
+ * for a certain bean. Exports an "errors" variable of type
+ * {@link org.springframework.validation.Errors} for the given bean.
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
@@ -53,13 +54,14 @@ public class BindErrorsTag extends HtmlEscapingAwareTag {
 	 * Return the name of the bean that this tag checks.
 	 */
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 
 	protected final int doStartTagInternal() throws ServletException, JspException {
 		String resolvedName = ExpressionEvaluationUtils.evaluateString("name", this.name, pageContext);
 		this.errors = getRequestContext().getErrors(resolvedName, isHtmlEscape());
+
 		if (this.errors != null && this.errors.hasErrors()) {
 			this.pageContext.setAttribute(ERRORS_VARIABLE_NAME, this.errors, PageContext.REQUEST_SCOPE);
 			return EVAL_BODY_INCLUDE;
@@ -76,10 +78,10 @@ public class BindErrorsTag extends HtmlEscapingAwareTag {
 
 	/**
 	 * Retrieve the Errors instance that this tag is currently bound to.
-	 * Intended for cooperating nesting tags.
+	 * <p>Intended for cooperating nesting tags.
 	 */
 	public final Errors getErrors() {
-		return errors;
+		return this.errors;
 	}
 
 
