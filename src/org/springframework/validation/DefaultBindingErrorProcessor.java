972c2f5d4f0373a952a031052d643bf566cdd5b8
Juergen Hoeller <jhoeller@interface21.com>
2008-05-05 11:48:58 +0000

    register correct "missing field" errors within specified nested path
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16604 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/validation/DefaultBindingErrorProcessor.java
+++ src/org/springframework/validation/DefaultBindingErrorProcessor.java
@@ -53,7 +53,7 @@ public class DefaultBindingErrorProcessor implements BindingErrorProcessor {
 	public void processMissingFieldError(String missingField, BindingResult bindingResult) {
 		// Create field error with code "required".
 		String fixedField = bindingResult.getNestedPath() + missingField;
-		String[] codes = bindingResult.resolveMessageCodes(MISSING_FIELD_ERROR_CODE, fixedField);
+		String[] codes = bindingResult.resolveMessageCodes(MISSING_FIELD_ERROR_CODE, missingField);
 		Object[] arguments = getArgumentsForBindError(bindingResult.getObjectName(), fixedField);
 		bindingResult.addError(new FieldError(
 				bindingResult.getObjectName(), fixedField, "", true,
