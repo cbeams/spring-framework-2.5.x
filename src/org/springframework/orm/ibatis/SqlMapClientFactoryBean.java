2fa0c997c0b3d76c9ba709595a22d330742a3e30
Juergen Hoeller <jhoeller@interface21.com>
2008-05-05 12:04:43 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16613 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/orm/ibatis/SqlMapClientFactoryBean.java
+++ src/org/springframework/orm/ibatis/SqlMapClientFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -267,7 +267,7 @@ public class SqlMapClientFactoryBean implements FactoryBean, InitializingBean {
 
 	public void afterPropertiesSet() throws Exception {
 		if (this.configLocation == null) {
-			throw new IllegalArgumentException("configLocation is required");
+			throw new IllegalArgumentException("Property 'configLocation' is required");
 		}
 
 		if (this.lobHandler != null) {
