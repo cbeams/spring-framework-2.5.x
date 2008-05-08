4cad0184418cfb49cc5e45f8f6288404c71cb867
Juergen Hoeller <jhoeller@interface21.com>
2008-05-08 11:46:34 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16626 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- test/org/springframework/jmx/export/assembler/InterfaceBasedMBeanInfoAssemblerTests.java
+++ test/org/springframework/jmx/export/assembler/InterfaceBasedMBeanInfoAssemblerTests.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -21,10 +21,8 @@ package org.springframework.jmx.export.assembler;
  */
 public class InterfaceBasedMBeanInfoAssemblerTests extends AbstractJmxAssemblerTests {
 
-	protected static final String OBJECT_NAME = "bean:name=testBean4";
-
 	protected String getObjectName() {
-		return OBJECT_NAME;
+		return "bean:name=testBean4";
 	}
 
 	protected int getExpectedOperationCount() {
