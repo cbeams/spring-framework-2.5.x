0817c903be654fa8a3194b72f01f9d98bb0006dc
Juergen Hoeller <jhoeller@interface21.com>
2008-05-15 12:18:00 +0000

    added SimpleHttpServerJaxWsServiceExporter, allowing for exposure through a fully configurable Sun JDK 1.6 HttpServer; added "filters" and "authenticator" property to SimpleHttpServerFactoryBean
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16643 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/remoting/jaxws/SimpleJaxWsServiceExporter.java
+++ tiger/src/org/springframework/remoting/jaxws/SimpleJaxWsServiceExporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,49 +16,36 @@
 
 package org.springframework.remoting.jaxws;
 
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Executor;
-
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.core.task.support.ConcurrentExecutorAdapter;
-
 /**
- * Simple exporter for JAX-WS services, autodetecting annotation service beans
+ * Simple exporter for JAX-WS services, autodetecting annotated service beans
  * (through the JAX-WS {@link javax.jws.WebService} annotation) and exporting
- * them with a configured base address (by default "http://localhost:8080/").
- * The full address for each service will consist of the base address with
- * the service name appended (e.g. "http://localhost:8080/OrderService").
+ * them with a configured base address (by default "http://localhost:8080/")
+ * using the JAX-WS provider's built-in publication support. The full address
+ * for each service will consist of the base address with the service name
+ * appended (e.g. "http://localhost:8080/OrderService").
  *
  * <p>Note that this exporter will only work if the JAX-WS runtime actually
  * supports publishing with an address argument, i.e. if the JAX-WS runtime
  * ships an internal HTTP server. This is the case with the JAX-WS runtime
  * that's inclued in Sun's JDK 1.6 but not with the standalone JAX-WS 2.1 RI.
  *
+ * <p>For explicit configuration of JAX-WS endpoints with Sun's JDK 1.6
+ * HTTP server, consider using {@link SimpleHttpServerJaxWsServiceExporter}!
+ *
  * @author Juergen Hoeller
  * @since 2.5
- * @see javax.xml.ws.Endpoint#publish(String, Object)
+ * @see javax.jws.WebService
+ * @see javax.xml.ws.Endpoint#publish(String)
+ * @see SimpleHttpServerJaxWsServiceExporter
  */
-public class SimpleJaxWsServiceExporter implements BeanFactoryAware, DisposableBean {
+public class SimpleJaxWsServiceExporter extends AbstractJaxWsServiceExporter {
 
 	public static final String DEFAULT_BASE_ADDRESS = "http://localhost:8080/";
 
-
 	private String baseAddress = DEFAULT_BASE_ADDRESS;
-
-	private Executor executor;
-
-	private Map<String, Object> endpointProperties;
-
-	private final Set<Endpoint> publishedEndpoints = new LinkedHashSet<Endpoint>();
 
 
 	/**
@@ -74,70 +61,10 @@ public class SimpleJaxWsServiceExporter implements BeanFactoryAware, DisposableB
 		this.baseAddress = baseAddress;
 	}
 
-	/**
-	 * Set the JDK concurrent executor to use for dispatching incoming requests
-	 * to exported service instances.
-	 * @see javax.xml.ws.Endpoint#setExecutor
-	 */
-	public void setExecutor(Executor executor) {
-		this.executor = executor;
-	}
 
-	/**
-	 * Set the Spring TaskExecutor to use for dispatching incoming requests
-	 * to exported service instances.
-	 * @see javax.xml.ws.Endpoint#setExecutor
-	 */
-	public void setTaskExecutor(TaskExecutor executor) {
-		this.executor = new ConcurrentExecutorAdapter(executor);
-	}
-
-	/**
-	 * Set the property bag for the endpoint, including properties such as
-	 * "javax.xml.ws.wsdl.service" or "javax.xml.ws.wsdl.port"
-	 * @see javax.xml.ws.Endpoint#setProperties
-	 * @see javax.xml.ws.Endpoint#WSDL_SERVICE
-	 * @see javax.xml.ws.Endpoint#WSDL_PORT
-	 */
-	public void setEndpointProperties(Map<String, Object> endpointProperties) {
-		this.endpointProperties = endpointProperties;
-	}
-
-
-	/**
-	 * Obtains all web service beans and publishes them as JAX-WS endpoints.
-	 */
-	public void setBeanFactory(BeanFactory beanFactory) {
-		if (!(beanFactory instanceof ListableBeanFactory)) {
-			throw new IllegalStateException("SimpleJaxWsServiceExporter requires a ListableBeanFactory");
-		}
-		ListableBeanFactory lbf = (ListableBeanFactory) beanFactory;
-		String[] beanNames = lbf.getBeanNamesForType(Object.class, false, false);
-		for (String beanName : beanNames) {
-			Class<?> type = lbf.getType(beanName);
-			WebService annotation = type.getAnnotation(WebService.class);
-			if (annotation != null) {
-				Endpoint endpoint = Endpoint.create(lbf.getBean(beanName));
-				if (this.executor != null) {
-					endpoint.setExecutor(this.executor);
-				}
-				if (this.endpointProperties != null) {
-					endpoint.setProperties(this.endpointProperties);
-				}
-				String fullAddress = this.baseAddress + annotation.serviceName();
-				endpoint.publish(fullAddress);
-				this.publishedEndpoints.add(endpoint);
-			}
-		}
-	}
-
-	/**
-	 * Stops all published endpoints, taking the web services offline.
-	 */
-	public void destroy() {
-		for (Endpoint endpoint : this.publishedEndpoints) {
-			endpoint.stop();
-		}
+	protected void publishEndpoint(Endpoint endpoint, WebService annotation) {
+		String fullAddress = this.baseAddress + annotation.serviceName();
+		endpoint.publish(fullAddress);
 	}
 
 }
