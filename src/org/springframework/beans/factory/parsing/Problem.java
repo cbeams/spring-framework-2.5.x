c9d1e949288dc7220d50b81928170396d7c25757
Juergen Hoeller <jhoeller@interface21.com>
2008-05-19 19:35:08 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/branches/mbranch-2-0@16665 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/parsing/Problem.java
+++ src/org/springframework/beans/factory/parsing/Problem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,14 +80,14 @@ public class Problem {
 	 * Get the message detailing the problem.
 	 */
 	public String getMessage() {
-		return message;
+		return this.message;
 	}
 
 	/**
 	 * Get the location within a bean configuration source that triggered the error.
 	 */
 	public Location getLocation() {
-		return location;
+		return this.location;
 	}
 
 	/**
@@ -110,7 +110,7 @@ public class Problem {
 	 * Get the underlying expection that caused the error (may be <code>null</code>).
 	 */
 	public Throwable getRootCause() {
-		return rootCause;
+		return this.rootCause;
 	}
 
 
