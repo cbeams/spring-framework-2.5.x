baea5d2162151b102ede7d2fa9ebe818204f6e94
Juergen Hoeller <jhoeller@interface21.com>
2008-05-26 22:18:33 +0000

    polishing
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@16690 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/beans/factory/config/TypedStringValue.java
+++ src/org/springframework/beans/factory/config/TypedStringValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ public class TypedStringValue implements BeanMetadataElement {
 
 	/**
 	 * Set the String value.
-	 * Only necessary for manipulating a registered value,
+	 * <p>Only necessary for manipulating a registered value,
 	 * for example in BeanFactoryPostProcessors.
 	 * @see PropertyPlaceholderConfigurer
 	 */
@@ -93,7 +93,7 @@ public class TypedStringValue implements BeanMetadataElement {
 
 	/**
 	 * Set the type to convert to.
-	 * Only necessary for manipulating a registered value,
+	 * <p>Only necessary for manipulating a registered value,
 	 * for example in BeanFactoryPostProcessors.
 	 * @see PropertyPlaceholderConfigurer
 	 */
