ee3a464c292a88ba8fb45fd71b9105e968b70157
Juergen Hoeller <jhoeller@interface21.com>
2008-10-16 17:25:25 +0000

    added @Override annotations
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@17025 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- tiger/src/org/springframework/ui/ExtendedModelMap.java
+++ tiger/src/org/springframework/ui/ExtendedModelMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2008 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,28 +26,34 @@ import java.util.Map;
  * @author Juergen Hoeller
  * @since 2.5.1
  */
+@SuppressWarnings("serial")
 public class ExtendedModelMap extends ModelMap implements Model {
 
+	@Override
 	public ExtendedModelMap addAttribute(String attributeName, Object attributeValue) {
 		super.addAttribute(attributeName, attributeValue);
 		return this;
 	}
 
+	@Override
 	public ExtendedModelMap addAttribute(Object attributeValue) {
 		super.addAttribute(attributeValue);
 		return this;
 	}
 
-	public ExtendedModelMap addAllAttributes(Collection attributeValues) {
+	@Override
+	public ExtendedModelMap addAllAttributes(Collection<?> attributeValues) {
 		super.addAllAttributes(attributeValues);
 		return this;
 	}
 
+	@Override
 	public ExtendedModelMap addAllAttributes(Map<String, ?> attributes) {
 		super.addAllAttributes(attributes);
 		return this;
 	}
 
+	@Override
 	public ExtendedModelMap mergeAttributes(Map<String, ?> attributes) {
 		super.mergeAttributes(attributes);
 		return this;
@@ -57,5 +63,4 @@ public class ExtendedModelMap extends ModelMap implements Model {
 	public Map<String, Object> asMap() {
 		return this;
 	}
-
 }
