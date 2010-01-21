fe5ecc06ec2c225bff4f9fd5639dc928d02d1bcf
Thomas Risberg <trisberg@interface21.com>
2010-01-21 21:27:15 +0000

    fixed toString handling (SPR-5582 backport)
    git-svn-id: https://src.springframework.org/svn/spring-maintenance/trunk@17144 fd5a2b45-1f63-4059-99e9-3c7cb7fd75c8

--- src/org/springframework/jdbc/datasource/TransactionAwareDataSourceProxy.java
+++ src/org/springframework/jdbc/datasource/TransactionAwareDataSourceProxy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 the original author or authors.
+ * Copyright 2002-2010 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -194,6 +194,7 @@ public class TransactionAwareDataSourceProxy extends DelegatingDataSource {
 				else {
 					buf.append(" from DataSource [").append(this.targetDataSource).append("]");
 				}
+				return buf.toString();
 			}
 			else if (method.getName().equals("close")) {
 				// Handle close method: only close if not within a transaction.
